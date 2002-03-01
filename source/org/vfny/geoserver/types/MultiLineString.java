9a03d09c2ce3d8bb4abee4c03bcadfeb103f0d9b
robhranac <robhranac@ef1d6e69-97e5-0310-af46-8a06194da32a>
2002-03-01 23:18:04 +0000

    refactoring
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@18 ef1d6e69-97e5-0310-af46-8a06194da32a

--- source/org/vfny/geoserver/types/MultiLineString.java
+++ source/org/vfny/geoserver/types/MultiLineString.java
@@ -1,10 +1,8 @@
 /* Copyright (c) 2001 Vision for New York - www.vfny.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, availible at the root application directory.
  */
-
 package org.vfny.geoserver.types;
 
-import org.postgresql.util.*;
 import java.sql.*;
 import java.util.*;
 
@@ -16,72 +14,45 @@ import java.util.*;
  * @version 0.9 alpha, 11/01/01
  *
  */
-public class MultiLineString extends Geometry 
-{
+public class MultiLineString extends Geometry {
 
-	LineString[] lines;
+		protected LineString[] lines;
 	
-	public MultiLineString() 
-	{
-		type = MULTILINESTRING;
-	}
+		public MultiLineString() {
+				type = MULTILINESTRING;
+		}
 
-	public MultiLineString(LineString[] lines) 
-	{
-		this();
-		this.lines = lines;
-		dimension = lines[0].dimension;
-	}
-	
-	public MultiLineString(String value) throws SQLException
-	{
-		this();
-		value = value.trim();
-		if ( value.indexOf("MULTILINESTRING") == 0 ) 
-		{
-			PGtokenizer t = new PGtokenizer(PGtokenizer.removePara(value.substring(15).trim()),',');
-			int nlines = t.getSize();
-			lines = new LineString[nlines];
-			for( int p = 0; p < nlines; p++)
-			{
-				lines[p] = new LineString(t.getToken(p));
-			}
-			dimension = lines[0].dimension;
-		} else {
-			throw new SQLException("postgis.multilinestringgeometry");
+		public MultiLineString(LineString[] lines) {
+				this();
+				this.lines = lines;
+				dimension = lines[0].dimension;
 		}
-	}
 	
-	public String toString() 
-	{
-		return "MULTILINESTRING " + getValue();
-	}
+		public String toString() {
+				return "MULTILINESTRING " + getValue();
+		}
 
-	public String getValue() 
-	{
-		StringBuffer b = new StringBuffer("(");
-		for( int p = 0; p < lines.length; p++ )
-		{
-			if( p > 0 ) b.append(",");
-			b.append(lines[p].getValue());
+		public String getValue() {
+				StringBuffer b = new StringBuffer("(");
+				for( int p = 0; p < lines.length; p++ ) {
+						if( p > 0 ) b.append(",");
+						b.append(lines[p].getValue());
+				}
+				b.append(")");
+				return b.toString();
 		}
-		b.append(")");
-		return b.toString();
-	}
 	
-	public int numLines() 
-	{
-		return lines.length;
-	}
+		public int numLines() {
+				return lines.length;
+		}
 	
-	public LineString getLine(int idx) 
-	{
-		if( idx >= 0 & idx < lines.length ) {
-			return lines[idx];
+		public LineString getLine(int idx) {
+				if( idx >= 0 & idx < lines.length ) {
+						return lines[idx];
+				}
+				else {
+						return null;
+				}
 		}
-		else {
-			return null;
-		}
-	}
 
 }
