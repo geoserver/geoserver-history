024d3572c67c6167d8919d200c1483f8357d526a
robhranac <robhranac@ef1d6e69-97e5-0310-af46-8a06194da32a>
2002-08-22 22:30:02 +0000

    test commit
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@41 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0ad9e0bfe267f6605b0e0ffc166b5353252d8aef [formerly 385ecc1e506be76b699ca19cec0c317129f039a9]
    Former-commit-id: 1b6e14c960a364d2b22917c6fbf806420b9c288f

--- geoserver/src/org/vfny/geoserver/requests/Authenticator.java
+++ geoserver/src/org/vfny/geoserver/requests/Authenticator.java
@@ -13,70 +13,70 @@ import org.apache.log4j.Category;
 /**
  * Assists in the authentication of IP addresses by this server. 
  *
- * @author Rob Hranac, Vision for New York
+ * @author Rob Hranac, TOPP
  * @version $0.9 beta, 11/01/01$
  */
 public class Authenticator {
 
 
-		// THIS CLASS IS TOTALLY UNIMPLEMENTED
-		// JUST A SMALL EXPERIMENT THAT WILL LATER BE INTEGRATED
-
-		/** Standard logging instance for the class */
-		private Category _log = Category.getInstance( Authenticator.class.getName() );
-
-		/** Creates a list of feature types allowed for each IP */
-		private Hashtable featureTypes = new Hashtable();
-
-		/** Creates a list of hosts allowed for this server installation */
-		private Hashtable allowedHosts = new Hashtable();
+    // THIS CLASS IS TOTALLY UNIMPLEMENTED
+    // JUST A SMALL EXPERIMENT THAT WILL LATER BE INTEGRATED
+    
+    /** Standard logging instance for the class */
+    private Category _log = Category.getInstance( Authenticator.class.getName() );
+    
+    /** Creates a list of feature types allowed for each IP */
+    private Hashtable featureTypes = new Hashtable();
+    
+    /** Creates a list of hosts allowed for this server installation */
+    private Hashtable allowedHosts = new Hashtable();
 		
-
-	 /**
-		* Enpty constuctor.
-		*
-		*/ 
-		public Authenticator() {
-				allowedHosts.put( "10.254.0.29", "ALLOWED");
-				
-		}
-
-
-	 /**
-		* Checks to see if a given IP is allowed to access the layers on this server.
-		*
-		* @param ipAddress The servlet request object.
-		*/
-		public boolean isAllowed(String ipAddress) {
-				if( allowedHosts.containsKey(ipAddress) ) {
-						return true;
-				}
-				else {
-						return false;
-				}
-		}
-
-
-	 /**
-		* Checks to see if a given IP is allowed to access specific feature types on this server.
-		*
-		* @param ipAddress The servlet request object.
-		* @param featureType The servlet response object.
-		*/
-		public boolean isAllowed(String ipAddress, String featureType) {
-				//((Hashtable) allowedHosts) = featureTypes.get(featureType);
-				if( allowedHosts != null ) {
-						if( allowedHosts.containsKey(ipAddress) ) {
-								return true;
-						}
-						else
-								return false;
-				}
-				else
-						return false;
-
-		}
-
+    
+    /**
+     * Enpty constuctor.
+     *
+     */ 
+    public Authenticator() {
+        allowedHosts.put( "10.254.0.29", "ALLOWED");
+        
+    }
+    
+    
+    /**
+     * Checks to see if a given IP is allowed to access the layers on this server.
+     *
+     * @param ipAddress The servlet request object.
+     */
+    public boolean isAllowed(String ipAddress) {
+        if( allowedHosts.containsKey(ipAddress) ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    
+    /**
+     * Checks to see if a given IP is allowed to access specific feature types on this server.
+     *
+     * @param ipAddress The servlet request object.
+     * @param featureType The servlet response object.
+     */
+    public boolean isAllowed(String ipAddress, String featureType) {
+        //((Hashtable) allowedHosts) = featureTypes.get(featureType);
+        if( allowedHosts != null ) {
+            if( allowedHosts.containsKey(ipAddress) ) {
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+        
+    }
+    
 }
 
 
