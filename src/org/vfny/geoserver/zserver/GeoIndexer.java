68c5bfc7928ecccb15a2f12470dbbdb750b14b18
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2002-11-22 22:13:48 +0000

    80 lines and logger tweaks
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@84 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: dfd907759e5bd9f26ec5ae79c1de0c76af45a0f5 [formerly d7b4aba79c69acec8ee3687ee2cae8e84e1ea7a3]
    Former-commit-id: e18db259c97ce979c8eaa7cf24860dae852f281c

--- src/org/vfny/geoserver/zserver/GeoIndexer.java
+++ src/org/vfny/geoserver/zserver/GeoIndexer.java
@@ -12,8 +12,6 @@ import org.apache.lucene.document.DateField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.index.IndexReader;
-//import org.apache.lucene.analysis.SimpleAnalyzer;
-//import org.apache.lucene.analysis.StopAnalyzer;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.index.Term;
@@ -76,7 +74,8 @@ public class GeoIndexer {
     /**
      * Constructor to initializes indexer.
      *
-     * @param serverProps should contain datafolder, fieldmap and database keys.
+     * @param serverProps should contain datafolder, 
+     * fieldmap and database keys.
      */
     public GeoIndexer(Properties serverProps) {
 	this.serverProps = serverProps;
@@ -85,7 +84,8 @@ public class GeoIndexer {
 	//searcher = new IndexSearcher(pathToIndex);
 	try {
 	    dataFolder = new File(serverProps.getProperty("datafolder"));
-	    FileInputStream fis = new FileInputStream(serverProps.getProperty("fieldmap"));
+	    FileInputStream fis = 
+		new FileInputStream(serverProps.getProperty("fieldmap"));
 	    attrMap = new Properties();
 	    attrMap.load(fis);
 	} catch (FileNotFoundException e) {
@@ -103,19 +103,19 @@ public class GeoIndexer {
      */
     public int update() {
 	try {
-        Set currentFiles = new HashSet();
-	List docsToAdd = new ArrayList();
-	
-	
-	if (dataFolder.isDirectory()) {
-	    //DirectoryFilter dirFilter = new DirectoryFilter();
+	    Set currentFiles = new HashSet();
+	    List docsToAdd = new ArrayList();
+	    
+	    if (dataFolder.isDirectory()) {
+		//DirectoryFilter dirFilter = new DirectoryFilter();
 	    File[] dirFiles = dataFolder.listFiles();
 	   	    IndexSearcher searcher = null;
 	       try {
-		   
+		   LOGGER.finer("using index at " + pathToIndex);
 		   searcher = new IndexSearcher(pathToIndex);
 	       } catch (IOException e) {
-		   LOGGER.info("No index found at: " + pathToIndex + ".  Creating new index" );
+		   LOGGER.info("No zserver index found at: " + pathToIndex + 
+			       ".  Creating new index" );
 	       }		
 	        for (int i = 0; i < dirFiles.length; i++) {
 		    File curDir = dirFiles[i];
@@ -128,39 +128,43 @@ public class GeoIndexer {
 		    //try {
 		    if (searcher != null) {
 			//search to see if this file is already in the index
-			Term searchTerm = new Term("path", path);
+			Term searchTerm = new Term(PATH_FIELD, path);
 			Query searchQuery = new TermQuery(searchTerm);
-			LOGGER.finer("search term is " + searchTerm + " query is " + searchQuery);
 			hits = searcher.search(searchQuery);
-			//hits = searcher.search(new TermQuery(new Term(PATH_FIELD, path)));
+			
 		    }
-			//} catch (IOException e) {
-			//LOGGER.(e.getMessage());
-			//}		    
+		    
 		    try {
 			if (hits != null && hits.length() > 0) {
 			    Document doc = hits.doc(0);
-			    long docModified = DateField.stringToTime(hits.doc(0).get(MODIFIED_FIELD));
+			    long docModified = 
+				DateField.stringToTime
+				(hits.doc(0).get(MODIFIED_FIELD));
 			    //if it is check the modification time.
 			    if (fileModified > docModified) {
-				LOGGER.finer("file at " + path + "has been modified recently");
+				LOGGER.finer("file at " + path + 
+					     "has been modified recently");
 				//if modified then put in the docs to add.
-				docsToAdd.add(XMLDocument.Document(curFile, attrMap));
-				//don't add it to current files so that the old one will be
-				//deleted.
+				docsToAdd.add
+				    (XMLDocument.Document(curFile, attrMap));
+				//don't add it to current files so that 
+				//the old one will be deleted.
 			    } else {
-				//if it hasn't been modified, then it goes in the
-				//the list of files currently stored by the index.
-				LOGGER.info("adding path " + path + " to currentFiles");
+			     //if it hasn't been modified, then it goes in the
+			     //the list of files currently stored by the index.
+				LOGGER.finer("adding path " + path + 
+					     " to currentFiles");
 				currentFiles.add(path);
 			    }
 			} else {
-			    //no hits, the file isn't in the index, process and add.
-			    docsToAdd.add(XMLDocument.Document(curFile, attrMap));
+		       //no hits, the file isn't in the index, process and add.
+			    docsToAdd.add
+				(XMLDocument.Document(curFile, attrMap));
 			}
 		    } catch (Exception e) {
 			LOGGER.severe("Problem with storing xml document " 
-				      + path + " : " + e.getMessage() + "\nstack: ");
+				      + path + " : " + e.getMessage() + 
+				      "\nstack: ");
 			StackTraceElement[] trace = e.getStackTrace();
 			for (int j = 0; j < trace.length; j++) {
 			    LOGGER.finer(trace[j].toString());
@@ -169,17 +173,17 @@ public class GeoIndexer {
 		}
 		}
 		boolean createNew;
-		if (searcher != null) {   //if we were able to open the searcher
+		if (searcher != null) { //if we were able to open the searcher
 		    searcher.close();  //then close it
 		    createNew = false; //and don't create a new index.
-		} else {  //if not then there is not an index at the path given.
+		} else { //if not then there is not an index at the path given.
 		    createNew = true; //so create a new index.
 		}
-			
 		addDocs(docsToAdd, createNew, currentFiles);
 		return docsToAdd.size();
 		} else {
-	    LOGGER.warning("The datafolder server property : " + dataFolder + " is not valid");
+	    LOGGER.warning("The datafolder server property : " 
+			   + dataFolder + " is not valid");
 	    return 0;
 	}
 
@@ -192,9 +196,9 @@ public class GeoIndexer {
 
 
     /**
-     * Helper method to update.  Handles iterating through currently stored documents
-     * to see if any should be added or deleted, and then adds the appropriate 
-     * documents.  Also creates a new index if needed.
+     * Helper method to update.  Handles iterating through currently 
+     * stored documents to see if any should be added or deleted, and then 
+     * adds the appropriate documents.  Also creates a new index if needed.
      * @param docsToAdd a list of the document paths to add to the index.
      * @param searcher The index searcher used.
      * @param currentFiles The set of files that should not be deleted.
@@ -211,10 +215,10 @@ public class GeoIndexer {
 		for (int i = 0; i < max; i++) {
 		    if (!reader.isDeleted(i)) {
 			Document curDoc = reader.document(i);
-			String docPath = curDoc.get("path");
+			String docPath = curDoc.get(PATH_FIELD);
 			if (!currentFiles.contains(docPath)) {
 				//if the path that this was indexed from is
-				//not in our list of current files then delete it.
+			     //not in our list of current files then delete it.
 			    reader.delete(i);
 			    LOGGER.finer("updating doc at " + docPath);
 			}
