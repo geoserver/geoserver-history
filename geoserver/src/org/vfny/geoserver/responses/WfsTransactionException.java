af5f65a135e3701441580642b547d6abb59b3b42
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2002-12-18 19:12:14 +0000

    initial commit
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@92 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7d6bb5b119129e30d5e56e144a4300dbad893b8a [formerly 9d057631c05bb62d2df54db37a9616c2d11ba455]
    Former-commit-id: d4947e7db325985151c85bb2d8fd54c35bc99895

---
+++ geoserver/src/org/vfny/geoserver/responses/WfsTransactionException.java
@@ -0,0 +1,97 @@
+/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root 
+ * application directory.
+ */
+package org.vfny.geoserver.responses;
+
+import java.util.logging.Logger;
+
+/**
+ * This exception can turn itself into the appropriate WFS_TransactionResponse
+ * xml, allowing transaction classes to throw exceptions that can be reported
+ * back to the clients.
+ * 
+ * @author Chris Holmes, TOPP
+ * @version $VERSION$
+ */
+public class WfsTransactionException extends WfsException {
+    
+    /** the standard exception that was thrown */
+    protected Exception standardException = new Exception();
+    
+    /** handle of the transaction request */
+    protected String handle = new String();
+
+    /** Class logger */
+    private static Logger LOGGER =  
+        Logger.getLogger("org.vfny.geoserver.responses");
+        
+    /** Empty constructor. */
+    public WfsTransactionException () { super(); }
+
+   /**
+     * Constructor for message and handle.
+     * @param message indicates to the user what went wrong.
+     * @param handle the string of the transaction that failed.
+     */
+    public WfsTransactionException (String message, String handle) {
+        super( message );
+	this.handle = handle;
+    }
+    
+    
+    /**
+     * Constructor for an exception and a handle.
+     * @param message indicates to the user what went wrong.
+     * @param handle the string of the transaction that failed.
+     */
+    public WfsTransactionException (Exception e, String handle) {
+        super( e.getMessage() );
+	this.handle = handle;
+    }
+    
+    
+    /**
+     * @param message indicates to the user what went wrong.
+     * @param locator The message for the .
+     * @param handle the string of the transaction that failed.
+     */
+    public WfsTransactionException (String message, 
+				    String locator, String handle) {
+        super(message, locator);
+	this.handle = handle;
+    }
+    
+    /**
+     * Constructor for an exception, messages and handles.
+     * @param e the root exception.
+     * @param preMessage more information about exception.
+     * @param message indicates to the user what went wrong.
+     * @param handle the string of the transaction that failed.
+     */
+    public WfsTransactionException (Exception e, String preMessage, 
+				    String locator, String handle) {
+        super(e, preMessage, locator);
+	this.handle = handle;
+    }
+
+    /**
+     * sets the handle, can be used when the initial handle is wrong.
+     */    
+    public void setHandle(String handle){
+	this.handle = handle;
+    }
+    
+    
+    /**
+     * Returns a WFS_TransactionResponse xml string indicating the failure.
+     */
+    public String getXmlResponse () {
+        String returnXml;
+	WfsTransResponse response = 
+	    new WfsTransResponse(WfsTransResponse.FAILED, handle);
+	response.setLocator(locator);
+	response.setMessage(this.preMessage + ": " + this.getMessage());
+	return response.getXmlResponse();
+    }    
+}
