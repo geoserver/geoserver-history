30717a074ce940be706c99a8860ffaaec923e51a
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-01-07 16:51:30 +0000

    got rid of featureMember, now uses typeRepository
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@129 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 85f75eee7d37e2f4a4b61cb70bf8cd8b9a7edb50 [formerly 5d2b7fc5932843e47612d4dfeb54c91ba47c4847]
    Former-commit-id: 2f3f151ccb364fdd09269c00650955ac2660c9b1

--- geoserver/src/org/vfny/geoserver/requests/TransactionFeatureHandler.java
+++ geoserver/src/org/vfny/geoserver/requests/TransactionFeatureHandler.java
@@ -15,8 +15,14 @@ import org.geotools.gml.GMLHandlerFeature;
 import org.geotools.gml.GMLFilterFeature;
 import org.geotools.filter.FilterFilter;
 import org.geotools.feature.FeatureType;
+import org.geotools.feature.AttributeType;
+import org.geotools.feature.AttributeTypeDefault;
+import org.geotools.feature.FeatureFactory;
+import org.geotools.feature.FeatureTypeFactory;
+import org.geotools.feature.Feature;
 import com.vividsolutions.jts.geom.Geometry;
 import org.vfny.geoserver.responses.WfsException;
+import org.vfny.geoserver.config.TypeRepository;
 
 /**
  * Uses SAX to extact a Transactional request from and incoming XML stream.
@@ -32,9 +38,36 @@ public class TransactionFeatureHandler
     private static Logger LOGGER = 
         Logger.getLogger("org.vfny.geoserver.requests");
 
-    private boolean inFilter = false;
+        /** Stores current feature attributes. */
+    private Vector attributes = new Vector();
+    private Vector attributeNames = new Vector();
+    /** Stores current feature attributes. */
+    private boolean insideAttribute = false;
+    
+    /** Stores current feature attributes. */
+    private boolean insideFeature = false;
+
+   /** Stores current feature attributes. */
+    private Object tempValue = null;
+    private String attName = "";
+    //private FeatureSchema metadata = new FeatureSchema();
+    
+    // Static Globals to handle some expected elements
+    /** GML namespace string. */
+    private static final String GML_NAMESPACE = "http://www.opengis.net/gml";
+    
+    /** The current namespace we're in. */
+    private String NAMESPACE;
+    /** Some sort of feature name. */
+    private String FEATURE_MEMBER_NAME = "featureMember";
+    private String typeName = "GenericFeature";
+    
+
 
     private TransactionFilterHandler parent;
+
+    private TypeRepository typeInfo = TypeRepository.getInstance();
+
       /**
      * Constructor with parent, which must implement GMLHandlerJTS.
      *
@@ -61,20 +94,96 @@ public class TransactionFeatureHandler
     public void startElement(String namespaceURI, String localName, 
                              String qName, Attributes atts)
         throws SAXException {
-	LOGGER.finer("starting element " + localName + " in transFeat");
-	if( localName.endsWith("Member") ) {
-	    LOGGER.finer("should start a transactionFeature");
-	    //if(localName.equals("Filter")) {
-            inFilter = true;
+	  
+	
+        // if it ends with Member we'll assume it's a feature for the time being
+        if (typeInfo.getType(localName) != null  ) {
+	    typeName = localName;
+            attributes = new Vector();
+            attributeNames = new Vector();
+            //currentFeature = new FeatureFlat();
+            insideFeature = true;
+            tempValue = null;
+            LOGGER.finest("Starting a feature " + localName);
+	    
+        } 
+	if (insideFeature) {
+            LOGGER.finest("inside feature " + localName);
+            for (int i = 0; i < atts.getLength(); i++){
+                String name = atts.getLocalName(i);
+                if (name.equalsIgnoreCase("fid")){
+                    //currentFeature.setTypeName(localName);
+                    typeName = new String(localName);
+                    LOGGER.finest("set type name " + localName);
+                }
+                attributes.add(atts.getValue(i));
+                attributeNames.add(name);
+            }
+            if(!typeName.equalsIgnoreCase(localName)){
+                if (attName.equals("")){
+                    LOGGER.finest("setting attName to " + localName);
+                    attName = localName;
+                } else {
+                    LOGGER.finest("adding " + localName + " to " + attName);
+                    attName = attName+"/"+localName;
+                }
+                LOGGER.finest("attName now equals " + attName);
+            }
+            insideAttribute = true;
+            return;
+        } else if(insideAttribute){
+            LOGGER.finest("inside attribute");
+            
+        }else{
+	    parent.startElement(namespaceURI, localName, qName, atts);
+            
         }
-
-	super.startElement(namespaceURI, localName, qName, atts);
     }
 
-     /**
+	
+
+    
+
+    /**
+     * Reads the only internal characters read by pure GML parsers, which are
+     * coordinates.  These coordinates are sent to the coordinates reader class
+     * which interprets them appropriately, depending on the its current state.
+     *
+     * @param ch Raw coordinate string from the GML document.
+     * @param start Beginning character position of raw coordinate string.
+     * @param length Length of the character string.
+     * @throws SAXException Some parsing error occurred while reading
+     * coordinates.
+     */
+    public void characters(char[] ch, int start, int length)
+    throws SAXException {
+        
+        // the methods here read in both coordinates and coords and take the
+        // grunt-work out of this task for geometry handlers.
+        // See the documentation for CoordinatesReader to see what this entails
+        String rawAttribute = new String(ch, start, length);
+        
+        if (insideAttribute) {
+            try {
+                tempValue = new Integer(rawAttribute);
+            } catch (NumberFormatException e1){
+                try {
+                    tempValue = new Double(rawAttribute);
+                } catch (NumberFormatException e2){
+                    tempValue = new String(rawAttribute);
+                }
+            }
+            
+        } else {
+	    parent.characters(ch, start, length);
+	}
+    }
+    
+    
+    /**
      * Checks for GML element end and - if not a coordinates element - sends it
      * directly on down the chain to the appropriate parent handler.  If it is
-     * a coordinates (or coord) element, it uses internal methods to set the 
+     * a coordinates (or coord) element, it uses internal methods to set the
      * current state of the coordinates reader appropriately.
      *
      * @param namespaceURI Namespace of the element.
@@ -82,15 +191,56 @@ public class TransactionFeatureHandler
      * @param qName Full name of the element, including namespace prefix.
      * @throws SAXException Parsing error occurred while reading coordinates.
      */
-    public void endElement(String namespaceURI, String localName, String qName)
-				throws SAXException {
-	if ( localName.endsWith("Member") ) {
-	    //if(localName.equals("Filter")) {
-            inFilter = false;
-        LOGGER.fine("finished a filter with transactionfilter");
-	}
-	super.endElement(namespaceURI, localName, qName);
+    public void endElement(String namespaceURI, String localName, String qName) throws SAXException {        
+        if (typeName.equals(localName)) {
+            AttributeType attDef[] = new AttributeTypeDefault[attributes.size()];
+            for (int i = 0; i < attributes.size(); i++){
+                attDef[i] = new AttributeTypeDefault((String) attributeNames.get(i),attributes.get(i).getClass());
+            }
+            try {
+                FeatureType schema = FeatureTypeFactory.create(attDef).setTypeName(typeName);
+                schema.setNamespace(namespaceURI);
+                FeatureFactory fac = new FeatureFactory(schema);
+                Feature feature = fac.create((Object []) attributes.toArray());
+                //currentFeature.setAttributes((Object []) attributes.toArray());
+                parent.feature(feature);
+                LOGGER.finest("resetting attName at end of feature");
+                attName = "";
+            }
+            catch (org.geotools.feature.SchemaException sve){
+                //TODO: work out what to do in this case!
+                //_log.error("Unable to create valid schema",sve);
+            }
+            catch (org.geotools.feature.IllegalFeatureException ife){
+                //TODO: work out what to do in this case!
+                //_log.error("Unable to build feature",ife);
+            }
+            insideFeature = false;
+            
+        } else if (insideAttribute) {
+            LOGGER.finest("end - inside attribute [" + tempValue + "]");
+            if (tempValue != null && !tempValue.toString().trim().equals("")) {
+                attributes.add(tempValue);
+                attributeNames.add(attName);
+                
+            }
+            int index =  attName.lastIndexOf('/');
+            if(index > -1 ){
+                LOGGER.finest("removing " + attName.substring(index+1));
+                attName = attName.substring(0,index);
+            } else {
+                attName = "";
+            }
+            LOGGER.finest("attName now equals " + attName);
+            insideAttribute = false;
+        } else {
+	    parent.endElement(namespaceURI, localName, qName);
+	    LOGGER.finest("end - inside feature");
+            //insideFeature = false;
+
+        }
     }
+
 
     /**
      * Manages the start of a new main or sub geometry.  This method looks at
@@ -101,12 +251,19 @@ public class TransactionFeatureHandler
      *
      * @param geometry The geometry from the child.
      */
+
     public void geometry(Geometry geometry) {
-	if (inFilter) {
+	
+	if (insideFeature) {
+	    if(attName.equals("")){
+		attributeNames.addElement("geometry");
+	    }else{
+		attributeNames.addElement(attName);
+	    }
+	    attributes.addElement(geometry);
 	    LOGGER.finer("calling gmlFeatureFilter for geom " + geometry);
-	    super.geometry(geometry);
 	} else {
-	    LOGGER.finer("passing geometry to parent " + geometry);
+	    
 	    parent.geometry(geometry);
 	}
     }
