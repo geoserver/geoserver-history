f2548df3c85bff713581a24cc21c3d9dbbc68481
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-01-15 22:41:31 +0000

    added maxFeatures and property selection
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@152 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5e7296ce4c2d0189807be98b118df74dc532c350 [formerly 7a383c6569005320b5367eca9bfe2c4449f75b3b]
    Former-commit-id: a378187868df837397c49da3c974989065f78e05

--- src/org/vfny/geoserver/responses/FeatureResponse.java
+++ src/org/vfny/geoserver/responses/FeatureResponse.java
@@ -11,7 +11,10 @@ import com.vividsolutions.jts.geom.Geometry;
 import org.geotools.feature.Feature;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureType;
+import org.geotools.feature.FeatureTypeFactory;
+import org.geotools.feature.FeatureTypeFlat;
 import org.geotools.feature.AttributeType;
+import org.geotools.feature.SchemaException;
 import org.geotools.data.DataSource;
 import org.geotools.data.DataSourceException;
 import org.geotools.data.postgis.PostgisConnection;
@@ -46,11 +49,12 @@ public class FeatureResponse {
 
         TypeRepository repository = TypeRepository.getInstance();
         StringBuffer result = new StringBuffer();
+	int maxFeatures = request.getMaxFeatures();
                     
         // main handler and return string
         //  generate GML for heander for each table requested
         for(int i = 0, n = request.getQueryCount(); i < n; i++) {            
-            result.append(getQuery(request.getQuery(i), repository));
+            result.append(getQuery(request.getQuery(i), repository, maxFeatures));
             LOG.finest("ended feature");
         }        
 
@@ -71,8 +75,8 @@ public class FeatureResponse {
      * Parses the GetFeature reqeust and returns a contentHandler.
      * @return XML response to send to client
      */ 
-    private static String getQuery(Query query, TypeRepository repository)
-        throws WfsException {
+    private static String getQuery(Query query, TypeRepository repository, 
+				   int maxFeatures) throws WfsException {
         
 	LOG.finest("about to get query: " + query);
         TypeInfo meta = repository.getType(query.getTypeName());
@@ -90,11 +94,39 @@ public class FeatureResponse {
         db.setLogin(meta.getUser(), meta.getPassword());
 	LOG.finest("setting user and pass " + meta.getUser() + meta.getPassword());
 	FeatureCollection collection = null;
+	FeatureType schema = null;
+	String tableName = meta.getName();
 	try {
-	    DataSource data = new PostgisDataSource(db, meta.getName());
+	schema = PostgisDataSource.makeSchema(tableName, db);
+	if (!query.allRequested()) {
+	    List propertyNames = query.getPropertyNames();
+	    AttributeType[] properties = new AttributeType[propertyNames.size()];
+	    try {
+	    for(int i = 0; i < propertyNames.size(); i++) {
+		properties[i] = 
+		    schema.getAttributeType(propertyNames.get(i).toString());
+		//TODO: make sure geometry is included, or change featuretypeflat
+		//to not have required geometry.
+            }
+	    //HACK: we should not rely on schema being a featureTypeFlat.  And
+	    //get and set srid methods will go away soon (which should make it
+	    //so the datasource works without these hacks)
+	    int srid = ((FeatureTypeFlat)schema).getSRID();
+	    schema = FeatureTypeFactory.create(properties);	    
+	    ((FeatureTypeFlat)schema).setSRID(srid);
+	    } catch (SchemaException e) {
+		throw new WfsException(e, "While trying to create schema for" +
+				       "return feature", getLocator(query));
+	    }
+	}
+
+	    DataSource data = 
+		new PostgisDataSource(db, tableName, schema, maxFeatures);
+	    LOG.finest("++++++++++++++++++filter is " + query.getFilter());
             collection = data.getFeatures(query.getFilter());
         } catch(DataSourceException e) {
-            throw new WfsException(e, "While getting features from datasource", getLocator(query));
+            throw new WfsException(e, "While getting features from datasource",
+				   getLocator(query));
         }
 
         LOG.finest("successfully retrieved collection");
@@ -104,20 +136,24 @@ public class FeatureResponse {
 	gml.startFeatureType(meta.getName(), meta.getSrs());
         LOG.finest("started feature type");
 	if (features.length > 0) {
-        FeatureType schema = features[0].getSchema();
+	    //FeatureType schema = features[0].getSchema();
         AttributeType[] attributeTypes = schema.getAttributeTypes();
         Object[] attributes;
-	int geometryPosition = schema.getDefaultGeometry().getPosition();
-
+	AttributeType geometryAttr = schema.getDefaultGeometry();
+	int geometryPosition = -1;
+	if (geometryAttr != null) {
+	    geometryPosition = geometryAttr.getPosition();
+	}
         LOG.finest("about to create gml");
         LOG.finest("initializing..." + attributeTypes[schema.attributeTotal() - 1].getClass().toString());
-        gml.initializeGeometry(attributeTypes[geometryPosition].
+	if (geometryAttr != null) {
+	    gml.initializeGeometry(attributeTypes[geometryPosition].
                                getType(), 
                                meta.getName(), 
                                meta.getSrs(), 
                                attributeTypes[geometryPosition].
                                getName());
-
+	}
         for(int i = 0, m = features.length; i < m; i++) {
 
             LOG.finest("fid: " + features[i].getId());
