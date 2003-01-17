fd7abcbed2054d709e7ba37a64fd46a9e2c227d6
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-01-17 19:30:59 +0000

    added start up for zserver module
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@162 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4783c439856332024981aac07ad8518f538db3f5 [formerly cefa23dec6ebef3c12558b4e3afe47870f994622]
    Former-commit-id: 7efc248caef493d277b0785988094a653e867249

--- src/org/vfny/geoserver/servlets/FreefsLog.java
+++ src/org/vfny/geoserver/servlets/FreefsLog.java
@@ -6,10 +6,12 @@ package org.vfny.geoserver.servlets;
 
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Properties;
 import javax.servlet.http.*;
 //import javax.servlet.*;
 import org.geotools.resources.Geotools;
 import org.vfny.geoserver.config.ConfigInfo;
+import org.vfny.geoserver.zserver.GeoZServer;
 
 //Logging system
 import java.util.logging.Logger;
@@ -25,7 +27,10 @@ import java.util.logging.Level;
  *
  */
 public class FreefsLog extends HttpServlet {
- 
+
+        /** Standard logging instance for class */
+    private static final Logger LOGGER = 
+        Logger.getLogger("org.vfny.geoserver.servlet");
    
     /** Default name for feature type schemas */
     private static final String CONFIG_FILE =  "configuration.xml";
@@ -38,6 +43,8 @@ public class FreefsLog extends HttpServlet {
     private static final Logger LOG = Logger.getLogger("org.vfny.geoserver.servlets");
    
 
+    private GeoZServer server;
+
     /**
      * Initializes logging and config.
      *
@@ -48,6 +55,17 @@ public class FreefsLog extends HttpServlet {
 	String path = root + CONFIG_DIR;
 	LOG.finest("init with path" + path);
 	ConfigInfo cfgInfo = ConfigInfo.getInstance(path);
+	Properties zserverProps = new Properties();
+	zserverProps.put("port", "5210"); //HACK -allow user to configure this!
+	zserverProps.put("datafolder", cfgInfo.getTypeDir());
+	zserverProps.put("fieldmap", path + cfgInfo.GEO_MAP_FILE);
+	zserverProps.put("database", root + CONFIG_DIR + "zserver-index");
+	try {
+	    server = new GeoZServer(zserverProps);
+	    server.start();
+	} catch (java.io.IOException e) {
+	    LOGGER.info("zserver module could not start: " + e.getMessage());
+	}
 
     }
     
@@ -62,4 +80,10 @@ public class FreefsLog extends HttpServlet {
         //BasicConfigurator.configure();
     }
     
+    public void destroy() {
+	LOGGER.finer("shutting down zserver");
+	server.shutdown(1);
+	
+    }
+
 }
