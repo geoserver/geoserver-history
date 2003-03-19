5676b6b0b97f9306b72e8ca1a55e9a78e88eaa67
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-03-19 00:30:28 +0000

    initial commit
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@240 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7b4b5f7d225e0c19ccc891b2352750cede8d2e0f [formerly fe3e2b76fbc392701219dfe94879dfc45c6674b0]
    Former-commit-id: 1b37fd6c12067ad5c26e1d018c57b4c9d9eb75ff

---
+++ src/org/vfny/geoserver/config/ConfigurationException.java
@@ -0,0 +1,33 @@
+/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root 
+ * application directory.
+ */
+package org.vfny.geoserver.config;
+
+
+/**
+ * Thrown when there is an error in configuration.
+ * 
+ * @version $VERSION$
+ * @author Chris Holmes
+ */
+public class ConfigurationException extends Exception {
+    /**
+     * Constructs a new instance of ConfigurationException
+     * 
+     * @param msg A message explaining the exception
+     */
+    public ConfigurationException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs a new instance of ConfigurationException 
+     * 
+     * @param msg A message explaining the exception
+     * @param exp the throwable object which caused this exception
+     */
+    public ConfigurationException(String msg, Throwable exp) {
+        super(msg, exp);
+    }
+}
