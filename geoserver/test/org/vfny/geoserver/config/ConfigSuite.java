69d27a982b6cc83a68e48e56132abfea4cc0c84d
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-03-19 00:33:36 +0000

    added tests for ServiceConfig
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@242 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 445287610ca93f86d4ed20a8334440ca6aad6305 [formerly 0843e9e135ec757e74e8a6d384f9de0f44688a12]
    Former-commit-id: 4cd9a5405ff73d823a63553f2b321fdf231827a2

--- geoserver/test/org/vfny/geoserver/config/ConfigSuite.java
+++ geoserver/test/org/vfny/geoserver/config/ConfigSuite.java
@@ -11,20 +11,8 @@ import java.util.logging.Level;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.PrecisionModel;
-import org.geotools.filter.FilterFactory;
-import org.geotools.filter.Filter;
-import org.geotools.filter.AbstractFilter;
-import org.geotools.filter.FidFilter;
-import org.geotools.filter.GeometryFilter;
-import org.geotools.filter.CompareFilter;
-import org.geotools.filter.DefaultExpression;
-import org.geotools.filter.AttributeExpression;
-import org.geotools.filter.LiteralExpression;
 import org.geotools.resources.Geotools;
+import org.geotools.resources.Log4JFormatter;
 import org.vfny.geoserver.requests.FeatureRequest;
 import org.vfny.geoserver.requests.Query;
 
@@ -32,13 +20,15 @@ import org.vfny.geoserver.requests.Query;
  * Tests the get feature request handling.
  *
  * @author Rob Hranac, TOPP
+ * @author Chris Holmes, TOPP
  * @version $VERSION$
  */
 public class ConfigSuite extends TestCase {
 
     /* Initializes the logger. */
     static {
-        Geotools.init("Log4JFormatter", Level.FINE);
+        Geotools.init("Log4JFormatter", Level.FINER);
+	Log4JFormatter.init("org.vfny.geoserver", Level.FINER);
     }
     
     /** Class logger */
@@ -47,11 +37,11 @@ public class ConfigSuite extends TestCase {
 
     /** Unit test data directory */
     private static final String CONFIG_DIR = 
-        System.getProperty("user.dir") + "/misc/documents/configuration.xml";
+        System.getProperty("user.dir") + "/misc/unit/config/";
 
     /** Unit test data directory */
     private static final String TYPE_DIR = 
-        System.getProperty("user.dir") + "/misc/data/featureTypes";
+        System.getProperty("user.dir") + "/misc/testData/featureTypes";
 
 
     private ConfigInfo config;
@@ -77,6 +67,9 @@ public class ConfigSuite extends TestCase {
      * Tests are run via the static methods in this suite.  The tests        *
      * themselves are quite generic, so documentation is minimal.            *
      *************************************************************************/
+
+
+
     public void test1() throws Exception {        
         LOGGER.fine(repo.toString());
         LOGGER.fine("has two types: " + (repo.typeCount() == 2));
@@ -100,5 +93,64 @@ public class ConfigSuite extends TestCase {
         LOGGER.fine("locked: " + repo.isLocked("roads"));
         assertTrue(!repo.isLocked("roads"));
     }
+
+    /* REVISIT: Can't seem to get JUnit to sleep correctly
+       right now.
+    public void testExpiry() throws Exception {
+	LOGGER.fine("locked: " + repo.isLocked("roads"));
+        assertTrue(!repo.isLocked("roads"));
+        String key = repo.lock("roads", 1);
+        LOGGER.fine("locked: " + repo.isLocked("roads"));
+        assertTrue(repo.isLocked("roads"));
+	//wait(61000);
+        boolean success = repo.unlock("roads", key);
+        LOGGER.fine("locked: " + repo.isLocked("roads"));
+        assertTrue(!repo.isLocked("roads"));
+	}*/
+
+    public void testServiceConfig() throws Exception {
+	ServiceConfig config = 
+	    ServiceConfig.getInstance(CONFIG_DIR + "service-config1.xml");
+	LOGGER.fine("config is " + config + ", matches " + testConfig(config));
+    }
+
+    public void testServiceConfigOld() throws Exception {
+	ServiceConfig config = 
+	    ServiceConfig.getInstance(CONFIG_DIR + "service-config2.xml");
+	LOGGER.fine("config is " + config + ", matches " + testConfig(config));
+    }
+
+    public void testNoConfigFile() {
+	try {
+	    ServiceConfig config = 
+	    ServiceConfig.getInstance(CONFIG_DIR + "config.xml");
+	    //should throw error here.
+	    fail();
+	} catch (ConfigurationException e) {
+	    LOGGER.fine("successfully caught config exception: " + e.getMessage());
+	}
+    }
+
+    public void testBadConfigFile() {
+	try {
+	    ServiceConfig config = 
+	    ServiceConfig.getInstance(TYPE_DIR + "/rail/info.xml");
+	    //should throw error here.
+	    fail();
+	} catch (ConfigurationException e) {
+	    LOGGER.fine("successfully caught config exception: " + e.getMessage());
+	}
+    }
+
+     private boolean testConfig(ServiceConfig servConfig){
+	 LOGGER.fine("title: " + servConfig.getTitle() + ", Fees: " + servConfig.getFees());
+	 return (servConfig.getName().equals("FreeFS") &&
+		 servConfig.getTitle().equals("TOPP GeoServer") &&
+		 servConfig.getAbstract().equals("This is a test server.") &&
+		 servConfig.getFees().equals("none") &&
+		 servConfig.getKeywords().get(0).equals("WFS") &&
+		 servConfig.getKeywords().get(1).equals("OGC") &&
+		 servConfig.getOnlineResource().equals("http://beta.openplans.org/geoserver"));
+        }
 
 }
