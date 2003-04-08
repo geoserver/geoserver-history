c75bb86fa0c647786aef6402b19a8f08626c9f7a
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-04-08 21:38:11 +0000

    changes to reflect new feature level locking in typeRepository
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@271 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e6291b46e4d56069f3f5438a3b9185dff65ae344 [formerly cb248412ca0de2aedbc8b23b4bca65d4786d042a]
    Former-commit-id: 41337e7456f04f1d8095bd7ac36dc2334bca0c81

--- geoserver/src/org/vfny/geoserver/responses/LockResponse.java
+++ geoserver/src/org/vfny/geoserver/responses/LockResponse.java
@@ -7,14 +7,17 @@ package org.vfny.geoserver.responses;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
 import java.util.logging.Logger;
+import org.geotools.filter.Filter;
 import org.vfny.geoserver.requests.LockRequest;
 import org.vfny.geoserver.config.TypeInfo;
 import org.vfny.geoserver.config.TypeRepository;
 
 
 /**
- * Handles a Transaction request and creates a TransactionResponse string.
+ * Handles a Lock request and creates a LockResponse string.
  *
  *@author Chris Holmes, TOPP
  *@version $VERSION$
@@ -30,8 +33,6 @@ public class LockResponse {
     /** Constructor, which is required to take a request object. */ 
     private LockResponse () {}
 
-    
-
     /**
      * Parses the GetFeature reqeust and returns a contentHandler.
      * @return XML response to send to client
@@ -42,41 +43,62 @@ public class LockResponse {
 	LOGGER.finer("about to do Lock response on:" + request);
 	List locks = request.getLocks();
 	LockRequest.Lock curLock = (LockRequest.Lock)locks.get(0);
-	String lockId;
-	List lockedList = new ArrayList();
+	boolean lockAll = request.getLockAll();
+	String lockId = null;
 	if (curLock == null) {
 	    throw new WfsException("A LockFeature request must contain at " +
 				   "least one LOCK element");
 	} else {
-	    checkLock(curLock);
 	    String curTypeName = curLock.getFeatureType();
-	    lockId = repository.lock(curTypeName, request.getExpiry());
-	    lockedList.add(curTypeName);
-		
+	    Filter curFilter = curLock.getFilter();
+	    lockId = repository.lock(curTypeName, curFilter, 
+				     lockAll, request.getExpiry());
 	    for(int i = 1, n = locks.size(); i < n; i++) {  
 		curLock = (LockRequest.Lock)locks.get(i);
 		curTypeName = curLock.getFeatureType();
-		checkLock(curLock);
-		repository.lock(curTypeName, lockId);
-		lockedList.add(curTypeName);
+		curFilter = curLock.getFilter();
+		repository.addToLock(curTypeName, curFilter, lockAll, lockId);
 	    }
 	}
-	return generateXml(lockId);
+	return generateXml(lockId, lockAll, 
+			   repository.getLockedFeatures(lockId),
+			   repository.getNotLockedFeatures(lockId));
     }
 
-    private static String generateXml(String lockId){
+    private static String generateXml(String lockId, boolean lockAll,
+				      Set lockedFeatures, Set notLockedFeatures){
 	String indent = "   ";
 	StringBuffer returnXml = new StringBuffer("<?xml version=\"1.0\" ?>");
 	returnXml.append("\n<WFS_LockFeatureResponse\n");
 	returnXml.append(indent + "xmlns=\"http://www.opengis.net/wfs\"\n");
 	//this not needed yet, only when FeaturesLocked element used.
-     //returnXml.append(indent +"xmlns:ogc=\"http://www.opengis.net/ogc\"\n");
+	if (!lockAll) {
+	  returnXml.append(indent +"xmlns:ogc=\"http://www.opengis.net/ogc\"\n");
+	}
 	returnXml.append(indent + "xmlns:xsi=\"http://www.w3.org/2001/" + 
 			 "XMLSchema-instance\"\n");
 	//REVISIT: this probably isn't right, need to learn about xml schemas
 	returnXml.append(indent + "xsi:schemaLocation=\"http://www.opengis" +
-			 ".net/ogc ../wfs/1.0.0/WFS-transaction.xsd\">\n");
+			 ".net/wfs ../wfs/1.0.0/WFS-transaction.xsd\">\n");
 	returnXml.append(indent + "<LockId>" + lockId + "</LockId>\n");
+	if (!lockAll) {
+	    returnXml.append(indent + "<FeaturesLocked>\n"); 
+		for (Iterator i = lockedFeatures.iterator();
+		     i.hasNext();) {
+		    returnXml.append(indent + indent);
+		    returnXml.append("<ogc:FeatureId fid=\"" + i.next() + "\"/>\n");
+		}
+	    returnXml.append(indent + "</FeaturesLocked>\n");
+	    if (notLockedFeatures != null && notLockedFeatures.size() > 0){
+		returnXml.append("<FeaturesNotLocked>\n");
+		for (Iterator i = notLockedFeatures.iterator();
+		     i.hasNext();) {
+		    returnXml.append(indent + indent);
+		    returnXml.append("<ogc:FeatureId fid=\"" + i.next() + "\"/>\n");
+		}
+		returnXml.append("</FeaturesNotLocked>\n");
+	    }
+	}
 	returnXml.append(indent + "</WFS_LockFeatureResponse>");
 
 	return returnXml.toString();
@@ -94,29 +116,29 @@ public class LockResponse {
      * request SOME for the lockAction then it doesn't matter if a few features
      * are locked.
      */
-    private static void checkLock(LockRequest.Lock testLock) 
+    /*    private static void checkLock(LockRequest.Lock testLock) 
 	throws WfsException{
     /* This functions by throwing WfsExceptions instead of returning a boolean,
-     * because we want to  chain the exception all the way to the user. */ 
+     * because we want to  chain the exception all the way to the user.
 	String typeName = testLock.getFeatureType();
 	if(repository.getType(typeName) == null) {
 	    throw new WfsException("typeName " + typeName + " not found on " +
 				   "this server", testLock.getHandle());
 	}
-	if (repository.isLocked(typeName)){
+	//if (repository.isLocked(typeName)){
 	    //Only when lockALL?  Kind of moot point right now.
-	    throw new WfsException("Could not lock " + typeName + " features" +
-				   ", they are already locked", 
-				   testLock.getHandle());
-	}
-	if (testLock.getFilter() != null) {
-	    throw new WfsException("Only FeatureType locking is currently "
-				   + "supported, try your lock request " + 
-				   "again without a filter element", 
-				   testLock.getHandle());
-	}
+	//   throw new WfsException("Could not lock " + typeName + " features" +
+	//			   ", they are already locked", 
+	//			   testLock.getHandle());
+	//}
+	//if (testLock.getFilter() != null) {
+	//   throw new WfsException("Only FeatureType locking is currently "
+	//			   + "supported, try your lock request " + 
+	//			   "again without a filter element", 
+	//			   testLock.getHandle());
+	//}
 
-    }
+     }*/
 
 
 }
