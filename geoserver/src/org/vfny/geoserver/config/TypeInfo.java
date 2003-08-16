ee1f03a64de529d913901b5d1e99b3979d8bf29a
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-08-16 21:19:59 +0000

    better handling and reporting of configuration errors
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@447 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 221c2331fc40a452bf39a105f76f3c481b58b363 [formerly 34e7d7a5ab7ce316b8c7c6a5afa0c5434c04d46d]
    Former-commit-id: 19cfd148ec4fe463932e9a088cbb127814b0a77b

--- geoserver/src/org/vfny/geoserver/config/TypeInfo.java
+++ geoserver/src/org/vfny/geoserver/config/TypeInfo.java
@@ -73,15 +73,15 @@ public class TypeInfo {
      * @param featureTypeName The query from the request object.
      */ 
     public TypeInfo(String typeName) {
-	LOG.finest("reading typeinfo for " + typeName);
+    LOG.finest("reading typeinfo for " + typeName);
         readTypeInfo(typeName);
-	
+    
     }
 
     /**
      * gets the Namespace prefix used internally for this featureType.*/
     public String getPrefix(){
-	return this.prefix;
+    return this.prefix;
     }
     
     /** Sets the prefix.  The prefix should generally be set during feature
@@ -89,10 +89,10 @@ public class TypeInfo {
      * such as ns01:rail, where rail is the name and ns01 is the prefix.
      */
     void setPrefix(String prefix){ 
-	if (config.getNSUri(prefix) == null){
-	    config.addPrefixNamespace(prefix);
-	}
-	this.prefix = prefix;
+    if (config.getNSUri(prefix) == null){
+        config.addPrefixNamespace(prefix);
+    }
+    this.prefix = prefix;
     }
 
     /**
@@ -103,7 +103,7 @@ public class TypeInfo {
      * location where it _should_ be located.
      */
     public String getSchemaFile(){
-	return pathToSchemaFile;
+    return pathToSchemaFile;
     }
 
     /**
@@ -111,19 +111,19 @@ public class TypeInfo {
      * in ConfigInfo for this to work right.
      */
     public String getXmlns(){
-	return config.getNSUri(this.prefix);
+    return config.getNSUri(this.prefix);
     }
     
     /** Fetches the feature type name (also the table name)  */
     //REVISIT: name getTableName()?  It should only be used for that purpose.
     public String getName() { 
-	LOG.finest("returning name " + internalType.getName());
-	return internalType.getName(); 
+    LOG.finest("returning name " + internalType.getName());
+    return internalType.getName(); 
     }
     
     /** Fetches the featureType name with its proper namespace prefix*/
     public String getFullName() {
-	return prefix + ":" + internalType.getName();
+    return prefix + ":" + internalType.getName();
     }
     
     /** Fetches the feature type abstract */
@@ -134,15 +134,15 @@ public class TypeInfo {
     
     /** Fetches the user-defined feature type keywords  */
      public String getKeywords() { 
-	StringBuffer keywords = new StringBuffer();
-	Iterator keywordIter = internalType.getKeywords().iterator();
-	while (keywordIter.hasNext()) {
-	    keywords.append(keywordIter.next().toString());
-	    if (keywordIter.hasNext()){
-		keywords.append(", ");
-	    }
-	}
-	return keywords.toString(); 
+    StringBuffer keywords = new StringBuffer();
+    Iterator keywordIter = internalType.getKeywords().iterator();
+    while (keywordIter.hasNext()) {
+        keywords.append(keywordIter.next().toString());
+        if (keywordIter.hasNext()){
+        keywords.append(", ");
+        }
+    }
+    return keywords.toString(); 
     }    
 
     /** Fetches the user-defined bounding box  */
@@ -195,7 +195,7 @@ public class TypeInfo {
      * Though that should probably happen in the datasource...
      */
     public String[] getMandatoryProps() {
-	return internalType.getMandatoryAtts();
+    return internalType.getMandatoryAtts();
     }
 
     //This is sort of bad.  It's fine if users always use locking, 
@@ -210,69 +210,22 @@ public class TypeInfo {
     //bad behaviour that happens when locking is not used.  In any case,
     //low priority.
     public DataSource getTransactionDataSource() throws WfsException {
+    if (transactionDS == null) {
+        try {
+        Map params = internalType.getDataParams();
+        transactionDS = DataSourceFinder.getDataSource(params);
 	if (transactionDS == null) {
-	    try {
-		Map params = internalType.getDataParams();
-		transactionDS = DataSourceFinder.getDataSource(params);
-	    } catch (DataSourceException e) {
+	    throw new WfsException("could not construct a datasource for " + 
+				   getFullName() + "with params: " + params);
+	}
+        } catch (DataSourceException e) {
             throw new WfsException(e, "While getting connection to datasource",
-				   getName());
-	    }
-	    LOG.finer("data source is " + transactionDS);
-	}
-	return transactionDS;
+                   getName());
+        }
+        LOG.finer("data source is " + transactionDS);
     }
-
-    /**
-     * returns a connection to the database containing the table for this
-     * type.  If this typeInfo has already made a connection to the database
-     * then this just returns that connection.  This method should _not_ be
-     * used if transactions are being performed, getNewConnection should be 
-     * used instead, or else the getFeature calls risk returning modified 
-     * features that have not yet been committed and could roll back.
-     */
-    /*public Connection getConnection() throws WfsException {
-	LOG.finer("getting connection for type: " + getName());
-	try {   
-	    if (dbConnection == null || dbConnection.isClosed()) {
-		this.dbConnection = getNewConnection();
-	    }
-	} catch (SQLException e) {
-	    String preMessage = "Problem getting Connection to db";
-	    String message = preMessage + ": " + e.getMessage();
-	    LOG.warning(message);
-	    throw new WfsException(e, preMessage, 
-				   "typeInfo for: " + getFullName());
-	}
-    	
-	return dbConnection;
-	}*/
-
-    /**
-     * returns a new connection.  This is a relatively expensive operation,
-     * so getConnection should be used where possible.  But this method must
-     * be used if transactions are being performed, or else you risk 
-     * inconsistent features.
-     * @return a new connection created using the information read from the
-     * type represented by this typeInfo.
-     * @throws SQLException if there were problems connecting with the SQL 
-     * database.
-     * @tasks TODO: put the charset in the configuration, for internationalization.
-     * this method would then set the right charSet.
-     */
-    /*public Connection getNewConnection() throws SQLException {
-	LOG.finer("getting new connection for type : " + getName());
-	PostgisConnectionFactory db = 
-	    new PostgisConnectionFactory (getHost(), getPort(), 
-					  getDatabaseName()); 
-	Properties dbProps = new Properties();
-	dbProps.put("user", getUser());
-	dbProps.put("password", getPassword());
-	//dbProps.put("charSet", "iso-8859-1");
-	Connection connection;
-	connection = db.getConnection(dbProps);
-	return connection;
-	}*/
+    return transactionDS;
+    }
 
     /**
      * gets the datasource associated with this typeInfo.  This uses the current
@@ -288,91 +241,36 @@ public class TypeInfo {
      * or the schema.
      */
     public DataSource getDataSource() 
-	throws WfsException {
+    throws WfsException {
+    if (featureDSource == null) {
+        try {
+        Map params = internalType.getDataParams();
+        LOG.finer("params is " + params);
+        featureDSource = DataSourceFinder.getDataSource(params);
 	if (featureDSource == null) {
-	    try {
-		Map params = internalType.getDataParams();
-		LOG.finer("params is " + params);
-		featureDSource = DataSourceFinder.getDataSource(params);
-	    } catch (DataSourceException e) {
-		throw new WfsException(e, "While getting connection to datasource",
-				       getName());
-	    }
-	    LOG.finer("data source is " + transactionDS);
+	    String mesg = "Please contact the admin, the configuration for " +
+		getFullName() + " has an error.  No datasource could be " + 
+		"constructed with the params: " + params + 
+		" from the info.xml file.";  
+	    throw new WfsException(mesg);
 	}
-	return featureDSource;
+        } catch (DataSourceException e) {
+        throw new WfsException(e, "While getting connection to datasource",
+                       getName());
+        }
+        LOG.finer("data source is " + transactionDS);
     }
-    //return transactionDS;
-
-    //this is handled in geotools now.
-    /**LOG.finer("about to get datasource for " + getName());
-	Connection connection = getConnection();
-	
-	DataSource data = null;
-	try {
-	    data = new PostgisDataSource(connection, getName(), maxFeatures);
-	    if (propertyNames != null  && propertyNames.size() > 0 &&
-		!propertyNames.get(0).toString().endsWith("*")) {
-		org.geotools.feature.FeatureType schema = data.getSchema();
-		AttributeType[] properties = new AttributeType[propertyNames.size()];
-		try {
-		    for(int i = 0; i < propertyNames.size(); i++) {
-			String curPropName = propertyNames.get(i).toString();
-			String[] splitName = curPropName.split("[.:/]");
-			String newPropName = curPropName;
-			if (splitName.length == 1) {
-			    newPropName = splitName[0];
-			} else {
-			    //REVISIT: not sure what to do if there are multiple
-			    //delimiters.  
-			    //REVISIT: should we examine the first value?  See
-			    //if the namespace or typename matches up right?
-			    //this is currently very permissive, just grabs
-			    //the value of the end.
-			    newPropName = splitName[splitName.length - 1];
-			}
-
-			properties[i] = 
-			    schema.getAttributeType(newPropName);
-			if (properties[i] == null) {
-			    AttributeType[] available = schema.getAttributeTypes();
-			    StringBuffer props = new StringBuffer();
-			    for (int j = 0; j < available.length; j++){
-				props.append(available[j].getName());
-				if (j < available.length - 1) {
-				    props.append(", ");
-				}
-			    }
-			    
-			    throw new WfsException("property name: " + 
-						   curPropName + " is "
-						   +"not a part of featureType"
-						   + ", the available properties"
-						   + " are: " + props);
-			}
-		    }
-		    schema = FeatureTypeFactory.create(properties);	 
-		}  catch (SchemaException e) {
-		    throw new WfsException(e, "While trying to create schema for " +
-					   "return feature", getName());
-		}
-		data.setSchema(schema);
-	    }
-	} catch (DataSourceException e) {
-            throw new WfsException(e, "While getting features from datasource",
-				   getName());
-				   }*/
-    //return data;
-    //}
+    return featureDSource;
+    }
 
     /**
      * Returns a capabilities XML fragment for a specific feature type.
      * @param version The version of the request (0.0.14 or 0.0.15)
      */ 
     public String getCapabilitiesXml(String version)  {     
-	LOG.finest("getting capabilities " + version);
+    LOG.finest("getting capabilities " + version);
         if(version.equals("0.0.14") || version.equals("1.0.0" )) {
-	    //1.0.0 is almost exactly like 0.0.14
+        //1.0.0 is almost exactly like 0.0.14
             return getCapabilitiesXmlv14(version);
         } else {
             return getCapabilitiesXmlv15();
@@ -385,28 +283,28 @@ public class TypeInfo {
      */ 
     private void readTypeInfo(String typeName) {
         
-	try {
-	    
-	    File featureTypeFile = new File(typeName);
-	    File parentDir = featureTypeFile.getParentFile();
-	    String parentDirName =  parentDir.getName();
-	    String typePrefix = config.getDefaultNSPrefix();
-	    int prefixDelimPos = parentDirName.indexOf(prefixFileDelimiter);
-	    if (prefixDelimPos > 0) {
-		typePrefix = parentDirName.substring(0, prefixDelimPos);
-	    } 
-	    setPrefix(typePrefix);
-	    pathToSchemaFile = 
-		new File(parentDir, config.SCHEMA_FILE).toString();
-	    LOG.finer("pathToSchema is " + pathToSchemaFile);
-	    //LOG.finer("prefix is " + prefix);
-	    
-	    internalType = FeatureType.getInstance(typeName);
-	    
-	} catch( ConfigurationException e ) {
+    try {
+        
+        File featureTypeFile = new File(typeName);
+        File parentDir = featureTypeFile.getParentFile();
+        String parentDirName =  parentDir.getName();
+        String typePrefix = config.getDefaultNSPrefix();
+        int prefixDelimPos = parentDirName.indexOf(prefixFileDelimiter);
+        if (prefixDelimPos > 0) {
+        typePrefix = parentDirName.substring(0, prefixDelimPos);
+        } 
+        setPrefix(typePrefix);
+        pathToSchemaFile = 
+        new File(parentDir, config.SCHEMA_FILE).toString();
+        LOG.finer("pathToSchema is " + pathToSchemaFile);
+        //LOG.finer("prefix is " + prefix);
+        
+        internalType = FeatureType.getInstance(typeName);
+        
+    } catch( ConfigurationException e ) {
             LOG.info("Trouble reading featureType info at " + typeName + 
-		     ": " + e.getMessage());
-	}
+             ": " + e.getMessage());
+    }
     }
     
 
@@ -417,30 +315,30 @@ public class TypeInfo {
      *
      */ 
     private String getCapabilitiesXmlv14(String version) {
-	// MAKE TERSE VERSION CAPABILITY
+    // MAKE TERSE VERSION CAPABILITY
         StringBuffer tempResponse = new StringBuffer("    <FeatureType>\n");
         String name = internalType.getName();
-	String latLonName = "LatLonBoundingBox";
-	boolean supportsAdd = false;
-	boolean supportsModify = false;
-	boolean supportsRemove = false;
-	try {
-	    DataSourceMetaData opInfo = getTransactionDataSource().getMetaData();
-	    
-	    supportsAdd = opInfo.supportsAdd();
-	    supportsModify = opInfo.supportsModify();
-	    supportsRemove = opInfo.supportsRemove();
-	} catch (WfsException e) {
-	    //don't make error on capabilities document just because we could
-	    //not connect to get the operations supported, just support less.
-	}
-	    if (!version.startsWith("0.0.1")) {
-	    //REVISIT: get this elsewhere?  Make sure that myns is
-	    //declared in the capabilities document returned.
-	    name = prefix + ":" + name;
-	    latLonName = "LatLongBoundingBox";
-	}
-	tempResponse.append("      <Name>" + name + "</Name>\n");
+    String latLonName = "LatLonBoundingBox";
+    boolean supportsAdd = false;
+    boolean supportsModify = false;
+    boolean supportsRemove = false;
+    try {
+        DataSourceMetaData opInfo = getTransactionDataSource().getMetaData();
+        
+        supportsAdd = opInfo.supportsAdd();
+        supportsModify = opInfo.supportsModify();
+        supportsRemove = opInfo.supportsRemove();
+    } catch (WfsException e) {
+        //don't make error on capabilities document just because we could
+        //not connect to get the operations supported, just support less.
+    }
+        if (!version.startsWith("0.0.1")) {
+        //REVISIT: get this elsewhere?  Make sure that myns is
+        //declared in the capabilities document returned.
+        name = prefix + ":" + name;
+        latLonName = "LatLongBoundingBox";
+    }
+    tempResponse.append("      <Name>" + name + "</Name>\n");
         tempResponse.append("      <Title>" + 
             internalType.getTitle() + "</Title>\n");
         tempResponse.append("      <Abstract>" + 
@@ -450,19 +348,19 @@ public class TypeInfo {
         tempResponse.append(
             "      <SRS>http://www.opengis.net/gml/srs/epsg#" + 
             internalType.getSRS() + "</SRS>\n");
-	if (supportsAdd || supportsModify || supportsRemove) {
+    if (supportsAdd || supportsModify || supportsRemove) {
         tempResponse.append("      <Operations>\n");
-	if (supportsAdd){
-	    tempResponse.append("        <Insert/>\n");
-	}
-	if (supportsModify){
-	    tempResponse.append("        <Update/>\n");
-	}
-	if (supportsRemove){
-	    tempResponse.append("        <Delete/>\n");
-	}
-	tempResponse.append("      </Operations>\n");
-	}
+    if (supportsAdd){
+        tempResponse.append("        <Insert/>\n");
+    }
+    if (supportsModify){
+        tempResponse.append("        <Update/>\n");
+    }
+    if (supportsRemove){
+        tempResponse.append("        <Delete/>\n");
+    }
+    tempResponse.append("      </Operations>\n");
+    }
         tempResponse.append(
             "      <" + latLonName + " minx=\"" + 
             internalType.getLatLonBoundingBox().getMinx() + 
@@ -481,7 +379,7 @@ public class TypeInfo {
                 //tempResponse.append("\" format=\"" + internalType.getMetadataURL().getFormat();
                 //tempResponse.append("\">" + internalType.getMetadataURL().getUrl() + "</MetaDataURL>\n";
         tempResponse.append("    </FeatureType>\n");
-	return tempResponse.toString();
+    return tempResponse.toString();
     }
     
      /**
@@ -492,7 +390,7 @@ public class TypeInfo {
         // SHOULD CHANGE TO STRING BUFFER
         // ALSO MAKE TERSE VERSION CAPABILITY
         StringBuffer tempResponse = 
-	    new StringBuffer("        <wfsfl:FeatureType>\n");
+        new StringBuffer("        <wfsfl:FeatureType>\n");
         tempResponse.append("            <wfsfl:Name>" + 
             internalType.getName() + "</wfsfl:Name>\n");
         tempResponse.append(
@@ -527,17 +425,17 @@ public class TypeInfo {
      * shut itself down.
      */
     public void close(){
-	if (dbConnection != null) {
-	    try {
-		LOG.finer("closing connection in " + getName());
-		dbConnection.close();
-	    } catch (SQLException e) {
-		LOG.finer("had trouble closing connection in " + getName() + ": " +
-			  e.getMessage());
-	    }
-	}
-	featureDSource = null;
-	transactionDS = null;
+    if (dbConnection != null) {
+        try {
+        LOG.finer("closing connection in " + getName());
+        dbConnection.close();
+        } catch (SQLException e) {
+        LOG.finer("had trouble closing connection in " + getName() + ": " +
+              e.getMessage());
+        }
+    }
+    featureDSource = null;
+    transactionDS = null;
     }
 
 }
