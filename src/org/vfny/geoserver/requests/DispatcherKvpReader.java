aef6fe1d7fd7a8b27a315e7bfc586a2f129dec7b
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-08-19 19:55:34 +0000

    fixed null bug if request element is not present
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@457 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 63f58681b5a87b3f24af4196dd153d596fbb30f0 [formerly 9ec289d42ba04fe63492ea6781957abc0afd59e9]
    Former-commit-id: 66381f705c67e9348be50af1b366498433b7fe31

--- src/org/vfny/geoserver/requests/DispatcherKvpReader.java
+++ src/org/vfny/geoserver/requests/DispatcherKvpReader.java
@@ -1,60 +1,62 @@
 /* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.util.logging.Logger;
 import org.vfny.geoserver.servlets.Dispatcher;
+import java.util.logging.Logger;
+
 
 /**
  * Reads in a generic request and attempts to determine its type.
- * 
- * @author Chris Holmes, TOPP
- * @version $Id: DispatcherKvpReader.java,v 1.1 2003/08/11 23:28:52 cholmesny Exp $
  *
+ * @author Chris Holmes, TOPP
+ * @version $Id: DispatcherKvpReader.java,v 1.2 2003/08/19 19:55:34 cholmesny Exp $
  */
-public class DispatcherKvpReader 
-    extends RequestKvpReader {
-
-      /** Class logger */
-    private static Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.requests");
+public class DispatcherKvpReader extends RequestKvpReader {
+    /** Class logger */
+    private static Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
 
     /**
-     * Constructor with raw request string.  This constructor
-     * parses the entire request string into a kvp hash table for
-     * quick access by sub-classes.
+     * Constructor with raw request string.  This constructor parses the entire
+     * request string into a kvp hash table for quick access by sub-classes.
+     *
      * @param rawRequest The raw request string from client.
      */
-    public DispatcherKvpReader (String rawRequest) {
+    public DispatcherKvpReader(String rawRequest) {
         super(rawRequest);
     }
-    
-    
-     /**
-      * Returns the request type for a given KVP string.
-      *
-      * @return Request type. 
-      */
-    public int getRequestType () {        
-        String responseType = ((String) kvpPairs.get("REQUEST")).toUpperCase();
-        
-    LOGGER.finer("request is " + responseType);
-        if(responseType.equals("GETCAPABILITIES")) {
-            return Dispatcher.GET_CAPABILITIES_REQUEST;
-        } else if( responseType.equals("DESCRIBEFEATURETYPE")) {
-            return Dispatcher.DESCRIBE_FEATURE_TYPE_REQUEST;
-        } else if( responseType.equals("GETFEATURE")) {
-            return Dispatcher.GET_FEATURE_REQUEST;
-        } else if( responseType.equals("TRANSACTION")) {
-            return Dispatcher.TRANSACTION_REQUEST;
-        } else if( responseType.equals("GETFEATUREWITHLOCK")) {
-            return Dispatcher.GET_FEATURE_LOCK_REQUEST;
-        } else if( responseType.equals("LOCKFEATURE")) {
-            return Dispatcher.LOCK_REQUEST;
-        }else {
+
+    /**
+     * Returns the request type for a given KVP string.
+     *
+     * @return Request type.
+     */
+    public int getRequestType() {
+        String responseType = ((String) kvpPairs.get("REQUEST"));
+
+        if (responseType != null) {
+            responseType = responseType.toUpperCase();
+
+            if (responseType.equals("GETCAPABILITIES")) {
+                return Dispatcher.GET_CAPABILITIES_REQUEST;
+            } else if (responseType.equals("DESCRIBEFEATURETYPE")) {
+                return Dispatcher.DESCRIBE_FEATURE_TYPE_REQUEST;
+            } else if (responseType.equals("GETFEATURE")) {
+                return Dispatcher.GET_FEATURE_REQUEST;
+            } else if (responseType.equals("TRANSACTION")) {
+                return Dispatcher.TRANSACTION_REQUEST;
+            } else if (responseType.equals("GETFEATUREWITHLOCK")) {
+                return Dispatcher.GET_FEATURE_LOCK_REQUEST;
+            } else if (responseType.equals("LOCKFEATURE")) {
+                return Dispatcher.LOCK_REQUEST;
+            } else {
+                return Dispatcher.UNKNOWN;
+            }
+        } else {
             return Dispatcher.UNKNOWN;
         }
-    }    
+    }
 }
