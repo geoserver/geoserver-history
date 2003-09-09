4041698f9b18d443e22cb951dbcb10c57762ee86
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-09 21:09:22 +0000

    jalopied and updated test data path
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@472 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4c2f472e30eba1ba9246487b48665d8744dd86f3 [formerly fe2e34a3825594d64c4fa02a3e1789441fe651b4]
    Former-commit-id: b2aa5b1c87aa9dd20290861c11374d739ce2ebf5

--- test/org/vfny/geoserver/zserver/ApplyXPathSuite.java
+++ test/org/vfny/geoserver/zserver/ApplyXPathSuite.java
@@ -1,128 +1,115 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.zserver;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.geotools.resources.Geotools;
 import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  * Tests the NumericField number and string conversion.
  *
  * @author Chris Holmes, TOPP
- * @version $VERSION$
+ * @version $Id: ApplyXPathSuite.java,v 1.4 2003/09/09 21:09:22 cholmesny Exp $
  */
-
 public class ApplyXPathSuite extends TestCase {
-
-    /* Initializes the logger. */
-    static {
-        Geotools.init("Log4JFormatter", Level.FINEST);
-    }
-
     /** Standard logging instance */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.zserver");
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.zserver");
 
-
-        /** Unit test data directory */
-    private static final String DATA_DIRECTORY = 
-        System.getProperty("user.dir") + "/misc/testData/unit/zserver";
-
+    /** Unit test data directory */
+    private static final String DATA_DIRECTORY = System.getProperty("user.dir")
+        + "/misc/unit/zserver";
     private String testPath = DATA_DIRECTORY + "/test1/metadata.xml";
-    
     private String fooPath = DATA_DIRECTORY + "/foo.xml";
-
     private String[] results = null;
 
     /**
      * Initializes the database and request handler.
+     *
+     * @param testName The name of the test to run.
      */
-    public ApplyXPathSuite (String testName) {
+    public ApplyXPathSuite(String testName) {
         super(testName);
     }
 
-
-    
     public static Test suite() {
         TestSuite suite = new TestSuite(ApplyXPathSuite.class);
-	LOGGER.fine("Creating ApplyXPath suite.");
+        LOGGER.fine("Creating ApplyXPath suite.");
+
         return suite;
     }
-    
 
     public void setUp() {
-
     }
 
-    
     public void testApply1() {
-	
-	String xpath = "//metadata/idinfo/citation/citeinfo/title/text()";
-	LOGGER.fine("testing xpath: " + xpath);
-	try {
-	    results = ApplyXPath.apply(testPath, xpath);
-	} catch (java.io.FileNotFoundException e) {
-	    fail("couldn't find file: " + e);
-	}
-	assertTrue(results[0].equals("Water Supply Watersheds "));
+        String xpath = "//metadata/idinfo/citation/citeinfo/title/text()";
+        LOGGER.fine("testing xpath: " + xpath);
 
+        try {
+            results = ApplyXPath.apply(testPath, xpath);
+        } catch (java.io.FileNotFoundException e) {
+            fail("couldn't find file: " + e);
+        }
+
+        assertTrue(results[0].equals("Water Supply Watersheds "));
     }
 
-      public void testApply2() {
+    public void testApply2() {
+        String xpath2 = "//metadata/idinfo/spdom/bounding//";
+        LOGGER.fine("testing xpath: " + xpath2);
 
-	String xpath2 = "//metadata/idinfo/spdom/bounding//";
-	LOGGER.fine("testing xpath: " + xpath2);
-	try {
-	    results = ApplyXPath.apply(testPath, xpath2);
-	} catch (FileNotFoundException e) {
-	    fail("couldn't find file: " + e);
-	}
-	assertTrue(results.length == 9);
-	
-      }
+        try {
+            results = ApplyXPath.apply(testPath, xpath2);
+        } catch (FileNotFoundException e) {
+            fail("couldn't find file: " + e);
+        }
+
+        assertTrue(results.length == 9);
+    }
 
     public void testApply3() {
+        String xpath3 = "//metadata/idinfo/spdom/bounding/westbc/text()";
+        LOGGER.fine("testing xpath: " + xpath3);
 
-	String xpath3 = "//metadata/idinfo/spdom/bounding/westbc/text()";
-	LOGGER.fine("testing xpath: " + xpath3);
-	try {
-	    results = ApplyXPath.apply(testPath, xpath3);
-	} catch (FileNotFoundException e) {
-	    fail("couldn't find file: " + e);
-	}	
-	assertTrue(results[0].equals("-83.9814 "));
+        try {
+            results = ApplyXPath.apply(testPath, xpath3);
+        } catch (FileNotFoundException e) {
+            fail("couldn't find file: " + e);
+        }
+
+        assertTrue(results[0].equals("-83.9814 "));
     }
-
 
     public void testApply4() {
-      
-	String xpath4 = "//metadata/nadfme";
-	LOGGER.fine("testing xpath: " + xpath4);
-	try {
-	    results = ApplyXPath.apply(testPath, xpath4);
-	} catch (FileNotFoundException e) {
-	    fail("couldn't find file: " + e);
-	}	
-	assertTrue(results.length == 0);
-    } 
+        String xpath4 = "//metadata/nadfme";
+        LOGGER.fine("testing xpath: " + xpath4);
 
-    public void testFileNotFound() {
-	String xpath = "//";
-	String failPath = DATA_DIRECTORY + "/blorg.xml";
-	try {
-	    results = ApplyXPath.apply(failPath, xpath);
-	    fail("didn't throw FileNotFoundException");
-	} catch (FileNotFoundException e) {
-	    LOGGER.finer("threw proper exception");
-	}
+        try {
+            results = ApplyXPath.apply(testPath, xpath4);
+        } catch (FileNotFoundException e) {
+            fail("couldn't find file: " + e);
+        }
 
+        assertTrue(results.length == 0);
     }
 
+    public void testFileNotFound() {
+        String xpath = "//";
+        String failPath = DATA_DIRECTORY + "/blorg.xml";
+
+        try {
+            results = ApplyXPath.apply(failPath, xpath);
+            fail("didn't throw FileNotFoundException");
+        } catch (FileNotFoundException e) {
+            LOGGER.finer("threw proper exception");
+        }
+    }
 }
