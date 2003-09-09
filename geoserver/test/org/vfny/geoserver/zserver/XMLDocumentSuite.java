645215e9ac7f4b86d0e4cc825527f31a9e2284c1
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-09 22:57:40 +0000

    jalopied and updated test data path
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@477 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2ce7c0502004589d5e9332ac506cadbcd2a78bc1 [formerly 86af74ae74f0b2d5de38cc90b0ea570b48986f9a]
    Former-commit-id: 3b56c1c77b55d562d006c4e8ad7e1a094e15cc9a

--- geoserver/test/org/vfny/geoserver/zserver/XMLDocumentSuite.java
+++ geoserver/test/org/vfny/geoserver/zserver/XMLDocumentSuite.java
@@ -1,201 +1,193 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.zserver;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.geotools.resources.Geotools;
-import java.io.FileNotFoundException;
-import org.apache.lucene.document.Document;
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.File;
-import org.apache.lucene.document.Field;
 import org.apache.lucene.document.DateField;
-
-// For XML components
-//import  org.w3c.dom.*;
-//import  org.apache.xml.serialize.OutputFormat;
-//import  org.apache.xml.serialize.Serializer;
-//import  org.apache.xml.serialize.SerializerFactory;
-//import  org.apache.xml.serialize.XMLSerializer;
+import org.apache.lucene.document.Document;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
- * Tests the NumericField number and string conversion.
+ * Tests the XMLDocument converter.
  *
  * @author Chris Holmes, TOPP
- * @version $VERSION$
+ * @version $Id: XMLDocumentSuite.java,v 1.3 2003/09/09 22:57:40 cholmesny Exp $
  */
-
 public class XMLDocumentSuite extends TestCase {
-
-    /* Initializes the logger. */
-    static {
-        Geotools.init("Log4JFormatter", Level.FINEST);
-    }
+    /* Initializes the logger. Uncomment to see log messages.*/
+    //static {
+    //    org.vfny.geoserver.config.Log4JFormatter.init("org.vfny.geoserver", Level.FINEST);
+    //}
 
     /** Standard logging instance */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.zserver");
-
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.zserver");
     private static final String UPDATE_FIELD = "//metadata/idinfo/status/update";
-
     private static final String EAST_FIELD = "//metadata/idinfo/spdom/bounding/eastbc";
-        /** Unit test data directory */
-    private static final String DATA_DIRECTORY = 
-        System.getProperty("user.dir") + "/misc/testData/unit/zserver";
 
-     private String testPath = DATA_DIRECTORY + "/test1/metadata.xml";
-
-    
-
+    /** Unit test data directory */
+    private static final String DATA_DIRECTORY = System.getProperty("user.dir")
+        + "/misc/unit/zserver";
+    private String testPath = DATA_DIRECTORY + "/test1/metadata.xml";
     private String testMapPath = DATA_DIRECTORY + "/geo.map";
-
     private Properties testMap;
 
-    private org.apache.lucene.document.Document doc;
-
-   
-
     /**
-     * Initializes the database and request handler.
+     * Constructor that calls super.
+     *
+     * @param testName the name of the test.
      */
-    public XMLDocumentSuite (String testName) {
+    public XMLDocumentSuite(String testName) {
         super(testName);
     }
 
-
     /**
      * Creates an instance of this suite.
+     *
+     * @return the new suite.
      */
     public static Test suite() {
         TestSuite suite = new TestSuite(XMLDocumentSuite.class);
-	LOGGER.fine("Creating XMLDocument suite.");
+        LOGGER.fine("Creating XMLDocument suite.");
+
         return suite;
     }
-    
 
     public void setUp() {
-	try {
-	    
-	    FileInputStream fis = new FileInputStream(testMapPath);
-	    testMap = new Properties();
-	    testMap.load(fis);
-	} catch (Exception e) {
-	    LOGGER.severe("problem loading property file: " + e);
-	}
-	String fieldName = testMap.getProperty(GeoProfile.Attribute.TITLE);
+        try {
+            FileInputStream fis = new FileInputStream(testMapPath);
+            testMap = new Properties();
+            testMap.load(fis);
+        } catch (Exception e) {
+            LOGGER.severe("problem loading property file: " + e);
+        }
 
+        //String fieldName = testMap.getProperty(GeoProfile.Attribute.TITLE);
     }
-
 
     public void testBasics() {
-	Properties map = new Properties();
-	//String xpath =  "/document/name/first_name/";
-	//map.setProperty("1", xpath);
-	String barPath = DATA_DIRECTORY + "/bar.xml";
-	File barFile = null;
-	Document doc = null;
-	try {
-	    barFile = new File(barPath);	    
-	    doc = XMLDocument.Document(barFile, map);
-	} catch (Exception e) {
-	    fail("exception creating document: " + e);
-	}
-	//assertTrue(doc.get(xpath).equals("John"));
-	assertTrue(doc.get("path").equals(barPath));
-	String lastMod = DateField.timeToString(barFile.lastModified());
-	assertTrue(doc.get("modified").equals(lastMod));
-	assertTrue(doc.get("true").equals("true"));
-	
+        Properties map = new Properties();
+
+        //String xpath =  "/document/name/first_name/";
+        //map.setProperty("1", xpath);
+        String barPath = DATA_DIRECTORY + "/bar.xml";
+        File barFile = null;
+        Document doc = null;
+
+        try {
+            barFile = new File(barPath);
+            doc = XMLDocument.Document(barFile, map);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        //assertTrue(doc.get(xpath).equals("John"));
+        assertTrue(doc.get("path").equals(barPath));
+
+        String lastMod = DateField.timeToString(barFile.lastModified());
+        assertTrue(doc.get("modified").equals(lastMod));
+        assertTrue(doc.get("true").equals("true"));
     }
-	
-	public void testPaths() {
-	    Properties map = new Properties();
-	    //String xpath =  "/document/name/first_name/";
-	    //map.setProperty("1", xpath);
-	    String barPath = DATA_DIRECTORY + "/bar.xml";
-	    File barFile = null;
-	    Document doc = null;
-	    try {
-		barFile = new File(barPath);	    
-		doc = XMLDocument.Document(barFile, map);
-		LOGGER.fine("doc is " + doc);
-	    } catch (Exception e) {
-		fail("exception creating document: " + e);
-	    }
-	    String htmlPath = DATA_DIRECTORY + "/bar.html";
-	    String sgmlPath = DATA_DIRECTORY + "/bar.sgml";
-	    String textPath = DATA_DIRECTORY + "/bar.txt";
-	    assertTrue(doc.get("html").equals(htmlPath));
-	    assertTrue(doc.get("sgml").equals(sgmlPath));
-	    assertTrue(doc.get("sutrs").equals(textPath));
-	    
-	}
+
+    public void testPaths() {
+        Properties map = new Properties();
+
+        //String xpath =  "/document/name/first_name/";
+        //map.setProperty("1", xpath);
+        String barPath = DATA_DIRECTORY + "/bar.xml";
+        File barFile = null;
+        Document doc = null;
+
+        try {
+            barFile = new File(barPath);
+            doc = XMLDocument.Document(barFile, map);
+            LOGGER.fine("doc is " + doc);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        String htmlPath = DATA_DIRECTORY + "/bar.html";
+        String sgmlPath = DATA_DIRECTORY + "/bar.sgml";
+        String textPath = DATA_DIRECTORY + "/bar.txt";
+        assertTrue(doc.get("html").equals(htmlPath));
+        assertTrue(doc.get("sgml").equals(sgmlPath));
+        assertTrue(doc.get("sutrs").equals(textPath));
+    }
 
     public void testWithMap() {
-	Properties map = new Properties();
-	String xpath =  "/document/name/first_name/";
-	map.setProperty("1", xpath);
-	String barPath = DATA_DIRECTORY + "/bar.xml";
-	File barFile = null;
-	Document doc = null;
-	try {
-	    barFile = new File(barPath);	    
-	    doc = XMLDocument.Document(barFile, map);
-	} catch (Exception e) {
-	    fail("exception creating document: " + e);
-	}
-	assertTrue(doc.get(xpath).equals("John"));
+        Properties map = new Properties();
+        String xpath = "/document/name/first_name/";
+        map.setProperty("1", xpath);
+
+        String barPath = DATA_DIRECTORY + "/bar.xml";
+        File barFile = null;
+        Document doc = null;
+
+        try {
+            barFile = new File(barPath);
+            doc = XMLDocument.Document(barFile, map);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        assertTrue(doc.get(xpath).equals("John"));
     }
-	    
+
     public void testConvenience1() {
-	Properties map = new Properties();
-	String xpath =  "/document/name/last_name/";
-	map.setProperty("1", xpath);
-	String barPath = DATA_DIRECTORY + "/bar.xml";
-	Document doc = null;
-	try {
-	    doc = XMLDocument.Document(barPath, map);
-	} catch (Exception e) {
-	    fail("exception creating document: " + e);
-	}
-	assertTrue(doc.get(xpath).equals("Doe"));
+        Properties map = new Properties();
+        String xpath = "/document/name/last_name/";
+        map.setProperty("1", xpath);
+
+        String barPath = DATA_DIRECTORY + "/bar.xml";
+        Document doc = null;
+
+        try {
+            doc = XMLDocument.Document(barPath, map);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        assertTrue(doc.get(xpath).equals("Doe"));
     }
 
     public void testConvenience2() {
-	Document doc = null;
-	try {
-	    doc = XMLDocument.Document(testPath, testMapPath);
-	} catch (Exception e) {
-	    fail("exception creating document: " + e);
-	}
-	assertTrue(doc.get(UPDATE_FIELD).equals("As needed"));
+        Document doc = null;
 
+        try {
+            doc = XMLDocument.Document(testPath, testMapPath);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        assertTrue(doc.get(UPDATE_FIELD).equals("As needed"));
     }
 
     public void testNumbers() {
-	Document doc = null;
-	try {
-	    doc = XMLDocument.Document(testPath, testMapPath);
-	} catch (Exception e) {
-	    fail("exception creating document: " + e);
-	}
-	
-	double east = NumericField.stringToNumber(doc.get(EAST_FIELD)).doubleValue();
-	LOGGER.fine("east " + east);
-	assertTrue(east == -75.631);
-	double extent = NumericField.stringToNumber(doc.get("extent")).doubleValue();
-	LOGGER.fine("extent " + extent);
-	assertTrue(extent == 19.77124208);
+        Document doc = null;
 
+        try {
+            doc = XMLDocument.Document(testPath, testMapPath);
+        } catch (Exception e) {
+            fail("exception creating document: " + e);
+        }
+
+        double east = NumericField.stringToNumber(doc.get(EAST_FIELD))
+                                  .doubleValue();
+        LOGGER.fine("east " + east);
+        assertTrue(east == -75.631);
+
+        double extent = NumericField.stringToNumber(doc.get("extent"))
+                                    .doubleValue();
+        LOGGER.fine("extent " + extent);
+        assertTrue(extent == 19.77124208);
     }
-	
-
 }
