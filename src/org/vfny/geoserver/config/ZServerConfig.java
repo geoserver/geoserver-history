fb44d23ed141846f4d135e447b744205e2c48a9c
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-10 20:22:29 +0000

    cleaned up with jalopy
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@488 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c59bb6d13a4ac62157efb29fc44e162538549f4f [formerly beeae22fda3bdcde7c8c91dd8fdba77513647e19]
    Former-commit-id: 930a67b12b0378976d96fda310c1d874317716bb

--- src/org/vfny/geoserver/config/ZServerConfig.java
+++ src/org/vfny/geoserver/config/ZServerConfig.java
@@ -1,183 +1,211 @@
- /* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-
 package org.vfny.geoserver.config;
 
-import java.util.Map;
-import java.util.HashMap;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Logger;
-import java.io.Reader;
-import java.io.Serializable;
-import java.io.Writer;
-import java.io.FileInputStream;  
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.FileReader;
-
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException; 
+import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.*;
-import org.xml.sax.InputSource;
-import org.xml.sax.DocumentHandler;
-import org.xml.sax.SAXException;
-
-import org.geotools.resources.Geotools;
-import org.geotools.resources.Log4JFormatter;
-import org.geotools.resources.MonolineFormatter;
 
 /**
- * This class represents the global geoserver configuration options that
- * are not part of the Service element of a capabilities response.  They
- * are currently part of the same config file, but making a seperate class
- * allows us to easily seperate the elements out to different configuration
- * files in the future, such as for a WMS or WCS.  
+ * This class represents the global geoserver configuration options that are
+ * used to configure the zserver module.
  *
  * @author Chris Holmes, TOPP
- * @version $VERSION$
-**/
+ * @version $Id: ZServerConfig.java,v 1.3 2003/09/10 20:22:29 cholmesny Exp $
+ */
 public class ZServerConfig implements java.io.Serializable {
-
+    /** The tag for which port to run zserver on. */
     public static final String PORT_TAG = "Port";
-    
+
+    /** The root tag for zserver params. */
     public static final String ROOT_TAG = "ZServer";
 
+    /** The tag for the location of the fieldmap file to use. */
     public static final String FIELDMAP_TAG = "FieldMap";
 
-    public static final String DATABASE_TAG = "OnlineResource";
-
+    /** The xml tag for whether zserver module should be run. */
     public static final String RUN_ATTR = "run";
-    
-        /** Standard logging in stance for class */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.config");
-    
+
+    /** Standard logging in stance for class */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.config");
+
+    /** The configuration singleton. */
     private static ConfigInfo cfgInfo = ConfigInfo.getInstance();
-    
+
+    /** The port to run zserver on.  Default is 5210 */
     private String port = "5210";
-    
+
+    /** The folder where the metadata files are. */
     private String dataFolder = cfgInfo.getTypeDir();
 
+    /** The location of the mapping between field names and numbers. */
     private String fieldmap;
 
+    /** The location of the lucene database. */
     private String database;
-    
+
+    /** Whethere the zserver should be run. */
     private boolean runServer = true;
 
-    /** The holds the mappings between prefixes and uri's*/
+    /** The holds the mappings between prefixes and uri's */
     private Properties zserverProps;
 
-    private ZServerConfig(boolean runServer){
-	this.runServer = false;
-    }
-
-    private ZServerConfig(String port, String fields, String database){
-	if (port != null && !port.equals("")) {
-	    this.port = port;
-	}
-	this.fieldmap = fields;
-	this.database = database;
-    } 
-
     /**
-     * static factory, reads a ZServerConfig from an xml file, using
-     * the default root tag.
+     * Private constructor specifying if server should be run.   Thould
+     * generally only be run with false as the param, since other information
+     * needs to be set if we are to be run.
      *
-     * @param configFile the path to the configuration file.
-     * @return the ZServerConfig object constructed from the xml elements
-     * of the file.
+     * @param runServer should be <tt>false</tt>.
      */
-    public static ZServerConfig getInstance(String configFile) 
-    throws ConfigurationException {
-	return getInstance(configFile, ROOT_TAG);
+    private ZServerConfig(boolean runServer) {
+        this.runServer = runServer;
     }
 
     /**
-     * static factory, reads a ZServerConfig from an xml file, using
-     * the passed in root tag.
+     * Constructor with necessary params to run the server.  Assumes runServer
+     * is true.
      *
-     * @param configFile the path to the configuration file.
-     * @param rootTag the tag of the element whose children are the appropriate
-     * configuration elements.
-     * @return the ZServerConfig object constructed from the xml elements
-     * of the file.
+     * @param port The port to run the zserver on.
+     * @param fields The location of the field mappings.
+     * @param database The location of the backend database of metadata.
      */
-    public static ZServerConfig getInstance(String configFile, String rootTag) 
-	throws ConfigurationException{
-	try {
-	    //FileInputStream fis = new FileInputStream(configFile);
-	    FileReader fis = new FileReader(configFile);
-	    InputSource in = new InputSource(fis);
-	     DocumentBuilderFactory dfactory = new org.apache.xerces.jaxp.DocumentBuilderFactoryImpl();
-	    dfactory.setNamespaceAware(true);
-	    Document wfsDoc = dfactory.newDocumentBuilder().parse(in);
-	    Element configElem = wfsDoc.getDocumentElement();
-	    String configTag = configElem.getTagName();
-	    LOGGER.finer("root tag is " + configTag);
-	    if (!configTag.equals(rootTag)){
-		configElem = 
-		    (Element)configElem.getElementsByTagName(rootTag).item(0);
-		if (configElem == null) {
-		    String message = "could not find zserver tag: "+ rootTag + 
-			" in file: " + configFile + ".  Zserver module is not"
-			+ " being run";
-		    LOGGER.info(message);
-		    //throw new ConfigurationException(message);
-		    //instead of throwing exception, just search whole document.
-		    return new ZServerConfig(false);
-		}
-	    }
-	
-	    String runZserver = configElem.getAttribute(RUN_ATTR);
-	    if (runZserver == null || !runZserver.equals("false")) {
-		String port = findTextFromTag(configElem, PORT_TAG);
-		String rootDir = cfgInfo.getRootDir();
-		String fieldMap = rootDir + cfgInfo.GEO_MAP_FILE;
-		String database = rootDir + "zserver-index";		
-		return new ZServerConfig(port, fieldMap, database);
-	    } 
+    private ZServerConfig(String port, String fields, String database) {
+        if ((port != null) && !port.equals("")) {
+            this.port = port;
+        }
 
-	} catch (IOException ioe) {
-	    String message = "problem reading file " + configFile  + "due to: "
-		+ ioe.getMessage();
-	    LOGGER.warning(message);
-	    throw new ConfigurationException(message, ioe);
-	} catch (ParserConfigurationException pce) {
-	    String message = "trouble with parser to read xml, make sure class"
-		+ "path is correct, reading file " + configFile;
-	    LOGGER.warning(message);
-	    throw new ConfigurationException(message, pce);
-	} catch (SAXException saxe){
-	    String message = "trouble parsing XML in " + configFile 
-		+ ": " + saxe.getMessage();
-	    LOGGER.warning(message);
-	    throw new ConfigurationException(message, saxe);
-	}
-	return new ZServerConfig(false);
-	
+        this.fieldmap = fields;
+        this.database = database;
     }
-    
+
+    /**
+     * Static factory, reads a ZServerConfig from an xml file, using the
+     * default root tag.
+     *
+     * @param configFile The path to the configuration file.
+     *
+     * @return The ZServerConfig object constructed from the xml elements of
+     *         the file.
+     *
+     * @throws ConfigurationException For any config problems.
+     */
+    public static ZServerConfig getInstance(String configFile)
+        throws ConfigurationException {
+        return getInstance(configFile, ROOT_TAG);
+    }
+
+    /**
+     * Static factory, reads a ZServerConfig from an xml file, using the passed
+     * in root tag.
+     *
+     * @param configFile The path to the configuration file.
+     * @param rootTag The tag of the element whose children are the appropriate
+     *        configuration elements.
+     *
+     * @return The ZServerConfig object constructed from the xml elements of
+     *         the file.
+     *
+     * @throws ConfigurationException For any config problems.
+     */
+    public static ZServerConfig getInstance(String configFile, String rootTag)
+        throws ConfigurationException {
+        try {
+            //FileInputStream fis = new FileInputStream(configFile);
+            FileReader fis = new FileReader(configFile);
+            InputSource in = new InputSource(fis);
+            DocumentBuilderFactory dfactory = new org.apache.xerces.jaxp.DocumentBuilderFactoryImpl();
+            dfactory.setNamespaceAware(true);
+
+            Document wfsDoc = dfactory.newDocumentBuilder().parse(in);
+            Element configElem = wfsDoc.getDocumentElement();
+            String configTag = configElem.getTagName();
+            LOGGER.finer("root tag is " + configTag);
+
+            if (!configTag.equals(rootTag)) {
+                configElem = (Element) configElem.getElementsByTagName(rootTag)
+                                                 .item(0);
+
+                if (configElem == null) {
+                    String message = "could not find zserver tag: " + rootTag
+                        + " in file: " + configFile
+                        + ".  Zserver module is not" + " being run";
+                    LOGGER.info(message);
+
+                    //throw new ConfigurationException(message);
+                    //instead of throwing exception, just search whole document.
+                    return new ZServerConfig(false);
+                }
+            }
+
+            String runZserver = configElem.getAttribute(RUN_ATTR);
+
+            if ((runZserver == null) || !runZserver.equals("false")) {
+                String port = findTextFromTag(configElem, PORT_TAG);
+                String rootDir = cfgInfo.getRootDir();
+                String fieldMap = rootDir + cfgInfo.GEO_MAP_FILE;
+                String database = rootDir + "zserver-index";
+
+                return new ZServerConfig(port, fieldMap, database);
+            }
+        } catch (IOException ioe) {
+            String message = "problem reading file " + configFile + "due to: "
+                + ioe.getMessage();
+            LOGGER.warning(message);
+            throw new ConfigurationException(message, ioe);
+        } catch (ParserConfigurationException pce) {
+            String message = "trouble with parser to read xml, make sure class"
+                + "path is correct, reading file " + configFile;
+            LOGGER.warning(message);
+            throw new ConfigurationException(message, pce);
+        } catch (SAXException saxe) {
+            String message = "trouble parsing XML in " + configFile + ": "
+                + saxe.getMessage();
+            LOGGER.warning(message);
+            throw new ConfigurationException(message, saxe);
+        }
+
+        return new ZServerConfig(false);
+    }
+
     //TODO: put this in a common utility file, as 3 classes use it, and 
     //it's a bit confusing getting the ServiceConfig log messages.
-    private static String findTextFromTag(Element root, String tag){
-	return ServiceConfig.findTextFromTag(root, tag);
+    private static String findTextFromTag(Element root, String tag) {
+        return ServiceConfig.findTextFromTag(root, tag);
     }
 
+    /**
+     * Whether the zserver module should be run.
+     *
+     * @return <tt>true</tt> if the server should be run.
+     */
     public boolean run() {
-	return this.runServer;
+        return this.runServer;
     }
 
+    /**
+     * Returns the properties to run the zserver, in that module's  preferred
+     * format.
+     *
+     * @return the Properties to run the zserver.
+     */
     public Properties getProps() {
-	Properties zserverProps = new Properties();
-	zserverProps.put("port", port);
-	zserverProps.put("datafolder", dataFolder);
-	zserverProps.put("fieldmap", fieldmap);
-	zserverProps.put("database", database);
-	return zserverProps;
+        Properties zserverProps = new Properties();
+        zserverProps.put("port", port);
+        zserverProps.put("datafolder", dataFolder);
+        zserverProps.put("fieldmap", fieldmap);
+        zserverProps.put("database", database);
+
+        return zserverProps;
     }
 }
