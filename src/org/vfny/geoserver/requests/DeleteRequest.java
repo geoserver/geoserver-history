8007c4a5403d60aba772a719aa7a22ce041b8cab
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-12 18:37:56 +0000

    cleaned for pmd, jalopied and javadoced
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@497 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d506c929eaf679632aede6b1717bcb50fdc0ba43 [formerly 9f00c0908a6a428ebcdac24ef63d15807e807b8f]
    Former-commit-id: 816739fe0ae81036dab9d26affc759a191671505

--- src/org/vfny/geoserver/requests/DeleteRequest.java
+++ src/org/vfny/geoserver/requests/DeleteRequest.java
@@ -1,140 +1,194 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
 import org.geotools.filter.Filter;
+import java.util.logging.Logger;
+
 
 /**
- * <p>Defines a WFS delete request, which is a type transaction request.  Full
- * transaction requests may contain one or more delete requests.  Note that 
- * this delete request object is slightly different than it is defined in the 
- * WFS 1.0 specification.  In the specification, the delete request may contain
- * one or more filters and feature types for deletion.  In this construction -
- * for consistency - each delete request may contain only a single feature
- * type delete operation and filter.  This rationalizes the design of the 
- * transaction object (which contains one or more sub requests) and its 
- * children requests, which contain only a single operation.</p>
+ * <p>
+ * Defines a WFS delete request, which is a type transaction request.  Full
+ * transaction requests may contain one or more delete requests.  Note that
+ * this delete request object is slightly different than it is defined in the
+ * WFS 1.0 specification.  In the specification, the delete request may
+ * contain one or more filters and feature types for deletion.  In this
+ * construction - for consistency - each delete request may contain only a
+ * single feature type delete operation and filter.  This rationalizes the
+ * design of the  transaction object (which contains one or more sub requests)
+ * and its  children requests, which contain only a single operation.
+ * </p>
+ * 
+ * <p>
+ * Each delete request contains a type name, (optionally) a filter, and
+ * (optionally) a release all boolean.  Release all defines the action to
+ * preform on feature locks.  If it is specified as true, then all feature
+ * locks are released when the transaction terminates.  If it is specified as
+ * false, only those records that are modified are released.
+ * </p>
  *
- * <p>Each delete request contains a type name, (optionally) a filter, and 
- * (optionally) a release all boolean.  Release all defines the action to 
- * preform on feature locks.  If it is specified as true, then all feature 
- * locks are released when the transaction terminates.  If it is specified as 
- * false, only those records that are modified are released.</p>
- *
- *@author Rob Hranac, TOPP
- *@version $VERSION$
+ * @author Rob Hranac, TOPP
+ * @author Chris Holmes, TOPP
+ * @version $Id: DeleteRequest.java,v 1.4 2003/09/12 18:37:56 cholmesny Exp $
  */
-public class DeleteRequest 
-    extends SubTransactionRequest {
-
+public class DeleteRequest extends SubTransactionRequest {
     /** Standard logging instance for class */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.requests");
-    
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
+
     /** Specifies the output format */
     protected static final String operation = "Delete";
 
+    /** specifies the op type, will always be delete. */
     protected static short operationType = DELETE;
 
     /** Specifies the output format */
     protected String typeName = null;
-        
+
     /** Specifies the features to lock. */
     protected Filter filter = null;
 
     /** Specifices the user-defined name for the entire get feature request */
     protected boolean releaseAll = true;
 
+    /**
+     * Empty constructor.
+     */
+    public DeleteRequest() {
+    }
 
-    /** Empty constructor. */ 
-    public DeleteRequest () {}
-
-
-    /*************************************************************************
+    /* ***********************************************************************
      * SIMPLE GET AND SET METHODS                                            *
-     *************************************************************************/
+     * ***********************************************************************/
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public String getTypeName() { return typeName; }
+    /**
+     * G
+     *
+     * @return The feature type name.
+     */
+    public String getTypeName() {
+        return typeName;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public void setTypeName(String typeName) { 
-        this.typeName = typeName; }
+    /**
+     * Sets the name of type to delete.
+     *
+     * @param typeName The feature type name.
+     */
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public Filter getFilter() { return filter; }
+    /**
+     * Gets the filter of what features should be returned.
+     *
+     * @return The geotools filter to query against.
+     */
+    public Filter getFilter() {
+        return filter;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public void setFilter(Filter filter) { this.filter = filter; }
+    /**
+     * Sets the filter of what features should be returned.
+     *
+     * @param filter The geotools filter to query against.
+     */
+    public void setFilter(Filter filter) {
+        this.filter = filter;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public boolean getReleaseAll() { return releaseAll; }
+    /**
+     * Gets whether all locks should be released after this transaction.
+     *
+     * @return <tt>true</tt> if all locks should be released.
+     */
+    public boolean getReleaseAll() {
+        return releaseAll;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public void setReleaseAll(boolean releaseAll) { 
-        this.releaseAll = releaseAll; }
+    /**
+     * Sets whether all locks should be released after this transaction.
+     *
+     * @param releaseAll if all locks should be released.
+     */
+    public void setReleaseAll(boolean releaseAll) {
+        this.releaseAll = releaseAll;
+    }
 
-    /** Sets the expiration of the locks (in minutes). */ 
-    public String getOperation() { return operation; }
+    /**
+     * Returns what type of transaction operation this is.
+     *
+     * @return the string "Delete"
+     */
+    public String getOperation() {
+        return operation;
+    }
 
-    public short getOpType() { return operationType; }
+    /**
+     * Gets the short representation of this operation.
+     *
+     * @return the short representation of delete.
+     */
+    public short getOpType() {
+        return operationType;
+    }
 
-    /*************************************************************************
+    /* ***********************************************************************
      * OVERRIDES OF toString AND equals METHODS.                             *
-     *************************************************************************/
+     * ***********************************************************************/
     public String toString() {
         StringBuffer returnString = new StringBuffer("Delete Feature Request");
         String indent = "\n    ";
         returnString.append("\n feature type:" + typeName);
         returnString.append("\n filter:" + filter.toString());
         returnString.append("\n release:" + releaseAll);
+
         return returnString.toString();
     }
 
     public boolean equals(Object obj) {
-        if (obj != null && obj.getClass() == this.getClass()){
-	    DeleteRequest request = (DeleteRequest)obj;
-	    boolean isEqual = true;
-	    
-	    if(this.typeName != null) {
-	       if (this.typeName.equals(request.getTypeName())) {
-		isEqual = isEqual && true;
-	       } else {
-		   isEqual = false;
-	       }
-	    } else {
-		isEqual = isEqual && (request.getTypeName() == null);
-	    }
-	    LOGGER.finest("checking feature type names for equality: " 
-			  + isEqual);
-	    
-	    if(this.releaseAll == request.getReleaseAll()) {
-		isEqual = isEqual && true;
-	    } else {
-		isEqual = false;
-	    }
-	    LOGGER.finest("checking release action equality: " + isEqual);
-		
-	    if(this.filter != null) {
-		if (this.filter.equals(request.getFilter())) {
-		    isEqual = isEqual && true;
-		} else {
-		    isEqual = false;
-		}
-	    } else {
-		isEqual = isEqual && (request.getFilter() == null);
-	    }
-	    LOGGER.finest("checking filter equality: " + isEqual);
-	    
-	    return isEqual;
-	} else {
-	    return false;
-	}
-    }
-} 
+        if ((obj != null) && (obj.getClass() == this.getClass())) {
+            DeleteRequest request = (DeleteRequest) obj;
+            boolean isEqual = true;
 
+            if (this.typeName != null) {
+                if (this.typeName.equals(request.getTypeName())) {
+                    isEqual = isEqual && true;
+                } else {
+                    isEqual = false;
+                }
+            } else {
+                isEqual = isEqual && (request.getTypeName() == null);
+            }
+
+            LOGGER.finest("checking feature type names for equality: "
+                + isEqual);
+
+            if (this.releaseAll == request.getReleaseAll()) {
+                isEqual = isEqual && true;
+            } else {
+                isEqual = false;
+            }
+
+            LOGGER.finest("checking release action equality: " + isEqual);
+
+            if (this.filter != null) {
+                if (this.filter.equals(request.getFilter())) {
+                    isEqual = isEqual && true;
+                } else {
+                    isEqual = false;
+                }
+            } else {
+                isEqual = isEqual && (request.getFilter() == null);
+            }
+
+            LOGGER.finest("checking filter equality: " + isEqual);
+
+            return isEqual;
+        } else {
+            return false;
+        }
+    }
+}
