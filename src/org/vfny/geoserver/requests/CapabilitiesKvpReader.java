558b481cb50b8ededcb524214720c2215fb3f07a
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-12 18:23:14 +0000

    jalopied and pmd clean-up
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@494 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ee506411d275eaec9ec7a0eeaefd94b56affa48a [formerly 03fbc357a5c791e21d03f2b4ce44851aa2ebed54]
    Former-commit-id: 99b4b9c5e397fda618233ebe4186a7c62da961f8

--- src/org/vfny/geoserver/requests/CapabilitiesKvpReader.java
+++ src/org/vfny/geoserver/requests/CapabilitiesKvpReader.java
@@ -1,33 +1,37 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
 
 /**
- * This utility reads in a GetCapabilities KVP request and turns it into an 
+ * This utility reads in a GetCapabilities KVP request and turns it into an
  * appropriate internal CapabilitiesRequest object, upon request.
  *
  * @author Rob Hranac, TOPP
- * @version $VERSION$
+ * @version $Id: CapabilitiesKvpReader.java,v 1.2 2003/09/12 18:23:14 cholmesny Exp $
  */
-public class CapabilitiesKvpReader 
-    extends RequestKvpReader {
+public class CapabilitiesKvpReader extends RequestKvpReader {
+    /**
+     * Constructor with raw request string.  Calls parent.
+     *
+     * @param request The raw string of a capabilities kvp request.
+     */
+    public CapabilitiesKvpReader(String request) {
+        super(request);
+    }
 
-    /** Constructor with raw request string.  Calls parent. */
-    public CapabilitiesKvpReader (String request) { super(request); }
-    
     /**
      * Get Capabilities request.
-     * @returns Capabilities request.
+     *
+     * @return Capabilities request.
      */
-    public CapabilitiesRequest getRequest () {
+    public CapabilitiesRequest getRequest() {
         CapabilitiesRequest currentRequest = new CapabilitiesRequest();
         currentRequest.setVersion(((String) kvpPairs.get("VERSION")));
-        currentRequest.setService(((String) kvpPairs.get("SERVICE")));        
+        currentRequest.setService(((String) kvpPairs.get("SERVICE")));
+
         return currentRequest;
-    }    
+    }
 }
