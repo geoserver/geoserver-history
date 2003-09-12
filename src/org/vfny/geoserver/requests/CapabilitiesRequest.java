53cdce0f1e9e78a09e9226501faa641a7ec7dadf
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-12 18:23:48 +0000

    jalopied and cleaned, moved equals logic to Request
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@495 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bfb2d415aa626cfd44b5065553f7076928a3356d [formerly ac8f5bf1d7687f0692caa1d239a2ef7b177131e0]
    Former-commit-id: 1e40eddf17698d17c7b6525b6f6ce01fd55495c0

--- src/org/vfny/geoserver/requests/CapabilitiesRequest.java
+++ src/org/vfny/geoserver/requests/CapabilitiesRequest.java
@@ -1,34 +1,44 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.util.*;
-
 /**
  * This class enforces a standard interface for GetCapabilities reqeusts
- * 
- * @version $VERSION$
+ *
  * @author Rob Hranac, TOPP
+ * @author Chris Holmes, TOPP
+ * @version $Id: CapabilitiesRequest.java,v 1.2 2003/09/12 18:23:48 cholmesny Exp $
  */
-public class CapabilitiesRequest 
-    extends Request {
+public class CapabilitiesRequest extends Request {
+    /**
+     * Empty constructor.
+     */
+    public CapabilitiesRequest() {
+        super();
+    }
 
-    /** Empty constructor. */
-    public CapabilitiesRequest () { super(); }
-
-
-    /*************************************************************************
-     * Overrides of toString and equals methods.                             *
-     *************************************************************************/
+    /**
+     * Returns a string representation of this CapabilitiesRequest.
+     *
+     * @return a string of with the service and version.
+     */
     public String toString() {
-        return "GetCapabilities [service: " + 
-            service + ", version: " + version + "]";
-    }   
+        return "GetCapabilities [service: " + service + ", version: " + version
+        + "]";
+    }
 
-    public boolean equals(CapabilitiesRequest request) {
-        return this.getService().equals(request.getService()) &&
-            this.getVersion().equals(request.getVersion());
-    }   
+    /**
+     * Override of equals.  Just calls super.equals, since there are no extra
+     * fields here that aren't in Request.
+     *
+     * @param o the object to test against.
+     *
+     * @return <tt>true</tt> if o is equal to this request.
+     */
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
 }
