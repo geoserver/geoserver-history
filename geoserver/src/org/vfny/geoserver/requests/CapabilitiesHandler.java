73e3b8ce1b9a7d9c569181d12e5d298a7932da05
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-12 18:23:14 +0000

    jalopied and pmd clean-up
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@494 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3d49aad46b16389c924d4b0d9c5b561b9db8fc88 [formerly 45e481077a98819d502aa33ba4873438e89ff74c]
    Former-commit-id: f55f80b0de10ec7de7e28ce54e5d064e5d4e1624

--- geoserver/src/org/vfny/geoserver/requests/CapabilitiesHandler.java
+++ geoserver/src/org/vfny/geoserver/requests/CapabilitiesHandler.java
@@ -1,67 +1,67 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
-import org.xml.sax.ContentHandler;
 import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.XMLFilterImpl;
+import java.util.logging.Logger;
+
 
 /**
  * A SAX content handler that acquires a GetCapabilities request from an
- * incoming XML stream.  
+ * incoming XML stream.
  *
  * @author Rob Hranac, TOPP
- * @version $VERSION$
+ * @version $Id: CapabilitiesHandler.java,v 1.2 2003/09/12 18:23:14 cholmesny Exp $
  */
-public class CapabilitiesHandler 
-    extends XMLFilterImpl  
-    implements ContentHandler {
-    
+public class CapabilitiesHandler extends XMLFilterImpl implements ContentHandler {
     /** Class logger */
-    private static Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.requests");
+    private static Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
 
     /** Internal Capabilities request for construction. */
-    private CapabilitiesRequest request = new CapabilitiesRequest();    
+    private CapabilitiesRequest request = new CapabilitiesRequest();
 
     /**
      * Returns the GetCapabilities request.
-     * @returns GetCapabilities request.
-     */ 
-    public CapabilitiesRequest getRequest() { return request; }
+     *
+     * @return GetCapabilities request.
+     */
+    public CapabilitiesRequest getRequest() {
+        return request;
+    }
 
-    /*************************************************************************
+    /* ***********************************************************************
      *  Standard SAX content handler methods                                 *
-     *************************************************************************/
+     * ***********************************************************************/
+
     /**
-     * Notes the start of the element and sets version and service tags, as 
+     * Notes the start of the element and sets version and service tags, as
      * required.
+     *
      * @param namespaceURI URI for namespace appended to element.
      * @param localName Local name of element.
      * @param rawName Raw name of element.
      * @param atts Element attributes.
-     */ 
-    public void startElement(String namespaceURI, 
-                             String localName, 
-                             String rawName, 
-                             Attributes atts)
-                throws SAXException {
-        
-        if(localName.equals("GetCapabilities")) {
-            for(int i = 0, n = atts.getLength(); i < n; i++) {
-                if(atts.getLocalName(i).equals("version")) {
-                    request.setVersion(atts.getValue(i) );
-                } else if(atts.getLocalName(i).equals("service")) {
-                    request.setService(atts.getValue(i) );
+     *
+     * @throws SAXException For any standard SAX errors.
+     */
+    public void startElement(String namespaceURI, String localName,
+        String rawName, Attributes atts) throws SAXException {
+        if (localName.equals("GetCapabilities")) {
+            LOGGER.finer("found capabilities start.");
+
+            for (int i = 0, n = atts.getLength(); i < n; i++) {
+                if (atts.getLocalName(i).equals("version")) {
+                    request.setVersion(atts.getValue(i));
+                } else if (atts.getLocalName(i).equals("service")) {
+                    request.setService(atts.getValue(i));
                 }
             }
-        }   
+        }
     }
-    
 }
