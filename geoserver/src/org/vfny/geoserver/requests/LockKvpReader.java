c469c43142066607d015472e3c2e807e1f8cab47
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-15 16:29:52 +0000

    jalopied and cleaned for pmd
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@508 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e72eca306c1778b7fd37fdbaa03a621a9e038255 [formerly 7d6ec76018a00b555945ddd030b1cccba19fa185]
    Former-commit-id: fd9ddd9233a86a9f85c639b6e867061af811a4ad

--- geoserver/src/org/vfny/geoserver/requests/LockKvpReader.java
+++ geoserver/src/org/vfny/geoserver/requests/LockKvpReader.java
@@ -1,64 +1,70 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
-import org.geotools.filter.Filter;
-import org.geotools.filter.FidFilter;
 import org.vfny.geoserver.responses.WfsException;
+import java.util.List;
+import java.util.logging.Logger;
+
 
 /**
  * This utility reads in a LockFeature KVP request and turns it into an
  * appropriate internal Lock type request object.
- * 
+ *
  * @author Rob Hranac, TOPP
- * @version beta, 12/01/01
+ * @version $Id: LockKvpReader.java,v 1.4 2003/09/15 16:29:52 cholmesny Exp $
  */
-public class LockKvpReader 
-     extends RequestKvpReader {
-   
+public class LockKvpReader extends RequestKvpReader {
     /** Class logger */
-    private static final Logger LOGGER =  
-        Logger.getLogger("org.vfny.geoserver.requests");
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
 
+    /**
+     * Constructor with raw request string.  Calls parent.
+     *
+     * @param request The raw string to construct the request from.
+     */
+    public LockKvpReader(String request) {
+        super(request);
+    }
 
-    /** Constructor with raw request string.  Calls parent. */
-    public LockKvpReader (String request) { super(request); }
-    
-    
     /**
      * Returns Lock feature request object.
+     *
      * @return Lock request objects
+     *
+     * @throws WfsException For any problems forming the request.
      */
-    public LockRequest getRequest()
-        throws WfsException {
-        
-        LockRequest currentRequest = new LockRequest();       
+    public LockRequest getRequest() throws WfsException {
+        LockRequest currentRequest = new LockRequest();
 
         // set global request parameters
         LOGGER.finest("setting global request parameters");
-        if( kvpPairs.containsKey("VERSION")) {
+
+        if (kvpPairs.containsKey("VERSION")) {
             currentRequest.setVersion((String) kvpPairs.get("VERSION"));
         }
-        if( kvpPairs.containsKey("REQUEST")) {
+
+        if (kvpPairs.containsKey("REQUEST")) {
             currentRequest.setRequest((String) kvpPairs.get("REQUEST"));
         }
-        if( kvpPairs.containsKey("EXPIRY")) {
-            currentRequest.
-                setExpiry(Integer.parseInt((String) kvpPairs.get("EXPIRY")));
+
+        if (kvpPairs.containsKey("EXPIRY")) {
+            currentRequest.setExpiry(Integer.parseInt(
+                    (String) kvpPairs.get("EXPIRY")));
         }
-        if( kvpPairs.containsKey("LOCKACTION")) {
+
+        if (kvpPairs.containsKey("LOCKACTION")) {
             String lockAction = (String) kvpPairs.get("LOCKACTION");
-            if(lockAction == null) {
+
+            if (lockAction == null) {
                 currentRequest.setLockAll(true);
-            } else if(lockAction.toUpperCase().equals("ALL")) {
-                currentRequest.setLockAll(true);                
-            } else if(lockAction.toUpperCase().equals("SOME")) {
-                currentRequest.setLockAll(false);                
+            } else if (lockAction.toUpperCase().equals("ALL")) {
+                currentRequest.setLockAll(true);
+            } else if (lockAction.toUpperCase().equals("SOME")) {
+                currentRequest.setLockAll(false);
             } else {
                 throw new WfsException("Illegal lock action: " + lockAction);
             }
@@ -66,30 +72,35 @@ public class LockKvpReader
 
         // declare tokenizers for repeating elements
         LOGGER.finer("setting query request parameters");
-        List typeList = readFlat((String)kvpPairs.get("TYPENAME"), 
-                                 INNER_DELIMETER);
-        LOGGER.finer("type list size: " + typeList.size());      
-        List filterList = readFilters((String) kvpPairs.get("FEATUREID"), 
-                                      (String) kvpPairs.get("FILTER"),
-                                      (String) kvpPairs.get("BBOX"));
-	if (typeList.size() == 0) {
-	    typeList = getTypesFromFids((String) kvpPairs.get("FEATUREID"));
-	    if (typeList.size() == 0) {
-		throw new WfsException("The typename element is mandatory if "
-				       + "no FEATUREID is present");
-	    }
-	}
+
+        List typeList = readFlat((String) kvpPairs.get("TYPENAME"),
+                INNER_DELIMETER);
+        LOGGER.finer("type list size: " + typeList.size());
+
+        List filterList = readFilters((String) kvpPairs.get("FEATUREID"),
+                (String) kvpPairs.get("FILTER"), (String) kvpPairs.get("BBOX"));
+
+        if (typeList.size() == 0) {
+            typeList = getTypesFromFids((String) kvpPairs.get("FEATUREID"));
+
+            if (typeList.size() == 0) {
+                throw new WfsException("The typename element is mandatory if "
+                    + "no FEATUREID is present");
+            }
+        }
+
         int featureSize = typeList.size();
         int filterSize = filterList.size();
-        
+
         // check for errors in the request
-        if((filterSize != featureSize) && (filterSize > 0) || 
-           ((filterSize > 0) && (featureSize == 0))) {
-            throw new WfsException("Filter size does not match" +  
-                                   " feature types.  Filter size: " + 
-                                   filterSize + " Feature size: "+featureSize);
+        if (((filterSize != featureSize) && (filterSize > 0))
+                || ((filterSize > 0) && (featureSize == 0))) {
+            throw new WfsException("Filter size does not match"
+                + " feature types.  Filter size: " + filterSize
+                + " Feature size: " + featureSize);
         } else {
-	    currentRequest.setLocks(typeList, filterList);
+            currentRequest.setLocks(typeList, filterList);
+
             return currentRequest;
         }
     }
