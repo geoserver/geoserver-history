98c0e17b97ef29fd0e42f37b0fc7daba5d057a79
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-15 23:07:16 +0000

    jalopied for 1.0 release
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@515 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ca8b31f28284f952e9ef6f577d0ad5445b257a8e [formerly e58ef21cd99cf461dab76012fe8f050d8a66dd5b]
    Former-commit-id: 10c802424d5e0a40eeb14e6cd01bae4ee6ad8a2e

--- geoserver/src/org/vfny/geoserver/servlets/Capabilities.java
+++ geoserver/src/org/vfny/geoserver/servlets/Capabilities.java
@@ -1,119 +1,113 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.servlets;
 
-import java.io.*;
-import java.util.*;
+import org.vfny.geoserver.config.ConfigInfo;
+import org.vfny.geoserver.requests.CapabilitiesKvpReader;
+import org.vfny.geoserver.requests.CapabilitiesRequest;
+import org.vfny.geoserver.requests.XmlRequestReader;
+import org.vfny.geoserver.responses.CapabilitiesResponse;
+import org.vfny.geoserver.responses.WfsException;
+import java.io.IOException;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.vfny.geoserver.config.ConfigInfo;
-import org.vfny.geoserver.requests.XmlRequestReader;
-import org.vfny.geoserver.requests.CapabilitiesRequest;
-import org.vfny.geoserver.requests.CapabilitiesKvpReader;
-import org.vfny.geoserver.responses.CapabilitiesResponse;
-import org.vfny.geoserver.responses.WfsException;
+
 
 /**
- * Implements the WFS GetCapabilities interface, which tells clients what the 
+ * Implements the WFS GetCapabilities interface, which tells clients what the
  * server can do.
  *
- *@author Rob Hranac, TOPP
- *@version $Version$
+ * @author Rob Hranac, TOPP
+ * @version $Id: Capabilities.java,v 1.5 2003/09/15 23:07:16 cholmesny Exp $
  */
-public class Capabilities
-    extends HttpServlet {
-
+public class Capabilities extends HttpServlet {
     /** Class logger */
-    private static Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.servlets");
-    
+    private static Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.servlets");
+
     /** Specifies mime type */
-    private static final String MIME_TYPE =
-	ConfigInfo.getInstance().getMimeType();
-    
-    
+    private static final String MIME_TYPE = ConfigInfo.getInstance()
+                                                      .getMimeType();
+
     /**
      * Handles all XML POST requests for GetCapabilities.
+     *
      * @param request The servlet request object.
      * @param response The servlet response object.
-     */ 
-    public void doPost(HttpServletRequest request,HttpServletResponse response)
+     *
+     * @throws ServletException For servlet problems.
+     * @throws IOException For problems writing.
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        
         // create temporary response string
         String tempResponse = new String();
-        
+
         // implements the main request/response logic
         try {
-            CapabilitiesRequest wfsRequest = 
-                XmlRequestReader.readGetCapabilities(request.getReader());
-            CapabilitiesResponse wfsResponse =  
-                new CapabilitiesResponse(wfsRequest);
+            CapabilitiesRequest wfsRequest = XmlRequestReader
+                .readGetCapabilities(request.getReader());
+            CapabilitiesResponse wfsResponse = new CapabilitiesResponse(wfsRequest);
             tempResponse = wfsResponse.getXmlResponse();
         }
-        
         // catches all errors; client should never see a stack trace 
-        catch (WfsException wfs) {
+         catch (WfsException wfs) {
             tempResponse = wfs.getXmlResponse();
-        }
-	catch (Throwable e) {
-	    WfsException wfse = new WfsException(e, "UNCAUGHT EXCEPTION",
-						 null);
-       	    tempResponse = wfse.getXmlResponse(true);
+        } catch (Throwable e) {
+            WfsException wfse = new WfsException(e, "UNCAUGHT EXCEPTION", null);
+            tempResponse = wfse.getXmlResponse(true);
             LOGGER.info("Had an undefined error: " + e.getMessage());
-	    e.printStackTrace();
+            e.printStackTrace();
         }
-        
+
         // set content type and return response, whatever it is 
         response.setContentType(MIME_TYPE);
-	LOGGER.finest("response encoding is " + response.getCharacterEncoding());
+        LOGGER.finest("response encoding is " + response.getCharacterEncoding());
         response.getWriter().write(tempResponse);
     }
-    
-    
+
     /**
      * Handles all KVP GET request for GetCapabilities.
+     *
      * @param request The servlet request object.
      * @param response The servlet response object.
+     *
+     * @throws ServletException For servlet problems.
+     * @throws IOException For problems writing.
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-
         // create temporary response string
         String tempResponse = new String();
-        
+
         // implements the main request/response logic
         try {
-            CapabilitiesKvpReader currentKvpRequest = 
-                new CapabilitiesKvpReader(request.getQueryString());
+            CapabilitiesKvpReader currentKvpRequest = new CapabilitiesKvpReader(request
+                    .getQueryString());
             CapabilitiesRequest wfsRequest = currentKvpRequest.getRequest();
-            CapabilitiesResponse wfsResponse = 
-                new CapabilitiesResponse(wfsRequest);
+            CapabilitiesResponse wfsResponse = new CapabilitiesResponse(wfsRequest);
             tempResponse = wfsResponse.getXmlResponse();
         }
-        
         // catches all errors; client should never see a stack trace 
-        catch (WfsException wfs) {
+         catch (WfsException wfs) {
             tempResponse = wfs.getXmlResponse();
-        }
-	catch (Throwable e) {
-	    WfsException wfse = new WfsException(e, "UNCAUGHT EXCEPTION",
-						 null);
-       	    tempResponse = wfse.getXmlResponse(true);
+        } catch (Throwable e) {
+            WfsException wfse = new WfsException(e, "UNCAUGHT EXCEPTION", null);
+            tempResponse = wfse.getXmlResponse(true);
             LOGGER.info("Had an undefined error: " + e.getMessage());
-	    e.printStackTrace();
+            e.printStackTrace();
         }
+
         LOGGER.finer("response is " + tempResponse);
+
         // set content type and return response, whatever it is 
         response.setContentType(MIME_TYPE);
-	LOGGER.finer("response encoding is " + response.getCharacterEncoding());
-        response.getWriter().write(tempResponse);        
+        LOGGER.finer("response encoding is " + response.getCharacterEncoding());
+        response.getWriter().write(tempResponse);
     }
 }
-
-
--- geoserver/src/org/vfny/geoserver/servlets/FreefsLog.java
+++ geoserver/src/org/vfny/geoserver/servlets/FreefsLog.java
@@ -1,102 +1,86 @@
-/* Copyright (c) 2001 Vision for New York - www.vfny.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root application directory.
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
  */
-
 package org.vfny.geoserver.servlets;
 
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.Iterator;
-import javax.servlet.http.*;
-//import javax.servlet.*;
-import org.geotools.resources.Geotools;
-import org.geotools.resources.Log4JFormatter;
-import org.geotools.resources.MonolineFormatter;
 import org.vfny.geoserver.config.ConfigInfo;
-import org.vfny.geoserver.zserver.GeoZServer;
 import org.vfny.geoserver.config.TypeRepository;
+import org.vfny.geoserver.zserver.GeoZServer;
+
 //Logging system
 import java.util.logging.Logger;
-import java.util.logging.Level;
-import java.util.prefs.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Initializes all logging functions.
- * 
+ *
  * @author Rob Hranac, Vision for New York
  * @author Chris Holmes, TOPP
- * @version 0.95 beta, 4/24/03
- *
+ * @version $Id: FreefsLog.java,v 1.12 2003/09/15 23:07:16 cholmesny Exp $
  */
 public class FreefsLog extends HttpServlet {
-
-    /** Change this variable for different amounts of log messages.  Options
-       include SEVERE, WARNING, INFO, FINER, FINEST (in order) */
-    private static Level loggingLevel = Level.FINER;
-
     /** Standard logging instance for class */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.servlet");
-   
-    /** Default name for configuration file */
-    private static final String CONFIG_FILE =  "configuration.xml";
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.servlet");
+
     /** Default name for configuration directory */
-    private static final String CONFIG_DIR =  "data/";
+    private static final String CONFIG_DIR = "data/";
 
-    private static final String BACKING_STORE_AVAIL = "BackingStoreAvail";
-    
-    /**
-     * The logger for the filter module.
-     */
-    private static final Logger LOG = Logger.getLogger("org.vfny.geoserver.servlets");
-   
-
+    /** The logger for the filter module. */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.servlets");
     private GeoZServer server;
+
     /**
      * Initializes logging and config.
-     *
-     */ 
+     */
     public void init() {
-	//HACK: java.util.prefs are awful.  See 
-	//http://www.allaboutbalance.com/disableprefs.  When the site comes 
-	//back up we should implement their better way of fixing the problem.
-	System.setProperty("java.util.prefs.syncInterval", "5000000");
-	String root = this.getServletContext().getRealPath("/");
-	String path = root + CONFIG_DIR;
-	LOG.finer("init with path: " + path);
-	ConfigInfo cfgInfo = ConfigInfo.getInstance(path);
-	if (cfgInfo.runZServer()) {
-	    try {
-		server = new GeoZServer(cfgInfo.getZServerProps());
-		server.start();
-	    } catch (java.io.IOException e) {
-		LOGGER.info("zserver module could not start: " + e.getMessage());
-	    }
-	}	
+        //HACK: java.util.prefs are awful.  See 
+        //http://www.allaboutbalance.com/disableprefs.  When the site comes 
+        //back up we should implement their better way of fixing the problem.
+        System.setProperty("java.util.prefs.syncInterval", "5000000");
+
+        String root = this.getServletContext().getRealPath("/");
+        String path = root + CONFIG_DIR;
+        LOGGER.finer("init with path: " + path);
+
+        ConfigInfo cfgInfo = ConfigInfo.getInstance(path);
+
+        if (cfgInfo.runZServer()) {
+            try {
+                server = new GeoZServer(cfgInfo.getZServerProps());
+                server.start();
+            } catch (java.io.IOException e) {
+                LOGGER.info("zserver module could not start: " + e.getMessage());
+            }
+        }
     }
-    
-    
+
     /**
      * Initializes logging.
      *
      * @param req The servlet request object.
-     * @param resp The servlet response object.
-     */ 
+     * @param res The servlet response object.
+     */
     public void doGet(HttpServletRequest req, HttpServletResponse res) {
         //BasicConfigurator.configure();
     }
 
     /**
      * Closes down the zserver if it is running, and frees up resources.
-     */    
+     */
     public void destroy() {
-	super.destroy();
-	TypeRepository.getInstance().closeTypeResources();
-	
-	LOGGER.finer("shutting down zserver");
-    if (server != null) server.shutdown(1);
-    
-    }
+        super.destroy();
+        TypeRepository.getInstance().closeTypeResources();
 
+        LOGGER.finer("shutting down zserver");
+
+        if (server != null) {
+            server.shutdown(1);
+        }
+    }
 }
