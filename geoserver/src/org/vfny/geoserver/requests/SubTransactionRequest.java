389c0f80ef4ad1c8605feb26d6aa73c2c3defa25
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-15 18:27:37 +0000

    jalopied and pmd cleaned
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@510 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 84dd23e4d3b5f4d9a27252574d0bee91103d2e81 [formerly d9187437461765fd3da3e82d330cc47cc8128510]
    Former-commit-id: 5f04d5503db9d8447d8d8ff3e4ec4cca23e29797

--- geoserver/src/org/vfny/geoserver/requests/SubTransactionRequest.java
+++ geoserver/src/org/vfny/geoserver/requests/SubTransactionRequest.java
@@ -1,49 +1,48 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
 import org.geotools.filter.Filter;
 import org.vfny.geoserver.responses.WfsException;
 
 /**
  * Parent class for Update, Insert, and Delete.
  *
- * @version $VERSION$
  * @author Rob Hranac, TOPP
- * @author Chris Holmes, TOPP 
+ * @author Chris Holmes, TOPP
+ * @version $Id: SubTransactionRequest.java,v 1.3 2003/09/15 18:27:37 cholmesny Exp $
  */
 public abstract class SubTransactionRequest {
-
     public static final short UPDATE = 0;
-
     public static final short INSERT = 1;
-
     public static final short DELETE = 2;
-
-    protected String handle = null; 
-
-    protected String typeName = null; 
-
     private static short operationType;
+    protected String handle = null;
+    protected String typeName = null;
 
-    public SubTransactionRequest() {}
+    public SubTransactionRequest() {
+    }
 
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
 
-    public void setHandle(String handle) { this.handle = handle; }
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 
-    public void setTypeName(String typeName) { this.typeName = typeName; }
+    public void setFilter(Filter filter) throws WfsException {
+    }
 
-    public void setFilter(Filter filter) throws WfsException {}
+    public String getTypeName() {
+        return typeName;
+    }
 
-    public String getTypeName() { return typeName; }
+    public String getHandle() {
+        return handle;
+    }
 
-    public String getHandle() { return handle; }
-   
     public abstract short getOpType();
-    
 }
