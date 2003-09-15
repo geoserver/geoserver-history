59b6c7f9c686c859984b34752fc39b59415fc239
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-15 16:29:52 +0000

    jalopied and cleaned for pmd
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@508 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 502368bc0ea91658dd147a17b9322adfd248f6da [formerly 61fa7b0d3ae055ebaff6596c5811ce87698f2fc2]
    Former-commit-id: fd3c8642df6313ad62f9569236e64bea128bfc7e

--- src/org/vfny/geoserver/requests/FilterHandlerImpl.java
+++ src/org/vfny/geoserver/requests/FilterHandlerImpl.java
@@ -1,59 +1,54 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.XMLFilterImpl;
 import org.geotools.filter.Filter;
 import org.geotools.filter.FilterHandler;
-import org.vfny.geoserver.responses.WfsException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.XMLFilterImpl;
+import java.util.logging.Logger;
+
 
 /**
  * Minimal class to implement the FilterHandler interface.
- * 
+ *
  * @author Rob Hranac, TOPP
- * @version $VERSION$
+ * @version $Id: FilterHandlerImpl.java,v 1.2 2003/09/15 16:29:52 cholmesny Exp $
  */
-public class FilterHandlerImpl 
-    extends XMLFilterImpl  
-    implements ContentHandler, FilterHandler {
-
+public class FilterHandlerImpl extends XMLFilterImpl implements ContentHandler,
+    FilterHandler {
     /** Class logger */
-    private static Logger LOGGER = 
-        Logger.getLogger("org.vfny.geoserver.requests");
-       
+    private static Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
+
     /** Tracks current filter */
     private Filter currentFilter = null;
 
-
-    /** Empty constructor. */
-    public FilterHandlerImpl () { super(); }
-    
-    
-    /*************************************************************************
-     *  Standard SAX content handler methods                                 *
-     *************************************************************************/
     /**
-     * Gets filter.
+     * Empty constructor.
+     */
+    public FilterHandlerImpl() {
+        super();
+    }
+
+    /**
+     * Recieves the filter from the filter parsing children.
+     *
      * @param filter (OGC WFS) Filter from (SAX) filter..
-     */ 
+     */
     public void filter(Filter filter) {
         LOGGER.finest("found filter: " + filter.toString());
         currentFilter = filter;
-    }    
+    }
 
     /**
-     * Gives filter.
-     * @returns (OGC WFS) Filter from (SAX) filter..
-     */ 
+     * Gives filter to whoever wants it.
+     *
+     * @return (OGC WFS) Filter from (SAX) filter..
+     */
     public Filter getFilter() {
         return currentFilter;
-    }    
+    }
 }
--- src/org/vfny/geoserver/requests/InsertRequest.java
+++ src/org/vfny/geoserver/requests/InsertRequest.java
@@ -1,129 +1,138 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.requests;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import java.util.List;
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.logging.Logger;
-import org.geotools.filter.Filter;
 import org.geotools.feature.Feature;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureCollections;
+import org.geotools.filter.Filter;
 import org.vfny.geoserver.responses.WfsException;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
 
 /**
- * Represents a request for an insert operation.  Does some type checking
- * by making sure that all features added have the same schema names 
- * (which is also the type name).  
- * TODO: add increased typechecking, make sure schemas match one another.
+ * Represents a request for an insert operation.  Does some type checking by
+ * making sure that all features added have the same schema names  (which is
+ * also the type name).   TODO: add increased typechecking, make sure schemas
+ * match one another.
  *
- * @version $VERSION$
  * @author Rob Hranac, TOPP
  * @author Chris Holmes, TOPP
+ * @version $Id: InsertRequest.java,v 1.6 2003/09/15 16:29:52 cholmesny Exp $
  */
-public class InsertRequest 
-    extends SubTransactionRequest {
-
-    
+public class InsertRequest extends SubTransactionRequest {
     /** Class logger */
-    private static final Logger LOGGER =  
-        Logger.getLogger("org.vfny.geoserver.requests");
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests");
 
     /** Indicates that this is an insert request. */
     public static final short operationType = INSERT;
 
-    /** The list of features to be inserted.*/
-    public FeatureCollection features;
+    /** The list of features to be inserted. */
+    private FeatureCollection features;
 
     /** flag to tell is all locked features should be released. */
     private boolean releaseAll;
-    
+
     /** Specifies the table of features */
     protected String typeName = null;
 
-    /** Empty constructor. */
-    public InsertRequest() { 
-	features = FeatureCollections.newCollection();
+    /**
+     * Empty constructor.
+     */
+    public InsertRequest() {
+        features = FeatureCollections.newCollection();
     }
 
-    
-    /** Gets whether all locked features should be released after
-     this transaction, or only those that were affected. */ 
-    public boolean getReleaseAll() { return releaseAll; }
+    /**
+     * Gets whether all locked features should be released after this
+     * transaction, or only those that were affected.
+     *
+     * @return <tt>true</tt> if all locked features should be released.
+     */
+    public boolean getReleaseAll() {
+        return releaseAll;
+    }
 
-    /** Sets  whether all locked features should be released after
-     this transaction, or only those that were affected. */ 
-    public void setReleaseAll(boolean releaseAll) { 
-        this.releaseAll = releaseAll; }
+    /**
+     * Sets  whether all locked features should be released after this
+     * transaction, or only those that were affected.
+     *
+     * @param releaseAll whether all locked features should be released.
+     */
+    public void setReleaseAll(boolean releaseAll) {
+        this.releaseAll = releaseAll;
+    }
 
-    /** 
+    /**
      * Adds a feature to this insert request.  Currently fairly permissive,
-     * just checks that the typenames match.
-     * The datasource will eventually complain, but it would be nice to
-     * do some more type-checking, to make sure the schemas match.
+     * just checks that the typenames match. The datasource will eventually
+     * complain, but it would be nice to do some more type-checking, to make
+     * sure the schemas match.
      *
      * @param feature To be inserted into the database.
+     *
      * @throws WfsException if added typeName does not match the set typeNames.
      */
     public void addFeature(Feature feature) throws WfsException {
-	if (typeName == null) {
-	    features.add(feature);
-	    typeName = feature.getFeatureType().getTypeName();
-	} else {
-	    String addTypeName = feature.getFeatureType().getTypeName();
-	    if(typeName.equals(addTypeName)){
-		features.add(feature);
-	    } else {
-		throw new WfsException("features do not match- added typeName: "
-				       + addTypeName + ", set typeName: " +
-				       typeName, handle);
-	    }
-	}
+        if (typeName == null) {
+            features.add(feature);
+            setTypeName(feature.getFeatureType().getTypeName());
+        } else {
+            String addTypeName = feature.getFeatureType().getTypeName();
 
+            if (typeName.equals(addTypeName)) {
+                features.add(feature);
+            } else {
+                throw new WfsException(
+                    "features do not match- added typeName: " + addTypeName
+                    + ", set typeName: " + typeName, handle);
+            }
+        }
     }
 
-    /** 
+    /**
      * Convenience method to add an array of features.  See addFeature.
      *
      * @param features array of features to be inserted.
+     *
      * @throws WfsException if the typeNames don't match.
      */
     public void addFeatures(Feature[] features) throws WfsException {
-	for (int i = 0; i < features.length; i++) {
-	    addFeature(features[i]);
-	}
+        for (int i = 0; i < features.length; i++) {
+            addFeature(features[i]);
+        }
     }
-    
+
     /**
      * Returns the type name of the features held in this request.
      *
      * @return the typeName.
      */
-    public String getTypeName(){
-	return typeName;
+    public String getTypeName() {
+        return typeName;
     }
 
     /**
-     * Sets the name.  This method should generally not be used, as
-     * features that are added set their own name and throw exceptions
-     * if they don't match the typename.  But this can be set before
-     * adding features if you want to ensure that they all match this
-     * name.
+     * Sets the name.  This method should generally not be used, as features
+     * that are added set their own name and throw exceptions if they don't
+     * match the typename.  But this can be set before adding features if you
+     * want to ensure that they all match this name.
+     *
      * @param typeName the name of the schema of the added features.
+     */
     public void setTypeName(String typeName) {
-	if (this.typeName == null || this.typeName.equals(typeName)) {
-	    this.typeName = typeName;
-	} else {
-	    //throw exception?  do nothing?  We should not
-	    //be setting a different type name if the typeName of
-	    //the schemas is different.  To throw exception we must
-	    //modify SubTransactionRequest.
-	}
+        if ((this.typeName == null) || this.typeName.equals(typeName)) {
+            this.typeName = typeName;
+        } else {
+            //throw exception?  do nothing?  We should not
+            //be setting a different type name if the typeName of
+            //the schemas is different.  To throw exception we must
+            //modify SubTransactionRequest.
+        }
     }
 
     /**
@@ -131,73 +140,94 @@ public class InsertRequest
      *
      * @return the array of features.
      */
-    public FeatureCollection getFeatures(){
-	return features;
+    public FeatureCollection getFeatures() {
+        return features;
     }
 
     /**
-     * Filters can not be added to an insert request.
+     * Filters can not be added to an insert request.  This is just an override
+     * of the setFilter method that throws an exception if called.
+     *
+     * @param filter a filter.
      *
      * @throws WfsException if called at all.
      */
-    public void setFilter(Filter filter)
-        throws WfsException {
-        throw new WfsException("Attempted to add filter (" + filter + 
-                               ") to update request: " + handle);
+    public void setFilter(Filter filter) throws WfsException {
+        throw new WfsException("Attempted to add filter (" + filter
+            + ") to update request: " + handle);
     }
 
-    /** returns the insert short. */
-    public short getOpType() { return operationType; }
-   
     /**
-     * gets the string representation of this request.
+     * Returns the insert short.
+     *
+     * @return the short representation of INSERT.
+     */
+    public short getOpType() {
+        return operationType;
+    }
+
+    /**
+     * Gets the string representation of this request.
+     *
+     * @return A string of the handle, releaseAll status, typeName and features
+     *         in this request.
      */
     public String toString() {
-	StringBuffer iRequest = new StringBuffer("Handle: " + handle);
-	iRequest.append("\nReleaseAll: " + releaseAll);
-	iRequest.append("\nTypeName: " + typeName + "\n");
-	Iterator featIter = features.iterator();
-	while (featIter.hasNext()){
-	    iRequest.append(featIter.next().toString() + "\n");
-	}
-	//for (int i = 0; i < features.size(); i++) {
-	//  iRequest.append(features.get(i).toString() + "\n");
-	//}
-	return iRequest.toString();
-    } 
+        StringBuffer iRequest = new StringBuffer("Handle: " + handle);
+        iRequest.append("\nReleaseAll: " + releaseAll);
+        iRequest.append("\nTypeName: " + typeName + "\n");
+
+        Iterator featIter = features.iterator();
+
+        while (featIter.hasNext()) {
+            iRequest.append(featIter.next().toString() + "\n");
+        }
+
+        return iRequest.toString();
+    }
 
     /**
-     * This method is currently broken, because geotools has not
-     * yet implemented equals methods for features.
+     * Override of equals.
+     *
+     * @param obj the object to test for equality.
+     *
+     * @return <tt>true</tt> if obj is equal to this.
+     *
+     * @task REVISIT: I think the feature iteration of geotools still will not
+     *       work, as it checks the featureID, but in an insert request the
+     *       fid is ignored.
      */
-     public boolean equals(Object obj) {
-	if (obj != null && obj.getClass() == this.getClass()){
-	    InsertRequest testInsert = (InsertRequest)obj;
-	    boolean isEqual = true;
-	    if(this.handle != null) {
-		isEqual = this.handle.equals(testInsert.handle);
-	    } else {
-		isEqual = (testInsert == null);
-	    }
-	    LOGGER.finest("handles are equal: " + isEqual);
-	    isEqual = (this.releaseAll == testInsert.releaseAll) && isEqual;
-	    LOGGER.finest("releaseAll equal: " + isEqual);
-	    
-	    if(this.features.size() == testInsert.features.size()){
-		//TODO: iterator through each collection.  THis will be
-		//better when datasources return FeatureDocument.
-		//for(int i = 0; i < testInsert.features.size(); i++) {
-		//  isEqual = isEqual && this.features.contains
-		//(testInsert.features.get(i));
-		//}
-	    } else {
-		isEqual = false;
-	    }
-	    LOGGER.finest("features are equal " + isEqual);
-	    return isEqual;
-	} else {
-	    return false;
-	}
+    public boolean equals(Object obj) {
+        if (!(obj instanceof InsertRequest)) {
+            return false;
+        }
+
+        InsertRequest testInsert = (InsertRequest) obj;
+        boolean isEqual = true;
+
+        if (this.handle != null) {
+            isEqual = this.handle.equals(testInsert.handle);
+        } else {
+            isEqual = (testInsert == null);
+        }
+
+        LOGGER.finest("handles are equal: " + isEqual);
+        isEqual = (this.releaseAll == testInsert.releaseAll) && isEqual;
+        LOGGER.finest("releaseAll equal: " + isEqual);
+
+        if (this.features.size() == testInsert.getFeatures().size()) {
+            //TODO: iterator through each collection.  THis will be
+            //better when datasources return FeatureDocument.
+            //for(int i = 0; i < testInsert.getFeatures().size(); i++) {
+            //isEqual = isEqual && this.features.contains
+            //(testInsert.getFeatures().get(i));
+            //}
+        } else {
+            isEqual = false;
+        }
+
+        LOGGER.finest("features are equal " + isEqual);
+
+        return isEqual;
     }
-    
 }
