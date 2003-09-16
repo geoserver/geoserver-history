dc89d07a8a720a6995650ca1cbf3da090a829a44
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-16 16:13:20 +0000

    added some comments, jalopied and pmd cleaned
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@522 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e625b13105f0003936dd422cef371f036659267d [formerly 3003b0ef81f00abcd2fa4d9eb4514e08363418e5]
    Former-commit-id: 80711c292db079576c3eab4d71bcb7f36a294648

--- geoserver/test/org/vfny/geoserver/responses/FeatureSuite.java
+++ geoserver/test/org/vfny/geoserver/responses/FeatureSuite.java
@@ -1,97 +1,108 @@
-/* Copyright (c) 2001 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root 
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
 package org.vfny.geoserver.responses;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.PrecisionModel;
-import org.geotools.filter.FilterFactory;
-import org.geotools.filter.Filter;
 import org.geotools.filter.AbstractFilter;
-import org.geotools.filter.FidFilter;
-import org.geotools.filter.GeometryFilter;
+import org.geotools.filter.AttributeExpression;
 import org.geotools.filter.CompareFilter;
 import org.geotools.filter.DefaultExpression;
-import org.geotools.filter.AttributeExpression;
+import org.geotools.filter.FilterFactory;
 import org.geotools.filter.LiteralExpression;
-import org.geotools.resources.Geotools;
-import org.vfny.geoserver.config.TypeRepository;
 import org.vfny.geoserver.config.ConfigInfo;
 import org.vfny.geoserver.requests.FeatureRequest;
 import org.vfny.geoserver.requests.Query;
+import java.util.logging.Logger;
+
 
 /**
- * Tests the get feature request handling.
+ * Tests the get feature response handling.  This class needs a good
+ * amount of work, but the cite test engine ends up testing things
+ * extensively, so it's not a very high priority.  For now it also
+ * relies on keeping the sample road feature in the misc/data/featureTypes
+ * directory, so we don't have to maintain multiple copies of that file.
+ * Ideally for response unit testing we should have a shapefile and
+ * a shp2pgsql script of that shapefile that will create the same
+ * features.  Perhaps parameter of which test to use, which would 
+ * use different featureType dirs, or just different info files (like
+ * info-pg.xml and info-shp.xml instead of info.xml, this would probably
+ * take some tweaking though). This would be nice to extend to all
+ * potential datasources, a sort of sanity check to make sure that a new
+ * pluggable datasource actually does everything needed by geoserver.
+ * <p>We also need a way to assert the returned test data, perhaps just
+ * using dom, or maybe the gt2 gml datasource.
  *
  * @author Rob Hranac, TOPP
- * @version $VERSION$
+ * @author Chris Holmes, TOPP
+ * @version $Id: FeatureSuite.java,v 1.4 2003/09/16 16:13:20 cholmesny Exp $
  */
 public class FeatureSuite extends TestCase {
+    //Initializes the logger. Uncomment to see log messages.
+    //static {
+    //org.vfny.geoserver.config.Log4JFormatter.init("org.vfny.geoserver", 
+    //java.util.logging.Level.FINE);
+    //}
 
-    /* Initializes the logger. */
-    static {
-        Geotools.init("Log4JFormatter", Level.FINEST);
-    }
-    
     /** Class logger */
-    private static final Logger LOGGER =  
-        Logger.getLogger("org.vfny.geoserver.requests");
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.responses");
 
     /** Unit test data directory */
-    private static final String CONFIG_DIR = 
-        System.getProperty("user.dir") + "/misc/unit/config/";
+    private static final String CONFIG_DIR = System.getProperty("user.dir")
+        + "/misc/unit/config/";
 
     /** Unit test data directory */
-    private static final String TYPE_DIR = 
-        System.getProperty("user.dir") + "/misc/data/featureTypes";
-
+    private static final String TYPE_DIR = System.getProperty("user.dir")
+        + "/misc/data/featureTypes";
 
     /** Holds mappings between HTTP and ASCII encodings */
     private static FilterFactory factory = FilterFactory.createFilterFactory();
-
     private ConfigInfo config;
-    private TypeRepository repo;
 
+    /**
+     * Constructor with super.
+     *
+     * @param testName The name of the test.
+     */
+    public FeatureSuite(String testName) {
+        super(testName);
+    }
 
-    /** Constructor with super. */
-    public FeatureSuite (String testName) { super(testName); }
-
-
-    /** Handles test set up details. */
+    /**
+     * Handles test set up details.
+     */
     public void setUp() {
         config = ConfigInfo.getInstance(CONFIG_DIR);
         config.setTypeDir(TYPE_DIR);
-        repo = TypeRepository.getInstance();
     }
 
-    /*************************************************************************
-     * XML TESTS                                                             *
-     *************************************************************************
-     * XML GetFeature parsing tests.  Each test reads from a specific XML    *
-     * file and compares it to the base request defined in the test itself.  *
-     * Tests are run via the static methods in this suite.  The tests        *
-     * themselves are quite generic, so documentation is minimal.            *
-     *************************************************************************/
-    public void test1() throws Exception {        
+    public void test1() throws Exception {
         // make base comparison objects
         Query query = new Query();
-        query.setTypeName("rail");
-        FeatureRequest request = new FeatureRequest();
-        request.addQuery(query);       
+        query.setTypeName("road");
 
-        //String gml = FeatureResponse.getXmlResponse(request);
-        //LOGGER.finest(gml);
-        // run test
-        //assertTrue(runXmlTest(baseRequest, "10", true));
+        AttributeExpression tempLeftExp = factory.createAttributeExpression(null);
+        tempLeftExp.setAttributePath("nlanes");
+
+        LiteralExpression tempRightExp = factory.createLiteralExpression(DefaultExpression.LITERAL_INTEGER);
+        tempRightExp.setLiteral(new Integer(8));
+
+        CompareFilter tempFilter = factory.createCompareFilter(AbstractFilter.COMPARE_EQUALS);
+        tempFilter.addRightValue(tempRightExp);
+        tempFilter.addLeftValue(tempLeftExp);
+        query.addFilter(tempFilter);
+
+        FeatureRequest request = new FeatureRequest();
+        request.addQuery(query);
+
+        String gml = FeatureResponse.getXmlResponse(request);
+        LOGGER.fine(gml);
+
+        //TODO: assert true on the strings returned.  Maybe use 
+        //the gt2 gml datasource?  Turn into DOM (gml4J?) and
+        //test what should be in there?  Or perhaps just rely
+        //on cite test engine for the response tests.
     }
 }
