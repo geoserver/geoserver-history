edadf3e0fba1f508ce464548a7d94ec1403b77bb
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-09-19 15:26:32 +0000

    better messages and updates for 1.0
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@548 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f0e502c26a20afc9d0b34037f88ab2813b049a7b [formerly e5d43bf627d8ccb210841d5e835a59f3e06227c8]
    Former-commit-id: 24c58ee28da3413504b0d1458c1883e7e5aa18d9

--- bin/startup.bat
+++ bin/startup.bat
@@ -1,7 +1,43 @@
+@echo off
 REM -----------------------------------------------------------------------------
 REM Start Script for GEOSERVER
 REM
-REM $Id: startup.bat,v 1.2 2003/01/23 15:49:59 cholmesny Exp $
+REM $Id: startup.bat,v 1.3 2003/09/19 15:26:32 cholmesny Exp $
 REM -----------------------------------------------------------------------------
 
-java -classpath C:\geoserver\lib\catalina\bin\bootstrap.jar;C:\geoserver\lib\catalina\server\lib\catalina.jar;C:\geoserver\lib\catalina\server\lib\servlet-cgi.jar;C:\geoserver\lib\catalina\server\lib\servlets-common.jar;C:\geoserver\lib\catalina\server\lib\servlets-default.jar;C:\geoserver\lib\catalina\server\lib\servlets-invoker.jar;C:\geoserver\lib\catalina\server\lib\servlets-manager.jar;C:\geoserver\lib\catalina\server\lib\servlets-snoop.jar;C:\geoserver\lib\catalina\server\lib\servlets-ssi.jar;C:\geoserver\lib\catalina\server\lib\servlets.webdav.jar;C:\geoserver\lib\catalina\server\lib\jakarta-regexp-1.2.jar;C:\geoserver\lib\catalina\lib\naming-factory.jar;C:\geoserver\lib\catalina\common\lib\crimson.jar;C:\geoserver\lib\catalina\common\lib\jasper-compiler.jar;C:\geoserver\lib\catalina\common\lib\jasper-runtime.jar;C:\geoserver\lib\catalina\common\lib\jaxp.jar;C:\geoserver\lib\catalina\common\lib\jndi.jar;C:\geoserver\lib\catalina\common\lib\naming-common.jar;C:\geoserver\lib\catalina\common\lib\naming-resources.jar;C:\geoserver\lib\catalina\common\lib\servlet.jar;C:\geoserver\lib\catalina\common\lib\tools.jar;C:\geoserver\lib\catalina\common\lib\log4j.jar;C:\geoserver\lib\embedded.jar;C:\geoserver\lib\jdbcPostgres.jar;C:\geoserver\lib\geotools.jar;C:\geoserver\lib\jts-1.0.0.jar;C:\geoserver\lib\xerces.jar;C:\geoserver\lib\castor.jar org.vfny.geoserver.EmbeddedTomcat start &
+if "%JAVA_HOME%" == "" goto noJava1
+
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJava2
+
+if "%GEOSERVER_HOME%" == "" goto noGeo1
+
+if not exist "%GEOSERVER_HOME%\bin\startup.bat" goto noGeo2
+
+REM -------------
+REM OK, we're ready to try actually runnning it.
+REM -------------
+
+java -classpath "%GEOSERVER_HOME%\lib\catalina\bin\bootstrap.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\catalina.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlet-cgi.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-common.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-default.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-invoker.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-manager.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-snoop.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets-ssi.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\servlets.webdav.jar";"%GEOSERVER_HOME%\lib\catalina\server\lib\jakarta-regexp-1.2.jar";"%GEOSERVER_HOME%\lib\catalina\lib\naming-factory.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\crimson.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\jasper-compiler.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\jasper-runtime.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\jaxp.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\jndi.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\naming-common.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\naming-resources.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\servlet.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\tools.jar";"%GEOSERVER_HOME%\lib\catalina\common\lib\log4j.jar";"%GEOSERVER_HOME%\lib\embedded.jar";"%GEOSERVER_HOME%\lib\jdbcPostgres.jar";"%GEOSERVER_HOME%\lib\geotools.jar";"%GEOSERVER_HOME%\lib\JTS-1.3.jar";"%GEOSERVER_HOME%\lib\xerces.jar";"%GEOSERVER_HOME%\lib\lucene.jar" org.vfny.geoserver.EmbeddedTomcat start &
+goto end
+
+:noJava1
+  echo The JAVA_HOME environment variable is not defined.
+  echo This environment variable is needed to run this program.
+goto end
+
+:noJava2
+  echo The JAVA_HOME environment variable is defined, but 'java.exe'
+  echo was not found there.
+goto end
+
+:noGeo1
+  echo The GEOSERVER_HOME environment variable is not defined.
+  echo This environment variable is needed to run this program.
+goto end
+
+:noGeo2
+  echo The GEOSERVER_HOME environment variable is not defined correctly.
+  echo This environment variable is needed to run this program.
+goto end
+
+:end
