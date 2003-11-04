36efffc97d417b6abce8e2b1b22c49c00565e4a1
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-04 22:44:41 +0000

    moved from request directory
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@628 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bd2f26a21d8e5425d714f1dd224a5a8e0a196a94 [formerly afd61ef1d9970dbabb5404c9eb7be120ab390fc4]
    Former-commit-id: 316a4ffdd0d3eba975b27cfbfec4d5f936190a11

---
+++ geoserver/src/org/vfny/geoserver/requests/readers/DispatcherKvpReader.java
@@ -0,0 +1,57 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.requests.readers;
+
+import org.vfny.geoserver.servlets.*;
+import java.util.*;
+import java.util.logging.*;
+
+
+/**
+ * Reads in a generic request and attempts to determine its type.
+ *
+ * @author Chris Holmes, TOPP
+ * @author Gabriel Roldán
+ * @version $Id: DispatcherKvpReader.java,v 1.1.2.1 2003/11/04 22:44:41 cholmesny Exp $
+ */
+public class DispatcherKvpReader {
+    /** Class logger */
+    private static Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.requests.readers");
+
+    /**
+     * Returns the request type for a given KVP set.
+     *
+     * @param kvPairs DOCUMENT ME!
+     *
+     * @return Request type.
+     */
+    public static int getRequestType(Map kvPairs) {
+        String responseType = ((String) kvPairs.get("REQUEST"));
+        LOGGER.finer("dispatcher got request " + responseType);
+
+        if (responseType != null) {
+            responseType = responseType.toUpperCase();
+
+            if (responseType.equals("GETCAPABILITIES")) {
+                return Dispatcher.GET_CAPABILITIES_REQUEST;
+            } else if (responseType.equals("DESCRIBEFEATURETYPE")) {
+                return Dispatcher.DESCRIBE_FEATURE_TYPE_REQUEST;
+            } else if (responseType.equals("GETFEATURE")) {
+                return Dispatcher.GET_FEATURE_REQUEST;
+            } else if (responseType.equals("TRANSACTION")) {
+                return Dispatcher.TRANSACTION_REQUEST;
+            } else if (responseType.equals("GETFEATUREWITHLOCK")) {
+                return Dispatcher.GET_FEATURE_LOCK_REQUEST;
+            } else if (responseType.equals("LOCKFEATURE")) {
+                return Dispatcher.LOCK_REQUEST;
+            } else {
+                return Dispatcher.UNKNOWN;
+            }
+        } else {
+            return Dispatcher.UNKNOWN;
+        }
+    }
+}
