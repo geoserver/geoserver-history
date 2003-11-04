5b19edee058d23cf1b20fcd96b32aeb1e4dd95d6
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-04 23:33:32 +0000

    initial commit
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@699 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cbde53e6dd54613a4f85264df9dd071180708618 [formerly 4eac11e5c3e25792d48a43cb8d16c5c2d984bf3f]
    Former-commit-id: e1f7f08b1764e8895339b68623d462c23ba91956

---
+++ geoserver/src/org/vfny/geoserver/servlets/WFService.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.servlets;
+
+import org.vfny.geoserver.ExceptionHandler;
+import org.vfny.geoserver.WfsExceptionHandler;
+
+
+/**
+ * base servlet class for all WFS requests
+ *
+ * @author Gabriel Roldán
+ * @version 0.1
+ */
+abstract public class WFService extends AbstractService {
+    /**
+     * a Web Feature Service exception handler
+     *
+     * @return an instance of WfsExceptionHandler
+     */
+    protected ExceptionHandler getExceptionHandler() {
+        return WfsExceptionHandler.getInstance();
+    }
+}
