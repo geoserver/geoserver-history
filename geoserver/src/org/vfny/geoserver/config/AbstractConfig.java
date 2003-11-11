444a480087033c57a0ab56bd90f1bcfb1c001fd8
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-11 02:46:16 +0000

    got rid of geotools junk headers
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@767 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7a142764b9d5cece1f770925417e5b775ae8faef [formerly 4bdde4badd8b48471b9bf2d518b0307ff1276927]
    Former-commit-id: e08b38128e5f2825dc1700c8fa11734e61578c03

--- geoserver/src/org/vfny/geoserver/config/AbstractConfig.java
+++ geoserver/src/org/vfny/geoserver/config/AbstractConfig.java
@@ -1,39 +1,3 @@
-/*
- *    Geotools2 - OpenSource mapping toolkit
- *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- */
-/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root
- * application directory.
- */
-/*
- *    Geotools2 - OpenSource mapping toolkit
- *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- */
 /* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
@@ -48,7 +12,7 @@ import java.util.logging.Logger;
  * DOCUMENT ME!
  *
  * @author Gabriel Roldán
- * @version 0.1
+ * @version $Id: AbstractConfig.java,v 1.1.2.3 2003/11/11 02:46:16 cholmesny Exp $
  */
 public abstract class AbstractConfig {
     /** DOCUMENT ME! */
