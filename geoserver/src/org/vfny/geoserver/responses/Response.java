d657da368724e96545af912d8886ca10185b8328
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-12 03:43:34 +0000

    Added best guess for Response comments
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@786 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f0d893aa178e88f54a71c7b71f07caf3bce85ee0 [formerly 451761237998e172e0f43b56268c3591489da7a4]
    Former-commit-id: cdef09ded190e6f47ad6f3ab3d9865cee9031105

--- geoserver/src/org/vfny/geoserver/responses/Response.java
+++ geoserver/src/org/vfny/geoserver/responses/Response.java
@@ -10,15 +10,45 @@ import java.io.*;
 
 
 /**
- * DOCUMENT ME!
- *
+ * Process and write out Requests?
+ * <p>
+ * JG - This class seems to do two things, process requests and hold the
+ * resulting Response ready to be written out.
+ * </p>
  * @author Gabriel Roldán
  * @version 0.1
  */
 public interface Response {
+    /**
+     * Performs the request.
+     * <p>
+     * We should clarify when a ServiceException is thrown? I would assume
+     * that a "failed" request should still result in a Response that we
+     * could write out. 
+     * </p>
+     * @param request
+     * @throws ServiceException
+     */
     public void execute(Request request) throws ServiceException;
 
+    /**
+     * MIME type of this Response - example <code>"text/xml"</code>.
+     * <p>
+     * There is some MIME stuff in JDK for reference:
+     * <ul>
+     * <li>java.awt.datatransfer package</li>
+     * <li>javax.mail.internet</li>
+     * <li>
+     * and a few other places as well.
+     * @return MIME Type
+     */
     public String getContentType();
 
+    /**
+     * Write this respone to the provided output stream.
+     * 
+     * @param out
+     * @throws ServiceException
+     */
     public void writeTo(OutputStream out) throws ServiceException;
 }
