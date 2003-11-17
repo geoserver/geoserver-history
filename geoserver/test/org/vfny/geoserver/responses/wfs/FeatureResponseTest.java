42446fa42a603335d2cc2ab608e490bfc7eb441b
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-17 09:06:37 +0000

    Starting to set up for JUnit testing
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@848 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bef506e095abc611352e6c3aafc136d88653c6f7 [formerly 7c5aa6fb9352b7e364b0527c7eaa7d4f06e0188a]
    Former-commit-id: c3fa5b1c18eeee806d41a37c366448fc3c65de23

---
+++ geoserver/test/org/vfny/geoserver/responses/wfs/FeatureResponseTest.java
@@ -0,0 +1,80 @@
+package org.vfny.geoserver.responses.wfs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geotools.data.*;
+import org.geotools.data.memory.*;
+
+import org.vfny.geoserver.requests.wfs.*;
+import org.vfny.geoserver.config.*;
+/**
+ * This is my attempt at testing FeatureResponse using
+ * normal JUnit tests.
+ * <p>
+ * Due to the interaction with Config.getInstance() this
+ * may not be possible.
+ * </p>
+ * <p>
+ * 
+ * @author jgarnett
+ */
+public class FeatureResponseTest extends DataTestCase {
+    CatalogConfig config;
+    FeatureResponse response;
+    
+    /**
+     * Constructor for FeatureResponseTest.
+     * @param arg0
+     */
+    public FeatureResponseTest(String arg0) {
+        super(arg0);
+    }
+
+    /*
+     * @see TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        response = new FeatureResponse();
+        Catalog cat = new DefaultCatalog();
+        Map config = new HashMap();
+        ServerConfig.load( config, cat );      
+        
+        
+    }
+    /*
+     * @see TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testFeatureResponse() {
+        FeatureRequest request = new FeatureRequest();
+        request.setHandle( "FeatureRequest" );
+                
+    }
+
+    public void testGetContentType() {
+    }
+
+    public void testWriteTo() {
+    }
+
+    /*
+     * Test for void execute(Request)
+     */
+    public void testExecuteRequest() {
+    }
+
+    /*
+     * Test for void execute(FeatureRequest)
+     */
+    public void testExecuteFeatureRequest() {
+    }
+
+    public void testAbort() {
+    }
+
+}
