199a69df6efc4cd7bc5cfd876c6abcd23af8ec84
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-11-23 05:15:46 +0000

    Changed set methods to return void. Added setTypeNames. Renamed several methods.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@909 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4512aa44462e2ce98bb1268f9c7ae8a7442fff6c [formerly 523cdbc64d28865531cbc7d3fa076889d76ec90f]
    Former-commit-id: f8f34e0e39b6870e79ecb8e46a4253cdb68335a8

--- geoserver/src/org/geotools/validation/Validation.java
+++ geoserver/src/org/geotools/validation/Validation.java
@@ -1,16 +1,96 @@
 package org.geotools.validation;
 
 /**
+ * Validation purpose.
+ * <p>
+ * Description of Validation ...
+ * <p>
+ * Capabilities:
+ * <ul>
+ * </li></li>
+ * </ul>
+ * Example Use:
+ * <pre><code>
+ * Validation x = new Validation(...);
+ * </code></pre>
  * 
- * 
- * @author jgarnett
+ * @author bowens, Refractions Research, Inc.
+ * @author $Author: jive $ (last modification)
+ * @version $Id: Validation.java,v 1.1.2.2 2003/11/23 05:15:46 jive Exp $
  */
 public interface Validation 
 {
+	
+	static final String ALL[] = null;	// test all featureTypes
+	
+	
+	/**
+	 * setName purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @param name
+	 * @return
+	 */
 	String 	setName( String name );
+	
+	/**
+	 * getName purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @return
+	 */
 	String 	getName();
-	String 	setDescription( String description );
+	
+	
+	
+	/**
+	 * setDescription purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @param description
+	 */
+	void 	setDescription( String description );
+	
+	/**
+	 * getDescription purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @return
+	 */
 	String 	getDescription();
-	int 		getPriority();
-	String[] getFeatureTypes();
+	
+	
+	
+	/**
+	 * getPriority purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @return
+	 */
+	int 	getPriority();
+	
+	
+	
+	/**
+	 * setTypeNames purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @param names
+	 */
+	void 	setTypeNames(String[] names);
+	
+	/**
+	 * getTypeNames purpose.
+	 * <p>
+	 * Description ...
+	 * </p>
+	 * @return
+	 */
+	String[] getTypeNames();
 }
