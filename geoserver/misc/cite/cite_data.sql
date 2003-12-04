02ec89e3702a3fbbfc8958c22298f8fcea5936ce
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-12-04 20:36:46 +0000

    updates for latest postgis, other and locks
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wms-merge@999 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6a9adf775e3ba5c83ca9a516fdfb76943c6d87c8 [formerly fc2a5944455416aaa6007060328129a03a538fd3]
    Former-commit-id: b33a5719a3e18d8ab539e496385390038cbee8ec

--- geoserver/misc/cite/cite_data.sql
+++ geoserver/misc/cite/cite_data.sql
@@ -51,6 +51,7 @@ CREATE TABLE "Seven" (
 	CHECK (((geometrytype("boundedBy") = 'POLYGON'::text) OR ("boundedBy" IS NULL)))
 );
 
+
 --
 -- TOC Entry ID 24 (OID 312275)
 --
@@ -58,7 +59,7 @@ CREATE TABLE "Seven" (
 --
 
 CREATE TABLE "Nulls" (
-	"gmldescription" character varying,
+	"description" character varying,
 	"name" character varying,
 	"boundedBy" geometry,
 	"integers" integer,
@@ -220,8 +221,13 @@ CREATE TABLE "Fifteen" (
 --
 
 CREATE TABLE "Locks" (
-	"gmlname" character varying,
-	"id" character varying
+	"boundedBy" character varying,
+	"id" character varying, 
+	"pointProperty" geometry,
+	CHECK ((srid("pointProperty") = 32615)),
+	CHECK (((geometrytype("pointProperty") = 'POINT'::text) OR ("pointProperty" IS NULL))),
+	CHECK ((srid("boundedBy") = 32615)),
+	CHECK (((geometrytype("boundedBy") = 'POLYGON'::text) OR ("boundedBy" IS NULL)))
 );
 
 --
@@ -231,8 +237,8 @@ CREATE TABLE "Locks" (
 --
 
 CREATE TABLE "Other" (
-	"gmldescription" character varying,
-	"gmlname" character varying,
+	"description" character varying,
+	"name" character varying,
 	"boundedBy" geometry,
 	"pointProperty" geometry,
 	"string1" character varying NOT NULL,
@@ -274,31 +280,33 @@ CREATE TABLE "Other" (
 
 
 COPY "geometry_columns" FROM stdin;
-	cite	Nulls	pointProperty	2	32615	POINT
- 	cite	Nulls	boundedBy	2	32615	POLYGON
- 	cite	Points	pointProperty	2	32615	POINT
- 	cite	Other	pointProperty	2	32615	POINT
-	cite	Lines	lineStringProperty	2	32615	LINESTRING
- 	cite	Polygons	polygonProperty	2	32615	POLYGON
-	cite	MPolygons	multiPolygonProperty	2	32615	MULTIPOLYGON
-	cite	MPoints	multiPointProperty	2	32615	MULTIPOINT
-	cite	MLines	multiLineStringProperty	2	32615	MULTILINESTRING
- 	cite	Other	boundedBy	2	32615	POLYGON
- 	cite	Seven	pointProperty	2	32615	POINT
- 	cite	Seven	boundedBy	2	32615	POLYGON
- 	cite	Fifteen	pointProperty	2	32615	POINT
- 	cite	Fifteen	boundedBy	2	32615	POLYGON
- 	cite	Updates	pointProperty	2	32615	POINT
- 	cite	Updates	boundedBy	2	32615	POLYGON
- 	cite	Inserts	pointProperty	2	32615	POINT
- 	cite	Inserts	boundedBy	2	32615	POLYGON
- 	cite	Deletes	pointProperty	2	32615	POINT
- 	cite	Deletes	boundedBy	2	32615	POLYGON
+	public	Nulls	pointProperty	2	32615	POINT	17539	3	\N
+ 	public	Nulls	boundedBy	2	32615	POLYGON	17539	4	\N
+ 	public	Points	pointProperty	2	32615	POINT	17539	5	\N
+ 	public	Other	pointProperty	2	32615	POINT	17539	6	\N
+	public	Lines	lineStringProperty	2	32615	LINESTRING	17539	7	\N
+ 	public	Polygons	polygonProperty	2	32615	POLYGON	17539	8	\N
+	public	MPolygons	multiPolygonProperty	2	32615	MULTIPOLYGON	17539	9	\N
+	public	MPoints	multiPointProperty	2	32615	MULTIPOINT	17539	10	\N
+	public	MLines	multiLineStringProperty	2	32615	MULTILINESTRING	17539	11	\N
+ 	public	Other	boundedBy	2	32615	POLYGON	17539	12	\N
+ 	public	Seven	pointProperty	2	32615	POINT	17539	13	\N
+ 	public	Seven	boundedBy	2	32615	POLYGON	17539	14	\N
+ 	public	Fifteen	pointProperty	2	32615	POINT	17539	15	\N
+ 	public	Fifteen	boundedBy	2	32615	POLYGON	17539	16	\N
+ 	public	Updates	pointProperty	2	32615	POINT	17539	17	\N
+ 	public	Updates	boundedBy	2	32615	POLYGON	17539	18	\N
+ 	public	Inserts	pointProperty	2	32615	POINT	17539	19	\N
+ 	public	Inserts	boundedBy	2	32615	POLYGON	17539	20	\N
+ 	public	Deletes	pointProperty	2	32615	POINT	17539	21	\N
+ 	public	Deletes	boundedBy	2	32615	POLYGON	17539	22	\N
+ 	public	Locks	pointProperty	2	32615	POINT	17539	21	\N
+ 	public	Locks	boundedBy	2	32615	POLYGON	17539	22	\N
 \.
 --
 -- Data for TOC Entry ID 39 (OID 113496)
 --
--- Name: county Type: TABLE DATA Owner: cite
+-- Name: county Type: TABLE DATA Owner: public
 --
 
 
@@ -316,7 +324,7 @@ SRID=32615;POLYGON((500000 500000,500000 500100,500100 500100,500100 500000,5000
 --
 -- Data for TOC Entry ID 59 (OID 312275)
 --
--- Name: NullFeature Type: TABLE DATA Owner: cite
+-- Name: NullFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -326,7 +334,7 @@ nullFeature	\N	SRID=32615;POLYGON((500000 500000,500000 500100,500100 500100,500
 --
 -- Data for TOC Entry ID 60 (OID 312300)
 --
--- Name: DeleteFeature Type: TABLE DATA Owner: cite
+-- Name: DeleteFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -338,7 +346,7 @@ SRID=32615;POLYGON((500000 500000,500000 500100,500100 500100,500100 500000,5000
 --
 -- Data for TOC Entry ID 61 (OID 312305)
 --
--- Name: InsertFeature Type: TABLE DATA Owner: cite
+-- Name: InsertFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -347,7 +355,7 @@ COPY "Inserts" FROM stdin;
 --
 -- Data for TOC Entry ID 62 (OID 312310)
 --
--- Name: UpdateFeature Type: TABLE DATA Owner: cite
+-- Name: UpdateFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -360,7 +368,7 @@ SRID=32615;POLYGON((500000 500000,500000 500100,500100 500100,500100 500000,5000
 --
 -- Data for TOC Entry ID 63 (OID 312315)
 --
--- Name: PointFeature Type: TABLE DATA Owner: cite
+-- Name: PointFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -370,7 +378,7 @@ t0000	SRID=32615;POINT(500050 500050)
 --
 -- Data for TOC Entry ID 64 (OID 312322)
 --
--- Name: LineStringFeature Type: TABLE DATA Owner: cite
+-- Name: LineStringFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -380,7 +388,7 @@ t0001	SRID=32615;LINESTRING(500125 500025,500175 500075)
 --
 -- Data for TOC Entry ID 65 (OID 312329)
 --
--- Name: PolygonFeature Type: TABLE DATA Owner: cite
+-- Name: PolygonFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -390,7 +398,7 @@ t0002	SRID=32615;POLYGON((500225 500025,500225 500075,500275 500050,500275 50002
 --
 -- Data for TOC Entry ID 66 (OID 312335)
 --
--- Name: MultiPointFeature Type: TABLE DATA Owner: cite
+-- Name: MultiPointFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -400,7 +408,7 @@ t0003	SRID=32615;MULTIPOINT(500325 500025,500375 500075)
 --
 -- Data for TOC Entry ID 67 (OID 312341)
 --
--- Name: MultiLineStringFeature Type: TABLE DATA Owner: cite
+-- Name: MultiLineStringFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -410,7 +418,7 @@ t0004	SRID=32615;MULTILINESTRING((500425 500025,500475 500075),(500425 500075,50
 --
 -- Data for TOC Entry ID 68 (OID 312348)
 --
--- Name: MultiPolygonFeature Type: TABLE DATA Owner: cite
+-- Name: MultiPolygonFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -420,7 +428,7 @@ t0005	SRID=32615;MULTIPOLYGON(((500525 500025,500550 500050,500575 500025,500525
 --
 -- Data for TOC Entry ID 69 (OID 312391)
 --
--- Name: FifteenFeature Type: TABLE DATA Owner: cite
+-- Name: FifteenFeature Type: TABLE DATA Owner: public
 --
 
 
@@ -444,84 +452,88 @@ SRID=32615;POLYGON((500000 500000,500000 500100,500100 500100,500100 500000,5000
 --
 -- Data for TOC Entry ID 70 (OID 312430)
 --
--- Name: LockFeature Type: TABLE DATA Owner: cite
+-- Name: LockFeature Type: TABLE DATA Owner: public
 --
 
 
 COPY "Locks" FROM stdin;
-\N	lfla0001
-\N	lfla0002
-\N	lfla0003
-\N	lfla0004
-\N	gfwlla0001
-\N	gfwlla0002
-\N	gfwlla0003
-\N	gfwlla0004
-\N	lfbt0001
-\N	lfbt0002
-\N	lfbt0003
-\N	lfbt0004
-\N	lfbt0005
-\N	lfbt0006
-\N	gfwlbt0001
-\N	gfwlbt0002
-\N	gfwlbt0003
-\N	gfwlbt0004
-\N	gfwlbt0005
-\N	gfwlbt0006
-\N	lfe0001
-\N	lfe0002
-\N	lfe0003
-\N	lfe0004
-\N	gfwle0001
-\N	gfwle0002
-\N	gfwle0003
-\N	gfwle0004
-\N	lfra0001
-\N	lfra0002
-\N	lfra0003
-\N	lfra0004
-\N	lfra0005
-\N	lfra0006
-\N	lfra0007
-\N	lfra0008
-\N	lfra0009
-\N	lfra0010
-\N	gfwlra0001
-\N	gfwlra0002
-\N	gfwlra0003
-\N	gfwlra0004
-\N	gfwlra0005
-\N	gfwlra0006
-\N	gfwlra0007
-\N	gfwlra0008
-\N	gfwlra0009
-\N	gfwlra0010
-\N	lfrs0001
-\N	lfrs0002
-\N	lfrs0003
-\N	lfrs0004
-\N	lfrs0005
-\N	lfrs0006
-\N	lfrs0007
-\N	lfrs0008
-\N	lfrs0009
-\N	lfrs0010
-\N	gfwlrs0001
-\N	gfwlrs0002
-\N	gfwlrs0003
-\N	gfwlrs0004
-\N	gfwlrs0005
-\N	gfwlrs0006
-\N	gfwlrs0007
-\N	gfwlrs0008
-\N	gfwlrs0009
-\N	gfwlrs0010
+\N	lfla0001	\N
+\N	lfla0002	\N
+\N	lfla0003	\N
+\N	lfla0004	\N
+\N	gfwlla0001	\N
+\N	gfwlla0002	\N
+\N	gfwlla0003	\N
+\N	gfwlla0004	\N
+\N	lfbt0001	\N
+\N	lfbt0002	\N
+\N	lfbt0003	\N
+\N	lfbt0004	\N
+\N	lfbt0005	\N
+\N	lfbt0006	\N
+\N	gfwlbt0001	\N
+\N	gfwlbt0002	\N
+\N	gfwlbt0003	\N
+\N	gfwlbt0004	\N
+\N	gfwlbt0005	\N
+\N	gfwlbt0006	\N
+\N	lfe0001	\N
+\N	lfe0002	\N
+\N	lfe0003	\N
+\N	lfe0004	\N
+\N	gfwle0001	\N
+\N	gfwle0002	\N
+\N	gfwle0003	\N
+\N	gfwle0004	\N
+\N	lfra0001	\N
+\N	lfra0002	\N
+\N	lfra0003	\N
+\N	lfra0004	\N
+\N	lfra0005	\N
+\N	lfra0006	\N
+\N	lfra0007	\N
+\N	lfra0008	\N
+\N	lfra0009	\N
+\N	lfra0010	\N
+\N	gfwlra0001	\N
+\N	gfwlra0002	\N
+\N	gfwlra0003	\N
+\N	gfwlra0004	\N
+\N	gfwlra0005	\N
+\N	gfwlra0006	\N
+\N	gfwlra0007	\N
+\N	gfwlra0008	\N
+\N	gfwlra0009	\N
+\N	gfwlra0010	\N
+\N	lfrs0001	\N
+\N	lfrs0002	\N
+\N	lfrs0003	\N
+\N	lfrs0004	\N
+\N	lfrs0005	\N
+\N	lfrs0006	\N
+\N	lfrs0007	\N
+\N	lfrs0008	\N
+\N	lfrs0009	\N
+\N	lfrs0010	\N
+\N	gfwlrs0001	\N
+\N	gfwlrs0002	\N
+\N	gfwlrs0003	\N
+\N	gfwlrs0004	\N
+\N	gfwlrs0005	\N
+\N	gfwlrs0006	\N
+\N	gfwlrs0007	\N
+\N	gfwlrs0008	\N
+\N	gfwlrs0009	\N
+\N	gfwlrs0010	\N
 \.
+
+UPDATE "Locks" SET "boundedBy" = GeometryFromText('POLYGON((500000 500000,500000 500100,500100 500100,500100 500000,500000 500000))', 32615) WHERE TRUE;
+UPDATE "Locks" SET "pointProperty" = GeometryFromText('POINT(500050 500050)', 32615) WHERE TRUE;
+
 --
 -- Data for TOC Entry ID 71 (OID 312570)
 --
--- Name: OtherFeature Type: TABLE DATA Owner: cite
+-- Name: OtherFeature Type: TABLE DATA Owner: public
 --
 
 
