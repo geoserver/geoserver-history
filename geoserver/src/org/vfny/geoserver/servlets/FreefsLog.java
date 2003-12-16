f3116fbd94dfe864e5201aa33a53fb6c6fd2c3e4
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-12-16 21:04:16 +0000

    added comment on close
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@1031 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 45a13be952476755d900358bf51af8f2e01b8aab [formerly 8a066bdc383e87ad641cbd4a532d21d0d94cbb1f]
    Former-commit-id: c9bb91320d0c564fae923504f1ce65f6a33bb297

--- geoserver/src/org/vfny/geoserver/servlets/FreefsLog.java
+++ geoserver/src/org/vfny/geoserver/servlets/FreefsLog.java
@@ -1,3 +1,19 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 /* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
@@ -20,7 +36,7 @@ import javax.servlet.http.*;
  *
  * @author Rob Hranac, Vision for New York
  * @author Chris Holmes, TOPP
- * @version $Id: FreefsLog.java,v 1.14 2003/12/16 18:46:10 cholmesny Exp $
+ * @version $Id: FreefsLog.java,v 1.15 2003/12/16 21:04:16 cholmesny Exp $
  */
 public class FreefsLog extends HttpServlet {
     /** Standard logging instance for class */
@@ -76,6 +92,12 @@ public class FreefsLog extends HttpServlet {
 
     /**
      * Closes down the zserver if it is running, and frees up resources.
+     *
+     * @task REVISIT: what we should consider is having geotools provide a
+     *       nicer way to clean up datastores's resources, something like a
+     *       close, so that we could just iterate through all the datastores
+     *       calling close. Once that's done we can clean up this method a
+     *       bit.
      */
     public void destroy() {
         super.destroy();
@@ -88,13 +110,13 @@ public class FreefsLog extends HttpServlet {
             Class sdepfClass = Class.forName(
                     "org.geotools.data.sde.SdeConnectionPoolFactory");
 
-            LOGGER.info("SDE datasource found, releasing resources");
+            LOGGER.fine("SDE datasource found, releasing resources");
 
             java.lang.reflect.Method m = sdepfClass.getMethod("getInstance",
                     new Class[0]);
             Object pfInstance = m.invoke(sdepfClass, new Object[0]);
 
-            LOGGER.info("got sde connection pool factory instance: "
+            LOGGER.fine("got sde connection pool factory instance: "
                 + pfInstance);
 
             java.lang.reflect.Method closeMethod = pfInstance.getClass()
