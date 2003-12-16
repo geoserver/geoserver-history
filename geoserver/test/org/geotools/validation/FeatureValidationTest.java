69f28fd2041f8bcbb256ae700b812637e21e7225
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-12-16 23:11:23 +0000

    Renamed IsValidGeometryFeatureValidation
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@1037 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4b269fea722a9a7bb7d1b7873ffe5a0d14a1cd5d [formerly 9d8a589f2c04c7bbe8b65d32896de96466e49908]
    Former-commit-id: 185e2686710e7df3f8360f3be5f3d9e2a7a27d6b

--- geoserver/test/org/geotools/validation/FeatureValidationTest.java
+++ geoserver/test/org/geotools/validation/FeatureValidationTest.java
@@ -1,34 +1,50 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.validation;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.geotools.data.*;
-import org.geotools.data.memory.*;
+import org.geotools.data.DataTestCase;
+import org.geotools.data.memory.MemoryDataStore;
 import org.geotools.feature.IllegalAttributeException;
-import org.geotools.validation.spatial.IsValidFeatureValidation;
+import org.geotools.validation.spatial.IsValidGeometryFeatureValidation;
 
-import org.vfny.geoserver.requests.wfs.*;
-import org.vfny.geoserver.config.*;
 
 /**
  * FeatureValidationTest purpose.
+ * 
  * <p>
  * Description of FeatureValidationTest ...
- * <p>
+ * </p>
  * 
+ * <p></p>
+ *
  * @author jgarnett, Refractions Research, Inc.
- * @author $Author: cholmesny $ (last modification)
- * @version $Id: FeatureValidationTest.java,v 1.2 2003/12/16 18:46:10 cholmesny Exp $
+ * @author $Author: jive $ (last modification)
+ * @version $Id: FeatureValidationTest.java,v 1.3 2003/12/16 23:11:23 jive Exp $
  */
 public class FeatureValidationTest extends DataTestCase {
     MemoryDataStore store;
-    
+
     /**
      * FeatureValidationTest constructor.
+     * 
      * <p>
      * Run test <code>testName</code>.
      * </p>
+     *
      * @param testName
      */
     public FeatureValidationTest(String testName) {
@@ -37,137 +53,129 @@ public class FeatureValidationTest extends DataTestCase {
 
     /**
      * Construct data store for use.
-     * 
-     * @see junit.framework.TestCase#setUp()
-     * 
+     *
      * @throws Exception
+     *
+     * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() throws Exception {
         super.setUp();
         store = new MemoryDataStore();
-        store.addFeatures( roadFeatures );
-        store.addFeatures( riverFeatures );     
+        store.addFeatures(roadFeatures);
+        store.addFeatures(riverFeatures);
     }
-    
+
     /**
      * Override tearDown.
      *
-     * @see junit.framework.TestCase#tearDown()
-     * 
      * @throws Exception
+     *
+     * @see junit.framework.TestCase#tearDown()
      */
     protected void tearDown() throws Exception {
         store = null;
         super.tearDown();
     }
-    
-	public void testIsValidFeatureValidation()
-   	{
-   		// the visitor
-		RoadValidationResults validationResults = new RoadValidationResults();
-		
-   		IsValidFeatureValidation validator = new IsValidFeatureValidation("isValidRoad", "Tests to see if a road is valid", IsValidFeatureValidation.ALL);
-   		validationResults.setValidation(validator);
-		assertTrue(validator.validate(this.newRoad,this.roadType, validationResults));
-		try {
-			this.newRoad = this.roadType.create(new Object[] {
-				new Integer(2), line(new int[] { 1, 2, 1, 2}), "r4"
-			}, "road.rd4");
-		} catch (IllegalAttributeException e) {}
-		assertTrue(!validator.validate(this.newRoad,this.roadType, validationResults));	// validate will return false
-   	}
-   	
-   	
-   	
-   	/**
-	 * testABunchOfValidations purpose.
-	 * <p>
-	 * Description ...
-	 * </p>
-	 * 
-	 */
-	public void testABunchOfValidations()
-   	{
-		// the visitor
-		RoadNetworkValidationResults roadValidationResults = new RoadNetworkValidationResults();
-		
-		
-		// various FeatureType tests
-		IsValidFeatureValidation isValidValidator1 = new IsValidFeatureValidation("isValidRoad", 
-																					"Tests to see if a road is valid", 
-																					new String[] {"roads"});
-		IsValidFeatureValidation isValidValidator2 = new IsValidFeatureValidation("isValidRail", 
-																					"Tests to see if a railway is valid", 
-																					new String[] {"roads", "rails"});
-		IsValidFeatureValidation isValidValidator3 = new IsValidFeatureValidation("isValidRiver", 
-																					"Tests to see if a river is valid", 
-																					new String[] {"rivers"});
-		IsValidFeatureValidation isValidValidator4 = new IsValidFeatureValidation("isValidAll", 
-																					"Tests to see if all geometries are valid", 
-																					IsValidFeatureValidation.ALL);
-		
-		// various Integrity tests
-		//
-		//
-		//
-		
-		
-		
-		// tell the RoadValidator what tests to use
-		roadValidationResults.setValidation(isValidValidator1);
-		roadValidationResults.setValidation(isValidValidator2);
-		roadValidationResults.setValidation(isValidValidator3);
-		roadValidationResults.setValidation(isValidValidator4);
-		
-		
-		
-		// run each feature validation test on the featureTypes it tests
-		String[] types1 = isValidValidator1.getTypeNames();
-		for (int i=0; i<types1.length; i++)
-		{
-			//isValidValidator1.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
-		}
-		String[] types2 = isValidValidator1.getTypeNames();
-		for (int i=0; i<types2.length; i++)
-		{
-			//isValidValidator2.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
-		}
-		String[] types3 = isValidValidator1.getTypeNames();
-		for (int i=0; i<types3.length; i++)
-		{
-			//isValidValidator3.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
-		}
-		String[] types4 = isValidValidator1.getTypeNames();
-		for (int i=0; i<types4.length; i++)
-		{
-			//isValidValidator4.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
-		}
-		
-		
-		
-		// check the results of the roadValidator
-		String[] roadFailures = roadValidationResults.getFailedMessages();
-		String[] roadWarnings = roadValidationResults.getWarningMessages();
-		boolean roadsPassed = true;
-		if (roadFailures.length != 0)
-		{
-			roadsPassed = false;
-			for (int i=0; i<roadFailures.length; i++)
-			{
-				System.out.println(roadFailures[i]);
-			}
-		}
-		if (roadWarnings.length != 0)
-		{
-			for (int i=0; i<roadWarnings.length; i++)
-			{
-				System.out.println(roadWarnings[i]);
-			}
-		}
-		
-		assertTrue(roadsPassed);
-		
-   	}
-   	
 
+    public void testIsValidFeatureValidation() {
+        // the visitor
+        RoadValidationResults validationResults = new RoadValidationResults();
+
+        IsValidGeometryFeatureValidation validator = new IsValidGeometryFeatureValidation("isValidRoad",
+                "Tests to see if a road is valid",
+                IsValidGeometryFeatureValidation.ALL);
+        validationResults.setValidation(validator);
+        assertTrue(validator.validate(this.newRoad, this.roadType,
+                validationResults));
+
+        try {
+            this.newRoad = this.roadType.create(new Object[] {
+                        new Integer(2), line(new int[] { 1, 2, 1, 2 }), "r4"
+                    }, "road.rd4");
+        } catch (IllegalAttributeException e) {
+        }
+
+        assertTrue(!validator.validate(this.newRoad, this.roadType,
+                validationResults)); // validate will return false
+    }
+
+    /**
+     * testABunchOfValidations purpose.
+     * 
+     * <p>
+     * Description ...
+     * </p>
+     */
+    public void testABunchOfValidations() {
+        // the visitor
+        RoadNetworkValidationResults roadValidationResults = new RoadNetworkValidationResults();
+
+        // various FeatureType tests
+        IsValidGeometryFeatureValidation isValidValidator1 = new IsValidGeometryFeatureValidation("isValidRoad",
+                "Tests to see if a road is valid", new String[] { "roads" });
+        IsValidGeometryFeatureValidation isValidValidator2 = new IsValidGeometryFeatureValidation("isValidRail",
+                "Tests to see if a railway is valid",
+                new String[] { "roads", "rails" });
+        IsValidGeometryFeatureValidation isValidValidator3 = new IsValidGeometryFeatureValidation("isValidRiver",
+                "Tests to see if a river is valid", new String[] { "rivers" });
+        IsValidGeometryFeatureValidation isValidValidator4 = new IsValidGeometryFeatureValidation("isValidAll",
+                "Tests to see if all geometries are valid",
+                IsValidGeometryFeatureValidation.ALL);
+
+        // various Integrity tests
+        //
+        //
+        //
+        // tell the RoadValidator what tests to use
+        roadValidationResults.setValidation(isValidValidator1);
+        roadValidationResults.setValidation(isValidValidator2);
+        roadValidationResults.setValidation(isValidValidator3);
+        roadValidationResults.setValidation(isValidValidator4);
+
+        // run each feature validation test on the featureTypes it tests
+        String[] types1 = isValidValidator1.getTypeNames();
+
+        for (int i = 0; i < types1.length; i++) {
+            //isValidValidator1.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
+        }
+
+        String[] types2 = isValidValidator1.getTypeNames();
+
+        for (int i = 0; i < types2.length; i++) {
+            //isValidValidator2.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
+        }
+
+        String[] types3 = isValidValidator1.getTypeNames();
+
+        for (int i = 0; i < types3.length; i++) {
+            //isValidValidator3.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
+        }
+
+        String[] types4 = isValidValidator1.getTypeNames();
+
+        for (int i = 0; i < types4.length; i++) {
+            //isValidValidator4.validate(featuresToTest(types[i]), featureType(types[i]), roadValidationResults);
+        }
+
+        // check the results of the roadValidator
+        String[] roadFailures = roadValidationResults.getFailedMessages();
+        String[] roadWarnings = roadValidationResults.getWarningMessages();
+        boolean roadsPassed = true;
+
+        if (roadFailures.length != 0) {
+            roadsPassed = false;
+
+            for (int i = 0; i < roadFailures.length; i++) {
+                System.out.println(roadFailures[i]);
+            }
+        }
+
+        if (roadWarnings.length != 0) {
+            for (int i = 0; i < roadWarnings.length; i++) {
+                System.out.println(roadWarnings[i]);
+            }
+        }
+
+        assertTrue(roadsPassed);
+    }
 }
--- geoserver/test/org/geotools/validation/IntegrityValidationTest.java
+++ geoserver/test/org/geotools/validation/IntegrityValidationTest.java
@@ -1,31 +1,51 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.validation;
-
-import java.util.HashMap;
 
 import org.geotools.data.*;
 import org.geotools.data.memory.*;
 import org.geotools.validation.attributes.UniqueFIDIntegrityValidation;
-import org.geotools.validation.spatial.IsValidFeatureValidation;
+import org.geotools.validation.spatial.IsValidGeometryFeatureValidation;
+import java.util.HashMap;
 
 
 /**
  * IntegrityValidationTest purpose.
+ * 
  * <p>
  * Description of IntegrityValidationTest ...
- * <p>
+ * </p>
  * 
+ * <p></p>
+ *
  * @author jgarnett, Refractions Research, Inc.
- * @author $Author: cholmesny $ (last modification)
- * @version $Id: IntegrityValidationTest.java,v 1.2 2003/12/16 18:46:10 cholmesny Exp $
+ * @author $Author: jive $ (last modification)
+ * @version $Id: IntegrityValidationTest.java,v 1.3 2003/12/16 23:11:23 jive Exp $
  */
 public class IntegrityValidationTest extends DataTestCase {
     MemoryDataStore store;
-    
+
     /**
      * FeatureValidationTest constructor.
+     * 
      * <p>
      * Run test <code>testName</code>.
      * </p>
+     *
      * @param testName
      */
     public IntegrityValidationTest(String testName) {
@@ -34,50 +54,43 @@ public class IntegrityValidationTest extends DataTestCase {
 
     /**
      * Construct data store for use.
-     * 
-     * @see junit.framework.TestCase#setUp()
-     * 
+     *
      * @throws Exception
+     *
+     * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() throws Exception {
         super.setUp();
         store = new MemoryDataStore();
-        store.addFeatures( roadFeatures );
-        store.addFeatures( riverFeatures );     
+        store.addFeatures(roadFeatures);
+        store.addFeatures(riverFeatures);
     }
-    
+
     /**
      * Override tearDown.
      *
-     * @see junit.framework.TestCase#tearDown()
-     * 
      * @throws Exception
+     *
+     * @see junit.framework.TestCase#tearDown()
      */
     protected void tearDown() throws Exception {
         store = null;
         super.tearDown();
     }
-    
-    
-	public void testUniqueFIDIntegrityValidation() throws Exception
-   	{
-   		// the visitor
-		RoadValidationResults validationResults = new RoadValidationResults();
-		
-   		UniqueFIDIntegrityValidation validator = new UniqueFIDIntegrityValidation("isValidRoad", "Tests to see if a road is valid", IsValidFeatureValidation.ALL, "FID");
-		validationResults.setValidation(validator);
-		
-		
-   		HashMap layers = new HashMap();
-   		layers.put("road", store.getFeatureSource("road"));
-		layers.put("river", store.getFeatureSource("river"));
-		
-		assertTrue(validator.validate(layers, null, validationResults));	// validate will return true
-   	}
-   	
-   	
-   	
 
-   	   	
+    public void testUniqueFIDIntegrityValidation() throws Exception {
+        // the visitor
+        RoadValidationResults validationResults = new RoadValidationResults();
 
+        UniqueFIDIntegrityValidation validator = new UniqueFIDIntegrityValidation("isValidRoad",
+                "Tests to see if a road is valid",
+                IsValidGeometryFeatureValidation.ALL, "FID");
+        validationResults.setValidation(validator);
+
+        HashMap layers = new HashMap();
+        layers.put("road", store.getFeatureSource("road"));
+        layers.put("river", store.getFeatureSource("river"));
+
+        assertTrue(validator.validate(layers, null, validationResults)); // validate will return true
+    }
 }
