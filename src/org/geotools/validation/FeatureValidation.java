122d27f1a2f6b5203fe283214186e69bbe53fbc3
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2003-12-16 22:40:03 +0000

    Update/correct comments.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@1034 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bb73d8fe960013b73b8300e964873f5b331cbd4f [formerly c18bddae9347e982e52c1f5154a9e9d44b006597]
    Former-commit-id: d695c42bc9863c82cef2dcfa4dec7a0cbae92050

--- src/org/geotools/validation/FeatureValidation.java
+++ src/org/geotools/validation/FeatureValidation.java
@@ -1,36 +1,84 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.validation;
 
-import org.geotools.feature.FeatureType;
 import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureType;
+
 
 /**
- * FeatureValidation PlugIns are used to check geospatial information
- * for internal consistency.
+ * Defined a per Feature validation test.
  * 
- * Each ValidationPlugIn is very specific in nature: it performs
- * one test extermly well.  This simplifies design decisions, documenation
- * configuration and use.
+ * <p>
+ * FeatureValidation PlugIns are used to check geospatial information for
+ * internal consistency.  Each ValidationPlugIn is very specific in nature: it
+ * performs one test extermly well.  This simplifies design decisions,
+ * documenation configuration and use.
+ * </p>
  * 
+ * <p>
  * Following the lead the excelent design work in the JUnit testing framework
- * validation results are collected by a ValidationResults object. This interface for the
- * ValidationResults object also allows it to collect warning information.
- *   
- * The PlugIn is also required to supply some metadata to aid in its deployment,
- * scripting, logging and execution and error recovery.
+ * validation results are collected by a ValidationResults object. This
+ * interface for the ValidationResults object also allows it to collect
+ * warning information.  The PlugIn is also required to supply some metadata
+ * to aid in its deployment, scripting, logging and execution and error
+ * recovery.
+ * </p>
  * 
- * @author jgarnett
+ * <p>
+ * Capabilities:
+ * 
+ * <ul>
+ * <li>
+ * Uses FeatureResults to allow environment to gather error/warning information
+ * as required (transaction XML document, JTable, logging system, etc...)
+ * </li>
+ * <li>
+ * Primiarly used as part of processing an Insert Element in the Transaction
+ * opperation of a Web Feature Server. (Allows us to fail a Feature without
+ * bothering the Database)
+ * </li>
+ * </ul>
+ * 
+ * Example Use (feature: id=1, name="foo", geom=linestring):
+ * <pre><code>
+ * RangeFeatureValidation test = new RangeFeatureValidation();
+ * 
+ * results.setValidation( test );
+ * test.setMin(0);
+ * test.validate( feature, featureType, results ); // true
+ * test.setMin(2);
+ * test.validate( feature, featureType, results ); // false
+ * </code></pre>
+ * </p>
+ *
+ * @author Jody Garnett, Refractions Research, Inc.
+ * @version $Id: FeatureValidation.java,v 1.3 2003/12/16 22:40:03 jive Exp $
  */
-public interface FeatureValidation extends Validation
-{
-   
+public interface FeatureValidation extends Validation {
     /**
      * Used to check features against this validation rule.
-     * 
-     * @param features FeatureCollection to validated
-     * @param type FeatureType used to aquire metadata such as schema information
-     * @param results Used to coallate results information 
+     *
+     * @param feature Feature to be Validated
+     * @param type FeatureType schema of feature
+     * @param results coallate results information
+     *
      * @return True if all the features pass this test.
      */
-    public boolean validate( Feature feature, FeatureType type, ValidationResults results ) throws Exception;
-    
+    public boolean validate(Feature feature, FeatureType type,
+        ValidationResults results) throws Exception;
 }
