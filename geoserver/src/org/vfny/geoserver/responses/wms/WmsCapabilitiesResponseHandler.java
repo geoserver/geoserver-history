0a336d339a0b0adabb4806004152bdd4b2614982
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-02 23:29:37 +0000

    updates to validate correctly
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@1354 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d8bcc91a116f56dccb0ceb566f12969d24c39028 [formerly ab47ef7ea4efa644ebb66d722a8e3ba3922d5062]
    Former-commit-id: a8fe276856c1d0cb8ac6abc9996794e559b54b13

--- geoserver/src/org/vfny/geoserver/responses/wms/WmsCapabilitiesResponseHandler.java
+++ geoserver/src/org/vfny/geoserver/responses/wms/WmsCapabilitiesResponseHandler.java
@@ -13,15 +13,20 @@ import java.util.*;
 
 
 /**
- * DOCUMENT ME!
+ * Handler methods to print out a valid wms capabilities document from the WMS
+ * config.  Will be called by a transformer in Capabilities response, and
+ * should turn itself into xml events that will then be consumed by a writer
+ * and written to the output stream (ie sent to the client).
  *
  * @author Gabriel Roldán
- * @version $Id: WmsCapabilitiesResponseHandler.java,v 1.3 2003/12/26 21:59:27 cholmesny Exp $
+ * @author Chris Holmes
+ * @version $Id: WmsCapabilitiesResponseHandler.java,v 1.4 2004/01/02 23:29:37 cholmesny Exp $
  */
 public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler {
     private static final String CAP_VERSION = ServerConfig.getInstance()
                                                           .getWMSConfig()
                                                           .getVersion();
+    protected String BBOX_ELEM_NAME = "LatLonBoundingBox";
 
     /**
      * Creates a new WmsCapabilitiesResponseHandler object.
@@ -33,11 +38,16 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
     }
 
     /**
-     * DOCUMENT ME!
+     * Prints the dtd declaration and root WMT_MS_Capabilities declaration.
      *
-     * @param config DOCUMENT ME!
+     * @param config Must be an instance of WMSConfig.  If not a class cast
+     *        exception will be thrown.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems creating the SAX.
+     *
+     * @task TODO: replace the digitalearth.gov dtd with our own locally
+     *       referenced one.  To do this we will need to modify the build.xml
+     *       file to copy it to the war.
      */
     protected void startDocument(ServiceConfig config)
         throws SAXException {
@@ -45,9 +55,9 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
 
         AttributesImpl atts = new AttributesImpl();
 
-        //handleSingleElem("!DOCTYPE WMT_MS_Capabilities SYSTEM \"http://www.digitalearth.gov/wmt/xml/capabilities_1_1_1.dtd\"", null);
-        //atts.addAttribute("WMT_MS_Capabilities
-        //atts = new AttributesImpl();
+        startElement("!DOCTYPE WMT_MS_Capabilities SYSTEM \"http://www"
+            + ".digitalearth.gov/wmt/xml/capabilities_1_1_1.dtd\"");
+
         atts.addAttribute("", "version", "version", "", CAP_VERSION);
         atts.addAttribute("", "", "updateSequence", "updateSequence",
             wmsConfig.getUpdateTime());
@@ -55,11 +65,11 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
     }
 
     /**
-     * DOCUMENT ME!
+     * Closes the WMT_MS_Capabilities element.
      *
-     * @param config DOCUMENT ME!
+     * @param config The wms config used to start the document.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
     public void endDocument(ServiceConfig config) throws SAXException {
         unIndent();
@@ -67,47 +77,47 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
     }
 
     /**
-     * DOCUMENT ME!
+     * Starts the Service element.
      *
-     * @param config DOCUMENT ME!
+     * @param config The Wms config to turn into a capabilities document.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException If anything goes wrong.
      */
     protected void startService(ServiceConfig config) throws SAXException {
         startElement("Service");
     }
 
     /**
-     * DOCUMENT ME!
+     * Calls endElement for Service
      *
-     * @param config DOCUMENT ME!
+     * @param config The Wms config to turn into a capabilities document.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
     protected void endService(ServiceConfig config) throws SAXException {
         endElement("Service");
     }
 
     /**
-     * DOCUMENT ME!
+     * Handles the service section of the document.  Just calls super which
+     * calls the appropriate sub methods.
      *
-     * @param config DOCUMENT ME!
+     * @param config The Wms config to turn into a capabilities document.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
     public void handleService(ServiceConfig config) throws SAXException {
         super.handleService(config);
-        indent();
-        handleContactInformation(config);
-        unIndent();
     }
 
     /**
-     * DOCUMENT ME!
+     * Handles the capabilities section - request, exceptions, vendor, sld and
+     * layers - of the caps document.
      *
-     * @param serviceConfig DOCUMENT ME!
+     * @param serviceConfig The Wms config to turn into a capabilities
+     *        document.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException If anything goes wrong.
      */
     protected void handleCapabilities(ServiceConfig serviceConfig)
         throws SAXException {
@@ -124,14 +134,29 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
         handleLayers(config);
         unIndent();
         endElement("Capability");
-
-        //        handleLayers(config);
     }
 
+    /**
+     * Handles the layers portion of the document.  Prints a root layer that
+     * all others descend from, as it appears that only one layer element is
+     * allowed.  For now just calls handle config, which prints out the
+     * server's title, abstract and keywords, which are all valid elements.
+     *
+     * @param config The Wms config to handle the layers of.
+     *
+     * @throws SAXException For any problems.
+     *
+     * @task REVISIT: print out a more wms appropriate root list.  Perhaps
+     *       consider putting 4326 (latlon) for the SRS.  I'm not sure if
+     *       child layers override or inherit.
+     */
     protected void handleLayers(WMSConfig config) throws SAXException {
         CatalogConfig catalog = server.getCatalog();
         Collection ftypes = catalog.getFeatureTypes().values();
         FeatureTypeConfig layer;
+        cReturn();
+        startElement("Layer");
+        handleConfig(config);
 
         for (Iterator it = ftypes.iterator(); it.hasNext();) {
             layer = (FeatureTypeConfig) it.next();
@@ -145,22 +170,33 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
                 endElement("Layer");
             }
         }
+
+        endElement("Layer");
     }
 
     /**
-     * calls super.handleFeatureType to add common FeatureType content such as
+     * Calls super.handleFeatureType to add common FeatureType content such as
      * Name, Title and LatLonBoundingBox, and then writes WMS specific layer
      * properties as Styles, Scale Hint, etc.
      *
-     * @param ftype DOCUMENT ME!
+     * @param ftype The featureType to write out.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
+     *
+     * @task TODO: write wms specific elements.
      */
     protected void handleFeatureType(FeatureTypeConfig ftype)
         throws SAXException {
         super.handleFeatureType(ftype);
     }
 
+    /**
+     * Handles the sld styling support provided by the server.
+     *
+     * @param config The WMS config to get sld info from.
+     *
+     * @throws SAXException For any problems.
+     */
     protected void handleSLD(WMSConfig config) throws SAXException {
         AttributesImpl sldAtts = new AttributesImpl();
         String supportsSLD = config.supportsSLD() ? "1" : "0";
@@ -168,7 +204,7 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
         String supportsUserStyle = config.supportsUserStyle() ? "1" : "0";
         String supportsRemoteWFS = config.supportsRemoteWFS() ? "1" : "0";
 
-        sldAtts.addAttribute("", "SupportsSLD", "SupportsSLD", "", supportsSLD);
+        sldAtts.addAttribute("", "SupportSLD", "SupportSLD", "", supportsSLD);
         sldAtts.addAttribute("", "UserLayer", "UserLayer", "", supportsUserLayer);
         sldAtts.addAttribute("", "UserStyle", "UserStyle", "", supportsUserStyle);
         sldAtts.addAttribute("", "RemoteWFS", "RemoteWFS", "", supportsRemoteWFS);
@@ -177,12 +213,28 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
         endElement("UserDefinedSymbolization");
     }
 
+    /**
+     * Handles the vendoer specific capabilities.  Right now there are none, so
+     * we do nothing.
+     *
+     * @param config The global config that may contain vendor specifics.
+     *
+     * @throws SAXException For any problems.
+     */
     protected void handleVendorSpecifics(WMSConfig config)
         throws SAXException {
-        startElement("VendorSpecificCapabilities");
-        endElement("VendorSpecificCapabilities");
+        //startElement("VendorSpecificCapabilities");
+        //endElement("VendorSpecificCapabilities");
     }
 
+    /**
+     * Handles the request portion of the document, printing out the
+     * capabilities and where to bind to them.
+     *
+     * @param config The global wms.
+     *
+     * @throws SAXException For any problems.
+     */
     protected void handleRequest(WMSConfig config) throws SAXException {
         startElement("Request");
         indent();
@@ -190,18 +242,18 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
         cReturn();
         handleCapability(config, "GetMap");
         unIndent();
-
-        /*
-           handleCapability(config, "GetCapabilities");
-           handleCapability(config, "DescribeFeatureType");
-           handleCapability(config, "GetFeature");
-           handleCapability(config, "Transaction");
-           handleCapability(config, "LockFeature");
-           handleCapability(config, "GetFeatureWithLock");
-         */
         endElement("Request");
     }
 
+    /**
+     * Handles an individual capability, printing where it can be found and the
+     * formats it supports.
+     *
+     * @param config The wms service config.
+     * @param capabilityName The name of the capability to print out.
+     *
+     * @throws SAXException For any problems.
+     */
     protected void handleCapability(WMSConfig config, String capabilityName)
         throws SAXException {
         boolean isPost = false;
@@ -251,19 +303,14 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
         endElement(capabilityName);
     }
 
-    protected void handleOnlineResource(String url) throws SAXException {
-        AttributesImpl attributes = new AttributesImpl();
-        attributes.addAttribute("", "xmlns:xlink", "xmlns:xlink", "",
-            "http://www.w3.org/1999/xlink");
-        attributes.addAttribute("", "xlink:type", "xlink:type", "", "simple");
-        attributes.addAttribute("", "xlink:href", "xlink:href", "", url);
-
-        indent();
-        startElement("OnlineResource", attributes);
-        endElement("OnlineResource");
-        unIndent();
-    }
-
+    /**
+     * Handles the printing of the exceptions information, prints the formats
+     * that GeoServer can return exceptions in.
+     *
+     * @param config The wms service global config.
+     *
+     * @throws SAXException For any problems.
+     */
     protected void handleExceptions(WMSConfig config) throws SAXException {
         cReturn();
         startElement("Exception");
@@ -285,11 +332,11 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
     }
 
     /**
-     * DOCUMENT ME!
+     * Handles the contact information portion of the service section.
      *
-     * @param config DOCUMENT ME!
+     * @param config The global service information.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
     protected void handleContactInformation(ServiceConfig config)
         throws SAXException {
@@ -328,14 +375,15 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
      * Overrides BasicConfig.handleKeywords to write the keywords list in WMS
      * style
      *
-     * @param kwords DOCUMENT ME!
+     * @param kwords A list of keywords to print out.  If null or of size 0
+     *        then nothing will be printed, since the keyword(s) element is
+     *        not mandatory.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
     protected void handleKeywords(List kwords) throws SAXException {
-        startElement("KeywordList");
-
-        if (kwords != null) {
+        if ((kwords != null) && (kwords.size() > 0)) {
+            startElement("KeywordList");
             indent();
 
             for (Iterator it = kwords.iterator(); it.hasNext();) {
@@ -349,32 +397,52 @@ public class WmsCapabilitiesResponseHandler extends CapabilitiesResponseHandler 
             }
 
             unIndent();
-        }
 
-        endElement("KeywordList");
+            endElement("KeywordList");
+        }
     }
 
     /**
      * Overrides CapabilitiesResponseHandler.handlerOnlineResource to write WMS
      * style service online resource
      *
-     * @param config DOCUMENT ME!
+     * @param config The WMS config to write out an online resource from.
      *
-     * @throws SAXException DOCUMENT ME!
+     * @throws SAXException For any problems.
      */
-    protected void handleOnlineResouce(ServiceConfig config)
+    protected void handleOnlineResource(ServiceConfig config)
         throws SAXException {
         String url = config.getOnlineResource();
-        AttributesImpl olAtts = new AttributesImpl();
-        String xlinkNs = "http://www.w3c.org/1999/xlink";
+        handleOnlineResource(url);
+    }
 
-        olAtts.addAttribute("", "xlink", "xmlns:xlink", "", xlinkNs);
+    /**
+     * Convenience method to print the appropriate xlink attributes for an
+     * online resource.
+     *
+     * @param url The url to be linked to.
+     *
+     * @throws SAXException For any problems.
+     */
+    protected void handleOnlineResource(String url) throws SAXException {
+        AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute("", "xmlns:xlink", "xmlns:xlink", "",
+            "http://www.w3.org/1999/xlink");
+        attributes.addAttribute("", "xlink:type", "xlink:type", "", "simple");
+        attributes.addAttribute("", "xlink:href", "xlink:href", "", url);
 
-        olAtts.addAttribute(xlinkNs, "type", "xlink:type", "", "simple");
-
-        olAtts.addAttribute(xlinkNs, "href", "xlink:href", "", url);
-
-        startElement("OnlineResource", olAtts);
+        indent();
+        startElement("OnlineResource", attributes);
         endElement("OnlineResource");
+        unIndent();
+    }
+
+    /**
+     * Gets the name of the WMS bbox element name.
+     *
+     * @return The string 'LatLonBoundingBox', as that's what wms uses.
+     */
+    protected String getBboxElementName() {
+        return "LatLonBoundingBox";
     }
 }
