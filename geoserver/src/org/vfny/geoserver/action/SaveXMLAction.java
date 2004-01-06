e6baeeb467cd67d1ec30a63edb71d9b4ff1c5eca
emperorkefka <emperorkefka@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-06 22:27:56 +0000

    Moved file, created in wrong directory.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/config@1506 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4b1d7e5819087a69a050b18a0e38de74db323e66 [formerly e7e5c495f7d7c6a6458fbe5935967bcfe1e7e606]
    Former-commit-id: 0f64df2b0a450752f24e36cb15b282ffb3f90137

---
+++ geoserver/src/org/vfny/geoserver/action/SaveXMLAction.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Jan 6, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.vfny.geoserver.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author User
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SaveXMLAction extends Action {
+	public ActionForward execute(ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws IOException, ServletException {
+			
+			//Fill me with code!!!
+			return null; 
+	}
+}
