a4e51eca8dc8c3d25fc6ecd9183eedfbbd2c69a6
emperorkefka <emperorkefka@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-09 01:20:16 +0000

    Updated WMS Actions. I believe they are complete. They do need documenting though
    git-svn-id: http://svn.codehaus.org/geoserver/branches/config@1633 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5b7dc266d90d358f7ef54eb643c6c0983121df35 [formerly 5395663c422e8fb2e2319351859690fa125c7968]
    Former-commit-id: 47207c39d34c115344f925b8b199423f779fe06f

---
+++ geoserver/src/org/vfny/geoserver/action/wms/WMSContentAction.java
@@ -0,0 +1,69 @@
+/*
+ * Created on Jan 6, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.vfny.geoserver.action.wms;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.vfny.geoserver.action.ConfigAction;
+import org.vfny.geoserver.config.WMSConfig;
+import org.vfny.geoserver.form.wms.WMSContentForm;
+
+/**
+ * @author rgould
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public final class WMSContentAction extends ConfigAction {
+	public ActionForward execute(ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws IOException, ServletException {
+
+
+		WMSContentForm contentForm = (WMSContentForm) form;
+
+		boolean enabled = contentForm.isEnabled();
+		System.out.println("Action.execute: setting internal enabled =" + enabled);	
+		if (contentForm.isEnabledChecked() == false) {
+			enabled = false;
+			System.out.println("Whoa, it was never actually checked. Setting to false.");	
+		}
+		String onlineResource = contentForm.getOnlineResource();
+		String updateTime = contentForm.getUpdateTime();
+		String[] selectedFeatures = contentForm.getSelectedFeatures();
+		String[] features = contentForm.getFeatures(); 
+	
+		WMSConfig config = getWMSConfig();
+		System.out.println("Action.execute: setting config.setEnabled =" + enabled);		
+		config.setEnabled(enabled);
+		config.setOnlineResource(new URL(onlineResource));
+		config.setUpdateTime(updateTime);
+		
+		Set set = new TreeSet();
+		if (selectedFeatures !=null) {
+			for (int i = 0; i < selectedFeatures.length;i++) {
+				set.add(selectedFeatures[i]);
+			}
+		}
+		
+		config.setEnabledFeatures(set);
+
+		return mapping.findForward("wmsConfigDescription");
+	}
+}
