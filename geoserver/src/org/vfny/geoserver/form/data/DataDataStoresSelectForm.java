49560cb0e8396f9f1f8f1b918421275ba23f7215
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-12 05:24:17 +0000

    Renamed to buttonAction
    git-svn-id: http://svn.codehaus.org/geoserver/branches/config@1719 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 16e3e821af96ccefceea0f1c2ab84cbca1a58ccb [formerly 8120ce0604ecb2cf1f861cf7e01f5aef821ac809]
    Former-commit-id: d555c899c0e00dd63dcd8ca6c8811814545815ad

--- geoserver/src/org/vfny/geoserver/form/data/DataDataStoresSelectForm.java
+++ geoserver/src/org/vfny/geoserver/form/data/DataDataStoresSelectForm.java
@@ -4,93 +4,85 @@
  */
 package org.vfny.geoserver.form.data;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
-import org.vfny.geoserver.config.DataConfig;
+import org.vfny.geoserver.config.ConfigRequests;
 
 /**
  * Select current DataStore for edit or delete Action.
+ * 
  * @author User, Refractions Research, Inc.
- * @author $Author: emperorkefka $ (last modification)
- * @version $Id: DataDataStoresSelectForm.java,v 1.1.2.4 2004/01/12 04:24:07 emperorkefka Exp $
+ * @author $Author: jive $ (last modification)
+ * @version $Id: DataDataStoresSelectForm.java,v 1.1.2.5 2004/01/12 05:24:17 jive Exp $
  */
 public class DataDataStoresSelectForm extends ActionForm {
-    /** Action that spawned us? Edit or Delete? */
-    private String action;
-       
-    /** This seams wrong */
-    private SortedSet dataStores;
+    
+    /** Action that spawned us must be "edit" or "delete" */
+    private String buttonAction;
     
     /** Selection from list - will be a dataStoreId */
-    private String selectedDataStore;
+    private String selectedDataStoreID;
     
+    private List dataStoreIds;
+    /** Reset form */
     public void reset(ActionMapping mapping, HttpServletRequest request) {
         super.reset( mapping, request );
-        //POPULATE dataStores WITH AVAILABLE DATA STORES
+        
+        // Pass data from congif layer to screen
+        // REVIST: Bad Design JSP should lookup data itself!
+        dataStoreIds = ConfigRequests.getDataConfig(request).listDataStoreIds();
+        
+        // Usual reset stuff
+        selectedDataStoreID = null; // nothing selected yet        
+        buttonAction = null; // updated when user submits form        
     }
+    
     /** Validate as required */
     public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
         ActionErrors errors = new ActionErrors();
         
         return errors;
     }    
-    
-    public SortedSet getDataStoreIDs () {
-        ServletContext context = getServlet().getServletContext();
-        DataConfig config =
-            (DataConfig) context.getAttribute(DataConfig.CONFIG_KEY);
 
-        return new TreeSet(config.getDataStores().keySet());
+    /** List of current DataStoreIds */
+    public List getDataStoreIds(){
+        return dataStoreIds;
     }
-	/**
-	 * getSelectedDataStore purpose.
-	 * <p>
-	 * Description ...
-	 * </p>
-	 * @return
-	 */
-	public String getSelectedDataStore() {
-		return selectedDataStore;
-	}
+    
+    /**
+     * DataStoreID selected by User.
+     * <p>
+     * If the user has not selected anything (is this possible?) we will 
+     * return <code>null</code>.
+     * </p>
+     * @return Selected DataStoreID or <code>null</code> if nothing is selected 
+     */
+    public String getSelectedDataStoreID() {
+        return selectedDataStoreID;
+    }
+    
+    /**
+     * The button the user hit to submit this form.
+     * <p>
+     * We are doubling up and having the Same action process both
+     * Edit and Delete.
+     * </p>
+     * @return Either <code>edit</code> or <code>delete</code>
+     */
+    public String getButtonAction() {
+        return buttonAction;
+    }
 
-	/**
-	 * setSelectedDataStore purpose.
-	 * <p>
-	 * Description ...
-	 * </p>
-	 * @param string
-	 */
-	public void setSelectedDataStore(String string) {
-		selectedDataStore = string;
-	}
-
-	/**
-	 * getAction purpose.
-	 * <p>
-	 * Description ...
-	 * </p>
-	 * @return
-	 */
-	public String getAction() {
-		return action;
-	}
-
-	/**
-	 * setAction purpose.
-	 * <p>
-	 * Description ...
-	 * </p>
-	 * @param string
-	 */
-	public void setAction(String string) {
-		action = string;
-	}
+    public void setButtonAction(String string) {
+        buttonAction = string;
+    }    
+    public void setSelectedDataStoreID(String string) {
+        selectedDataStoreID = string;
+    }
 
 }
