ca0dee63d44ef86edbbcb39a542cc8af90bfc2fc
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-12 04:41:38 +0000

    Checked in, had to rename to be description based. Need localiozation review
    git-svn-id: http://svn.codehaus.org/geoserver/branches/config@1708 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0a2aab85964f7b8ee65f18e15b98dbabf29f2517 [formerly 1d68e1c2e8de0c3a0fff1669467ba2f0d8eca872]
    Former-commit-id: 779fd7ee727b0463c95ac3b2ef72b3dc4889e4a0

--- geoserver/src/org/vfny/geoserver/form/data/DataDataStoresNewForm.java
+++ geoserver/src/org/vfny/geoserver/form/data/DataDataStoresNewForm.java
@@ -4,13 +4,22 @@
  */
 package org.vfny.geoserver.form.data;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.MessageResources;
+import org.vfny.geoserver.action.data.DataStoreUtils;
 
 /**
  * DataDataStoresNewForm purpose.
@@ -35,23 +44,73 @@ import org.apache.struts.action.ActionMapping;
  * 
  * @author User, Refractions Research, Inc.
  * @author $Author: jive $ (last modification)
- * @version $Id: DataDataStoresNewForm.java,v 1.1.2.2 2004/01/12 02:16:19 jive Exp $
+ * @version $Id: DataDataStoresNewForm.java,v 1.1.2.3 2004/01/12 04:41:38 jive Exp $
  */
 public class DataDataStoresNewForm extends ActionForm {
-    private TreeSet dataStoreTypes;
-    private String selectedDataStoreType;
+    /** Description provided by selected Datastore Factory */
+    private String selectedDescription;
+    
+    /** User provided dataStoreID */
     private String dataStoreID;
 
-    
-    public void reset(ActionMapping arg0, HttpServletRequest arg1) {
-        super.reset(arg0, arg1);
-        
-        //POPULATE dataStoreTypes
+    /** Default state of New form */
+    public void reset(ActionMapping mapping, HttpServletRequest request) {
+        super.reset( mapping, request);
+        selectedDescription = "";
+        dataStoreID = null;
     }
     
+    /**
+     * List of available DataStoreDescriptions.
+     * <p>
+     * Convience method for DataStureUtils.listDataStoresDescriptions().
+     * </p>
+     * @return Sorted set of DataStore Descriptions.
+     */
+    public List getDataStoreDescriptions(){
+        List descriptions = DataStoreUtils.listDataStoresDescriptions();
+        if( descriptions == null || descriptions.isEmpty() ){
+            return Collections.EMPTY_LIST;
+        }
+        return descriptions;
+    }    
+    private static final Pattern idPattern = Pattern.compile("^\\a$");
+     
+    /** Check NewForm for correct use */ 
     public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
         ActionErrors errors = new ActionErrors();
-        
+        MessageResources resources = (MessageResources) request.getAttribute( Action.MESSAGES_KEY );
+                    
+        if( !getDataStoreDescriptions().contains( getSelectedDescription() ) ){
+            String message = resources.getMessage("label.selectedDescription", getSelectedDescription() );
+            errors.add( "selectedDataStoreDescription",            
+                new ActionError("errors.required", message)
+            );
+        }
+        if( Pattern.matches("^\\w*$", getDataStoreID() )){
+                   
+            String message = resources.getMessage("label.dataStoreID",getDataStoreID() );
+            errors.add( "dataStoreID",            
+                new ActionError("errors.required", message)
+            );            
+        }
         return errors;
     }
+
+    public String getDataStoreID() {
+        return dataStoreID;
+    }
+
+    public String getSelectedDescription() {
+        return selectedDescription;
+    }
+
+    public void setDataStoreID(String string) {
+        dataStoreID = string;
+    }
+
+    public void setSelectedDescription(String string) {
+        selectedDescription = string;
+    }
+
 }
