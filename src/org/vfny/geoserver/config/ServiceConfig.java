8216f10201cecfee42235a2f88426e2af11555b7
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-21 00:41:36 +0000

    Fixed Jalopy errors
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@1915 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: db9fa0904b2cc6761457e06ce64d3f6f615728d3 [formerly 22224d02ce34bd43a4b15ef506d41ec78c685548]
    Former-commit-id: b6827641f442dc938571d856301006ac9189f1c5

--- src/org/vfny/geoserver/config/ServiceConfig.java
+++ src/org/vfny/geoserver/config/ServiceConfig.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 /*
  *    Geotools2 - OpenSource mapping toolkit
  *    http://geotools.org
@@ -16,345 +20,342 @@
  */
 package org.vfny.geoserver.config;
 
+import org.vfny.geoserver.global.dto.ServiceDTO;
 import java.net.URL;
 
-import org.vfny.geoserver.global.dto.ServiceDTO;
 
 /**
  * ServiceConfig purpose.
- * <p>
- * ServiceConfig is intended to be extended to provide some basic data storage facilities. 
- * This class represents the basic properties of a web service. 
- * <p>
  * 
+ * <p>
+ * ServiceConfig is intended to be extended to provide some basic data storage
+ * facilities.  This class represents the basic properties of a web service.
+ * </p>
+ * 
+ * <p></p>
+ *
  * @author dzwiers, Refractions Research, Inc.
- * @version $Id: ServiceConfig.java,v 1.12 2004/01/12 23:55:27 dmzwiers Exp $
+ * @version $Id: ServiceConfig.java,v 1.13 2004/01/21 00:41:36 jive Exp $
  */
-public class ServiceConfig{
-	
-	/**
-	 * Represents when the web service is enabled/disabled. True when enabled.
-	 */
-	private boolean enabled = true;
-	
-	/**
-	 * Online Reference URL for the web service. A location to look for when 
-	 * additional assistance is required.
-	 */
-	private URL onlineResource;
-	
-	/**
-	 * The name of the service.
-	 */
-	private String name;
-	
-	/**
-	 * The title of the service.
-	 */
-	private String title;
-	
-	/**
-	 * A short abstract about the service.
-	 */
-	private String _abstract;
-	
-	/**
-	 * A list of keywords associated with the service.
-	 */
-	private String[] keywords;
-	
-	/**
-	 * The fees associated with the service. When there are not any fees, the value 
-	 * "NONE" is used.
-	 */
-	private String fees;
-	
-	/**
-	 * The access constraints associated with the service. When there are not any, 
-	 * the value "NONE" is used.
-	 */
-	private String accessConstraints = "NONE";
-	
-	/**
-	 * Name of the person who maintains the web service. Should ideally be contact 
-	 * information such as webmaster@geoserver.org . 
-	 */
-	private String maintainer;
-	
-	/**
-	 * ServiceConfig constructor.
-	 * <p>
-	 * Creates an empty ServiceConfig representation with default values. 
-	 * </p>
-	 * @see defaultSettings()
-	 */
-	public ServiceConfig(){
-		enabled = true;
-		name = "";
-		title = "";
-		_abstract = "";
-		keywords = new String[0];
-		fees = "";
-		accessConstraints = "NONE";
-		maintainer = "";
-	}
-	
-	/**
-	 * ServiceConfig constructor.
-	 * <p>
-	 * This is equivalent to calling the load method. When a null value is passed in, 
-	 * the default values are used. All non-primary datatypes are cloned with the 
-	 * exception of Strings (which have a singleton hash table in memory representation).   
-	 * </p>
-	 * @param s The ServiceDTO object to copy into the new ServiceConfig object. 
-	 */
-	public ServiceConfig(ServiceDTO s){
-	 if(s == null){
-	 	throw new NullPointerException("");
-	 }
-	 update(s);
-	}
-	
-	/**
-	 * Implement loadDTO.
-	 * <p>
-	 * Takes a ServiceDTO and loads it into this ServiceConfig Object
-	 * </p>
-	 * @see org.vfny.geoserver.config.DataStructure#loadDTO(java.lang.Object)
-	 * 
-	 * @param dto an instance of ServiceDTO
-	 * @return false when obj null, or not correct class instance.
-	 */
-	public void update(ServiceDTO dto){
-	 if(dto==null)
-	 	throw new NullPointerException("Service Data Transfer Object required");
-	 ServiceDTO s = (ServiceDTO)dto;
-	 enabled = s.isEnabled();
-	 name = s.getName();
-	 title = s.getTitle();
-	 _abstract = s.getAbstract();
-	 try{
-		keywords = new String[s.getKeywords().length];
-		for(int i=0;i<keywords.length;i++)
-			keywords[i] = s.getKeywords()[i];
-	 }catch(Exception e){
-		// should only happen for null
-		keywords = new String[0];
-	 }
-	 fees = s.getFees();
-	 accessConstraints = s.getAccessConstraints();
-	 maintainer = s.getMaintainer();
-	 onlineResource = s.getOnlineResource();
-	}
-	
-	/**
-	 * Implement toDTO.
-	 * <p>
-	 * Returns a copy of the data in a ServiceDTO object
-	 * </p>
-	 * @see org.vfny.geoserver.config.DataStructure#toDTO()
-	 * 
-	 * @return a copy of the data in a ServiceDTO object
-	 */
-	// name needed to not match as the DTOs do not follow the same inheritance struture.
-	public ServiceDTO toServDTO(){
-		ServiceDTO sDto = new ServiceDTO();
-		sDto.setAbstract(_abstract);
-		sDto.setAccessConstraints(accessConstraints);
-		sDto.setEnabled(enabled);
-		sDto.setFees(fees);
-		String[] s = new String[keywords.length];
-		for(int i=0;i<keywords.length;i++)
-			s[i] = keywords[i];
-		sDto.setKeywords(s);
-		sDto.setMaintainer(maintainer);
-		sDto.setName(name);
-		sDto.setOnlineResource(onlineResource);
-		sDto.setTitle(title);
-		return sDto;
-	}
-	
-	/**
-	 * getName purpose.
-	 * 
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
+public class ServiceConfig {
+    /**
+     * Represents when the web service is enabled/disabled. True when enabled.
+     */
+    private boolean enabled = true;
 
-	/**
-	 * getOnlineResource purpose.
+    /**
+     * Online Reference URL for the web service. A location to look for when
+     * additional assistance is required.
+     */
+    private URL onlineResource;
 
-	 * @return
-	 */
-	public URL getOnlineResource() {
-		return onlineResource;
-	}
+    /** The name of the service. */
+    private String name;
 
-	/**
-	 * getTitle purpose.
-	 * 
-	 * @return
-	 */
-	public String getTitle() {
-		return title;
-	}
+    /** The title of the service. */
+    private String title;
 
-	/**
-	 * setName purpose.
-	 * 
-	 * @param string
-	 */
-	public void setName(String string) {
-		name = string;
-	}
+    /** A short abstract about the service. */
+    private String _abstract;
 
-	/**
-	 * setOnlineResource purpose.
-	 * 
-	 * @param url
-	 */
-	public void setOnlineResource(URL url) {
-		onlineResource = url;
-	}
+    /** A list of keywords associated with the service. */
+    private String[] keywords;
 
-	/**
-	 * setTitle purpose.
-	 * 
-	 * @param string
-	 */
-	public void setTitle(String string) {
-		title = string;
-	}
+    /**
+     * The fees associated with the service. When there are not any fees, the
+     * value  "NONE" is used.
+     */
+    private String fees;
 
-	/**
-	 * getAbstract purpose.
-	 * 
-	 * @return
-	 */
-	public String getAbstract() {
-		return _abstract;
-	}
+    /**
+     * The access constraints associated with the service. When there are not
+     * any,  the value "NONE" is used.
+     */
+    private String accessConstraints = "NONE";
 
-	/**
-	 * getAccessConstraints purpose.
-	 * 
-	 * @return
-	 */
-	public String getAccessConstraints() {
-		return accessConstraints;
-	}
+    /**
+     * Name of the person who maintains the web service. Should ideally be
+     * contact  information such as webmaster&amp;geoserver.org .
+     */
+    private String maintainer;
 
-	/**
-	 * isEnabled purpose.
-	 * 
-	 * @return
-	 */
-	public boolean isEnabled() {
-		return enabled;
-	}
+    /**
+     * ServiceConfig constructor.
+     * 
+     * <p>
+     * Creates an empty ServiceConfig representation with default values.
+     * </p>
+     *
+     * @see defaultSettings()
+     */
+    public ServiceConfig() {
+        enabled = true;
+        name = "";
+        title = "";
+        _abstract = "";
+        keywords = new String[0];
+        fees = "";
+        accessConstraints = "NONE";
+        maintainer = "";
+    }
 
-	/**
-	 * getFees purpose.
-	 * 
-	 * @return
-	 */
-	public String getFees() {
-		return fees;
-	}
+    /**
+     * ServiceConfig constructor.
+     * 
+     * <p>
+     * This is equivalent to calling the load method. When a null value is
+     * passed in,  the default values are used. All non-primary datatypes are
+     * cloned with the  exception of Strings (which have a singleton hash
+     * table in memory representation).
+     * </p>
+     *
+     * @param dto The ServiceDTO object to copy into the new ServiceConfig
+     *        object.
+     *
+     * @throws NullPointerException if dto was null;
+     */
+    public ServiceConfig(ServiceDTO dto) {
+        if (dto == null) {
+            throw new NullPointerException("Non null ServiceDTO required");
+        }
 
-	/**
-	 * getKeywords purpose.
-	 * 
-	 * @return
-	 */
-	public String[] getKeywords() {
-		return keywords;
-	}
+        update(dto);
+    }
 
-	/**
-	 * getMaintainer purpose.
-	 * 
-	 * @return
-	 */
-	public String getMaintainer() {
-		return maintainer;
-	}
+    /**
+     * Implement loadDTO.
+     * 
+     * <p>
+     * Takes a ServiceDTO and loads it into this ServiceConfig Object
+     * </p>
+     *
+     * @param dto an instance of ServiceDTO
+     *
+     * @throws NullPointerException if dto is null
+     *
+     * @see org.vfny.geoserver.config.DataStructure#loadDTO(java.lang.Object)
+     */
+    public void update(ServiceDTO dto) {
+        if (dto == null) {
+            throw new NullPointerException(
+                "Service Data Transfer Object required");
+        }
 
-	/**
-	 * setAbstract purpose.
-	 * 
-	 * @param string
-	 */
-	public void setAbstract(String string) {
-		_abstract = string;
-	}
+        ServiceDTO s = (ServiceDTO) dto;
+        enabled = s.isEnabled();
+        name = s.getName();
+        title = s.getTitle();
+        _abstract = s.getAbstract();
 
-	/**
-	 * setAccessConstraints purpose.
-	 * 
-	 * @param string
-	 */
-	public void setAccessConstraints(String string) {
-		accessConstraints = string;
-	}
+        try {
+            keywords = new String[s.getKeywords().length];
 
-	/**
-	 * setEnabled purpose.
-	 * 
-	 * @param b
-	 */
-	public void setEnabled(boolean b) {
-		enabled = b;
-	}
+            for (int i = 0; i < keywords.length; i++)
+                keywords[i] = s.getKeywords()[i];
+        } catch (Exception e) {
+            // should only happen for null
+            keywords = new String[0];
+        }
 
-	/**
-	 * setFees purpose.
-	 * 
-	 * @param string
-	 */
-	public void setFees(String string) {
-		fees = string;
-	}
+        fees = s.getFees();
+        accessConstraints = s.getAccessConstraints();
+        maintainer = s.getMaintainer();
+        onlineResource = s.getOnlineResource();
+    }
 
-	/**
-	 * setKeywords purpose.
-	 * 
-	 * @param list
-	 */
-	public void setKeywords(String[] list) {
-		keywords = list;
-	}
+    /**
+     * Implement toDTO.
+     * 
+     * <p>
+     * Returns a copy of the data in a ServiceDTO object
+     * </p>
+     *
+     * @return a copy of the data in a ServiceDTO object
+     *
+     * @see org.vfny.geoserver.config.DataStructure#toDTO()
+     */
 
-	/**
-	 * addKeyword purpose.
-	 * 
-	 * @param item The keyword to add to the list.
-	 * @see java.util.List#add
-	 */
-	/*public void addKeyword(String item) {
-		keywords.add(item);
-	}*/
+    // name needed to not match as the DTOs do not follow the same inheritance struture.
+    public ServiceDTO toServDTO() {
+        ServiceDTO sDto = new ServiceDTO();
+        sDto.setAbstract(_abstract);
+        sDto.setAccessConstraints(accessConstraints);
+        sDto.setEnabled(enabled);
+        sDto.setFees(fees);
 
-	/**
-	 * removeKeyword purpose.
-	 * 
-	 * @param item The keyword to remove from the list.
-	 * @return true if the item was removed.
-	 * @see java.util.List#remove
-	 */
-	/*public boolean removeKeyword(String item) {
-		return keywords.remove(item);
-	}*/
+        String[] s = new String[keywords.length];
 
-	/**
-	 * setMaintainer purpose.
-	 * 
-	 * @param string
-	 */
-	public void setMaintainer(String string) {
-		maintainer = string;
-	}
+        for (int i = 0; i < keywords.length; i++)
+            s[i] = keywords[i];
 
+        sDto.setKeywords(s);
+        sDto.setMaintainer(maintainer);
+        sDto.setName(name);
+        sDto.setOnlineResource(onlineResource);
+        sDto.setTitle(title);
+
+        return sDto;
+    }
+
+    /**
+     * getName purpose.
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * getOnlineResource purpose.
+     *
+     * @return
+     */
+    public URL getOnlineResource() {
+        return onlineResource;
+    }
+
+    /**
+     * getTitle purpose.
+     *
+     * @return
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * setName purpose.
+     *
+     * @param string
+     */
+    public void setName(String string) {
+        name = string;
+    }
+
+    /**
+     * setOnlineResource purpose.
+     *
+     * @param url
+     */
+    public void setOnlineResource(URL url) {
+        onlineResource = url;
+    }
+
+    /**
+     * setTitle purpose.
+     *
+     * @param string
+     */
+    public void setTitle(String string) {
+        title = string;
+    }
+
+    /**
+     * getAbstract purpose.
+     *
+     * @return
+     */
+    public String getAbstract() {
+        return _abstract;
+    }
+
+    /**
+     * getAccessConstraints purpose.
+     *
+     * @return
+     */
+    public String getAccessConstraints() {
+        return accessConstraints;
+    }
+
+    /**
+     * isEnabled purpose.
+     *
+     * @return
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * getFees purpose.
+     *
+     * @return
+     */
+    public String getFees() {
+        return fees;
+    }
+
+    /**
+     * getKeywords purpose.
+     *
+     * @return
+     */
+    public String[] getKeywords() {
+        return keywords;
+    }
+
+    /**
+     * getMaintainer purpose.
+     *
+     * @return
+     */
+    public String getMaintainer() {
+        return maintainer;
+    }
+
+    /**
+     * setAbstract purpose.
+     *
+     * @param string
+     */
+    public void setAbstract(String string) {
+        _abstract = string;
+    }
+
+    /**
+     * setAccessConstraints purpose.
+     *
+     * @param string
+     */
+    public void setAccessConstraints(String string) {
+        accessConstraints = string;
+    }
+
+    /**
+     * setEnabled purpose.
+     *
+     * @param b
+     */
+    public void setEnabled(boolean b) {
+        enabled = b;
+    }
+
+    /**
+     * setFees purpose.
+     *
+     * @param string
+     */
+    public void setFees(String string) {
+        fees = string;
+    }
+
+    /**
+     * setKeywords purpose.
+     *
+     * @param list
+     */
+    public void setKeywords(String[] list) {
+        keywords = list;
+    }
+
+    /**
+     * setMaintainer purpose.
+     *
+     * @param string
+     */
+    public void setMaintainer(String string) {
+        maintainer = string;
+    }
 }
