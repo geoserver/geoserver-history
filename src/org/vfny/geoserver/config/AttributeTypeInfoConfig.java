e4cc3ef8aff023236d73be374c2d20c7ac614a7a
dmzwiers <dmzwiers@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-01-21 00:43:53 +0000

    Code fromating + lib sorting.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@1916 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8962aa723c8ef20f3099e41dfea544bc8dbd9d36 [formerly b1568db1abff4813ec35970d0f0edfa83817fb5e]
    Former-commit-id: a94975ee5107079e729e362963fc152f40889f68

--- src/org/vfny/geoserver/config/AttributeTypeInfoConfig.java
+++ src/org/vfny/geoserver/config/AttributeTypeInfoConfig.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 /* Copyright (c) 2004 TOPP - www.openplans.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
@@ -8,184 +12,210 @@ import org.geotools.feature.AttributeType;
 import org.vfny.geoserver.global.dto.AttributeTypeInfoDTO;
 import org.vfny.geoserver.global.xml.GMLUtils;
 
+
 /**
  * Allows editing of AttributeTypeInfo.
+ * 
  * <p>
  * Represents most of a xs:element for an XMLSchema.
  * </p>
+ * 
  * <p>
- * we have three types of information to store, Schema defined types, 
- * references and extentions on types. If the type represented is either 
- * a reference or a Schema defined type  then isRef should be true. 
+ * we have three types of information to store, Schema defined types,
+ * references and extentions on types. If the type represented is either  a
+ * reference or a Schema defined type  then isRef should be true.
  * </p>
+ * 
  * <p>
  * Non-complex types are of the form:
  * </p>
+ * 
  * <ul>
- * <li><code>{element name='test' type='xs:string'/}</code></li>
- * <li><code>{element name='test' type='gml:PointType'/}</code></li>
+ * <li>
+ * <code>{element name='test' type='xs:string'/}</code>
+ * </li>
+ * <li>
+ * <code>{element name='test' type='gml:PointType'/}</code>
+ * </li>
  * </ul>
- * <p> 
+ * 
+ * <p>
  * These cases have their type name stored in this.type
  * </p>
  * 
  * <p>
- * For complex types such as 
- * <code>
- * {element name='test'}
- *  {xs:complexContent}
- *    {xs:extension base="gml:AbstractFeatureType"}
- *      {xs:sequence}
- *         {xs:element name="id" type="xs:string" minOccurs="0"/}
- *         {xs:element ref="gml:pointProperty" minOccurs="0"/}
- *      {/xs:sequence}
- *    {/xs:extension}
- *  {/xs:complexContent}
- * {/element}
- * </code>
+ * For complex types such as  <code>{element
+ * name='test'}{xs:complexContent}{xs:extension
+ * base="gml:AbstractFeatureType"}{xs:sequence}{xs:element name="id"
+ * type="xs:string" minOccurs="0"/}{xs:element ref="gml:pointProperty"
+ * minOccurs="0"/}{/xs:sequence}{/xs:extension}{/xs:complexContent}{/element}</code>
  * The type contains a similar XML fragment.
  * </p>
+ * 
  * <p>
  * minOccurs, maxOccurs and nillable are all attributes for all cases. There is
  * more stuff in the XMLSchema spec but we don't care.
  * </p>
- * 
+ *
  * @author dzwiers, Refractions Research, Inc.
- * @version $Id: AttributeTypeInfoConfig.java,v 1.14 2004/01/21 00:35:23 dmzwiers Exp $
+ * @version $Id: AttributeTypeInfoConfig.java,v 1.15 2004/01/21 00:43:53 dmzwiers Exp $
  */
 public class AttributeTypeInfoConfig {
-    
     /** Value of getType() used to indicate that fragement is in use */
     public static final String TYPE_FRAGMENT = "(xml fragment)";
-    
+
     /**
      * XML Fragment used to define stuff.
+     * 
      * <p>
      * This property is only used with getType() is equals to "(xml fragment)".
      * </p>
+     * 
      * <p>
      * baseGMLTypes can only be used in your XML fragment.
      * </p>
      */
     private String fragment;
-    
+
     /**
      * Maxmium number of occurances of this attribute in a feature.
+     * 
      * <p>
-     * For Features based on the Simple Feature Specification this
-     * should be a value of 1. If the attribute is optional it should
-     * still be 1, although often optional is represented by allowing the
-     * Attribute to be <code>nillable</code>.
+     * For Features based on the Simple Feature Specification this should be a
+     * value of 1. If the attribute is optional it should still be 1, although
+     * often optional is represented by allowing the Attribute to be
+     * <code>nillable</code>.
      * </p>
+     * 
      * <p>
      * Common Min..Max Occurs values:
      * </p>
+     * 
      * <ul>
-     * <li>0..<b>1</b>: attribute is optional</li>
-     * <li>1..<b>1</b>: attribute is required (usual for Simple Features)</li>
-     * <li>0..<b>N</b>: attribute forms a list that may be empty</li>
+     * <li>
+     * 0..<b>1</b>: attribute is optional
+     * </li>
+     * <li>
+     * 1..<b>1</b>: attribute is required (usual for Simple Features)
+     * </li>
+     * <li>
+     * 0..<b>N</b>: attribute forms a list that may be empty
+     * </li>
      * </ul>
      * 
+     *
      * @see AttributeTypeInfoDTO.isNillable
      */
     private int maxOccurs;
-    
-    /** attribute min occurs*/
+
+    /** attribute min occurs */
     private int minOccurs;
-    
-    /** attribute name*/
+
+    /** attribute name */
     private final String name;
-    
+
     /**
      * Indicate if the attribute is allowed to be <code>null</code>.
+     * 
      * <p>
-     * Nillable is often used to indicate that an attribute is optional.
-     * The use of minOccurs and maxOccurs may be a more correct way to
-     * indicate optional attribtues.
+     * Nillable is often used to indicate that an attribute is optional. The
+     * use of minOccurs and maxOccurs may be a more correct way to indicate
+     * optional attribtues.
      * </p>
+     *
      * @see AttributeTypeInfoDTO.minOccurs
      * @see AttributeTypeInfoDTO.maxOccurs
      */
     private boolean nillable;
-    
+
     /**
      * Element type, a well-known gml or xs type or <code>TYPE_FRAGMENT</code>.
+     * 
      * <p>
-     * If getType is equals to TYPE_FRAGMENT please consult getFragment()
-     * to examin the actual user's definition.
+     * If getType is equals to TYPE_FRAGMENT please consult getFragment() to
+     * examin the actual user's definition.
      * </p>
+     * 
      * <p>
      * Other than that getType should be one of the constants defined by
      * GMLUtils.
      * </p>
-     * @return type The element type, or <code>TYPE_FRAGMENT</code>
      */
     public String type;
 
     /**
      * Set up AttributeTypeInfo based on attributeType.
+     * 
      * <p>
      * Set up is determined by the AttributeTypeInfoDTO( AttributeType )
-     * constructor. This allows all Schema generation to be acomplished
-     * in the same palce.
+     * constructor. This allows all Schema generation to be acomplished in the
+     * same palce.
      * </p>
+     *
      * @param attributeType GeoTools2 attributeType used for configuration
      */
-    public AttributeTypeInfoConfig( AttributeType attributeType ){
+    public AttributeTypeInfoConfig(AttributeType attributeType) {
         name = attributeType.getName();
         minOccurs = 1;
         maxOccurs = 1;
-        
-        GMLUtils.Mapping mapping = GMLUtils.schema( name, attributeType.getType() );
-        if( mapping == null ){
+
+        GMLUtils.Mapping mapping = GMLUtils.schema(name, attributeType.getType());
+
+        if (mapping == null) {
             type = TYPE_FRAGMENT;
-            fragment = "<!-- definition for "+attributeType.getType()+" -->";            
-        }
-        else {
+            fragment = "<!-- definition for " + attributeType.getType()
+                + " -->";
+        } else {
             type = mapping.toString();
-            fragment = "";            
-        }        
+            fragment = "";
+        }
     }
-    
+
     /**
      * Set up AttributeTypeInfo based on Data Transfer Object.
-     * 
+     *
      * @param dto AttributeTypeInfoDTO used for configuration
      */
-    public AttributeTypeInfoConfig(AttributeTypeInfoDTO dto){
-        name = dto.getName(); 
-        if( dto.isComplex() ){
+    public AttributeTypeInfoConfig(AttributeTypeInfoDTO dto) {
+        name = dto.getName();
+
+        if (dto.isComplex()) {
             type = TYPE_FRAGMENT;
             fragment = dto.getType();
-        }
-        else {
+        } else {
             type = dto.getType();
             fragment = "";
         }
+
         minOccurs = dto.getMinOccurs();
         maxOccurs = dto.getMaxOccurs();
-        nillable = dto.isNillable();        
+        nillable = dto.isNillable();
     }
+
     /**
      * XML Fragment used to define stuff.
+     * 
      * <p>
      * This property is only used with getType() is equals to "(xml fragment)".
      * </p>
+     * 
      * <p>
      * baseGMLTypes can only be used in your XML fragment.
      * </p>
-     
+     *
      * @return Returns the fragment.
      */
     public String getFragment() {
-    	return fragment;
+        return fragment;
     }
 
     /**
      * getMaxOccurs purpose.
+     * 
      * <p>
      * The max number of occurences for this element.
      * </p>
+     *
      * @return max number of occurences
      */
     public int getMaxOccurs() {
@@ -194,9 +224,11 @@ public class AttributeTypeInfoConfig {
 
     /**
      * getMinOccurs purpose.
+     * 
      * <p>
      * the min number of occurences for this element
      * </p>
+     *
      * @return min number of occurences
      */
     public int getMinOccurs() {
@@ -205,9 +237,11 @@ public class AttributeTypeInfoConfig {
 
     /**
      * getName purpose.
+     * 
      * <p>
      * returns the element name
      * </p>
+     *
      * @return the element name
      */
     public String getName() {
@@ -216,14 +250,17 @@ public class AttributeTypeInfoConfig {
 
     /**
      * Element type, a well-known gml or xs type or <code>TYPE_FRAGMENT</code>.
+     * 
      * <p>
      * If getType is equals to <code>TYPE_FRAGMENT</code> please consult
      * getFragment() to examine the actual user's definition.
      * </p>
+     * 
      * <p>
      * Other than that getType should be one of the constants defined by
      * GMLUtils.
      * </p>
+     *
      * @return The element, or <code>TYPE_FRAGMENT</code>
      */
     public String getType() {
@@ -232,14 +269,18 @@ public class AttributeTypeInfoConfig {
 
     /**
      * Indicate if the attribute is allowed to be <code>null</code>.
+     * 
      * <p>
-     * Nillable is often used to indicate that an attribute is optional.
-     * The use of minOccurs and maxOccurs may be a more correct way to
-     * indicate optional attribtues.
+     * Nillable is often used to indicate that an attribute is optional. The
+     * use of minOccurs and maxOccurs may be a more correct way to indicate
+     * optional attribtues.
      * </p>
+     *
+     * @return <code>true </code> to indicate attribute is alloed to be
+     *         <code>null</code>
+     *
      * @see AttributeTypeInfoDTO.setMinOccurs
      * @see AttributeTypeInfoDTO.setMaxOccurs
-     * @return <code>true </code> to indicate attribute is alloed to be <code>null</code>
      */
     public boolean isNillable() {
         return nillable;
@@ -247,39 +288,51 @@ public class AttributeTypeInfoConfig {
 
     /**
      * XML Fragment used to define stuff.
+     * 
      * <p>
      * This property is only used with getType() is equals to "(xml fragment)".
      * </p>
+     * 
      * <p>
      * baseGMLTypes can only be used in your XML fragment.
      * </p>
+     *
      * @param fragment The fragment to set.
      */
-    
-    
     public void setFragment(String fragment) {
-    	this.fragment = fragment;
+        this.fragment = fragment;
     }
 
     /**
      * Maxmium number of occurances of this attribute in a feature.
+     * 
      * <p>
-     * For Features based on the Simple Feature Specification this
-     * should be a value of 1. If the attribute is optional it should
-     * still be 1, although often optional is represented by allowing the
-     * Attribute to be <code>nillable</code>.
+     * For Features based on the Simple Feature Specification this should be a
+     * value of 1. If the attribute is optional it should still be 1, although
+     * often optional is represented by allowing the Attribute to be
+     * <code>nillable</code>.
      * </p>
+     * 
      * <p>
      * Common Min..Max Occurs values:
      * </p>
+     * 
      * <ul>
-     * <li>0..<b>1</b>: attribute is optional</li>
-     * <li>1..<b>1</b>: attribute is required (usual for Simple Features)</li>
-     * <li>0..<b>N</b>: attribute forms a list that may be empty</li>
+     * <li>
+     * 0..<b>1</b>: attribute is optional
+     * </li>
+     * <li>
+     * 1..<b>1</b>: attribute is required (usual for Simple Features)
+     * </li>
+     * <li>
+     * 0..<b>N</b>: attribute forms a list that may be empty
+     * </li>
      * </ul>
      * 
-     * @see AttributeTypeInfoDTO.isNillable
+     *
      * @param max The maximum number of occurances
+     *
+     * @see AttributeTypeInfoDTO.isNillable
      */
     public void setMaxOccurs(int max) {
         maxOccurs = max;
@@ -287,35 +340,49 @@ public class AttributeTypeInfoConfig {
 
     /**
      * Minimum number of occrances of this attribute in a feature.
+     * 
      * <p>
-     * For Features based on the Simple Feture Specification this
-     * should be a value of 1. If the attribute is optional is should
-     * be 0, although often optional is represented by allowing the attribute
-     * to be nillable.
+     * For Features based on the Simple Feture Specification this should be a
+     * value of 1. If the attribute is optional is should be 0, although often
+     * optional is represented by allowing the attribute to be nillable.
      * </p>
      * Common Min..Max Occurs values:
+     * 
      * <ul>
-     * <li><b>0</b>..1: attribute is optional</li>
-     * <li><b>1</b>..1: attribute is required (usual for Simple Features)</li>
-     * <li><b>0</b>..N: attribute forms a list that may be empty</li>
+     * <li>
+     * <b>0</b>..1: attribute is optional
+     * </li>
+     * <li>
+     * <b>1</b>..1: attribute is required (usual for Simple Features)
+     * </li>
+     * <li>
+     * <b>0</b>..N: attribute forms a list that may be empty
+     * </li>
      * </ul>
-     * @see AttributeTypeInfoDTO.isNillable
+     * 
+     *
      * @param min The minimum number of occurances
+     *
+     * @see AttributeTypeInfoDTO.isNillable
      */
     public void setMinOccurs(int min) {
         minOccurs = min;
     }
-    
+
     /**
      * Indicate if the attribute is allowed to be <code>null</code>.
+     * 
      * <p>
-     * Nillable is often used to indicate that an attribute is optional.
-     * The use of minOccurs and maxOccurs may be a more correct way to
-     * indicate optional attribtues.
+     * Nillable is often used to indicate that an attribute is optional. The
+     * use of minOccurs and maxOccurs may be a more correct way to indicate
+     * optional attribtues.
      * </p>
+     *
+     * @param nillable <code>true </code> to indicate attribute is alloed to be
+     *        <code>null</code>
+     *
      * @see AttributeTypeInfoDTO.setMinOccurs
      * @see AttributeTypeInfoDTO.setMaxOccurs
-     * @param nillable <code>true </code> to indicate attribute is alloed to be <code>null</code>
      */
     public void setNillable(boolean nillable) {
         this.nillable = nillable;
@@ -323,16 +390,15 @@ public class AttributeTypeInfoConfig {
 
     /**
      * Element type, a well-known gml or xs type or <code>TYPE_FRAGMENT</code>.
-     * <p>
-     * If getType is equals to <code>TYPE_FRAGMENT</code> please consult getFragment()
-     * to examin the actual user's definition.
-     * <p>
      * 
-     * </p>
+     * <p>
+     * If getType is equals to <code>TYPE_FRAGMENT</code> please consult
+     * getFragment() to examin the actual user's definition. <br>
      * Other than that getType should be one of the constants defined by
      * GMLUtils.
      * </p>
-     * @return type The element type, or <code>TYPE_FRAGMENT</code>
+     *
+     * @param type DOCUMENT ME!
      */
     public void setType(String type) {
         this.type = type;
