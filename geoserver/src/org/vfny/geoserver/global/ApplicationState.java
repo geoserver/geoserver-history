a4b11e13a65823b15be529955083a8c0aaf7f96d
jive <jive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-02-01 19:16:06 +0000

    Split out edited/changed status by subsystem
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@1949 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8cbaafed4ec7db48ec1e76631b5f2e4eed86f20e [formerly 4f473cb233c65cb8de0da712333e97c8129d2b43]
    Former-commit-id: 8ccbf11139f6cceddc2dd6eaf7a0a807b2e050d7

--- geoserver/src/org/vfny/geoserver/global/ApplicationState.java
+++ geoserver/src/org/vfny/geoserver/global/ApplicationState.java
@@ -32,17 +32,23 @@ import javax.servlet.ServletException;
  * </p>
  *
  * @author dzwiers, Refractions Research, Inc.
- * @version $Id: ApplicationState.java,v 1.4 2004/01/31 00:27:23 jive Exp $
+ * @version $Id: ApplicationState.java,v 1.5 2004/02/01 19:16:06 jive Exp $
  */
 public class ApplicationState implements PlugIn {
     /** The key used to store this value in the Web Container */
     public static final String WEB_CONTAINER_KEY = "GeoServer.ApplicationState";
 
     /** true if the configuration has been changed (but not applied) */
-    private boolean configChanged = false;
-
+    private boolean wmsEdited;
+    private boolean wfsEdited;
+    private boolean dataEdited;
+    private boolean validationEdited;
+    
     /** true if the geoserve setup has been changed (but not saved) */
-    private boolean geoServerChanged = false;
+    private boolean wmsChanged;
+    private boolean wfsChanged;
+    private boolean dataChanged;
+    private boolean validationChanged;
 
     /** magic, be very careful with this array. defined below in loadStatus() */
     private int[] geoserverStatus = new int[10];
@@ -92,98 +98,109 @@ public class ApplicationState implements PlugIn {
      * @return <code>true</code> if Configuration needs changing.
      */
     public boolean isConfigChanged() {
-        return configChanged;
+        return wmsEdited || wfsEdited || dataEdited || validationEdited;
     }
-
+    public void setConfigChanged(boolean edited ){
+    	wmsEdited = edited;
+    	wfsEdited = edited;
+    	dataEdited = edited;
+    	validationEdited = edited;
+    }
     /**
      * True if the user has changed GeoServer and not yet saved the changes.
      *
      * @return <code>true</code> if GeoServer has been changed (but not saved)
      */
     public boolean isGeoServerChanged() {
-        return geoServerChanged;
+        return wmsChanged || wfsChanged || dataChanged || validationChanged;
+    }    
+    public void setGeoServerChanged(boolean changed){
+    	wmsChanged = changed;
+    	wfsChanged = changed;
+    	dataChanged = changed;
+    	validationChanged = changed;
     }
-
     /**
      * Notification that Global has been updated from XML config files
      */
     public void notifyLoadXML() {
-        geoServerChanged = true;
-        configChanged = false;
+    	setGeoServerChanged( true );  // is this right? JG expected false
+        setConfigChanged( false );
     }
 
     /**
      * Notification that Global has been updated from Configuration
      */
     public void notifyToGeoServer() {
-        geoServerChanged = true;
-        configChanged = false;
+    	setGeoServerChanged( true );
+        setConfigChanged( false );
     }
 
     /**
      * Notification that Global has been saved to XML config files.
      */
     public void notifiySaveXML() {
-        geoServerChanged = false;
+        setGeoServerChanged( false );
     }
 
     /**
      * Notification that the User has changed the Configuration
      */
     public void notifyConfigChanged() {
-        configChanged = true;
+        setConfigChanged( true );
     }
-    
+    /** Q: what is this supposed to do? */
     public int getWfsGood(){
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0)){
     		loadStatus();
+    	}
     	return geoserverStatus[1];
     }
-    
+    /** q: What foul manner of magic is this? */
     public int getWfsBad() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[2];
     }
-    
+    /** q: This does not make a lot of sense - did you want to consult both ConfigChanged and GeoServer changed? */
     public int getWfsDisabled() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[3];
     }
-    
+    /** Q: scary magic */
     public int getWmsGood(){
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[4];
     }
-    
+    /** Q: scary magic */
     public int getWmsBad() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[5];
     }
-    
+    /** Q: scary magic */
     public int getWmsDisabled() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[6];
     }
     
     public int getDataGood(){
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[7];
     }
     
     public int getDataBad() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[8];
     }
     
     public int getDataDisabled() {
-    	if(geoserverStatus[0] != (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverStatus[0] != (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverStatus[9];
     }
@@ -195,12 +212,27 @@ public class ApplicationState implements PlugIn {
      * Magic occurs here, so be careful. This sets the values in the geoserverStatus array.
      * </p>
      * <p>
-     * The array is broken into four blocks, [0],[1-3],[4-6],[7-9]. The first block represtents the application state at the time of data creation. Use to test whether the array needs to be reloaded. The next 3 blocks contain the data for wfs,wms and data modules. Each module has three portions: working, broken, and disabled. Each of these is represented as a percentage. 
+     * The array is broken into four blocks, [0],[1-3],[4-6],[7-9].
+     * <ul>
+     * <li>[0] -  The first block represtents the application state at the time
+     *            of data creation. Use to test whether the array needs to be
+     *            reloaded.</li> 
+     * <li>[1-3] - wfs (working, broken, disabled) percentages</li>
+     * <li>[4-6] - wms (working, broken, disabled) percentages</li>
+     * <li>[7-9] - data (working, broken, disabled) percentages</li>
+     * </ul>
      * </p>
-     *
      */
     private void loadStatus(){
-    	geoserverStatus[0] = (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0);
+    	// what does isGeoServerChanged() have to do with this
+    	// and why don't you use (isGeoServerChanged() ? 3 : 0)
+    	//
+    	// or are you trying to indicate a bit mask:
+    	// bit 1: isGeoServerChanged
+    	// bit 2: isConfigChanged
+    	//
+    	// And all this madness is a cut and paste mistake?
+    	geoserverStatus[0] = (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0);
     	GeoServer gs = (GeoServer) sc.getAttribute(GeoServer.WEB_CONTAINER_KEY);
     	if (gs == null)
     		return;
@@ -300,7 +332,8 @@ public class ApplicationState implements PlugIn {
      * @return
      */
     public Map getNameSpaceErrors(){
-    	if(geoserverNSErrors==null || geoserverStatus[0] == (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverNSErrors==null ||
+    	   geoserverStatus[0] == (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverNSErrors;
     }
@@ -321,7 +354,7 @@ public class ApplicationState implements PlugIn {
      * @return
      */
     public Map getDataStoreErrors(){
-    	if(geoserverDSErrors==null || geoserverStatus[0] == (geoServerChanged ? 1 : 0)+(geoServerChanged ? 2 : 0))
+    	if(geoserverDSErrors==null || geoserverStatus[0] == (isGeoServerChanged() ? 1 : 0)+(isGeoServerChanged() ? 2 : 0))
     		loadStatus();
     	return geoserverDSErrors;
     }
@@ -349,4 +382,100 @@ public class ApplicationState implements PlugIn {
     public List getWMSErrorKeys(){
     	return getNameSpaceErrorKeys();
     }
+	/**
+	 * @return Returns the dataChanged.
+	 */
+	public boolean isDataChanged() {
+		return dataChanged;
+	}
+	/**
+	 * @param dataChanged The dataChanged to set.
+	 */
+	public void setDataChanged(boolean dataChanged) {
+		this.dataChanged = dataChanged;
+	}
+	/**
+	 * @return Returns the dataEdited.
+	 */
+	public boolean isDataEdited() {
+		return dataEdited;
+	}
+	/**
+	 * @param dataEdited The dataEdited to set.
+	 */
+	public void setDataEdited(boolean dataEdited) {
+		this.dataEdited = dataEdited;
+	}
+	/**
+	 * @return Returns the validationChanged.
+	 */
+	public boolean isValidationChanged() {
+		return validationChanged;
+	}
+	/**
+	 * @param validationChanged The validationChanged to set.
+	 */
+	public void setValidationChanged(boolean validationChanged) {
+		this.validationChanged = validationChanged;
+	}
+	/**
+	 * @return Returns the validationEdited.
+	 */
+	public boolean isValidationEdited() {
+		return validationEdited;
+	}
+	/**
+	 * @param validationEdited The validationEdited to set.
+	 */
+	public void setValidationEdited(boolean validationEdited) {
+		this.validationEdited = validationEdited;
+	}
+	/**
+	 * @return Returns the wfsChanged.
+	 */
+	public boolean isWfsChanged() {
+		return wfsChanged;
+	}
+	/**
+	 * @param wfsChanged The wfsChanged to set.
+	 */
+	public void setWfsChanged(boolean wfsChanged) {
+		this.wfsChanged = wfsChanged;
+	}
+	/**
+	 * @return Returns the wfsEdited.
+	 */
+	public boolean isWfsEdited() {
+		return wfsEdited;
+	}
+	/**
+	 * @param wfsEdited The wfsEdited to set.
+	 */
+	public void setWfsEdited(boolean wfsEdited) {
+		this.wfsEdited = wfsEdited;
+	}
+	/**
+	 * @return Returns the wmsChanged.
+	 */
+	public boolean isWmsChanged() {
+		return wmsChanged;
+	}
+	/**
+	 * @param wmsChanged The wmsChanged to set.
+	 */
+	public void setWmsChanged(boolean wmsChanged) {
+		this.wmsChanged = wmsChanged;
+	}
+	/**
+	 * @return Returns the wmsEdited.
+	 */
+	public boolean isWmsEdited() {
+		return wmsEdited;
+	}
+	/**
+	 * @param wmsEdited The wmsEdited to set.
+	 */
+	public void setWmsEdited(boolean wmsEdited) {
+		this.wmsEdited = wmsEdited;
+	}
 }
