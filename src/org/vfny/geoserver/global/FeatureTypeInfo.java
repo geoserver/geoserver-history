94f8e23fd7a677f50256138ccb5d3c0b4e8e620f
dmzwiers <dmzwiers@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-03-08 21:59:12 +0000

    geos 108
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@2266 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ff9a5cb632cc33ec20ad99e5e3f164b8b92b53fe [formerly b43f74ac441465a2b3e123f5a823475becc0ea9f]
    Former-commit-id: e59a8cc2ec0ecbfc57e7ae35ca5dcf7788f6d347

--- src/org/vfny/geoserver/global/FeatureTypeInfo.java
+++ src/org/vfny/geoserver/global/FeatureTypeInfo.java
@@ -38,7 +38,7 @@ import com.vividsolutions.jts.geom.Envelope;
  * @author Gabriel Roldán
  * @author Chris Holmes
  * @author dzwiers
- * @version $Id: FeatureTypeInfo.java,v 1.32 2004/03/08 21:51:58 groldan Exp $
+ * @version $Id: FeatureTypeInfo.java,v 1.33 2004/03/08 21:59:12 dmzwiers Exp $
  */
 public class FeatureTypeInfo extends GlobalLayerSupertype
     implements FeatureTypeMetaData {
@@ -695,60 +695,53 @@ public class FeatureTypeInfo extends GlobalLayerSupertype
     	return getFeatureType(getFeatureSource());
     }
 
-    /**
-     *
-     */
     private FeatureType getFeatureType(FeatureSource fs) throws IOException {
     	if(ft == null){
-        int count = 0;
-        ft = fs.getSchema();
-        String[] baseNames = DataTransferObjectFactory.getRequiredBaseAttributes(
-            schemaBase);
-        AttributeType[] attributes = new AttributeType[schema.size() +
-            baseNames.length];
-        if (attributes.length > 0) {
-          int errors = 0;
-          for (; count < baseNames.length; count++) {
-            attributes[count - errors] = ft.getAttributeType(baseNames[count]);
-            if (attributes[count - errors] == null) {
-              // desired base attr is not availiable
-              errors++;
-            }
-          }
-
-          if (errors != 0) {
-            //resize array;
-            AttributeType[] tmp = new AttributeType[attributes.length - errors];
-            count = count - errors;
-            for (int i = 0; i < count; i++) {
-              tmp[i] = attributes[i];
-            }
-            attributes = tmp;
-          }
-
-          for (Iterator i = schema.iterator(); i.hasNext(); ) {
-            AttributeTypeInfo ati = (AttributeTypeInfo) i.next();
-            String attName = ati.getName();
-            attributes[count] = ft.getAttributeType(attName);
-            if (attributes[count] == null) {
-              throw new IOException("the FeatureType " + getName()
-                                    +
-                  " does not contains the configured attribute " + attName
-                                    + ". Check your schema configuration");
-            }
-            count++;
-          }
-          try {
-            ft = FeatureTypeFactory.newFeatureType(attributes,
-                                                   name);
-          }
-          catch (SchemaException ex) {
-          }
-          catch (FactoryConfigurationError ex) {
-          }
-
-        }
-      }
+    		if(schemaBase==null || schemaBase==""){
+    			ft = fs.getSchema();
+    		}else{
+    			int count = 0;
+    			ft = fs.getSchema();
+    			String[] baseNames = DataTransferObjectFactory.getRequiredBaseAttributes( schemaBase );
+    			AttributeType[] attributes = new AttributeType[schema.size()+baseNames.length];
+    			int errors = 0;
+    			for(;count<baseNames.length;count++){
+    				attributes[count-errors] = ft.getAttributeType(baseNames[count]);
+    				if (attributes[count-errors] == null) {
+    					// desired base attr is not availiable
+    					errors++;
+    				}
+    			}
+    		
+    			if(errors!=0){
+    				//	resize array;
+    				AttributeType[] tmp = new AttributeType[attributes.length-errors];
+    				count = count-errors;
+    				for(int i=0;i<count;i++){
+    					tmp[i] = attributes[i];
+    				}
+    				attributes = tmp;
+    			}
+    		
+            	for (Iterator i = schema.iterator(); i.hasNext();) {
+            		AttributeTypeInfo ati = (AttributeTypeInfo)i.next();
+            		String attName = ati.getName();
+            		attributes[count] = ft.getAttributeType(attName);
+            		if (attributes[count] == null) {
+            			throw new IOException("the FeatureType " + getName()
+            					+ " does not contains the configured attribute " + attName
+								+ ". Check your schema configuration");
+            		}
+            		count++;
+            	}
+            	try {
+            		ft = FeatureTypeFactory.newFeatureType(attributes,name);
+        		} catch (SchemaException ex) {
+        		} catch (FactoryConfigurationError ex) {
+        		}
+    		}
+    		
+    	}
     	return ft;
     }
 
