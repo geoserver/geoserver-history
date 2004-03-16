b435bc74618666a6a2561be2e14b02300df57363
dmzwiers <dmzwiers@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-03-16 23:58:31 +0000

    Fixes for jody, he can put the UI portion in so the results are put on the screen
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@2341 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 42012bb60a6be7e17bbdb8209418e68bff8fac7a [formerly 2b4e1cfc36d52ea3bd2d8fa45fc7e74e05fdcf3e]
    Former-commit-id: b3c7808f9e2b964c444ec1fcb25b33e6174623dc

--- src/org/vfny/geoserver/action/validation/TestValidationResults.java
+++ src/org/vfny/geoserver/action/validation/TestValidationResults.java
@@ -21,13 +21,16 @@ import org.geotools.validation.ValidationResults;
  * 
  * @author dzwiers, Refractions Research, Inc.
  * @author $Author: dmzwiers $ (last modification)
- * @version $Id: TestValidationResults.java,v 1.1 2004/02/28 00:54:26 dmzwiers Exp $
+ * @version $Id: TestValidationResults.java,v 1.2 2004/03/16 23:58:31 dmzwiers Exp $
  */
 public class TestValidationResults implements ValidationResults{
 	public static final String CURRENTLY_SELECTED_KEY = "TestValidationResults";
 	
 	Validation v = null;
-	public void setValidation(Validation v){this.v = v;}
+	
+	//HACK for JODY cause he messed up and then whined alot.
+	boolean run = false;
+	public void setValidation(Validation v){this.v = v;run=true;}
 	
 	Map errors = new HashMap();
 	public Map getErrors(){return errors;}
@@ -36,4 +39,22 @@ public class TestValidationResults implements ValidationResults{
 	Map warning = new HashMap();
 	public Map getWarnings(){return warning;}
 	public void warning(Feature f,String s){errors.put(f,s);}
+	
+	/**
+	 * Access run property.
+	 * 
+	 * @return Returns the run.
+	 */
+	public boolean isRun() {
+		return run;
+	}
+	
+	/**
+	 * Set run to run.
+	 *
+	 * @param run The run to set.
+	 */
+	public void setRun(boolean run) {
+		this.run = run;
+	}
 }
