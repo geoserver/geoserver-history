eb39dccf82079ee73f5fe776f866c6a1a68436dc
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-04-08 12:01:16 +0000

    fix for nullpointer exceptions with tomcat
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@2454 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6844be8eccbe57d8f06926a3e701c612122dfc47 [formerly 6aeaa0209ef5300aa35325e3ed3b4cecd8a9507c]
    Former-commit-id: a9a1bb2dc0d4bea96f9968fefff2ea2a3fc7bb0b

--- geoserver/src/org/vfny/geoserver/action/SaveXMLAction.java
+++ geoserver/src/org/vfny/geoserver/action/SaveXMLAction.java
@@ -2,24 +2,7 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-/*
- * Created on Jan 6, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package org.vfny.geoserver.action;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -34,124 +17,149 @@ import org.vfny.geoserver.global.dto.GeoServerDTO;
 import org.vfny.geoserver.global.dto.WFSDTO;
 import org.vfny.geoserver.global.dto.WMSDTO;
 import org.vfny.geoserver.global.xml.XMLConfigWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
  * Save GeoServer state to XML.
+ * 
  * <p>
  * This is a propert ConfigAction - you need to be logged in for this to work.
  * </p>
  */
 public class SaveXMLAction extends ConfigAction {
-	
-	public ActionForward execute(ActionMapping mapping,
-			ActionForm form,
-			UserContainer user,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws IOException, ServletException {
-		ActionForward r1 = saveGeoserver(mapping,form,request,response);
-		ActionForward r2 = saveValidation(mapping,form,request,response);
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+        UserContainer user, HttpServletRequest request,
+        HttpServletResponse response) throws IOException, ServletException {
+        ActionForward r1 = saveGeoserver(mapping, form, request, response);
+        ActionForward r2 = saveValidation(mapping, form, request, response);
 
-        getApplicationState( request ).notifiySaveXML();
+        getApplicationState(request).notifiySaveXML();
+
+        return mapping.findForward("config");
+    }
+
+    private ActionForward saveGeoserver(ActionMapping mapping, ActionForm form,
         
-		return mapping.findForward("config");
-	}
-	
-    private ActionForward saveGeoserver(ActionMapping mapping,
-    		                     ActionForm form,
-								 //UserContainer user,
-								 HttpServletRequest request,
-								 HttpServletResponse response)
+    //UserContainer user,
+    HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         ServletContext sc = request.getSession().getServletContext();
         File rootDir = new File(sc.getRealPath("/"));
 
         try {
-            XMLConfigWriter.store((WMSDTO)getWMS(request).toDTO(), (WFSDTO)getWFS(request).toDTO(),
-            		(GeoServerDTO)getWFS(request).getGeoServer().toDTO(), (DataDTO)getWFS(request).getData().toDTO(), rootDir);
+            XMLConfigWriter.store((WMSDTO) getWMS(request).toDTO(),
+                (WFSDTO) getWFS(request).toDTO(),
+                (GeoServerDTO) getWFS(request).getGeoServer().toDTO(),
+                (DataDTO) getWFS(request).getData().toDTO(), rootDir);
         } catch (ConfigurationException e) {
-        	e.printStackTrace();
+            e.printStackTrace();
             throw new ServletException(e);
-        }        	
+        }
+
         // We need to stash the current page?
         // or can we use null or something?
         //
         return mapping.findForward("config");
     }
-    
-    private ActionForward saveValidation(ActionMapping mapping,
-    		ActionForm form,
-			//UserContainer user,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws IOException, ServletException {
-    	ServletContext sc = request.getSession().getServletContext();
-    	File rootDir = new File(sc.getRealPath("/"));
-    	File plugInDir = new File(rootDir, "data/plugIns");
-    	File validationDir = new File(rootDir, "data/validation");
 
-    	Map plugIns = (Map)getWFS(request).getValidation().toPlugInDTO();
-    	Map testSuites = (Map)getWFS(request).getValidation().toTestSuiteDTO();
-    	
-    	Iterator i = null;
-    	
-    	i = plugIns.keySet().iterator();
-    	while(i.hasNext()){
-    		PlugInDTO dto = null;
-    		Object key = null;
-    		try {
-    			key = i.next();
-    			dto = (PlugInDTO)plugIns.get(key);
-    			FileWriter fw = new FileWriter(new File(plugInDir,dto.getName().replaceAll(" ","")+".xml"));
-    			XMLWriter.writePlugIn(dto,fw);
-    			fw.close();
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    			throw new ServletException(e);
-    		}
-    	}
-    	// deletes of plug ins here
-    	/*File[] pluginFL = plugInDir.listFiles();
-    	for(int j=0;j<pluginFL.length;j++){
-    		String flName = pluginFL[j].getName();
-    		flName = flName.substring(0,flName.length()-4);
-    		if(plugIns.get(flName)==null){
-    			// delete this
-    			pluginFL[j].delete();
-    		}
-    	}*/
-    	
-    	
-    	i = testSuites.keySet().iterator();
-    	while(i.hasNext()){
-    		TestSuiteDTO dto = null;
-    		try {
-    			dto = (TestSuiteDTO)testSuites.get(i.next());
-    			FileWriter fw = new FileWriter(new File(validationDir,dto.getName().replaceAll(" ","")+".xml"));
-    			XMLWriter.writeTestSuite(dto,fw);
-    			fw.close();
-    		} catch (Exception e) {
-    			System.err.println(dto.getClass());
-    			e.printStackTrace();
-    			throw new ServletException(e);
-    		}
-    	}
-		// deletes of testSuites here
-		File[] testsFL = validationDir.listFiles();
-		for(int j=0;j<testsFL.length;j++){
-			String flName = testsFL[j].getName();
-			flName = flName.substring(0,flName.length()-4);
-			if(testSuites.get(flName)==null){
-				// delete this
-				testsFL[j].delete();
-			}
-		}
-    	
-    	getApplicationState( request ).notifiySaveXML();	
-    	// We need to stash the current page?
-    	// or can we use null or something?
-    	//
-    	return mapping.findForward("config.validation");
+    private ActionForward saveValidation(ActionMapping mapping,
+        ActionForm form, 
+    //UserContainer user,
+    HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        ServletContext sc = request.getSession().getServletContext();
+        File rootDir = new File(sc.getRealPath("/"));
+        File plugInDir = new File(rootDir, "data/plugIns");
+        File validationDir = new File(rootDir, "data/validation");
+
+        Map plugIns = (Map) getWFS(request).getValidation().toPlugInDTO();
+        Map testSuites = (Map) getWFS(request).getValidation().toTestSuiteDTO();
+
+        Iterator i = null;
+
+        if (plugIns != null) {
+            i = plugIns.keySet().iterator();
+
+            while (i.hasNext()) {
+                PlugInDTO dto = null;
+                Object key = null;
+
+                try {
+                    key = i.next();
+                    dto = (PlugInDTO) plugIns.get(key);
+
+                    FileWriter fw = new FileWriter(new File(plugInDir,
+                                dto.getName().replaceAll(" ", "") + ".xml"));
+                    XMLWriter.writePlugIn(dto, fw);
+                    fw.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new ServletException(e);
+                }
+            }
+        }
+
+        // deletes of plug ins here
+
+        /*File[] pluginFL = plugInDir.listFiles();
+           for(int j=0;j<pluginFL.length;j++){
+                   String flName = pluginFL[j].getName();
+                   flName = flName.substring(0,flName.length()-4);
+                   if(plugIns.get(flName)==null){
+                           // delete this
+                           pluginFL[j].delete();
+                   }
+           }*/
+        if (testSuites != null) {
+            i = testSuites.keySet().iterator();
+
+            while (i.hasNext()) {
+                TestSuiteDTO dto = null;
+
+                try {
+                    dto = (TestSuiteDTO) testSuites.get(i.next());
+
+                    FileWriter fw = new FileWriter(new File(validationDir,
+                                dto.getName().replaceAll(" ", "") + ".xml"));
+                    XMLWriter.writeTestSuite(dto, fw);
+                    fw.close();
+                } catch (Exception e) {
+                    System.err.println(dto.getClass());
+                    e.printStackTrace();
+                    throw new ServletException(e);
+                }
+            }
+        }
+
+        // deletes of testSuites here
+        File[] testsFL = validationDir.listFiles();
+
+        if (testsFL != null) {
+            for (int j = 0; j < testsFL.length; j++) {
+                String flName = testsFL[j].getName();
+                flName = flName.substring(0, flName.length() - 4);
+
+                if (testSuites.get(flName) == null) {
+                    // delete this
+                    testsFL[j].delete();
+                }
+            }
+        }
+
+        getApplicationState(request).notifiySaveXML();
+
+        // We need to stash the current page?
+        // or can we use null or something?
+        //
+        return mapping.findForward("config.validation");
     }
 }
