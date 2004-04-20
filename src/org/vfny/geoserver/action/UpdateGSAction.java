274d08942770931269ff2d981f4b22f389deb1b5
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-04-20 17:10:13 +0000

    update for bug in demo conf target, with no validation suites
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@2587 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a9f3e5ab22da24497d23459f0fda2f9e8ac845bf [formerly 12edeaa97092186ab9ca0aaabd07a13f7c052fdb]
    Former-commit-id: 28172b91a25107b15ace949126099639459766c6

--- src/org/vfny/geoserver/action/UpdateGSAction.java
+++ src/org/vfny/geoserver/action/UpdateGSAction.java
@@ -10,15 +10,6 @@
  */
 package org.vfny.geoserver.action;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -29,8 +20,18 @@ import org.vfny.geoserver.global.dto.DataDTO;
 import org.vfny.geoserver.global.dto.GeoServerDTO;
 import org.vfny.geoserver.global.dto.WFSDTO;
 import org.vfny.geoserver.global.dto.WMSDTO;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 /**
  * Update GeoServer with the current configuration.
+ * 
  * <p>
  * This is a real ConfigAction - you need to be logged in to use it.
  * </p>
@@ -39,78 +40,77 @@ import org.vfny.geoserver.global.dto.WMSDTO;
  *         Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class UpdateGSAction extends ConfigAction {
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+        UserContainer user, HttpServletRequest request,
+        HttpServletResponse response) throws IOException, ServletException {
+        ActionForward r1 = updateGeoserver(mapping, form, request, response);
+        ActionForward r2 = updateValidation(mapping, form, request, response);
 
-	public ActionForward execute(ActionMapping mapping,
-			ActionForm form,
-			UserContainer user,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws IOException, ServletException {
-		ActionForward r1 = updateGeoserver(mapping,form,request,response);
-		ActionForward r2 = updateValidation(mapping,form,request,response);
-		
-		return mapping.findForward("config");
-	}
-	
+        return mapping.findForward("config");
+    }
+
     public ActionForward updateGeoserver(ActionMapping mapping,
-    							 ActionForm form,
-								 // UserContainer user,
-								 HttpServletRequest request,
-								 HttpServletResponse response)
+        ActionForm form, 
+    // UserContainer user,
+    HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         GeoServer gs;
         ServletContext sc = request.getSession().getServletContext();
 
         try {
-        	WMSDTO wmsDTO = getWMSConfig().toDTO();
-        	WFSDTO wfsDTO = getWFSConfig().toDTO();
-        	GeoServerDTO geoserverDTO = getGlobalConfig().toDTO();
-        	DataDTO dataDTO = getDataConfig().toDTO();
-        	
-        	getWFS(request).load(wfsDTO); 
-        	getWMS(request).load(wmsDTO); 
-        	getWFS(request).getGeoServer().load(geoserverDTO); 
-        	getWFS(request).getData().load(dataDTO);
-        	
-        	getApplicationState().notifyToGeoServer();
-        	
+            WMSDTO wmsDTO = getWMSConfig().toDTO();
+            WFSDTO wfsDTO = getWFSConfig().toDTO();
+            GeoServerDTO geoserverDTO = getGlobalConfig().toDTO();
+            DataDTO dataDTO = getDataConfig().toDTO();
+
+            getWFS(request).load(wfsDTO);
+            getWMS(request).load(wmsDTO);
+            getWFS(request).getGeoServer().load(geoserverDTO);
+            getWFS(request).getData().load(dataDTO);
+
+            getApplicationState().notifyToGeoServer();
         } catch (ConfigurationException e) {
-        	e.printStackTrace();
+            e.printStackTrace();
             throw new ServletException(e);
         }
 
         // We need to stay on the same page!
-        getApplicationState( request ).notifyToGeoServer();
-        
+        getApplicationState(request).notifyToGeoServer();
+
         return mapping.findForward("config");
     }
-    
+
     public ActionForward updateValidation(ActionMapping mapping,
-    		ActionForm form,
-			// UserContainer user,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws IOException, ServletException {
-    	GeoServer gs;
-    	ServletContext sc = request.getSession().getServletContext();
-    	gs = (GeoServer) sc.getAttribute(GeoServer.WEB_CONTAINER_KEY);
+        ActionForm form, 
+    // UserContainer user,
+    HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        GeoServer gs;
+        ServletContext sc = request.getSession().getServletContext();
+        gs = (GeoServer) sc.getAttribute(GeoServer.WEB_CONTAINER_KEY);
 
-    	try {
-    		Map plugins = new HashMap();
-    		Map testSuites = new HashMap(); 
-    		if(getValidationConfig().toDTO(plugins,testSuites)){
-    			getWFS(request).getValidation().load( testSuites,plugins );
-    		}else{
-    			throw new ConfigurationException("ValidationConfig experienced an error exporting Data Transpher Objects.");
-    		}
-    	} catch (ConfigurationException e) {
-    		e.printStackTrace();
-    		throw new ServletException(e);
-    	}
+        try {
+            Map plugins = new HashMap();
+            Map testSuites = new HashMap();
 
-    	// We need to stay on the same page!
-    	getApplicationState( request ).notifyToGeoServer();
-    	
-    	return mapping.findForward("config.validation");
+            if (getValidationConfig().toDTO(plugins, testSuites)) {
+                //sorry, no time to really test this, but I got a null pointer
+                //exception with the demo build target. ch
+                if (getWFS(request).getValidation() != null) {
+                    getWFS(request).getValidation().load(testSuites, plugins);
+                }
+            } else {
+                throw new ConfigurationException(
+                    "ValidationConfig experienced an error exporting Data Transpher Objects.");
+            }
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+
+        // We need to stay on the same page!
+        getApplicationState(request).notifyToGeoServer();
+
+        return mapping.findForward("config.validation");
     }
 }
