739485d897949eaf9b50aadd8023398d30fd5cc6
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-06-03 19:34:36 +0000

    update to always use file response (so that commits always get reported)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@2688 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 28f27338d022997c8388d0a6e6589ec2aef8cdda [formerly 8a89b0b0e05483481c59f46cf0b3a8af6b8ec132]
    Former-commit-id: b29a93c952e03f75590d3da5b4903226c49e290c

--- geoserver/src/org/vfny/geoserver/servlets/wfs/Transaction.java
+++ geoserver/src/org/vfny/geoserver/servlets/wfs/Transaction.java
@@ -4,19 +4,16 @@
  */
 package org.vfny.geoserver.servlets.wfs;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.vfny.geoserver.global.dto.WFSDTO;
-import org.vfny.geoserver.requests.Requests;
+import org.vfny.geoserver.ServiceException;
 import org.vfny.geoserver.requests.readers.KvpRequestReader;
 import org.vfny.geoserver.requests.readers.XmlRequestReader;
 import org.vfny.geoserver.requests.readers.wfs.DeleteKvpReader;
 import org.vfny.geoserver.requests.readers.wfs.TransactionXmlReader;
 import org.vfny.geoserver.responses.Response;
 import org.vfny.geoserver.responses.wfs.TransactionResponse;
+import org.vfny.geoserver.servlets.AbstractService;
 import org.vfny.geoserver.servlets.WFService;
+import java.util.Map;
 
 
 /**
@@ -26,35 +23,60 @@ import org.vfny.geoserver.servlets.WFService;
  *
  * @author Chris Holmes, TOPP
  * @author Gabriel Roldán
- * @version $Id: Transaction.java,v 1.10 2004/02/20 22:23:45 jive Exp $
+ * @version $Id: Transaction.java,v 1.11 2004/06/03 19:34:36 cholmesny Exp $
  */
 public class Transaction extends WFService {
     /**
-     * DOCUMENT ME!
+     * Returns the handler for a transaction.
      *
-     * @return DOCUMENT ME!
+     * @return An instance of the TransactionResponse class.
      */
     protected Response getResponseHandler() {
         return new TransactionResponse();
     }
 
     /**
-     * DOCUMENT ME!
+     * Returns the delete kvp reader, as delete is the only transaction that
+     * can be made with key value pairs.
      *
-     * @param params DOCUMENT ME!
+     * @param params A map of the kvps.
      *
-     * @return DOCUMENT ME!
+     * @return The delete kvp reader.
      */
     protected KvpRequestReader getKvpReader(Map params) {
         return new DeleteKvpReader(params);
     }
 
     /**
-     * DOCUMENT ME!
+     * Returns the xml reader for transactions.
      *
-     * @return DOCUMENT ME!
+     * @return The transaction xml reader.
+     *
+     * @see TransactionXmlReader
      */
     protected XmlRequestReader getXmlRequestReader() {
         return new TransactionXmlReader();
+    }
+
+    /**
+     * Override to always return a FILE strategy.  This ensures that errors
+     * with commits will always be reported.  The expense of using a FILE
+     * response here is not that great, as transaction responses are generally
+     * not very long.  The importance of reporting an error with a commit is
+     * far greater than returning a slightly quicker response time.
+     *
+     * @return The fileStrategy.
+     *
+     * @throws ServiceException If the fileStrategy is not availble.
+     */
+    protected AbstractService.ServiceStrategy getServiceStrategy()
+        throws ServiceException {
+        //There's probably a better way to do this, but I'm confused by
+        //all the inner classes and interfaces and how to get access to
+        //them.  This method appears to work, so we'll stick with it
+        //until someone implements a cleaner way.
+        Class strategyClass = (Class) serviceStrategys.get("FILE");
+
+        return super.getServiceStrategy(strategyClass);
     }
 }
