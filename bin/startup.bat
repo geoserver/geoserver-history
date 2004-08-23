250075cd86abd1045c909ec37233b142e56ce2e7
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-08-23 13:42:48 +0000

    re-added working scripts
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@2837 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f3e74085737d23b36754b93ceabcc773c5856023 [formerly 847cc985c2df04911294d1006842c81f889e3ad4]
    Former-commit-id: c37f72d7062d9310cf6f2c622e7025c2dfd87c90

---
+++ bin/startup.bat
@@ -0,0 +1,60 @@
+@echo off
+REM -----------------------------------------------------------------------------
+REM Start Script for GEOSERVER
+REM
+REM $Id: startup.bat,v 1.5 2004/08/23 13:42:48 cholmesny Exp $
+REM -----------------------------------------------------------------------------
+
+if "%JAVA_HOME%" == "" goto noJava1
+
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJava2
+
+if "%GEOSERVER_HOME%" == "" goto noGeo1
+
+if not exist "%GEOSERVER_HOME%\bin\startup.bat" goto noGeo2
+
+REM -------------
+REM OK, we're ready to try actually runnning it.
+REM -------------
+
+java -jar "%GEOSERVER_HOME%\bin\start.jar"
+
+goto end
+
+:noJava1
+  echo The JAVA_HOME environment variable is not defined.
+  echo This environment variable is needed to run this program.
+goto end
+
+:noJava2
+  echo The JAVA_HOME environment variable is defined, but 'java.exe'
+  echo was not found there.
+goto end
+
+:noGeo1
+  if exist start.jar goto doGeo1
+  echo The GEOSERVER_HOME environment variable is not defined.
+  echo This environment variable is needed to run this program.
+goto end
+
+:doGeo1
+echo GEOSERVER_HOME environment variable not found.  Using current
+echo directory.  Please set GEOSERVER_HOME for future uses.
+ java -jar start.jar
+ goto end
+
+:noGeo2
+  if exist start.jar goto doGeo2
+  echo The GEOSERVER_HOME environment variable is not defined correctly.
+  echo This environment variable is needed to run this program.
+goto end
+
+:doGeo2
+  echo GEOSERVER_HOME environment variable not properly set.  Using parent
+  echo directory of this script.  Please set GEOSERVER_HOME correctly for 
+  echo future uses.
+  java -jar start.jar
+goto end
+
+:end
+ pause
