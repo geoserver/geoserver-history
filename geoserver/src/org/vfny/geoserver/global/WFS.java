65d188f9a0130046ec0a9fce0db1034418028369
cholmesny <cholmesny@ef1d6e69-97e5-0310-af46-8a06194da32a>
2004-09-09 16:54:19 +0000

    updates for srs xml style in wfs instead of geoserver
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@2872 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 55d66de56669e89531edf6a5adf54d3e7043e59c [formerly f1e2d3dbc09b864d5ea0e6f0eea438adbe839f3e]
    Former-commit-id: ed9ad2db0b4472a639dabbc6cd34d57e51c9082b

--- geoserver/src/org/vfny/geoserver/global/WFS.java
+++ geoserver/src/org/vfny/geoserver/global/WFS.java
@@ -26,15 +26,14 @@ import org.vfny.geoserver.global.dto.WFSDTO;
  *
  * @author Gabriel Roldán
  * @author Chris Holmes
- * @version $Id: WFS.java,v 1.7 2004/02/12 00:43:20 dmzwiers Exp $
+ * @version $Id: WFS.java,v 1.8 2004/09/09 16:54:19 cholmesny Exp $
  */
 public class WFS extends Service {
-    private boolean gmlPrefixing;
     public static final String WEB_CONTAINER_KEY = "WFS";
-    
+    private boolean gmlPrefixing;
     private GeoValidator gv;
-    
     private int serviceLevel;
+    private boolean srsXmlStyle;
 
     /**
      * WFS constructor.
@@ -49,21 +48,10 @@ public class WFS extends Service {
     public WFS(WFSDTO config) {
         super(config.getService());
         gmlPrefixing = config.isGmlPrefixing();
+        srsXmlStyle = config.isSrsXmlStyle();
         serviceLevel = config.getServiceLevel();
     }
-    
-    /**
-     * load purpose.
-     * <p>
-     * Loads a new data set into this object.
-     * </p>
-     * @param config
-     */
-    public void load (WFSDTO config) {
-    	super.load(config.getService());
-    	gmlPrefixing = config.isGmlPrefixing();
-    	serviceLevel = config.getServiceLevel();
-    }
+
     /**
      * WFS constructor.
      * 
@@ -75,6 +63,22 @@ public class WFS extends Service {
      */
     WFS() {
         super(new ServiceDTO());
+    }
+
+    /**
+     * load purpose.
+     * 
+     * <p>
+     * Loads a new data set into this object.
+     * </p>
+     *
+     * @param config
+     */
+    public void load(WFSDTO config) {
+        super.load(config.getService());
+        srsXmlStyle = config.isSrsXmlStyle();
+        gmlPrefixing = config.isGmlPrefixing();
+        serviceLevel = config.getServiceLevel();
     }
 
     /**
@@ -94,9 +98,11 @@ public class WFS extends Service {
      */
     public Object toDTO() {
         WFSDTO dto = new WFSDTO();
-        dto.setService((ServiceDTO)super.toDTO());
+        dto.setService((ServiceDTO) super.toDTO());
         dto.setGmlPrefixing(gmlPrefixing);
         dto.setServiceLevel(serviceLevel);
+        dto.setSrsXmlStyle(srsXmlStyle);
+
         return dto;
     }
 
@@ -125,31 +131,66 @@ public class WFS extends Service {
     public void setGmlPrefixing(boolean b) {
         gmlPrefixing = b;
     }
-	/**
-	 * Access gv property.
-	 * 
-	 * @return Returns the gv.
-	 */
-	public GeoValidator getValidation() {
-		return gv;
-	}
 
-	/**
-	 * Set gv to gv.
-	 *
-	 * @param gv The gv to set.
-	 */
-	void setValidation(GeoValidator gv) {
-		this.gv = gv;
-	}
+    /**
+     * Whether the srs xml attribute should be in the EPSG:4326 (non-xml)
+     * style, or in the http://www.opengis.net/gml/srs/epsg.xml#4326 style.
+     *
+     * @return <tt>true</tt> if the srs is reported with the xml style
+     */
+    public boolean isSrsXmlStyle() {
+        return srsXmlStyle;
+    }
 
-	/**
-	 * Access serviceLevel property.
-	 * 
-	 * @return Returns the serviceLevel.
-	 */
-	public int getServiceLevel() {
-		return serviceLevel;
-	}
+    /**
+     * Sets whether the srs xml attribute should be in the EPSG:4326 (non-xml)
+     * style, or in the http://www.opengis.net/gml/srs/epsg.xml#4326 style.
+     *
+     * @param doXmlStyle whether the srs style should be xml or not.
+     */
+    public void setSrsXmlStyle(boolean doXmlStyle) {
+        this.srsXmlStyle = doXmlStyle;
+    }
 
+    /**
+     * Gets the prefix for the srs name, based on the SrsXmlStyle property.  If
+     * srsXmlStyle is <tt>true</tt> then it is of the xml style, if false then
+     * it is EPSG: style.  More apps seem to like the EPSG: style, but the
+     * specs seem to lean more to the xml type.  The xml style should actually
+     * be more complete, with online lookups for the URI's, but I've seen no
+     * real online srs services.
+     *
+     * @return <tt>http://www.opengis.net/gml/srs/epsg.xml#</tt> if srsXmlStyle
+     *         is  <tt>true</tt>, <tt>EPSG:</tt> if <tt>false</tt>
+     */
+    public String getSrsPrefix() {
+        return srsXmlStyle ? "http://www.opengis.net/gml/srs/epsg.xml#" : "EPSG:";
+    }
+
+    /**
+     * Access gv property.
+     *
+     * @return Returns the gv.
+     */
+    public GeoValidator getValidation() {
+        return gv;
+    }
+
+    /**
+     * Set gv to gv.
+     *
+     * @param gv The gv to set.
+     */
+    void setValidation(GeoValidator gv) {
+        this.gv = gv;
+    }
+
+    /**
+     * Access serviceLevel property.
+     *
+     * @return Returns the serviceLevel.
+     */
+    public int getServiceLevel() {
+        return serviceLevel;
+    }
 }
