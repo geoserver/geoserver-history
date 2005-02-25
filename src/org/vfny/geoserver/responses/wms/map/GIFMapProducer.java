e57e040ee7de4135f32cedebaa001e75e802c3e9
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-02-25 20:37:36 +0000

    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@3132 ef1d6e69-97e5-0310-af46-8a06194da32a
    Former-commit-id: e712a835de5804411db36b6bed51769b01dadbb8 [formerly aa84ebaefab8abf3a08e715b141cb8c305287fa5]
    Former-commit-id: fe89df9f4a7c60fe4f258f8b759ef30161476dbd

---
+++ src/org/vfny/geoserver/responses/wms/map/GIFMapProducer.java
@@ -0,0 +1,61 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.responses.wms.map;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+import org.vfny.geoserver.WmsException;
+import org.vfny.geoserver.responses.wms.WMSMapContext;
+import org.vfny.geoserver.responses.wms.map.gif.GIFOutputStream;
+
+
+/**
+ * Handles a GetMap request that spects a map in GIF format.
+ *
+ * @author Didier Richard
+ * @version $Id
+ */
+public class GIFMapProducer extends DefaultRasterMapProducer {
+    /** DOCUMENT ME! */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.vfny.geoserver.responses.wms.map");
+    /**
+     * Transforms the rendered image into the appropriate format, streaming to
+     * the output stream.
+     *
+     * @param format The name of the format
+     * @param image The image to be formatted.
+     * @param outStream The stream to write to.
+     *
+     * @throws WmsException
+     * @throws IOException DOCUMENT ME!
+     */
+    protected void formatImageOutputStream(String format,BufferedImage image,OutputStream outStream)
+                 throws WmsException, IOException {
+        LOGGER.fine("image/gif");
+        WMSMapContext mapCtx = getMapContext();
+        if (mapCtx.isTransparent()) {
+            try {
+                GIFOutputStream.writeGIF(outStream,
+                                         image,
+                                         GIFOutputStream.STANDARD_256_COLORS,
+                                         Color.BLACK);
+            } catch (Exception e) {
+                LOGGER.warning(e.toString());
+            }
+        } else {
+            try {
+                GIFOutputStream.writeGIF(outStream, image);
+            } catch (Exception e) {
+                LOGGER.warning(e.toString());
+            }
+        }
+    }
+
+}
