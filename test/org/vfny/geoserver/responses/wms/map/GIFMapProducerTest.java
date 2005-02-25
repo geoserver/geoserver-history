387ae8062df364b240fb8ab64ba7f85c3436533e
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-02-25 20:39:09 +0000

    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@3133 ef1d6e69-97e5-0310-af46-8a06194da32a
    Former-commit-id: f21431659a7e5704c45e4e0708e4820aa4f61616 [formerly a891cb9bfe5d7d20d0893dd264de9f8768e900e7]
    Former-commit-id: b4e17fa7d3e3566c7de7d9ddfa0c5f4300c7129e

---
+++ test/org/vfny/geoserver/responses/wms/map/GIFMapProducerTest.java
@@ -0,0 +1,68 @@
+package org.vfny.geoserver.responses.wms.map;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
+
+
+
+/**
+ * Loads a sample image and checks that GIFMapProducer correctly encodes it.
+ *
+ * @author Gabriel Roldan, Axios Engineering
+ * @version $Id$
+ */
+public class GIFMapProducerTest extends DefaultRasterMapProducerTest {
+    /**
+     *
+     */
+    public GIFMapProducerTest() {
+        super();
+    }
+    
+    protected DefaultRasterMapProducer getProducerInstance(){
+    	return new GIFMapProducer();
+    }
+    
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param testName DOCUMENT ME!
+     * @param producer DOCUMENT ME!
+     */
+    protected void assertNotBlank(String testName,
+        DefaultRasterMapProducer producer){
+    	
+        BufferedImage image = producer.getImage();
+        
+        BufferedImage product = null;
+        File tmpGif = null;
+        try {
+			tmpGif = new File("/tmp/" + testName + ".gif");
+			//tmpGif.deleteOnExit();
+			OutputStream out = new FileOutputStream(tmpGif);
+			producer.formatImageOutputStream("image/gif", image, out);
+			out.flush();
+			out.close();
+			
+			product = ImageIO.read(tmpGif);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+				
+		int w = image.getWidth();
+        int h = image.getHeight();
+        
+        assertEquals(w, product.getWidth());
+        assertEquals(h, product.getHeight());
+        
+        System.out.println("width=" + w + ", height=" + h);
+
+        showImage(testName, product);
+    }
+}
