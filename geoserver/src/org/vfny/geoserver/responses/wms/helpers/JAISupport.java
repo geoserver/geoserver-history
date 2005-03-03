e3827dd7c1338a86495a9bbe18a146b6faa99ab0
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-03-03 19:43:29 +0000

    JAI availability checking and supported formats lookup refactored to this helper class so it can be used from the various jai dependant stuff
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@3149 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 760ba907e3ef371ee90d5cad803b3f2671817e0d [formerly 945e3fb5e452d6ecf4071c70c9f8faf2a1812050]
    Former-commit-id: a2ce938fa1199787a03a38a7b52d34bf321d7449

---
+++ geoserver/src/org/vfny/geoserver/responses/wms/helpers/JAISupport.java
@@ -0,0 +1,104 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.responses.wms.helpers;
+
+import org.geotools.renderer.lite.LiteRenderer2;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+
+/**
+ * Helper class to deal with JAI availability and image encoding
+ */
+public final class JAISupport {
+    /** shared package's logger */
+    private static final Logger LOGGER = Logger.getLogger(JAISupport.class.getPackage()
+                                                                          .getName());
+
+    /**
+     * Set&lt;String&gt; of the MIME types the available JAI library supports,
+     * or the empty set if it is not available.
+     */
+    private static Set supportedFormats;
+
+    /**
+     * Returns the set of supported formats by the available JAI library, or
+     * the empty set if not available.
+     *
+     * @return Set&lt;String&gt; of the MIME types the available JAI library
+     *         supports, or the empty set if it is not available.
+     */
+    public static Set getSupportedFormats() {
+        if (supportedFormats == null) {
+            //LiteRenderer renderer = null;
+            String[] mimeTypes = null;
+
+            LiteRenderer2 testRenderer = null;
+
+            try {
+                testRenderer = new LiteRenderer2();
+                mimeTypes = ImageIO.getWriterMIMETypes();
+            } catch (NoClassDefFoundError ncdfe) {
+                supportedFormats = Collections.EMPTY_SET;
+                LOGGER.warning("could not find jai: " + ncdfe);
+
+                //this will occur if JAI is not present, so please do not
+                //delete, or we get really nasty messages on getCaps for wms.
+            }
+
+            if ((testRenderer == null) || (mimeTypes == null)) {
+                LOGGER.info("renderer was null, so jai not found");
+                supportedFormats = Collections.EMPTY_SET;
+            } else {
+                supportedFormats = new HashSet();
+
+                List formatsList = Arrays.asList(mimeTypes);
+
+                for (Iterator it = formatsList.iterator(); it.hasNext();) {
+                    String curFormat = it.next().toString();
+
+                    if (!curFormat.equals("")) {
+                        supportedFormats.add(curFormat);
+                    }
+                }
+
+                if (LOGGER.isLoggable(Level.CONFIG)) {
+                    StringBuffer sb = new StringBuffer(
+                            "Supported JAIMapResponse's MIME Types: [");
+
+                    for (Iterator it = supportedFormats.iterator();
+                            it.hasNext();) {
+                        sb.append(it.next());
+
+                        if (it.hasNext()) {
+                            sb.append(", ");
+                        }
+                    }
+
+                    sb.append("]");
+                    LOGGER.config(sb.toString());
+                }
+            }
+        }
+
+        return supportedFormats;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return <code>true</code> if JAI is available
+     */
+    public static boolean isJaiAvailable() {
+        return getSupportedFormats().size() > 0;
+    }
+}
