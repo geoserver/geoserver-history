88f9e939ad275893695bae4f31994003f34f1ac2
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-10-13 17:37:02 +0000

    GEOS-399, better postgis errors
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@3821 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: aa4f851adb134bad99c3e838e9cb1413516560b6 [formerly 1e08437aa42d25d1cd730208be0cf4d17e4c4499]
    Former-commit-id: 02b6cd7fcd6f65862d6660711f6b7d2eab384db7

--- geoserver/src/org/vfny/geoserver/action/data/DataFeatureTypesNewAction.java
+++ geoserver/src/org/vfny/geoserver/action/data/DataFeatureTypesNewAction.java
@@ -12,6 +12,8 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -77,59 +79,75 @@ public class DataFeatureTypesNewAction extends ConfigAction {
         DataStoreConfig dsConfig = dataConfig.getDataStore(dataStoreID);
         DataStore dataStore = dsConfig.findDataStore(request.getSession().getServletContext());
 
-        FeatureType featureType = dataStore.getSchema(featureTypeName);
+        FeatureTypeConfig ftConfig;
+        //JD: GEOS-399, wrap rest of method in try catch block in order to 
+        // report back nicely to app
+		try {
+			FeatureType featureType = dataStore.getSchema(featureTypeName);
 
-        FeatureTypeConfig ftConfig = new FeatureTypeConfig(dataStoreID,
-                featureType, false);
+			ftConfig = new FeatureTypeConfig(dataStoreID,
+			        featureType, false);
 
-        // DJB: this comment looks old - SRS support is much better now.  
-        //     TODO: delete this comment (but wait a bit)
-        // What is the Spatial Reference System for this FeatureType?
-        // 
-        // getDefaultGeometry().getCoordinateSystem() should help but is null
-        // getDefaultGeometry().getGeometryFactory() could help, with getSRID(), but it is null
-        // 
-        // So we will use 0 which means Cartisian Coordinates aka don't know
-        //
-        // Only other thing we could do is ask for a geometry and see what it's
-        // SRID number is?
-        //
-        
-        ftConfig.setSRS(0);
-        
-        // attempt to get a better SRS
-        try {
-        	CoordinateReferenceSystem crs = featureType.getDefaultGeometry().getCoordinateSystem();
-        	Set idents = crs.getIdentifiers();
-        	Iterator it = idents.iterator();
-        	while (it.hasNext())
-        	{
-        		Identifier id = (Identifier) it.next();
-        		if (id.toString().indexOf("EPSG:") != -1)    // this should probably use the Citation, but this is easier!
-        		{
-        			//we have an EPSG #, so lets use it!
-        			String str_num = id.toString().substring(id.toString().indexOf(':')+1);
-        			int num = Integer.parseInt(str_num);
-        			ftConfig.setSRS(num);
-        			break;  // take the first EPSG
-        		}
-        	}
-        }catch(Exception e)
-		{
-        	e.printStackTrace(); // not a big deal - we'll default to 0.
+			// DJB: this comment looks old - SRS support is much better now.  
+			//     TODO: delete this comment (but wait a bit)
+			// What is the Spatial Reference System for this FeatureType?
+			// 
+			// getDefaultGeometry().getCoordinateSystem() should help but is null
+			// getDefaultGeometry().getGeometryFactory() could help, with getSRID(), but it is null
+			// 
+			// So we will use 0 which means Cartisian Coordinates aka don't know
+			//
+			// Only other thing we could do is ask for a geometry and see what it's
+			// SRID number is?
+			//
+			
+			ftConfig.setSRS(0);
+			
+			// attempt to get a better SRS
+			try {
+				CoordinateReferenceSystem crs = featureType.getDefaultGeometry().getCoordinateSystem();
+				Set idents = crs.getIdentifiers();
+				Iterator it = idents.iterator();
+				while (it.hasNext())
+				{
+					Identifier id = (Identifier) it.next();
+					if (id.toString().indexOf("EPSG:") != -1)    // this should probably use the Citation, but this is easier!
+					{
+						//we have an EPSG #, so lets use it!
+						String str_num = id.toString().substring(id.toString().indexOf(':')+1);
+						int num = Integer.parseInt(str_num);
+						ftConfig.setSRS(num);
+						break;  // take the first EPSG
+					}
+				}
+			}catch(Exception e)
+			{
+				e.printStackTrace(); // not a big deal - we'll default to 0.
+			}
+			
+			
+			FeatureSource fs = dataStore.getFeatureSource(featureType.getTypeName());
+			        
+			// TODO translate to lat long, pending
+			//This should not be done by default, as it is an expensive operation.
+			//especially for very large tables.  User may know it, if not he
+			//can hit the generate button (which is why it's there).
+			//ftConfig.setLatLongBBox(DataStoreUtils.getBoundingBoxEnvelope(fs));
+			
+			//Extent ex = featureType.getDefaultGeometry().getCoordinateSystem().getValidArea();
+			//ftConfig.setLatLongBBox(ex);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+			
+			ActionErrors errors = new ActionErrors();
+            errors.add(ActionErrors.GLOBAL_ERROR,
+                new ActionError("error.exception", e.getMessage()));
+
+            saveErrors(request, errors);
+
+            return mapping.findForward("config.data.type.new");
 		}
-        
-        
-        FeatureSource fs = dataStore.getFeatureSource(featureType.getTypeName());
-                
-        // TODO translate to lat long, pending
-        //This should not be done by default, as it is an expensive operation.
-        //especially for very large tables.  User may know it, if not he
-        //can hit the generate button (which is why it's there).
-        //ftConfig.setLatLongBBox(DataStoreUtils.getBoundingBoxEnvelope(fs));
-        
-        //Extent ex = featureType.getDefaultGeometry().getCoordinateSystem().getValidArea();
-        //ftConfig.setLatLongBBox(ex);
 
         request.getSession().setAttribute(DataConfig.SELECTED_FEATURE_TYPE, ftConfig);
         request.getSession().removeAttribute(DataConfig.SELECTED_ATTRIBUTE_TYPE);
@@ -137,4 +155,6 @@ public class DataFeatureTypesNewAction extends ConfigAction {
         user.setFeatureTypeConfig( ftConfig );
         return mapping.findForward("config.data.type.editor");
     }
+    
+    
 }
