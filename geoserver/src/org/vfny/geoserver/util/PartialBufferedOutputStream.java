33d4a8347b34a47c52a83a922aba426f8f74f648
bowens <bowens@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-10-24 20:31:55 +0000

    changed flush to only flush if buffer is full added forceFlush
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@3865 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 23f540b56b4055752efcbbbe765f4c3179399c18 [formerly 395804a9136a38a916d251b4d7856b4b6e723ac5]
    Former-commit-id: 1f7561f82c734654caa1313953c9802140eea028

--- geoserver/src/org/vfny/geoserver/util/PartialBufferedOutputStream.java
+++ geoserver/src/org/vfny/geoserver/util/PartialBufferedOutputStream.java
@@ -42,8 +42,7 @@ import javax.servlet.http.HttpServletResponse;
  * -close() will NOT flush remaining bytes to the output stream, as per the contract 
  * with OutputStream. Close will remove any references to OutputStreams and its 
  * HttpServletResponse.
- * -flush() will flush the buffer first and then any information stored in the underlying 
- * OutputStream. Everything will be flushed when flush is called.
+ * -fush() will NOT flush if the buffer is not full.
  * -abort() will succeed if the buffer is not full yet. If the buffer has been filled and
  * the information has been written out to response's OutputStream, the abort reports as
  * having failed. The point of this PartialBufferedOutputStream is to allow an abort before 
@@ -166,10 +165,31 @@ public class PartialBufferedOutputStream extends OutputStream
 	/* (non-Javadoc)
 	 * @see java.io.OutputStream#flush()
 	 * 
-	 * Flush when the user says to. Even if the buffer is not maxed out yet.
-	 * We need to revisit this.
+	 * If the buffer is not maxed yet, don't flush. If a flush is really really needed,
+	 * call forceFlush().
 	 */
 	public synchronized void flush() throws IOException 
+	{
+		if (closed)
+			return;
+		
+		if (sendToUser)
+		{
+			if (out_real != null)
+				out_real.flush();
+		}
+		
+	}
+	
+	/**
+	 * <b>forceFlush</b><br>
+	 * <br>
+	 * <b>Description:</b><br>
+	 * Flush when the user says to. Even if the buffer is not maxed out yet.
+	 * We need to revisit this.
+	 * @throws IOException
+	 */
+	public synchronized void forceFlush() throws IOException 
 	{
 		if (closed)
 			return;
@@ -185,7 +205,6 @@ public class PartialBufferedOutputStream extends OutputStream
 		if (out_real != null)
 			out_real.flush();
 	}
-	
 	
 	/* (non-Javadoc)
 	 * @see java.io.OutputStream#close()
