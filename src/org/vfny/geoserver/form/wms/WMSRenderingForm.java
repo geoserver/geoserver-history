665a83cde0497d230d809ad1db86e0d94c692d04
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-10-25 18:09:25 +0000

    GEOS-428
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@3867 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e5b40b1ddd121e7aef522d79e998594d85c3ab46 [formerly 9ebb0ea6a69cdaccfbf6baecf78c0c3424857be1]
    Former-commit-id: 0654bbece697b4cabefc185f81c677431aa0428f

--- src/org/vfny/geoserver/form/wms/WMSRenderingForm.java
+++ src/org/vfny/geoserver/form/wms/WMSRenderingForm.java
@@ -17,6 +17,29 @@ public class WMSRenderingForm extends ActionForm {
 	String svgRenderer;
 	boolean svgAntiAlias;
 	
+	/*
+     * Because of the way that STRUTS works, if the user does not check the enabled box,
+     * or unchecks it, setEnabled() is never called, thus we must monitor setEnabled()
+     * to see if it doesn't get called. This must be accessible, as ActionForms need to
+     * know about it -- there is no way we can tell whether we are about to be passed to
+     * an ActionForm or not.
+     *
+     * Probably a better way to do this, but I can't think of one.
+     * -rgould
+     *
+     * TODO: Hey richard Jody here - Struts knows that boolean properties are
+     * not set if the user does nothing. Apparently that is why the reset
+     * method exists.
+     * Reset is called *every* time on ActionForm. Before the populate
+     * process has a go at things.
+     *
+     * The problem is that reset() retrieves the WFS's config enabled value
+     * and uses that to pre-populate the form. Thus, if they deselect it, setEnabled is
+     * never called, and enabled still remains true. The way I have done it isn't simple,
+     * but it works just fine.
+     */
+    private boolean svgAntiAliasChecked = false;
+    
 	public WMSRenderingForm() {
 		svgRenderers = new ArrayList();
 		svgRenderers.add(WMSConfig.SVG_SIMPLE);
@@ -61,6 +84,7 @@ public class WMSRenderingForm extends ActionForm {
      * @param svgAntiAlias anti alias hint.
      */
     public void setSvgAntiAlias(boolean svgAntiAlias) {
+    	svgAntiAliasChecked = true;
     	this.svgAntiAlias = svgAntiAlias;
     }
     
@@ -69,5 +93,23 @@ public class WMSRenderingForm extends ActionForm {
      */
     public boolean getSvgAntiAlias() {
     	return svgAntiAlias;
+    }
+    
+    /**
+     * DOCUMENT ME!
+     *
+     * @return
+     */
+    public boolean isSvgAntiAliasChecked() {
+        return svgAntiAliasChecked;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param b
+     */
+    public void setEnabledChecked(boolean svgAntiAliasChecked) {
+    	this.svgAntiAliasChecked = svgAntiAliasChecked;
     }
 }
