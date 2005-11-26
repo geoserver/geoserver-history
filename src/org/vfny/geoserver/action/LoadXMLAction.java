66304e2a9135c5b56e9a39b8e9dac8747a1921a9
cholmes <cholmes@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-11-26 11:24:08 +0000

    fixes for GEOS-487.  This should be pretty backwards compatible even with the old data_dir stuff.  It will read the old in, but will write out in the new format.  Also fixed up validation to use data_dir, since it seemed to neglect it, and a few fixes for style stuff as well
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@3971 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fa32b7dd25e7384585973733941f5b4f5d89abe4 [formerly d7b8e5da8cd91e2a9608d17501be3e887295c9a1]
    Former-commit-id: ca2eba1026e841135b2c2e0a43ae9fdc11950b46

--- src/org/vfny/geoserver/action/LoadXMLAction.java
+++ src/org/vfny/geoserver/action/LoadXMLAction.java
@@ -52,6 +52,11 @@ import org.vfny.geoserver.global.xml.XMLConfigReader;
  * <p>
  * Q: Does this need to load the Validation Processor as well?
  * </p>
+ * @REVISIT: There seems to be quite a bit of code duplication in this class
+ *           with GeoServerPlugIn, loading things, especially with the
+ *           validation stuff.  Anyway we could cut that down?  Have one call
+ *           the other?  Too close to release to do sucha refactoring but
+ *           in 1.4 we should. -CH
  */
 public class LoadXMLAction extends ConfigAction {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
@@ -152,11 +157,12 @@ public class LoadXMLAction extends ConfigAction {
             loadGeoserver(mapping, form, request, response);
         }
 
-        File rootDir = new File(sc.getRealPath("/"));
-        File plugInDir = new File(rootDir, "data/plugIns");
-        File validationDir = new File(rootDir, "data/validation");
+        //CH- fixed for data dir, looks like this got missed first time around.
+        File rootDir = GeoserverDataDirectory.getGeoserverDataDirectory(sc);
 
         try {
+            File plugInDir = findConfigDir(rootDir, "plugIns");
+            File validationDir = findConfigDir(rootDir, "validation");
             Map plugIns = XMLReader.loadPlugIns(plugInDir);
             Map testSuites = XMLReader.loadValidations(validationDir, plugIns);
             ValidationConfig vc = new ValidationConfig(plugIns, testSuites);
@@ -169,5 +175,9 @@ public class LoadXMLAction extends ConfigAction {
         }
 
         return mapping.findForward("config.validation");
+    }
+
+    private File findConfigDir(File rootDir, String name) throws Exception {
+	return GeoserverDataDirectory.findConfigDir(rootDir, name);
     }
 }
