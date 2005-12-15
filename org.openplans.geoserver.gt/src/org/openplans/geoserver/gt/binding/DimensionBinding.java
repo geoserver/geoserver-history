6db01bcf80f17d6713a595e82709020d15c2c80a
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2005-12-15 19:32:06 +0000

    git-svn-id: http://svn.codehaus.org/geoserver/spike/geoserver-2.0@4029 ef1d6e69-97e5-0310-af46-8a06194da32a
    Former-commit-id: bfe238c7db34231b0d7557dd18abbfc936000a81 [formerly df39eb74350d83f190fc14846739466b398ad64c]
    Former-commit-id: 36d98e4e7a5b5beb283c5daa644c1a68961574d5

--- org.openplans.geoserver.gt/src/org/openplans/geoserver/gt/binding/DimensionBinding.java
+++ org.openplans.geoserver.gt/src/org/openplans/geoserver/gt/binding/DimensionBinding.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.openplans.geoserver.binding.KvpBinding;
-import org.openplans.geoserver.binding.KvpRequestReader;
 
 public class DimensionBinding extends KvpBinding {
 
