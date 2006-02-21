e0d0f93a0f8d98315a5568d3c717ab2c1406d1e1
bowens <bowens@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-02-21 03:44:58 +0000

    updated to new mapbuilder version
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@4187 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d811fb52e1be524229fbabf763c4a9b0b04f0845 [formerly 37eba5ce8731ada19189a8b4bbdeed0a301e9dc2]
    Former-commit-id: cf15d5e834dead6d66b4fa5729bd14fa652c46e2

--- cite/confUserBasic/data/mbdemos/lib/widget/ButtonBase.js
+++ cite/confUserBasic/data/mbdemos/lib/widget/ButtonBase.js
@@ -1,122 +1,78 @@
-/*
-License: GPL as per: http://www.gnu.org/copyleft/gpl.html
-$Id: ButtonBase.js,v 1.8 2005/02/21 05:10:56 madair1 Exp $
-*/
-
-// Ensure this object's dependancies are loaded.
-mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
-
-/**
- * Base Button object that all Buttons extend.  A Button is a tool represented
- * by an image and an optional second image for the enabled state.
- * @constructor
- * @base WidgetBase
- * @author Mike Adair mike.adairATccrs.nrcan.gc.ca
- * @param button Pointer to the button instance being created.
- * @param widgetNode The tool node from the Config XML file.
- * @param model The parent model object (optional).
- */
-function ButtonBase(button, widgetNode, model) {
-  button.stylesheet = new XslProcessor(baseDir+"/widget/Button.xsl");
-  var buttonBarNode = widgetNode.selectSingleNode("mb:buttonBar");
-  if ( buttonBarNode ) {
-    button.htmlTagId = buttonBarNode.firstChild.nodeValue;
-  } else {
-    alert("buttonBar property required for object:" + widgetNode.nodeName );
-  }
-
-  var base = new WidgetBase(button, widgetNode, model);
-
-  //set the button type
-  button.buttonType = widgetNode.selectSingleNode("mb:class").firstChild.nodeValue;
-  if (button.buttonType == "RadioButton") button.enabled = false;
-
-  //pre-load the button bar images; add them to the config
-  button.disabledImage = document.createElement("IMG");
-  button.disabledImage.src = config.skinDir + widgetNode.selectSingleNode("mb:disabledSrc").firstChild.nodeValue;
-
-  var enabledImage = widgetNode.selectSingleNode("mb:enabledSrc");
-  if (enabledImage) {
-    button.enabledImage = document.createElement("IMG");
-    button.enabledImage.src = config.skinDir + enabledImage.firstChild.nodeValue;
-  }
-
-  this.prePaint = function(objRef) {
-    objRef.resultDoc = objRef.widgetNode;
-  }
-
-  /**
-   * Override this in buttons which inherit from this object to carry out the action.
-   * This is the function that will be called either when the button is selected
-   * via the select() method or on a mouseup event if there is an associated 
-   * mouseHandler property in config.
-   */
-  this.doAction = function() {}
-
-  /**
-   * Called when a user clicks on a button.  Switches the image to the enabled 
-   * button source, enables and disables associated tools, then calls the 
-   * doAction method defined in derived classes.
-   */
-  this.select = function() {
-    if (this.buttonType == "RadioButton") {
-      if (this.node.selectedRadioButton) {
-        with (this.node.selectedRadioButton) {
-          image.src = disabledImage.src;
-          enabled = false;
-          if ( mouseHandler ) mouseHandler.enabled = false;
-          doSelect(false,this);
-        }
-      }
-      this.node.selectedRadioButton = this;
-      this.image.src = this.enabledImage.src;
-    }
-
-    //enable this tool and any dependancies
-    this.enabled = true;
-    if ( this.mouseHandler ) this.mouseHandler.enabled = true;
-    this.doSelect(true,this);
-  }
-
-  /**
-   * Override this function in Buttons to process select/deselect calls.
-   * @param selected True when selected, false when deselected.
-   * @objRef Reference to this object.
-   */
-  this.doSelect = function(selected, objRef) {
-  }
-
-  var selected = widgetNode.selectSingleNode("mb:selected");
-  if (selected && selected.firstChild.nodeValue) button.selected = true;
-
-  this.initMouseHandler = function(objRef) {
-    /** Mouse handler which this tool will register listeners with. */
-    var mouseHandler = objRef.widgetNode.selectSingleNode("mb:mouseHandler");
-    if (mouseHandler) {
-      objRef.mouseHandler = eval("config.objects." + mouseHandler.firstChild.nodeValue);
-      if (!objRef.mouseHandler) {
-        alert("error finding mouseHandler:"+mouseHandler.firstChild.nodeValue+" for tool:"+objRef.id);
-      }
-    } else {
-      objRef.mouseHandler = null;
-    }
-  }
-
-  /**
-   * Initialise buttonBase.
-   * @param objRef Reference to this object.
-   */
-  this.buttonInit = function(objRef) {
-    objRef.image = document.getElementById( objRef.id );
-    if (objRef.selected) objRef.select();
-  }
-
-  // If this object is being created because a child is extending this object,
-  // then child.properties = this.properties
-  for (sProperty in this) {
-    button[sProperty] = this[sProperty];
-  }
-
-  button.model.addListener("refresh",button.buttonInit,button);
-  button.model.addListener("init", button.initMouseHandler, button);
+mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
+function ButtonBase(widgetNode,model){
+this.stylesheet=new XslProcessor(baseDir+"/widget/Button.xsl");
+var buttonBarNode=widgetNode.selectSingleNode("mb:buttonBar");
+if(buttonBarNode){
+this.htmlTagId=buttonBarNode.firstChild.nodeValue;
+}else{
+alert("buttonBar property required for object:"+widgetNode.nodeName);
+}
+WidgetBaseXSL.apply(this,new Array(widgetNode,model));
+this.buttonType=widgetNode.selectSingleNode("mb:class").firstChild.nodeValue;
+if(this.buttonType=="RadioButton")this.enabled=false;
+var disabledImage=widgetNode.selectSingleNode("mb:disabledSrc");
+if(disabledImage){
+this.disabledImage=document.createElement("IMG");
+this.disabledImage.src=config.skinDir+disabledImage.firstChild.nodeValue;
+}
+var enabledImage=widgetNode.selectSingleNode("mb:enabledSrc");
+if(enabledImage){
+this.enabledImage=document.createElement("IMG");
+this.enabledImage.src=config.skinDir+enabledImage.firstChild.nodeValue;
+}
+var cursorNode=this.widgetNode.selectSingleNode("mb:cursor");
+if(cursorNode!=null){
+var cursor=cursorNode.firstChild.nodeValue;
+this.cursor=cursor;
+}else{
+this.cursor="default";}
+this.prePaint=function(objRef){
+objRef.resultDoc=objRef.widgetNode;
+}
+this.doAction=function(){}
+this.select=function(){
+var a=document.getElementById("mainMapContainer");
+if(a!=null){
+a.style.cursor=this.cursor;
+} 
+if(this.buttonType=="RadioButton"){
+if(this.node.selectedRadioButton){
+with(this.node.selectedRadioButton){
+if(disabledImage)image.src=disabledImage.src;
+enabled=false;
+if(mouseHandler)mouseHandler.enabled=false;
+link.className="mbButton";
+doSelect(false,this);
+}
+}
+this.node.selectedRadioButton=this;
+if(this.enabledImage)this.image.src=this.enabledImage.src;
+this.link.className="mbButtonSelected";
+}
+this.enabled=true;
+if(this.mouseHandler)this.mouseHandler.enabled=true;
+this.doSelect(true,this);
+}
+this.doSelect=function(selected,objRef){
+}
+var selected=widgetNode.selectSingleNode("mb:selected");
+if(selected&&selected.firstChild.nodeValue)this.selected=true;
+this.initMouseHandler=function(objRef){
+var mouseHandler=objRef.widgetNode.selectSingleNode("mb:mouseHandler");
+if(mouseHandler){
+objRef.mouseHandler=eval("config.objects."+mouseHandler.firstChild.nodeValue);
+if(!objRef.mouseHandler){
+alert("error finding mouseHandler:"+mouseHandler.firstChild.nodeValue+" for button:"+objRef.id);
+}
+}else{
+objRef.mouseHandler=null;
+}
+}
+this.buttonInit=function(objRef){
+objRef.image=document.getElementById(objRef.id+"_image");
+objRef.link=document.getElementById(objRef.outputNodeId);
+if(objRef.selected)objRef.select();
+}
+this.model.addListener("refresh",this.buttonInit,this);
+this.model.addListener("init",this.initMouseHandler,this);
 }
--- cite/confUserBasic/data/mbdemos/lib/widget/WebServiceForm.js
+++ cite/confUserBasic/data/mbdemos/lib/widget/WebServiceForm.js
@@ -1,54 +1,45 @@
-/*
-Author:       Mike Adair mike.adairATccrs.nrcan.gc.ca
-License:      GPL as per: http://www.gnu.org/copyleft/gpl.html
-
-$Id: WebServiceForm.js,v 1.5 2005/01/04 05:17:22 madair1 Exp $
-*/
-
-// Ensure this object's dependancies are loaded.
-mapbuilder.loadScript(baseDir+"/widget/WidgetBase.js");
-
-/**
- * Widget to display the AOI box coordinates
- *
- * @constructor
- * @base WidgetBase
- * @param widgetNode This widget's object node from the configuration document.
- * @param model The model that this widget is a view of.
- */
-
-function WebServiceForm(widgetNode, model) {
-  var base = new WidgetBase(this, widgetNode, model);
-
-  /**
-   * Handles submission of the form (via javascript in an <a> tag)
-   */
-  this.submitForm = function() {
-
-    //create the http GET URL
-    //TBD: handle POST submission
-    //TBD: create filter request
-    var webServiceUrl = this.webServiceForm.action + "?";
-    for (var i=0; i<this.webServiceForm.elements.length; ++i) {
-      var element = this.webServiceForm.elements[i];
-      webServiceUrl += element.name + "=" + element.value + "&";
-    }
-    if (this.debug) alert(webServiceUrl);
-    config.loadModel( this.targetModel.id, webServiceUrl);
-  }
-
-  /**
-   * Refreshes the form onblur handlers when this widget is painted.
-   * @param objRef Pointer to this CurorTrack object.
-   */
-  this.postPaint = function(objRef) {
-    objRef.webServiceForm = document.getElementById(objRef.formName);
-    //objRef.WebServiceForm.onsubmit = objRef.submitForm;
-    //objRef.WebServiceForm.mapsheet.onblur = objRef.setMapsheet;
-  }
-
-  //set some properties for the form output
-  this.formName = "WebServiceForm_";// + mbIds.getId();
-  this.stylesheet.setParameter("formName", this.formName);
+mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
+function WebServiceForm(widgetNode,model){
+WidgetBaseXSL.apply(this,new Array(widgetNode,model));
+this.formElements=new Object();
+this.submitForm=function(){
+this.webServiceForm=document.getElementById(this.formName);
+var webServiceUrl=this.webServiceForm.action+"?";
+for(var i=0;i<this.webServiceForm.elements.length;++i){
+var element=this.webServiceForm.elements[i];
+webServiceUrl+=element.name+"="+element.value+"&";
+this.formElements[element.name]=element.value;
 }
-
+if(this.debug)alert(webServiceUrl);
+var httpPayload=new Object();
+httpPayload.method="get";
+httpPayload.url=webServiceUrl;
+this.targetModel.newRequest(this.targetModel,httpPayload);
+}
+this.handleKeyPress=function(event){
+var keycode;
+var target;
+if(event){
+keycode=event.which;
+target=event.currentTarget;
+}else{
+keycode=window.event.keyCode;
+target=window.event.srcElement.form;
+}
+if(keycode==13){target.parentWidget.submitForm();
+return false;
+}
+}
+this.postPaint=function(objRef){
+objRef.webServiceForm=document.getElementById(objRef.formName);
+objRef.webServiceForm.parentWidget=objRef;
+objRef.webServiceForm.onkeypress=objRef.handleKeyPress;
+}
+this.formName="WebServiceForm_"+mbIds.getId();
+this.stylesheet.setParameter("formName",this.formName);
+this.prePaint=function(objRef){
+for(var elementName in objRef.formElements){
+objRef.stylesheet.setParameter(elementName,objRef.formElements[elementName]);
+}
+}
+}
