ad35133cdc63d971cf61b67359d74312524d6312
afabiani <afabiani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-02-24 16:18:44 +0000

    Removed file/folder
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wcs-branch@4227 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: de8c6e704954598a500d870cba89f7f400fdb184 [formerly 22d950d240d3fd96b79298bb3eb5d21c236e0b8c]
    Former-commit-id: 81b4d2f7a8fa41fe8ae3fdbfe2218ce61a2f0cc7

--- trunk/geoserver/cite/confUserBasic/data/mbdemos/lib/widget/FeatureInfo.js
+++
@@ -1,7 +0,0 @@
-mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
-function FeatureInfo(widgetNode,model){
-WidgetBaseXSL.apply(this,new Array(widgetNode,model));
-this.setAttr=function(objRef,xpath,value){
-objRef.model.setXpathValue(objRef.model,xpath,value);
-}
-}
