be8103be421eeec8a8c1774b2d0cd3adaeacc54f
simboss <simboss@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-03-17 16:25:37 +0000

    -adding test for png map producer
    git-svn-id: http://svn.codehaus.org/geoserver/branches/wcs-branch@4318 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cf56fa4a194a9c2a97250fb911302c0634c9a5d5 [formerly d9969c1c8d2794afeb8149ad618374aaf26207ed]
    Former-commit-id: b1f2f962e657a359e0a803ab88128ecdb8201750

---
+++ trunk/geoserver/test/org/vfny/geoserver/wms/responses/map/PNGMapProducerTest.java
@@ -0,0 +1,57 @@
+package org.vfny.geoserver.wms.responses.map;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
+
+import org.vfny.geoserver.wms.responses.DefaultRasterMapProducer;
+import org.vfny.geoserver.wms.responses.DefaultRasterMapProducerTest;
+
+public class PNGMapProducerTest extends DefaultRasterMapProducerTest {
+
+	public PNGMapProducerTest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected DefaultRasterMapProducer getProducerInstance() {
+		return new GIFMapProducer("image/png"); // DJB: set content enconding
+												// correctly
+	}
+
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param testName
+	 *            DOCUMENT ME!
+	 * @param producer
+	 *            DOCUMENT ME!
+	 */
+	protected void assertNotBlank(String testName,
+			DefaultRasterMapProducer producer) {
+
+	
+		
+		try {
+			final File tmpPNG = File.createTempFile("temporaryPNGFIles",".png");
+			
+			// tmpGif.deleteOnExit();
+			OutputStream out = new FileOutputStream(tmpPNG);
+			producer.writeTo(out);
+			final long iniT=System.currentTimeMillis();
+			out.flush();
+			out.close();
+			System.out.println("******** "+(System.currentTimeMillis()-iniT)+"  ********");
+			showImage(tmpPNG.getAbsolutePath(),ImageIO.read(tmpPNG));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	
+	}
+}
