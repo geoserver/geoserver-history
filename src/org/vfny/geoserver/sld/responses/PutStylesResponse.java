c9b04631f8853c1663ef180d432be3ea69a304e2
bowens <bowens@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-21 16:32:11 +0000

    sld wizard
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1_3_0a_geotools_2_2_x@4577 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8e564679df0c4e54d64cf212198a2adc09099869 [formerly 37eaef53cb9a43c074b479e3b1df13a0d7194828]
    Former-commit-id: fce1d638fa993479a13d332bb495514402317519

---
+++ src/org/vfny/geoserver/sld/responses/PutStylesResponse.java
@@ -0,0 +1,44 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+
+package org.vfny.geoserver.sld.responses;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.vfny.geoserver.Request;
+import org.vfny.geoserver.Response;
+import org.vfny.geoserver.ServiceException;
+import org.vfny.geoserver.global.GeoServer;
+import org.vfny.geoserver.global.Service;
+
+public class PutStylesResponse implements Response{
+
+	public void execute(Request request) throws ServiceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getContentType(GeoServer gs) throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getContentEncoding() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void writeTo(OutputStream out) throws ServiceException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void abort(Service gs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
