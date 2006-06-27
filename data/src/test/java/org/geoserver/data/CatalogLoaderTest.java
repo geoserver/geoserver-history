e3c2d17e697883692e9d7fd1d480aaaf0ca43ec6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-27 19:03:14 +0000

    changed from shapefile for testing to property datastore
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4627 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 06d842981858506365092a5a0e63c5332e740b7c [formerly e42f052e5b82ed07e413d8e7a252ca0f7c7d8c21]
    Former-commit-id: 4e56521f37f2beb11ba0fc6ee13b15c9f8c7fce9

--- data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
+++ data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
@@ -1,15 +1,19 @@
 package org.geoserver.data;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import junit.framework.TestCase;
+
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class CatalogLoaderTest extends TestCase {
 
 
 	public void testLoadCatalog() throws Exception {
 		ClassPathXmlApplicationContext context = 
-			new ClassPathXmlApplicationContext("classpath*:**/applicationContext.xml");
+			new ClassPathXmlApplicationContext("classpath*:/applicationContext.xml");
 		
 		GeoServerCatalog catalog = (GeoServerCatalog) context.getBean("catalog");
 		assertFalse(catalog.members( null ).isEmpty());	
