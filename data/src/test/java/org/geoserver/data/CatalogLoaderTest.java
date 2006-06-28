f0ff531319da15ebfb228233da712ebdfcd8c20e
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-28 14:53:10 +0000

    changed test to run dynamically
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4634 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b5a42ab7c20655db73dcebcff400a4bc4246088b [formerly fe8ae9d36fd3fa16878b8b975f1625871ba96e4e]
    Former-commit-id: bbaf6ccf7eed18c7dcb376f4e4933f2e7ef0c5ec

--- data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
+++ data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
@@ -1,31 +1,89 @@
 package org.geoserver.data;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import junit.framework.TestCase;
 
+import org.geotools.catalog.Catalog;
 import org.geotools.catalog.ServiceFactory;
+import org.geotools.catalog.ServiceFinder;
+import org.geotools.catalog.defaults.DefaultCatalog;
+import org.geotools.catalog.defaults.DefaultServiceFinder;
 import org.geotools.catalog.property.PropertyServiceFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class CatalogLoaderTest extends TestCase {
 
 
 	public void testLoadCatalog() throws Exception {
-		ClassPathXmlApplicationContext context = 
-			new ClassPathXmlApplicationContext("classpath*:/applicationContext.xml");
+		DocumentBuilder builder = 
+			DocumentBuilderFactory.newInstance().newDocumentBuilder();
 		
-		Collection factories = context.getBeansOfType( ServiceFactory.class )
-			.values();
-		assertFalse( factories.isEmpty() );
+		Document doc = builder.newDocument();
+		doc.appendChild( doc.createElement( "catalog" ) );
 		
-		PropertyServiceFactory factory = 
-			(PropertyServiceFactory) factories.iterator().next();
-		assertTrue( factory.canProcess( new File( "/tmp" ).toURI() ) );
+		Element dataStoresElement = doc.createElement( "datastores" );
+		doc.getDocumentElement().appendChild( dataStoresElement );
 		
-		GeoServerCatalog catalog = (GeoServerCatalog) context.getBean("catalog");
-		assertFalse(catalog.members( null ).isEmpty());	
+		Element dataStoreElement = doc.createElement( "datastore" );
+		dataStoresElement.appendChild( dataStoreElement );
+		
+		Element connectionParamsElement = doc.createElement( "connectionParameters" );
+		dataStoreElement.appendChild( connectionParamsElement );
+		
+		Element parameterElement = doc.createElement( "parameter" );
+		parameterElement.setAttribute( "name", "directory" );
+		parameterElement.setAttribute( "value", System.getProperty( "java.io.tmpdir" ) );
+		connectionParamsElement.appendChild( parameterElement );
+		
+		Transformer tx = TransformerFactory.newInstance().newTransformer();
+		
+		DOMSource source = new DOMSource( doc );
+		
+		final File catalogFile = File.createTempFile( "catalog", "xml" );
+		catalogFile.deleteOnExit();
+		StreamResult result = new StreamResult( catalogFile );
+		
+		tx.transform( source, result );
+		
+		Catalog catalog = new DefaultCatalog();
+		ServiceFinder finder = new DefaultServiceFinder( catalog ) {
+			
+			public List getServiceFactories() {
+				ArrayList list = new ArrayList();
+				list.add( new PropertyServiceFactory() );
+				
+				return list;
+			};
+		};
+		
+		CatalogLoader loader = new CatalogLoader( catalog, finder );
+		loader.setResourceLoader( 
+			new ResourceLoader() {
+				public Resource getResource(String path) {
+					return new FileSystemResource( catalogFile );
+				}
+			}
+		);
+		
+		loader.afterPropertiesSet();
+		assertFalse( catalog.members( null ).isEmpty() );
 		
 	}
 }
