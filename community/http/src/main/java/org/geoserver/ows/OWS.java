e37c1462521aac2a7601bdb3b7052a82fe03c541
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-28 16:46:46 +0000

    added
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4636 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a5e0b6c7df74d39403ebab1c65089b6b2204c0a0 [formerly b3e6ff2048890b156bc2023ccdbee42565568c0e]
    Former-commit-id: 5f97d292269709c23446cb8473b7f56db2225b08

---
+++ community/http/src/main/java/org/geoserver/ows/OWS.java
@@ -0,0 +1,213 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.ows;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Bean containing the properties of an Open Web Service (OWS). 
+ * <p>
+ * An OWS bean is a proper java bean which contains properties common to 
+ * various types of open web services. This class is intended to be subclassed.
+ * </p>
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public class OWS {
+
+	/**
+	 * Service enabled flag
+	 */
+	private boolean enabled;
+	/**
+	 * Url pointing to the service
+	 */
+	private URL onlineResource;
+	/**
+	 * Name
+	 */
+	private String name;
+	/**
+	 * Title
+	 */
+	private String title;
+	/**
+	 * Abstract
+	 */
+	private String serverAbstract;
+	/**
+	 * Fees for using the service.
+	 */
+	private String fees;
+	/**
+	 * Service access constraints.
+	 */
+	private String accessConstraints;
+	/**
+	 * Maintainer of the service.
+	 */
+	private String maintainer;
+	
+	/**
+	 * List of keywords associated with the service. 
+	 */
+	private String[] keywords;
+	
+	/**
+	 * Client properties
+	 */
+	private Map clientProperties;
+	
+	/**
+     * <p>
+     * Returns whether is service is enabled.
+     * </p>
+     *
+     * @return true when enabled.
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+    
+    /**
+     * <p>
+     * Returns the Online Resource for this Service.
+     * </p>
+     *
+     * @return URL The Online resource.
+     */
+    public URL getOnlineResource() {
+        return onlineResource;
+    }
+
+    public void setOnlineResource(URL onlineResource) {
+		this.onlineResource = onlineResource;
+	}
+    
+    /**
+     * <p>
+     * A description of this service.
+     * </p>
+     *
+     * @return String This Service's abstract.
+     */
+    public String getAbstract() {
+        return serverAbstract;
+    }
+
+    public void setAbtract( String serverAbstract) {
+    		this.serverAbstract = serverAbstract;
+    }
+    
+    /**
+     * <p>
+     * A description of this service's access constraints.
+     * </p>
+     *
+     * @return String This service's access constraints.
+     */
+    public String getAccessConstraints() {
+        return accessConstraints;
+    }
+
+    public void setAccessConstraints(String accessConstraints) {
+		this.accessConstraints = accessConstraints;
+	}
+    
+    /**
+     * <p>
+     * A description of the fees for this service.
+     * </p>
+     *
+     * @return String the fees for this service.
+     */
+    public String getFees() {
+        return fees;
+    }
+
+    public void setFees(String fees) {
+		this.fees = fees;
+	}
+    
+    /**
+     * <p>
+     * A list of the keywords for this service.
+     * </p>
+     *
+     * @return List keywords for this service
+     */
+    public String[] getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String[] keywords) {
+		this.keywords = keywords;
+	}
+    
+    /**
+     * <p>
+     * The name of the maintainer for this service.
+     * </p>
+     *
+     * @return String maintainer for this service.
+     */
+    public String getMaintainer() {
+        return maintainer;
+    }
+
+    public void setMaintainer(String maintainer) {
+		this.maintainer = maintainer;
+	}
+    
+    /**
+     * <p>
+     * The name for this service.
+     * </p>
+     *
+     * @return String the service's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+		this.name = name;
+	}
+    
+    /**
+     * <p>
+     * The title for this service.
+     * </p>
+     *
+     * @return String the service's title.
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+		this.title = title;
+	}
+    
+    /**
+     * <p>
+     * Client properties for the service.
+     * </p>
+     */
+    public Map getClientProperties() {
+		return clientProperties;
+	}
+    
+    public void setClientProperties(Map clientProperties) {
+		this.clientProperties = clientProperties;
+	}
+    
+}
