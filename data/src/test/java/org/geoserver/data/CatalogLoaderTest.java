9e1474131882f6b0cc539678db98ebfc24aca384
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-28 14:23:28 +0000

    more tests
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4632 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9776a57689b4c93381d9abba29a21a0227f3fb19 [formerly 7945656006275e6358546aaaa3c31a2e9c104432]
    Former-commit-id: f3201eab9d462596be16cc8cfdf41da6efa6f525

--- data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
+++ data/src/test/java/org/geoserver/data/CatalogLoaderTest.java
@@ -1,7 +1,10 @@
 package org.geoserver.data;
 
+import java.util.Collection;
+
 import junit.framework.TestCase;
 
+import org.geotools.catalog.ServiceFactory;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +17,10 @@ public class CatalogLoaderTest extends TestCase {
 	public void testLoadCatalog() throws Exception {
 		ClassPathXmlApplicationContext context = 
 			new ClassPathXmlApplicationContext("classpath*:/applicationContext.xml");
+		
+		Collection factories = context.getBeansOfType( ServiceFactory.class )
+			.values();
+		assertFalse( factories.isEmpty() );
 		
 		GeoServerCatalog catalog = (GeoServerCatalog) context.getBean("catalog");
 		assertFalse(catalog.members( null ).isEmpty());	
