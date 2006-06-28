3521eaa41e722b5e8a881121fbe1e73eb5456c5c
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-06-28 17:08:08 +0000

    added post test
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4642 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 496dd6beb52168a9b082609a06bf4a9df4db11fa [formerly d0efc494668f707673434578e4038a21ea31818c]
    Former-commit-id: 0fc728c6b7ab2fc469a7920e855cdb89b88b9507

--- community/wfs/src/test/java/org/geoserver/wfs/GetCapabilitiesTest.java
+++ community/wfs/src/test/java/org/geoserver/wfs/GetCapabilitiesTest.java
@@ -1,23 +1,36 @@
 package org.geoserver.wfs;
 
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import junit.framework.TestCase;
 
+import org.apache.xml.utils.DOMBuilder;
 import org.geoserver.ows.http.Dispatcher;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import com.mockobjects.servlet.MockHttpServletRequest;
 import com.mockobjects.servlet.MockHttpServletResponse;
+import com.mockobjects.servlet.MockServletInputStream;
 import com.mockobjects.servlet.MockServletOutputStream;
 
 public class GetCapabilitiesTest extends TestCase {
 
-	public void testGetCapabilities() throws Exception {
-		
-		ClassPathXmlApplicationContext context = 
+	ClassPathXmlApplicationContext context;
+	
+	protected void setUp() throws Exception {
+		context = 
 			new ClassPathXmlApplicationContext( "classpath*:applicationContext.xml" );
+	}
+	
+	public void testGetCapabilitiesGet() throws Exception {
 		
 		Dispatcher dispatcher = 
 			(Dispatcher) context.getBean( "dispatcher" );
@@ -37,6 +50,47 @@ public class GetCapabilitiesTest extends TestCase {
 		request.setupGetInputStream(null);
 		
 		dispatcher.handleRequest( request, response );
+		
+		DocumentBuilder builder = 
+			DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		
+		Document doc = builder.parse( 
+			new InputSource( new StringReader( response.getOutputStreamContents() ) ) 
+		);
+		assertEquals( "WFS_Capabilities", doc.getDocumentElement().getNodeName() );
+	}
+	
+	public void testGetCapabilitiesPost() throws Exception {
+		Dispatcher dispatcher = 
+			(Dispatcher) context.getBean( "dispatcher" );
+		
+		String xml = "<GetCapabilities service=\"WFS\" " + 
+			"xmlns=\"http://www.opengis.net/wfs\" " + 
+			"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " + 
+			"xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd\"/>" ;
+
+		MockServletInputStream input = new MockServletInputStream();
+		input.setupRead( xml.getBytes() );
+		
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setupGetContextPath("/geoserver");
+		request.setupGetMethod("POST");
+		request.setupGetInputStream( input );
+		request.setupGetParameterMap(null);
+		
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		response.setupOutputStream( new MockServletOutputStream() );
+		
+		dispatcher.handleRequest( request, response );
+		
+		DocumentBuilder builder = 
+			DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		
+		Document doc = builder.parse( 
+			new InputSource( new StringReader( response.getOutputStreamContents() ) ) 
+		);
+		assertEquals( "WFS_Capabilities", doc.getDocumentElement().getNodeName() );
 	}
 	
 }
+ 
