066e7b9272c8a20db69a3248dbcad7616d5c28f7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-07-12 16:12:03 +0000

    adaptable catalog support
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4704 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2d2ed9af4d8edef8bbbf1949a9bdf51b0297f36f [formerly d4a0e6aa957436c0b5c9301dd2d6c8b0397cf8c6]
    Former-commit-id: 83ca40070fd045e4fc18b15ffcbd9dbabc9db949

--- data/src/main/java/org/geoserver/data/GeoServerResolveAdapterFactoryFinder.java
+++ data/src/main/java/org/geoserver/data/GeoServerResolveAdapterFactoryFinder.java
@@ -1,58 +1,25 @@
 package org.geoserver.data;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
-import org.geotools.catalog.Resolve;
 import org.geotools.catalog.ResolveAdapterFactory;
-import org.geotools.catalog.ResolveAdapterFactoryFinder;
+import org.geotools.catalog.adaptable.ResolveAdapterFactoryFinder;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class GeoServerResolveAdapterFactoryFinder implements
-		ResolveAdapterFactoryFinder, ApplicationContextAware {
+public class GeoServerResolveAdapterFactoryFinder extends 
+		ResolveAdapterFactoryFinder implements ApplicationContextAware {
 
 	ApplicationContext applicationContext;
 	
-	public boolean has( Resolve resolve, Class adapter ) {
-		return factory( resolve, adapter) != null;
-	}
-
-	public ResolveAdapterFactory find( Resolve resolve, Class adapter ) {
-		return factory( resolve, adapter );
-	}
-
 	public void setApplicationContext( ApplicationContext applicationContext ) 
 		throws BeansException {
 		
 		this.applicationContext = applicationContext;
 	}
 	
-	ResolveAdapterFactory factory( Resolve resolve, Class adapter ) {
-		List matches = new ArrayList();
-		
-		for ( Iterator f = factories().iterator(); f. hasNext(); ) {
-			ResolveAdapterFactory factory = (ResolveAdapterFactory) f.next();
-			if ( factory.canAdapt( resolve, adapter ) ) {
-				matches.add( factory );
-			}
-		}
-		
-		if ( matches.isEmpty() )
-			return null;
-		
-		if ( matches.size() != 1 ) {
-			String msg = "Multiple adapters found.";
-			throw new IllegalStateException( msg );
-		}
-		
-		return (ResolveAdapterFactory) matches.get( 0 );
-	}
-	
-	Collection factories() {
+	public Collection getResolveAdapterFactories() {
 		return applicationContext.getBeansOfType( ResolveAdapterFactory.class )
 			.values();
 	}
