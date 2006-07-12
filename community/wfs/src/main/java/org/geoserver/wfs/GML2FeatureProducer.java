ff16031e52505b84d5d0d09ae9a7e3f1d1425746
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-07-12 15:47:18 +0000

    added
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@4699 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9c3aec8f80f38885db79f4109883c82b32002e61 [formerly e531ca3bf6c0fafb32f4c34bc7ae59d1ae71e361]
    Former-commit-id: 449deae3f4717dfdd764dcb28f786f0c1f6eb94c

---
+++ community/wfs/src/main/java/org/geoserver/wfs/GML2FeatureProducer.java
@@ -0,0 +1,249 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.wfs;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import javax.xml.transform.TransformerException;
+
+import org.geoserver.data.GeoServerCatalog;
+import org.geoserver.ows.ServiceException;
+import org.geoserver.wfs.feature.FeatureTypeInfo;
+import org.geotools.data.FeatureLock;
+import org.geotools.data.FeatureResults;
+import org.geotools.gml.producer.FeatureTransformer;
+import org.geotools.gml.producer.FeatureTransformer.FeatureTypeNamespaces;
+
+/**
+ * Encodes features in Geographic Markup Language (GML) version 2.
+ * 
+ * <p>
+ * GML2-GZIP format is just GML2 with gzip compression. If GML2-GZIP format was
+ * requested, <code>getContentEncoding()</code> will retutn
+ * <code>"gzip"</code>, otherwise will return <code>null</code>
+ * </p>
+ *
+ * @author Gabriel Rold?n
+ * @version $Id$
+ */
+public class GML2FeatureProducer implements FeatureProducer {
+    private static final int NO_FORMATTING = -1;
+    private static final int INDENT_SIZE = 2;
+
+    public static final String formatName = "GML2";
+    public static final String formatNameCompressed = "GML2-GZIP";
+    
+    /**
+     * This is a "magic" class provided by Geotools that writes out GML for an
+     * array of FeatureResults.
+     * 
+     * <p>
+     * This class seems to do all the work, if you have a problem with GML you
+     * will need to hunt it down. We supply all of the header information in
+     * the execute method, and work through the featureList in the writeTo
+     * method.
+     * </p>
+     * 
+     * <p>
+     * This value will be <code>null</code> until execute is called.
+     * </p>
+     */
+    private FeatureTransformer transformer;
+
+    /** will be true if GML2-GZIP output format was requested */
+    private boolean compressOutput = false;
+
+    /**
+     * the results of a getfeature request wich this object will encode as GML2
+     */
+    private GetFeatureResults results;
+
+    /** The getFeature operation */
+    GetFeature operation;
+    
+    /**
+     * Creates the producer with a reference to the GetFeature operation 
+     * using it.
+     */
+    public GML2FeatureProducer( GetFeature operation ) {
+    		this.operation = operation;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param outputFormat DOCUMENT ME!
+     *
+     * @return true if <code>outputFormat</code> is GML2 or GML2-GZIP
+     */
+    public boolean canProduce(String outputFormat) {
+        return formatName.equalsIgnoreCase(outputFormat)
+        || formatNameCompressed.equalsIgnoreCase(outputFormat);
+    }
+
+    /**
+     * prepares for encoding into GML2 format, optionally compressing its
+     * output in gzip, if outputFormat is equal to GML2-GZIP
+     *
+     * @param outputFormat DOCUMENT ME!
+     * @param results DOCUMENT ME!
+     *
+     * @throws IOException DOCUMENT ME!
+     */
+    public void prepare(String outputFormat, GetFeatureResults results)
+        throws IOException {
+        this.compressOutput = formatNameCompressed.equalsIgnoreCase(outputFormat);
+        this.results = results;
+
+        transformer = new FeatureTransformer();
+
+        FeatureTypeNamespaces ftNames = transformer.getFeatureTypeNamespaces();
+        int maxFeatures = operation.getMaxFeatures();
+
+        StringBuffer typeNames = new StringBuffer();
+        FeatureResults features;
+        FeatureTypeInfo meta = null;
+        String prefix = null;
+        
+        GeoServerCatalog catalog = operation.getCatalog();
+        WFS wfs = operation.getWFS();
+        
+        int resCount = results.getResultsetsCount();
+        Map ftNamespaces = new HashMap(resCount);
+
+        for (int resIndex = 0; resIndex < resCount; resIndex++) {
+            features = results.getFeatures(resIndex);
+            meta = results.getTypeInfo(resIndex);
+            prefix = meta.getNamespacePrefix();
+            String uri = catalog.getNamespaceSupport().getURI( prefix );
+            
+            ftNames.declareNamespace(features.getSchema(), prefix, uri);
+
+            if (ftNamespaces.containsKey(uri)) {
+                String location = (String) ftNamespaces.get(uri);
+                ftNamespaces.put(uri, location + "," + meta.name());
+            } 
+            else {
+            		 
+                ftNamespaces.put(
+                		uri, wfs.getOnlineResource() + "request=DescribeFeatureType&typeName=" + meta.name()
+            		);
+            }
+        }
+
+        System.setProperty("javax.xml.transform.TransformerFactory",
+            "org.apache.xalan.processor.TransformerFactoryImpl");
+
+        transformer.setIndentation(wfs.isVerbose() ? INDENT_SIZE
+                                                      : (NO_FORMATTING));
+        transformer.setNumDecimals(wfs.getNumDecimals());
+        transformer.setFeatureBounding(wfs.isFeatureBounding());
+        transformer.setEncoding(wfs.getCharSet());
+
+        String wfsSchemaLoc = wfs.getSchemaBaseURL()
+            + "wfs/1.0.0/WFS-basic.xsd";
+
+        transformer.addSchemaLocation("http://www.opengis.net/wfs", wfsSchemaLoc);
+
+        for (Iterator it = ftNamespaces.keySet().iterator(); it.hasNext();) {
+            String uri = (String) it.next();
+            transformer.addSchemaLocation(uri, (String) ftNamespaces.get(uri));
+        }
+
+        transformer.setGmlPrefixing(wfs.getCiteConformanceHacks());
+
+        FeatureLock featureLock = results.getFeatureLock();
+
+        if (featureLock != null) {
+            transformer.setLockId(featureLock.getAuthorization());
+        }
+
+        transformer.setSrsName(wfs.getSrsPrefix() + meta.getSRS());
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param gs DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public String getMimeType() {
+    		//JD: fix this
+    		//return gs.getMimeType();
+    		return null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public String getContentEncoding() {
+        return compressOutput ? "gzip" : null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param output DOCUMENT ME!
+     *
+     * @throws ServiceException DOCUMENT ME!
+     * @throws IOException DOCUMENT ME!
+     * @throws IllegalStateException DOCUMENT ME!
+     */
+    public void encode(OutputStream output)
+        throws ServiceException, IOException {
+    	
+        if (results == null) {
+            throw new IllegalStateException(
+                "It seems prepare() has not been called"
+                + " or has not succeed");
+        }
+
+        GZIPOutputStream gzipOut = null;
+
+        if (compressOutput) {
+            gzipOut = new GZIPOutputStream(output);
+            output = gzipOut;
+        }
+
+        // execute should of set all the header information
+        // including the lockID
+        //
+        // execute should also fail if all of the locks could not be aquired
+        List resultsList = results.getFeatures();
+        FeatureResults[] featureResults = (FeatureResults[]) resultsList
+            .toArray(new FeatureResults[resultsList.size()]);
+
+        try {
+            transformer.transform(featureResults, output);
+
+            //we need to "finish" here because if not,it is possible that the gzipped
+            //content do not gets completely written
+            if (gzipOut != null) {
+                gzipOut.finish();
+                gzipOut.flush();
+            }
+        } 
+        catch (TransformerException gmlException) {
+            ServiceException serviceException = 
+            		new ServiceException(operation .getHandle() + " error:" + gmlException.getMessage());
+            serviceException.initCause(gmlException);
+            throw serviceException;
+        }
+    }
+    
+    public void produce(String outputFormat, GetFeatureResults results, OutputStream output) throws ServiceException, IOException {
+    		prepare( outputFormat, results );
+    		encode( output );
+    }
+}
---
+++ data/src/main/java/org/geoserver/data/AdaptingGeoServerCatalog.java
@@ -0,0 +1,72 @@
+package org.geoserver.data;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.geotools.catalog.ResolveAdapterFactory;
+
+import org.geotools.catalog.adaptable.AdaptingCatalog;
+import org.geotools.catalog.adaptable.ResolveAdapterFactoryFinder;
+import org.xml.sax.helpers.NamespaceSupport;
+
+/**
+ * GeoServer catalog which supports resolve adapting. 
+ * <p>
+ * In order to perform resolve adapting for a particular class, a
+ * {@link ResolveAdapterFactory} must be registered in a spring context. 
+ * Consider the following adapter factory:
+ * <code>
+ * 	<pre>
+ * 	class MyResolveAdapterFactory {
+ * 
+ *    boolean canAdapt( Resolve resolve, Class adaptee ) {
+ *      return Foo.class.isAssignableFrom( adaptee );
+ *    }
+ *    
+ *    Object adapt( Resolve resolve, Class adaptee, ProgressLister monitor) {
+ *      Foo foo = (Foo) resolve.resolve( adatpee, resolve ) ;
+ *      return new FooAdapter( foo );
+ *    }
+ *  }
+ * 	</pre>
+ * </code>
+ * It would be registered as follows:
+ * <code>
+ * 	<pre>
+ * 	&lt;bean id="fooAdapterFactory" class="org.xyz.FooAdapter"/> 
+ * 	</pre>
+ * </code>
+ * 
+ * </p>
+ * 
+ * @see {@link org.geotools.catalog.ResolveAdapterFactory}
+ * @see {@link org.geotools.catalog.adaptable.AdaptingCatalog}.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public class AdaptingGeoServerCatalog extends AdaptingCatalog
+	implements GeoServerCatalog {
+
+	public AdaptingGeoServerCatalog( ResolveAdapterFactoryFinder adapterFinder ) {
+		super( new DefaultGeoServerCatalog(), adapterFinder );
+	}
+	
+	public AdaptingGeoServerCatalog( 
+		GeoServerCatalog catalog, ResolveAdapterFactoryFinder adapterFinder 
+	) {
+		super( catalog, adapterFinder );
+	}
+	
+	public NamespaceSupport getNamespaceSupport() {
+		return ((DefaultGeoServerCatalog) resolve ).getNamespaceSupport();
+	}
+	
+	public List services( Class resolvee ) throws IOException {
+		return DefaultGeoServerCatalog.services( this, resolvee );
+	}
+
+	public List resources( Class resolvee ) throws IOException {
+		return DefaultGeoServerCatalog.resources( this, resolvee );
+	}
+}
