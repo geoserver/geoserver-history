ee557633646195f19ddffa9308a80ca278db0264
afabiani <afabiani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-07-17 12:20:43 +0000

    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x_debugged_trunk_wcs@4749 ef1d6e69-97e5-0310-af46-8a06194da32a
    Former-commit-id: 1272f97959a2779edcca1c34990a67f9fcab81b4 [formerly 381584c2615d06f5bfc8ed1838d2cef5ad397686]
    Former-commit-id: 2deaa353fcee248b49d5169d45b4f991cab18d3e

---
+++ wcs/src/main/java/org/vfny/geoserver/wcs/requests/WCSRequest.java
@@ -0,0 +1,57 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.wcs.requests;
+
+import org.vfny.geoserver.Request;
+import org.vfny.geoserver.global.GeoServer;
+import org.vfny.geoserver.global.WCS;
+import org.vfny.geoserver.wcs.servlets.WCService;
+
+/**
+ * DOCUMENT ME!
+ * 
+ * @author $Author: Alessio Fabiani (alessio.fabiani@gmail.com) $ (last modification)
+ * @author $Author: Simone Giannecchini (simboss1@gmail.com) $ (last modification)
+ */
+public class WCSRequest extends Request {
+    public static final String WCS_SERVICE_TYPE = "WCS";
+
+    /**
+     * A WCSRequest configured with WCS_SERVICE_TYPE
+     */
+    public WCSRequest(String requestType, WCService service) {
+        super(WCS_SERVICE_TYPE, requestType, service);
+    }
+
+
+    /**
+     * Sets the wcs service object. 
+     */
+    public void setWCService(WCService wcs) {
+    		setServiceRef(wcs);
+    }
+    
+    /**
+     * Returns the wcs service object..
+     */
+    public WCService getWCService() {
+    		return (WCService) getServiceRef();
+    }
+    
+    /**
+     * Convenience method for obtaining the global wcs service instance.
+     */
+    public WCS getWCS() {
+    		return getWCService().getWCS();
+    }
+
+    /**
+     * Convenience method for obtaining the global geoserver instance.
+     */
+    public GeoServer getGeoServer(){
+    	GeoServer gs = getWCS().getGeoServer();
+    	return gs;
+    }
+}
