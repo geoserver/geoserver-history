ad897a1b2be576684d01bfb6a55e7ce260871684
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-08-16 15:03:54 +0000

    fixing build for cc test
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@4890 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ab5bd78c0db5576e73297bbe5dee5545b709062e [formerly 37ba6a0257579c5396e2204d6a3f6a7c74b6c64a]
    Former-commit-id: dbd03bc18c8b999597e2cd64210b38aca4c5de36

--- main/src/main/java/org/vfny/geoserver/Request.java
+++ main/src/main/java/org/vfny/geoserver/Request.java
@@ -1,4 +1,3 @@
-breaking the build
 /* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
