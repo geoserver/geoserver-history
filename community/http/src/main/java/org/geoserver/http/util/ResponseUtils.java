e1376e7876399158fa193360d473ae415afc38c8
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-08-22 04:56:02 +0000

    added
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@4929 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5abf12086d72d5ee50f4f95170c9e0c84ebb6ac5 [formerly b78e86226f5fcb7acb5da19ad04d7d9881d9645e]
    Former-commit-id: 4ea399d4b6dbbff29e7e4fcc753eda55ad0969ab

---
+++ community/http/src/main/java/org/geoserver/http/util/ResponseUtils.java
@@ -0,0 +1,139 @@
+/* Copyright (c) 2001, 2003 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.http.util;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Logger;
+
+
+//TODO: JD: move this class to http module
+public class ResponseUtils {
+  
+  /**
+   * Parses the passed string, and encodes the special characters (used in
+   * xml for special purposes) with the appropriate codes. e.g. '<' is
+   * changed to '&lt;'
+   *
+   * @param inData The string to encode into xml.
+   *
+   * @return the encoded string. Returns null, if null is passed as argument
+   *
+   */
+    public static String encodeXML( String inData)  {
+        //return null, if null is passed as argument
+        if (inData == null) {
+            return null;
+        }
+
+        //if no special characters, just return
+        //(for optimization. Though may be an overhead, but for most of the
+        //strings, this will save time)
+        if ((inData.indexOf('&') == -1) && (inData.indexOf('<') == -1)
+                && (inData.indexOf('>') == -1) && (inData.indexOf('\'') == -1)
+                && (inData.indexOf('\"') == -1)) {
+            return inData;
+        }
+
+        //get the length of input String
+        int length = inData.length();
+
+        //create a StringBuffer of double the size (size is just for guidance
+        //so as to reduce increase-capacity operations. The actual size of
+        //the resulting string may be even greater than we specified, but is
+        //extremely rare)
+        StringBuffer buffer = new StringBuffer(2 * length);
+
+        char charToCompare;
+
+        //iterate over the input String
+        for (int i = 0; i < length; i++) {
+            charToCompare = inData.charAt(i);
+
+            //if the ith character is special character, replace by code
+            if (charToCompare == '&') {
+                buffer.append("&amp;");
+            } else if (charToCompare == '<') {
+                buffer.append("&lt;");
+            } else if (charToCompare == '>') {
+                buffer.append("&gt;");
+            } else if (charToCompare == '\"') {
+                buffer.append("&quot;");
+            } else if (charToCompare == '\'') {
+                buffer.append("&apos;");
+            } else {
+                buffer.append(charToCompare);
+            }
+        }
+
+        //return the encoded string
+        return buffer.toString();
+    }
+
+     /**
+     * Writes <CODE>string</CODE> into writer, escaping &, ', ", <, and >
+     * with the XML excape strings.
+     */
+    public static void writeEscapedString(Writer writer, String string)
+        throws IOException {
+        for(int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if(c == '<')
+                writer.write("&lt;");
+            else if(c == '>')
+                writer.write("&gt;");
+            else if(c == '&')
+                writer.write("&amp;");
+            else if(c == '\'')
+                writer.write("&apos;");
+            else if(c == '"')
+                writer.write("&quot;");
+            else
+		writer.write(c);
+        }
+    }
+    
+    /**
+     * Appends a query string to a url.
+     * <p>
+     * This method checks <code>url</code> to see if the appended query string requires a '?' or 
+     * '&' to be prepended.
+     * </p>
+     * 
+     * @param url The base url.
+     * @param queryString The query string to be appended, should not contain the '?' character.
+     * 
+     * @return A full url with the query string appended.
+     */
+    public static String appendQueryString( String url, String queryString ) {
+    		if ( url.endsWith( "?" ) || url.endsWith( "&" ) )
+    			return url + queryString;
+ 
+    		if ( url.indexOf( '?' ) != -1 ) {
+    			return url + "&" + queryString;
+    		}
+    		
+    		return url + "?" + queryString; 
+    }
+    
+    /**
+     * Appends a path tpo a url.
+     * <p>
+     * This method checks <code>url</code> to see if the appended path requires a '/' to be 
+     * prepended.
+     * </p>
+     * @param url The base url.
+     * @param path The path to be appended to the url.
+     * 
+     * @return The full url with the path appended.
+     */
+    public static String appendPath( String url, String path ) {
+    		if ( path.startsWith( "/" ) ) 
+    			path = path.substring( 1 );
+    		
+    		return url.endsWith( "/" ) ? url + path : url + "/" + path;
+    }
+}
+
