05ae08f7d3eaaba4df11739bfeb8214ce743ef39
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-08-29 16:59:48 +0000

    made extension of 1.0.0 operation
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@4963 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 808c5452edbbb4674d4b3e50e1a47c5d2520c543 [formerly c073b1fdbb682eb59b5a9fc3ad3730df696dbf68]
    Former-commit-id: 7107fea9eb48f79bd50a04ebcf87232bac8ae237

--- community/wfs/src/main/java/org/geoserver/wfs/v1_1_0/GetFeature.java
+++ community/wfs/src/main/java/org/geoserver/wfs/v1_1_0/GetFeature.java
@@ -1,9 +1,20 @@
 package org.geoserver.wfs.v1_1_0;
 
+import java.math.BigInteger;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.geoserver.data.GeoServerCatalog;
+import org.geoserver.wfs.EMFUtils;
+import org.geoserver.wfs.Query;
+import org.geoserver.wfs.WFS;
+import org.geoserver.wfs.WFSException;
 import org.geotools.feature.FeatureCollection;
+import org.opengis.filter.sort.SortBy;
 
 import net.opengis.wfs.v1_1_0.GetFeatureType;
 import net.opengis.wfs.v1_1_0.WFSFactory;
+import net.opengis.wfs.v1_1_0.WFSPackage;
 
 /**
  * WFS 1.1.0 GetFeature operation.
@@ -11,29 +22,52 @@ import net.opengis.wfs.v1_1_0.WFSFactory;
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  *
  */
-public class GetFeature {
+public class GetFeature extends org.geoserver.wfs.GetFeature {
 
-	
-	GetFeatureType request;
-	
-	public FeatureCollection getFeature() {
-		return getFeature( request() );
+	public GetFeature(WFS wfs, GeoServerCatalog catalog) {
+		super(wfs, catalog, WFSFactory.eINSTANCE );
+	}
+
+	public void setSrsName( List srsName ) throws WFSException {
+		batchSet( "srsName", srsName );
 	}
 	
-	public FeatureCollection getFeature( GetFeatureType request ) {
-		return null;
+	public void setSortBy( List sortBy ) throws WFSException {
+		batchSet( "sortBy", sortBy );
 	}
 	
-	protected GetFeatureType request() {
+	protected EObject request() {
 		if ( request == null ) {
 			synchronized ( this ) {
 				if ( request == null ) {
-					request = WFSFactory.eINSTANCE.createGetFeatureType();
+					request = factory.create( WFSPackage.eINSTANCE.getGetFeatureType() );
 				}
 			}
 		}
 		
 		return request;
 	}
+	
+	protected EObject query() {
+		return factory.create( WFSPackage.eINSTANCE.getQueryType() );
+	}
+	
+	protected Query query( EObject q ) {
+		Query query = super.query( q );
+		
+		//set the wfs 1.1. specific stuff
+		if ( EMFUtils.isSet( q, "srsName" ) ) {
+			query.setSrsName( (String) EMFUtils.get( q, "srsName" ) );
+		}
+		
+		if ( EMFUtils.isSet( q, "sortBy" ) ) {
+			query.setSortBy( (SortBy) EMFUtils.get( q, "sortBy") );
+		}
+		
+		return query;
+	}
+	
+	
+	
 	
 }
