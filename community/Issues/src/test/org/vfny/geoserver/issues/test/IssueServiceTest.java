381d27d31c39f55f3846ae014b59534bf8109dbe
quintona <quintona@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-09-03 14:20:55 +0000

    Added the basic unit test and the spring file
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@4992 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1203f87c543dfa15d7bb34b20e9221bd27fee272 [formerly 86136bb55a201c5b3a7a8068f20fe3a7bcad34b3]
    Former-commit-id: dadc19c56368fa20c1cd69c8e0ce376a40584674

---
+++ community/Issues/src/test/org/vfny/geoserver/issues/test/IssueServiceTest.java
@@ -0,0 +1,26 @@
+package org.vfny.geoserver.issues.test;
+
+import junit.framework.TestCase;
+
+public class IssueServiceTest extends TestCase {
+   
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testAdding(){
+        fail("Test not yet implemented");
+        
+    }
+    
+    public void testModify(){
+        fail("Test not yet implemented");
+    }
+    
+    public void testRemove(){
+        fail("Test not yet implemented");    
+    }
+
+}
