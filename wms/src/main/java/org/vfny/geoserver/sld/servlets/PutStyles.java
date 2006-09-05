b743b6e34b4ac10eae48ec714d8df9c9a74a2470
bowens <bowens@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-09-05 18:19:14 +0000

    changed so it is not required to have an existing feature type to save a style
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@4993 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1012863e820e6159b051802a1939189f9ceec38b [formerly a1c66b03d1ae3b02d59350e1028136813b6f887d]
    Former-commit-id: 73c6bed6f99ec872a0546d95bb109e3d7df33a0c

--- wms/src/main/java/org/vfny/geoserver/sld/servlets/PutStyles.java
+++ wms/src/main/java/org/vfny/geoserver/sld/servlets/PutStyles.java
@@ -457,6 +457,7 @@ public class PutStyles extends AbstractService {
         // get our featureType by the layerName
         List keys = dataConfig.getFeatureTypeConfigKeys();
         Iterator it = keys.iterator();
+        layerName = null;
         while (it.hasNext())// get the full featureType name that has the datastore prefix
         {
         	String o = it.next().toString();
@@ -468,9 +469,12 @@ public class PutStyles extends AbstractService {
         	}
         }
         
-        // get the feature type and save the style for it
-        FeatureTypeConfig featureTypeConfig = dataConfig.getFeatureTypeConfig(layerName);
-        featureTypeConfig.setDefaultStyle(styleName);
+        // get the feature type and save the style for it, if the feature type exists yet
+        // If there is no FT there that may mean that the user is just creating it.
+        if (layerName != null) {
+        	FeatureTypeConfig featureTypeConfig = dataConfig.getFeatureTypeConfig(layerName);
+        	featureTypeConfig.setDefaultStyle(styleName);
+        }
         
 		// if successful, return "success"
 		//response.setContentType(success_mime_type);
