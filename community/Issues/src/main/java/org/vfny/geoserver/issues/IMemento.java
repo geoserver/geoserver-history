c307c2e5d994b8f4b354ad64815b57fa792fded4
quintona <quintona@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-09-09 13:37:28 +0000

    Added an implementation of the XMLMemento. Linked this to the issues object via the wrapper, the service will not persist any memento passed to it as an xml document.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5018 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b6c455aaf0f08629bb0ba61fd2f886d8bd56b228 [formerly 8f25331d4576ec4f0778aaf0eeaac3988a2f572e]
    Former-commit-id: ec78c095f4eaa358ed28b2028fc2ce01215c923d

--- community/Issues/src/main/java/org/vfny/geoserver/issues/IMemento.java
+++ community/Issues/src/main/java/org/vfny/geoserver/issues/IMemento.java
@@ -1,5 +1,6 @@
 package org.vfny.geoserver.issues;
 
+import java.io.IOException;
 
 /**
  * The generic memento interface for the issues module within geoserver.
@@ -9,45 +10,143 @@ package org.vfny.geoserver.issues;
  * @since 1.1.0
  */
 public interface IMemento {
-    
     /**
-     * Creates a new child of this memento
-     * @return The new child
+     * Special reserved key used to store the memento id 
+     * (value <code>"org.eclipse.ui.id"</code>).
+     *
+     * @see #getID()
+     */
+    public static final String TAG_ID = "IMemento.id"; //$NON-NLS-1$
+
+    /**
+     * Creates a new child of this memento with the given type.
+     * <p>
+     * The <code>getChild</code> and <code>getChildren</code> methods
+     * are used to retrieve children of a given type.
+     * </p>
+     *
+     * @param type the type
+     * @return a new child memento
+     * @see #getChild
+     * @see #getChildren
      */
     public IMemento createChild(String type);
+
     /**
-     * Creats a new child of this memento with the given id
-     * Throws an exception if a duplicate id is passed to the method
-     * @param id
-     * @return The new child
+     * Creates a new child of this memento with the given type and id.
+     * The id is stored in the child memento (using a special reserved
+     * key, <code>TAG_ID</code>) and can be retrieved using <code>getId</code>.
+     * <p>
+     * The <code>getChild</code> and <code>getChildren</code> methods
+     * are used to retrieve children of a given type.
+     * </p>
+     *
+     * @param type the type
+     * @param id the child id
+     * @return a new child memento with the given type and id
+     * @see #getID
      */
-    public IMemento createChild(String type, String id) throws Exception;
+    public IMemento createChild(String type, String id);
+
     /**
-     * Returns the child specified by the given id
-     * @param id
-     * @return The child memento, null if the child does not exist
+     * Returns the first child with the given type id.
+     *
+     * @param type the type id
+     * @return the first child with the given type
      */
     public IMemento getChild(String type);
+
     /**
-     * Returns the children of this memento
-     * @return An array of children
+     * Returns all children with the given type id.
+     *
+     * @param type the type id
+     * @return the list of children with the given type
      */
     public IMemento[] getChildren(String type);
+
     /**
-     * Returns the id of this memento
+     * Returns the floating point value of the given key.
+     *
+     * @param key the key
+     * @return the value, or <code>null</code> if the key was not found or was found
+     *   but was not a floating point number
+     */
+    public Float getFloat(String key);
+
+    /**
+     * Returns the id for this memento.
+     *
+     * @return the memento id, or <code>null</code> if none
+     * @see #createChild(java.lang.String,java.lang.String)
      */
     public String getID();
+
     /**
-     * Returns a value as identified by the key
-     * @param key
-     * @return The value, null if the key could not be resolved
+     * Returns the integer value of the given key.
+     *
+     * @param key the key
+     * @return the value, or <code>null</code> if the key was not found or was found
+     *   but was not an integer
      */
-    public String getValue(String key);
+    public Integer getInteger(String key);
+
     /**
-     * Adds a value to this memento
-     * @param key The key to associate the value to
-     * @param value The actual value
+     * Returns the string value of the given key.
+     *
+     * @param key the key
+     * @return the value, or <code>null</code> if the key was not found
      */
-    public void putValue(String key, String value);
-    
+    public String getString(String key);
+
+    /**
+     * Returns the data of the Text node of the memento. Each memento is allowed
+     * only one Text node.
+     * 
+     * @return the data of the Text node of the memento, or <code>null</code>
+     * if the memento has no Text node.
+     * @since 2.0
+     */
+    public String getTextData();
+
+    /**
+     * Sets the value of the given key to the given floating point number.
+     *
+     * @param key the key
+     * @param value the value
+     */
+    public void putFloat(String key, float value);
+
+    /**
+     * Sets the value of the given key to the given integer.
+     *
+     * @param key the key
+     * @param value the value
+     */
+    public void putInteger(String key, int value);
+
+    /**
+     * Copy the attributes and children from  <code>memento</code>
+     * to the receiver.
+     *
+     * @param memento the IMemento to be copied.
+     */
+    public void putMemento(IMemento memento);
+
+    /**
+     * Sets the value of the given key to the given string.
+     *
+     * @param key the key
+     * @param value the value
+     */
+    public void putString(String key, String value);
+
+    /**
+     * Sets the memento's Text node to contain the given data. Creates the Text node if
+     * none exists. If a Text node does exist, it's current contents are replaced. 
+     * Each memento is allowed only one text node.
+     * 
+     * @param data the data to be placed on the Text node
+     * @since 2.0
+     */
+    public void putTextData(String data);
 }
--- community/Issues/src/main/java/org/vfny/geoserver/issues/MementoWrapper.java
+++ community/Issues/src/main/java/org/vfny/geoserver/issues/MementoWrapper.java
@@ -1,14 +1,25 @@
 package org.vfny.geoserver.issues;
 
+import java.io.IOException;
+
 public class MementoWrapper {
     public static IMemento getMementoFromString(String mementoString){
-        return null;
+        try {
+            return XMLMemento.loadMementoFromString(mementoString);
+        } catch (Exception e) {
+            return null;
+        }
         
     }
-    
+    private static final String ROOT_ID = "MementoData";
     public static String getStringFromMemento(IMemento memento){
-        return null;
-        
+        XMLMemento newMemento = XMLMemento.createWriteRoot(ROOT_ID);
+        newMemento.putMemento(memento);
+        try {
+            return newMemento.saveToString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 
 }
