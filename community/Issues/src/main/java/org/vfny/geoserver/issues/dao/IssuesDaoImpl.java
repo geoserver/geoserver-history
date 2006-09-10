d830e64ebd7b5ba202d6170abfb1f4fee84a4133
quintona <quintona@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-09-10 11:25:27 +0000

    added dao impl
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5025 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a59d8d6f28c7430bb9a8b93f66a8cf46f097935d [formerly 10aff38bb5ba4354ef665ba0510863ad0e06f0d4]
    Former-commit-id: 065a8f00267d480b94e3edc079e5a60a20c70014

---
+++ community/Issues/src/main/java/org/vfny/geoserver/issues/dao/IssuesDaoImpl.java
@@ -0,0 +1,40 @@
+package org.vfny.geoserver.issues.dao;
+
+import java.util.Collection;
+
+import org.vfny.geoserver.issues.IIssue;
+import org.vfny.geoserver.issues.Target;
+
+public class IssuesDaoImpl implements IssuesDao {
+
+    public IIssue findIssueById( int id ) {
+        return null;
+    }
+
+    public Collection<IIssue> findByTarget( Target target ) {
+        return null;
+    }
+
+    public void insertIssue( IIssue issue ) {
+    }
+
+    public void insertIssues( Collection<IIssue> issues ) {
+    }
+
+    public void updateIssue( IIssue issue ) {
+    }
+
+    public void updateIssues( Collection<IIssue> issues ) {
+    }
+
+    public Collection<IIssue> getAllIssues() {
+        return null;
+    }
+
+    public void removeIssue( IIssue issue ) {
+    }
+
+    public void removeIssues( Collection<IIssue> issues ) {
+    }
+
+}
