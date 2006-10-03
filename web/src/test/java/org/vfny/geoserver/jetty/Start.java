e8ce6360dc919c8fdbf03e3941db0563873a0d4b
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-03 09:42:06 +0000

    Fixed logging
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5094 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 92e1b0894dc48e5753afddcbdddf6221c5950b42 [formerly 886567052c044f867f43b7b4b2447b8b543617c5]
    Former-commit-id: d5b28da0cc3331d82a1b16a5337b6e7ee13ab8b0

--- web/src/test/java/org/vfny/geoserver/jetty/Start.java
+++ web/src/test/java/org/vfny/geoserver/jetty/Start.java
@@ -1,5 +1,6 @@
 package org.vfny.geoserver.jetty;
 
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -8,43 +9,43 @@ import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 
-
 /**
  * Jetty starter, will run geoserver inside the Jetty web container.<br>
  * Useful for debugging, especially in IDE were you have direct dependencies
- * between the sources of the various modules (such as Eclipse). 
+ * between the sources of the various modules (such as Eclipse).
+ * 
  * @author wolf
- *
+ * 
  */
 public class Start {
-	private static final Logger log = LogManager.getLogManager().getLogger(Start.class.getName());
-	
+	private static final Logger log = Logger.getLogger(Start.class.getName());
+
 	public static void main(String[] args) {
 		Server jettyServer = null;
-        try {
-            jettyServer = new Server();
+		try {
+			jettyServer = new Server();
 
-            SocketConnector conn = new SocketConnector();
-            conn.setPort(8080);
-            jettyServer.setConnectors(new Connector[]{conn});
+			SocketConnector conn = new SocketConnector();
+			conn.setPort(8080);
+			jettyServer.setConnectors(new Connector[] { conn });
 
-            WebAppContext wah = new WebAppContext();
-            wah.setContextPath("/geoserver");
-            wah.setWar("src/main/webapp");
-            jettyServer.setHandler(wah);
+			WebAppContext wah = new WebAppContext();
+			wah.setContextPath("/geoserver");
+			wah.setWar("src/main/webapp");
+			jettyServer.setHandler(wah);
 
-            jettyServer.start();
-        }
-        catch (Exception e) {
-            log.severe("Could not start the Jetty server: " + e);
-            if (jettyServer != null) {
-                try {
-                    jettyServer.stop();
-                }
-                catch (Exception e1) {
-                    log.severe("Unable to stop the jetty server: " + e1);
-                }
-            }
-        }
+			jettyServer.start();
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Could not start the Jetty server: "
+					+ e.getMessage(), e);
+			if (jettyServer != null) {
+				try {
+					jettyServer.stop();
+				} catch (Exception e1) {
+					log.log(Level.SEVERE, "Unable to stop the "
+							+ "Jetty server:" + e1.getMessage(), e1);
+				}
+			}
+		}
 	}
 }
