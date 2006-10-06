b0623a0e6335cf69783277d66beac96c94541899
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-06 19:16:01 +0000

    updated to use new configuration
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5126 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3b5370d6753f6f74ee8c6a01036839b0d0fa3753 [formerly e9f032ab20622dad6b55e52b684e61d6025a1647]
    Former-commit-id: d021524f9df6f25b019823048808dadb178c1753

--- community/ows/src/test/java/org/geoserver/xml/ows/OWSXMLTest.java
+++ community/ows/src/test/java/org/geoserver/xml/ows/OWSXMLTest.java
@@ -4,6 +4,7 @@ import net.opengis.ows.v1_0_0.GetCapabilitiesType;
 import net.opengis.ows.v1_0_0.OWSFactory;
 
 import org.geoserver.xml.ows.v1_0_0.OWSBindingConfiguration;
+import org.geoserver.xml.ows.v1_0_0.OWSConfiguration;
 import org.geoserver.xml.ows.v1_0_0.OWSSchemaLocationResolver;
 import org.geotools.xlink.bindings.XLINKBindingConfiguration;
 import org.geotools.xlink.bindings.XLINKSchemaLocationResolver;
@@ -17,23 +18,8 @@ import junit.framework.TestCase;
 public class OWSXMLTest extends TestCase {
 
 	public void testGetCapabilities() throws Exception {
-		Configuration configuration = new Configuration() {
-
-			public void configureBindings( MutablePicoContainer container ) {
-				new XSBindingConfiguration().configure( container );
-				new XLINKBindingConfiguration().configure( container );
-				new OWSBindingConfiguration().configure( container );
-				
-			}
-
-			public void configureContext( MutablePicoContainer container ) {
-				container.registerComponentInstance( OWSFactory.eINSTANCE );
-				container.registerComponentImplementation( XLINKSchemaLocationResolver.class );
-				container.registerComponentImplementation( OWSSchemaLocationResolver.class );
-				
-			}
-		};
-		
+		Configuration configuration = new OWSConfiguration(); 
+			
 		Parser parser = new Parser( configuration );
 		Object o = parser.parse( getClass().getResourceAsStream( "getCapabilities.xml") );
 		
