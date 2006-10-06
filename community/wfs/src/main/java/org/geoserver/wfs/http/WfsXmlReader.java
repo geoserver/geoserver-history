27abb8432bb54744595f306847ece61626a0383d
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-06 19:35:50 +0000

    updated to use new xml configuration api
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5136 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f87f30b63352400b4462e088d4b39a00533c71e7 [formerly 263cd43613450f760102f6ff7b0a2b43fc818b60]
    Former-commit-id: 074d647d550f851758f632163967bbad2211707a

--- community/wfs/src/main/java/org/geoserver/wfs/http/WfsXmlReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/WfsXmlReader.java
@@ -2,62 +2,26 @@ package org.geoserver.wfs.http;
 
 import java.io.InputStream;
 
-import net.opengis.wfs.WFSFactory;
-
+import org.geoserver.data.GeoServerCatalog;
 import org.geoserver.ows.http.XmlReader;
 import org.geoserver.wfs.xml.wfs.WFS;
-import org.geoserver.wfs.xml.wfs.WFSBindingConfiguration;
-import org.geoserver.wfs.xml.wfs.WFSSchemaLocationResolver;
-import org.geoserver.wfs.xml.wfs.WFSSchemaLocator;
-import org.geotools.filter.FilterFactoryFinder;
-import org.geotools.filter.v1_0.OGCBindingConfiguration;
-import org.geotools.filter.v1_0.OGCSchemaLocationResolver;
-import org.geotools.gml2.bindings.GMLBindingConfiguration;
-import org.geotools.gml2.bindings.GMLSchemaLocationResolver;
-import org.geotools.xlink.bindings.XLINKBindingConfiguration;
-import org.geotools.xlink.bindings.XLINKSchemaLocationResolver;
+import org.geoserver.wfs.xml.wfs.WFSConfiguration;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.Parser;
-import org.geotools.xs.bindings.XSBindingConfiguration;
-import org.picocontainer.MutablePicoContainer;
-
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.impl.CoordinateArraySequenceFactory;
 
 public class WfsXmlReader extends XmlReader {
 
-	public WfsXmlReader( String element ) {
+	GeoServerCatalog catalog;
+	
+	public WfsXmlReader( String element, GeoServerCatalog catalog ) {
 		super( WFS.NAMESPACE, element, "1.0.0" );
+		this.catalog = catalog;
 	}
 
 	public Object parse( InputStream input ) throws Exception {
-		Configuration configuration = new Configuration() {
-
-			public void configureBindings(MutablePicoContainer container) {
-				new XSBindingConfiguration().configure( container );
-				new XLINKBindingConfiguration().configure( container );
-				new OGCBindingConfiguration().configure( container );
-				new GMLBindingConfiguration().configure( container );
-				new WFSBindingConfiguration().configure( container );
-			}
-
-			public void configureContext(MutablePicoContainer container) {
-				container.registerComponentImplementation( GeometryFactory.class );
-				container.registerComponentInstance( CoordinateArraySequenceFactory.instance() );
-				container.registerComponentInstance( FilterFactoryFinder.createFilterFactory() );
-				
-				container.registerComponentInstance( WFSFactory.eINSTANCE );
-		
-				container.registerComponentImplementation( XLINKSchemaLocationResolver.class );
-				container.registerComponentImplementation( OGCSchemaLocationResolver.class );
-				container.registerComponentImplementation( GMLSchemaLocationResolver.class );
-				container.registerComponentImplementation( WFSSchemaLocationResolver.class );
-		
-				container.registerComponentImplementation( WFSSchemaLocator.class );
-			}
+	
+		Configuration configuration = new WFSConfiguration( catalog ); 
 			
-		};
-		
 		Parser parser = new Parser( configuration, input );
 		return parser.parse();
 	}
