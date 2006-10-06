29e2d5a199b2abcbe4ffef48ed15441cd3c549f9
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-06 19:15:39 +0000

    added
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5125 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 727934c5ae4c203e9f9a1f6ee43183fe064b5118 [formerly c7901f3c529a646cc6571240a21eaa558f948a11]
    Former-commit-id: c5bcf4827d0eea15739bd9d494520c042305c0f8

---
+++ community/ows/src/main/java/org/geoserver/xml/ows/v1_0_0/OWSConfiguration.java
@@ -0,0 +1,77 @@
+package org.geoserver.xml.ows.v1_0_0;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import net.opengis.ows.v1_0_0.OWSFactory;
+
+import org.eclipse.xsd.util.XSDSchemaLocationResolver;
+import org.geotools.xlink.XLINKConfiguration;
+import org.geotools.xml.BindingConfiguration;
+import org.geotools.xml.Configuration;
+import org.picocontainer.MutablePicoContainer;
+
+/**
+ * Parser configuration for ows schema.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class OWSConfiguration extends Configuration {
+
+	/**
+	 * Creates a new configuration, adding the dependency on {@link OWSConfiguration}.
+	 */
+	public OWSConfiguration() {
+		super();
+		
+		addDependency( new XLINKConfiguration() );
+	}
+
+	/**
+	 * @return {@link OWS#NAMESPACE}, http://www.opengis.net/ows
+	 */
+	public String getNamespaceURI() {
+		return OWS.NAMESPACE;
+	}
+
+	/**
+	 * @return the owsAll.xsd file of the ows schema.
+	 */
+	public URL getSchemaFileURL() throws MalformedURLException {
+		return new URL( 
+			getSchemaLocationResolver().resolveSchemaLocation( null, getNamespaceURI(), "owsAll.xsd" )	
+		);
+	}
+
+	/**
+	 * @return A new instance of {@link OWSBindingConfiguration}.
+	 */
+	public BindingConfiguration getBindingConfiguration() {
+		return new OWSBindingConfiguration();
+	}
+
+	/**
+	 * @return A new instance of {@link OWSSchemaLocationResolver}.
+	 */
+	public XSDSchemaLocationResolver getSchemaLocationResolver() {
+		return new OWSSchemaLocationResolver();
+	}
+	
+	  /**
+     * Configures the ows context.
+     * <p>
+     * The following factories are registered:
+     * <ul>
+     * <li>{@link OWSFactory}
+     * </ul>
+     * </p>
+     */
+	protected void configureContext(MutablePicoContainer container) {
+		super.configureContext( container );
+		
+		container.registerComponentInstance( OWSFactory.eINSTANCE );
+	}
+	
+
+}
