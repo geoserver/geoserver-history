7ad0fc5dc82dc7fc76cd8aae793de4a5a87b8314
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-13 22:01:33 +0000

    utility class for reflective operations on services
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5204 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5fcadfdbcdbf4f6b9fb9e0fda9e3afa7f93c6ef2 [formerly 4bdbc1249075f36a04ec4c43cafb86b8459bf83f]
    Former-commit-id: bded18c3538ebe00352fbcaabbe7d34b6ea96b0e

---
+++ community/http/src/main/java/org/geoserver/ows/http/OWSUtils.java
@@ -0,0 +1,82 @@
+package org.geoserver.ows.http;
+
+import java.lang.reflect.Method;
+
+/**
+ * Utility class for performing reflective operations.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class OWSUtils {
+
+	/**
+	 * Returns a setter method for a property of java bean.
+	 * 
+	 * @param clazz The type of the bean.
+	 * @param property The property name.
+	 * @param type The type of the property.
+	 * 
+	 * @return The setter method, or <code>null</code> if not found.
+	 */
+	public static Method setter( Class clazz, String property, Class type ) {
+		//TODO: this lookup doesn't deal with classes that might have two setters
+		// that are in the same class hierachy
+		Method[] methods = clazz.getMethods();
+		for ( int i = 0; i < methods.length; i++) {
+			Method method = methods[ i ];
+			if ( method.getName().equalsIgnoreCase( "set" + property ) ) {
+				if ( 
+					method.getParameterTypes().length == 1 &&
+					method.getParameterTypes()[ 0 ].isAssignableFrom( type )
+				) {
+					return method;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns a method with a pariticular name of a class, ignoring method
+	 * paramters.
+	 * 
+	 * @param clazz The class 
+	 * @param name The name of the method.
+	 * 
+	 * @return The method, or <code>null</code> if it could not be found.
+	 */
+	public static Method method( Class clazz, String name ) {
+		Method[] methods = clazz.getMethods();
+		for ( int i = 0; i < methods.length; i++) {
+			Method method = methods[ i ];
+			if ( method.getName().equalsIgnoreCase( name ) ) {
+				return method;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns an object of a particular type in a list of objects of 
+	 * various types.
+	 * 
+	 * @param parameters A list of objects, of various types.
+	 * @param type The type of paramter to be returned.
+	 * 
+	 * @return The object of the specified type, or <code>null</code>
+	 */
+	public static Object parameter( Object[] parameters, Class type ) {
+		
+		for ( int i = 0; i < parameters.length; i++ ) {
+			Object parameter = parameters[ i ];
+			if ( parameter != null && type.isAssignableFrom( parameter.getClass() ) ) {
+				return parameter;
+			}
+		}
+		
+		return null;
+	}
+}
