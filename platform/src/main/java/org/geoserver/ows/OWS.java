741069271177b95ae77964a364e9b68fc88c5413
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-13 21:55:55 +0000

    added identifier property
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5200 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8f64eba9abf4c1a32a7bffeb7e024fa3a1850794 [formerly 6ab9a4404e1100b90239e2dc9d59f57944ce00b6]
    Former-commit-id: 285d3c1bd914c1d4ba16addd2c37e845f7e88271

--- platform/src/main/java/org/geoserver/ows/OWS.java
+++ platform/src/main/java/org/geoserver/ows/OWS.java
@@ -20,6 +20,10 @@ import java.util.Map;
 public class OWS {
 
 	/**
+	 * id of the service
+	 */
+	private String id;
+	/**
 	 * Service enabled flag
 	 */
 	private boolean enabled;
@@ -80,6 +84,22 @@ public class OWS {
 	 * Local used to look up schemas.
 	 */
     private String schemaBaseURL;
+    
+    /**
+     * Creates a new OWS. 
+     * 
+     * @param id The id of the service.
+     */
+    public OWS( String id ) {
+    	this.id = id;
+    }
+    
+    /**
+     * @return The identifier of the service
+     */
+    public String getId() {
+		return id;
+	}
     
 	/**
      * <p>
