0e29627a3d4efc2fd033733918b53d75e8d7ac38
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-16 02:45:54 +0000

    added check for namespace qualified property name
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5249 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fce2a9392437dc58ccd8b684883321f77e577f3a [formerly 6b7d74329802cf1649e2472813357cbd26877f74]
    Former-commit-id: c53c902f24d3b5e8b028dafb6eecb4a3856abcb6

--- community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.geoserver.http.util.KvpUtils;
 import org.geoserver.ows.http.KvpParser;
 import org.geotools.filter.FilterFactory;
+import org.geotools.filter.v1_0.OGCPropertyNameTypeBinding;
+import org.opengis.filter.expression.PropertyName;
 
 public class PropertyNameKvpReader extends KvpParser {
 
@@ -22,7 +24,17 @@ public class PropertyNameKvpReader extends KvpParser {
 			List l2 = (List) l1.get( i );
 			for ( int j = 0; j < l2.size(); j++ ) {
 				String name = (String) l2.get( j );
-				l2.set( j, filterFactory.property( name ) );
+				
+				//strip off namespace prefix.
+		        //Our feature model does not suppor tthe notion of namespace for attributes
+		        // and there is a lot of code that expects this to be a non-qualified name.
+		        // Also, the PropertyName interface doesn't support a namespace
+				if ( name.indexOf( ":" ) != -1 ) {
+					name = name.substring( name.indexOf( ":" ) + 1 );
+				}
+				
+				PropertyName propertyName = filterFactory.property( name ); 
+				l2.set( j, propertyName );
 			}
 		}
 		
