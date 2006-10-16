895df518dc5daadeff5c458d06cb95fdbe647802
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-10-16 17:14:59 +0000

    added base class for qname kvp parsers
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@5281 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fc099a9c457e75746506cbb6d8d759fd0dc6f646 [formerly c04cf8acddbf8be89468b45cddc94fb4ee28594d]
    Former-commit-id: 8aa3886f5e70a3a91c48e8d66abb263502207415

--- community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
@@ -1,44 +1,17 @@
 package org.geoserver.wfs.http.kvp;
 
-import java.util.List;
+import org.geoserver.data.GeoServerCatalog;
 
-import org.geoserver.http.util.KvpUtils;
-import org.geoserver.ows.http.KvpParser;
-import org.geotools.filter.FilterFactory;
-import org.geotools.filter.v1_0.OGCPropertyNameTypeBinding;
-import org.opengis.filter.expression.PropertyName;
+/**
+ * Parses propertyName=... into a list of qualified names.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public class PropertyNameKvpReader extends QNameKvpParser {
 
-public class PropertyNameKvpReader extends KvpParser {
-
-	FilterFactory filterFactory;
-	
-	public PropertyNameKvpReader( FilterFactory filterFactory ) {
-		super( "propertyname", List.class );
-		this.filterFactory = filterFactory;
-	}
-	
-	public Object parse( String value ) throws Exception {
-		List l1 =  KvpUtils.readNested( value );
+	public PropertyNameKvpReader( GeoServerCatalog catalog ) {
+		super( "propertyName", catalog );
 		
-		for ( int i = 0; i < l1.size(); i++ ) {
-			List l2 = (List) l1.get( i );
-			for ( int j = 0; j < l2.size(); j++ ) {
-				String name = (String) l2.get( j );
-				
-				//strip off namespace prefix.
-		        //Our feature model does not suppor tthe notion of namespace for attributes
-		        // and there is a lot of code that expects this to be a non-qualified name.
-		        // Also, the PropertyName interface doesn't support a namespace
-				if ( name.indexOf( ":" ) != -1 ) {
-					name = name.substring( name.indexOf( ":" ) + 1 );
-				}
-				
-				PropertyName propertyName = filterFactory.property( name ); 
-				l2.set( j, propertyName );
-			}
-		}
-		
-		return l1;
 	}
-
 }
--- community/wfs/src/main/java/org/geoserver/wfs/http/kvp/TypeNameKvpReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/kvp/TypeNameKvpReader.java
@@ -1,77 +1,18 @@
 package org.geoserver.wfs.http.kvp;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.xml.namespace.QName;
-
 import org.geoserver.data.GeoServerCatalog;
-import org.geoserver.http.util.KvpUtils;
-import org.geoserver.ows.http.KvpParser;
 
 /**
- * Parses a kvp value of hte form 'typeName=', into a {@link javax.xml.namespace.QName}..
+ * Parses typeName=... into a list of qualified names.
  * 
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  *
  */
-public class TypeNameKvpReader extends KvpParser {
+public class TypeNameKvpReader extends QNameKvpParser {
 
-	/**
-	 * catalog for namespace lookups.
-	 */
-	GeoServerCatalog catalog;
-	
 	public TypeNameKvpReader( GeoServerCatalog catalog ) {
-		super( "typeName", List.class );
-		this.catalog = catalog;
+		super( "typeName", catalog);
+		
 	}
 
-	/**
-	 * Parses the string representation of type names into a list of {@link QName} 
-	 * objects.
-	 * 
-	 */
-	public Object parse( String value ) throws Exception {
-	
-		List values = KvpUtils.readFlat( value, "," );
-		List qNames = new ArrayList( values.size() );
-		
-		for ( Iterator v = values.iterator(); v.hasNext(); ) {
-			value = (String) v.next();
-			qNames.add( qName( value ) );
-		}
-		
-		return qNames;
-	}
-	
-	/**
-	 * Parses a single string representation of a type name, ( <prefix>:<local>, or <local> )
-	 * into a {@link QName }.
-	 * <p>
-	 * If the latter form is supplied the QName is given the default namespace as specified in the 
-	 * catalog.
-	 * </p>
-	 * @param value
-	 * @return
-	 */
-	public QName qName( String value ) {
-		int i = value.indexOf( ':' );
-		if ( i != -1 ) {
-			String prefix = value.substring( 0, i );
-			String local = value.substring( i + 1 );
-			
-			String uri = catalog.getNamespaceSupport().getURI( prefix );
-			
-			return new QName( uri, local, prefix );
-		}
-		else {
-			String uri = catalog.getNamespaceSupport().getURI( "" );
-			String prefix = catalog.getNamespaceSupport().getPrefix( uri );
-			String local = value;
-			
-			return  new QName( uri, local, prefix );
-		}
-	}
 }
