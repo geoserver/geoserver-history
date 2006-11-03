865ec10189e12084c301f49af2618f768186fbc9
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-03 07:01:40 +0000

    testing with validation
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5439 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 533fb0fb669ecbdbb9609a73b282e1e5742ef4d3 [formerly 459575aa9116901545f599078a4bfe7dc5f95b99]
    Former-commit-id: c4f07b6ba2aea03bb00341e377eedb321cdb3307

--- community/wfs/src/test/java/org/geoserver/wfs/v1_1_0/http/GetCapabilitiesHttpTest.java
+++ community/wfs/src/test/java/org/geoserver/wfs/v1_1_0/http/GetCapabilitiesHttpTest.java
@@ -1,6 +1,7 @@
 package org.geoserver.wfs.v1_1_0.http;
 
 import org.geoserver.http.GeoServerHttpTestSupport;
+import org.geoserver.wfs.xml.v1_0_0.WFS;
 import org.w3c.dom.Document;
 
 import com.meterware.httpunit.WebResponse;
@@ -12,7 +13,7 @@ public class GetCapabilitiesHttpTest extends GeoServerHttpTestSupport {
 			return;
 		
 	    WebResponse response = get( "wfs?service=WFS&request=getCapabilities&version=1.1.0" );
-		Document doc = response.getDOM();
+		Document doc = validate( response, WFS.NAMESPACE, "http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" );
 		assertEquals( "WFS_Capabilities", doc.getDocumentElement().getNodeName() );
 		assertEquals( "1.1.0", doc.getDocumentElement().getAttribute( "version" ) );
 	}
