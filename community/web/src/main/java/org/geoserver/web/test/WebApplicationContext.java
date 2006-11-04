9193b34c27847d28760c06b0eb94aea66b05b2b2
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-04 01:58:43 +0000

    added test application context
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5458 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cc3c113de85be0c4ea71b51ddac8bdcb539b6ad1 [formerly 638aa2946ab2c172720c97141c981a7dd9919ecd]
    Former-commit-id: d54450521050af2de515d22cedc03b226639d500

---
+++ community/web/src/main/java/org/geoserver/web/test/WebApplicationContext.java
@@ -0,0 +1,22 @@
+package org.geoserver.web.test;
+
+/**
+ * Provides a test application context containing bean definitions of web support 
+ * classes. 
+ * <p>
+ * <pre>
+ * &lt;beans>
+ *
+ *	  &lt;!-- GeoServer data directory initializer -->
+ *	  &lt;bean id="dataDirectoryInitializer" class="org.geoserver.web.GeoServerDataDirectoryInitializer"/>
+ *	
+ *  &lt;/beans>
+ * </pre>
+ * </p>
+ * 
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class WebApplicationContext {
+
+}
