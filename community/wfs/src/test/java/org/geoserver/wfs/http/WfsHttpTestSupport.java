e776d2824570bd1e58b13d31ef7f6c991f0e8a61
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-05 04:55:37 +0000

    added wfs test support class
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5475 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5636e6bdaebcd3840eec1d111508d9f6b24ca420 [formerly 8020b3304d18cc165187ab48e97ddd622d5d6bfb]
    Former-commit-id: 089410dd2e3260b78a9524ae8691d168cf5a935d

---
+++ community/wfs/src/test/java/org/geoserver/wfs/http/WfsHttpTestSupport.java
@@ -0,0 +1,25 @@
+package org.geoserver.wfs.http;
+
+import org.geoserver.data.test.DataApplicationContext;
+import org.geoserver.http.test.GeoServerHttpTestSupport;
+import org.geoserver.web.test.WebApplicationContext;
+import org.geoserver.wfs.test.WfsApplicationContext;
+
+/**
+ * Test support class for WFS.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class WfsHttpTestSupport extends GeoServerHttpTestSupport {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		//load up some bean definitions
+		getGeoServer().loadBeanDefinitions( DataApplicationContext.getBeanDefinitions() );
+		getGeoServer().loadBeanDefinitions( WebApplicationContext.getBeanDefinitions() );
+		getGeoServer().loadBeanDefinitions( WfsApplicationContext.getBeanDefinitions() );
+	}
+	
+}
