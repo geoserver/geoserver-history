0cf8c0acef8d6aabff72832a73698f05d2564f0c
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-14 04:43:18 +0000

    overrides for envelope
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5534 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c387dc5881894dcc0c39ba11a31299391e6a99c7 [formerly 3a86c85aa4f293de48f5f6e130f1e89fff6c9094]
    Former-commit-id: 69c5a64708b21450f785455cc6c0cd8462a6fa1c

--- community/wfs/src/main/java/org/geoserver/wfs/xml/GML3FeatureTransformer.java
+++ community/wfs/src/main/java/org/geoserver/wfs/xml/GML3FeatureTransformer.java
@@ -1,7 +1,10 @@
 package org.geoserver.wfs.xml;
 
+import java.util.Set;
+
 import org.geotools.feature.Feature;
 import org.geotools.gml.producer.FeatureTransformer;
+import org.geotools.gml.producer.GeometryTransformer.GeometryTranslator;
 import org.geotools.gml3.bindings.GML;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
@@ -16,6 +19,10 @@ public class GML3FeatureTransformer extends FeatureTransformer {
 		return new GML3FeatureTranslator( handler, prefix, ns, featureTypeNamespaces, schemaLocationSupport );
 	}
 	
+	protected void loadGmlAttributes(Set set) {
+		set.add( "name" );
+		set.add( "description" );
+	}
 	
 	public static class GML3FeatureTranslator extends FeatureTranslator {
 
@@ -26,6 +33,20 @@ public class GML3FeatureTransformer extends FeatureTransformer {
 			super( handler, prefix, ns, featureTypeNamespaces, schemaLocationSupport ); 
 		}
 		
+		protected GeometryTranslator createGeometryTranslator(ContentHandler handler) {
+			return new GML3GeometryTranslator( handler );
+		}
+		
+		protected GeometryTranslator createGeometryTranslator(ContentHandler handler, int numDecimals) {
+			return new GML3GeometryTranslator(handler, numDecimals);
+		}
+		
+		protected GeometryTranslator createGeometryTranslator(ContentHandler handler, int numDecimals, boolean useDummyZ) {
+			return new GML3GeometryTranslator(handler, numDecimals, useDummyZ);
+		}
+		
+		
+		
 		protected Attributes encodeFeatureId( Feature f ) {
 			
 			AttributesImpl atts = new AttributesImpl();
@@ -35,6 +56,11 @@ public class GML3FeatureTransformer extends FeatureTransformer {
 			
 			return atts;
 		} 
+		
+		protected String boxElement() {
+			return "Envelope";
+		}
+		
 		
 	}
 }
