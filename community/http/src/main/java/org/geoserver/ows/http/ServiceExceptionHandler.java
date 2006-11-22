ba8769e26d5cf9314e0e3cca08d4b1ac00019b61
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-22 08:17:07 +0000

    made exception handler take service instead of operation
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5566 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d6a0cfa4c9ffd8f5a194d124f1695488cb99a6dd [formerly 81628a87db2024998ca4f51246f9f34ec6bff182]
    Former-commit-id: 652928d241bd6fab2384c0bf2c94b9ff158e632c

--- community/http/src/main/java/org/geoserver/ows/http/ServiceExceptionHandler.java
+++ community/http/src/main/java/org/geoserver/ows/http/ServiceExceptionHandler.java
@@ -1,11 +1,9 @@
 package org.geoserver.ows.http;
 
-import java.io.OutputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.geoserver.ows.Operation;
 import org.geoserver.ows.Service;
 import org.geoserver.ows.ServiceException;
 
@@ -37,11 +35,11 @@ public abstract class ServiceExceptionHandler {
 	 * Handles the service exception.
 	 * 
 	 * @param exception The service exception.
-	 * @param operation The operation that generated the exception.
+	 * @param service The service that generated the exception
 	 * @param response The response to report the exception to.
 	 */
 	public abstract void handleServiceException( 
-		ServiceException exception, Operation operation, HttpServletResponse response
+		ServiceException exception, Service service, HttpServletResponse response
 	);
 	
 }
