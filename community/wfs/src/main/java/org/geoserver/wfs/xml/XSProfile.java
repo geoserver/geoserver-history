c4b0fbf439f59dc738c601edd399ccb56ccf04d7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-22 08:26:26 +0000

    formatting
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5574 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 39bf09484dd6bb088e8cc1b07cf620ab7ff761aa [formerly c3259c4cee3dbc5ddb153660e1fdd3d7c58f3539]
    Former-commit-id: a2801cf281cadbd241e0ab625666b2b30d398aff

--- community/wfs/src/main/java/org/geoserver/wfs/xml/XSProfile.java
+++ community/wfs/src/main/java/org/geoserver/wfs/xml/XSProfile.java
@@ -3,6 +3,8 @@ package org.geoserver.wfs.xml;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.xml.namespace.QName;
+
 import org.geotools.feature.Name;
 import org.geotools.xs.XSSchema;
 import org.geotools.xs.bindings.XS;
@@ -15,16 +17,24 @@ public class XSProfile extends TypeMappingProfile {
 
 	static Set profile = new HashSet();
 	static {
-		profile.add( new Name(  XS.NAMESPACE, "byte" ) );
-		profile.add( new Name(  XS.NAMESPACE, "short" ) );
-		profile.add( new Name(  XS.NAMESPACE, "int" ) );
-		profile.add( new Name(  XS.NAMESPACE, "float" ) );
-		profile.add( new Name(  XS.NAMESPACE, "long" ) );
-		profile.add( new Name(  XS.NAMESPACE, "QName" ) );
-		profile.add( new Name(  XS.NAMESPACE, "date" ) );
-		profile.add( new Name(  XS.NAMESPACE, "boolean" ) );
-		profile.add( new Name(  XS.NAMESPACE, "double" ) );
-		profile.add( new Name(  XS.NAMESPACE, "string" ) );
+		profile.add( name(  XS.BYTE ) ); 	//Byte.class
+		profile.add( name(  XS.SHORT ) );	//Short.class
+		profile.add( name(  XS.INT ) );  	//Integer.class
+		profile.add( name(  XS.FLOAT ) );	//Float.class
+		profile.add( name(  XS.LONG ) ); 	//Long.class
+		profile.add( name(  XS.QNAME ) );	//Qname.class
+		profile.add( name(  XS.DATE ) );	//Date.class
+		profile.add( name(  XS.DATETIME ) );//Calendar.class
+		profile.add( name(  XS.BOOLEAN) );	//Boolean.class
+		profile.add( name(  XS.DOUBLE ) );	//Double.class
+		profile.add( name(  XS.STRING ) );	//String.class
+		profile.add( name(  XS.INTEGER ) );	//BigInteger.class
+		profile.add( name(  XS.DECIMAL ) );	//BigDecimal.class
+		profile.add( name(  XS.ANYURI ) );  //URI.class
+	}
+	
+	static Name name( QName qName ) {
+		return new Name( qName.getNamespaceURI(), qName.getLocalPart() );
 	}
 	
 	public XSProfile() {
