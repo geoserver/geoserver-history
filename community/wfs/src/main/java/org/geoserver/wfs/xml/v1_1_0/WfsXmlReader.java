13d3bc7df55b830f09b2c66ffec3e12443e12247
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-28 21:31:52 +0000

    setting the character encoding
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5626 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3a5effbf9ca04f355f902a3660d1af552a97e03c [formerly e02e6b10b520f035f14393a2ec821b5d2a89f10e]
    Former-commit-id: 4599c86d1b6c67694b9122568852cb17cc4369b1

--- community/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/WfsXmlReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/WfsXmlReader.java
@@ -4,23 +4,39 @@ import java.io.InputStream;
 
 import org.geoserver.data.GeoServerCatalog;
 import org.geoserver.ows.http.XmlRequestReader;
+import org.geoserver.wfs.WFS;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.Parser;
+import org.xml.sax.InputSource;
 
 public class WfsXmlReader extends XmlRequestReader  {
 
+	/**
+	 * WFs configuration
+	 */
+	WFS wfs;
+	
+	/**
+	 * Catalog
+	 */
 	GeoServerCatalog catalog;
 	
-	public WfsXmlReader( String element, GeoServerCatalog catalog ) {
-		super( WFS.NAMESPACE, element, "1.1.0" );
+	public WfsXmlReader( String element, WFS wfs, GeoServerCatalog catalog ) {
+		super( org.geoserver.wfs.xml.v1_1_0.WFS.NAMESPACE, element, "1.1.0" );
+		this.wfs = wfs;
 		this.catalog = catalog;
 	}
 
 	public Object read(InputStream input) throws Exception {
 		Configuration configuration = new WFSConfiguration( catalog ); 
 	
-		Parser parser = new Parser( configuration, input );
-		return parser.parse();
+		Parser parser = new Parser( configuration );
+		
+		//set the input source with the correct encoding
+		InputSource source = new InputSource( input );
+		source.setEncoding( wfs.getCharSet().name() );
+		
+		return parser.parse( source );
 	}
 
 }
