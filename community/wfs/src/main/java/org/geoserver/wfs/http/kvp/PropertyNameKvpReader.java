b512934b76e24357297e36a8e7a3bbead9518b21
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-28 21:28:49 +0000

    parsing as strings, not qname
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5622 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 664be0d385d725d70bda51890da4b852473a067d [formerly b71005d4efa4370c5d88f2c8225dac02e9b954a1]
    Former-commit-id: 38d6f4f33b59d10570c5bead214c9e29270edecf

--- community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/kvp/PropertyNameKvpReader.java
@@ -1,6 +1,10 @@
 package org.geoserver.wfs.http.kvp;
 
+import java.util.List;
+
 import org.geoserver.data.GeoServerCatalog;
+import org.geoserver.http.util.KvpUtils;
+import org.geoserver.ows.http.KvpParser;
 
 /**
  * Parses propertyName=... into a list of qualified names.
@@ -8,10 +12,14 @@ import org.geoserver.data.GeoServerCatalog;
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  *
  */
-public class PropertyNameKvpReader extends QNameKvpParser {
+public class PropertyNameKvpReader extends KvpParser {
 
-	public PropertyNameKvpReader( GeoServerCatalog catalog ) {
-		super( "propertyName", catalog );
-		
+	public PropertyNameKvpReader() {
+		super( "propertyName", List.class );
 	}
+	
+	public Object parse(String value) throws Exception {
+		return KvpUtils.readNested( value );
+	}
+	
 }
