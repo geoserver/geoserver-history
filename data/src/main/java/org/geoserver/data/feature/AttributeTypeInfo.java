c040be5e092ed2a9e63bbda53753967e541a8e0e
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-11-28 21:17:47 +0000

    made reflecting of underlying feature type
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5616 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3d3263d7af6ba994d7f2e75323e383fe8a62ec64 [formerly c66bedeb6926781a8e6bb4385679955f0ac52e04]
    Former-commit-id: 2d558ad97be286035876a246f08079823852367a

--- data/src/main/java/org/geoserver/data/feature/AttributeTypeInfo.java
+++ data/src/main/java/org/geoserver/data/feature/AttributeTypeInfo.java
@@ -25,26 +25,17 @@ import org.geotools.feature.AttributeType;
  * @version $Id: AttributeTypeInfo.java,v 1.12 2004/06/26 19:51:24 jive Exp $
  */
 public class AttributeTypeInfo {
-	private String name;
-	private int minOccurs;
-	private int maxOccurs;
-	private boolean nillable;
-	private String typeName;
-	private boolean isComplex;
-
-    /** Readl GeoTools2 AttributeType */
+	
+	/** Real GeoTools2 AttributeType */
     private AttributeType type;
     private Map meta;
 
     public AttributeTypeInfo(AttributeType type) {
         this.type = type;
+        
         meta = new HashMap();
     }
     
-    public String getName(){
-    		return name;
-    }
-
     /**
      * @return The underlying attribute type.
      *
@@ -52,6 +43,13 @@ public class AttributeTypeInfo {
      */
     public AttributeType getAttributeType() {
         return type;
+    }
+
+    /**
+     * @return The name of the attribute type
+     */
+    public String getName(){
+		return type.getName();
     }
 
     /**
@@ -63,69 +61,6 @@ public class AttributeTypeInfo {
         this.type = type;
     }
 
-    /**
-     * Access AttributeName
-     *
-     * @return
-     *
-     * @see org.geotools.data.AttributeTypeMetaData#getAttributeName()
-     */
-    public String getAttributeName() {
-    		String r = typeName;
-
-        if (r==null && type != null) {
-            r = type.getName();
-        }
-
-        return r;
-    }
-
-    /**
-     * Element type, a well-known gml or xs type or <code>TYPE_FRAGMENT</code>.
-     * 
-     * <p>
-     * If getType is equals to <code>TYPE_FRAGMENT</code> please consult
-     * getFragment() to examine the actual user's definition.
-     * </p>
-     * 
-     * <p>
-     * Other than that getType should be one of the constants defined by
-     * GMLUtils.
-     * </p>
-     *
-     * @return The element, or <code>TYPE_FRAGMENT</code>
-     */    
-    public String getType(){
-        if( isComplex ){
-            return "(xml fragment)";
-        }
-        else {
-            return typeName;
-        }        
-    }
-    
-    /**
-     * XML Fragment used to define stuff.
-     * 
-     * <p>
-     * This property is only used with getType() is equals to "(xml fragment)".
-     * </p>
-     * 
-     * <p>
-     * baseGMLTypes can only be used in your XML fragment.
-     * </p>
-     *
-     * @param fragment The fragment to set.
-     */    
-    public String getFragment(){
-        if( isComplex ){
-            return typeName;
-        }
-        else {
-            return null;
-        }
-    }
-    
     /**
      * Implement containsMetaData.
      *
@@ -171,7 +106,7 @@ public class AttributeTypeInfo {
 	 * @return Returns the maxOccurs.
 	 */
 	public int getMaxOccurs() {
-		return maxOccurs;
+		return type.getMaxOccurs();
 	}
 
 	/**
@@ -180,7 +115,7 @@ public class AttributeTypeInfo {
 	 * @return Returns the minOccurs.
 	 */
 	public int getMinOccurs() {
-		return minOccurs;
+		return type.getMinOccurs();
 	}
 
 	/**
@@ -189,6 +124,6 @@ public class AttributeTypeInfo {
 	 * @return Returns the nillable.
 	 */
 	public boolean isNillable() {
-		return nillable;
+		return type.isNillable();
 	}
 }
