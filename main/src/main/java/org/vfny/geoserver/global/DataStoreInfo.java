c98da4f28e36812efccee96ecf70c95c662acba5
bowens <bowens@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-01 21:19:03 +0000

    removed requirement for shapefiles(data files) to be under the data/ sub-directory of the dara directory. It was searching for everything under data/, but really it should allow any location in the data directory
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.4.x@5637 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7881301d4d725974dc593b49b74cb61b072963c2 [formerly 616335b94e431f09bd9c2ad83fd0f4e0f29dfce3]
    Former-commit-id: 095f16c6a38584dc6b747f43d95e979dfee9853a

--- main/src/main/java/org/vfny/geoserver/global/DataStoreInfo.java
+++ main/src/main/java/org/vfny/geoserver/global/DataStoreInfo.java
@@ -178,7 +178,7 @@ public class DataStoreInfo extends GlobalLayerSupertype implements Service {
                 if (key != null && key.matches(".* *url") && value instanceof String) {
                     String path = (String) value;
                     LOGGER.finer("in string url");
-                    if (path.startsWith("file:data/")) {
+                    if (path.startsWith("file:")) {
                         path = path.substring(5); // remove 'file:' prefix
 
                         File file = new File(baseDir, path);
@@ -189,14 +189,14 @@ public class DataStoreInfo extends GlobalLayerSupertype implements Service {
 		    //Not sure about this
                 } else if (value instanceof URL
                         && ((URL) value).getProtocol().equals("file")) {
-		    LOGGER.finer("in URL url");
+                	LOGGER.finer("in URL url");
                     URL url = (URL) value;
                     String path = url.getPath();
-		    LOGGER.finer("path is " + path);
-		    if (path.startsWith("data/")){
-			File file = new File(baseDir, path);
-			entry.setValue(file.toURL());
-		    }
+				    LOGGER.finer("path is " + path);
+				    //if (path.startsWith("data/")){
+				    	File file = new File(baseDir, path);
+				    	entry.setValue(file.toURL());
+				    //}
                 } /*else if ("dbtype".equals(key) && value instanceof String) {
                     String val = (String) value;
 
