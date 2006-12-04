d1ab52705a07229e2b331dcc4715e7cae05e860f
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-04 21:38:50 +0000

    added some utility methods
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5650 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 15e321db66a4c502a8df60529834e2bc674bec97 [formerly f9fa7347ec3055306eca405f428a4d77f5855574]
    Former-commit-id: 0d1978595754e3966a26685bc3ada129f88e4fdd

--- community/http/src/main/java/org/geoserver/ows/http/OWSUtils.java
+++ community/http/src/main/java/org/geoserver/ows/http/OWSUtils.java
@@ -1,5 +1,6 @@
 package org.geoserver.ows.http;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -32,6 +33,59 @@ public class OWSUtils {
 				) {
 					return method;
 				}
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns a getter method for a property of java bean.
+	 * 
+	 * @param clazz The type of the bean.
+	 * @param property The property name.
+	 * @param type The type of the property, may be null.
+	 * 
+	 * @return The setter method, or <code>null</code> if not found.
+	 */
+	public static Method getter( Class clazz, String property, Class type ) {
+		//TODO: this lookup doesn't deal with classes that might have two setters
+		// that are in the same class hierachy
+		Method[] methods = clazz.getMethods();
+		for ( int i = 0; i < methods.length; i++) {
+			Method method = methods[ i ];
+			if ( method.getName().equalsIgnoreCase( "get" + property ) ) {
+				if ( type != null ) {
+					if ( type.equals( method.getReturnType() ) ) {
+						return method;
+					}	
+				}
+				else {
+					return method;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Reflectivley retreives a propety from a java bean.
+	 * 
+	 * @param object The java bean.
+	 * @param property The property to retreive.
+	 * @param type Teh type of the property to retreive.
+	 * 
+	 * @return The property, or null if it could not be found..
+	 */
+	public static Object property( Object object, String property, Class type ) {
+		Method getter = getter( object.getClass(), property, type );
+		if ( getter != null ) {
+			try {
+				return getter.invoke( object, null );
+			}
+			catch( Exception e ) {
+				//TODO: log this
 			}
 		}
 		
