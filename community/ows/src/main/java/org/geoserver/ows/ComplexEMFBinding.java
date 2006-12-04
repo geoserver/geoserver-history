ce15fff3abb46acc6de525ab1a66ec2bea5f5610
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-04 21:38:00 +0000

    added an instanceof check for an EObjecT
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5649 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d1f14115ecacdc33611e022026af87b51fc5162b [formerly 3a0bf0096761c9486a697b5f19ae257465ab4256]
    Former-commit-id: bf2bf481607e4453fec16d2826ee3465b4310cbb

--- community/ows/src/main/java/org/geoserver/ows/ComplexEMFBinding.java
+++ community/ows/src/main/java/org/geoserver/ows/ComplexEMFBinding.java
@@ -9,9 +9,11 @@ import org.geotools.xml.AbstractComplexBinding;
 public abstract class ComplexEMFBinding extends AbstractComplexBinding {
 
 	public Object getProperty(Object object, QName name) throws Exception {
-		EObject eObject = (EObject) object;
-		if ( EMFUtils.has( eObject, name.getLocalPart() ) ) {
-			return EMFUtils.get( eObject, name.getLocalPart() );
+		if ( object instanceof EObject ) {
+			EObject eObject = (EObject) object;
+			if ( EMFUtils.has( eObject, name.getLocalPart() ) ) {
+				return EMFUtils.get( eObject, name.getLocalPart() );
+			}	
 		}
 		
 		return null;
