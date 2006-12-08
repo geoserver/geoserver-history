803d977b869f967cf98a8303693cc2fc5900a607
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-08 21:06:08 +0000

    added some custom bindings
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5679 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2979379c7d0619332f0dc23885d143b1c948b0f7 [formerly 6c39d0830740ca238c4707d81a3179a508f3631f]
    Former-commit-id: e790fbe13c762b754793cbb06c9f16f338065f8a

--- community/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/WFSConfiguration.java
+++ community/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/WFSConfiguration.java
@@ -6,11 +6,16 @@ import org.eclipse.xsd.util.XSDSchemaLocationResolver;
 import org.geoserver.data.GeoServerCatalog;
 import org.geoserver.wfs.xml.FeatureTypeSchema;
 import org.geoserver.wfs.xml.WFSHandlerFactory;
+import org.geoserver.wfs.xml.filter.v1_1.PropertyNameTypeBinding;
+import org.geoserver.wfs.xml.gml3.CircleTypeBinding;
 import org.geoserver.xml.ows.v1_0_0.OWSConfiguration;
+import org.geotools.filter.v1_1.OGC;
 import org.geotools.filter.v1_1.OGCConfiguration;
 import org.geotools.gml3.GMLConfiguration;
+import org.geotools.gml3.bindings.GML;
 import org.geotools.xml.BindingConfiguration;
 import org.geotools.xml.Configuration;
+import org.geotools.xml.Parser;
 import org.picocontainer.MutablePicoContainer;
 
 public class WFSConfiguration extends Configuration {
@@ -52,4 +57,10 @@ public class WFSConfiguration extends Configuration {
 		context.registerComponentInstance( new WFSHandlerFactory( catalog, FeatureTypeSchema.GML3.class ) );
 	}
 
+	protected void configureBindings(MutablePicoContainer container) {
+		//register our custom bindings
+		//container.registerComponentImplementation( OGC.PROPERTYNAMETYPE, PropertyNameTypeBinding.class );
+		container.registerComponentImplementation( GML.CircleType, CircleTypeBinding.class );
+	}
+	
 }
