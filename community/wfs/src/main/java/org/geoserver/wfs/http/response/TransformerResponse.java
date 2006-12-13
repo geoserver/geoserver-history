be7a67253c07a09664607e4b222b9016048f1bcd
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-13 00:27:23 +0000

    checking accepted formats to determine mime type
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5690 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f6fea6f763eb82498382f159948bb08d883191ae [formerly 371b1517599992659fa0d0ebe8c6203e4681661f]
    Former-commit-id: f9429a00e689802a7ca9cede8a78c014bc24259f

--- community/wfs/src/main/java/org/geoserver/wfs/http/response/TransformerResponse.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/response/TransformerResponse.java
@@ -2,10 +2,15 @@ package org.geoserver.wfs.http.response;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.xml.transform.TransformerException;
 
+import net.opengis.wfs.GetCapabilitiesType;
+
 import org.geoserver.ows.Operation;
+import org.geoserver.ows.http.OWSUtils;
 import org.geoserver.ows.http.Response;
 import org.geotools.xml.transform.TransformerBase;
 
@@ -16,6 +21,21 @@ public class TransformerResponse extends Response {
 	}
 
 	public String getMimeType(Operation operation) {
+		GetCapabilitiesType request = 
+			(GetCapabilitiesType) OWSUtils.parameter( operation.getParameters(), GetCapabilitiesType.class );
+		
+		if ( request != null && request.getAcceptFormats() != null ) {
+			//look for an accepted format
+			List formats = request.getAcceptFormats().getOutputFormat();
+			for ( Iterator f = formats.iterator(); f.hasNext(); ) {
+				String format = (String) f.next();
+				if ( format.endsWith( "/xml" ) ) {
+					return format;
+				}
+			}
+		}
+		
+		//default
 		return "application/xml";
 	}
 	
