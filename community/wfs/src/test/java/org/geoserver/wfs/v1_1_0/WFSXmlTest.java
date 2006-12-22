977400b888fa4205cc7094405f5cc868311a2f76
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2006-12-22 17:08:53 +0000

    added test for invalid / valid requests
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5741 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3450c4e5c7c034dc254c0d249375ec63826a579a [formerly 5127132fc131e9e731d9df332b12a43f8146d6ca]
    Former-commit-id: 1e448519190d8026d77272bf4909ad1677d4ce1a

--- community/wfs/src/test/java/org/geoserver/wfs/v1_1_0/WFSXmlTest.java
+++ community/wfs/src/test/java/org/geoserver/wfs/v1_1_0/WFSXmlTest.java
@@ -7,20 +7,18 @@ import org.geoserver.data.GeoServerCatalog;
 import org.geoserver.data.GeoServerResolveAdapterFactoryFinder;
 import org.geoserver.wfs.xml.v1_1_0.WFS;
 import org.geoserver.wfs.xml.v1_1_0.WFSConfiguration;
-import org.geotools.catalog.adaptable.ResolveAdapterFactoryFinder;
 import org.geotools.xml.Configuration;
+import org.geotools.xml.Parser;
 import org.geotools.xml.test.XMLTestSupport;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class WFSXmlTest extends XMLTestSupport {
 
+	GeoServerCatalog catalog = new DefaultGeoServerCatalog( 
+		new GeoServerResolveAdapterFactoryFinder()
+	);
 	
 	protected Configuration createConfiguration() {
-		GeoServerCatalog catalog = new DefaultGeoServerCatalog( 
-			new GeoServerResolveAdapterFactoryFinder()
-		);
-		
 		return new WFSConfiguration( catalog );
 	}
 	
@@ -34,6 +32,20 @@ public class WFSXmlTest extends XMLTestSupport {
 			
 		GetCapabilitiesType getCapabilities = (GetCapabilitiesType) parse();
 		assertNotNull( getCapabilities );
+	}
+	
+	public void testValid() throws Exception {
+		Parser parser = new Parser( new WFSConfiguration( catalog ) );
+		parser.parse( getClass().getResourceAsStream( "GetFeature.xml" ) );
+		
+		assertEquals( 0, parser.getValidationErrors().size() );
+	}
+	
+	public void testInvalid() throws Exception {
+		Parser parser = new Parser( new WFSConfiguration( catalog ) );
+		parser.parse( getClass().getResourceAsStream( "GetFeature-invalid.xml" ) );
+		
+		assertTrue( parser.getValidationErrors().size() > 0 );
 	}
 
 }
