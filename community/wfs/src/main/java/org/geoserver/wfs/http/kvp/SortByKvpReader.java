c967feb739fe0ef06818c02c08981d906a3ee89a
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-01-17 19:47:53 +0000

    reading sortBy parameter as nested
    git-svn-id: http://svn.codehaus.org/geoserver/branches/ows4@5809 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cdec6f752e394be5658d899f90ab6f16437eba00 [formerly ea3deb0f7869ef044a749c2e2f4f7bc3ebb289ec]
    Former-commit-id: ab8f6e0a25c3adcd87e87656f876f1be9809e0a0

--- community/wfs/src/main/java/org/geoserver/wfs/http/kvp/SortByKvpReader.java
+++ community/wfs/src/main/java/org/geoserver/wfs/http/kvp/SortByKvpReader.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.geoserver.http.util.KvpUtils;
 import org.geoserver.ows.http.KvpParser;
 import org.geotools.filter.FilterFactory;
+import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.sort.SortOrder;
 
 /**
@@ -26,22 +27,30 @@ public class SortByKvpReader extends KvpParser {
 	}
 	
 	public Object parse( String value ) throws Exception {
-		List values = KvpUtils.readFlat( value );
+		List values = KvpUtils.readNested( value );
 		List sortBy = new ArrayList( values.size() );
 		
-		for ( Iterator i = values.iterator();  i.hasNext(); ) {
-			String s = (String) i.next();
-			String[] nameOrder = s.split( " " );
-			String propertyName = nameOrder[0];
-			SortOrder order = SortOrder.ASCENDING;
+		for ( Iterator j = values.iterator();  j.hasNext(); ) {
+			List/*<String>*/ l1 = (List) j.next();
+			List/*<SortBy>*/ l2 = new ArrayList();
 			
-			if ( nameOrder.length > 1 ) {
-				if ( "D".equalsIgnoreCase( nameOrder[1] ) ) {
-					order = SortOrder.DESCENDING;
+			for ( Iterator i = l1.iterator(); i.hasNext(); ) {
+				String s = (String) i.next();
+				String[] nameOrder = s.split( " " );
+				String propertyName = nameOrder[0];
+				SortOrder order = SortOrder.ASCENDING;
+				
+				if ( nameOrder.length > 1 ) {
+					if ( "D".equalsIgnoreCase( nameOrder[1] ) ) {
+						order = SortOrder.DESCENDING;
+					}
 				}
+				
+				SortBy sort = filterFactory.sort( propertyName, order );
+				l2.add( sort );
 			}
-		
-			sortBy.add( filterFactory.sort( propertyName, order ) );
+			
+			sortBy.add( l2 );
 		}
 		
 		return sortBy;
