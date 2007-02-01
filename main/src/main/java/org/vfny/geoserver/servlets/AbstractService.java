51637f63d152e952b8fbd14e2ec8fb9a4e57aea9
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-02-01 20:07:27 +0000

    added method to get at the response object
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5937 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 46743339b131b70ae7abe3ccdb8822eec8e77455 [formerly e32673136809878a95e916d7f46efddfd3f3b11a]
    Former-commit-id: 4939005f49cc8b3a1940437fb160e989ac0692e9

--- main/src/main/java/org/vfny/geoserver/servlets/AbstractService.java
+++ main/src/main/java/org/vfny/geoserver/servlets/AbstractService.java
@@ -672,6 +672,17 @@ public abstract class AbstractService extends HttpServlet implements Application
     protected Response getResponseHandler() {
         return null;
     }
+    
+    /**
+     * This method was added in order to adapt the old style servlet services
+     * to the new ows dispatching interface, without having to modify the 
+     * services themselves.
+     * 
+     * @return A call to {@link #getResponseHandler()}.
+     */
+    public final Response getResponse() {
+    	return getResponseHandler();
+    }
 
     /**
      * Gets a reader that will figure out the correct Key Vaule Pairs for this
