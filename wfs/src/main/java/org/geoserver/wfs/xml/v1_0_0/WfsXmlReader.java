6e6b669f21bb9ee2ecdff6f6e2b95480a36fd9cb
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-02-01 20:02:14 +0000

    refactored to take an xml configuration directly
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5929 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d4738abdc1d255dd1709b18acfa195e9fe666438 [formerly 94fa395d65588612d4cb2dacb53423e2b1da5df5]
    Former-commit-id: 51ffb0b18d98a1cabe39f79a74fbdbfe653236b8

--- wfs/src/main/java/org/geoserver/wfs/xml/v1_0_0/WfsXmlReader.java
+++ wfs/src/main/java/org/geoserver/wfs/xml/v1_0_0/WfsXmlReader.java
@@ -4,31 +4,27 @@
  */
 package org.geoserver.wfs.xml.v1_0_0;
 
-import org.geoserver.ows.XmlRequestReader;
-import org.geoserver.wfs.xml.FeatureTypeSchemaBuilder;
-import org.geotools.util.Version;
-import org.geotools.xml.Configuration;
-import org.geotools.xml.Parser;
-import org.vfny.geoserver.global.Data;
 import java.io.InputStream;
+
 import javax.xml.namespace.QName;
+
+import org.geoserver.ows.XmlRequestReader;
+import org.geotools.util.Version;
+import org.geotools.xml.Parser;
 
 
 public class WfsXmlReader extends XmlRequestReader {
-    Data catalog;
-    FeatureTypeSchemaBuilder schemaBuilder;
-
-    public WfsXmlReader(String element, Data catalog, FeatureTypeSchemaBuilder schemaBuilder) {
+    /**
+     * Xml Configuration
+     */
+    WFSConfiguration configuration;
+    
+    public WfsXmlReader(String element, WFSConfiguration configuration) {
         super(new QName(WFS.NAMESPACE, element), new Version("1.0.0"));
-        this.catalog = catalog;
-        this.schemaBuilder = schemaBuilder;
     }
 
     public Object read(InputStream input) throws Exception {
-        Configuration configuration = new WFSConfiguration(catalog, schemaBuilder);
-
         Parser parser = new Parser(configuration);
-
         return parser.parse(input);
     }
 }
