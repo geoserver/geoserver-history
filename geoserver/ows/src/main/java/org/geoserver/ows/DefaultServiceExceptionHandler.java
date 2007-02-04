c9b45a1c9d08a83e306f35f30ad55c7f25d1abb6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-02-04 18:20:20 +0000

    javadocs
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@5988 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7dd4319d9a88f88714010a81295b974104438405 [formerly f07567c0f75ce76ca2235ea51abf73362db2ff7d]
    Former-commit-id: 3080b2a8685e73ddb1459c9d51e54e759e0239dc

--- geoserver/ows/src/main/java/org/geoserver/ows/DefaultServiceExceptionHandler.java
+++ geoserver/ows/src/main/java/org/geoserver/ows/DefaultServiceExceptionHandler.java
@@ -21,7 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /**
- * A default implementation of {@link ServiceExceptionHandler}.
+ * A default implementation of {@link ServiceExceptionHandler} which outputs 
+ * as service exception in a <code>ows:ExceptionReport></code> document.
  * <p>
  * This service exception handler will generate an OWS exception report,
  * see {@linkplain http://schemas.opengis.net/ows/1.0.0/owsExceptionReport.xsd}.
