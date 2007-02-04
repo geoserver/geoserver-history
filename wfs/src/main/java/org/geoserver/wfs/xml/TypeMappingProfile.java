ea6b1c6d5bec20f763ca6853bc9b925aa8817841
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-02-04 18:26:35 +0000

    checkign type hieracthy when looking up by class
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@5998 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e5ca616a2d1935e9e515270a02d8d1ec03c3eab6 [formerly 41f9302abbdde4a909473c69d42554615d595960]
    Former-commit-id: af6da53a4936f23bafa83647ff0e0c8a90d3a377

--- wfs/src/main/java/org/geoserver/wfs/xml/TypeMappingProfile.java
+++ wfs/src/main/java/org/geoserver/wfs/xml/TypeMappingProfile.java
@@ -8,6 +8,10 @@ import org.geotools.feature.AttributeType;
 import org.geotools.feature.type.ProfileImpl;
 import org.opengis.feature.type.Name;
 import org.opengis.feature.type.Schema;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -27,19 +31,62 @@ public class TypeMappingProfile extends ProfileImpl {
 
     /**
      * Obtains the {@link AttributeType} mapped to a particular class.
-     *
+     * <p>
+     * If an exact match cannot be made, then those types which are supertypes 
+     * of clazz are examined.
+     * </p>
      * @param clazz The class.
      *
      * @return The AttributeType, or <code>null</code> if no atttribute
      * type mapped to <code>clazz</code>
      */
     public AttributeType type(Class clazz) {
+    	ArrayList assignable = new ArrayList();
+    	
         for (Iterator i = values().iterator(); i.hasNext();) {
             AttributeType type = (AttributeType) i.next();
 
+            if ( type.getType().isAssignableFrom( clazz ) ) {
+            	assignable.add( type ); 
+            }
+            
             if (clazz.equals(type.getType())) {
                 return type;
             }
+        }
+        
+        if ( assignable.isEmpty() ) {
+        	return null;
+        }
+        
+        if ( assignable.size() == 1 ) {
+        	return (AttributeType) assignable.get( 0 );
+        }
+        else {
+        	//sort
+        	Comparator comparator = new Comparator() {
+
+				public int compare(Object o1, Object o2) {
+					AttributeType a1 = (AttributeType) o1;
+					AttributeType a2 = (AttributeType) o2;
+					
+					if ( a1.getType().equals( a2.getType() ) ) {
+						return 0;
+					}
+					
+					if ( a1.getType().isAssignableFrom( a2.getType() ) ) {
+						return 1;
+					}
+					
+					return -1;
+				}
+        		
+        	};
+        	
+        	Collections.sort( assignable, comparator );
+        	if ( !assignable.get( 0 ).equals( assignable.get( 1 ) ) ) {
+        		return (AttributeType) assignable.get( 0 );
+        	}
         }
 
         return null;
@@ -55,13 +102,62 @@ public class TypeMappingProfile extends ProfileImpl {
      * to <code>clazz</code>
      */
     public Name name(Class clazz) {
+    	ArrayList assignable = new ArrayList();
+    	
         for (Iterator i = entrySet().iterator(); i.hasNext();) {
             Map.Entry entry = (Map.Entry) i.next();
             AttributeType type = (AttributeType) entry.getValue();
 
+            if ( type.getType().isAssignableFrom( clazz ) ) {
+            	assignable.add( entry ); 
+            }
+            
             if (clazz.equals(type.getType())) {
                 return (Name) entry.getKey();
             }
+        }
+
+        if ( assignable.isEmpty() ) {
+        	return null;
+        }
+        
+        if ( assignable.size() == 1 ) {
+        	return (Name) ( (Map.Entry) assignable.get( 0 ) ).getKey();
+        }
+        else {
+        	//sort
+        	Comparator comparator = new Comparator() {
+
+				public int compare(Object o1, Object o2) {
+					Map.Entry e1 = (Map.Entry) o1;
+					Map.Entry e2 = (Map.Entry) o2;
+					
+					AttributeType a1 = (AttributeType) e1.getValue();
+					AttributeType a2 = (AttributeType) e2.getValue();
+					
+					if ( a1.getType().equals( a2.getType() ) ) {
+						return 0;
+					}
+					
+					if ( a1.getType().isAssignableFrom( a2.getType() ) ) {
+						return 1;
+					}
+					
+					return -1;
+				}
+        		
+        	};
+        	
+        	Collections.sort( assignable, comparator );
+        	
+        	Map.Entry e1 = (Map.Entry) assignable.get( 0 );
+        	Map.Entry e2 = (Map.Entry) assignable.get( 1 );
+        	AttributeType a1 = (AttributeType) e1.getValue();
+			AttributeType a2 = (AttributeType) e2.getValue();
+			
+        	if ( !a1.getType().equals( a2.getType() )) {
+        		return (Name) e1.getKey();
+        	}
         }
 
         return null;
