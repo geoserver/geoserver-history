04b88eaeee67188846dc55140507a2c436e0cd5c
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-02-07 03:43:26 +0000

    changed xml reader api to take a Reader, not an INputStream
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@6054 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: af9b6420cb8069fe343977f69f80eaba7fb0d75f [formerly ed20b05c7ade5a3aed3661c2baffccd6b174e638]
    Former-commit-id: 2bc593552cce11d2234f98b870b12bc029059fc7

--- main/src/main/java/org/geoserver/ows/adapters/XmlRequestReaderAdapter.java
+++ main/src/main/java/org/geoserver/ows/adapters/XmlRequestReaderAdapter.java
@@ -4,16 +4,15 @@
  */
 package org.geoserver.ows.adapters;
 
+import java.io.Reader;
+import java.lang.reflect.Constructor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.namespace.QName;
+
 import org.geoserver.ows.HttpServletRequestAware;
 import org.vfny.geoserver.servlets.AbstractService;
 import org.vfny.geoserver.util.requests.readers.XmlRequestReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.Constructor;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.namespace.QName;
 
 
 public class XmlRequestReaderAdapter extends org.geoserver.ows.XmlRequestReader
@@ -35,15 +34,13 @@ public class XmlRequestReaderAdapter extends org.geoserver.ows.XmlRequestReader
         this.request = request;
     }
 
-    public Object read(InputStream input) throws Exception {
+    public Object read(Reader reader) throws Exception {
         //create an instance of the delegate
         Constructor c = delegateClass.getConstructor(new Class[] { AbstractService.class });
         XmlRequestReader delegate = (XmlRequestReader) c.newInstance(new Object[] { service });
 
         //TODO: simulate the behaviour of the old dispatcher, xml encoding 
         // madness
-        Reader reader = new InputStreamReader(input);
-
         return delegate.read(reader, request);
     }
 }
