9bf3ffc43edd232ca55e0538f28c965fcc727fcf
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-03-14 15:39:10 +0000

    made template loader handler templates relative to class
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@6301 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e675970bf27c594fca6eeec83b5dd9c7cfde43c1 [formerly af1088c3fadd55129cbc7ad2bdb9e754688293d2]
    Former-commit-id: 3edee3e44b3bd038a8857c7a7d7bfb1f1f6cd3d2

--- main/src/main/java/org/geoserver/template/GeoServerTemplateLoader.java
+++ main/src/main/java/org/geoserver/template/GeoServerTemplateLoader.java
@@ -1,12 +1,17 @@
 package org.geoserver.template;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.logging.Logger;
 
+import org.geotools.feature.FeatureType;
 import org.vfny.geoserver.global.ConfigurationException;
 import org.vfny.geoserver.global.GeoserverDataDirectory;
 
+import freemarker.cache.ClassTemplateLoader;
 import freemarker.cache.FileTemplateLoader;
 import freemarker.cache.TemplateLoader;
 import freemarker.template.Configuration;
@@ -31,11 +36,14 @@ import freemarker.template.Configuration;
  * In {@link #findTemplateSource(String)}, the following lookup heuristic is 
  * applied to locate a file based on the given path.
  * <ol>
- * 	<li>The path directly
- * 	<li>The path relative to '<data_dir>/templates'
- *  <li>The path relative to '<data_dir>/featureTypes'
- *  <li>The path relative each directory under '<data_dir>/featureTypes'
+ *  <li>The path relative to '<data_dir>/featureTypes/[featureType]' 
+ *  	given that a feature ( {@link #setFeatureType(String)} ) has been set
+ *  <li>The path relative to '<data_dir>/featureTypes'	
+ *  <li>The path relative to '<data_dir>/templates'
+ *  <li>The path relative to the calling class with {@link Class#getResource(String)}.
  * </ol>
+ * <b>Note:</b> If method 5 succeeds, the resulting template will be copied to 
+ * the 'templates' directory of the data directory.
  * </p>
  *
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
@@ -43,83 +51,192 @@ import freemarker.template.Configuration;
  */
 public class GeoServerTemplateLoader implements TemplateLoader {
 
+	/** logger */
+	static Logger LOGGER = Logger.getLogger( "org.geoserver" );
+	
 	/**
 	 * Delegate file based template loader
 	 */
-	FileTemplateLoader loader;
-	
-	public GeoServerTemplateLoader() throws IOException {
+	FileTemplateLoader fileTemplateLoader;
+	/**
+	 * Delegate class based template loader, may be null depending on how 
+	 */
+	ClassTemplateLoader classTemplateLoader;
+	/**
+	 * Feature type directory to load template against
+	 */
+	String featureType;
+	/**
+	 * Constructs the template loader.
+	 * 
+	 * @param caller The "calling" class, used to look up templates based with
+	 * {@link Class#getResource(String)}, may be <code>null</code>
+	 * 
+	 * @throws IOException
+	 */
+	public GeoServerTemplateLoader( Class caller ) throws IOException {
 		//create a file template loader to delegate to
-		loader = new FileTemplateLoader();
+		fileTemplateLoader = 
+			new FileTemplateLoader( GeoserverDataDirectory.getGeoserverDataDirectory() );
+		
+		//create a class template loader to delegate to
+		if ( caller != null ) {
+			classTemplateLoader = new ClassTemplateLoader( caller, "" );
+		}
+	}
+	
+	/**
+	 * Sets the feature type in which templates are loaded against.
+	 * <p>
+	 * 
+	 * </p>
+	 * @param featureType
+	 */
+	public void setFeatureType(String featureType) {
+		this.featureType = featureType;
 	}
 	
 	public Object findTemplateSource(String path) throws IOException {
-		//first try the template directly
-		File template = (File) loader.findTemplateSource( path );
+		
+		File template = null;
+		
+		//first check relative to set feature type
+		if ( featureType != null ) {
+			template = (File) fileTemplateLoader.findTemplateSource(
+				"featureTypes" + File.separator + featureType + 
+					File.separator + path 
+			);
+			if ( template != null ) {
+				return template;
+			}
+		}
+		
+		//next, try relative to feature types
+		template = (File) fileTemplateLoader.findTemplateSource( 
+			"featureTypes" + File.separator + path
+		);
+		if( template != null ) {
+			return template;
+		}
+	
+		//next, check the templates directory
+		
+		template = (File) fileTemplateLoader.findTemplateSource( 
+			"templates" + File.separator + path
+		);
 		if ( template != null ) {
 			return template;
 		}
 		
-		//next, check the templates directory
-		try {
-			File templates = GeoserverDataDirectory.findConfigFile( "templates" );
-			if ( templates.exists() && templates.isDirectory() ) {
-				template = (File) loader.findTemplateSource( 
-					new File( templates, path ).getAbsolutePath()
-				);
+		//final effort to use a class resource
+		if ( classTemplateLoader != null ) {
+			Object source = classTemplateLoader.findTemplateSource( path );
+			//wrap the source in a source that maintains the orignial path
+			if ( source != null ) {
+				return new ClassTemplateSource( path, source );
 			}
-			if ( template != null ) {
-				return template;
-			}
-		} 
-		catch (ConfigurationException e) {
-			throw (IOException) new IOException().initCause( e );
-		}
-		
-		//next, try relative to feature types
-		try {
-			File featureTypes = 
-				GeoserverDataDirectory.findConfigFile( "featureTypes" );
-			if ( featureTypes.exists() && featureTypes.isDirectory() ) {
-				template = (File) loader.findTemplateSource( 
-					new File( featureTypes, path ).getAbsolutePath()	
-				);
-				if( template != null ) {
-					return template;
-				}
-			}
-			
-			//lastly, try looking for each feature type
-			File[] dirs = featureTypes.listFiles();
-			for ( int i = 0; i < dirs.length; i++ ) {
-				if ( dirs[ i ].isDirectory() ) {
-					template = (File) loader.findTemplateSource(
-						new File( dirs[ i ], path ).getAbsolutePath()	
-					);
-					if ( template != null ) {
-						return null;
-					}
-				}
-			}
-			
-		}
-		catch(ConfigurationException e ) {
-			throw (IOException) new IOException().initCause( e );
 		}
 		
 		return null;
 	}
 
 	public long getLastModified(Object source) {
-		return loader.getLastModified( source );
+		if ( source instanceof File ) {
+			//loaded from file
+			return fileTemplateLoader.getLastModified( source );	
+		}
+		else {
+			//loaded from class
+			ClassTemplateSource wrapper = (ClassTemplateSource) source;
+			return classTemplateLoader.getLastModified( wrapper.source );
+		}
+		
 	}
 
 	public Reader getReader(Object source, String encoding) throws IOException {
-		return loader.getReader( source, encoding );
+		if ( source instanceof File ) {
+			//loaded from file
+			return fileTemplateLoader.getReader( source, encoding );	
+		}
+		else {
+			//loaded from class, in this case we want to copy the template to 
+			// the data directory, this will allow users to modify "default"
+			// templates
+			ClassTemplateSource wrapper = (ClassTemplateSource) source;
+			
+			// get teh resource for the raw source
+			Reader reader = classTemplateLoader.getReader( wrapper.source, encoding );
+			
+			//create the target filem, this file shojld already exist because
+			// the template would have been loaded as a file if it did
+			try {
+				File templateFile = new File( 
+					GeoserverDataDirectory.findConfigFile( "templates" ), wrapper.path
+				);
+				
+				//TODO: we might wanto use an output stream writer to preserve 
+				// the file encoding
+				BufferedWriter writer = new BufferedWriter( new FileWriter( templateFile ) );
+				int c = -1;
+				while( ( c = reader.read() ) != -1 ) {
+					writer.write( c );
+				}
+				
+				writer.flush();
+				writer.close();
+			} 
+			catch (Exception e) {
+				LOGGER.warning( "Unable to copy template: '" + wrapper.path + "' to data directory." );
+				throw (IOException) new IOException(). initCause( e );
+			}
+			
+			return classTemplateLoader.getReader( wrapper.source, encoding );
+			
+		}
+		
 	}
 	
 	public void closeTemplateSource(Object source) throws IOException {
-		loader.closeTemplateSource( source );
+		if( source instanceof File ) {
+			fileTemplateLoader.closeTemplateSource( source );	
+		}
+		else {
+			ClassTemplateSource wrapper = (ClassTemplateSource) source;
+			
+			//close the raw source
+			classTemplateLoader.closeTemplateSource( wrapper.source );
+			
+			//cleanup
+			wrapper.path = null;
+			wrapper.source = null;
+		}
+		
 	}
 
+	/**
+	 * Template source for use when a template is loaded from a class.
+	 * <p>
+	 * Used to store the intial path so the template can be copied to the data 
+	 * directory.
+	 * </p>
+	 * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+	 *
+	 */
+	static class ClassTemplateSource {
+		
+		/**
+		 * The path used to load the template.
+		 */
+		String path;
+		
+		/**
+		 * The raw source from the class template loader
+		 */
+		Object source;
+		
+		public ClassTemplateSource( String path, Object source ) {
+			this.path = path;
+			this.source = source;
+		}
+	}
 }
