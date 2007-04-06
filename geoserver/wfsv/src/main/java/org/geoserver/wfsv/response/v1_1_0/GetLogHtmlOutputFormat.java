0b585a7528b4ed1905b5a3e93f37d704b11f4cc7
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-06 14:41:19 +0000

    Added HTML output for GetLog
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6455 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 99d60c68dd3f8e48b9ea1bc6255f9d43f9a9b95a [formerly 95279102ddd4661ec46a877bb51eb57208c4cb4d]
    Former-commit-id: f6184a71f6d5d4d251addd6f95fc5e0258a40382

---
+++ geoserver/wfsv/src/main/java/org/geoserver/wfsv/response/v1_1_0/GetLogHtmlOutputFormat.java
@@ -0,0 +1,69 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.wfsv.response.v1_1_0;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import net.opengis.wfs.FeatureCollectionType;
+
+import org.geoserver.ows.Response;
+import org.geoserver.platform.Operation;
+import org.geoserver.platform.ServiceException;
+import org.geoserver.template.FeatureWrapper;
+import org.geoserver.template.GeoServerTemplateLoader;
+import org.geotools.feature.FeatureCollection;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+/**
+ * Html, templatized output format for the GetLog response
+ * @author Andrea Aime, TOPP
+ *
+ */
+public class GetLogHtmlOutputFormat extends Response {
+    private static Configuration templateConfig;
+
+    static {
+        //initialize the template engine, this is static to maintain a cache 
+        // over instantiations of kml writer
+        templateConfig = new Configuration();
+        templateConfig.setObjectWrapper(new FeatureWrapper());
+    }
+
+    public GetLogHtmlOutputFormat() {
+        super(FeatureCollectionType.class);
+    }
+
+    public String getMimeType(Object value, Operation operation) throws ServiceException {
+        return "text/html";
+    }
+
+    public void write(Object value, OutputStream output, Operation operation) throws IOException, ServiceException {
+        FeatureCollectionType fct = (FeatureCollectionType) value;
+        FeatureCollection fc = (FeatureCollection) fct.getFeature().get(0);
+
+        // setup template subsystem
+        GeoServerTemplateLoader templateLoader = new GeoServerTemplateLoader(getClass());
+        templateLoader.setFeatureType(fc.getSchema().getTypeName());
+
+        Template template = null;
+        synchronized (templateConfig) {
+            templateConfig.setTemplateLoader(templateLoader);
+            template = templateConfig.getTemplate("wfsvGetLog.ftl");
+        }
+
+        try {
+            template.process(fc, new OutputStreamWriter(output));
+        } catch (TemplateException e) {
+            String msg = "Error occured processing template.";
+            throw (IOException) new IOException(msg).initCause(e);
+        }
+    }
+
+}
