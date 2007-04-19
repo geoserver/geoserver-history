e3898ce14c40fa2cd1f90ca439b61f5c8a18e983
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-19 18:23:48 +0000

    Jalopy reformats
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@6544 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 72dfef7d59055a927ec419c2d63af0d45dd0085f [formerly 91192b595ae4dc38d7a578d6331c8e35fbd11df0]
    Former-commit-id: d0b2b7ec1457ece17c0f36078ec333d604d30cbd

--- main/src/test/java/org/geoserver/test/GeoServerTestApplicationContext.java
+++ main/src/test/java/org/geoserver/test/GeoServerTestApplicationContext.java
@@ -1,28 +1,31 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 package org.geoserver.test;
 
-import javax.servlet.ServletContext;
-
+import com.mockrunner.mock.web.MockServletContext;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.ui.context.Theme;
 import org.springframework.web.context.WebApplicationContext;
+import javax.servlet.ServletContext;
 
-import com.mockrunner.mock.web.MockServletContext;
 
 /**
  * A spring application context used for GeoServer testing.
- * 
+ *
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  *
  */
-public class GeoServerTestApplicationContext extends
-        ClassPathXmlApplicationContext implements WebApplicationContext {
-
+public class GeoServerTestApplicationContext extends ClassPathXmlApplicationContext
+    implements WebApplicationContext {
     ServletContext servletContext;
-    
-    public GeoServerTestApplicationContext(String configLocation, ServletContext servletContext ) throws BeansException {
-        super(new String[]{configLocation}, false );
+
+    public GeoServerTestApplicationContext(String configLocation, ServletContext servletContext)
+        throws BeansException {
+        super(new String[] { configLocation }, false);
         this.servletContext = servletContext;
     }
 
@@ -33,5 +36,4 @@ public class GeoServerTestApplicationContext extends
     public Theme getTheme(String themeName) {
         return null;
     }
-
 }
