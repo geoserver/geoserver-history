fcea27793bb577bb322db0c68d07a9a6a7c5b25f
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-19 13:45:38 +0000

    Jalopy reformats
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6525 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 74cc851e20c69791c0a23f5f24f934b63cc62ffe [formerly e9e6c5212e679b8f05013b216cad3bc1857a3d38]
    Former-commit-id: f66ac6a5c8ab09585f639a840adf7c819c7e3210

--- geoserver/ows/src/main/java/org/geoserver/ows/FilePublisher.java
+++ geoserver/ows/src/main/java/org/geoserver/ows/FilePublisher.java
@@ -1,19 +1,21 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 package org.geoserver.ows;
 
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicMatch;
+import org.geoserver.platform.GeoServerResourceLoader;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.jmimemagic.Magic;
-import net.sf.jmimemagic.MagicMatch;
-
-import org.geoserver.platform.GeoServerResourceLoader;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 /**
  * Controller which publishes files through a web interface.
@@ -40,22 +42,22 @@ import org.springframework.web.servlet.mvc.AbstractController;
  *
  */
 public class FilePublisher extends AbstractController {
-
     /**
      * Resource loader
      */
     protected GeoServerResourceLoader loader;
-    
+
     /**
      * Creates the new file publisher.
-     * 
+     *
      * @param loader The loader used to locate files.
      */
-    public FilePublisher( GeoServerResourceLoader loader ) {
+    public FilePublisher(GeoServerResourceLoader loader) {
         this.loader = loader;
     }
-    
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+        HttpServletResponse response) throws Exception {
         String ctxPath = request.getContextPath();
         String reqPath = request.getRequestURI();
         reqPath = reqPath.substring(ctxPath.length());
@@ -66,35 +68,40 @@ public class FilePublisher extends AbstractController {
 
         //load the file
         File file = loader.find(reqPath);
+
         if (file == null) {
             //return a 404
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
             return null;
         }
 
         //figure out the mime type
-        MagicMatch match = Magic.getMagicMatch( file, true );
-        if ( match == null ) {
+        MagicMatch match = Magic.getMagicMatch(file, true);
+
+        if (match == null) {
             //return a 415: Unsupported Media Type
-            response.setStatus( HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE );
+            response.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+
             return null;
         }
+
         response.setContentType(match.getMimeType());
 
         //TODO: should hte encpoding be gotten from the file?
         response.setCharacterEncoding("UTF-8");
 
         //copy teh content to the output
-        byte[] buffer = new byte[ 8192 ];
-        InputStream input = new FileInputStream( file );
+        byte[] buffer = new byte[8192];
+        InputStream input = new FileInputStream(file);
         OutputStream output = response.getOutputStream();
-        
+
         try {
             int n = -1;
-            while( ( n = input.read( buffer ) ) != -1 ) {
-                output.write( buffer, 0, n );
+
+            while ((n = input.read(buffer)) != -1) {
+                output.write(buffer, 0, n);
             }
-            
         } finally {
             output.flush();
             input.close();
@@ -102,6 +109,4 @@ public class FilePublisher extends AbstractController {
 
         return null;
     }
-
-    
 }
