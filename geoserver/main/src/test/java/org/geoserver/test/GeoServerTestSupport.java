94f8b3141cd5f3da4356bb8094ee0750ebf148b7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-20 18:07:52 +0000

    added some convience methods for creating mock requests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6547 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 28743e155ae8d6ac0b9730a803a3ed81c3418c87 [formerly 705c4e3c7e02d5173b99e314d9d8197706c959d7]
    Former-commit-id: 5844b5fc5e3f5b42b01af031e53c401833ed7d0c

--- geoserver/main/src/test/java/org/geoserver/test/GeoServerTestSupport.java
+++ geoserver/main/src/test/java/org/geoserver/test/GeoServerTestSupport.java
@@ -4,15 +4,30 @@
  */
 package org.geoserver.test;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.xml.namespace.QName;
+
 import com.mockrunner.mock.web.MockServletContext;
+
 import junit.framework.TestCase;
 import org.geoserver.data.test.MockData;
+import org.geoserver.ows.util.ResponseUtils;
 import org.geotools.data.FeatureSource;
 import org.vfny.geoserver.global.Data;
 import org.vfny.geoserver.global.GeoServer;
+import org.vfny.geoserver.global.GeoserverDataDirectory;
 import java.io.IOException;
 import javax.xml.namespace.QName;
 
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpSession;
+import com.mockrunner.mock.web.MockServletContext;
+import com.mockrunner.mock.web.MockServletInputStream;
 
 /**
  * Base test class for GeoServer unit tests.
@@ -73,6 +88,7 @@ public class GeoServerTestSupport extends TestCase {
 
         //kill the data directory
         dataDirectory.tearDown();
+        GeoserverDataDirectory.destroy();
     }
 
     /**
@@ -97,5 +113,60 @@ public class GeoServerTestSupport extends TestCase {
     protected FeatureSource getFeatureSource(QName typeName)
         throws IOException {
         return getCatalog().getFeatureSource(typeName.getPrefix(), typeName.getLocalPart());
+    }
+    /**
+     * Convenience method for subclasses to create mock http servlet requests.
+     * <p>
+     * Examples of using this method are:
+     * <pre>
+     * <code>
+     *   createRequest( "wfs?request=GetCapabilities" );  //get
+     *   createRequest( "wfs" ); //post
+     * </code>
+     * </pre>
+     * </p>
+     * @param path The path for the request and optional the query string.
+     * @return
+     */
+    protected MockHttpServletRequest createRequest( String path ) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        
+        request.setScheme( "http" );
+        request.setServerName( "localhost" );
+        request.setContextPath( "/geoserver" );
+        request.setRequestURI( 
+            ResponseUtils.stripQueryString( ResponseUtils.appendPath( "/geoserver/", path ) ) 
+        );
+        request.setQueryString( ResponseUtils.stripQueryString( path ));
+        request.setRemoteAddr( "127.0.0.1" );
+        request.setServletPath( path );
+        kvp( request, path );
+        
+        MockHttpSession session = new MockHttpSession();
+        session.setupServletContext( new MockServletContext() );
+        request.setSession( session );
+        
+        request.setUserPrincipal( null );
+        
+        return request;
+    }
+    
+    /*
+     * Helper method to create the kvp params from the query string.
+     */
+    private void kvp( MockHttpServletRequest request, String path ) {
+        int index = path.indexOf( '?' );
+        if ( index == -1 ) {
+            return;
+        }
+        
+        String queryString = path.substring( index + 1 );
+        StringTokenizer st = new StringTokenizer( queryString, "&" );
+        while( st.hasMoreTokens() ) {
+                String token = st.nextToken();
+                String[] keyValuePair = token.split( "=" );
+                request.setupAddParameter( keyValuePair[ 0 ], keyValuePair[ 1] );
+        }
+        
     }
 }
