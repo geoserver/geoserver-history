4bfde83341cdeff5f43b268e605f445f91a88edf
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-20 18:12:54 +0000

    added kml transformer tests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6553 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ce7bd05dfa8f778c82d3091fdbf57fc8a2d594aa [formerly d63d7c88d628e244026a2b23d8934a64550b7be5]
    Former-commit-id: e8137557da0f4d4b99a6d9e2a9459ed37abaf5c6

---
+++ geoserver/wms/src/test/java/org/geoserver/wms/WMSTestSupport.java
@@ -0,0 +1,101 @@
+package org.geoserver.wms;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.geoserver.test.GeoServerTestSupport;
+import org.geotools.map.DefaultMapLayer;
+import org.geotools.map.MapLayer;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.styling.Style;
+import org.vfny.geoserver.global.ConfigurationException;
+import org.vfny.geoserver.global.FeatureTypeInfo;
+import org.vfny.geoserver.global.MapLayerInfo;
+import org.vfny.geoserver.global.WMS;
+import org.vfny.geoserver.global.dto.CoverageInfoDTO;
+import org.vfny.geoserver.global.dto.FeatureTypeInfoDTO;
+import org.vfny.geoserver.wms.requests.GetMapRequest;
+import org.vfny.geoserver.wms.servlets.GetMap;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+/**
+ * Base support class for wms tests.
+ * <p>
+ * Deriving from this test class provides the test case with preconfigured
+ * geoserver and wms objects.
+ * </p>
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public class WMSTestSupport extends GeoServerTestSupport {
+
+    /**
+     * @return The global wms singleton from the application context.
+     */
+    protected WMS getWMS() {
+        return (WMS) applicationContext.getBean( "wms" );
+    }
+    
+    /**
+     * Convenience method for subclasses to create a map layer from a layer name.
+     * <p>
+     * The map layer is created with the default style for the layer.
+     * </p>
+     * @param layerName The name of the layer.
+     *  
+     * @return A new map layer.
+     */
+    protected MapLayer createFeatureMapLayer( QName layerName ) throws IOException {
+        //TODO: support coverages
+        FeatureTypeInfo info = getCatalog().getFeatureTypeInfo( layerName );
+        Style style = info.getDefaultStyle();
+        
+        return new DefaultMapLayer( info.getFeatureSource(), style );
+    }
+ 
+    /**
+     * Convenience method for subclasses to create a new GetMapRequest object.
+     * <p>
+     * The returned object has the following properties:
+     *  <ul>
+     *    <li>styles set to default styles for layers specified
+     *    <li>bbox set to (-180,-90,180,180 )
+     *    <li>crs set to epsg:4326
+     *  </ul>
+     *  Caller must set additional parameters of request as need be.
+     * </p>
+     * 
+     * @param The layer names of the request.
+     * 
+     * @return A new GetMapRequest object.
+     */
+    protected GetMapRequest createGetMapRequest( QName[] layerNames ) {
+        GetMapRequest request = new GetMapRequest( new GetMap( getWMS() ) ) ;
+        
+        MapLayerInfo[] layers = new MapLayerInfo[ layerNames.length ];
+        List styles = new ArrayList();
+        for ( int i = 0; i < layerNames.length; i++ ) {
+            FeatureTypeInfo ftInfo = getCatalog().getFeatureTypeInfo( layerNames[i] );
+            styles.add( ftInfo.getDefaultStyle() );
+            try {
+                layers[i] =  
+                    new MapLayerInfo( (FeatureTypeInfoDTO) ftInfo.toDTO(), getCatalog() );
+            } 
+            catch (ConfigurationException e) {
+                throw new RuntimeException( e );
+            }
+        }
+        
+        request.setLayers( layers );
+        request.setStyles( styles );
+        request.setBbox( new Envelope( -180,-90,180,90 ) );
+        request.setCrs( DefaultGeographicCRS.WGS84 );
+        
+        return request;
+    }
+}
---
+++ geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/kml/KMLTransformerTest.java
@@ -0,0 +1,147 @@
+package org.vfny.geoserver.wms.responses.map.kml;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.geoserver.data.test.MockData;
+import org.geoserver.wms.WMSTestSupport;
+import org.geotools.data.FeatureSource;
+import org.geotools.map.DefaultMapLayer;
+import org.geotools.map.MapLayer;
+import org.geotools.styling.Style;
+import org.vfny.geoserver.config.FeatureTypeConfig;
+import org.vfny.geoserver.global.Data;
+import org.vfny.geoserver.global.FeatureTypeInfo;
+import org.vfny.geoserver.global.MapLayerInfo;
+import org.vfny.geoserver.global.dto.FeatureTypeInfoDTO;
+import org.vfny.geoserver.wms.WMSMapContext;
+import org.vfny.geoserver.wms.requests.GetMapRequest;
+import org.vfny.geoserver.wms.servlets.GetMap;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.vividsolutions.jts.geom.Envelope;
+
+public class KMLTransformerTest extends WMSTestSupport {
+
+    WMSMapContext mapContext;
+    MapLayer mapLayer;
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        //get some data
+        Data catalog = getCatalog();
+        FeatureSource featureSource = getFeatureSource( MockData.BASIC_POLYGONS );
+        Style style = catalog.getStyle( MockData.BASIC_POLYGONS.getLocalPart() );
+        
+        //create a map context
+        GetMap getMap = new GetMap( getWMS() );
+        GetMapRequest request = new GetMapRequest( getMap );
+        request.setBbox( new Envelope( -180,-90,180,90 ) );
+        
+        FeatureTypeInfo ftInfo = catalog.getFeatureTypeInfo( MockData.BASIC_POLYGONS );
+        
+        MapLayerInfo mapLayerInfo = 
+            new MapLayerInfo( (FeatureTypeInfoDTO) ftInfo.toDTO(), catalog );
+        request.setLayers( new MapLayerInfo[] { mapLayerInfo } );
+        mapContext = new WMSMapContext( request );
+        
+        mapLayer = new DefaultMapLayer( featureSource, style );
+        mapLayer.setTitle( featureSource.getSchema().getTypeName() );
+        mapContext.addLayer( mapLayer );
+        
+        MockHttpServletRequest httpRequest = createRequest( "wms" );
+        request.setHttpServletRequest( httpRequest );
+    }
+    
+    public void testVectorTransformer() throws Exception {
+       
+        KMLVectorTransformer transformer = 
+            new KMLVectorTransformer( mapContext, mapLayer );
+        transformer.setIndentation( 2 );
+        
+        FeatureSource featureSource = mapLayer.getFeatureSource();
+        int nfeatures = featureSource.getFeatures().size();
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        transformer.transform( featureSource.getFeatures(), System.out );
+        transformer.transform( featureSource.getFeatures(), output );
+        
+        DocumentBuilder docBuilder = 
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = 
+            docBuilder.parse( new ByteArrayInputStream( output.toByteArray() ) );
+        
+        Element element = document.getDocumentElement();
+        assertEquals( "Document", element.getNodeName() );
+        assertEquals( nfeatures, element.getElementsByTagName("Document").getLength() );
+    }
+    
+    public void testRasterTransformerInline() throws Exception {
+        KMLRasterTransformer transformer 
+            = new KMLRasterTransformer( mapContext );
+        transformer.setInline( true );
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        transformer.transform( mapLayer, output );
+        
+        DocumentBuilder docBuilder = 
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = 
+            docBuilder.parse( new ByteArrayInputStream( output.toByteArray() ) );
+        
+        assertEquals( "Document", document.getDocumentElement().getNodeName() );
+        
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "Folder" ).getLength() );
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "GroundOverlay" ).getLength() );
+        
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "href" ).getLength() );
+        
+        Element href = (Element) document.getElementsByTagName( "href" ).item( 0 );
+        assertEquals( "layer_0.png", href.getFirstChild().getNodeValue() );
+    }
+    
+    public void testRasterTransformerNotInline() throws Exception {
+        KMLRasterTransformer transformer 
+            = new KMLRasterTransformer( mapContext );
+        transformer.setInline( false );
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        transformer.transform( mapLayer, output );
+        
+        DocumentBuilder docBuilder = 
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = 
+            docBuilder.parse( new ByteArrayInputStream( output.toByteArray() ) );
+        
+        assertEquals( "Document", document.getDocumentElement().getNodeName() );
+        
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "Folder" ).getLength() );
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "GroundOverlay" ).getLength() );
+        
+        assertEquals( mapContext.getLayerCount(), document.getElementsByTagName( "href" ).getLength() );
+        
+        Element href = (Element) document.getElementsByTagName( "href" ).item( 0 );
+        assertTrue( href.getFirstChild().getNodeValue().startsWith( "http://localhost" ) );
+        
+    }
+    
+    public void testTransformer() throws Exception {
+        KMLTransformer transformer = new KMLTransformer();
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        transformer.transform( mapContext, output );
+        
+        DocumentBuilder docBuilder = 
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = 
+            docBuilder.parse( new ByteArrayInputStream( output.toByteArray() ) );
+        
+        assertEquals( "kml", document.getDocumentElement().getNodeName() );
+    }
+}
