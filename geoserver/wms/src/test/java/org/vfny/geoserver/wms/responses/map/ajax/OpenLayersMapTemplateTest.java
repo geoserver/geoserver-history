bf2720d49d9786ff732aaf4f2f87a5d98c8056e3
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-04-21 00:09:15 +0000

    test case for open layers map producer
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6556 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d472d73d12320eb2d5106db06bbb2aa68a075aa5 [formerly b65037021c647b690a30245b723eda1db5b32406]
    Former-commit-id: 1c99aa79f3685edf621eeb83862e3e7b91a2c512

--- geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/ajax/OpenLayersMapTemplateTest.java
+++ geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/ajax/OpenLayersMapTemplateTest.java
@@ -1,19 +1,26 @@
 package org.vfny.geoserver.wms.responses.map.ajax;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
 
-import org.vfny.geoserver.wms.responses.map.openlayers.OpenLayersMapProducer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.TestCase;
+
+import org.geoserver.data.test.MockData;
+import org.geoserver.wms.WMSTestSupport;
+import org.vfny.geoserver.wms.WMSMapContext;
+import org.vfny.geoserver.wms.requests.GetMapRequest;
+import org.vfny.geoserver.wms.responses.map.openlayers.OpenLayersMapProducer;
+import org.w3c.dom.Document;
+
 import freemarker.ext.beans.BeansWrapper;
 import freemarker.template.Configuration;
-import freemarker.template.DefaultObjectWrapper;
-import freemarker.template.ObjectWrapper;
-import freemarker.template.SimpleHash;
 import freemarker.template.Template;
 
-public class OpenLayersMapTemplateTest extends TestCase {
+public class OpenLayersMapTemplateTest extends WMSTestSupport {
 
     public void test() throws Exception {
         Configuration cfg = new Configuration();
@@ -23,6 +30,23 @@ public class OpenLayersMapTemplateTest extends TestCase {
         Template template = cfg.getTemplate("OpenLayersMapTemplate.ftl");
         assertNotNull(template);
         
+        GetMapRequest request = createGetMapRequest( MockData.BASIC_POLYGONS );
+        WMSMapContext mapContext = new WMSMapContext();
+        mapContext.addLayer( createMapLayer( MockData.BASIC_POLYGONS ) );
+        mapContext.setRequest( request );
+        mapContext.setMapWidth( 256 );
+        mapContext.setMapHeight( 256 );
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        template.process( mapContext, new OutputStreamWriter( output ) );
+        
+        DocumentBuilder docBuilder = 
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = 
+            docBuilder.parse( new ByteArrayInputStream( output.toByteArray() ) );
+        assertNotNull( document );
+        
+        assertEquals( "html", document.getDocumentElement().getNodeName() );
     }
     
     
