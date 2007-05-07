611942c4466442906a6cdf9c20967e79e146bc29
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-05-07 17:07:29 +0000

    Fix for GEOS-1073 and finishing touch for GEOS-1074, bye bye MapBuilder
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@6706 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5c922eccf1318efef2369bfb22a6b92bf4e9ec3e [formerly 4b7236c9d5c4ce6e4f066d7ec06f4c5c0d802bbd]
    Former-commit-id: 8941e2338e426127cda64a21ff3f0200a5c79422

--- web/src/main/webapp/mb/lib/model/OwsCatResources.js
+++
@@ -1,8 +0,0 @@
-mapbuilder.loadScript(baseDir+"/model/FeatureCollection.js");
-function OwsCatResources(modelNode,parent){
-FeatureCollection.apply(this,new Array(modelNode,parent));
-this.namespace="xmlns:owscat='http://www.ec.gc.ca/owscat' xmlns:gml='http://www.opengis.net/gml' xmlns:wfs='http://www.opengis.net/wfs'";
-}
-OwsCatResources.prototype.getFeatureNode=function(featureName){
-return this.doc.selectSingleNode(this.nodeSelectXpath+"[owscat:name='"+featureName+"']");
-}
--- web/src/main/webapp/mb/lib/widget/CollectionList.js
+++
@@ -1,4 +0,0 @@
-mapbuilder.loadScript(baseDir+"/widget/WidgetBaseXSL.js");
-function CollectionList(widgetNode,model){
-WidgetBaseXSL.apply(this,new Array(widgetNode,model));
-}
