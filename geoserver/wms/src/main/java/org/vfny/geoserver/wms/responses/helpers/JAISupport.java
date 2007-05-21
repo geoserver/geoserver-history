b12fad19bacbf141c786ca7a02254032b6fe50bc
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-05-21 16:05:01 +0000

    Fix for GEOS-1100, avoid WMS GetCapabilities lockup
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@6829 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4b9496a940b940b9a1e0bf17170d2684d47c5f21 [formerly 217fca226116671e53e53288ad362d72e151a7ca]
    Former-commit-id: 89d9e16204a064d4fca2fbffde97ff1cf88b5411

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/helpers/JAISupport.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/helpers/JAISupport.java
@@ -65,13 +65,9 @@ public final class JAISupport {
      */
     public static Set getSupportedFormats() {
         if (supportedFormats == null) {
-            //LiteRenderer renderer = null;
             String[] mimeTypes = null;
 
-            LiteRenderer2 testRenderer = null;
-
             try {
-                testRenderer = new LiteRenderer2();
                 mimeTypes = ImageIO.getWriterMIMETypes();
             } catch (NoClassDefFoundError ncdfe) {
                 supportedFormats = Collections.EMPTY_SET;
@@ -81,8 +77,8 @@ public final class JAISupport {
                 //delete, or we get really nasty messages on getCaps for wms.
             }
 
-            if ((testRenderer == null) || (mimeTypes == null)) {
-                LOGGER.info("renderer was null, so jai not found");
+            if (mimeTypes == null) {
+                LOGGER.info("Jai not found? Should be always there");
                 supportedFormats = Collections.EMPTY_SET;
             } else {
                 supportedFormats = new HashSet();
