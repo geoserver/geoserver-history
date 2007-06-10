b248e6322857a34cfed26d8f9572203b639eadc7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-06-10 00:14:48 +0000

    added some utility methods for doing getFeatureInfo wms request
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6960 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 91a5e381fd4c6475ea5100db1e1e9400685e9bdd [formerly d16a9b2df38b44417af7a7562ecd0802ca7ae351]
    Former-commit-id: 7f5a569ebb2354c9b8cc1585d7801e9ce375d45c

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/map/kml/KMLUtils.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/map/kml/KMLUtils.java
@@ -8,7 +8,6 @@ import com.vividsolutions.jts.geom.Envelope;
 import org.geotools.map.MapLayer;
 import org.vfny.geoserver.util.Requests;
 import org.vfny.geoserver.wms.WMSMapContext;
-import org.vfny.geoserver.wms.requests.GetMapRequest;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.AttributesImpl;
 import java.util.HashMap;
@@ -20,15 +19,23 @@ import java.util.Map;
  * Some convenience methods used by the kml transformers.
  *
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
- *
+ * TODO: these methods should be moved to a WMSRequestUtils class.
  */
 public class KMLUtils {
     /**
      * Encodes the url of a GetMap request from a map context + map layer.
+     * <p>
+     * If the <tt>mapLayer</tt> argument is <code>null</code>, the request is
+     * made including all layers in the <tt>mapContexT</tt>.
+     * </p>
+     * <p>
+     * If the <tt>bbox</tt> argument is <code>null</code>. {@link WMSMapContext#getAreaOfInterest()}
+     * is used for the bbox parameter.
+     * </p>
      *
      * @param mapContext The map context.
-     * @param mapLayer The Map layer.
-     * @param bbox The bounding box of the request.
+     * @param mapLayer The Map layer, may be <code>null</code>.
+     * @param bbox The bounding box of the request, may be <code>null</code>.
      * @param kvp Additional or overidding kvp parameters, may be <code>null</code>
      * @param tile Flag controlling wether the request should be made against tile cache
      *
@@ -43,13 +50,36 @@ public class KMLUtils {
         map.put("request", "GetMap");
         map.put("version", "1.1.1");
         map.put("format", "image/png");
-        map.put("layers", mapLayer.getTitle());
-        map.put("styles", mapLayer.getStyle().getName());
+        
+        StringBuffer layers = new StringBuffer();
+        StringBuffer styles = new StringBuffer();
+        
+        if ( mapLayer != null ) {
+             layers.append( mapLayer.getTitle() );
+             styles.append( mapLayer.getStyle().getName() );
+        }
+        else {
+            for ( int i = 0; i < mapContext.getLayerCount(); i++ ) {
+                MapLayer layer = (MapLayer) mapContext.getLayer( i );
+                layers.append( layer.getTitle() ).append( "," );
+                styles.append( layer.getStyle().getName() ).append( "," );   
+            }
+            layers.setLength(layers.length()-1);
+            styles.setLength(styles.length()-1);
+        }
+        
+        map.put("layers", layers.toString() );
+        map.put("styles", styles.toString() );
+        
         map.put("height", String.valueOf(mapContext.getMapHeight()));
         map.put("width", String.valueOf(mapContext.getMapWidth()));
 
+        if ( bbox == null ) {
+            bbox = mapContext.getAreaOfInterest();
+        }
+        
         map.put("bbox", encode(bbox));
-
+        
         map.put("srs", "EPSG:4326");
         map.put("transparent", "true");
 
@@ -79,9 +109,12 @@ public class KMLUtils {
 
     /**
      * Encodes the url of a GetMap request from a map context + map layer.
-     *
+     * <p>
+     * If the <tt>mapLayer</tt> argument is <code>null</code>, the request is
+     * made including all layers in the <tt>mapContexT</tt>.
+     * </p>
      * @param mapContext The map context.
-     * @param mapLayer The Map layer.
+     * @param mapLayer The Map layer, may be <code>null</code>
      * @param kvp Additional or overidding kvp parameters, may be <code>null</code>
      * @param tile Flag controlling wether the request should be made against tile cache
      *
@@ -91,6 +124,73 @@ public class KMLUtils {
         return getMapUrl(mapContext, mapLayer, mapContext.getAreaOfInterest(), null, tile);
     }
 
+    /**
+     * Encodes the url of a GetFeatureInfo request from a map context + map layer.
+     * <p>
+     * If the <tt>mapLayer</tt> argument is <code>null</code>, the request is
+     * made including all layers in the <tt>mapContexT</tt>.
+     * </p>
+     * @param mapContext The map context.
+     * @param mapLayer The Map layer, may be <code>null</code>.
+     * @param x the x parameter of the request.
+     * @param y the y parameter of the request.
+     * @param kvp Additional or overidding kvp parameters, may be <code>null</code>
+     * 
+     * @return The full url for a getFeatureInfo request.
+     */
+    public static String getFeatureInfoUrl(WMSMapContext mapContext, MapLayer mapLayer, double x, double y, String[] kvp ) {
+        //parameters
+        HashMap map = new HashMap();
+
+        map.put("service", "wms");
+        map.put("request", "GetFeatureInfo");
+        map.put("version", "1.1.1");
+        map.put("format", "image/png");
+        map.put("info_format", "text/html");
+        map.put( "x", ""+x);
+        map.put( "y", ""+y);
+        map.put( "height", "256" );
+        map.put( "width", "256" );
+        
+        StringBuffer layers = new StringBuffer();
+        StringBuffer styles = new StringBuffer();
+        
+        if ( mapLayer != null ) {
+             layers.append( mapLayer.getTitle() );
+             styles.append( mapLayer.getStyle().getName());
+        }
+        else {
+            for ( int i = 0; i < mapContext.getLayerCount(); i++ ) {
+                MapLayer layer = (MapLayer) mapContext.getLayer( i );
+                layers.append( layer.getTitle() ).append( "," );
+                layers.append( layer.getStyle().getName() ).append( "," );
+            }
+            layers.setLength(layers.length()-1);
+            styles.setLength(styles.length()-1);
+        }
+        
+        map.put("query_layers", layers.toString());
+        map.put( "layers", layers.toString());
+        map.put( "styles", styles.toString());
+        
+        if ( mapLayer != null ) {
+            map.put( "bbox", encode(mapLayer.getBounds()));
+        }
+        else {
+            map.put( "bbox", encode(mapContext.getAreaOfInterest()));
+        }
+        
+        //overrides / additions
+        for ( int i = 0; kvp != null &&  i < kvp.length; i += 2 ) {
+            map.put( kvp[i], kvp[i+1] );
+        }
+        
+        String baseUrl = Requests.getBaseUrl( mapContext.getRequest().getHttpServletRequest(), mapContext.getRequest().getGeoServer() );
+        baseUrl += "wms?";
+        
+        return encode( mapContext, map, baseUrl );
+    }
+    
     /**
      * Encodes the url for a GetLegendGraphic request from a map context + map layer.
      *
