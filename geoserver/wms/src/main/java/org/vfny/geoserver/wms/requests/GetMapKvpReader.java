fe70ee509a1444980575b70f3c702a02bfc5aa4f
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-06-12 10:28:45 +0000

    Fix for GEOS-529, ported WMS filter parameters to trunk
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@6974 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7ba276196ae3dc89cb68fabdd604ff350a02ee72 [formerly dbca1314ed57428f1ede0166c2c6a18421b0b638]
    Former-commit-id: 11ac4b4bbe2bbfc3be4b2bd1602785dcd8527cb3

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
@@ -943,12 +943,8 @@ public class GetMapKvpReader extends WmsKvpRequestReader {
     protected void parseFilterParam(GetMapRequest request)
         throws WmsException {
         String rawFilter = getValue("FILTER");
+        String rawCqlFilter = getValue("CQL_FILTER");
         String rawIdFilter = getValue("FEATUREID");
-
-        if ((rawFilter != null) && (rawIdFilter != null)) {
-            throw new WmsException("GetMap KVP request contained "
-                + "conflicting filters.  Filter: " + rawFilter + ", fid: " + rawFilter);
-        }
 
         // in case of a mixed request, get with sld in post body, layers
         // are not parsed, so we can't parse filters neither...
@@ -972,9 +968,32 @@ public class GetMapKvpReader extends WmsKvpRequestReader {
             } catch (ServiceException e) {
                 throw new WmsException(e);
             }
-        } else if ((rawIdFilter != null) && !rawIdFilter.equals("")) {
-            filters = readFidFilters(rawFilter);
-        } else {
+        }
+
+        if ((rawIdFilter != null) && !rawIdFilter.equals("")) {
+            if (filters != null) {
+                throw new WmsException("GetMap KVP request contained "
+                    + "conflicting filters.  Filter: " + rawFilter + ", fid: " + rawFilter);
+            }
+
+            filters = readFidFilters(rawIdFilter);
+        }
+
+        if ((rawCqlFilter != null) && !rawCqlFilter.equals("")) {
+            if (filters != null) {
+                throw new WmsException("GetMap KVP request contained "
+                    + "conflicting filters.  Filter: " + rawFilter + ", fid: " + rawFilter
+                    + ", cql: " + rawCqlFilter);
+            }
+
+            try {
+                filters = readCQLFilter(rawCqlFilter);
+            } catch (ServiceException e) {
+                throw new WmsException(e);
+            }
+        }
+
+        if (filters == null) {
             return;
         }
 
