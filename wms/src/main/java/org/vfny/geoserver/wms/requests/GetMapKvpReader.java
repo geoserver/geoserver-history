2656f09f0a590b8e52d495eaedfb655ec58fc0af
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-06-14 14:36:01 +0000

    Fix for GEOS-1003, GetMap request are handling versioning now
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@6986 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8aee6eeae14d1ff9e8fa83e12797659127cded13 [formerly 08d8fcbeec5a90c2ac5f9836e68ff94035d0ef56]
    Former-commit-id: fd43a3135cb6a95b1f6191809260c1cdc933d1e2

--- wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
+++ wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
@@ -338,6 +338,11 @@ public class GetMapKvpReader extends WmsKvpRequestReader {
         if (origin != null) {
             request.setTilesOrigin(parseTilesOrigin(origin));
         }
+        
+        // feature version (for versioned requests)
+        String featureVersion = getValue("FEATUREVERSION");
+        request.setFeatureVersion(featureVersion);
+        
 
         /** KML/KMZ score value */
         String KMScore = getValue("KMSCORE");
