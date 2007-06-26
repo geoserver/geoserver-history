4376d6816e80daef2903de065d4649317b059d12
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-06-26 15:52:46 +0000

    added xml reader for sld in body
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7104 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6872ef3590092a337b198768eb11a223b37d2546 [formerly 53e992650f05d7d042fc7d26f16c33636a0956af]
    Former-commit-id: d0dbce6e8bb17e6733a7b23b3b5872f6ee157979

---
+++ geoserver/wms/src/main/java/org/geoserver/wms/xml/SLDXmlRequestReader.java
@@ -0,0 +1,48 @@
+package org.geoserver.wms.xml;
+
+import java.io.Reader;
+
+import javax.xml.namespace.QName;
+
+import org.geoserver.ows.XmlRequestReader;
+import org.geoserver.wms.kvp.GetMapKvpRequestReader;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.styling.SLDParser;
+import org.geotools.styling.StyleFactory;
+import org.geotools.styling.StyledLayerDescriptor;
+import org.vfny.geoserver.wms.requests.GetMapRequest;
+
+/**
+ * Reads 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public class SLDXmlRequestReader extends XmlRequestReader {
+
+    StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory(null);
+    
+    public SLDXmlRequestReader() {
+        super("http://www.opengis.net/sld", "StyledLayerDescriptor" );
+    }
+
+    public void setStyleFactory(StyleFactory styleFactory) {
+        this.styleFactory = styleFactory;
+    }
+    
+   
+    public Object read(Object request, Reader reader) throws Exception {
+        if ( request == null ) {
+            throw new IllegalArgumentException( "request must be not null" );
+        }
+        
+        GetMapRequest getMap = (GetMapRequest) request;
+        StyledLayerDescriptor sld = 
+            new SLDParser( styleFactory, reader ).parseSLD();
+        
+        //process the sld 
+        GetMapKvpRequestReader.processSld(getMap, sld);
+    
+        return getMap;
+    }
+    
+}
