9b12b9b82b82eb0a7938f343539e6b57589ec2f2
amanfredi <amanfredi@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-07-03 21:49:21 +0000

    updated Classes.js, fixed parse error in sldoutline.xml
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7155 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 710a89551293981d3c361a92a72a0c30e9f66bac [formerly 58edd8a927808e966a7132546ffef2ad6f31cd2b]
    Former-commit-id: 99bcc3161d5c3524f53cdf7569e52d98ccb5b46e

--- unsupported/sldeditor/src/Classes.js
+++ unsupported/sldeditor/src/Classes.js
@@ -1,6 +1,7 @@
 // finish adding all classes
 // add Validation methods
-// figure out when to fill in the data/go up the hierarchy
+// add utility methods
+// figure out when to fill in the data and go up the hierarchy
 
 // StyledLayerDescriptor Class 
 function StyledLayerDescriptor() {
@@ -28,14 +29,11 @@ function StyledLayerDescriptor() {
     this.userLayer;
 }
 StyledLayerDescriptor.prototype.toString = function() {
-    var str = '<?xml version="1.0" encoding="UTF-8"?>\n<StyledLayerDescriptor version="1.0.0">\n';
-    str += this.name.toString();
-    str += this.title.toString();
-    str += this.abs.toString();
-    str += this.namedLayer.toString();
-    str += this.userLayer.toString();
-    str += '</StyledLayerDescriptor>';
-    return str;
+    var str = this.name.toString() + this.title.toString() + this.abs.toString() + this.namedLayer.toString() + this.userLayer.toString();
+    if (str.length != 0)
+	return '<?xml version="1.0" encoding="UTF-8"?>\n<StyledLayerDescriptor version="1.0.0">\n' + str + '</StyledLayerDescriptor>\n';
+    else
+	return '';
 }
     
 // Name Class
@@ -44,7 +42,7 @@ StyledLayerDescriptor.prototype.toString = function() {
     }
 Name.prototype.toString = function() {
     if (this.value.length != 0) 
-	return '<Name>' + this.value + '</Name>\n';
+	return '<Name>\n' + this.value + '\n</Name>\n';
     else
 	return '';
 }
@@ -53,9 +51,9 @@ Name.prototype.toString = function() {
     function Title(title) {
 	this.value = title.toString();
     }
-Name.prototype.toString = function() {
+Title.prototype.toString = function() {
     if (this.value.length != 0) 
-	return '<Title>' + this.value + '</Title>\n';
+	return '<Title>\n' + this.value + '\n</Title>\n';
     else
 	return '';
 }
@@ -64,9 +62,20 @@ Name.prototype.toString = function() {
     function Abs(abs) {
 	this.value = abs.toString();
     }
-Name.prototype.toString = function() {
+Abs.prototype.toString = function() {
     if (this.value.length != 0) 
-	return '<Abstract>' + this.value + '</Abstract>\n';
+	return '<Abstract>\n' + this.value + '\n</Abstract>\n';
+    else
+	return '';
+}
+
+// Value Class
+    function Value(value) {
+	this.value = value.toString();
+    }
+Value.prototype.toString = function() {
+    if (this.value.length != 0)
+	return '<Value>\n' + this.value + '\n</Value>\n';
     else
 	return '';
 }
@@ -94,20 +103,72 @@ Name.prototype.toString = function() {
 	
     }
 NamedLayer.prototype.toString = function() {
-    var str = '<NamedLayer>\n';
-    str += this.name.toString();
-    str += this.layerFeatureConstraints.toString();
-    str += this.namedStyle.toString();
-    str += this.userStyle.toString();
-    str += '</NamedLayer>\n';
-    return str;
+    var str = this.name.toString() + this.layerFeatureConstraints.toString() + this.namedStyle.toString() + this.userStyle.toString();
+    if (str.length != 0) 
+	return '<NamedLayer>\n' + str + '</NamedLayer>\n';
+    else 
+	return '';
 }
 
 // UserLayer Class
+    function UserLayer() {
+	// <Name>
+	// optional
+	// name of the UserLayer
+	this.name;
+	
+	// <RemoteOWS>
+	// optional
+	// Specifies an external feature/coverage server
+	this.remoteOWS;
 
+	// <LayerFeatureConstraints>
+	// required
+	this.layerFeatureConstraints;
+	
+	// <UserStyle>
+	// multiple
+	this.userStyle
+    }
+UserLayer.prototype.toString = function() {
+    var str = this.name.toString() + this.remoteOWS.toString() + this.layerFeatureConstraints.toString() + userStyle.toString();
+    if (str.length != 0)
+	return '<UserLayer>\n' + str + '</UserLayer>\n';
+    else 
+	return '';
+}
+    
 // RemoteOWS Class
+    function RemoteOWS() {
+	// <Service>
+	// required
+	// WFS or WCS
+	this.service;
+
+	// <OnlineResource>
+	// required
+	// xlink URI
+	this.onlineResource;
+    }
+RemoteOWS.prototype.toString = function() {
+    var str = this.service.toString() + this.onlineResource.toString();
+    if (str.length != 0) 
+	return '<RemoteOWS>\n' + str + '</RemoteOWS>\n';
+    else
+	return '';
+}
 
 // Service Class
+    function Service(service) {
+	// should be WFS or WCS
+	this.value = service.toString();
+    }
+Service.prototype.toString = function() {
+    if (this.value.length != 0) 
+	return '<Service>\n' + this.value + '\n</Service>\n';
+    else
+	return '';
+}
 
 // LayerFeatureConstraings Class
     function LayerFeatureConstraints() {
@@ -117,7 +178,7 @@ NamedLayer.prototype.toString = function() {
 	this.FeatureTypeConstraint;
     }
 LayerFeatureConstraints.prototype.toString = function() {
-    // ???
+    // ??? TODO
 }
     
 // FeatureTypeConstraint Class
@@ -138,12 +199,11 @@ LayerFeatureConstraints.prototype.toString = function() {
 	this.extent;
     }
 FeatureTypeConstraint.prototype.toString = function() {
-    var str = '<FeatureTypeConstraint>\n';
-    str += this.featureTypeName.toString();
-    str += this.filter.toString();
-    str += this.extent.toString();
-    str += '</FeatureTypeConstraint>\n';
-    return str;
+    var str = this.featureTypeName.toString() + this.filter.toString() + this.extent.toString();
+    if (str.length != 0)
+	return '<FeatureTypeConstraint>\n' + str + '</FeatureTypeConstraint>\n';
+    else
+	return '';
 }
 
 // FeatureTypeName Class
@@ -152,35 +212,241 @@ FeatureTypeConstraint.prototype.toString = function() {
     }
 FeatureTypeName.prototype.toString = function() {
     if (this.value.length != 0) 
-	return '<FeatureTypeName>' + this.value + '</FeatureTypeName>\n';
+	return '<FeatureTypeName>\n' + this.value + '\n</FeatureTypeName>\n';
     else
 	return '';
 }
 
 // Filter Class
+// ??? TODO
 
 // ElseFilter Class
+    function ElseFilter() {
+	// doesn't need to do anything
+    }
+ElseFilter.prototype.toString = function() {
+    return '\n<ElseFilter/>\n';
+}
 
 // Expression Class
+// ??? TODO
 
 // Extent Class
-
-
+    function Extent() {
+	// <Name>
+	// required
+	// name of the extent
+	this.name;
+	
+	// <Value>
+	// required
+	// string data value
+	this.value;
+    }
+Extent.prototype.toString = function () {
+    var str = this.name.toString() + this.value.toString();
+    if (str.length != 0) 
+	return '<Extent>\n' + str + '</Extent>\n';
+    else
+	return '';
+}
 
 // NamedStyle Class
+    function NamedStyle() {
+	// <Name>
+	// required
+	// name of the style
+	this.name;
+    }
+NamedStyle.prototype.toString = function () {
+    var str = this.name.toString();
+    if (str.length != 0)
+	return '<NamedStyle>\n' + str + '</NamedStyle>\n';
+    else 
+	return '';
+}
 
 // UserStyle Class
+    function UserStyle() {
+	// <Name>
+	// optional
+	// name of the UserStyle
+	this.name;
+	
+	// <Title>
+	// optional
+	// title of the UserStyle
+	this.title;
+	
+	// <Abstract>
+	// optional
+	// description of the UserStyle
+	this.abs;
+	
+	// <IsDefault>
+	// optional
+	// is this the default style for the layer?
+	this.isDefault;
+	
+	// <FeatureTypeStyle>
+	// multiple
+	this.featureTypeStyle;
+    }
+UserStyle.prototype.toString = function () {
+    var str = this.name.toString() + this.title.toString() + this.abs.toString() + this.isDefault.toString() + this.featureTypeStyle.toString();
+    if (str.length != 0)
+	return '<UserStyle>\n' + str + '</UserStyle>\n';
+    else
+	return '';
+}
 
 // IsDefault Class
+    function IsDefault(value) {
+	// should be 1 or 0
+	this.value = value.toString();
+    }
+IsDefault.prototype.toString = function () {
+    if (this.value.length != 0)
+	return '<IsDefault>\n' + this.value + '\n</IsDefault>\n';
+    else
+	return '';
+}
 
 // FeatureTypeStyle Class
+    function FeatureTypeStyle() {
+	// <Name>
+	// optional
+	this.name;
+
+	// <Title>
+	// optional
+	this.title;
+
+	// <Abstract>
+	// optional
+	this.abs;
+
+	// <FeatureTypeName>
+	// optional
+	this.featureTypeName;
+
+	// SemanticTypeIdentifier
+	// optional, multiple
+	this.semanticTypeIdentifier;
+
+	// <Rule>
+	// multiple
+	this.rule;
+    }
+FeatureTypeStyle.prototype.toString = function () {
+    var str = this.name.toString() + this.title.toString() + this.abs.toString() + this.featureTypeName.toString() + this.semanticTypeIdentifier.toString() + this.rule.toString();
+    if (str.length != 0)
+	return '<FeatureTypeStyle>\n' + str + '</FeatureTypeStyle>\n';
+    else
+	return '';
+}
 
 // SemanticTypeIdentifier Class
+    function SemanticTypeIdentifier(value) {
+	// undefined string
+	// use these reserved strings to indicate that a FeatureTypeStyle may be used
+	// with any feature type with the corresponding default geometry type
+	// i.e. no feature properties are referenced in the FeatureTypeStyle
+	// generic:line
+	// generic:polygon
+	// generic:point
+	// generic:text
+	// generic:raster
+	// generic:any
+	this.value = value.toString;
+    }
+SemanticTypeIdentifier.prototype.toString = function () {
+    if (this.value.length != 0)
+	return '<SemanticTypeIdentifier>\n' + this.value + '\n</SemanticTypeIdentifier>\n';
+    else
+	return '';
+}
 
 // Rule Class
+    function Rule() {
+	// <Name>
+	// optional
+	// name of the rule
+	this.name;
+	
+	// <Title>
+	// optional
+	// title of the rule
+	this.title;
+	
+	// <Abstract>
+	// optional
+	// description of the rule
+	this.abs;
+	
+	// <LegendGraphic>
+	// optional
+	// defines a graphic symbol to be displayed in the legend for this rule
+	this.legendGrahpic;
+	
+	// may only have 1 filter OR 1 ElseFilter
+	// <Filter>
+	// optional
+	this.filter;
+	
+	// <ElseFilter>
+	// optional
+	this.elseFilter;
+	
+	// <MinScaleDenominator>
+	// optional
+	// defines the minimum map-rendering scale for which the rule should be applied
+	this.minScaleDenominator;
+	
+	// <MaxScaleDenominator>
+	// optional
+	// defines the maximum map-rendering scale for which the rule should be applied
+	this.maxScaleDenominator;
+	
+	// <LineSymbolizer>
+	// optional
+	// used to style a stroke along a linear geometry type
+	this.lineSymbolizer;
+	
+	// <PolygonSymbolizer>
+	// optional
+	// used to draw a polygon or other area-type geometry
+	// including filling its interior and stroking its border (outline)
+	this.polygonSymbolizer;
+	
+	// <PointSymbolizer>
+	// optional
+	// used to draw a graphic at a point
+	this.pointSymbolizer;
+	
+	// <TextSymbolizer>
+	// optional
+	// used to style text labels
+	this.textSymbolizer;
+	
+	// <RasterSymbolizer>
+	// optional
+	// describes how to render raster/matrix-coverage data
+	// e.g. satellite photos, DEMs
+	this.rasterSymbolizer;
+
+    }
+Rule.prototype.toString = function () {
+    var str = this.name.toString() + this.title.toString() + this.abs.tostring() + this.legendGraphic.toString() + this.filter.toString() + this.elseFilter.toString() + this.minScaleDenominator.toString() + this.maxScaleDenominator.toString() + this.lineSymbolizer.toString() + this.polygonSymbolizer.toString() + this.pointSymbolizer.toString() + this.textSymbolizer.toString() + this.rasterSymbolizer.toString();
+    if (str.length != 0)
+	return '<Rule>\n' + str + '</Rule>\n';
+    else
+	return '';
+}
 
 // LegendGraphic Class
     
+
 // Graphic Class
     
 // ExternalGraphic Class
