a5f42a440e5bbb6f9dbaa383f7e612dde23d296d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-07-05 13:15:52 +0000

    Fix for GEOT-1188, unit test for it, and moved code up to new filters
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@7178 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f23519837587dbd02379e881ac42563ab765d743 [formerly 358f67de4a5e8449051edc59f66d1bdb2fc17ea8]
    Former-commit-id: c324770d6b2f09a6f66dc1c85af060d1c3298d3d

---
+++ wms/src/test/java/org/vfny/geoserver/wms/responses/featureinfo/GetFeatureInfoTest.java
@@ -0,0 +1,19 @@
+package org.vfny.geoserver.wms.responses.featureinfo;
+
+import org.geoserver.data.test.MockData;
+import org.geoserver.wms.WMSTestSupport;
+
+public class GetFeatureInfoTest extends WMSTestSupport {
+
+    /**
+     * Tests a simple GetFeatureInfo works, and that the result contains the expected polygon
+     * @throws Exception
+     */
+    public void testSimple() throws Exception {
+        String layer = MockData.FORESTS.getPrefix() + ":" + MockData.FORESTS.getLocalPart();
+        String request = "wms?bbox=-0.002,-0.002,0.002,0.002&styles=&format=jpeg&info_format=text/plain&request=GetFeatureInfo&layers=" + layer + "&query_layers=" + layer + "&width=20&height=20&x=10&y=10";
+        String result = getAsString(request); 
+        assertNotNull(result);
+        assertTrue(result.indexOf("Green Forest") > 0);
+    }
+}
