91ee2bd21223945494b7b9471a153a0ef40c4a8a
saul.farber <saul.farber@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-07-19 00:59:33 +0000

    Fix to make SLD_BODY= getmap requests with raster NamedLayer/NamedStyle pairs work correctly.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.5.x@7275 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1bf96462ed90c61c47b2368e18c1fb6389fc67c0 [formerly 3311ece180974bfac9483dc207fd0bd0e51ea3c0]
    Former-commit-id: c4d47f9e725e692a79f81296e2d919fe43fa7758

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/requests/GetMapKvpReader.java
@@ -1217,7 +1217,18 @@ public class GetMapKvpReader extends WmsKvpRequestReader {
 					currStyle = findStyleOf(request, currLayer.getFeature(),
 							styledLayers);
 				} else if (currLayer.getType() == MapLayerInfo.TYPE_RASTER) {
-					currStyle = findStyle(request, "raster");
+				    try {
+				        currStyle = findStyleOf(request, currLayer.getFeature(),
+	                            styledLayers);
+				    } catch (WmsException wm) {
+				        currStyle = findStyleOf(request, currLayer.getFeature(),
+                                styledLayers);
+                        if (currStyle == null) {
+                            //nope, there's no default raster style.  Give up.
+                            throw new WmsException(wm.getMessage() + "  Also tried to use " +
+                                    "the generic raster style 'raster', but it wasn't available.");
+                        }
+				    }
 				}
 
 				layers.add(currLayer);
@@ -1279,10 +1290,21 @@ public class GetMapKvpReader extends WmsKvpRequestReader {
 					addStyles(request, currLayer, styledLayers[i], layers,
 							styles);
 				} else if (currLayer.getType() == MapLayerInfo.TYPE_RASTER) {
-					currStyle = findStyle(request, "raster");
-
-					layers.add(currLayer);
-					styles.add(currStyle);
+				    try {
+				        addStyles(request, currLayer, styledLayers[i], layers,
+                                styles);
+				    } catch (WmsException wm) {
+				        //hmm, well, the style they specified in the wms request
+				        //wasn't found.  Let's try the default raster style named 'raster'
+				        currStyle = findStyle(request, "raster");
+				        if (currStyle == null) {
+				            //nope, there's no default raster style.  Give up.
+				            throw new WmsException(wm.getMessage() + "  Also tried to use " +
+				            		"the generic raster style 'raster', but it wasn't available.");
+				        }
+				        layers.add(currLayer);
+                        styles.add(currStyle);
+				    }
 				}
 			}
 		}
