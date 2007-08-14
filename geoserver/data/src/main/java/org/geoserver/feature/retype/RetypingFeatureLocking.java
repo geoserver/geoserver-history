938580b52d5e5de825fe95b4e39e170d8af09c0d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-08-14 15:09:52 +0000

    A simple datastore used to rename invalid feature type names
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7429 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 95069c2380d266b7a4459347e280be7d5a99ce2d [formerly 3580a40c654277f912b5cff3affe908516369963]
    Former-commit-id: 2740fb7d937230e183723972e37db5b105e4724d

---
+++ geoserver/data/src/main/java/org/geoserver/feature/retype/RetypingFeatureLocking.java
@@ -0,0 +1,56 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.feature.retype;
+
+import java.io.IOException;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.FeatureLock;
+import org.geotools.data.FeatureLocking;
+import org.geotools.data.Query;
+import org.opengis.filter.Filter;
+
+/**
+ * Renaming wrapper for a {@link FeatureLocking} instance, to be used along with {@link RetypingDataStore} 
+ */
+class RetypingFeatureLocking extends RetypingFeatureStore implements FeatureLocking {
+
+    public RetypingFeatureLocking(DataStore ds, FeatureLocking wrapped, FeatureTypeMap typeMap) {
+        super(ds, wrapped, typeMap);
+    }
+    
+    FeatureLocking featureLocking() {
+        return (FeatureLocking) wrapped;
+    }
+
+    public int lockFeatures() throws IOException {
+        return featureLocking().lockFeatures();
+    }
+
+    public int lockFeatures(Query query) throws IOException {
+        return featureLocking().lockFeatures(retypeQuery(query));
+    }
+
+    public int lockFeatures(Filter filter) throws IOException {
+        return featureLocking().lockFeatures(filter);
+    }
+
+    public void setFeatureLock(FeatureLock lock) {
+        featureLocking().setFeatureLock(lock);
+    }
+
+    public void unLockFeatures() throws IOException {
+        featureLocking().unLockFeatures();
+    }
+
+    public void unLockFeatures(Filter filter) throws IOException {
+        featureLocking().unLockFeatures(filter);
+    }
+
+    public void unLockFeatures(Query query) throws IOException {
+        featureLocking().unLockFeatures(retypeQuery(query));
+    }
+
+}
---
+++ geoserver/data/src/main/java/org/geoserver/feature/retype/RetypingFeatureStore.java
@@ -0,0 +1,64 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.feature.retype;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.geoserver.feature.RetypingFeatureCollection;
+import org.geoserver.feature.RetypingFeatureCollection.RetypingFeatureReader;
+import org.geotools.data.DataStore;
+import org.geotools.data.FeatureReader;
+import org.geotools.data.FeatureStore;
+import org.geotools.data.Transaction;
+import org.geotools.feature.AttributeType;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.filter.Filter;
+
+/**
+ * Renaming wrapper for a {@link FeatureStore} instance, to be used along with {@link RetypingDataStore} 
+ */
+public class RetypingFeatureStore extends RetypingFeatureSource implements FeatureStore {
+
+    public RetypingFeatureStore(DataStore ds, FeatureStore wrapped, FeatureTypeMap typeMap) {
+        super(ds, wrapped, typeMap);
+    }
+
+    protected FeatureStore featureStore() {
+        return (FeatureStore) wrapped;
+    }
+
+    public Transaction getTransaction() {
+        return featureStore().getTransaction();
+    }
+
+    public void setTransaction(Transaction transaction) {
+        featureStore().setTransaction(transaction);
+    }
+
+    public void modifyFeatures(AttributeType type, Object value, Filter filter) throws IOException {
+        featureStore().modifyFeatures(type, value, filter);
+    }
+
+    public void removeFeatures(Filter filter) throws IOException {
+        featureStore().removeFeatures(filter);
+    }
+
+    public void modifyFeatures(AttributeType[] type, Object[] value, Filter filter)
+            throws IOException {
+        featureStore().modifyFeatures(type, value, filter);
+    }
+
+    public void setFeatures(FeatureReader reader) throws IOException {
+        featureStore().setFeatures(
+                new RetypingFeatureReader(reader, typeMap.getOriginalFeatureType()));
+    }
+
+    public Set addFeatures(FeatureCollection collection) throws IOException {
+        return featureStore().addFeatures(
+                new RetypingFeatureCollection(collection, typeMap.originalFeatureType));
+    }
+
+}
