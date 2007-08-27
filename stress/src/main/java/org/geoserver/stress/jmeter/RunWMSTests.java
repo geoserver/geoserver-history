4a2ce99cd83f62249ddc2d437ae67e3edb20af19
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-08-27 10:24:10 +0000

    Copying stress test module over to trunk
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@7461 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 986a837bd680aa6a4ecc51161738b0092c7b1e8a [formerly 853629d751e0c41e5d2755e4b1896bb735eaf414]
    Former-commit-id: 9ddc85470e37b13f4b8fc20eca5f575dfa5c8f2f

---
+++ stress/src/main/java/org/geoserver/stress/jmeter/RunWMSTests.java
@@ -0,0 +1,24 @@
+/* Copyright (c) 2001, 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.stress.jmeter;
+
+import java.io.IOException;
+
+
+/**
+ * Runs the WMS tests one after the other
+ * @author Administrator
+ *
+ */
+public class RunWMSTests {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final int loops = 7; // up to 64 concurrent threads
+
+        JMeterRunner runner = new JMeterRunner();
+        runner.runTest("StatesWMSGetMap", loops);
+        runner.runTest("StatesWMSGetMapSafePalette", loops);
+        runner.runTest("StatesWMSGetMapSafePaletteGif", loops);
+    }
+}
