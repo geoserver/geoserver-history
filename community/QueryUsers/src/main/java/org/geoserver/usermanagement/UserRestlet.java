c5040fccdd1e44fac3acdb68529e9f17bd282d9f
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-10-02 18:27:24 +0000

    Tied the EditableUserDAO object to the appropriate PUT request so that users can be modified (added/replaced)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@7560 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 268c6ad17eac6f74ba3f5c77694059b2c59b58dd [formerly 947cafc66292fdc45f35620a53d6dc99eeeba8b8]
    Former-commit-id: 8f3f5c78cfdd4d2ac9da48d82ad93539399bdd82

--- community/QueryUsers/src/main/java/org/geoserver/usermanagement/UserRestlet.java
+++ community/QueryUsers/src/main/java/org/geoserver/usermanagement/UserRestlet.java
@@ -3,6 +3,8 @@ package org.geoserver.usermanagement;
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.userdetails.UserDetails;
 import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.acegisecurity.userdetails.memory.UserAttribute;
+import org.acegisecurity.userdetails.memory.UserAttributeEditor;
 import org.geoserver.security.EditableUserDAO;
 import org.restlet.Restlet;
 import org.restlet.data.MediaType;
@@ -13,86 +15,68 @@ import org.restlet.resource.StringRepresentation;
 import org.springframework.dao.DataAccessException;
 
 /**
-  * The UserRestlet provides the basic user query and editing features of the 
-  * user management API.  Basically, it handles the requests that actually deal
-  * with users.
-  * @author David Winslow <dwinslow@openplans.org>
-  */
+ * The UserRestlet provides the basic user query and editing features of the 
+ * user management API.  Basically, it handles the requests that actually deal
+ * with users.
+ * @author David Winslow <dwinslow@openplans.org>
+ */
 public class UserRestlet extends Restlet {
-  private EditableUserDAO myUserService;
+	private EditableUserDAO myUserService;
 
-  /**
-    * Currently, the UserRestlet constructor requires an EditableUserDAO rather
-    * than any UserDetailsService.  Maybe it would make sense to have it hide the 
-    * user modification features when using other UserDetailsServices instead?
-    */
-  public UserRestlet(EditableUserDAO eud) {
-	  myUserService = eud;
-  }
+	/**
+	 * Currently, the UserRestlet constructor requires an EditableUserDAO rather
+	 * than any UserDetailsService.  Maybe it would make sense to have it hide the 
+	 * user modification features when using other UserDetailsServices instead?
+	 */
+	public UserRestlet(EditableUserDAO eud) {
+		myUserService = eud;
+	}
 
-  public void handle(Request request, Response response){
-    // what to do?
-    String username = request.getAttributes().get("name").toString();
-    
-    
-    if (request.getMethod().equals(Method.POST)){
-      String roles = request.getEntityAsForm().getFirst("roles").getValue();
-      response.setEntity(new StringRepresentation(
-                         roles,
-                         MediaType.TEXT_PLAIN));      
-    } else if (request.getMethod().equals(Method.GET)){
-        response.setEntity(new StringRepresentation(
-                           fetchDetailsByUserName(username),
-            MediaType.TEXT_PLAIN) );
-    }
- 
+	public void handle(Request request, Response response) {
+		// what to do?
+		String username = request.getAttributes().get("name").toString();
 
-  }
+		if (request.getMethod().equals(Method.PUT)) {
+			String roles;
+			try {
+				roles = request.getEntity().getText();
+				UserAttributeEditor uae = new UserAttributeEditor();
+				uae.setAsText(roles);
+				myUserService.setUserDetails(username, (UserAttribute)uae.getValue());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				roles = "failure";
+			}
 
+			response.setEntity(new StringRepresentation(roles,
+					MediaType.TEXT_PLAIN));
+		} else if (request.getMethod().equals(Method.GET)) {
+			response.setEntity(new StringRepresentation(
+					fetchDetailsByUserName(username), MediaType.TEXT_PLAIN));
+		}
+	}
 
-//   public void userExists(HttpServletRequest request, HttpServletResponse response)
-//       throws ServletException, IOException {
-// 
-//     Map kvPairs = KvpRequestReader.parseKvpSet(request.getQueryString());
-// 
-//     String message = "If you see this someone screwed up";
-//     String username = kvPairs.get("USERNAME").toString();
-//     try{
-//       UserDetails user = myUserService.loadUserByUsername(username);
-//       GrantedAuthority[] auths = user.getAuthorities();
-//       message = user.getUsername() + ": " ;	
-//       for (int i = 0; i < auths.length; i++){
-//         message += auths[i].toString() + "; ";
-//       }
-//     } catch (UsernameNotFoundException unfe){
-//       message = "User " + username + " does not exist.";
-//     } catch (DataAccessException dae){
-//       message = "Could not access database, please try again later.";
-//     } 
-// 		
-//     response.getOutputStream().write( message.getBytes() ); 
-//   }
+	/**
+	 * Get user information from the UserDetailsService and return it as a String
+	 * containing the granted authorities for the user.
+	 */
+	private String fetchDetailsByUserName(String username) {
+		String message = "If you see this someone screwed up";
 
-  /**
-    * Get user information from the UserDetailsService and return it as a String
-    * containing the granted authorities for the user.
-    */
-  private String fetchDetailsByUserName(String username){
-    String message = "If you see this someone screwed up";
-    
-    try{
-      UserDetails user = myUserService.loadUserByUsername(username);
-      GrantedAuthority[] auths = user.getAuthorities();
-      message = user.getUsername() + ": " ; 
-      for (int i = 0; i < auths.length; i++){
-        message += auths[i].toString() + "; ";
-      }
-    } catch (UsernameNotFoundException unfe){
-      message = "User " + username + " does not exist.";
-    } catch (DataAccessException dae){
-      message = "Could not access database, please try again later.";
-    } 
-        
-    return message;
-    }
+		try {
+			UserDetails user = myUserService.loadUserByUsername(username);
+			GrantedAuthority[] auths = user.getAuthorities();
+			message = user.getUsername() + ": ";
+			for (int i = 0; i < auths.length; i++) {
+				message += auths[i].toString() + "; ";
+			}
+		} catch (UsernameNotFoundException unfe) {
+			message = "User " + username + " does not exist.";
+		} catch (DataAccessException dae) {
+			message = "Could not access database, please try again later.";
+		}
+
+		return message;
+	}
 }
