c5b27bc1df0d11880057c4d62e103a7bce50a41a
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-11-20 22:32:44 +0000

    Added JDOM + XPath (Jaxen) dependencies
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7804 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 86fb97ed6f7cc35ddf909e951ca1d86f7bc73960 [formerly 63132c89b8ea385eb93483000f3ce892e0b3fb8e]
    Former-commit-id: 3a0616ef7e32c3d88ed34f5045a771ff51543d9a

--- geoserver/community/QueryUsers/src/main/java/org/geoserver/usermanagement/HTMLFormat.java
+++ geoserver/community/QueryUsers/src/main/java/org/geoserver/usermanagement/HTMLFormat.java
@@ -6,8 +6,14 @@ package org.geoserver.usermanagement;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.io.IOException;
 import org.restlet.resource.Representation;
 import org.geoserver.restconfig.HTMLTemplate;
+
+import org.jdom.input.SAXBuilder;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
 
 public class HTMLFormat implements DataFormat{
 
@@ -22,6 +28,22 @@ public class HTMLFormat implements DataFormat{
     }
 
     public Map readRepresentation(Representation rep){
+	try{
+	    SAXBuilder builder = new SAXBuilder();
+	    Document doc = builder.build(rep.getStream());
+	    Map map = new HashMap();
+
+	    XPath passwordPath = XPath.newInstance("/html/body/ul/li/span");
+	    Object passwordObj = passwordPath.selectSingleNode(doc);
+	    map.put("password", passwordObj.toString());
+
+	    return map; 
+	} catch (JDOMException jde){
+	    // TODO: handle exception gracefully
+	} catch (IOException ioe){
+	    // TODO: handle exception gracefully
+	}
+
 	return new HashMap();
     }
 }
