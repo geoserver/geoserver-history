ab1dff637d5fa199af2cef038fe931001f46d1c0
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-11-21 21:47:52 +0000

    Refactored a bit (taking user-specific code out of the HTML and XML format classes)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@7817 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 82bd0ced7e7fbbea0cb31792e5095865cac6a154 [formerly a2e6e330dc96fa114e77173f04d82262be3ee6a0]
    Former-commit-id: 00ff71383a9b7698828848fbd356fee49aecc266

---
+++ community/QueryUsers/src/main/java/org/geoserver/usermanagement/UserXMLFormat.java
@@ -0,0 +1,58 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.usermanagement;
+
+import org.geoserver.restconfig.XMLTemplate;
+
+import org.restlet.resource.Representation;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.data.MediaType;
+
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
+import org.jdom.Element;
+import org.jdom.Attribute;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UserXMLFormat extends XMLFormat{
+
+    public UserXMLFormat(String templ){
+	super(templ);
+    }
+
+    public Map readRepresentation(Representation rep){
+	try{
+	    SAXBuilder builder = new SAXBuilder();
+	    Document doc = builder.build(rep.getStream());
+	    Map map = new HashMap();
+	    XPath passwordPath = XPath.newInstance("/user/password");
+	    XPath rolePath = XPath.newInstance("/user/roles/role/@name");
+	    Element passwordObj = (Element)passwordPath.selectSingleNode(doc);
+	    List roleObjs = rolePath.selectNodes(doc);
+	    List roles = new ArrayList();
+
+	    Iterator it = roleObjs.iterator();
+	    while (it.hasNext()){
+		Attribute att = (Attribute)it.next();
+		roles.add(att.getValue());
+	    }
+
+	    map.put("password", passwordObj.getText());
+	    map.put("roles", roles);
+
+	    return map; 
+	} catch (Exception e){
+	    e.printStackTrace();
+	    return new HashMap();
+	}
+    }
+}
