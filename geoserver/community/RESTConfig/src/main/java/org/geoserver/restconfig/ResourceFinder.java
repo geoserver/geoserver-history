cb9941a3c9b1bae0430ecfa483358e090638193d
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-11-27 16:36:38 +0000

    Made SLD's available again.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7872 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6467e0d68d1e0c62db55a1d567a8c361157e719b [formerly f87f45c69896fee81589ce107431bd8afb91b853]
    Former-commit-id: 2761150eef4f73055073c4e606b27a60a67176e5

--- geoserver/community/RESTConfig/src/main/java/org/geoserver/restconfig/ResourceFinder.java
+++ geoserver/community/RESTConfig/src/main/java/org/geoserver/restconfig/ResourceFinder.java
@@ -11,47 +11,49 @@ import org.vfny.geoserver.config.DataConfig;
 import org.vfny.geoserver.config.WMSConfig;
 
 public class ResourceFinder extends Finder {
-	public final static int RESOURCE_DATASTORE = 1;
-	public final static int RESOURCE_FEATURETYPE = 2;
-	public final static int RESOURCE_COVERAGE = 3;
-	public final static int RESOURCE_STYLE = 4;
-	public final static int RESOURCE_PROJECTION = 5;
-	public final static int RESOURCE_LAYERGROUP = 6;
-	public final static int RESOURCE_INDEX = 7;
-	
-	private DataConfig myDataConfig;
-	private WMSConfig myWMSConfig;
-	private Router myRouter;
-	private int myType = 0;
-	
-	public ResourceFinder(int type, Context context, DataConfig dc, WMSConfig wmsc, Router router){
-		super(context);
-		myDataConfig = dc;
-		myWMSConfig = wmsc;
-		myRouter = router;
-		myType = type;
-	}
-	
-	public Resource findTarget(Request request, Response response) {
-		switch(myType) {
-			case RESOURCE_DATASTORE:
-				return (request.getAttributes().containsKey("datastore") ? 
-				    new DataStoreResource(getContext(), request, response, myDataConfig) :
-				    new DataStoreListResource(getContext(), request, response, myDataConfig));
-			case RESOURCE_FEATURETYPE:
-				return new FeatureTypeResource(getContext(), request, response, myDataConfig);		
-			case RESOURCE_COVERAGE:
-			        return (request.getAttributes().containsKey("coverage") ? 
-				    new CoverageResource(getContext(), request, response, myDataConfig) : 
-				    new CoverageListResource(getContext(), request, response, myDataConfig));
-                        case RESOURCE_STYLE:
-                                return new StyleResource(getContext(), request, response, myDataConfig);
-			case RESOURCE_LAYERGROUP:
-			        return new LayerGroupResource(getContext(), request, response, myWMSConfig);
-			case RESOURCE_INDEX:
-			        return new IndexResource(getContext(), request, response, myRouter);
-			default:
-				return null;
-		}		
-	}
+    public final static int RESOURCE_DATASTORE = 1;
+    public final static int RESOURCE_FEATURETYPE = 2;
+    public final static int RESOURCE_COVERAGE = 3;
+    public final static int RESOURCE_STYLE = 4;
+    public final static int RESOURCE_PROJECTION = 5;
+    public final static int RESOURCE_LAYERGROUP = 6;
+    public final static int RESOURCE_INDEX = 7;
+
+    private DataConfig myDataConfig;
+    private WMSConfig myWMSConfig;
+    private Router myRouter;
+    private int myType = 0;
+
+    public ResourceFinder(int type, Context context, DataConfig dc, WMSConfig wmsc, Router router){
+	super(context);
+	myDataConfig = dc;
+	myWMSConfig = wmsc;
+	myRouter = router;
+	myType = type;
+    }
+
+    public Resource findTarget(Request request, Response response) {
+	switch(myType) {
+	    case RESOURCE_DATASTORE:
+		return (request.getAttributes().containsKey("datastore") ? 
+			new DataStoreResource(getContext(), request, response, myDataConfig) :
+			new DataStoreListResource(getContext(), request, response, myDataConfig));
+	    case RESOURCE_FEATURETYPE:
+		return new FeatureTypeResource(getContext(), request, response, myDataConfig);		
+	    case RESOURCE_COVERAGE:
+		return (request.getAttributes().containsKey("coverage") ? 
+			new CoverageResource(getContext(), request, response, myDataConfig) : 
+			new CoverageListResource(getContext(), request, response, myDataConfig));
+	    case RESOURCE_STYLE:
+		return (request.getAttributes().containsKey("style") ? 
+			new StyleResource(getContext(), request, response, myDataConfig) : 
+			new StyleListResource(getContext(), request, response, myDataConfig));
+	    case RESOURCE_LAYERGROUP:
+		return new LayerGroupResource(getContext(), request, response, myWMSConfig);
+	    case RESOURCE_INDEX:
+		return new IndexResource(getContext(), request, response, myRouter);
+	    default:
+		return null;
+	}		
+    }
 }
