49bdb10a9e9325b519e3a3208a555f8847b43175
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-11-29 15:52:42 +0000

    Factored out validating dom read into the superclass... may be interesting to have in the GeoserverTestSupport itself in fact...
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@7903 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b3de1de09f579363222b84399a4aea956e23b1f3 [formerly b1724265e5d3150aa6c0f35e02778d4ff3e64274]
    Former-commit-id: 7be5d7e6639e59bb3e202fd57b4563af078817a0

--- geoserver/community/wcs111/src/test/java/org/geoserver/wcs/test/WCSTestSupport.java
+++ geoserver/community/wcs111/src/test/java/org/geoserver/wcs/test/WCSTestSupport.java
@@ -4,11 +4,22 @@
  */
 package org.geoserver.wcs.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.geoserver.data.test.MockData;
 import org.geoserver.test.GeoServerTestSupport;
 import org.vfny.geoserver.global.WCS;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import com.mockrunner.mock.web.MockHttpServletResponse;
 
 /**
  * Base support class for wcs tests.
@@ -41,5 +52,42 @@ public class WCSTestSupport extends GeoServerTestSupport {
     protected void populateDataDirectory(MockData dataDirectory) throws Exception {
         dataDirectory.addCoverage(TASMANIA_DEM, WCSTestSupport.class.getResource("tazdem.tiff"), TIFF, null);
         dataDirectory.addCoverage(TASMANIA_BM, WCSTestSupport.class.getResource("tazbm.tiff"), TIFF, null);
+    }
+    
+    /**
+     * Makes a request, parses it into a DOM making sure the document conforms to the XML schema
+     * @param path
+     * @return the parsed document
+     * @throws Exception
+     */
+    protected Document getAsDomAndValidate(final String path) throws Exception {
+        MockHttpServletResponse response = getAsServletResponse(path);
+        InputStream is = new ByteArrayInputStream( response.getOutputStreamContent().getBytes() );
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); 
+        factory.setNamespaceAware( true );
+        factory.setValidating(true);
+        factory.setAttribute( "http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
+        "http://www.w3.org/2001/XMLSchema");
+        
+        
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        builder.setErrorHandler(new ErrorHandler() {
+        
+            public void warning(SAXParseException exception) throws SAXException {
+                System.out.println(exception.getMessage());
+            }
+        
+            public void fatalError(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+                fail();
+            }
+        
+            public void error(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+                fail();
+            }
+        
+        });
+        return builder.parse( is );
     }
 }
