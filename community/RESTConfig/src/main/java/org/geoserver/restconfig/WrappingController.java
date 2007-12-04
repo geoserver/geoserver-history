30ee9030e52945ca1ac92297885fb75d3a5562ea
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-12-04 18:09:26 +0000

    Last compiling version before switch over to using org.vfny.geoserver.global.* rather than org.vfny.geoserver.config.*
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@7932 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 514912040204b4ba931498852b614f17636384ae [formerly b3f7c3dfafc4b0ab0a70d2a41c1f7812de301b99]
    Former-commit-id: fa04095cab859aafda5fe176273ef0d75e37caff

--- community/RESTConfig/src/main/java/org/geoserver/restconfig/WrappingController.java
+++ community/RESTConfig/src/main/java/org/geoserver/restconfig/WrappingController.java
@@ -8,6 +8,7 @@ import org.restlet.Router;
 import org.springframework.beans.BeansException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.context.ApplicationContext;
 
 import com.noelios.restlet.ext.servlet.ServletConverter;
 
@@ -15,72 +16,70 @@ import org.vfny.geoserver.config.DataConfig;
 import org.vfny.geoserver.config.WMSConfig;
 
 /**
-  * Simple AbstractController implementation that does the translation between
-  * Spring requests and Restlet requests.
-  */
+ * Simple AbstractController implementation that does the translation between
+ * Spring requests and Restlet requests.
+ */
 public class WrappingController extends AbstractController {
 
-	public static String METHOD_PUT = "PUT";
-	public static String METHOD_DELETE = "DELETE";
+    public static String METHOD_PUT = "PUT";
+    public static String METHOD_DELETE = "DELETE";
 
-	ServletConverter myConverter;
+    ServletConverter myConverter;
 
-        public WrappingController(){
-		super();
-		setSupportedMethods(new String[]{
-				METHOD_GET,
-				METHOD_POST,
-				METHOD_PUT,
-				METHOD_DELETE,
-				METHOD_HEAD}
-		);
-	}
-	
-	protected void initApplicationContext() throws BeansException {
-		super.initApplicationContext();
+    public WrappingController(){
+        super();
+        setSupportedMethods(new String[]{
+                METHOD_GET,
+                METHOD_POST,
+                METHOD_PUT,
+                METHOD_DELETE,
+                METHOD_HEAD}
+                );
+    }
 
-		myConverter = new ServletConverter(getServletContext());
-		myConverter.setTarget(createRoot());
-	}
+    protected void initApplicationContext() throws BeansException {
+        super.initApplicationContext();
 
-	protected ModelAndView handleRequestInternal(HttpServletRequest req,
-			HttpServletResponse resp) throws Exception {
-		myConverter.service(req, resp);
+        myConverter = new ServletConverter(getServletContext());
+        myConverter.setTarget(createRoot());
+    }
 
-		return null;
-	}
+    protected ModelAndView handleRequestInternal(HttpServletRequest req,
+            HttpServletResponse resp) throws Exception {
+        myConverter.service(req, resp);
 
-	public Restlet createRoot() {
-		Router router = new Router();
+        return null;
+    }
 
-		DataConfig dc = (DataConfig) getApplicationContext().getBean("dataConfig");
-		WMSConfig wmsc = (WMSConfig) getApplicationContext().getBean("wmsConfig");
+    public Restlet createRoot() {
+        Router router = new Router();
+        ApplicationContext context = getApplicationContext();
 
-		router.attach("", new ResourceFinder(ResourceFinder.RESOURCE_INDEX, router.getContext(), dc, wmsc, router));
-		router.attach("/datastores.{type}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc, wmsc, router));
-		router.attach("/datastores", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc, wmsc, router));
-		router.attach("/datastores/{datastore}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc, wmsc, router));
-		router.attach("/datastores/{datastore}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc, wmsc, router));
-		// This rule messes everything up: router.attach("/datastores/{datastore}/featuretypes", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc));
-		router.attach("/datastores/{datastore}/featuretypes/{featuretype}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_FEATURETYPE, router.getContext(), dc, wmsc, router));
-		router.attach("/datastores/{datastore}/featuretypes/{featuretype}", new ResourceFinder(ResourceFinder.RESOURCE_FEATURETYPE, router.getContext(), dc, wmsc, router));
-                router.attach("/styles/{style}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), dc, wmsc, router));
-                router.attach("/styles/{style}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), dc, wmsc, router));
-                router.attach("/styles.{type}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), dc, wmsc, router));
-                router.attach("/styles", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores/{coveragestore}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores/{coveragestore}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores/{coveragestore}/coverages/{coverage}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGE, router.getContext(), dc, wmsc, router));
-                router.attach("/coveragestores/{coveragestore}/coverages/{coverage}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGE, router.getContext(), dc, wmsc, router));
-                router.attach("/dummy/{name}", new DummyRestlet(getApplicationContext()));
-		router.attach("/layergroups.{type}", new ResourceFinder(ResourceFinder.RESOURCE_LAYERGROUP, router.getContext(), dc, wmsc, router));
-		router.attach("/layergroups", new ResourceFinder(ResourceFinder.RESOURCE_LAYERGROUP, router.getContext(), dc, wmsc, router));
-		router.attach("/projections", new DummyRestlet(getApplicationContext()));
-		router.attach("/projections/{projection}", new DummyRestlet(getApplicationContext()));
-		
-		return router;
-	}
+        router.attach("", new ResourceFinder(ResourceFinder.RESOURCE_INDEX, router.getContext(), context, router));
+        router.attach("/datastores.{type}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), context, router));
+        router.attach("/datastores", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), context, router));
+        router.attach("/datastores/{datastore}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), context, router));
+        router.attach("/datastores/{datastore}", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), context, router));
+        // This rule messes everything up: router.attach("/datastores/{datastore}/featuretypes", new ResourceFinder(ResourceFinder.RESOURCE_DATASTORE, router.getContext(), dc));
+        router.attach("/datastores/{datastore}/featuretypes/{featuretype}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_FEATURETYPE, router.getContext(), context, router));
+        router.attach("/datastores/{datastore}/featuretypes/{featuretype}", new ResourceFinder(ResourceFinder.RESOURCE_FEATURETYPE, router.getContext(), context, router));
+        router.attach("/styles/{style}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), context, router));
+        router.attach("/styles/{style}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), context, router));
+        router.attach("/styles.{type}", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), context, router));
+        router.attach("/styles", new ResourceFinder(ResourceFinder.RESOURCE_STYLE, router.getContext(), context, router));
+        router.attach("/coveragestores.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), context, router));
+        router.attach("/coveragestores", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), context, router));
+        router.attach("/coveragestores/{coveragestore}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), context, router));
+        router.attach("/coveragestores/{coveragestore}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGESTORE, router.getContext(), context, router));
+        router.attach("/coveragestores/{coveragestore}/coverages/{coverage}.{type}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGE, router.getContext(), context, router));
+        router.attach("/coveragestores/{coveragestore}/coverages/{coverage}", new ResourceFinder(ResourceFinder.RESOURCE_COVERAGE, router.getContext(), context, router));
+        router.attach("/dummy/{name}", new DummyRestlet(getApplicationContext()));
+        router.attach("/layergroups.{type}", new ResourceFinder(ResourceFinder.RESOURCE_LAYERGROUP, router.getContext(), context, router));
+        router.attach("/layergroups", new ResourceFinder(ResourceFinder.RESOURCE_LAYERGROUP, router.getContext(), context, router));
+        router.attach("/projections", new DummyRestlet(getApplicationContext()));
+        router.attach("/projections/{projection}", new DummyRestlet(getApplicationContext()));
+
+        return router;
+    }
 
 }
