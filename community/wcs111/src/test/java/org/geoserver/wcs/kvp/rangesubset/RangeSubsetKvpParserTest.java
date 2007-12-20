bdd23870fdc1686867d61129774c5ca5292a44f1
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-12-20 08:27:24 +0000

    Added rangesubset kvp parser based on javacc, will remove the generated code once the gt2 jjtree plugin can handle javacc 4
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8058 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c2e0d3ae237c8aa61d5f6d0323bc6490be1f66fd [formerly c4c2f89b8a7e87cc7ea7a35e9765a859927ce230]
    Former-commit-id: eaa3f0c89dedd65216f27dc800ace06b210fc0a0

---
+++ community/wcs111/src/test/java/org/geoserver/wcs/kvp/rangesubset/RangeSubsetKvpParserTest.java
@@ -0,0 +1,22 @@
+package org.geoserver.wcs.kvp.rangesubset;
+
+import junit.framework.TestCase;
+import net.opengis.wcs.v1_1_1.RangeSubsetType;
+
+import org.geoserver.wcs.kvp.RangeSubsetKvpParser;
+
+public class RangeSubsetKvpParserTest extends TestCase {
+    RangeSubsetKvpParser parser = new RangeSubsetKvpParser();
+    
+    public void testSimpleFields() throws Exception {
+        RangeSubsetType rs = (RangeSubsetType) parser.parse("radiance;temperature");
+        assertNotNull(rs);
+        assertEquals(2, rs.getFieldSubset().size());
+    }
+    
+    public void testInterpolation() throws Exception {
+        RangeSubsetType rs = (RangeSubsetType) parser.parse("radiance:linear;temperature:nearest");
+        assertNotNull(rs);
+        assertEquals(2, rs.getFieldSubset().size());
+    }
+}
