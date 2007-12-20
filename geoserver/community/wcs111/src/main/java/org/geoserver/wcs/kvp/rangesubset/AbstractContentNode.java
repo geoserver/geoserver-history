fb0b6a552fb0121842a3fccc5e42db54caa9ea33
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-12-20 08:27:24 +0000

    Added rangesubset kvp parser based on javacc, will remove the generated code once the gt2 jjtree plugin can handle javacc 4
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8058 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 469d181e4d3ca36da9d31fe7a9d67e9e7d50d485 [formerly 77e09c134e320b9768cc79aa9ea6a0c5a26ffc66]
    Former-commit-id: 51b2cc5810623693d22ed57ae96e07e641f8809e

---
+++ geoserver/community/wcs111/src/main/java/org/geoserver/wcs/kvp/rangesubset/AbstractContentNode.java
@@ -0,0 +1,14 @@
+package org.geoserver.wcs.kvp.rangesubset;
+
+public class AbstractContentNode {
+    protected String content;
+
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
