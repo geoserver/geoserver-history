531725f7ea28225e5e33fc5910a2d548dba0fdba
mbarto <mbarto@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-12-21 09:34:49 +0000

    Initial commit
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.6.x@8065 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7c2529947e0a0c58e4f5b20f49e7d88f45627057 [formerly e77f80baa6eb465b4f3c2eab307194d18d062a17]
    Former-commit-id: 8617e60b1a19a896511652fcc58fef40ac30b8a0

---
+++ geoserver/community/imagemap/src/main/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/AbortedException.java
@@ -0,0 +1,17 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.wms.responses.map.htmlimagemap;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Mauro Bartolomeoli 
+ */
+public class AbortedException extends Exception {
+    public AbortedException(String msg) {
+        super(msg);
+    }
+}
