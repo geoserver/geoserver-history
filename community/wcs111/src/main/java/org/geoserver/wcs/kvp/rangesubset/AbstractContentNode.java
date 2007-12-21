4dcb3b07e02bdc7925a5129eae5939b26d98a6a0
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2007-12-21 14:02:20 +0000

    Added headers and comments
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8069 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: faae52fb8f69b47ebf7d44dc2627b1be29b252db [formerly ba1a6e9afd4f91e7e8c7620c39f38f5465a555e6]
    Former-commit-id: 889ba56bf3b8fd256aa8835fb6191d3bb3ddd8fb

--- community/wcs111/src/main/java/org/geoserver/wcs/kvp/rangesubset/AbstractContentNode.java
+++ community/wcs111/src/main/java/org/geoserver/wcs/kvp/rangesubset/AbstractContentNode.java
@@ -1,6 +1,15 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 package org.geoserver.wcs.kvp.rangesubset;
 
-public class AbstractContentNode {
+/**
+ * Abstract node that add the concept of a node content 
+ * @author Andrea Aime - TOPP
+ *
+ */
+public abstract class AbstractContentNode implements Node {
     protected String content;
 
     public String getContent() {
