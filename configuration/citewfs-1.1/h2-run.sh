8f19f463c94b1de8409109fb794c2c04367b8454
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-01-09 17:46:27 +0000

    moved to citewfs-1.1-h2 configuration
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8138 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6531603cd967b7ee595bcaae0cdb0e283b992889 [formerly e9b0fdf61839d7ffbe4ec404425edf7a2eee3cdf]
    Former-commit-id: c577f8fa08ad6e21b886f5d05b5963f7db67a48e

--- configuration/citewfs-1.1/h2-run.sh
+++
@@ -1,2 +0,0 @@
-H2_HOME="/opt/h2"
-java -cp $H2_HOME/bin/h2.jar:/home/jdeolive/.m2/repository/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar:/home/jdeolive/.m2/repository/org/geotools/gt2-main/2.4-SNAPSHOT/gt2-main-2.4-SNAPSHOT.jar:/home/jdeolive/.m2/repository/com/vividsolutions/jts/1.8/jts-1.8.jar org.h2.tools.Server
