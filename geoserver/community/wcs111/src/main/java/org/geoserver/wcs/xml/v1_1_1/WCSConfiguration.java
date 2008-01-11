73cd14d2923f4215eb786021af16e8ab54f7a1a3
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-01-11 18:29:00 +0000

    More xml parsing work
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8150 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 82cd152366902cae24c406a6896ec19b16b6518c [formerly 364dd5cc16339af9164b7e28c2ff95af004841d3]
    Former-commit-id: 4ce3ac6aecea0645ee165c6a79636ab0bd7f253b

--- geoserver/community/wcs111/src/main/java/org/geoserver/wcs/xml/v1_1_1/WCSConfiguration.java
+++ geoserver/community/wcs111/src/main/java/org/geoserver/wcs/xml/v1_1_1/WCSConfiguration.java
@@ -34,49 +34,12 @@ public class WCSConfiguration extends Configuration {
        addDependency(new OWSConfiguration());
     }
     
-    /**
-     * Registers the bindings for the configuration.
-     *
-     * @generated
-     */
-    protected final void registerBindings( MutablePicoContainer container ) {
-        //Types
-//        container.registerComponentImplementation(WCS.AxisType,AxisTypeBinding.class);
-//        container.registerComponentImplementation(WCS.CoverageDescriptionType,CoverageDescriptionTypeBinding.class);
-//        container.registerComponentImplementation(WCS.CoverageDomainType,CoverageDomainTypeBinding.class);
-//        container.registerComponentImplementation(WCS.CoveragesType,CoveragesTypeBinding.class);
-//        container.registerComponentImplementation(WCS.CoverageSummaryType,CoverageSummaryTypeBinding.class);
-//        container.registerComponentImplementation(WCS.DomainSubsetType,DomainSubsetTypeBinding.class);
-//        container.registerComponentImplementation(WCS.FieldType,FieldTypeBinding.class);
-//        container.registerComponentImplementation(WCS.GridCrsType,GridCrsTypeBinding.class);
-//        container.registerComponentImplementation(WCS.IdentifierType,IdentifierTypeBinding.class);
-//        container.registerComponentImplementation(WCS.ImageCRSRefType,ImageCRSRefTypeBinding.class);
-//        container.registerComponentImplementation(WCS.InterpolationMethodBaseType,InterpolationMethodBaseTypeBinding.class);
-//        container.registerComponentImplementation(WCS.InterpolationMethodType,InterpolationMethodTypeBinding.class);
-//        container.registerComponentImplementation(WCS.OutputType,OutputTypeBinding.class);
-//        container.registerComponentImplementation(WCS.RangeSubsetType,RangeSubsetTypeBinding.class);
-//        container.registerComponentImplementation(WCS.RangeType,RangeTypeBinding.class);
-//        container.registerComponentImplementation(WCS.RequestBaseType,RequestBaseTypeBinding.class);
-//        container.registerComponentImplementation(WCS.SpatialDomainType,SpatialDomainTypeBinding.class);
-//        container.registerComponentImplementation(WCS.TimeDurationType,TimeDurationTypeBinding.class);
-//        container.registerComponentImplementation(WCS.TimePeriodType,TimePeriodTypeBinding.class);
-//        container.registerComponentImplementation(WCS.TimeSequenceType,TimeSequenceTypeBinding.class);
-//        container.registerComponentImplementation(WCS._AvailableKeys,_AvailableKeysBinding.class);
-//        container.registerComponentImplementation(WCS._AxisSubset,_AxisSubsetBinding.class);
-//        container.registerComponentImplementation(WCS._Capabilities,_CapabilitiesBinding.class);
-//        container.registerComponentImplementation(WCS._Contents,_ContentsBinding.class);
-//        container.registerComponentImplementation(WCS._CoverageDescriptions,_CoverageDescriptionsBinding.class);
-//        container.registerComponentImplementation(WCS._GetCapabilities,_GetCapabilitiesBinding.class);
-//        container.registerComponentImplementation(WCS._GetCoverage,_GetCoverageBinding.class);
-//        container.registerComponentImplementation(WCS._InterpolationMethods,_InterpolationMethodsBinding.class);
-//        container.registerComponentImplementation(WCS.RangeSubsetType_FieldSubset,RangeSubsetType_FieldSubsetBinding.class);
-    }
-    
     @Override
     protected void registerBindings(Map bindings) {
         super.registerBindings(bindings);
         
         final EFactory wcsFactory = Wcs111Factory.eINSTANCE;
+        register(bindings, wcsFactory, WCS.RequestBaseType);
         register(bindings, wcsFactory, WCS._DescribeCoverage);
         register(bindings, wcsFactory, WCS._GetCoverage);
         register(bindings, wcsFactory, WCS.DomainSubsetType);
@@ -87,11 +50,7 @@ public class WCSConfiguration extends Configuration {
         register(bindings, wcsFactory, WCS.RangeSubsetType_FieldSubset);
         register(bindings, wcsFactory, WCS._AxisSubset);
         register(bindings, wcsFactory, WCS.OutputType);
-
-        // todo: move these to the geotools ows module when done
-        final EFactory owsFactory = Ows11Factory.eINSTANCE; 
-        register(bindings, owsFactory, OWS.BoundingBoxType);
-        register(bindings, owsFactory, OWS.CodeType);
+        register(bindings, wcsFactory, WCS.GridCrsType);
         
     }
     
