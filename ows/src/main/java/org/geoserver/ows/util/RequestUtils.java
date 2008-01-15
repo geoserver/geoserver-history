b2220baff2840268f46a2d5b3b3e316caea29e98
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-01-15 15:12:25 +0000

    GEOS-1669 Exception reports by mistake n times the same message instead of using the messages got from the stack trace
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8167 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e5e3616f504985038784b81706a291e77820526b [formerly 9584fb68e4e764ba4518660db0562f2d42886a2e]
    Former-commit-id: d413331e5f7d3aeae448f647d56d4e1063a09f51

--- ows/src/main/java/org/geoserver/ows/util/RequestUtils.java
+++ ows/src/main/java/org/geoserver/ows/util/RequestUtils.java
@@ -6,8 +6,14 @@ package org.geoserver.ows.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.geoserver.platform.ServiceException;
+import org.geotools.util.Version;
 
 
 /**
@@ -76,6 +82,8 @@ public class RequestUtils {
         }
     }
     
+    
+    
     /**
      * Forces the base url to a certain string. Useful to redirect the xml schema
      * locations to the local file system during testing (since no web server is running during unit tests)
@@ -83,5 +91,170 @@ public class RequestUtils {
      */
     public static void setForcedBaseUrl(String url) {
         forcedBaseUrl = url;
+    }
+
+    /**
+     * Given a list of provided versions, and a list of accepted versions, this method will
+     * return the negotiated version to be used for response according to the pre OWS 1.1 specifications,
+     * that is, WMS 1.1, WMS 1.3, WFS 1.0, WFS 1.1 and WCS 1.0
+     * @param providedList a non null, non empty list of provided versions (in "x.y.z" format)
+     * @param acceptedList a list of accepted versions, eventually null or empty (in "x.y.z" format)
+     * @return the negotiated version to be used for response
+     */
+    public static String getVersionPreOws(List<String> providedList, List<String> acceptedList) {
+        //first figure out which versions are provided
+        TreeSet<Version> provided = new TreeSet<Version>();
+        for (String v : providedList) {
+            provided.add(new Version(v));
+        }
+        
+        // if no accept list provided, we return the biggest
+        if(acceptedList == null || acceptedList.isEmpty())
+            return provided.last().toString();
+    
+        //next figure out what the client accepts (and check they are good version numbers)
+        TreeSet<Version> accepted = new TreeSet<Version>();
+        for (String v : acceptedList) {
+            if (!v.matches("[0-99]\\.[0-99]\\.[0-99]")) {
+                String msg = v + " is an invalid version numver";
+                throw new ServiceException(msg, "VersionNegotiationFailed");
+            }
+            
+            accepted.add(new Version(v));
+        }
+    
+        // prune out those not provided
+        for (Iterator<Version> v = accepted.iterator(); v.hasNext();) {
+            Version version = (Version) v.next();
+    
+            if (!provided.contains(version)) {
+                v.remove();
+            }
+        }
+    
+        // lookup a matching version
+        String version = null;
+        if (!accepted.isEmpty()) {
+            //return the highest version provided
+            version = ((Version) accepted.last()).toString();
+        } else {
+            for (String v : acceptedList) {
+                accepted.add(new Version(v));
+            }
+    
+            //if highest accepted less then lowest provided, send lowest
+            if ((accepted.last()).compareTo(provided.first()) < 0) {
+                version = (provided.first()).toString();
+            }
+    
+            //if lowest accepted is greater then highest provided, send highest
+            if ((accepted.first()).compareTo(provided.last()) > 0) {
+                version = (provided.last()).toString();
+            }
+    
+            if (version == null) {
+                //go through from lowest to highest, and return highest provided 
+                // that is less than the highest accepted
+                Iterator<Version> v = provided.iterator();
+                Version last = v.next();
+    
+                for (; v.hasNext();) {
+                    Version current = v.next();
+    
+                    if (current.compareTo(accepted.last()) > 0) {
+                        break;
+                    }
+    
+                    last = current;
+                }
+    
+                version = last.toString();
+            }
+        }
+        
+        return version;
+    }
+    
+    /**
+     * Given a list of provided versions, and a list of accepted versions, this method will
+     * return the negotiated version to be used for response according to the OWS 1.1 specification
+     * (at the time of writing, only WCS 1.1.1 is using it)
+     * @param providedList a non null, non empty list of provided versions (in "x.y.z" format)
+     * @param acceptedList a list of accepted versions, eventually null or empty (in "x.y.z" format)
+     * @return the negotiated version to be used for response
+     */
+    public static String getVersionOws11(List<String> providedList, List<String> acceptedList) {
+        return null;
+//        //first figure out which versions are provided
+//        TreeSet<Version> provided = new TreeSet<Version>();
+//        for (String v : providedList) {
+//            provided.add(new Version(v));
+//        }
+//        
+//        // if no accept list provided, we throw an exception
+//        if(acceptedList == null || acceptedList.isEmpty())
+//            
+//    
+//        //next figure out what the client accepts (and check they are good version numbers)
+//        TreeSet<Version> accepted = new TreeSet<Version>();
+//        for (String v : acceptedList) {
+//            if (!v.matches("[0-99]\\.[0-99]\\.[0-99]")) {
+//                String msg = v + " is an invalid version numver";
+//                throw new ServiceException(msg, "VersionNegotiationFailed");
+//            }
+//            
+//            accepted.add(new Version(v));
+//        }
+//    
+//        // prune out those not provided
+//        for (Iterator<Version> v = accepted.iterator(); v.hasNext();) {
+//            Version version = (Version) v.next();
+//    
+//            if (!provided.contains(version)) {
+//                v.remove();
+//            }
+//        }
+//    
+//        // lookup a matching version
+//        String version = null;
+//        if (!accepted.isEmpty()) {
+//            //return the highest version provided
+//            version = ((Version) accepted.last()).toString();
+//        } else {
+//            for (String v : acceptedList) {
+//                accepted.add(new Version(v));
+//            }
+//    
+//            //if highest accepted less then lowest provided, send lowest
+//            if ((accepted.last()).compareTo(provided.first()) < 0) {
+//                version = (provided.first()).toString();
+//            }
+//    
+//            //if lowest accepted is greater then highest provided, send highest
+//            if ((accepted.first()).compareTo(provided.last()) > 0) {
+//                version = (provided.last()).toString();
+//            }
+//    
+//            if (version == null) {
+//                //go through from lowest to highest, and return highest provided 
+//                // that is less than the highest accepted
+//                Iterator<Version> v = provided.iterator();
+//                Version last = v.next();
+//    
+//                for (; v.hasNext();) {
+//                    Version current = v.next();
+//    
+//                    if (current.compareTo(accepted.last()) > 0) {
+//                        break;
+//                    }
+//    
+//                    last = current;
+//                }
+//    
+//                version = last.toString();
+//            }
+//        }
+//        
+//        return version;
     }
 }
