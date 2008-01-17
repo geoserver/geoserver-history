ba789ba85996a27ed2e507ef8ad1f1a4f7f76708
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-01-17 15:08:55 +0000

    Added ows 1.1 style negotiation abilities to wcs 1.1.1
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8207 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: aa714337ea2bffc418ec828b16c73034a1a90d31 [formerly 4c24c54e3278403e7d460e81f658616617b49dbd]
    Former-commit-id: 858e5f9a2442a9a21fff86f7a5122e89cb9af7c2

--- geoserver/ows/src/main/java/org/geoserver/ows/util/RequestUtils.java
+++ geoserver/ows/src/main/java/org/geoserver/ows/util/RequestUtils.java
@@ -6,6 +6,7 @@ package org.geoserver.ows.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
@@ -115,10 +116,7 @@ public class RequestUtils {
         //next figure out what the client accepts (and check they are good version numbers)
         TreeSet<Version> accepted = new TreeSet<Version>();
         for (String v : acceptedList) {
-            if (!v.matches("[0-99]\\.[0-99]\\.[0-99]")) {
-                String msg = v + " is an invalid version numver";
-                throw new ServiceException(msg, "VersionNegotiationFailed");
-            }
+            checkVersionNumber(v, null);
             
             accepted.add(new Version(v));
         }
@@ -184,77 +182,57 @@ public class RequestUtils {
      * @return the negotiated version to be used for response
      */
     public static String getVersionOws11(List<String> providedList, List<String> acceptedList) {
-        return null;
-//        //first figure out which versions are provided
-//        TreeSet<Version> provided = new TreeSet<Version>();
-//        for (String v : providedList) {
-//            provided.add(new Version(v));
-//        }
-//        
-//        // if no accept list provided, we throw an exception
-//        if(acceptedList == null || acceptedList.isEmpty())
-//            
-//    
-//        //next figure out what the client accepts (and check they are good version numbers)
-//        TreeSet<Version> accepted = new TreeSet<Version>();
-//        for (String v : acceptedList) {
-//            if (!v.matches("[0-99]\\.[0-99]\\.[0-99]")) {
-//                String msg = v + " is an invalid version numver";
-//                throw new ServiceException(msg, "VersionNegotiationFailed");
-//            }
-//            
-//            accepted.add(new Version(v));
-//        }
-//    
-//        // prune out those not provided
-//        for (Iterator<Version> v = accepted.iterator(); v.hasNext();) {
-//            Version version = (Version) v.next();
-//    
-//            if (!provided.contains(version)) {
-//                v.remove();
-//            }
-//        }
-//    
-//        // lookup a matching version
-//        String version = null;
-//        if (!accepted.isEmpty()) {
-//            //return the highest version provided
-//            version = ((Version) accepted.last()).toString();
-//        } else {
-//            for (String v : acceptedList) {
-//                accepted.add(new Version(v));
-//            }
-//    
-//            //if highest accepted less then lowest provided, send lowest
-//            if ((accepted.last()).compareTo(provided.first()) < 0) {
-//                version = (provided.first()).toString();
-//            }
-//    
-//            //if lowest accepted is greater then highest provided, send highest
-//            if ((accepted.first()).compareTo(provided.last()) > 0) {
-//                version = (provided.last()).toString();
-//            }
-//    
-//            if (version == null) {
-//                //go through from lowest to highest, and return highest provided 
-//                // that is less than the highest accepted
-//                Iterator<Version> v = provided.iterator();
-//                Version last = v.next();
-//    
-//                for (; v.hasNext();) {
-//                    Version current = v.next();
-//    
-//                    if (current.compareTo(accepted.last()) > 0) {
-//                        break;
-//                    }
-//    
-//                    last = current;
-//                }
-//    
-//                version = last.toString();
-//            }
-//        }
-//        
-//        return version;
+        //first figure out which versions are provided
+        TreeSet<Version> provided = new TreeSet<Version>();
+        for (String v : providedList) {
+            provided.add(new Version(v));
+        }
+        
+        // if no accept list provided, we return the biggest supported version
+        if(acceptedList == null || acceptedList.isEmpty())
+            return provided.last().toString();
+            
+    
+        // next figure out what the client accepts (and check they are good version numbers)
+        List<Version> accepted = new ArrayList<Version>();
+        for (String v : acceptedList) {
+            checkVersionNumber(v, "AcceptVersions");
+            
+            accepted.add(new Version(v));
+        }
+    
+        // from the specification "The server, upon receiving a GetCapabilities request, shall scan 
+        // through this list and find the first version number that it supports"
+        Version negotiated = null;
+        for (Iterator<Version> v = accepted.iterator(); v.hasNext();) {
+            Version version = (Version) v.next();
+    
+            if (provided.contains(version)) {
+                negotiated = version;
+                break;
+            }
+        }
+        
+        // from the spec: "If the list does not contain any version numbers that the server 
+        // supports, the server shall return an Exception with 
+        // exceptionCode="VersionNegotiationFailed"
+        if(negotiated == null)
+            throw new ServiceException("Could not find any matching version", "VersionNegotiationFailed");
+        
+        return negotiated.toString();
+    }
+
+    /**
+     * Checks the validity of a version number (the specification version numbers, three dot
+     * separated integers between 0 and 99). Throws a ServiceException if the version number
+     * is not valid.
+     * @param v the version number (in string format)
+     * @param the locator for the service exception (may be null)
+     */
+    public static void checkVersionNumber(String v, String locator) throws ServiceException {
+        if (!v.matches("[0-99]\\.[0-99]\\.[0-99]")) {
+            String msg = v + " is an invalid version number";
+            throw new ServiceException(msg, "VersionNegotiationFailed", locator);
+        }
     }
 }
