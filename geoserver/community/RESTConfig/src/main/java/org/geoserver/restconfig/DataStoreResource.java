772534e03c98ed8d6d1438ee1844609ac8b15e73
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-01-25 21:57:34 +0000

    Basic PUT and DELETE support for DataStores
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8279 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fef72b672f8380f1c8da7298a57bc3163fe6f78c [formerly b2e16e1ff73da57d317a3d45004bf31f7081c443]
    Former-commit-id: ab177628c5e01c38327aa88390efba6b7cdf3561

--- geoserver/community/RESTConfig/src/main/java/org/geoserver/restconfig/DataStoreResource.java
+++ geoserver/community/RESTConfig/src/main/java/org/geoserver/restconfig/DataStoreResource.java
@@ -4,18 +4,31 @@
  */
 package org.geoserver.restconfig;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.geotools.data.DataStoreFactorySpi;
 import org.geotools.data.DataStoreFactorySpi.Param;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.MediaType;
+import org.restlet.data.Status;
+import org.restlet.resource.StringRepresentation;
 import org.vfny.geoserver.config.DataConfig;
 import org.vfny.geoserver.config.DataStoreConfig;
+import org.vfny.geoserver.global.ConfigurationException;
+import org.vfny.geoserver.global.Data;
+import org.vfny.geoserver.global.GeoserverDataDirectory;
+import org.vfny.geoserver.global.dto.DataDTO;
+import org.vfny.geoserver.global.xml.XMLConfigWriter;
+import org.vfny.geoserver.util.DataStoreUtils;
 
 
 /**
@@ -25,12 +38,10 @@ import org.vfny.geoserver.config.DataStoreConfig;
  */
 public class DataStoreResource extends MapResource {
     private DataConfig myDC;
-    private DataStoreConfig myDSC;
+    private Data myData;
 
-    public DataStoreResource(Context context, Request request, Response response, DataConfig dc) {
-        super(context, request, response);
-        myDC = dc;
-        myDSC = findMyDataStore();
+    public DataStoreResource(){
+        super();
     }
 
     public void setDataConfig(DataConfig dc){
@@ -40,6 +51,15 @@ public class DataStoreResource extends MapResource {
     public DataConfig getDataConfig(){
         return myDC;
     }
+    
+    public void setData(Data d){
+        myData = d;
+    }
+    
+    public Data getData(){
+        return myData;
+    }
+   
 
     private DataStoreConfig findMyDataStore() {
         Map attributes = getRequest().getAttributes();
@@ -64,42 +84,155 @@ public class DataStoreResource extends MapResource {
     }
 
     public Map getMap() {
-        myDSC = findMyDataStore();
+        try{
+            DataStoreConfig myDSC = findMyDataStore();
 
-        // TODO: Fill this in better (what about different types of datastore?)
-        // TODO: Handle parameters with spaces in their names
-        Map map = new HashMap();
-        map.put("Enabled", myDSC.isEnabled());
-        map.put("Namespace", myDSC.getNameSpaceId());
-        map.put("Description", (myDSC.getAbstract() == null ? "" : myDSC.getAbstract()));
+            Map map = new HashMap();
+            map.put("Enabled", Boolean.toString(myDSC.isEnabled()));
+            map.put("Namespace", myDSC.getNameSpaceId());
+            map.put("Description", (myDSC.getAbstract() == null ? "" : myDSC.getAbstract()));
+            map.put("DataStoreType", myDSC.getFactory().getDisplayName());
 
-        DataStoreFactorySpi factory = myDSC.getFactory();
+            DataStoreFactorySpi factory = myDSC.getFactory();
 
-        List storeSpecificParameters = new ArrayList();
-        Param[] parameters = factory.getParametersInfo();
-        for (int i = 0; i < parameters.length; i++){
-            Param p = parameters[i];
-            if (!("namespace".equals(p.key))){
-                Object value = myDSC.getConnectionParams().get(p.key);
-                Map entry = new HashMap();
-                if (value == null) {
-                    entry.put("value", "");
-                } else if (value instanceof String){
-                    entry.put("value", value);
-                } else {
-                    entry.put("value", p.text(value));
+            List storeSpecificParameters = new ArrayList();
+            Param[] parameters = factory.getParametersInfo();
+            for (int i = 0; i < parameters.length; i++){
+                Param p = parameters[i];
+                if (!("namespace".equals(p.key))){
+                    Object value = myDSC.getConnectionParams().get(p.key);
+                    Map entry = new HashMap();
+                    if (value == null) {
+                        entry.put("value", "");
+                    } else if (value instanceof String){
+                        entry.put("value", value);
+                    } else {
+                        entry.put("value", p.text(value));
+                    }
+                    String key = p.key.substring(p.key.lastIndexOf(':') + 1);
+                    entry.put("name", key);
+                    entry.put("type", p.type.getName());
+                    entry.put("required", Boolean.toString(p.required));
+                    storeSpecificParameters.add(entry);
                 }
-                String key = p.key.substring(p.key.lastIndexOf(':') + 1);
-                entry.put("name", key);
-                entry.put("type", p.type.getName());
-                entry.put("required", Boolean.toString(p.required));
-                storeSpecificParameters.add(entry);
+            }
+
+            map.put("Params", storeSpecificParameters);
+
+            return map;
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    public boolean allowPut(){
+        return true;
+    }
+    
+    @Override
+    protected void putMap(Map details) /*throws Exception*/ {
+        try{
+    	String dataStoreName = (String)getRequest().getAttributes().get("datastore");
+    	DataStoreConfig myDSC = findMyDataStore();
+    	
+    	if (myDSC == null){
+//            DataStoreFactorySpi factory = 
+//                findDataStoreFactory((String)details.get("DataStoreType"));
+    	    myDSC = new DataStoreConfig(
+    	    		dataStoreName,
+    	    		(String)details.get("DataStoreType")
+    	            );
+    	}
+    	
+    	myDSC.setEnabled(Boolean.valueOf((String)details.get("Enabled")));
+    	myDSC.setNameSpaceId((String)details.get("Namespace"));
+    	myDSC.setAbstract((String)details.get("Description"));
+    	
+        Map params = buildParamMap(((List)details.get("Params")),
+        		myDSC.getFactory().getParametersInfo());
+        
+        myDSC.setConnectionParams(params);
+
+        if (myDC.getDataStores().containsKey(dataStoreName)){
+        	myDC.removeDataStore(dataStoreName);
+        }
+        
+        myDC.addDataStore(myDSC);
+        myData.load(myDC.toDTO());
+        
+        saveConfiguration();
+        getResponse().setEntity(new StringRepresentation("Succeeded writing configuration!", MediaType.TEXT_PLAIN));
+        } catch (Exception e){
+            getResponse().setEntity(new StringRepresentation("Couldn't write config due to " + e, MediaType.TEXT_PLAIN));
+        }
+    }
+    
+    private Map simplifyParameterMap(List parameters){
+    	Map simple = new HashMap();
+    	
+        Iterator it = parameters.iterator();
+        while (it.hasNext()){
+        	Map m = (Map)it.next();
+        	simple.put(m.get("name"), m.get("value"));
+        }
+    	
+    	return simple;
+    }
+    
+    private Map buildParamMap(List paramValues, Param[] paramSpecs){
+    	Map simplified = simplifyParameterMap(paramValues);
+    	Map returnable = new HashMap();
+        for (Iterator i = simplified.keySet().iterator(); i.hasNext();) {
+            String key = (String) i.next();
+
+            Param param = DataStoreUtils.find(paramSpecs, key);
+
+            if (param == null) {
+            	// TODO: actually report the error.
+            	return null;
+            }
+
+            Object value = null;
+
+            try {
+                value = param.lookUp(simplified);
+            } catch (IOException erp) {
+            }
+
+            if ((value != null) && !"".equals(value)) {
+                returnable.put(key, value);
             }
         }
+        
+        return returnable;
+    }
+    
+    private void saveConfiguration() throws ConfigurationException{
+        getData().load(getDataConfig().toDTO());
+        XMLConfigWriter.store((DataDTO)getData().toDTO(),
+        		GeoserverDataDirectory.getGeoserverDataDirectory()
+        		);
+    }
 
-        map.put(factory.getClass().getSimpleName(), storeSpecificParameters);
+    public boolean allowDelete(){
+        return true;
+    }
 
-        return map;
+    public void handleDelete(){
+        String name = (String)getRequest().getAttributes().get("datastore");
+        if (myDC.getDataStores().containsKey(name)){
+            myDC.removeDataStore(name);
+            getResponse().setStatus(Status.SUCCESS_OK);
+            getResponse().setEntity(new StringRepresentation(name + " found and deleted!",
+                        MediaType.TEXT_PLAIN)
+            );
+            
+        } else {
+            getResponse().setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+            getResponse().setEntity(new StringRepresentation(name + " not found!",
+                        MediaType.TEXT_PLAIN
+                        )
+            );
+        }
     }
 }
-
