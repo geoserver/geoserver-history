a616a622c37bd4d80efbe12e597c8a8e4198cd33
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-02-08 03:44:25 +0000

    tweaked h2 loading script
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8366 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: dae3f515c8125b84cb5049b6adbb79cd8c0f35af [formerly 848871b0832e199878228e96213770a5957ee035]
    Former-commit-id: e92e45db7e5978bdfed4aebc5a4376c0991932da

--- configuration/citewfs-1.1-h2/h2.sh
+++ configuration/citewfs-1.1-h2/h2.sh
@@ -1,6 +1,6 @@
-H2_HOME="/opt/h2"
-M2_REPO="/home/jdeolive/.m2/repository"
-CP="$H2_HOME/bin/h2.jar:$M2_REPO/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar:$M2_REPOy/org/geotools/gt2-main/2.4-SNAPSHOT/gt2-main-2.4-SNAPSHOT.jar:$M2_REPO/com/vividsolutions/jts/1.8/jts-1.8.jar"
+H2="$HOME/.m2/repository/org/h2database/h2/1.0-SNAPSHOT/h2-1.0-SNAPSHOT.jar"
+M2_REPO="$HOME/.m2/repository"
+CP="$H2:$M2_REPO/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar:$M2_REPOy/org/geotools/gt2-main/2.4-SNAPSHOT/gt2-main-2.4-SNAPSHOT.jar:$M2_REPO/com/vividsolutions/jts/1.8/jts-1.8.jar"
 SF="2"
 
 if [ "load" = "$1" ]; then
