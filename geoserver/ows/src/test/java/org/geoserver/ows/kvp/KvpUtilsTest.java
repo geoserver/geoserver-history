4f5bd963b4f8d3bc9ed5cd04b87bbc13234f44a2
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-02-25 15:31:10 +0000

    GEOS-1763, replaced string literals by known tokenizers where appropriate, and un-deprecated readFlat(String, String)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8445 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a109551055efa1b54c02e72466cd10751c90bd87 [formerly 24a0ef7c26edd65d76abe550f55860103a42769a]
    Former-commit-id: a35b4164869e58dd700aaae687e9ebcffa62210a

--- geoserver/ows/src/test/java/org/geoserver/ows/kvp/KvpUtilsTest.java
+++ geoserver/ows/src/test/java/org/geoserver/ows/kvp/KvpUtilsTest.java
@@ -31,4 +31,47 @@ public class KvpUtilsTest extends TestCase {
     	assertEquals(1, result.size());
     	assertEquals(0, ((List) result.get(0)).size());
     }
+    
+    public void testWellKnownTokenizers(){
+        String[] expected;
+        List actual;
+        
+        expected = new String[]{"1", "2", "3", ""};
+        actual = KvpUtils.readFlat("1,2,3,", KvpUtils.INNER_DELIMETER);
+        assertKvp(expected, actual);
+
+        expected = new String[]{"abc", "def", ""};
+        actual = KvpUtils.readFlat("(abc)(def)()", KvpUtils.OUTER_DELIMETER);
+        assertKvp(expected, actual);
+
+        expected = new String[]{"", "A=1", "B=2", ""};
+        actual = KvpUtils.readFlat(";A=1;B=2;", KvpUtils.CQL_DELIMITER);
+        assertKvp(expected, actual);
+
+        expected = new String[]{"ab", "cd", "ef", ""};
+        actual = KvpUtils.readFlat("ab&cd&ef&", KvpUtils.KEYWORD_DELIMITER);
+        assertKvp(expected, actual);
+
+        expected = new String[]{"A", "1 "};
+        actual = KvpUtils.readFlat("A=1 ", KvpUtils.VALUE_DELIMITER);
+        assertKvp(expected, actual);
+    }
+    
+    public void testRadFlatUnkownDelimiter(){
+        List actual;
+        
+        final String[] expected = new String[]{"1", "2", "3", ""};
+        actual = KvpUtils.readFlat("1^2^3^", "\\^");
+        assertKvp(expected, actual);
+
+        actual = KvpUtils.readFlat("1-2-3-", "-");
+        assertKvp(expected, actual);
+    }
+    
+    private void assertKvp(String[] expected, List actual){
+        List expectedList = Arrays.asList(expected);
+        assertEquals(expectedList.size(), actual.size());
+        assertEquals(expectedList, actual);
+    }
+    
 }
