140c2851dd107c85a5b84f2b7a8fbf71ff0725b7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-03-12 17:53:24 +0000

    Added test case
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8550 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b54022eb97e691e0ab5eeb0e0caa83f15808c0db [formerly 926884fb5dd137741435b0d2f65207a7ce64d56d]
    Former-commit-id: 1caf7e3cfe0db0fbee15862dafb5164308835fec

---
+++ community/geosync/src/test/java/org/geoserver/geosync/GeoSyncControllerTest.java
@@ -0,0 +1,29 @@
+package org.geoserver.geosync;
+
+import org.geoserver.test.GeoServerTestSupport;
+import org.w3c.dom.Document;
+
+public class GeoSyncControllerTest extends GeoServerTestSupport {
+
+   
+    public void testFeed() throws Exception {
+        String insert = "<wfs:Transaction service=\"WFS\" version=\"1.1.0\" "
+            + "xmlns:cgf=\"http://www.opengis.net/cite/geometry\" "
+            + "xmlns:ogc=\"http://www.opengis.net/ogc\" "
+            + "xmlns:wfs=\"http://www.opengis.net/wfs\" "
+            + "xmlns:gml=\"http://www.opengis.net/gml\"> "
+            + "<wfs:Insert srsName=\"EPSG:32615\"> " + "<cgf:Points>" + "<cgf:pointProperty>"
+            + "<gml:Point>" + "<gml:pos>1 1</gml:pos>" + "</gml:Point>"
+            + "</cgf:pointProperty>" + "<cgf:id>t0003</cgf:id>" + "</cgf:Points>"
+            + "</wfs:Insert>" + "</wfs:Transaction>";
+        
+        Document d = postAsDOM( "wfs", insert );
+        assertEquals( "wfs:TransactionResponse", d.getDocumentElement().getNodeName() );
+        
+        d = getAsDOM("/history?request=feed");
+        assertEquals( "feed", d.getDocumentElement().getNodeName() );
+        
+        assertNotNull( getFirstElementByTagName( d, "wfs:Insert" ) );
+            
+    }
+}
