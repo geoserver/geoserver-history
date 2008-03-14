382827f3dbbfba4d52ec23e92e96c7c2087b3b8f
arneke <arneke@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-03-14 21:57:36 +0000

    Cosmetic cleanup of WMS Reflector code, correct indentation
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8567 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 12d28c739d9204c4034e626c892e7d84d37fc45e [formerly 6a5a396cb9049b263828d9a5a9462e5a71e99b26]
    Former-commit-id: 2a1b7827ac3f1e524b81f96f247b2e9bd3d632ad

--- geoserver/wms/src/main/java/org/geoserver/wms/DefaultWebMapService.java
+++ geoserver/wms/src/main/java/org/geoserver/wms/DefaultWebMapService.java
@@ -4,7 +4,10 @@
  */
 package org.geoserver.wms;
 
-import com.vividsolutions.jts.geom.Envelope;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.geometry.jts.ReferencedEnvelope;
@@ -35,14 +38,10 @@ import org.vfny.geoserver.wms.servlets.GetFeatureInfo;
 import org.vfny.geoserver.wms.servlets.GetLegendGraphic;
 import org.vfny.geoserver.wms.servlets.GetMap;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.vividsolutions.jts.geom.Envelope;
 
-
-
-public class DefaultWebMapService implements WebMapService, ApplicationContextAware {
+public class DefaultWebMapService implements WebMapService,
+        ApplicationContextAware {
     /**
      * default for 'format' parameter.
      */
@@ -76,27 +75,28 @@ public class DefaultWebMapService implements WebMapService, ApplicationContextAw
     /**
      * default for 'bbox' paramter
      */
-    public static ReferencedEnvelope BBOX = new ReferencedEnvelope(new Envelope(-180, 180, -90, 90),
-            DefaultGeographicCRS.WGS84);
-    
-    
+    public static ReferencedEnvelope BBOX = new ReferencedEnvelope(
+            new Envelope(-180, 180, -90, 90), DefaultGeographicCRS.WGS84);
+
     /**
      * The current getMap request
      */
     GetMapRequest getMap = null;
-    
+
     /**
      * Application context
      */
     ApplicationContext context;
 
     public void setApplicationContext(ApplicationContext context)
-        throws BeansException {
+            throws BeansException {
         this.context = context;
     }
 
-    public WMSCapabilitiesResponse getCapabilities(WMSCapabilitiesRequest request) {
-        Capabilities capabilities = (Capabilities) context.getBean("wmsGetCapabilities");
+    public WMSCapabilitiesResponse getCapabilities(
+            WMSCapabilitiesRequest request) {
+        Capabilities capabilities = (Capabilities) context
+                .getBean("wmsGetCapabilities");
 
         return (WMSCapabilitiesResponse) capabilities.getResponse();
     }
@@ -106,7 +106,8 @@ public class DefaultWebMapService implements WebMapService, ApplicationContextAw
     }
 
     public DescribeLayerResponse describeLayer(DescribeLayerRequest request) {
-        DescribeLayer describeLayer = (DescribeLayer) context.getBean("wmsDescribeLayer");
+        DescribeLayer describeLayer = (DescribeLayer) context
+                .getBean("wmsDescribeLayer");
 
         return (DescribeLayerResponse) describeLayer.getResponse();
     }
@@ -122,18 +123,21 @@ public class DefaultWebMapService implements WebMapService, ApplicationContextAw
     }
 
     public GetFeatureInfoResponse getFeatureInfo(GetFeatureInfoRequest request) {
-        GetFeatureInfo getFeatureInfo = (GetFeatureInfo) context.getBean("wmsGetFeatureInfo");
+        GetFeatureInfo getFeatureInfo = 
+            (GetFeatureInfo) context.getBean("wmsGetFeatureInfo");
 
         return (GetFeatureInfoResponse) getFeatureInfo.getResponse();
     }
 
-    public GetLegendGraphicResponse getLegendGraphic(GetLegendGraphicRequest request) {
-        GetLegendGraphic getLegendGraphic = (GetLegendGraphic) context.getBean("wmsGetLegendGraphic");
+    public GetLegendGraphicResponse getLegendGraphic(
+            GetLegendGraphicRequest request) {
+        GetLegendGraphic getLegendGraphic = 
+            (GetLegendGraphic) context.getBean("wmsGetLegendGraphic");
 
         return (GetLegendGraphicResponse) getLegendGraphic.getResponse();
     }
 
-    //refector operations
+    // refector operations
     public GetMapResponse reflect(GetMapRequest request) {
         return getMapReflect(request);
     }
@@ -141,14 +145,15 @@ public class DefaultWebMapService implements WebMapService, ApplicationContextAw
     public GetMapResponse getMapReflect(GetMapRequest request) {
         getMap = (GetMapRequest) request;
 
-        //set the defaults
+        // set the defaults
         if (getMap.getFormat() == null) {
             getMap.setFormat(FORMAT);
         }
-        
+
         if ((getMap.getStyles() == null) || getMap.getStyles().isEmpty()) {
-            //set styles to be the defaults for the specified layers
-            //TODO: should this be part of core WMS logic? is so lets throw this
+            // set styles to be the defaults for the specified layers
+            // TODO: should this be part of core WMS logic? is so lets throw
+            // this
             // into the GetMapKvpRequestReader
             if ((getMap.getLayers() != null) && (getMap.getLayers().length > 0)) {
                 ArrayList styles = new ArrayList(getMap.getLayers().length);
@@ -162,176 +167,179 @@ public class DefaultWebMapService implements WebMapService, ApplicationContextAw
                 getMap.setStyles(STYLES);
             }
         }
-        
+
         this.autoSetBoundsAndSize();
-        
+
         return getMap(getMap);
     }
-    
+
     /**
      * This method tries to automatically determine SRS, bounding box and output
-     *  size based on the layers provided by the user and any other parameters.
+     * size based on the layers provided by the user and any other parameters.
      * 
      * If bounds are not specified by the user, they are automatically se to the
-     * union of the bounds of all layers. 
+     * union of the bounds of all layers.
      * 
      * The size of the output image defaults to 512 pixels, the height is
-     * automatically determined based on the width to height ratio of the 
+     * automatically determined based on the width to height ratio of the
      * requested layers. This is also true if either height or width are
-     * specified by the user. If both height and width are specified by the user,
-     * the automatically determined bounding box will be adjusted to fit inside 
-     * these bounds.
+     * specified by the user. If both height and width are specified by the
+     * user, the automatically determined bounding box will be adjusted to fit
+     * inside these bounds.
      */
     private void autoSetBoundsAndSize() {
-    	// Get the layers
+        // Get the layers
         MapLayerInfo[] layers = getMap.getLayers();
-        
-   	 	// Determine the SRS first
+
+        // Determine the SRS first
         String reqSRS = getMap.getSRS();
         CoordinateReferenceSystem reqCRS = getMap.getCrs();
         boolean useNativeBounds = true;
-        if(getMap.getSRS() == null || reqSRS.equalsIgnoreCase("EPSG:4326")) {
-        	useNativeBounds = false;
-        	reqSRS = "EPSG:4326";
-        	getMap.setSRS(reqSRS);
-			try {
-				reqCRS = CRS.decode(reqSRS);
-			} catch (NoSuchAuthorityCodeException e) {
-				e.printStackTrace();
-			} catch (FactoryException e) {
-				e.printStackTrace();
-			}
-			getMap.setCrs(reqCRS);
+        if (getMap.getSRS() == null || reqSRS.equalsIgnoreCase("EPSG:4326")) {
+            useNativeBounds = false;
+            reqSRS = "EPSG:4326";
+            getMap.setSRS(reqSRS);
+            try {
+                reqCRS = CRS.decode(reqSRS);
+            } catch (NoSuchAuthorityCodeException e) {
+                e.printStackTrace();
+            } catch (FactoryException e) {
+                e.printStackTrace();
+            }
+            getMap.setCrs(reqCRS);
         } else {
-        	for(int i=0; useNativeBounds && i<layers.length; i++) {
+            for (int i = 0; useNativeBounds && i < layers.length; i++) {
                 if (layers[i] == null) {
                     continue;
                 } else if (useNativeBounds = layers[i].getFeature() != null) {
                     useNativeBounds = layers[i].getFeature().getSRS()
-                    .equalsIgnoreCase(reqSRS);
+                            .equalsIgnoreCase(reqSRS);
                 } else if (layers[i].getRemoteFeatureSource() != null) {
                     // Not sure about this, but how to find the SRS?
                     useNativeBounds = false;
                 } else if (layers[i].getCoverage() != null) {
                     useNativeBounds = false;
                 } else {
-                    //?
+                    // ?
                 }
-        	}
+            }
         }
-        		
-		// Ready to determine the bounds based on the layers, if not specified
+
+        // Ready to determine the bounds based on the layers, if not specified
         Envelope layerbbox = null;
-        
+
         boolean specifiedBbox = (getMap.getBbox() != null);
-        
-        if(specifiedBbox) {
-        	layerbbox = getMap.getBbox();
+
+        if (specifiedBbox) {
+            layerbbox = getMap.getBbox();
         } else {
-        	// Get the bounding box from the layers
-        	for(int i=0; i<layers.length; i++) {
-        		Envelope curbbox = null;
-        		
-        		FeatureTypeInfo curFTI = layers[i].getFeature();
-        		try {
-        			if (curFTI != null) {
+            // Get the bounding box from the layers
+            for (int i = 0; i < layers.length; i++) {
+                Envelope curbbox = null;
+
+                FeatureTypeInfo curFTI = layers[i].getFeature();
+                try {
+                    if (curFTI != null) {
                         // Local feature type
                         if (useNativeBounds) {
-                                curbbox = curFTI.getLatLongBoundingBox();
+                            curbbox = curFTI.getLatLongBoundingBox();
                         } else {
-                                curbbox = curFTI.getBoundingBox();
+                            curbbox = curFTI.getBoundingBox();
                         }
 
-        			} else if (layers[i].getRemoteFeatureSource() != null) {
-                        // This layer was requested through a remote SLD or something similar
-                        curbbox = layers[i].getRemoteFeatureSource()
-                        .getBounds();
+                    } else if (layers[i].getRemoteFeatureSource() != null) {
+                        // This layer was requested through a remote SLD or
+                        // something similar
+                        curbbox = 
+                            layers[i].getRemoteFeatureSource().getBounds();
 
-        			} else if (layers[i].getCoverage() != null) {
+                    } else if (layers[i].getCoverage() != null) {
                         // This is a coverage?
-                        // The following is awfully convoluted... too many Envelope classes
+                        // The following is awfully convoluted... too many
+                        // Envelope classes
                         GeneralEnvelope genEnv = layers[i].getCoverage()
-                        .getWGS84LonLatEnvelope();
+                                .getWGS84LonLatEnvelope();
                         double[] ll = genEnv.getLowerCorner().getCoordinates();
                         double[] ur = genEnv.getUpperCorner().getCoordinates();
                         curbbox = new Envelope(ll[0], ll[1], ur[0], ur[1]);
-        			} else {
+                    } else {
                         // ?
-        			}	
-        		} catch(IOException e) {
-    				e.printStackTrace();
-    			}
-        		
-        		if(i == 0) {
-        			layerbbox = new Envelope(curbbox);
-        		} else {
-        			layerbbox.expandToInclude(curbbox);
-        		}
-        	}
-        	// Reproject if we have to
-       		if(! useNativeBounds) {
-       			try {
-        			ReferencedEnvelope ref = new ReferencedEnvelope(layerbbox, CRS.decode("EPSG:4326"));      	
-        			layerbbox = ref.transform(reqCRS, true);
-       			} catch (NoSuchAuthorityCodeException e) {
-       				e.printStackTrace();
-       			} catch (TransformException e) {
-       				e.printStackTrace();
-       			} catch (FactoryException e) {
-       				e.printStackTrace();
-				}
-       		}
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (i == 0) {
+                    layerbbox = new Envelope(curbbox);
+                } else {
+                    layerbbox.expandToInclude(curbbox);
+                }
+            }
+            // Reproject if we have to
+            if (!useNativeBounds) {
+                try {
+                    ReferencedEnvelope ref = new ReferencedEnvelope(layerbbox,
+                            CRS.decode("EPSG:4326"));
+                    layerbbox = ref.transform(reqCRS, true);
+                } catch (NoSuchAuthorityCodeException e) {
+                    e.printStackTrace();
+                } catch (TransformException e) {
+                    e.printStackTrace();
+                } catch (FactoryException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-       
+
         // Just in case
-        if(layerbbox == null) layerbbox = BBOX;
-        
+        if (layerbbox == null)
+            layerbbox = BBOX;
+
         double bbheight = layerbbox.getHeight();
         double bbwidth = layerbbox.getWidth();
-        double bbratio = bbwidth/bbheight;
-        
-        if(!specifiedBbox) {
-        	// Zoom out 4%, accomodates for reprojection etc
-        	layerbbox.expandBy(layerbbox.getWidth()/50, layerbbox.getHeight()/50);
-    	}
-        
-        
+        double bbratio = bbwidth / bbheight;
+
+        if (!specifiedBbox) {
+            // Zoom out 4%, accomodates for reprojection etc
+            layerbbox.expandBy(layerbbox.getWidth() / 50,
+                    layerbbox.getHeight() / 50);
+        }
+
         double mheight = getMap.getHeight();
         double mwidth = getMap.getWidth();
-        
-        if(mheight > 0.5 && mwidth > 0.5 && specifiedBbox) {
-        	// This person really doesnt want our help
-        } else {
-        	if(mheight > 0.5 && mwidth > 0.5) {
-        		// Fully specified, need to adjust bbox
-        		double mratio = mwidth/mheight;
-        		// Adjust bounds to be less than ideal to meet spec
-        		if(bbratio > mratio) {
-        			// Too wide, need to increase height of bb
-        			double diff = ((bbwidth / mratio ) - bbheight)/2;
-        			layerbbox.expandBy(0, diff);
-        		} else {
-        			// Too tall, need to increase width of bb
-        			double diff = ((bbheight * mratio ) - bbwidth)/2;
-        			layerbbox.expandBy(diff, 0);
-        		}
-        	} else if(mheight > 0.5) {
-        		mwidth = bbratio * mheight;
-        	} else { 
-        		if(mwidth > 0.5) {
-        			// We're set
-        		} else {
-        			// Fall through to the default
-        			mwidth = WIDTH;
-        		}
-        		mheight = mwidth / bbratio;
-        	}
-        	
 
-        	// Actually set the bounding box and size of image
-        	getMap.setBbox(layerbbox);    
-        	getMap.setWidth((int)mwidth);
-        	getMap.setHeight((int)mheight);
+        if (mheight > 0.5 && mwidth > 0.5 && specifiedBbox) {
+            // This person really doesnt want our help
+        } else {
+            if (mheight > 0.5 && mwidth > 0.5) {
+                // Fully specified, need to adjust bbox
+                double mratio = mwidth / mheight;
+                // Adjust bounds to be less than ideal to meet spec
+                if (bbratio > mratio) {
+                    // Too wide, need to increase height of bb
+                    double diff = ((bbwidth / mratio) - bbheight) / 2;
+                    layerbbox.expandBy(0, diff);
+                } else {
+                    // Too tall, need to increase width of bb
+                    double diff = ((bbheight * mratio) - bbwidth) / 2;
+                    layerbbox.expandBy(diff, 0);
+                }
+            } else if (mheight > 0.5) {
+                mwidth = bbratio * mheight;
+            } else {
+                if (mwidth > 0.5) {
+                    // We're set
+                } else {
+                    // Fall through to the default
+                    mwidth = WIDTH;
+                }
+                mheight = mwidth / bbratio;
+            }
+
+            // Actually set the bounding box and size of image
+            getMap.setBbox(layerbbox);
+            getMap.setWidth((int) mwidth);
+            getMap.setHeight((int) mheight);
         }
-    }     
+    }
 }
