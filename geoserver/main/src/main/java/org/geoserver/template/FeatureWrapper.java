0d4d6034de3058a9d6f69e99c1f92ebd614ba572
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-03-20 20:24:04 +0000

    applying formatting before changing code for GEOS-1819
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.6.x@8614 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 71e47aaf5688ab37733524c503c59837e9c6525c [formerly f9c1212f278e2593d6497ffd6fa86207f2236f86]
    Former-commit-id: 4086adb73c74620d8676720bef09300a6f0a3515

--- geoserver/main/src/main/java/org/geoserver/template/FeatureWrapper.java
+++ geoserver/main/src/main/java/org/geoserver/template/FeatureWrapper.java
@@ -22,10 +22,8 @@ import freemarker.ext.beans.BeansWrapper;
 import freemarker.ext.beans.CollectionModel;
 import freemarker.template.Configuration;
 import freemarker.template.SimpleHash;
-import freemarker.template.SimpleSequence;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
-
 
 /**
  * Wraps a {@link Feature} in the freemarker {@link BeansWrapper} interface
@@ -33,62 +31,67 @@ import freemarker.template.TemplateModelException;
  * {@link FeatureCollection}.
  * <p>
  * When a {@link FeatureCollection} is being processed by the template, it is
- * available via the <code>$features</code> variable, which can be broken down into single features and attributes following this hierarchy:
+ * available via the <code>$features</code> variable, which can be broken down
+ * into single features and attributes following this hierarchy:
  * <ul>
- *   <li>features -> feature</li>
- *     <ul>
- *       <li>fid (String)</li>
- *       <li>typeName (String)</li>
- *       <li>attributes -> attribute</li>
- *       <ul>
- *         <li>value (String), a default String representation of the attribute value</li>
- *         <li>rawValue (Object), the actual attribute value if it's non null, the empty string otherwise</li>
- *         <li>name (String)</li>
- *         <li>type (String)</li>
- *         <li>isGeometry (Boolean)</li>
- *       </ul>
- *     </ul>
+ * <li>features -> feature</li>
+ * <ul>
+ * <li>fid (String)</li>
+ * <li>typeName (String)</li>
+ * <li>attributes -> attribute</li>
+ * <ul>
+ * <li>value (String), a default String representation of the attribute value</li>
+ * <li>rawValue (Object), the actual attribute value if it's non null, the
+ * empty string otherwise</li>
+ * <li>name (String)</li>
+ * <li>type (String)</li>
+ * <li>isGeometry (Boolean)</li>
  * </ul>
- * Example of a template processing a feature collection which will print
- * out the features id of every feature in the collection.
+ * </ul>
+ * </ul>
+ * Example of a template processing a feature collection which will print out
+ * the features id of every feature in the collection.
+ * 
  * <pre><code>
  *  &lt;#list features as feature&gt;
  *  FeatureId: ${feature.fid}
  *  &lt;/#list&gt;
  * </code></pre>
+ * 
  * </p>
  * <p>
- * To use this wrapper,use the {@link Configuration#setObjectWrapper(freemarker.template.ObjectWrapper)}
+ * To use this wrapper,use the
+ * {@link Configuration#setObjectWrapper(freemarker.template.ObjectWrapper)}
  * method:
+ * 
  * <pre>
  *         <code>
  *  //features we want to apply template to
  *  FeatureCollection features = ...;
- *
  *  //create the configuration and set the wrapper
  *  Configuration cfg = new Configuration();
  *  cfg.setObjectWrapper( new FeatureWrapper() );
- *
  *  //get the template and go
- *  Template template = cfg.getTemplate( "foo.ftl" );
+ *  Template template = cfg.getTemplate( &quot;foo.ftl&quot; );
  *  template.process( features, System.out );
- *
- *         </code>
+ * </code>
  * </pre>
+ * 
  * </p>
  * </p>
+ * 
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  * @author Andrea Aime, TOPP
- *
+ * 
  */
 public class FeatureWrapper extends BeansWrapper {
     public FeatureWrapper() {
         setSimpleMapWrapper(true);
     }
-    
+
     /**
-     * Returns a sensible String value for attributes so they are
-     * easily used by templates.
+     * Returns a sensible String value for attributes so they are easily used by
+     * templates.
      * <p>
      * Special cases:
      * <ul>
@@ -96,30 +99,31 @@ public class FeatureWrapper extends BeansWrapper {
      * <li>for Boolean values returns "true" or "false"</li>
      * <li>for null values returns an empty string</li>
      * <li>for any other value returns its toString()</li>
-     * </ul> 
+     * </ul>
      * </p>
      * 
-     * @param o could be an instance of Date (a special case)
+     * @param o
+     *            could be an instance of Date (a special case)
      * @return the formated date as a String, or the object
      */
     protected String wrapValue(Object o) {
-        if(o == null){
-            //nulls throw tempaltes off, use empty string
+        if (o == null) {
+            // nulls throw tempaltes off, use empty string
             return "";
         }
-    	if ( o instanceof Date ) { 
-    	    return DateFormat.getInstance().format( (Date)o ); 
-    	}
-    	if( o instanceof Boolean){
-    	    return ((Boolean)o).booleanValue()? "true" : "false";
-    	}
-    	return String.valueOf(o);
+        if (o instanceof Date) {
+            return DateFormat.getInstance().format((Date) o);
+        }
+        if (o instanceof Boolean) {
+            return ((Boolean) o).booleanValue() ? "true" : "false";
+        }
+        return String.valueOf(o);
     }
 
     public TemplateModel wrap(Object object) throws TemplateModelException {
-        //check for feature collection
+        // check for feature collection
         if (object instanceof FeatureCollection) {
-            //create a model with just one variable called 'features'
+            // create a model with just one variable called 'features'
             SimpleHash map = new SimpleHash();
             map.put("features", new CollectionModel((FeatureCollection) object, this));
             map.put("type", wrap(((FeatureCollection) object).getSchema()));
@@ -127,8 +131,8 @@ public class FeatureWrapper extends BeansWrapper {
             return map;
         } else if (object instanceof FeatureType) {
             FeatureType ft = (FeatureType) object;
-            
-            // create a variable "attributes" which his a list of all the 
+
+            // create a variable "attributes" which his a list of all the
             // attributes, but at the same time, is a map keyed by name
             Map attributeMap = new LinkedHashMap();
             for (int i = 0; i < ft.getAttributeCount(); i++) {
@@ -142,7 +146,8 @@ public class FeatureWrapper extends BeansWrapper {
                 attributeMap.put(type.getLocalName(), attribute);
             }
 
-            // build up the result, feature type is represented by its name an attributes
+            // build up the result, feature type is represented by its name an
+            // attributes
             SimpleHash map = new SimpleHash();
             map.put("attributes", new SequenceMapModel(attributeMap, this));
             map.put("name", ft.getTypeName());
@@ -150,14 +155,15 @@ public class FeatureWrapper extends BeansWrapper {
         } else if (object instanceof Feature) {
             Feature feature = (Feature) object;
 
-            //create the model
+            // create the model
             SimpleHash map = new SimpleHash();
 
-            //first add the feature id
+            // first add the feature id
             map.put("fid", feature.getID());
             map.put("typeName", feature.getFeatureType().getTypeName());
 
-            //next add variables for each attribute, variable name = name of attribute
+            // next add variables for each attribute, variable name = name of
+            // attribute
             Map attributeMap = new LinkedHashMap();
 
             for (int i = 0; i < feature.getNumberOfAttributes(); i++) {
@@ -166,10 +172,11 @@ public class FeatureWrapper extends BeansWrapper {
                 Map attribute = new HashMap();
                 Object value = feature.getAttribute(i);
                 attribute.put("value", wrapValue(value));
-                if ( value == null ) {
-                    //some special case checks
+                if (value == null) {
+                    // some special case checks
                     attribute.put("rawValue", "");
-                    attribute.put("isGeometry", Boolean.valueOf(Geometry.class.isAssignableFrom(attDescriptor.getBinding())));
+                    attribute.put("isGeometry", Boolean.valueOf(Geometry.class
+                            .isAssignableFrom(attDescriptor.getBinding())));
                 } else {
                     attribute.put("rawValue", value);
                     attribute.put("isGeometry", Boolean.valueOf(value instanceof Geometry));
@@ -182,13 +189,13 @@ public class FeatureWrapper extends BeansWrapper {
                 attributeMap.put(attDescriptor.getLocalName(), attribute);
             }
 
-            // create a variable "attributes" which his a list of all the 
+            // create a variable "attributes" which his a list of all the
             // attributes, but at the same time, is a map keyed by name
             map.put("attributes", new SequenceMapModel(attributeMap, this));
 
             return map;
         }
-        
+
         return super.wrap(object);
     }
 }
