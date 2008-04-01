be45034f9b4df90bcdde7f2b558f51030d4ecef3
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-01 14:13:30 +0000

    Return URLs for layers that have just been created
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.6.x@8690 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 71845b42d21ea669fd1d5c265824bd391141af3a [formerly 2489b4847a2b40a9afcf1fe9a0bb4d3c5116325f]
    Former-commit-id: 0fd2176de2492e18d4d679c20463df572ff892d5

--- geoserver/community/csv/src/main/java/org/geoserver/csv/rest/DataLayersResource.java
+++ geoserver/community/csv/src/main/java/org/geoserver/csv/rest/DataLayersResource.java
@@ -3,29 +3,34 @@ package org.geoserver.csv.rest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
-import org.apache.commons.fileupload.DefaultFileItemFactory;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.io.IOUtils;
 import org.geoserver.csv.CsvService;
+import org.geoserver.csv.LayerResult;
+import org.geoserver.rest.JSONFormat;
+import org.restlet.data.MediaType;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.ext.fileupload.RestletFileUpload;
-import org.vfny.geoserver.global.GeoserverDataDirectory;
 
 public class DataLayersResource extends AbstractLayersResource {
     public DataLayersResource(CsvService csvService) {
         super(csvService);
     }
-    
+
     @Override
     public boolean allowPost() {
         return true;
     }
-    
+
     @Override
     protected List<String> getLayers() throws IOException {
         return csvService.getDataLayers();
@@ -35,43 +40,66 @@ public class DataLayersResource extends AbstractLayersResource {
         final Response resp = getResponse();
         File csv = null;
         try {
-            FileItemFactory factory = new DefaultFileItemFactory();
+            FileItemFactory factory = new DiskFileItemFactory();
             RestletFileUpload upload = new RestletFileUpload(factory);
             List<FileItem> items = upload.parseRequest(getRequest());
-            
-            
-            
+
+            // lookup the parameters, the file, the geometry layer name, and the
+            // join field
             FileItem file = null;
             String geometryLayer = null;
-
+            String joinField = null;
             for (FileItem fileItem : items) {
-                if(fileItem.getFieldName().equalsIgnoreCase("geometryLayer"))
+                if (fileItem.getFieldName().equalsIgnoreCase("geometryLayer"))
                     geometryLayer = fileItem.getString();
-                else if(fileItem.getFieldName().equalsIgnoreCase("file"))
+                else if (fileItem.getFieldName().equalsIgnoreCase("file"))
                     file = fileItem;
+                else if (fileItem.getFieldName().equalsIgnoreCase("joinField"))
+                    joinField = fileItem.getString();
             }
-            
-            if(file == null) {
-                resp.setEntity("'file' parameter is required but not provided");
+
+            if (file == null) {
+                resp.setEntity("'file' parameter is required but not provided", MediaType.TEXT_PLAIN);
                 resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
                 return;
-            } else if(geometryLayer == null) {
-                resp.setEntity("'geometryLayer' parameter is required but not provided");
+            } else if (geometryLayer == null) {
+                resp
+                        .setEntity("'geometryLayer' parameter is required but not provided", MediaType.TEXT_PLAIN);
                 resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
                 return;
-            } else if(!csvService.getGeometryLayers().contains(geometryLayer)) {
-                resp.setEntity("geometryLayer " + geometryLayer + " is unknown, valid values are " + csvService.getGeometryLayers());
+            } else if (joinField == null) {
+                resp
+                        .setEntity("'joinField' parameter is required but not provided", MediaType.TEXT_PLAIN);
+                resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+                return;
+            } else if (!csvService.getGeometryLayers().contains(geometryLayer)) {
+                resp.setEntity("geometryLayer " + geometryLayer
+                        + " is unknown, valid values are "
+                        + csvService.getGeometryLayers(), MediaType.TEXT_PLAIN);
                 resp.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
                 return;
             }
-            
-             csv = File.createTempFile("csv", ".csv");
-             IOUtils.copy(file.getInputStream(), new FileOutputStream(csv));
-             csvService.configureCsvFile(geometryLayer, "zcta", csv);
+
+            csv = File.createTempFile("csv", ".csv");
+            IOUtils.copy(file.getInputStream(), new FileOutputStream(csv));
+            List results = csvService.configureCsvFile(geometryLayer,
+                    joinField, csv);
+            JSONFormat format = new JSONFormat(MediaType.TEXT_PLAIN);
+            resp.setEntity(format.makeRepresentation(convertToMapList(results)));
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error occurred managing csv file upload request", e);
+            LOGGER.log(Level.SEVERE,
+                    "Error occurred managing csv file upload request", e);
             resp.setStatus(Status.SERVER_ERROR_INTERNAL);
+            resp.setEntity(e.getMessage(), MediaType.TEXT_PLAIN);
         }
+    }
+
+    private Object convertToMapList(List<LayerResult> layers) {
+        List<Map> results = new ArrayList<Map>();
+        for (LayerResult layer : layers) {
+            results.add(Collections.singletonMap(layer.getLayerName(), getRequest().getResourceRef().toString() + "/" + layer.getLayerName()));
+        }
+        return results;
     }
 
 }
