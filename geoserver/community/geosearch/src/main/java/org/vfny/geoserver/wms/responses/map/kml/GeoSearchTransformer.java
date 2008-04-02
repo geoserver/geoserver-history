30b20cee20c6d7831c932db6984f6fe2647fc2db
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-02 16:00:39 +0000

    Fleshed out the custom KML code a bit, but it only inserts a static URL for now
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8718 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a31bd6965458b83093e63bab91ab0d181eb6a441 [formerly 8dd6fb1ad136eeae0dfefebf8fbee91c7ac00a4f]
    Former-commit-id: 7ee783e58a50cee93d9efad61c99e5892c8f1045

--- geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/GeoSearchTransformer.java
+++ geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/GeoSearchTransformer.java
@@ -1,12 +1,25 @@
 package org.vfny.geoserver.wms.responses.map.kml;
 
 import org.vfny.geoserver.wms.WMSMapContext;
+import org.xml.sax.ContentHandler;
 import org.geotools.map.MapLayer;
+import org.geotools.xml.transform.Translator;
 
 public class GeoSearchTransformer extends KMLTransformer {
 
-    protected GeoSearchVectorTransformer createVectorTransformer(WMSMapContext mapContext, MapLayer layer){
-        return new GeoSearchVectorTransformer(mapContext, layer);
+    public Translator createTranslator(ContentHandler handler){
+        return new KML3Translator(handler);
+    }
+
+    protected class KML3Translator extends KMLTranslator {
+        public KML3Translator(ContentHandler handler){
+            super(handler);
+        }
+
+        protected KMLVectorTransformer createVectorTransformer(WMSMapContext mapContext,
+                MapLayer layer){
+            return new GeoSearchVectorTransformer(mapContext, layer);
+        }
     }
 }
 
