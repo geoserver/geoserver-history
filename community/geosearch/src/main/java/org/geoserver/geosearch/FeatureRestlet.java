4b4a6a49ab2c2f6c8f41c6b8478dadc718408d85
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-04 15:46:21 +0000

    refactored feature reslet, handles both layer and feature id case
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8767 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e5dc78918efc8a88fe8ab20794e6a151ff4e94ff [formerly cc1c31aefb1f383f71d88824ad884d9d575d666f]
    Former-commit-id: 8ab98463d39c48a17c7763aef3e25b47b34edcac

--- community/geosearch/src/main/java/org/geoserver/geosearch/FeatureRestlet.java
+++ community/geosearch/src/main/java/org/geoserver/geosearch/FeatureRestlet.java
@@ -2,181 +2,127 @@ package org.geoserver.geosearch;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.logging.Logger;
 
-import org.geoserver.ows.KvpParser;
-import org.geoserver.ows.util.CaseInsensitiveMap;
-import org.geoserver.platform.GeoServerExtensions;
+import org.geoserver.ows.util.KvpMap;
+import org.geoserver.ows.util.KvpUtils;
 import org.geoserver.rest.RESTUtils;
+import org.geoserver.rest.RestletException;
+import org.geoserver.wms.WebMapService;
 import org.geoserver.wms.kvp.GetMapKvpRequestReader;
 import org.geotools.util.logging.Logging;
+import org.restlet.Restlet;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.restlet.resource.OutputRepresentation;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.vfny.geoserver.config.DataConfig;
 import org.vfny.geoserver.global.Data;
+import org.vfny.geoserver.global.FeatureTypeInfo;
+import org.vfny.geoserver.global.NameSpaceInfo;
 import org.vfny.geoserver.global.WMS;
 import org.vfny.geoserver.wms.requests.GetMapRequest;
 import org.vfny.geoserver.wms.responses.GetMapResponse;
-import org.vfny.geoserver.wms.responses.map.kml.GeoSearchMapProducerFactory;
-import org.vfny.geoserver.wms.servlets.GetMap;
 
-public class FeatureRestlet extends GeoServerProxyAwareRestlet implements ApplicationContextAware {
+public class FeatureRestlet extends Restlet {
     private static Logger LOGGER = Logging.getLogger("org.geoserver.geosearch");
 
-    private Data myData;
-    private DataConfig myDataConfig;
     private WMS myWMS;
-    private GetMap myGetMap;
-    private ApplicationContext myContext;
-
-    public void setData(Data d){
-        myData = d;
-    }
-
-    public void setGetMap(GetMap gm){
-        myGetMap = gm;
-    }
+    private Data catalog;
+    private WebMapService webMapService;
 
     public void setWms(WMS wms){
         myWMS = wms;
-    }
-
-    public void setDataConfig(DataConfig dc){
-        myDataConfig = dc;
-    }
-
-    public void setApplicationContext(ApplicationContext context){
-        myContext = context;
-    }
-
-    public Data getData(){
-        return myData;
-    }
-
-    public GetMap getGetMap(){
-        return myGetMap;
     }
 
     public WMS getWms(){
         return myWMS;
     }
 
-    public DataConfig getDataConfig(){
-        return myDataConfig;
+    public void setCatalog(Data catalog) {
+        this.catalog = catalog;
     }
-
-    public ApplicationContext getApplicationContext(){
-        return myContext;
+    
+    public void setWebMapService(WebMapService webMapService) {
+        this.webMapService = webMapService;
     }
-
+    
     public FeatureRestlet() {
     }
 
     public void handle(Request request, Response response){
-        try{
-            if (request.getMethod().equals(Method.GET)){
+
+        if (request.getMethod().equals(Method.GET)){
+            try {
                 doGet(request, response);
-            } else {
-                response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+            } 
+            catch (Exception e) {
+                throw new RuntimeException( e );
             }
-        } catch (Exception e){
-            LOGGER.severe("Badness (" + e + ") while handling layer request!");
-            e.printStackTrace();
-            response.setStatus(Status.SERVER_ERROR_INTERNAL);
+        } else {
+            response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
         }
+ 
     }
 
     public void doGet(Request request, Response response) throws Exception{
+        String layer = (String)request.getAttributes().get("layer");
         String namespace = (String)request.getAttributes().get("namespace");
-        String layername = (String)request.getAttributes().get("layer");
-        String featureId = (String)request.getAttributes().get("feature");
-        String format    = (String)request.getAttributes().get("format");
-        String bu = getBaseURL(request);
-        int lastslash = bu.lastIndexOf("/");
-        bu = bu.substring(0, lastslash);
-        GeoSearchMapProducerFactory.BASE_URL = bu;
+        String feature = (String)request.getAttributes().get("feature");
         
-        if (request.getMethod().equals(Method.GET)) {
-            GetMapKvpRequestReader reader = new GetMapKvpRequestReader(getWms());
-            reader.setHttpRequest( RESTUtils.getServletRequest( request ) );
-            
-            Map raw = new HashMap();
-            raw.put("layers", namespace + ":" + layername); 
-            raw.put("styles", "polygon");
-            raw.put("format", "geosearch-kml");
-            raw.put("srs", "epsg:4326");
-            raw.put("bbox", "-180,-90,180,90");
-            raw.put("height", "600");
-            raw.put("width", "800");
-            raw.put("featureid", layername + "." + featureId);
-
-            final GetMapRequest gmreq = (GetMapRequest) reader.read((GetMapRequest) reader.createRequest(), parseKvp(raw), raw);
-            
-            final GetMapResponse gmresp = new GetMapResponse(getWms(), getApplicationContext());
-            response.setEntity(new OutputRepresentation(new MediaType("application/xml+kml")){
-                    public void write(OutputStream os){
-                    try{
-                    gmresp.execute(gmreq);
-                    gmresp.writeTo(os);
-                    } catch (IOException ioe){
-                    // blah, this will never happen
-                    }
-                    }
-
-                    });
-
-        } else {
-            response.setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+        NameSpaceInfo ns = catalog.getNameSpace(namespace);
+        if ( ns == null ) {
+            throw new RestletException( "No such namespace:" + namespace, Status.CLIENT_ERROR_NOT_FOUND );
         }
-    }
-
-    /**
-     * Parses a raw set of kvp's into a parsed set of kvps.
-     *
-     * @param kvp Map of String,String.
-     */
-    protected Map parseKvp(Map /*<String,String>*/ raw)
-        throws Exception {
-            //parse the raw values
-            List parsers = GeoServerExtensions.extensions(KvpParser.class, getApplicationContext());
-            Map kvp = new CaseInsensitiveMap(new HashMap());
-
-            for (Iterator e = raw.entrySet().iterator(); e.hasNext();) {
-                Map.Entry entry = (Map.Entry) e.next();
-                String key = (String) entry.getKey();
-                String val = (String) entry.getValue();
-                Object parsed = null;
-
-                for (Iterator p = parsers.iterator(); p.hasNext();) {
-                    KvpParser parser = (KvpParser) p.next();
-
-                    if (key.equalsIgnoreCase(parser.getKey())) {
-                        parsed = parser.parse(val);
-
-                        if (parsed != null) {
-                            break;
-                        }
-                    }
-                }
-
-                if (parsed == null) {
-                    parsed = val;
-                }
-
-                kvp.put(key, parsed);
+        
+        FeatureTypeInfo featureType = null;
+        try {
+            featureType = catalog.getFeatureTypeInfo(layer,ns.getUri());
+        }
+        catch( NoSuchElementException e ) {
+            //ignore, handled later
+        }
+        if ( featureType == null ) {
+            throw new RestletException( "No such layer:" + layer, Status.CLIENT_ERROR_NOT_FOUND);    
+        }
+            
+        //create some kvp and pass through to GetMapKvpreader
+        KvpMap raw = new KvpMap();
+        raw.put("layers", namespace + ":" + layer);
+        raw.put("format", "geosearch-kml");
+        
+        if ( feature != null ) {
+            raw.put("featureid", layer + "." + feature);    
+        }
+        
+        GetMapKvpRequestReader reader = new GetMapKvpRequestReader(getWms());
+        reader.setHttpRequest( RESTUtils.getServletRequest( request ) );
+        
+        //parse into request object
+        raw = KvpUtils.normalize(raw);
+        KvpMap kvp = new KvpMap( raw );
+        KvpUtils.parse( kvp );
+        final GetMapRequest getMapRequest =  
+            (GetMapRequest) reader.read( (GetMapRequest) reader.createRequest(), kvp, raw );
+        getMapRequest.setBaseUrl( RESTUtils.getBaseURL(request));
+        
+        //delegate to wms reflector
+        final GetMapResponse getMapResponse = webMapService.reflect(getMapRequest);
+        
+        //wrap resposne in a reslet output rep
+        OutputRepresentation output = new OutputRepresentation( 
+            new MediaType("application/xml+kml")  
+        ) {
+            public void write(OutputStream outputStream) throws IOException {
+                getMapResponse.execute(getMapRequest);
+                getMapResponse.writeTo(outputStream);
             }
+        };
+        response.setEntity( output );
 
-            return kvp;
-        }
+        
+    }
 }
 
