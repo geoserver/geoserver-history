6db2a3bb74ece4f7c9d1c9e47f4e1e162ea541a0
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-16 19:27:31 +0000

    Quantile-based algorithm for attribute-based regionator
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@8855 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 821272d7604df7d7f3b939943210ac5e1913f9b2 [formerly 5455875c9f5faec42744f37c59e7a9815c47ba86]
    Former-commit-id: 6cf6d0820fa340aae87e120d3db1ba1bb7bfa7bc

--- community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
+++ community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
@@ -1,58 +1,91 @@
 package org.vfny.geoserver.wms.responses.map.kml;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.geotools.data.FeatureSource;
 import org.geotools.data.Query;
+import org.geotools.data.DefaultQuery;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
+import org.geotools.factory.CommonFactoryFinder;
+import org.opengis.filter.FilterFactory;
+import org.opengis.filter.sort.SortBy;
+import org.opengis.filter.sort.SortOrder;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.vfny.geoserver.wms.WMSMapContext;
 
 /**
- * Strategy for regionating based on algorithmic stuff related to the actual data.
- * This strategy is fairly ill-defined and should be considered highly experimental. 
+ * Strategy for regionating based on algorithmic stuff related to the actual
+ * data. This strategy is fairly ill-defined and should be considered highly
+ * experimental.
  * 
  * @author David Winslow
  */
-public class DataRegionatingStrategy implements RegionatingStrategy{
+public class DataRegionatingStrategy implements RegionatingStrategy {
 
     private static Logger LOGGER = org.geotools.util.logging.Logging.getLogger("org.geoserver.geosearch");
     private static long MAX_LEVELS = 5;
+    private static long FEATURES_PER_TILE = 5;
 
-    private Long myMin; 
+    private Long myMin;
     private Long myMax;
-    private int myZoomLevel;
 
-    public void preProcess(WMSMapContext con, int index){
+    public void preProcess(WMSMapContext con, int index) {
+        preProcessEven(con, index);
+    }
+
+    public void preProcessEven(WMSMapContext con, int index) {
         try {
-            FeatureSource<SimpleFeatureType, SimpleFeature> source = (FeatureSource<SimpleFeatureType, SimpleFeature>) con
-                .getLayer(index).getFeatureSource();
-            ReferencedEnvelope fullBounds = source.getBounds();
-            ReferencedEnvelope requestBounds = con.getAreaOfInterest();
-            try {
-                boolean reprojectBBox = 
-                    (fullBounds.getCoordinateReferenceSystem() != null)
-                    && !CRS.equalsIgnoreMetadata(
-                            requestBounds.getCoordinateReferenceSystem(),
-                            fullBounds.getCoordinateReferenceSystem()
-                            );
-                if (reprojectBBox) {
-                    requestBounds = requestBounds.transform(fullBounds
-                            .getCoordinateReferenceSystem(), true);
-                }
-            } catch (Exception e) {
+            long zoomLevel = getZoomLevel(con, index);
+            long featuresToSkip = findSkipAmount(zoomLevel);
+            long featureCount = findLayerSize(zoomLevel);
+
+            FilterFactory ff = (FilterFactory)CommonFactoryFinder.getFilterFactory(null);
+            FeatureSource source = con.getLayer(index).getFeatureSource();
+
+            DefaultQuery query =
+                new DefaultQuery(Query.ALL);
+            SortBy sortBy = ff.sort("cat", SortOrder.ASCENDING);
+            query.setSortBy(new SortBy[]{sortBy});
+            FeatureCollection col = source.getFeatures(Query.ALL); 
+//            LOGGER.info("Collection contains " + col.size() + " features.");
+//            LOGGER.info("Zoom level: " + zoomLevel);
+//            LOGGER.info("Skipping params: " + featuresToSkip + " : " + featureCount);
+
+            Iterator it = col.iterator();
+
+            try{
+                for (int i = 0; i < featuresToSkip - 1; i++) it.next();
+
+                SimpleFeature f = (SimpleFeature) it.next();
+                myMax = (Long)f.getAttribute("cat");
+
+                for (int i = 0; i < featureCount; i++) it.next();
+                f = (SimpleFeature)it.next();
+                myMin = (Long)f.getAttribute("cat");
+            } finally {
+                col.close(it);
             }
+        } catch (Exception e) {
+            LOGGER.severe("Failure while trying to regionate by data: " + e);
+            e.printStackTrace();
+        }
+    }
 
-            myZoomLevel = 1 - (int)(Math.log(requestBounds.getWidth() / fullBounds.getWidth()));
-            LOGGER.info("I've decided this is at zoom level: " + myZoomLevel);
+    private void preProcessBasic(WMSMapContext con, int index) {
+        try {
+            long zoomLevel = getZoomLevel(con, index);
+            // LOGGER.info("I've decided this is at zoom level: " + zoomLevel);
 
-            FeatureCollection col = con.getLayer(index).getFeatureSource()
-                .getFeatures(Query.ALL);
-            // dummied out... for now
+            FeatureCollection col = con.getLayer(index).getFeatureSource().getFeatures(Query.ALL);
+
             Iterator it = col.iterator();
             myMin = null;
             myMax = null;
@@ -72,23 +105,58 @@ public class DataRegionatingStrategy implements RegionatingStrategy{
             }
 
             long range = myMax - myMin;
-            myMin = myMin + (myZoomLevel * range / MAX_LEVELS);
+            myMin = myMin + (zoomLevel * range / MAX_LEVELS);
             myMax = myMin + (range / MAX_LEVELS);
 
-            LOGGER.info("Regionating data: max: " + myMax + ", min: " + myMin);
         } catch (Exception e) {
             LOGGER.severe("Tried to regionate data, but failed due to " + e);
         }
     }
 
-    public boolean include(SimpleFeature feature){
-        try{
+    /**
+     * Find the number of features that are at lower zoomlevels than the one provided.  When
+     * skipping around the sorted list of features to find the minimum value of the range 
+     * included at this zoomlevel.
+     */
+    private long findSkipAmount(long zoomLevel){
+        long skipAmount = 0;
+        for (long i = 0; i < zoomLevel; i++){
+            skipAmount += Math.pow(4, i) * FEATURES_PER_TILE;
+        }
+        return skipAmount;
+    }
+
+    private long findLayerSize(long zoomLevel){
+        return (long)Math.pow(4,zoomLevel) * FEATURES_PER_TILE;
+    }
+
+    private long getZoomLevel(WMSMapContext context, int index)
+        throws Exception {
+            FeatureSource<SimpleFeatureType, SimpleFeature> source = 
+                (FeatureSource<SimpleFeatureType, SimpleFeature>) context.getLayer(index).getFeatureSource();
+            ReferencedEnvelope fullBounds = source.getBounds();
+            ReferencedEnvelope requestBounds = context.getAreaOfInterest();
+            boolean reprojectBBox = (fullBounds.getCoordinateReferenceSystem() != null)
+                && !CRS.equalsIgnoreMetadata(
+                        requestBounds.getCoordinateReferenceSystem(), 
+                        fullBounds.getCoordinateReferenceSystem()
+                        );
+            if (reprojectBBox) {
+                requestBounds = requestBounds.transform(fullBounds.getCoordinateReferenceSystem(), true);
+            } else {
+                LOGGER.info("Data-based regionating code couldn't reproject, please make sure your datastore is configured properly (hint: for postgis add -s <srsid> to the shp2psql command arguments)");
+            }
+            return 1 - (int) (Math.log(requestBounds.getWidth() / fullBounds.getWidth()));
+        }
+
+    public boolean include(SimpleFeature feature) {
+        try {
             Object att = feature.getAttribute("cat");
             long l = ((Long) att).longValue();
 
-            return myMin <= l && myMax >= l;
-        } catch (Exception e){
-            LOGGER.info(e.getMessage());
+            return myMin.longValue() <= l && myMax.longValue() >= l;
+        } catch (Exception e) {
+            LOGGER.info("Encountered problem while trying to apply data regionating filter: " + e);
         }
         return false;
     }
