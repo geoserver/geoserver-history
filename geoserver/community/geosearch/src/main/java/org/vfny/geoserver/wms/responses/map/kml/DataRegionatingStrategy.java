39b54ee94fb019862e4756a8585de0d6bc9ea377
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-21 19:57:58 +0000

    Refactory data regionator before implementing range cache.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8882 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9e3c06993cbf281556751f582e8ec8bf908954e8 [formerly 5a0e8051c36528bbc68afd6ed4004362d221cb34]
    Former-commit-id: b328eb245f9bba22d285e151a11072a13f03c82f

--- geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
+++ geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
@@ -7,13 +7,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.geotools.data.DefaultQuery;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.Query;
-import org.geotools.data.DefaultQuery;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
-import org.geotools.factory.CommonFactoryFinder;
 import org.opengis.filter.FilterFactory;
 import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.sort.SortOrder;
@@ -35,24 +35,62 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
     private static long FEATURES_PER_TILE = 5;
     private static String myAttributeName = "count";
 
+    private List myRanges;
     private Long myMin;
     private Long myMax;
+    private long myZoomLevel;
 
+    /**
+     * Create a data regionating strategy that uses the specified attribute.
+     * @param attname the name of the attribute to use.  The attribute must be numeric.
+     */
     public DataRegionatingStrategy(String attname){
         myAttributeName = attname;
     }
 
     public void preProcess(WMSMapContext con, int index) {
-        LOGGER.severe("Regionate attribute: " + myAttributeName);
-        preProcessEven(con, index);
+        myZoomLevel = getZoomLevel(con, index);
+        myRanges = getRangesFromCache(con, index);
+        if (myRanges == null){
+            myRanges = preProcessBasic(con, index);
+            addRangesToCache(con, index, myRanges);
+        }
+        setRange(myRanges);
+    }
+
+    /**
+     * Check the cache and find the cached range values for the current combination of (layer, zoomlevel, attribute). 
+     * If the values are not cached, return null.
+     *
+     * @param con the WMSMapContext for the current request
+     * @param index the numeric index of the layer currently being processed
+     */
+    private List getRangesFromCache(WMSMapContext con, int index){
+        return null;
+    }
+
+    /**
+     * Cache the range values for the current combination of (layer, zoomlevel, attribute)
+     */
+    private void addRangesToCache(WMSMapContext con, int index, List ranges){
+    }
+
+    private void setRange(List ranges){
+        myMin = (myZoomLevel <= 1) ? null : new Long(((Number)ranges.get((int)(myZoomLevel - 1))).longValue());
+        myMax = (myZoomLevel >= ranges.size()) 
+            ? new Long(((Number)ranges.get((int)(ranges.size() - 1))).longValue()) 
+            : new Long(((Number)ranges.get((int)(myZoomLevel))).longValue());
+    }
+
+    private boolean inRange(Number n){
+        if ((myMin != null) && (myMin.longValue() > n.longValue())) return false;
+        if ((myMax != null) && (myMax.longValue() < n.longValue())) return false;
+        return true;
     }
 
     public void preProcessEven(WMSMapContext con, int index) {
+        List ranges = new ArrayList();
         try {
-            long zoomLevel = getZoomLevel(con, index);
-            long featuresToSkip = findSkipAmount(zoomLevel);
-            long featureCount = findLayerSize(zoomLevel);
-
             FilterFactory ff = (FilterFactory)CommonFactoryFinder.getFilterFactory(null);
             FeatureSource source = con.getLayer(index).getFeatureSource();
 
@@ -61,26 +99,18 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
             SortBy sortBy = ff.sort(myAttributeName, SortOrder.ASCENDING);
             query.setSortBy(new SortBy[]{sortBy});
             FeatureCollection col = source.getFeatures(Query.ALL); 
-//            LOGGER.info("Collection contains " + col.size() + " features.");
-//            LOGGER.info("Zoom level: " + zoomLevel);
-//            LOGGER.info("Skipping params: " + featuresToSkip + " : " + featureCount);
 
             Iterator it = col.iterator();
 
             try{
-                SimpleFeature f;
-                for (int i = 0; i < featuresToSkip - 1; i++) f = (SimpleFeature)it.next();
-                f = (SimpleFeature) it.next();
-
-                if (f.getAttribute(myAttributeName) != null)
-                    myMax = ((Number)f.getAttribute(myAttributeName)).longValue();
-
-                for (int i = 0; it.hasNext() && i < featureCount; i++) f = (SimpleFeature)it.next();
-
-                if (it.hasNext()) f = (SimpleFeature)it.next();
-
-                if (f.getAttribute(myAttributeName) != null)
-                    myMin = ((Number)f.getAttribute(myAttributeName)).longValue();
+                SimpleFeature f = null;
+                for (int i = 0; i < MAX_LEVELS; i++){
+                    int j = 0;
+                    do {
+                        f = (SimpleFeature)it.next();
+                    } while (j < getFeatureCountAtZoomLevel(i));
+                    ranges.add(f.getAttribute(myAttributeName));
+                }
             } finally {
                 col.close(it);
             }
@@ -90,7 +120,8 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
         }
     }
 
-    private void preProcessBasic(WMSMapContext con, int index) {
+    private List preProcessBasic(WMSMapContext con, int index) {
+        List ranges = new ArrayList();
         try {
             long zoomLevel = getZoomLevel(con, index);
             // LOGGER.info("I've decided this is at zoom level: " + zoomLevel);
@@ -98,30 +129,33 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
             FeatureCollection col = con.getLayer(index).getFeatureSource().getFeatures(Query.ALL);
 
             Iterator it = col.iterator();
-            myMin = null;
-            myMax = null;
+            Long min = null;
+            Long max = null;
 
             try {
                 while (it.hasNext()) {
                     SimpleFeature f = (SimpleFeature) it.next();
 
                     long l = ((Number) f.getAttribute(myAttributeName)).longValue();
-                    if (myMin == null || myMin.longValue() > l)
-                        myMin = new Long(l);
-                    if (myMax == null || myMax.longValue() < l)
-                        myMax = new Long(l);
+                    if (min == null || min.longValue() > l)
+                        min = new Long(l);
+                    if (max == null || max.longValue() < l)
+                        max = new Long(l);
                 }
             } finally {
                 col.close(it);
             }
 
-            long range = myMax - myMin;
-            myMin = myMin + (zoomLevel * range / MAX_LEVELS);
-            myMax = myMin + (range / MAX_LEVELS);
+            long range = max - min;
 
+            for (long i = 1; i < MAX_LEVELS; i++){
+                ranges.add(min + (i * range / MAX_LEVELS));
+            }
         } catch (Exception e) {
             LOGGER.severe("Tried to regionate data, but failed due to " + e);
         }
+
+        return ranges;
     }
 
     /**
@@ -132,17 +166,21 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
     private long findSkipAmount(long zoomLevel){
         long skipAmount = 0;
         for (long i = 0; i < zoomLevel; i++){
-            skipAmount += Math.pow(4, i) * FEATURES_PER_TILE;
+            skipAmount += getFeatureCountAtZoomLevel(i);
         }
         return skipAmount;
+    }
+
+    private long getFeatureCountAtZoomLevel(long zoomLevel){
+        return (long)(Math.pow(4, zoomLevel) * FEATURES_PER_TILE);
     }
 
     private long findLayerSize(long zoomLevel){
         return (long)Math.pow(4,zoomLevel) * FEATURES_PER_TILE;
     }
 
-    private long getZoomLevel(WMSMapContext context, int index)
-        throws Exception {
+    private long getZoomLevel(WMSMapContext context, int index){
+        try{
             FeatureSource<SimpleFeatureType, SimpleFeature> source = 
                 (FeatureSource<SimpleFeatureType, SimpleFeature>) context.getLayer(index).getFeatureSource();
             ReferencedEnvelope fullBounds = source.getBounds();
@@ -160,15 +198,20 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
                 + "shp2psql command arguments)"
                 );
             }
-            return 1 - (int) (Math.log(requestBounds.getWidth() / fullBounds.getWidth()));
+            return Math.max(1, 1 - (int) (Math.log(requestBounds.getWidth() / fullBounds.getWidth())));
+        } catch (Exception e){
+            LOGGER.severe("Zoom Level calculation failed, error was: " + e);
+            return 1;
         }
+    }
 
     public boolean include(SimpleFeature feature) {
         try {
             Object att = feature.getAttribute(myAttributeName);
-            long l = ((Number) att).longValue();
+            Number num = (Number)att;
 
-            return myMin.longValue() <= l && myMax.longValue() >= l;
+            // return myMin.longValue() <= l && myMax.longValue() >= l;
+            return inRange(num);
         } catch (Exception e) {
             LOGGER.info("Encountered problem while trying to apply data regionating filter: " + e);
         }
