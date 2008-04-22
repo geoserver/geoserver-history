cae7c0a935457f8af382f9229bbc9aa809bf0724
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-04-22 15:01:54 +0000

    Add code to actually cache the range values.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@8885 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 74bf7c509e2278691a2e369fe5225bd6d8c6f90f [formerly bddf0865698355ffc7fd25a963ffd0561434a75c]
    Former-commit-id: 28030ea4eb35e8702a1c64584349cc85d364c256

--- geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
+++ geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/DataRegionatingStrategy.java
@@ -6,6 +6,14 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
 import org.geotools.data.DefaultQuery;
 import org.geotools.data.FeatureSource;
@@ -19,7 +27,10 @@ import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.sort.SortOrder;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.vfny.geoserver.config.FeatureTypeConfig;
+import org.vfny.geoserver.global.MapLayerInfo;
 import org.vfny.geoserver.wms.WMSMapContext;
+import org.vfny.geoserver.global.GeoserverDataDirectory;
 
 /**
  * Strategy for regionating based on algorithmic stuff related to the actual
@@ -66,16 +77,67 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
      * @param index the numeric index of the layer currently being processed
      */
     private List getRangesFromCache(WMSMapContext con, int index){
+        try{
+            File cache = findCacheFile(con, index, myZoomLevel);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(cache)));
+            String line;
+            List ranges = new ArrayList();
+            while ((line = reader.readLine()) != null){
+                ranges.add(Long.valueOf(line));
+            }
+            return ranges;
+        } catch (Exception e){
+        }
+
         return null;
     }
 
     /**
      * Cache the range values for the current combination of (layer, zoomlevel, attribute)
+     * @param con the WMSMapContext for the current request
+     * @param index the numeric index of the layer currently being processed
+     * @param ranges the range values for the current request 
      */
     private void addRangesToCache(WMSMapContext con, int index, List ranges){
+        try{
+            File cache = findCacheFile(con, index, myZoomLevel);
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(cache))));
+            Iterator it = ranges.iterator();
+            while (it.hasNext()){
+                writer.println(it.next());
+            }
+            writer.flush();
+            writer.close();
+        } catch (Exception e){
+            // it's okay, we just won't cache these values
+            LOGGER.info("Error while trying to cache calculated range values: " + e.getMessage());
+        }
+    }
+
+    private File findCacheFile(WMSMapContext con, int index, long zoomLevel){
+        File f = null;
+        try{
+            FeatureSource source = con.getLayer(index).getFeatureSource();
+            MapLayerInfo[] config = con.getRequest().getLayers();
+            for (int i = 0; i < config.length; i++){
+                MapLayerInfo layer = config[i];
+                if (layer.getName().equals(con.getLayer(index).getTitle())){
+                    f = GeoserverDataDirectory.findCreateConfigDir("featureTypes");
+                    f = new File(f, layer.getDirName());
+                    f = new File(f, myAttributeName + ".cache");
+                    break;
+                } 
+            }
+        } catch (Exception e){
+            LOGGER.severe("Exception while finding the location for the cachefile!");
+            e.printStackTrace();
+        }
+        return f;
     }
 
     private void setRange(List ranges){
+    	if (ranges.size() == 0) return;
+    	
         myMin = (myZoomLevel <= 1) ? null : new Long(((Number)ranges.get((int)(myZoomLevel - 1))).longValue());
         myMax = (myZoomLevel >= ranges.size()) 
             ? new Long(((Number)ranges.get((int)(ranges.size() - 1))).longValue()) 
@@ -88,7 +150,7 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
         return true;
     }
 
-    public void preProcessEven(WMSMapContext con, int index) {
+    private void preProcessEven(WMSMapContext con, int index) {
         List ranges = new ArrayList();
         try {
             FilterFactory ff = (FilterFactory)CommonFactoryFinder.getFilterFactory(null);
@@ -136,11 +198,11 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
                 while (it.hasNext()) {
                     SimpleFeature f = (SimpleFeature) it.next();
 
-                    long l = ((Number) f.getAttribute(myAttributeName)).longValue();
-                    if (min == null || min.longValue() > l)
-                        min = new Long(l);
-                    if (max == null || max.longValue() < l)
-                        max = new Long(l);
+                    Number value = (Number) f.getAttribute(myAttributeName);
+                    if (min == null || min.longValue() > value.longValue())
+                        min = new Long(value.longValue());
+                    if (max == null || max.longValue() < value.longValue())
+                        max = new Long(value.longValue());
                 }
             } finally {
                 col.close(it);
@@ -151,8 +213,11 @@ public class DataRegionatingStrategy implements RegionatingStrategy {
             for (long i = 1; i < MAX_LEVELS; i++){
                 ranges.add(min + (i * range / MAX_LEVELS));
             }
+        } catch (NullPointerException npe){
+        	LOGGER.severe("Failed to find specified attribute; check the attribute name in your request.");
         } catch (Exception e) {
             LOGGER.severe("Tried to regionate data, but failed due to " + e);
+            e.printStackTrace();
         }
 
         return ranges;
