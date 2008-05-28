fed2253e99f5e2b6fae91b9decf11aded4110063
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-05-28 06:45:22 +0000

    made OWS an interface and deprecated in place of ServiceInfo
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9020 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a0244f158d877770b72d783f53b3fee63499ecae [formerly fc8c8804b0650863c2adaf2fd9858b3cc6c83008]
    Former-commit-id: 511636d316be56c3f42399c1509e6c693bcc25a6

--- geoserver/ows/src/test/java/org/geoserver/ows/Hello.java
+++ geoserver/ows/src/test/java/org/geoserver/ows/Hello.java
@@ -4,8 +4,90 @@
  */
 package org.geoserver.ows;
 
-public class Hello extends OWS {
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class Hello implements OWS {
     public String getId() {
         return "hello";
+    }
+
+    public String getAbstract() {
+        return null;
+    }
+
+    public String getAccessConstraints() {
+        return null;
+    }
+
+    public Map getClientProperties() {
+        return null;
+    }
+
+    public String getFees() {
+        return null;
+    }
+
+    public List getKeywords() {
+        return null;
+    }
+
+    public String getMaintainer() {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public URL getOnlineResource() {
+        return null;
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setAbstract(String serverAbstract) {
+    }
+
+    public void setAccessConstraints(String accessConstraints) {
+    }
+
+    public void setEnabled(boolean enabled) {
+    }
+
+    public void setFees(String fees) {
+    }
+
+    public void setMaintainer(String maintainer) {
+    }
+
+    public void setName(String name) {
+    }
+
+    public void setOnlineResource(URL onlineResource) {
+    }
+
+    public void setTitle(String title) {
+    }
+
+    public String getSchemaBaseURL() {
+        return null;
+    }
+
+    public void setSchemaBaseURL(String schemaBaseURL) {
+    }
+
+    public boolean isVerbose() {
+        return false;
+    }
+
+    public void setVerbose(boolean verbose) {
     }
 }
