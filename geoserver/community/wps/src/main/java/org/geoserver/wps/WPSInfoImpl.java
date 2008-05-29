e03cbb782070dbe65d79050128edda2019c8032f
lreed <lreed@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-05-29 22:18:30 +0000

    WPS loader files added.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9068 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4271aa2cc29ce217db24903f8e8b25de0bfaf8dd [formerly a4304cb23c606a77bcde8983198ac1bf95dd462a]
    Former-commit-id: ef7333120e3bc33f3113f45b002746fca61fdcb1

---
+++ geoserver/community/wps/src/main/java/org/geoserver/wps/WPSInfoImpl.java
@@ -0,0 +1,12 @@
+/**
+ * @author lreed@refractions.net
+ */
+
+package org.geoserver.wps;
+
+import org.geoserver.config.impl.ServiceInfoImpl;
+
+public class WPSInfoImpl extends ServiceInfoImpl implements WPSInfo
+{
+	
+}
