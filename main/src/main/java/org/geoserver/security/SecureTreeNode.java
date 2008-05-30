df749512109c0dae496705b29d1a94da10fc614f
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-05-30 16:45:24 +0000

    First set of classes needed to load the layers.properties file and make access decisions based on its contents. So far these are untested, but also not hooked to geoserver in any way (so harmless)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9077 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ea01967fbf6966965c252bfad6276cb22032dd2f [formerly e0e988132335abb9f96201e08894c1012fa45b13]
    Former-commit-id: 36a2ab2bf5466c0bb55ec0c42e9b2487e7c7db7a

---
+++ main/src/main/java/org/geoserver/security/SecureTreeNode.java
@@ -0,0 +1,135 @@
+package org.geoserver.security;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.acegisecurity.Authentication;
+import org.acegisecurity.GrantedAuthority;
+
+/**
+ * Represents a hierarchical security tree node. The tree as a whole is
+ * represented by its root
+ * 
+ * @author Andrea Aime - TOPP
+ * 
+ */
+class SecureTreeNode {
+    private Map<String, SecureTreeNode> children = new HashMap<String, SecureTreeNode>();
+
+    private SecureTreeNode parent;
+
+    private Map<AccessMode, Set<String>> authorizedRoles = new HashMap<AccessMode, Set<String>>();
+
+    /**
+     * Builds a child of the specified parent node
+     * 
+     * @param parent
+     */
+    private SecureTreeNode(SecureTreeNode parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * Builds the root of a security tree
+     */
+    SecureTreeNode() {
+        // nothing to do
+    }
+
+    /**
+     * Returns a child with the specified name, or null
+     * 
+     * @param name
+     * @return
+     */
+    SecureTreeNode getChild(String name) {
+        return children.get(name);
+    }
+
+    /**
+     * Adds a child to this path element
+     * 
+     * @param name
+     * @return
+     */
+    SecureTreeNode addChild(String name) {
+        if (getChild(name) != null)
+            throw new IllegalArgumentException("This pathElement " + name
+                    + " is already among my children");
+
+        SecureTreeNode child = new SecureTreeNode(this);
+        children.put(name, child);
+        return child;
+    }
+
+    /**
+     * Tells if the user is allowed to access the PathElement with the specified
+     * access mode. If no information can be found in the current node, the
+     * decision is delegated to the parent. If the root is reached and it has no
+     * security definition, access will be granted. Otherwise, the first path
+     * element with a role list for the specified access mode will return true
+     * if the user has a {@link GrantedAuthority} matching one of the specified
+     * roles, false otherwise
+     * 
+     * @param user
+     * @param mode
+     * @return
+     */
+    boolean canAccess(Authentication user, AccessMode mode) {
+        Set<String> roles = authorizedRoles.get(mode);
+
+        // if we don't know, we ask the parent, otherwise we assume
+        // the object is unsecured
+        if (roles == null || roles.isEmpty()) {
+            if (parent == null)
+                return true;
+            else
+                return parent.canAccess(user, mode);
+        }
+
+        // let's scan thru the the authorities granted to the user and
+        // see if he matches any of the write roles
+        for (GrantedAuthority authority : user.getAuthorities()) {
+            if (roles.contains(authority.getAuthority()))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the authorized roles for the specified access mode
+     */
+    Set<String> getAuthorizedRoles(AccessMode mode) {
+        return authorizedRoles.get(mode);
+    }
+
+    /**
+     * Sets the authorized roles for the specified access mode
+     */
+    void setAuthorizedRoles(AccessMode mode, Set<String> roles) {
+        authorizedRoles.put(mode, roles);
+    }
+
+    /**
+     * Utility method that drills down from the current node using the specified
+     * list of child names, and returns the latest element found along that path
+     * (might not be correspondent to the full path specified, security paths
+     * can be incomplete, the definition of the parent applies to the missing
+     * children as well)
+     * 
+     * @param pathElements
+     * @return
+     */
+    SecureTreeNode getDeepestNode(String[] pathElements) {
+        SecureTreeNode curr = this;
+        for (int i = 0; i < pathElements.length; i++) {
+            final SecureTreeNode next = curr.getChild(pathElements[i]);
+            if (next == null)
+                return curr;
+            else
+                curr = next;
+        }
+        return curr;
+    }
+}
