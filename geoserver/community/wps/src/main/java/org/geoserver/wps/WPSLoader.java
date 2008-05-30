20681a729a6e1cd60a260cc6d6d4286ca5983933
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-05-30 22:28:15 +0000

    made wps loader extension of xstream loader
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9087 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 541cf10dede32c83f469f70105e48d92e6bb403e [formerly 9f3e961d099f4753d902cfc8c32bde0314a4f0c8]
    Former-commit-id: 2f5b59c27218b050509e4f8c2662087a92da249f

--- geoserver/community/wps/src/main/java/org/geoserver/wps/WPSLoader.java
+++ geoserver/community/wps/src/main/java/org/geoserver/wps/WPSLoader.java
@@ -6,15 +6,30 @@ package org.geoserver.wps;
 
 import org.geoserver.config.GeoServer;
 import org.geoserver.config.ServiceInfo;
-import org.geoserver.config.util.LegacyServicesReader;
-import org.geoserver.config.util.ServiceLoader;
+import org.geoserver.config.util.XStreamServiceLoader;
+import org.geoserver.platform.GeoServerResourceLoader;
 
-public class WPSLoader extends ServiceLoader
+/**
+ * Service loader for the Web Processing Service.
+ * 
+ * @author Lucas Reed, Refractions Research
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class WPSLoader extends XStreamServiceLoader
 {
-	public ServiceInfo load(LegacyServicesReader reader, GeoServer geoServer) throws Exception
-	{
-		WPSInfo wps = new WPSInfoImpl();
-		
-		return wps;
-	}
+    public WPSLoader(GeoServerResourceLoader resourceLoader) {
+        super(resourceLoader);
+    }
+
+    public String getServiceId() {
+        return "wps";
+    }
+    
+    protected ServiceInfo createServiceFromScratch(GeoServer gs) {
+        WPSInfoImpl wps = new WPSInfoImpl();
+        wps.setId( getServiceId() );
+        
+        return wps;
+    }
 }
