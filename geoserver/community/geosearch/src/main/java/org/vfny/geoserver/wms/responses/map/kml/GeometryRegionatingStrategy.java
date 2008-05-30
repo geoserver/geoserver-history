61ee2b4f43f2b268818ad3b79093018de065a02a
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-05-30 22:00:02 +0000

    Initial template for cached hierarchy implementation of the Geometry regionating strategy.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9086 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a76153653d460ee995208139848dab9b102d2599 [formerly 41339b51b9d1b64870362ab95dcc464e1464cbfe]
    Former-commit-id: 749b5ae0bc26497577cb2f7dba3a07c8468528c9

--- geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/GeometryRegionatingStrategy.java
+++ geoserver/community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/GeometryRegionatingStrategy.java
@@ -1,24 +1,61 @@
 package org.vfny.geoserver.wms.responses.map.kml;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.geotools.data.FeatureSource;
+import java.util.Comparator;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.filter.FilterFactory;
+import org.geotools.data.DefaultQuery;
+import org.geotools.data.Query;
+import org.geoserver.ows.HttpErrorCodeException;
 import org.vfny.geoserver.wms.WMSMapContext;
 import org.geotools.map.MapLayer;
 
-import com.vividsolutions.jts.geom.Geometry;
 
-public class GeometryRegionatingStrategy implements RegionatingStrategy{
-
+public class GeometryRegionatingStrategy extends CachedHierarchyRegionatingStrategy{
     Logger LOGGER = org.geotools.util.logging.Logging.getLogger("org.geoserver.geosearch");
+    public static int FEATURES_PER_TILE = 100;
 
-    public void preProcess(WMSMapContext con, MapLayer layer){
-        // dummied out. For now...
+    protected String findCacheTable(WMSMapContext con, MapLayer layer){
+        return super.findCacheTable(con, layer) + "_geometry_regionator";
     }
 
-    public boolean include(SimpleFeature feature){
-        Geometry geom = (Geometry)feature.getDefaultGeometry();
+    protected TileLevel createTileHierarchy(WMSMapContext con, MapLayer layer){
+        LOGGER.info("About to build the geometry hierarchy");
+        try{
+            FeatureSource<SimpleFeatureType, SimpleFeature> source = 
+                (FeatureSource<SimpleFeatureType, SimpleFeature>) layer.getFeatureSource();
+            CoordinateReferenceSystem nativeCRS = source.getSchema().getDefaultGeometry().getCRS();
+            ReferencedEnvelope fullBounds = TileLevel.getWorldBounds();
+            fullBounds = fullBounds.transform(nativeCRS, true);
+            TileLevel root = TileLevel.makeRootLevel(fullBounds, FEATURES_PER_TILE, new GeometryComparator());
+            FilterFactory ff = (FilterFactory)CommonFactoryFinder.getFilterFactory(null);
+            DefaultQuery query = new DefaultQuery(Query.ALL);
+            FeatureCollection col = source.getFeatures(query);
 
-        return geom.getLength() > 0.01;
+            root.populate(col);
+
+            return root;
+        } catch (Exception e){
+            LOGGER.log(Level.SEVERE, "Error while trying to regionate by geometry.", e);
+            throw new HttpErrorCodeException(500, "Error while trying to regionate by geometry.", e);
+        }
+    }
+
+    private class GeometryComparator implements Comparator {
+        public int compare(Object a, Object b){
+            SimpleFeature fa = (SimpleFeature) a;
+            SimpleFeature fb = (SimpleFeature) b;
+            
+            return 0;
+        }
     }
 }
+
