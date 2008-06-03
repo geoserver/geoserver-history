cb094ed9426d8305cc644904ec5581dd1a071de6
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-03 12:57:40 +0000

    Generic wrapper and tests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9110 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f398f31f002ef2bbf10208c29c67134c81cb8c6d [formerly 2222afc924cdcada8cd3439e9344fea6bf23b349]
    Former-commit-id: a31e002e22409dbf1bbdc487003df04878b59869

--- geoserver/main/src/main/java/org/geoserver/catalog/Wrapper.java
+++ geoserver/main/src/main/java/org/geoserver/catalog/Wrapper.java
@@ -4,14 +4,32 @@
  */
 package org.geoserver.catalog;
 
+
 /**
  * Generic interface that can be implemented by object wrappers that allow
- * client code to unwrap their contentes
+ * client code to unwrap their contents
  * 
  * @author Andrea Aime - TOPP
  * 
  * @param <T>
  */
 public interface Wrapper<T> {
+    /**
+     * Unwraps the object, returning the wrapped instance (this is not
+     * recursive, if you need full unwrapping use
+     * {@link WrapperUtils#deepUnwrap(Wrapper))
+     * 
+     * @return
+     */
     T unwrap();
+
+    /**
+     * Returns true if this either implements the interface argument or is
+     * directly a wrapper for an object that does. Returns false otherwise. 
+     * 
+     * @param iface
+     *            a Class defining an interface.
+     * @return true if this implements the interface or directly wraps an object that does.
+     */
+    boolean isWrapperFor(Class<?> iface);
 }
