c66d074c110aa59649d9077bba52080ae6905174
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-03 13:21:26 +0000

    GEOS-1967, Make IdentifierKvpParser generic
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9112 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a631e01476709d24f5ebed1a8c7333608f3b35be [formerly 613d236343dd076546ccbea36d0f322fadb071dd]
    Former-commit-id: 73fb1976e229f0f2f183f884e21f0619e2f9b375

--- wcs1_1/src/main/java/org/geoserver/wcs/kvp/IdentifierKvpParser.java
+++ wcs1_1/src/main/java/org/geoserver/wcs/kvp/IdentifierKvpParser.java
@@ -4,20 +4,18 @@
  */
 package org.geoserver.wcs.kvp;
 
-import static org.vfny.geoserver.wcs.WcsException.WcsExceptionCode.InvalidParameterValue;
-import net.opengis.ows11.CodeType;
-import net.opengis.ows11.Ows11Factory;
+import static org.vfny.geoserver.wcs.WcsException.WcsExceptionCode.*;
 
-import org.geoserver.ows.KvpParser;
+import org.geoserver.ows.kvp.CodeTypeKvpParser;
 import org.vfny.geoserver.global.Data;
 import org.vfny.geoserver.wcs.WcsException;
 
-public class IdentifierKvpParser extends KvpParser {
+public class IdentifierKvpParser extends CodeTypeKvpParser {
 
     private Data catalog;
 
     public IdentifierKvpParser(Data catalog) {
-        super("identifier", CodeType.class);
+        super("identifier", "wcs");
         this.catalog = catalog;
     }
 
@@ -25,10 +23,9 @@ public class IdentifierKvpParser extends KvpParser {
     public Object parse(String value) throws Exception {
         Integer type = catalog.getLayerType(value);
         if (!Data.TYPE_RASTER.equals(type))
-            throw new WcsException("Could not find coverage '" + value + "'", InvalidParameterValue, "identifier");
-        CodeType result = Ows11Factory.eINSTANCE.createCodeType();
-        result.setValue(value);
-        return result;
+            throw new WcsException("Could not find coverage '" + value + "'",
+                    InvalidParameterValue, "identifier");
+        return super.parse(value);
     }
 
 }
