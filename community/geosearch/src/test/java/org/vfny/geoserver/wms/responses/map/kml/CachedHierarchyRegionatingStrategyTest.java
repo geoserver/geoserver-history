9d2c858eb5b972277924d30a4737465c5788fb02
arneke <arneke@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-05 23:29:50 +0000

    GeoSearch - makes the tile pyramid start at the smallest tile that can cover then entire layer
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9155 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cfd4216f126c1bf42b06f0e497f606d61daaf6b9 [formerly 94108f966bc7615c647078b228166550be4823a3]
    Former-commit-id: c0de3fb2e61e84964d603f2a8d9122d687da9309

--- community/geosearch/src/test/java/org/vfny/geoserver/wms/responses/map/kml/CachedHierarchyRegionatingStrategyTest.java
+++ community/geosearch/src/test/java/org/vfny/geoserver/wms/responses/map/kml/CachedHierarchyRegionatingStrategyTest.java
@@ -6,6 +6,8 @@ import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeature;
 import java.util.Iterator;
+
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Polygon;
 
 public class CachedHierarchyRegionatingStrategyTest extends RegionatingTestSupport {
@@ -30,5 +32,46 @@ public class CachedHierarchyRegionatingStrategyTest extends RegionatingTestSuppo
             assertEquals(y, tileCoords[1]);
             assertEquals(z, tileCoords[2]);
         }
+    }
+    
+    public void testExpandToTile() throws Exception {
+    	// Prepare for failure
+		ReferencedEnvelope worldBounds = 
+			new ReferencedEnvelope(
+					new Envelope(10.0, -180.0, 90.0, -90.0), 
+					CRS.decode("EPSG:4326"));
+		
+		assertNull(CachedHierarchyRegionatingStrategy.expandToTile(worldBounds));
+		
+		// West
+		ReferencedEnvelope west = 
+			new ReferencedEnvelope(
+					new Envelope(0, -180.0, 90.0, -90.0), 
+					CRS.decode("EPSG:4326"));
+		
+		assertEquals(CachedHierarchyRegionatingStrategy.expandToTile(west),west);
+		
+		// East
+		ReferencedEnvelope east = 
+			new ReferencedEnvelope(
+					new Envelope(180.0, 0, 90.0, -90.0), 
+					CRS.decode("EPSG:4326"));
+		
+		assertEquals(CachedHierarchyRegionatingStrategy.expandToTile(east),east);
+		
+		
+		// Somewhere that needs real adjustment
+		ReferencedEnvelope somewhere = 
+			new ReferencedEnvelope(
+					new Envelope(-5.0, -44.0, 2.01, 34.0), 
+					CRS.decode("EPSG:4326"));
+		
+		ReferencedEnvelope adjustedSomewhere = CachedHierarchyRegionatingStrategy.expandToTile(somewhere);
+		
+		assertEquals(adjustedSomewhere.getMinX(), -45.0);
+		assertEquals(adjustedSomewhere.getMaxX(), 0.0);
+		
+		assertEquals(adjustedSomewhere.getMinY(), 0.0);
+		assertEquals(adjustedSomewhere.getMaxY(), 45.0);
     }
 }
