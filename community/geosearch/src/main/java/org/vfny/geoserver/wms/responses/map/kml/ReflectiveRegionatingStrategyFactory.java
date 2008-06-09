7c0400e5c90d2e91f57d7a67d5209401127519f9
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-09 19:39:25 +0000

    Add RegionatingStrategyFactory to act as spring extension point.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9182 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ac1bcf62e0d7556a76d0ee3b80c3fcb03fdc91b6 [formerly c78df021f7b93ae2a8f7e624f11b1d37a4959337]
    Former-commit-id: fedb1977979b9f871b2776c5998afc17fb52629e

---
+++ community/geosearch/src/main/java/org/vfny/geoserver/wms/responses/map/kml/ReflectiveRegionatingStrategyFactory.java
@@ -0,0 +1,56 @@
+package org.vfny.geoserver.wms.responses.map.kml;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class ReflectiveRegionatingStrategyFactory implements RegionatingStrategyFactory {
+    private static final Logger LOGGER = 
+        org.geotools.util.logging.Logging.getLogger("org.geoserver.geosearch");
+
+
+    String myName;
+    String myClassName;
+    Class myStrategyClass;
+
+    public ReflectiveRegionatingStrategyFactory(String name, String className){
+        myName = name;
+        myClassName = className;
+    }
+
+    public ReflectiveRegionatingStrategyFactory(String name, Class strategy){
+        myName = name;
+        myStrategyClass = strategy;
+    }
+
+    public boolean canHandle(String strategyName){
+        return (myName != null) && myName.equalsIgnoreCase(strategyName);
+    }
+
+    public RegionatingStrategy createStrategy(){
+        try{
+            return (RegionatingStrategy)getStrategyClass().newInstance();
+        } catch (Exception e){
+            LOGGER.log(Level.SEVERE, 
+                    "Couldn't create " + myStrategyClass + " in ReflectiveRegionatingStrategyFactory", 
+                    e
+                    );
+        }
+        return new SLDRegionatingStrategy();
+    }
+
+    protected Class getStrategyClass() {
+        if (myStrategyClass != null)
+            return myStrategyClass;
+
+        try{
+            myStrategyClass = Class.forName(myClassName);
+        } catch (Exception e){
+            LOGGER.log(Level.SEVERE,
+                "Failed to find class " + myClassName + " for ReflectiveRegionatingStrategy.",
+                e
+                );
+        }
+
+        return myStrategyClass;
+    }
+}
