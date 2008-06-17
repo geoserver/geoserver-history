9bb20ae3717fbd240a2686d295ea6fe76f128626
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-17 20:03:17 +0000

    added docs, chagned title -> titleKey
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9289 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9a74a3a72ccffc278a2b16129ba8ce87216c34fa [formerly 614a0e5972e8519a610c96f8040159e76ff06600]
    Former-commit-id: 5064ad336941b5db3250dcab37182cbe6ea6750a

--- community/web2/src/main/java/org/geoserver/web/ComponentInfo.java
+++ community/web2/src/main/java/org/geoserver/web/ComponentInfo.java
@@ -7,7 +7,8 @@ import org.apache.wicket.Component;
 /**
  * Information about a component being plugged into a user interface.
  * <p>
- * Subclasses of this class are used to imnplement user interface "extension points". 
+ * Subclasses of this class are used to implement user interface "extension points".
+ * For an example see {@link MainPageInfo}.
  * </p>
  * 
  * @author Andrea Aime, The Open Planning Project
@@ -17,25 +18,60 @@ import org.apache.wicket.Component;
  */
 public abstract class ComponentInfo<C extends Component> implements Serializable {
 
+    /**
+     * the id of the component
+     */
     String id;
+    /**
+     * the title of the component
+     */
     String title;
+    /**
+     * the class of the component
+     */
     Class<C> componentClass;
     
+    /**
+     * The id of the component.
+     */
     public String getId() {
         return id;
     }
+    /**
+     * Sets the id of the component.
+     */
     public void setId(String id) {
         this.id = id;
     }
-    public String getTitle() {
+    /**
+     * The i18n key for the title of the component.
+     * <p>
+     * The exact way this title is used depends one the component. For instance
+     * if the component is a page, the title could be the used for a link to the
+     * page. If the component is a panel in a tabbed panel, the title might be 
+     * the label on the tab.
+     * </p>
+     */
+    public String getTitleKey() {
         return title;
     }
-    public void setTitle(String title) {
+    /**
+     * The i18n key for the title of the component.
+     */
+    public void setTitleKey(String title) {
         this.title = title;
     }
+    
+    /**
+     * The implementation class of the component.
+     */
     public Class<C> getComponentClass() {
         return componentClass;
     }
+    
+    /**
+     * Sets the implementation class of the component.
+     */
     public void setComponentClass(Class<C> componentClass) {
         this.componentClass = componentClass;
     }
