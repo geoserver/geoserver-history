a07ecee73d7b9873b547b1cdd4ed1abf250d1b0b
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-18 09:09:35 +0000

    Fix so that resource config form can be submitted.  Converters to allow setting more complex data Real Soon Now.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9301 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bb9b684af7253b038d4126da4c06370a6e4c5dcb [formerly 453dc7cfd7f0d20898d9bd3cc74ac802d15794bd]
    Former-commit-id: d6120efa7741deb214da3eedff1243eabdae9aa3

--- community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPanel.java
+++ community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPanel.java
@@ -1,15 +1,12 @@
 package org.geoserver.web.data;
 
 import org.apache.wicket.markup.html.panel.Panel;
-import org.geoserver.catalog.ResourceInfo;
 
 public class ResourceConfigurationPanel extends Panel {
 	private static final long serialVersionUID = 4881474189619124359L;
 
-	private ResourceInfo myResourceInfo;
-	
-	public ResourceConfigurationPanel(String id, ResourceInfo info){
+	public ResourceConfigurationPanel(String id){
 		super(id);
-		myResourceInfo = info;
 	}
+	
 }
