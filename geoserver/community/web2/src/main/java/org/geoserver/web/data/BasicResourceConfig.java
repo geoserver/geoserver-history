338056541fbd42294597c4586521573b11b1763f
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 18:15:17 +0000

    Slightly nicer version of resource configuration panel, with a list editor for the keywords.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9335 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f0085cf9ca150c288b75ff681fa5f887e849aab7 [formerly 6374f2c1a3b9de6990b61186488ccbe06bd55493]
    Former-commit-id: 1219113e02ba09f21a2a0fae348778c063aafd63

--- geoserver/community/web2/src/main/java/org/geoserver/web/data/BasicResourceConfig.java
+++ geoserver/community/web2/src/main/java/org/geoserver/web/data/BasicResourceConfig.java
@@ -1,21 +1,52 @@
 package org.geoserver.web.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.ListMultipleChoice;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ComponentPropertyModel;
+import org.apache.wicket.model.PropertyModel;
 
 public class BasicResourceConfig extends ResourceConfigurationPanel {
 
-	public BasicResourceConfig(String id) {
-		super(id);
+	String myNewKeyword = "";
+	List<String> mySelectedKeywords;
+	
+	public BasicResourceConfig(String id, IModel model) {
+		super(id, model);
+		init();
 		add(new Label("section", "Basic"));
 		add(new TextField("title"));
 		add(new TextField("abstract"));
-//		add(new TextField("keywords"));
-//		add(new TextField("SRS"));
-//		add(new TextField("nativeBoundingBox"));
-//		add(new TextField("boundingBox"));
+		add(new ListMultipleChoice("keywords", new PropertyModel(this, "mySelectedKeywords"), new ComponentPropertyModel("keywords")));
+        add(new Button("removeKeywords"){
+            @Override
+            public void onSubmit(){
+                getResourceInfo().getKeywords().removeAll(mySelectedKeywords);
+                mySelectedKeywords.clear();
+            }
+        });
+        add(new TextField("newKeyword", new PropertyModel(this, "myNewKeyword")));
+        add(new Button("addKeyword"){
+            @Override
+            public void onSubmit(){
+                getResourceInfo().getKeywords().add(myNewKeyword);
+                myNewKeyword = "";
+            }
+        });
+		// add(new TextField("SRS"));
+		// add(new TextField("nativeBoundingBox"));
+		// add(new TextField("boundingBox"));
 	}
-
+	
+	private void init(){
+		myNewKeyword = "";
+		mySelectedKeywords = new ArrayList<String>();
+	}
+	
 	private static final long serialVersionUID = 677955476932894110L;
-
 }
