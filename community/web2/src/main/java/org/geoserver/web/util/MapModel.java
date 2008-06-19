3b69b62b0bfdf465528806884289135e57d87cc7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 14:34:37 +0000

    updated javadocs
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9332 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2dc54f7afa0a4535484caf1e62f76aa697ca336a [formerly df09ab9e29060e883aaf8109aea83cd0504562b2]
    Former-commit-id: c2d58394577eec623d06e1d6f03e12360d93e5cf

--- community/web2/src/main/java/org/geoserver/web/util/MapModel.java
+++ community/web2/src/main/java/org/geoserver/web/util/MapModel.java
@@ -6,25 +6,29 @@ import org.apache.wicket.model.IModel;
 
 /**
  * A model which backs onto an underlying map.
+ * <p>
+ * The semantics of this model are similar to {@link #PropertyModel} except for
+ * that expressions map to keys of a map rather than java bean property names.
+ * </p>
  * @author Justin Deoliveira, The Open Planning Project
  *
  */
 public class MapModel implements IModel {
 
     Map map;
-    String key;
+    String expression;
     
-    public MapModel( Map map, String key ) {
+    public MapModel( Map map, String expression ) {
         this.map = map;
-        this.key = key;
+        this.expression = expression;
     }
     
     public Object getObject() {
-        return map.get( key );
+        return map.get( expression );
     }
 
     public void setObject(Object object) {
-        map.put( key, object );
+        map.put( expression, object );
     }
 
     public void detach() {
