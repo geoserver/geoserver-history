a2a11e70e7cf4ceefc2868a336a3b0d7509f3624
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 14:10:10 +0000

    moved data tree to seperate component, added to DataPage
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9322 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 86c86dfea845ade60a6ccc9b61793fcc774f60a5 [formerly aac22510e9a2f4ed18ef6b76a8feae08b1de810e]
    Former-commit-id: 1591db92223aa9eadae4b6c3998cd57f9046baf8

--- community/web2/src/main/java/org/geoserver/web/data/DataPage.java
+++ community/web2/src/main/java/org/geoserver/web/data/DataPage.java
@@ -1,15 +1,25 @@
 package org.geoserver.web.data;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Form;
 import org.geoserver.web.GeoServerBasePage;
+import org.geoserver.web.data.tree.DataTreeTable;
 
 public class DataPage extends GeoServerBasePage {
 
     public DataPage( ) {
-        
-//        add( new DataPanel( "dataPanel" ) );
-//        
-//        add( new BookmarkablePageLink( "addWorkspaceLink", AddWorkspacePage.class ) );
-//        add( new BookmarkablePageLink( "addDataSetLink", AddDataSetPage.class ) );
-        
+    	final DataTreeTable tree = new DataTreeTable( "dataTree", this );
+    	add( tree );
+    	
+    	Form form = new Form("controlForm");
+        form.add(new AjaxButton("collapseAll") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                tree.getTreeState().collapseAll();
+                target.addComponent(tree);
+            }
+        });
+        add(form);
     }
 }
