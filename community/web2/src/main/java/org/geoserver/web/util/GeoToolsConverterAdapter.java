3ac03cc0649cfc08e5cbaecddff7b6016bb6c8b0
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 20:49:45 +0000

    Add a whitelist converter locator, get rid of the automated one since it was stepping on Wicket's toes.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9347 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5516650c386c3cce5e98829374a5969877b7801b [formerly b6de953bf54eee8e4db58076bc40251112b383b8]
    Former-commit-id: e163948a0495b771e8ea8e0ec9e656d91a9b522f

---
+++ community/web2/src/main/java/org/geoserver/web/util/GeoToolsConverterAdapter.java
@@ -0,0 +1,33 @@
+package org.geoserver.web.util;
+
+import org.apache.wicket.util.convert.IConverter;
+import org.geotools.util.Converter;
+
+import java.util.Locale;
+
+public class GeoToolsConverterAdapter implements IConverter {
+    Converter myConverter;
+    Class<?> myTarget;
+
+    public GeoToolsConverterAdapter(Converter c, Class<?> target){
+        myConverter = c;
+        myTarget = target;
+    }
+
+    public Object convertToObject(String str, Locale locale){
+        try{
+            return myConverter.convert(str, myTarget);
+        } catch (Exception e){
+            return null;
+        }
+
+    }
+
+    public String convertToString(Object o, Locale locale){
+        try{
+            return myConverter.convert(o, String.class);
+        } catch (Exception e){
+            return null;
+        }
+    }
+}
