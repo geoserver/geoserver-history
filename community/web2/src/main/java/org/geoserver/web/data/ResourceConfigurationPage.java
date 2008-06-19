8ac581a21da8a1e2f61045796f705f851fdb9880
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 18:15:17 +0000

    Slightly nicer version of resource configuration panel, with a list editor for the keywords.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9335 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3d6e357cafd9d81d277e6bd663bde6f817e81ba3 [formerly d07e6dfa3c1f9d9fa8b8a3f5e1dc0d662f0d24e9]
    Former-commit-id: 6ffc8b6ec72949f4223e83e1fc78f3024cf2dc6b

--- community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPage.java
+++ community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPage.java
@@ -1,11 +1,13 @@
 package org.geoserver.web.data;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.geoserver.catalog.ResourceInfo;
 import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerBasePage;
@@ -13,12 +15,14 @@ import org.geoserver.web.GeoServerBasePage;
 public class ResourceConfigurationPage extends GeoServerBasePage {
 
 	private ResourceInfo myResourceInfo;
+    private IModel myResourceModel;
 	
 	public ResourceConfigurationPage(ResourceInfo info) {
 		myResourceInfo = info;
+        myResourceModel = new CompoundPropertyModel(myResourceInfo);
 		
 		add(new Label("resourcename", myResourceInfo.getId()));
-        Form theForm = new Form("resource", new CompoundPropertyModel(myResourceInfo));
+        Form theForm = new Form("resource", myResourceModel);
         add(theForm);
         theForm.add(new ConfigurationSectionListView("configuration"));
 	}
@@ -28,7 +32,10 @@ public class ResourceConfigurationPage extends GeoServerBasePage {
 		private static final long serialVersionUID = -6575960326680386479L;
 
 		public ConfigurationSectionListView(String id) {
-			super(id, ((GeoServerApplication)getGeoServerApplication()).getBeansOfType(ResourceConfigurationPanelInfo.class));
+            super(id, 
+                    ((GeoServerApplication)getGeoServerApplication())
+                    .getBeansOfType(ResourceConfigurationPanelInfo.class)
+                 );
 		}
 
 		@Override
@@ -36,11 +43,13 @@ public class ResourceConfigurationPage extends GeoServerBasePage {
 			ResourceConfigurationPanelInfo panelInfo = (ResourceConfigurationPanelInfo) item
 					.getModelObject();
 			try {
-				ResourceConfigurationPanel panel = panelInfo
-						.getComponentClass().getConstructor(String.class).newInstance("content");
+                ResourceConfigurationPanel panel = panelInfo
+                    .getComponentClass()
+                    .getConstructor(String.class, IModel.class)
+                    .newInstance("content", myResourceModel);
 				item.add((Component) panel);
 			} catch (Exception e) {
-                e.printStackTrace();
+                throw new WicketRuntimeException("Failed to add pluggable resource configuration panels", e);
 			}
 		}
 	}
--- community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPanel.java
+++ community/web2/src/main/java/org/geoserver/web/data/ResourceConfigurationPanel.java
@@ -1,12 +1,18 @@
 package org.geoserver.web.data;
 
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.geoserver.catalog.ResourceInfo;
 
 public class ResourceConfigurationPanel extends Panel {
 	private static final long serialVersionUID = 4881474189619124359L;
 
-	public ResourceConfigurationPanel(String id){
-		super(id);
+	public ResourceConfigurationPanel(String id, IModel model){
+		super(id, model);
+	}
+	
+	public ResourceInfo getResourceInfo(){
+		return (ResourceInfo)getModelObject();
 	}
 	
 }
