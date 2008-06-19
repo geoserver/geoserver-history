d9a3589f5b969049415c11e0f3fa4eebf332001f
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-19 13:56:35 +0000

    datastore configuration updates
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9321 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b9f3d6fd58d81d337073783ccfb72238950de306 [formerly 2e09a91ca2132bf083bdd3b6b353b08b262675a9]
    Former-commit-id: 99bef67a48732bccadc369f3f53104df3bc08d77

--- community/web2/src/main/java/org/geoserver/web/data/datastore/DataStoreConfiguration.java
+++ community/web2/src/main/java/org/geoserver/web/data/datastore/DataStoreConfiguration.java
@@ -1,25 +1,31 @@
 package org.geoserver.web.data.datastore;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.repeater.RepeatingView;
+import org.geoserver.catalog.Catalog;
 import org.geoserver.web.GeoServerBasePage;
+import org.geoserver.web.GeoServerHomePage;
+import org.geoserver.web.data.DataPage;
 import org.geoserver.web.data.datastore.panel.CheckBoxParamPanel;
 import org.geoserver.web.data.datastore.panel.PasswordParamPanel;
 import org.geoserver.web.data.datastore.panel.TextParamPanel;
+import org.geoserver.web.data.tree.GeoServerDataTreePage;
+import org.geotools.data.DataAccessFactory;
+import org.geotools.data.DataStoreFactorySpi;
 import org.geotools.data.DataAccessFactory.Param;
 import org.geotools.data.postgis.PostgisDataStoreFactory;
+import org.vfny.geoserver.util.DataStoreUtils;
 
 /**
  * Provides a form to configure a geotools DataStore
@@ -28,25 +34,71 @@ import org.geotools.data.postgis.PostgisDataStoreFactory;
  */
 public class DataStoreConfiguration extends GeoServerBasePage {
 
-    public DataStoreConfiguration() {
-        Param[] paramInfo = new PostgisDataStoreFactory().getParametersInfo();
+    /**
+     * Holds datastore parameters. Properties will be settled by the form input
+     * fields.
+     */
+    private final Map<String, ?> parametersMap;
 
-        List<Param> parameterPanels = Arrays.asList(paramInfo);
+    public DataStoreConfiguration(final String dataStoreFactDisplayName) {
+        parametersMap = new HashMap<String, Object>();
 
-        Form paramsForm = new Form("form");
+        final List<ParamInfo> paramsInfo = new ArrayList<ParamInfo>();
+        {
+            DataStoreFactorySpi dsFact = DataStoreUtils.aquireFactory(dataStoreFactDisplayName);
+            if (dsFact == null) {
+                throw new IllegalArgumentException("Can't locate a datastore factory named '"
+                        + dataStoreFactDisplayName + "'");
+            }
+            Param[] dsParams = dsFact.getParametersInfo();
+            for (Param p : dsParams) {
+                paramsInfo.add(new ParamInfo(p));
+            }
+        }
+
+        final Form paramsForm = new Form("dataStoreForm") {
+            @Override
+            public void onSubmit() {
+                setResponsePage(GeoServerHomePage.class);
+            }
+        };
+
         add(paramsForm);
 
-        ListView paramsList = new ListView("parameters", parameterPanels) {
+        final TextField textField = new TextField("dataStoreId");
+        paramsForm.add(textField);
+
+        ListView paramsList = new ListView("parameters", paramsInfo) {
             @Override
             protected void populateItem(ListItem item) {
-                Param parameter = (Param) item.getModelObject();
-                Component inputComponent = getInputComponent("parameterPanel", parameter);
+                ParamInfo parameter = (ParamInfo) item.getModelObject();
+                Component inputComponent = getInputComponent("parameterPanel", parametersMap,
+                        parameter);
+                if (parameter.getTitle() != null) {
+                    inputComponent.add(new SimpleAttributeModifier("title", parameter.getTitle()));
+                }
                 item.add(inputComponent);
             }
         };
         // needed for form components not to loose state
         paramsList.setReuseItems(true);
+
         paramsForm.add(paramsList);
+
+        paramsForm.add(new Button("cancel") {
+            @Override
+            public void onSubmit() {
+                setResponsePage(DataPage.class);
+            }
+        });
+
+        paramsForm.add(new Button("submit") {
+            @Override
+            public void onSubmit() {
+                Catalog catalog = getCatalog();
+                setResponsePage(GeoServerDataTreePage.class);
+            }
+        });
     }
 
     /**
@@ -56,15 +108,16 @@ public class DataStoreConfiguration extends GeoServerBasePage {
      * @param param
      * @return
      */
-    private Panel getInputComponent(final String componentId, final Param param) {
-        final Class binding = param.type;
+    private Panel getInputComponent(final String componentId, final Map<String, ?> paramsMap,
+            final ParamInfo param) {
+        final Class binding = param.getBinding();
         Panel parameterPanel;
         if (Boolean.class == binding) {
-            parameterPanel = new CheckBoxParamPanel(componentId, param);
+            parameterPanel = new CheckBoxParamPanel(componentId, paramsMap, param);
         } else if (String.class == binding && param.isPassword()) {
-            parameterPanel = new PasswordParamPanel(componentId, param);
+            parameterPanel = new PasswordParamPanel(componentId, paramsMap, param);
         } else {
-            parameterPanel = new TextParamPanel(componentId, param);
+            parameterPanel = new TextParamPanel(componentId, paramsMap, param);
         }
         return parameterPanel;
     }
--- community/web2/src/main/java/org/geoserver/web/data/datastore/panel/CheckBoxParamPanel.java
+++ community/web2/src/main/java/org/geoserver/web/data/datastore/panel/CheckBoxParamPanel.java
@@ -1,9 +1,13 @@
 package org.geoserver.web.data.datastore.panel;
 
+import java.util.Map;
+
 import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.geoserver.web.data.datastore.ParamInfo;
+import org.geoserver.web.util.MapModel;
 import org.geotools.data.DataAccessFactory.Param;
 
 /**
@@ -14,12 +18,13 @@ public class CheckBoxParamPanel extends Panel {
 
     private static final long serialVersionUID = -8587266542399491587L;
 
-    public CheckBoxParamPanel(final String id, final Param parameter) {
+    public CheckBoxParamPanel(final String id, final Map<String, ?> paramsMap,
+            final ParamInfo parameter) {
         super(id);
-        Label label = new Label("paramName", parameter.key);
-        CheckBox checkBox = new CheckBox("paramValue");
-        final boolean checked = Boolean.valueOf(String.valueOf(parameter.sample));
-        if(checked){
+        Label label = new Label("paramName", parameter.getName());
+        CheckBox checkBox = new CheckBox("paramValue", new MapModel(paramsMap, parameter.getName()));
+        final boolean checked = Boolean.valueOf(String.valueOf(parameter.getValue()));
+        if (checked) {
             checkBox.add(new SimpleAttributeModifier("checked", "checked"));
         }
         add(label);
