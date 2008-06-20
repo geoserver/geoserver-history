dcf89ef7b3ddec9441b90a70d01f8f81f5e03eca
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-20 09:56:51 +0000

    serv ice navigation
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9381 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c7b6f660361485c4bcc70bb94c667672ff0373a4 [formerly d5144d19b80b957aa6610db8d91f2e458152e9b1]
    Former-commit-id: 603eccf4518029f3ba984ef9c4d82d1c395d6cfe

--- community/web2/wfs/src/main/java/org/geoserver/wfs/web/WFSPage.java
+++ community/web2/wfs/src/main/java/org/geoserver/wfs/web/WFSPage.java
@@ -1,7 +1,11 @@
 package org.geoserver.wfs.web;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.geoserver.web.GeoServerBasePage;
 
 public class WFSPage extends GeoServerBasePage {
 
+    public WFSPage() {
+        
+    }
 }
