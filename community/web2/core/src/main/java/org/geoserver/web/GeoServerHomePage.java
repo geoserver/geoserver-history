d7f35715e58ba9228a626863e98fee3a3c450387
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-20 09:53:11 +0000

    primary navigation and i18n
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9380 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ba89d1bde8def5aae7b3c709dd857f759151d149 [formerly 5a49e2e38d6ba1dccbee4c380b8183fcb0b033e6]
    Former-commit-id: 6b22de7898eec77d71b3b62ec79800e423dbd976

--- community/web2/core/src/main/java/org/geoserver/web/GeoServerHomePage.java
+++ community/web2/core/src/main/java/org/geoserver/web/GeoServerHomePage.java
@@ -1,52 +1,7 @@
 package org.geoserver.web;
 
-import java.util.List;
-
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.geoserver.catalog.ResourceInfo;
-import org.geoserver.catalog.StoreInfo;
-import org.geoserver.web.data.ResourceConfigurationPage;
-
 public class GeoServerHomePage extends GeoServerBasePage {
 
     public GeoServerHomePage() {
-        
-        //TODO: ensure order by adding the well-known pages manually and then 
-        // processing any remaining pages
-        final List<MainPageInfo> pages = ((GeoServerApplication) getApplication()).getBeansOfType(MainPageInfo.class);
-        ListView view = new ListView( "pages", pages ) {
-			private static final long serialVersionUID = 8943715788283282175L;
-
-			protected void populateItem(ListItem item) {
-                MainPageInfo page = (MainPageInfo) item.getModelObject();
-                
-                BookmarkablePageLink link = new BookmarkablePageLink( "page", page.getComponentClass() ) ;
-                link.add( new Label( "label", page.getTitleKey() ) );
-                item.add(link);
-            }
-        };
-        
-        add( view );
-        
-        List<ResourceInfo> resources = getGeoServer().getCatalog().getResources(ResourceInfo.class);
-        view = new ListView("resources", resources){
-        	@Override
-        	protected void populateItem(ListItem item) {
-        		final ResourceInfo info = (ResourceInfo)item.getModelObject();
-        		Link link = new Link("resourcelink"){
-        			@Override
-        			public void onClick() {
-        				setResponsePage(new ResourceConfigurationPage(info));
-        			}
-        		};
-        		link.add(new Label("resourcelabel", info.getId()));
-        		item.add(link);
-        	}
-        };
-        add(view);
     }
 }
