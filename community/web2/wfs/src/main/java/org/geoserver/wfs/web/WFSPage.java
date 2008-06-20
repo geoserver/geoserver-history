ae56ee67a641d531ccc74700bb9134ade2ded5bd
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-20 19:14:14 +0000

    first cut of wfs admin page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9395 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3d45e2377172068bac67672fffdcf50ebbf45081 [formerly ca76e06581da94a3c6fb7648e395b7bc952db758]
    Former-commit-id: c8fe240a12f693b3acb8053d9d9739f7dfab43ad

--- community/web2/wfs/src/main/java/org/geoserver/wfs/web/WFSPage.java
+++ community/web2/wfs/src/main/java/org/geoserver/wfs/web/WFSPage.java
@@ -1,11 +1,31 @@
 package org.geoserver.wfs.web;
 
-import org.apache.wicket.markup.html.basic.Label;
-import org.geoserver.web.GeoServerBasePage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.Radio;
+import org.apache.wicket.markup.html.form.RadioGroup;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.geoserver.web.services.BaseServiceAdminPage;
+import org.geoserver.wfs.WFSInfo;
 
-public class WFSPage extends GeoServerBasePage {
+public class WFSPage extends BaseServiceAdminPage<WFSInfo> {
 
-    public WFSPage() {
-        
+    protected Class<WFSInfo> getServiceClass() {
+        return WFSInfo.class;
     }
+    
+    protected void build(WFSInfo info, Form form) {
+        
+        //service level
+        RadioGroup sl = new RadioGroup( "serviceLevel" );
+        form.add( sl );
+        
+        sl.add( new Radio( "basic", new Model( WFSInfo.ServiceLevel.BASIC ) ) );
+        sl.add( new Radio( "transactional", new Model( WFSInfo.ServiceLevel.TRANSACTIONAL  ) ) );
+        sl.add( new Radio( "complete", new Model( WFSInfo.ServiceLevel.COMPLETE ) ) );
+        
+        //max features
+        form.add( new TextField( "maxFeatures" ) );
+    }
+    
 }
