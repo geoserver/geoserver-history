e38f8bac719eb472e005595d0d72f013043c4d74
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-21 08:31:59 +0000

    Added HTTP layer configuration panel.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9402 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9904086ddf3db37cabb9a63c785fd8c094e1869f [formerly 24b7307e149b8acf94a02bb5f9675315ffa4aff7]
    Former-commit-id: 3b38fecc3ccf80318b85200d4e8833b07413903c

---
+++ community/web2/core/src/main/java/org/geoserver/web/publish/HTTPLayerConfig.java
@@ -0,0 +1,17 @@
+package org.geoserver.web.publish;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.TextField;
+import org.geoserver.web.util.MapModel;
+
+public class HTTPLayerConfig extends LayerConfigurationPanel {
+    public HTTPLayerConfig(String id, IModel model){
+        super(id, model);
+        add(new Label("section", "HTTP Settings"));
+        add(new CheckBox("cachingEnabled", new MapModel(getLayerInfo().getResource().getMetadata(), "cachingEnabled")));
+        add(new TextField("cacheAgeMax", new MapModel(getLayerInfo().getResource().getMetadata(), "cacheAgeMax")));
+    }
+}
+
