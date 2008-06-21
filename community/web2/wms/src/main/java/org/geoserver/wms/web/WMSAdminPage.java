bd9f70db332add1dee7da0645ec85ce66e56679f
(no author) <(no author)@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-21 15:57:16 +0000

    additions to the wms admin page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9465 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b20faa680fb71f8d55b1b615da2c2031a12a7421 [formerly 9f3831e6e61b6961705857d80e402371b7ac5082]
    Former-commit-id: b8dbbdf2ba23bd519fe2c92a5bf260dbc06ad81e

--- community/web2/wms/src/main/java/org/geoserver/wms/web/WMSAdminPage.java
+++ community/web2/wms/src/main/java/org/geoserver/wms/web/WMSAdminPage.java
@@ -1,15 +1,18 @@
 package org.geoserver.wms.web;
 
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.Radio;
-import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.markup.html.form.CheckBox;
 import org.geoserver.web.services.BaseServiceAdminPage;
 import org.geoserver.wms.WMSInfo;
+import org.geoserver.wms.WatermarkInfo;
+
+import java.util.Arrays;
 
 public class WMSAdminPage extends BaseServiceAdminPage<WMSInfo> {
 
+	
     protected Class<WMSInfo> getServiceClass() {
         return WMSInfo.class;
     }
@@ -17,16 +20,13 @@ public class WMSAdminPage extends BaseServiceAdminPage<WMSInfo> {
     protected void build(WMSInfo info, Form form) {
         
     	form.add( new TextField("interpolation"));
-        //service level
-        /*RadioGroup sl = new RadioGroup( "serviceLevel" );
-        form.add( sl );
+    	form.add( new CheckBox("watermark.enabled"));
+    	form.add( new TextField("watermark.uRL"));
+    	form.add( new TextField("watermark.transparency"));
+
+    	form.add(new DropDownChoice("watermark.position", Arrays.asList(WatermarkInfo.Position.values())));
         
-        sl.add( new Radio( "basic", new Model( WFSInfo.ServiceLevel.BASIC ) ) );
-        sl.add( new Radio( "transactional", new Model( WFSInfo.ServiceLevel.TRANSACTIONAL  ) ) );
-        sl.add( new Radio( "complete", new Model( WFSInfo.ServiceLevel.COMPLETE ) ) );
-        
-        //max features
-        form.add( new TextField( "maxFeatures" ) );*/
+
     }
     
 }
