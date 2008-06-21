69f6d56ee50e8219c5784a1d9d25afe7c8037cdb
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-21 09:30:55 +0000

    added namespace configuration skeleton page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9412 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4899181efff117d75d272d895cd4f2285338c5a4 [formerly 36eb914fc1862dc7cbcf974b0efb93dacd3e48f5]
    Former-commit-id: b8be7650520512cdf5cc01826bedc014b602d8f2

---
+++ geoserver/community/web2/core/src/main/java/org/geoserver/web/data/NamespacesConfigurationPage.java
@@ -0,0 +1,10 @@
+package org.geoserver.web.data;
+
+import org.geoserver.web.GeoServerBasePage;
+
+public class NamespacesConfigurationPage extends GeoServerBasePage {
+
+    public NamespacesConfigurationPage(){
+        
+    }
+}
