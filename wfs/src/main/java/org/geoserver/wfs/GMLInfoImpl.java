65f1aa85795ed3c46f8e406efc732f316354286c
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-21 10:37:33 +0000

    implemented hashCode() and equals(), added maxFeatures to WFSInfo
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9423 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 79d419a4edf919851e67c529964f9c32f1054d13 [formerly c2ca0b68ee5bb461d49d2b11dfbc3fcfeeaaa9db]
    Former-commit-id: fd00f391bf8f916c901ea5a69fd8090eb936ca54

--- wfs/src/main/java/org/geoserver/wfs/GMLInfoImpl.java
+++ wfs/src/main/java/org/geoserver/wfs/GMLInfoImpl.java
@@ -21,6 +21,33 @@ public class GMLInfoImpl implements GMLInfo {
         this.featureBounding = featureBounding;
     }
 
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (featureBounding ? 1231 : 1237);
+        result = prime * result
+                + ((srsNameStyle == null) ? 0 : srsNameStyle.hashCode());
+        return result;
+    }
 
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!( obj instanceof GMLInfo)) 
+            return false;
+        
+        final GMLInfo other = (GMLInfo) obj;
+        if (featureBounding != other.isFeatureBounding())
+            return false;
+        if (srsNameStyle == null) {
+            if (other.getSrsNameStyle() != null)
+                return false;
+        } else if (!srsNameStyle.equals(other.getSrsNameStyle()))
+            return false;
+        return true;
+    }
 
+    
 }
--- wfs/src/main/java/org/geoserver/wfs/WFSInfoImpl.java
+++ wfs/src/main/java/org/geoserver/wfs/WFSInfoImpl.java
@@ -8,7 +8,12 @@ import org.geoserver.config.impl.ServiceInfoImpl;
 public class WFSInfoImpl extends ServiceInfoImpl implements WFSInfo {
 
     Map<Version,GMLInfo> gml = new HashMap<Version, GMLInfo>();
-    ServiceLevel serviceLevel;
+    ServiceLevel serviceLevel = ServiceLevel.COMPLETE;
+    int maxFeatures = Integer.MAX_VALUE;
+    
+    public WFSInfoImpl() {
+        System.out.println("here");
+    }
     
     public Map<Version, GMLInfo> getGML() {
         return gml;
@@ -21,5 +26,47 @@ public class WFSInfoImpl extends ServiceInfoImpl implements WFSInfo {
     public void setServiceLevel(ServiceLevel serviceLevel) {
         this.serviceLevel = serviceLevel;
     }
+    
+    public void setMaxFeatures(int maxFeatures) {
+        this.maxFeatures = maxFeatures;
+    }
+    
+    public int getMaxFeatures() {
+        return maxFeatures;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((gml == null) ? 0 : gml.hashCode());
+        result = prime * result + maxFeatures;
+        result = prime * result
+                + ((serviceLevel == null) ? 0 : serviceLevel.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (!( obj instanceof WFSInfo) )
+            return false;
+        final WFSInfo other = (WFSInfo) obj;
+        if (gml == null) {
+            if (other.getGML() != null)
+                return false;
+        } else if (!gml.equals(other.getGML()))
+            return false;
+        if (maxFeatures != other.getMaxFeatures())
+            return false;
+        if (serviceLevel == null) {
+            if (other.getServiceLevel() != null)
+                return false;
+        } else if (!serviceLevel.equals(other.getServiceLevel()))
+            return false;
+        return true;
+    }
 }
