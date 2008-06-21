e63eadfc6898d7f89c7a8a95d0af99853f044861
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-21 11:13:06 +0000

    Separate resource configuration page into Data and Publishing tabs.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9434 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e1bc5beecbb9ff68c9dd3a18581469a23a76f2aa [formerly 7c5be6334f6e85cd6ac4e20d2b8f8405e3d6a852]
    Former-commit-id: 141740944b440f1e94dba9b52cd15a37364f4235

--- community/web2/core/src/main/java/org/geoserver/web/data/ResourceConfigurationPage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/ResourceConfigurationPage.java
@@ -1,5 +1,8 @@
 package org.geoserver.web.data;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.markup.html.basic.Label;
@@ -7,7 +10,9 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.IModel;
 import org.geoserver.catalog.ResourceInfo;
 import org.geoserver.catalog.LayerInfo;
@@ -15,6 +20,10 @@ import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.publish.LayerConfigurationPanel;
 import org.geoserver.web.publish.LayerConfigurationPanelInfo;
+
+import org.apache.wicket.extensions.markup.html.tabs.ITab;
+import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
+import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 
 public class ResourceConfigurationPage extends GeoServerBasePage {
 
@@ -49,8 +58,21 @@ public class ResourceConfigurationPage extends GeoServerBasePage {
         add(new Label("resourcename", myResourceInfo.getId()));
         Form theForm = new Form("resource", myResourceModel);
         add(theForm);
-        theForm.add(new ResourceConfigurationSectionListView("resources"));
-        theForm.add(new LayerConfigurationSectionListView("layers"));
+        List<ITab> tabs = new ArrayList<ITab>();
+        tabs.add(new AbstractTab(new Model("Data")){
+            public Panel getPanel(String panelID){
+                return new ListPanel(panelID, new ResourceConfigurationSectionListView("theList"));
+            }
+        });
+
+        tabs.add(new AbstractTab(new Model("Publishing")){
+            public Panel getPanel(String panelID) {
+                return new ListPanel(panelID, new LayerConfigurationSectionListView("theList"));
+            }
+        });
+        theForm.add(new TabbedPanel("tabs", tabs));
+        /* theForm.add(new ResourceConfigurationSectionListView("resources"));
+        theForm.add(new LayerConfigurationSectionListView("layers"));*/
         theForm.add(new Button("saveButton"){
             public void onSubmit(){
                 if (isNew){
