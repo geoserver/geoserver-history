cba138b4691e67cde3392450335d60e345a881b9
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-23 07:39:12 +0000

    More icons integration
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9483 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7557d98588c4dbdaeafe6e87e6fa9c6c11551132 [formerly 6ebbd7eed7d0d24bde5fa70f0f4d5573e523e8ea]
    Former-commit-id: 6280458d3b834153a521d5564ebdd256adc1001c

--- geoserver/community/web2/core/src/main/java/org/geoserver/web/data/NewDataPage.java
+++ geoserver/community/web2/core/src/main/java/org/geoserver/web/data/NewDataPage.java
@@ -7,10 +7,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.data.coverage.RasterCoverageConfiguration;
 import org.geoserver.web.data.datastore.DataStoreConfiguration;
@@ -31,27 +34,33 @@ import org.opengis.coverage.grid.Format;
  * @author Gabriel Roldan
  */
 public class NewDataPage extends GeoServerBasePage {
+    
+    transient Map<String, DataAccessFactory> dataStores = getAvailableDataStores();
+    transient Map<String, Format> coverages = getAvailableCoverageStores();
 
     /**
      * Creates the page components to present the list of available vector and
      * raster data source types
      * 
      * @param workspaceId
-     *            the id of the workspace to attach the new resource store to.
+     *                the id of the workspace to attach the new resource store
+     *                to.
      */
     public NewDataPage(final String workspaceId) {
 
-        final Map<String, String> dataStoreNames = getAvailableDataStoreNames();
-        final Map<String, String> coverageNames = getAvailableCoverageStoreNames();
-
-        final ArrayList<String> sortedDsNames = new ArrayList<String>(dataStoreNames.keySet());
+        final ArrayList<String> sortedDsNames = new ArrayList<String>(
+                dataStores.keySet());
         Collections.sort(sortedDsNames);
 
-        final ListView dataStoreLinks = new ListView("vectorResources", sortedDsNames) {
+        final ListView dataStoreLinks = new ListView("vectorResources",
+                sortedDsNames) {
             @Override
             protected void populateItem(ListItem item) {
-                final String dataStoreFactoryName = item.getModelObjectAsString();
-                final String description = dataStoreNames.get(dataStoreFactoryName);
+                final String dataStoreFactoryName = item
+                        .getModelObjectAsString();
+                final DataAccessFactory factory = dataStores
+                        .get(dataStoreFactoryName);
+                final String description = factory.getDescription();
                 Link link;
                 link = new Link("resourcelink") {
                     @Override
@@ -63,27 +72,36 @@ public class NewDataPage extends GeoServerBasePage {
                 link.add(new Label("resourcelabel", dataStoreFactoryName));
                 item.add(link);
                 item.add(new Label("resourceDescription", description));
+                item.add(new Image("storeIcon",
+                        getStoreIcon(factory.getClass())));
             }
         };
 
-        final List<String> sortedCoverageNames = new ArrayList<String>(coverageNames.keySet());
+        final List<String> sortedCoverageNames = new ArrayList<String>(
+                coverages.keySet());
         Collections.sort(sortedCoverageNames);
 
-        final ListView coverageLinks = new ListView("rasterResources", sortedCoverageNames) {
+        final ListView coverageLinks = new ListView("rasterResources",
+                sortedCoverageNames) {
             @Override
             protected void populateItem(ListItem item) {
-                final String coverageFactoryName = item.getModelObjectAsString();
-                final String description = coverageNames.get(coverageFactoryName);
+                final String coverageFactoryName = item
+                        .getModelObjectAsString();
+                Format format = coverages.get(coverageFactoryName);
+                final String description = format.getDescription();
                 Link link;
                 link = new Link("resourcelink") {
                     @Override
                     public void onClick() {
-                        setResponsePage(new RasterCoverageConfiguration(workspaceId, coverageFactoryName));
+                        setResponsePage(new RasterCoverageConfiguration(
+                                workspaceId, coverageFactoryName));
                     }
                 };
                 link.add(new Label("resourcelabel", coverageFactoryName));
                 item.add(link);
                 item.add(new Label("resourceDescription", description));
+                item.add(new Image("storeIcon",
+                        getStoreIcon(format.getClass())));
             }
         };
 
@@ -91,18 +109,38 @@ public class NewDataPage extends GeoServerBasePage {
         add(coverageLinks);
     }
 
+    protected ResourceReference getStoreIcon(Class factoryClass) {
+        // look for the associated panel info if there is one
+        List<DataStorePanelInfo> infos = getGeoServerApplication()
+                .getBeansOfType(DataStorePanelInfo.class);
+        for (DataStorePanelInfo panelInfo : infos) {
+            if (panelInfo.getFactoryClass().equals(factoryClass))
+                return new ResourceReference(panelInfo.getIconBase(), panelInfo
+                        .getIcon());
+        }
+
+        if (DataAccessFactory.class.isAssignableFrom(factoryClass))
+            // fall back on generic vector icon otherwise
+            return new ResourceReference(GeoServerApplication.class,
+                    "img/icons/geosilk/vector.png");
+        else
+            return new ResourceReference(GeoServerApplication.class,
+                    "img/icons/geosilk/raster.png");
+
+    }
+
     /**
      * @return the name/description set of available datastore factories
      */
-    private Map<String, String> getAvailableDataStoreNames() {
+    private Map<String, DataAccessFactory> getAvailableDataStores() {
         final Iterator<DataAccessFactory> availableDataStores;
         availableDataStores = DataAccessFinder.getAvailableDataStores();
 
-        Map<String, String> storeNames = new HashMap<String, String>();
+        Map<String, DataAccessFactory> storeNames = new HashMap<String, DataAccessFactory>();
 
         while (availableDataStores.hasNext()) {
             DataAccessFactory factory = availableDataStores.next();
-            storeNames.put(factory.getDisplayName(), factory.getDescription());
+            storeNames.put(factory.getDisplayName(), factory);
         }
         return storeNames;
     }
@@ -110,11 +148,11 @@ public class NewDataPage extends GeoServerBasePage {
     /**
      * @return the name/description set of available raster formats
      */
-    private Map<String, String> getAvailableCoverageStoreNames() {
+    private Map<String, Format> getAvailableCoverageStores() {
         Format[] availableFormats = GridFormatFinder.getFormatArray();
-        Map<String, String> formatNames = new HashMap<String, String>();
+        Map<String, Format> formatNames = new HashMap<String, Format>();
         for (Format format : availableFormats) {
-            formatNames.put(format.getName(), format.getDescription());
+            formatNames.put(format.getName(), format);
         }
         return formatNames;
     }
