51c2bee63d03450ee5bfc13e6f7f3a60dfe5a414
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-25 02:51:22 +0000

    implemented equals() and hashCode()
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@9512 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fe391ebd15cd20205d7ec831aabcc8a8b5388a86 [formerly 3f5147dee2c0474a9fda04eb319fe0e5264edbe0]
    Former-commit-id: 6a6b907739dba9c4bf53c0b6f89bc7b1859275e6

--- main/src/main/java/org/geoserver/config/impl/ContactInfoImpl.java
+++ main/src/main/java/org/geoserver/config/impl/ContactInfoImpl.java
@@ -144,6 +144,101 @@ public class ContactInfoImpl implements ContactInfo {
     public void setOnlineResource(String onlineResource) {
         this.onlineResource = onlineResource;
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((address == null) ? 0 : address.hashCode());
+        result = PRIME * result + ((addressCity == null) ? 0 : addressCity.hashCode());
+        result = PRIME * result + ((addressCountry == null) ? 0 : addressCountry.hashCode());
+        result = PRIME * result + ((addressPostalCode == null) ? 0 : addressPostalCode.hashCode());
+        result = PRIME * result + ((addressState == null) ? 0 : addressState.hashCode());
+        result = PRIME * result + ((addressType == null) ? 0 : addressType.hashCode());
+        result = PRIME * result + ((contactEmail == null) ? 0 : contactEmail.hashCode());
+        result = PRIME * result + ((contactFacsimile == null) ? 0 : contactFacsimile.hashCode());
+        result = PRIME * result + ((contactOrganization == null) ? 0 : contactOrganization.hashCode());
+        result = PRIME * result + ((contactPerson == null) ? 0 : contactPerson.hashCode());
+        result = PRIME * result + ((contactPosition == null) ? 0 : contactPosition.hashCode());
+        result = PRIME * result + ((contactVoice == null) ? 0 : contactVoice.hashCode());
+        result = PRIME * result + ((onlineResource == null) ? 0 : onlineResource.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof ContactInfo))
+            return false;
+        final ContactInfo other = (ContactInfo) obj;
+        if (address == null) {
+            if (other.getAddress() != null)
+                return false;
+        } else if (!address.equals(other.getAddress()))
+            return false;
+        if (addressCity == null) {
+            if (other.getAddressCity() != null)
+                return false;
+        } else if (!addressCity.equals(other.getAddressCity()))
+            return false;
+        if (addressCountry == null) {
+            if (other.getAddressCountry() != null)
+                return false;
+        } else if (!addressCountry.equals(other.getAddressCountry()))
+            return false;
+        if (addressPostalCode == null) {
+            if (other.getAddressPostalCode() != null)
+                return false;
+        } else if (!addressPostalCode.equals(other.getAddressPostalCode()))
+            return false;
+        if (addressState == null) {
+            if (other.getAddressState() != null)
+                return false;
+        } else if (!addressState.equals(other.getAddressState()))
+            return false;
+        if (addressType == null) {
+            if (other.getAddressType() != null)
+                return false;
+        } else if (!addressType.equals(other.getAddressType()))
+            return false;
+        if (contactEmail == null) {
+            if (other.getContactEmail() != null)
+                return false;
+        } else if (!contactEmail.equals(other.getContactEmail()))
+            return false;
+        if (contactFacsimile == null) {
+            if (other.getContactFacsimile() != null)
+                return false;
+        } else if (!contactFacsimile.equals(other.getContactFacsimile()))
+            return false;
+        if (contactOrganization == null) {
+            if (other.getContactOrganization() != null)
+                return false;
+        } else if (!contactOrganization.equals(other.getContactOrganization()))
+            return false;
+        if (contactPerson == null) {
+            if (other.getContactPerson() != null)
+                return false;
+        } else if (!contactPerson.equals(other.getContactPerson()))
+            return false;
+        if (contactPosition == null) {
+            if (other.getContactPosition() != null)
+                return false;
+        } else if (!contactPosition.equals(other.getContactPosition()))
+            return false;
+        if (contactVoice == null) {
+            if (other.getContactVoice()!= null)
+                return false;
+        } else if (!contactVoice.equals(other.getContactVoice()))
+            return false;
+        if (onlineResource == null) {
+            if (other.getOnlineResource() != null)
+                return false;
+        } else if (!onlineResource.equals(other.getOnlineResource()))
+            return false;
+        return true;
+    }
 }
