1ca925ba131a3ded5171d41858c3c2dbd50f16b0
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-06-27 14:02:17 +0000

    Import fix
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9561 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cfca05f3795b39c25fcd8c6327b1e4854ae1c2a5 [formerly 67cdd602ae957092f9a7121496becc6e5f5f079c]
    Former-commit-id: c776757f92ba1a4f4243f1f00ea2dab6827027b5

--- geoserver/community/web2/core/src/main/java/org/geoserver/web/GeoServerApplication.java
+++ geoserver/community/web2/core/src/main/java/org/geoserver/web/GeoServerApplication.java
@@ -12,7 +12,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.logging.Level;
