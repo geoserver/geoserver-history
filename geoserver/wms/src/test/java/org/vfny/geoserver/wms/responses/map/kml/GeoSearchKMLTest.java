efe4485930c80b63f7778dd240f9c0fd80dfe00e
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-17 13:49:34 +0000

    Regionating strategy refactoring, added external sorting strategy, complain when the user asks for a strategy that's not there, deal with centroids falling on tile borders, added random strategy
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@9658 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6029fc18d958d0cd03a987793b06cd51781d55bb [formerly 0139ffeb159b1cb06f80de28f0bba916716af933]
    Former-commit-id: 384c05689e57e6becd73f4ae858fdca479eacfaa

--- geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/kml/GeoSearchKMLTest.java
+++ geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/kml/GeoSearchKMLTest.java
@@ -20,26 +20,26 @@ public class GeoSearchKMLTest extends RegionatingTestSupport {
         assertEquals("kml", document.getDocumentElement().getTagName());;
     }
 
-//    /**
-//     * Test that requests regionated by data actually return stuff.
-//     */
-//    public void testDataRegionator() throws Exception{
-//        final String path = 
-//            "wms?request=getmap&service=wms&version=1.1.1" + 
-//            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
-//            "&layers=" + MockData.DIVIDED_ROUTES.getPrefix() + ":" + MockData.DIVIDED_ROUTES.getLocalPart() + 
-//            "&styles=" + MockData.DIVIDED_ROUTES.getLocalPart() + 
-//            "&height=1024&width=1024&srs=EPSG:4326" +  
-//            "&format_options=regionateBy:data;regionateAttr:NUM_LANES";
-//
-//        Document document = getAsDOM(path + "&bbox=-180,-90,0,90");
-//        assertEquals("kml", document.getDocumentElement().getTagName());
-//        int westCount = document.getDocumentElement().getElementsByTagName("Placemark").getLength();
-//
-//        assertStatusCodeForGet(204, path + "&bbox=0,-90,180,90");
-//
-//        assertEquals(1, westCount);
-//    }
+    /**
+     * Test that requests regionated by data actually return stuff.
+     */
+    public void testDataRegionator() throws Exception{
+        final String path = 
+            "wms?request=getmap&service=wms&version=1.1.1" + 
+            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
+            "&layers=" + MockData.DIVIDED_ROUTES.getPrefix() + ":" + MockData.DIVIDED_ROUTES.getLocalPart() + 
+            "&styles=" + MockData.DIVIDED_ROUTES.getLocalPart() + 
+            "&height=1024&width=1024&srs=EPSG:4326" +  
+            "&format_options=regionateBy:external-sorting;regionateAttr:NUM_LANES";
+
+        Document document = getAsDOM(path + "&bbox=-180,-90,0,90");
+        assertEquals("kml", document.getDocumentElement().getTagName());
+        int westCount = document.getDocumentElement().getElementsByTagName("Placemark").getLength();
+
+        assertStatusCodeForGet(204, path + "&bbox=0,-90,180,90");
+
+        assertEquals(1, westCount);
+    }
 
      /**
       * Test that requests regionated by geometry actually return stuff.
@@ -51,7 +51,25 @@ public class GeoSearchKMLTest extends RegionatingTestSupport {
             "&layers=" + MockData.DIVIDED_ROUTES.getPrefix() + ":" + MockData.DIVIDED_ROUTES.getLocalPart() + 
             "&styles=" + MockData.DIVIDED_ROUTES.getLocalPart() + 
             "&height=1024&width=1024&srs=EPSG:4326" +  
-            "&format_options=regionateBy:data;regionateAttr:the_geom";
+            "&format_options=regionateBy:geometry;regionateAttr:the_geom";
+        Document document = getAsDOM(path + "&bbox=-180,-90,0,90");
+        assertEquals("kml", document.getDocumentElement().getTagName());
+        assertEquals(1, document.getDocumentElement().getElementsByTagName("Placemark").getLength());
+
+        assertStatusCodeForGet(204, path + "&bbox=0,-90,180,90");
+    }
+     
+     /**
+      * Test that requests regionated by random criteria actually return stuff.
+      */
+     public void testRandomRegionator() throws Exception{
+        final String path = 
+            "wms?request=getmap&service=wms&version=1.1.1" + 
+            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
+            "&layers=" + MockData.DIVIDED_ROUTES.getPrefix() + ":" + MockData.DIVIDED_ROUTES.getLocalPart() + 
+            "&styles=" + MockData.DIVIDED_ROUTES.getLocalPart() + 
+            "&height=1024&width=1024&srs=EPSG:4326" +  
+            "&format_options=regionateBy:random";
         Document document = getAsDOM(path + "&bbox=-180,-90,0,90");
         assertEquals("kml", document.getDocumentElement().getTagName());
         assertEquals(1, document.getDocumentElement().getElementsByTagName("Placemark").getLength());
@@ -72,62 +90,64 @@ public class GeoSearchKMLTest extends RegionatingTestSupport {
             "&height=1024&width=1024&srs=EPSG:4326" +  
             "&format_options=regionateBy:bogus";
          Document document = getAsDOM(path + "&bbox=0,-90,180,90");
-         assertEquals("kml", document.getDocumentElement().getTagName());
+         assertEquals("ServiceExceptionReport", document.getDocumentElement().getTagName());
     }
 
 
-//    /**
-//     * Test whether geometries that cross tiles get put into both of them.
-//     */
-//    public void testBigGeometries() throws Exception {
-//        final String path = 
-//            "wms?request=getmap&service=wms&version=1.1.1" + 
-//            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
-//            "&layers=" + CENTERED_POLY.getPrefix() + ":" + CENTERED_POLY.getLocalPart() + 
-//            "&styles=" + 
-//            "&height=1024&width=1024&srs=EPSG:4326" +  
-//            "&format_options=regionateBy:data;regionateattr:foo";
-//
-//        Document document = getAsDOM(path + "&bbox=-180,-90,0,90");
-//        assertEquals("kml", document.getDocumentElement().getTagName());
-//        assertEquals(1, document.getDocumentElement().getElementsByTagName("Placemark").getLength());
-//
-//        assertStatusCodeForGet(204, path + "&bbox=0,-90,180,90");
-//    }
+    /**
+     * Test whether geometries that cross tiles get put into both of them.
+     */
+    public void testBigGeometries() throws Exception {
+        final String path = 
+            "wms?request=getmap&service=wms&version=1.1.1" + 
+            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
+            "&layers=" + CENTERED_POLY.getPrefix() + ":" + CENTERED_POLY.getLocalPart() + 
+            "&styles=" + 
+            "&height=1024&width=1024&srs=EPSG:4326" +  
+            "&format_options=regionateBy:external-sorting;regionateattr:foo";
 
-//    /**
-//     * Test whether specifying different regionating strategies changes the results.
-//     */
-//    public void testStrategyChangesStuff() throws Exception {
-//        final String path = 
-//            "wms?request=getmap&service=wms&version=1.1.1" + 
-//            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
-//            "&layers=" + TILE_TESTS.getPrefix() + ":" + TILE_TESTS.getLocalPart() + 
-//            "&bbox=-180,-90,0,90&styles=" + 
-//            "&height=1024&width=1024&srs=EPSG:4326";
-//
-//        FeatureTypeInfo fti = getFeatureTypeInfo(TILE_TESTS);
-//        fti.setRegionateFeatureLimit(2);
-//
-//        Document geo = getAsDOM(path + "&format_options=regionateBy:data;regionateattr:location");
-//        assertEquals("kml", geo.getDocumentElement().getTagName());
-//
-//        NodeList geoPlacemarks = geo.getDocumentElement().getElementsByTagName("Placemark");
-//        assertEquals(2, geoPlacemarks.getLength());
-//
-//        Document data = getAsDOM(path + "&format_options=regionateBy:data;regionateAttr:z");
-//        assertEquals("kml", data.getDocumentElement().getTagName());
-//
-//        NodeList dataPlacemarks = data.getDocumentElement().getElementsByTagName("Placemark");
-//        assertEquals(2, dataPlacemarks.getLength());
-//
-//        for (int i = 0; i < geoPlacemarks.getLength(); i++){
-//            String geoName = ((Element)geoPlacemarks.item(i)).getAttribute("id");
-//            String dataName = ((Element)dataPlacemarks.item(i)).getAttribute("id");
-//
-//            assertTrue(geoName + " and " + dataName + " should not be the same!", 
-//                    !geoName.equals(dataName)
-//            );
-//        }
-//    }
+        assertStatusCodeForGet(204, path +  "&bbox=-180,-90,0,90");
+        
+        Document document = getAsDOM(path + "&bbox=0,-90,180,90");
+        assertEquals("kml", document.getDocumentElement().getTagName());
+        assertEquals(1, document.getDocumentElement().getElementsByTagName("Placemark").getLength());
+
+        
+    }
+
+    /**
+     * Test whether specifying different regionating strategies changes the results.
+     */
+    public void testStrategyChangesStuff() throws Exception {
+        final String path = 
+            "wms?request=getmap&service=wms&version=1.1.1" + 
+            "&format=" + KMLMapProducerFactory.MIME_TYPE + 
+            "&layers=" + TILE_TESTS.getPrefix() + ":" + TILE_TESTS.getLocalPart() + 
+            "&bbox=-180,-90,0,90&styles=" + 
+            "&height=1024&width=1024&srs=EPSG:4326";
+
+        FeatureTypeInfo fti = getFeatureTypeInfo(TILE_TESTS);
+        fti.setRegionateFeatureLimit(2);
+
+        Document geo = getAsDOM(path + "&format_options=regionateBy:geometry;regionateattr:location");
+        assertEquals("kml", geo.getDocumentElement().getTagName());
+
+        NodeList geoPlacemarks = geo.getDocumentElement().getElementsByTagName("Placemark");
+        assertEquals(2, geoPlacemarks.getLength());
+
+        Document data = getAsDOM(path + "&format_options=regionateBy:external-sorting;regionateAttr:z");
+        assertEquals("kml", data.getDocumentElement().getTagName());
+
+        NodeList dataPlacemarks = data.getDocumentElement().getElementsByTagName("Placemark");
+        assertEquals(2, dataPlacemarks.getLength());
+
+        for (int i = 0; i < geoPlacemarks.getLength(); i++){
+            String geoName = ((Element)geoPlacemarks.item(i)).getAttribute("id");
+            String dataName = ((Element)dataPlacemarks.item(i)).getAttribute("id");
+
+            assertTrue(geoName + " and " + dataName + " should not be the same!", 
+                    !geoName.equals(dataName)
+            );
+        }
+    }
 }
