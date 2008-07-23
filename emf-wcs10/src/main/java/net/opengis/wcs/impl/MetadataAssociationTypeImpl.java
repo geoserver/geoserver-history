61e392ca17f8132d05850229ef0a835c6a363b2f
afabiani <afabiani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-23 15:43:11 +0000

    - WCS 1.0 EMF model and bindings
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x_WCS10_EMF@9706 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 09574c6bbfcf9c754747975e0ed8b40f692d64f5

---
+++ emf-wcs10/src/main/java/net/opengis/wcs/impl/MetadataAssociationTypeImpl.java
@@ -0,0 +1,44 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package net.opengis.wcs.impl;
+
+import net.opengis.gml.impl.MetaDataPropertyTypeImpl;
+
+import net.opengis.wcs.MetadataAssociationType;
+import net.opengis.wcs.Wcs10Package;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Metadata Association Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class MetadataAssociationTypeImpl extends MetaDataPropertyTypeImpl implements MetadataAssociationType {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected MetadataAssociationTypeImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected EClass eStaticClass() {
+        return Wcs10Package.Literals.METADATA_ASSOCIATION_TYPE;
+    }
+
+} //MetadataAssociationTypeImpl
