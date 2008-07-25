bdd3c3a85a9cd5fb3b620871515c62f8dc363eee
bencaradocdavies <bencaradocdavies@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-25 03:47:33 +0000

    Code cleanup and refactoring to use BindingUtils. (Patch received from Peter Warren.)
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.6.x@9713 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8eb36a2202993f85e2170e6b738f1824056916a5 [formerly b9b9dc646bd73582c8012bf1a40afa8a24d6ad6a]
    Former-commit-id: 015babac37b81764afd3bc6b30ec3b0df87fa227

--- geoserver/community/community-schemas/wfs-c/src/main/java/org/geoserver/wfs/xml/v1_1_0/overrides/ISOEnvelopeTypeBinding.java
+++ geoserver/community/community-schemas/wfs-c/src/main/java/org/geoserver/wfs/xml/v1_1_0/overrides/ISOEnvelopeTypeBinding.java
@@ -1,99 +1,100 @@
-/**
- * 
+/* Copyright (c) 2008 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
  */
+
 package org.geoserver.wfs.xml.v1_1_0.overrides;
 
-import java.net.URI;
-import java.util.Map;
-
 import javax.xml.namespace.QName;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.geotools.feature.Name;
-import org.geotools.feature.iso.UserData;
 import org.geotools.gml3.bindings.EnvelopeTypeBinding;
 import org.geotools.gml3.bindings.GML;
-import org.geotools.referencing.CRS;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.feature.Attribute;
-import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.xml.sax.Attributes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
+ * Binding object for the type http://www.opengis.net/gml:Envelope
+ * 
  * @author Peter Warren
- *
+ * 
  */
 public class ISOEnvelopeTypeBinding extends EnvelopeTypeBinding {
-	 
-	public Object getProperty(Object object, QName name){
-	        Attribute attribute = (Attribute) object;
-	
-	        
-	        try
-	        {
-	        	Map clientProperties = (Map) ((UserData) attribute).getUserData(Attributes.class);
-	        	if (clientProperties != null) {
-	                Object prop =  clientProperties.get(new Name(name.getLocalPart()));
-	                if (prop != null) {
-	                    return prop;
-	                }
-	            }
-	        }
-	        catch (Exception ex)
-	        {
-	        	return null;
-	        }
-	        return null;
-	}
 
+    /**
+     * Get a property of the attribute.
+     * 
+     * @param object
+     *                attribute (UserData Attribute)
+     * @param name
+     *                name of property
+     * 
+     * @return the property or null if not present
+     * 
+     * @see org.geotools.gml3.bindings.EnvelopeTypeBinding#getProperty(java.lang.Object,
+     *      javax.xml.namespace.QName)
+     */
+    public Object getProperty(Object object, QName name) {
+        return BindingUtils.getPropertyFromUserData(object, name);
+    }
+
+    /**
+     * Encode the Envelope object
+     * 
+     * @param object
+     *                attribute (UserData Attribute)
+     * @param document
+     *                document to encode to
+     * @param value
+     *                the element to encode object to
+     * 
+     * @see org.geotools.gml3.bindings.EnvelopeTypeBinding#encode(java.lang.Object,
+     *      org.w3c.dom.Document,g.w3c.dom.Element)
+     */
     public Element encode(Object object, Document document, Element value)
-    throws Exception {
-    	
-    if ( object instanceof Envelope)
-    {
-    	Envelope envelope = (Envelope) object;
-    
-
-    	if (envelope.isNull()) {
-    		value.appendChild(document.createElementNS(GML.NAMESPACE, GML.Null.getLocalPart()));
-    	}
-    	else
-    	{
-    		Element lowerCorner = document.createElementNS(GML.NAMESPACE,"lowerCorner");
-    		lowerCorner.appendChild(document.createTextNode(envelope.getMinX()+" "+envelope.getMinY()));
-    		value.appendChild(lowerCorner);
-    		Element upperCorner = document.createElementNS(GML.NAMESPACE,"upperCorner");
-    		upperCorner.appendChild(document.createTextNode(envelope.getMaxX()+" "+envelope.getMaxY()));
-    		value.appendChild(upperCorner);
-    	}
- 
+            throws Exception {
+        if (object instanceof Envelope) {
+            Envelope envelope = (Envelope) object;
+            if (envelope.isNull()) {
+                value.appendChild(document.createElementNS(GML.NAMESPACE,
+                        GML.Null.getLocalPart()));
+            } else {
+                Element lowerCorner = document.createElementNS(GML.NAMESPACE,
+                        "lowerCorner");
+                lowerCorner.appendChild(document.createTextNode(envelope
+                        .getMinX()
+                        + " " + envelope.getMinY()));
+                value.appendChild(lowerCorner);
+                Element upperCorner = document.createElementNS(GML.NAMESPACE,
+                        "upperCorner");
+                upperCorner.appendChild(document.createTextNode(envelope
+                        .getMaxX()
+                        + " " + envelope.getMaxY()));
+                value.appendChild(upperCorner);
+            }
+        } else {
+            // we will be creating internal object from individual attribute
+            // values.
+            Attribute envelope = (Attribute) object;
+            Object lc = getProperty(envelope, new QName(GML.NAMESPACE,
+                    "lowerCorner"));
+            if (lc != null) {
+                Element lowerCorner = document.createElementNS(GML.NAMESPACE,
+                        "lowerCorner");
+                lowerCorner.appendChild(document.createTextNode(lc.toString()));
+                value.appendChild(lowerCorner);
+            }
+            Object uc = getProperty(envelope, new QName(GML.NAMESPACE,
+                    "upperCorner"));
+            if (uc != null) {
+                Element upperCorner = document.createElementNS(GML.NAMESPACE,
+                        "upperCorner");
+                upperCorner.appendChild(document.createTextNode(uc.toString()));
+                value.appendChild(upperCorner);
+            }
+        }
+        return value;
     }
-    else 
-    {
-    	// we will be creating internal object from individual attribute values.
-    	Attribute envelope = (Attribute) object;
-    	Object lc = getProperty(envelope, new QName (GML.NAMESPACE, "lowerCorner"));
-    	if ( lc != null )
-    	{
-    		Element lowerCorner = document.createElementNS(GML.NAMESPACE,"lowerCorner");
-    		lowerCorner.appendChild(document.createTextNode( lc.toString()));
-    		value.appendChild(lowerCorner);
-    	}
 
-      	Object uc = getProperty(envelope, new QName (GML.NAMESPACE, "upperCorner"));
-    	if ( uc != null )
-    	{
-    		Element upperCorner = document.createElementNS(GML.NAMESPACE,"upperCorner");
-    		upperCorner.appendChild(document.createTextNode( uc.toString()));
-    		value.appendChild(upperCorner);
-    	}
-
-    }
-    
-    return value;
-    } 
-	
 }
