89b8995ed5bbfc5a9a4cb0c8ebe58610a82f0d27
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-25 15:31:55 +0000

    Alternating CSS classes for styles list.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9715 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a86c33687ce3a7b7f33df66c4776cb66b7731e3a [formerly ca989f789bc74fa5462bc8a3bdadabe0f3afc054]
    Former-commit-id: 41f1c6e341a149b79ad6ce0f1fbedff359e95d31

--- geoserver/community/web2/wms/src/main/java/org/geoserver/wms/web/data/StylesPage.java
+++ geoserver/community/web2/wms/src/main/java/org/geoserver/wms/web/data/StylesPage.java
@@ -7,7 +7,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.repeater.RefreshingView;
 import org.apache.wicket.markup.repeater.Item;
@@ -31,25 +33,40 @@ public class StylesPage extends GeoServerBasePage {
             }
 
             @Override
-            protected void populateItem(Item item){
+            protected void populateItem(final Item item){
+                item.add(
+                    new SimpleAttributeModifier(
+                        "class", 
+                        item.getIndex() % 2 == 0 ? "even" : "odd"
+                        )
+                    );
                 final StyleInfo info = (StyleInfo)item.getModelObject();
                 item.add(new AjaxEditableLabel("name"));
                 item.add(new Link("edit"){
-                    @Override
-                    public void onClick(){
-                        setResponsePage(new StyleEditorPage(info));
-                    }
-                });
+                        @Override
+                        public void onClick(){
+                            setResponsePage(new StyleEditorPage(info));
+                        }
+                    });
                 item.add(new Link("delete"){
-                    @Override
-                    public void onClick(){
-                        getCatalog().remove(info);
-                    }
-                });
+                        @Override
+                        public void onClick(){
+                            getCatalog().remove(info);
+                        }
+                    });
             }
         });
-        add(new AjaxEditableLabel("name", new PropertyModel(this, "name")));
-        add(new Link("add"){
+        
+        WebMarkupContainer container = new WebMarkupContainer("input");
+        container.add(
+                new SimpleAttributeModifier(
+                    "class",
+                    getCatalog().getStyles().size() % 2 == 0 ? "even" : "odd"
+                    )
+                );
+        add(container);
+        container.add(new AjaxEditableLabel("name", new PropertyModel(this, "name")));
+        container.add(new Link("add"){
             @Override
             public void onClick(){
             }
