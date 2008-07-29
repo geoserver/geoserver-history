c35a889683470b32b972dba7fbeb472ea91930eb
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-29 17:06:44 +0000

    Adding a couple of comments in the jpeg writer related to GEOS-2061
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@9741 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 88a79749287b8614ab27746a697eb0e231edd3a7 [formerly d3c8ca4beffc0b4aea27d09085314e975dacf646]
    Former-commit-id: 27415177f8a9e9e922831aedc83111e046f8823a

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/map/jpeg/JPEGMapProducer.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/map/jpeg/JPEGMapProducer.java
@@ -66,14 +66,16 @@ public final class JPEGMapProducer extends DefaultRasterMapProducer {
             LOGGER.fine("About to write a JPEG image.");
         }
 
-        if((JPEGNativeAcc.booleanValue()||!hasJAIWriter)&&(image.getMinX()!=0 || image.getMinY()!=0))
-        {
-        	final WritableRaster raster=(WritableRaster) image.getData();
+        if((JPEGNativeAcc.booleanValue()||!hasJAIWriter)&&(image.getMinX()!=0 || image.getMinY()!=0)) {
+        	// the JPEG native writer does a direct access to the writable raster in a way
+            // that does not respect minx/miny settings. This in turn results in the issue
+            // described at http://jira.codehaus.org/browse/GEOS-2061
+            final WritableRaster raster=(WritableRaster) image.getData();
         	final BufferedImage finalImage= new BufferedImage(image.getColorModel(),raster.createWritableTranslatedChild(0, 0),image.getColorModel().isAlphaPremultiplied(),null);
             new ImageWorker(finalImage).writeJPEG(outStream, "JPEG", 0.75f, JPEGNativeAcc.booleanValue());
+        } else {
+        	new ImageWorker(image).writeJPEG(outStream, "JPEG", 0.75f, JPEGNativeAcc.booleanValue());
         }
-        else
-        	new ImageWorker(image).writeJPEG(outStream, "JPEG", 0.75f, JPEGNativeAcc.booleanValue());	
 
         if (LOGGER.isLoggable(Level.FINE)) {
             LOGGER.fine("Writing a JPEG done!!!");
