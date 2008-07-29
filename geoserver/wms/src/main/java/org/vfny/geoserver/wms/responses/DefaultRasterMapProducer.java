cf9aeaf6de59fa5bd2213bff95dfa9f064441573
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-07-29 11:12:40 +0000

    GEOS-2057, WMS raster interpolation settings are ignored
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.6.x@9739 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 286e19970962da057465ed1582b2dc3f6df1dfff [formerly 0f3a4ec7fb9c29a2af3a108ff0cbbe30feedb258]
    Former-commit-id: 65a6f81678ee4eccaecae3d7f62a40c5c8470399

--- geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/DefaultRasterMapProducer.java
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/DefaultRasterMapProducer.java
@@ -248,20 +248,22 @@ public abstract class DefaultRasterMapProducer extends
                     RenderingHints.VALUE_ANTIALIAS_ON);
         }
 
-        // turn off/on interpolation rendering hint
-        if ((wms != null)
-                && WMSConfig.INT_NEAREST.equals(wms.getAllowInterpolation())) {
-            hintsMap.put(JAI.KEY_INTERPOLATION, NN_INTERPOLATION);
-        } else if ((wms != null)
-                && WMSConfig.INT_BIlINEAR.equals(wms.getAllowInterpolation())) {
-            hintsMap.put(JAI.KEY_INTERPOLATION, BIL_INTERPOLATION);
-        } else if ((wms != null)
-                && WMSConfig.INT_BICUBIC.equals(wms.getAllowInterpolation())) {
-            hintsMap.put(JAI.KEY_INTERPOLATION, BIC_INTERPOLATION);
-        }
-        // line look better with this hint, they are less blurred
-        hintsMap.put(RenderingHints.KEY_STROKE_CONTROL,
-                RenderingHints.VALUE_STROKE_NORMALIZE);
+		// turn off/on interpolation rendering hint
+		if ((wms != null)
+				&& WMSConfig.INT_NEAREST.equals(wms.getAllowInterpolation())) {
+			hintsMap.put(JAI.KEY_INTERPOLATION, NN_INTERPOLATION);
+			hintsMap.put(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+		} else if ((wms != null)
+				&& WMSConfig.INT_BIlINEAR.equals(wms.getAllowInterpolation())) {
+			hintsMap.put(JAI.KEY_INTERPOLATION, BIL_INTERPOLATION);
+			hintsMap.put(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		} else if ((wms != null)
+				&& WMSConfig.INT_BICUBIC.equals(wms.getAllowInterpolation())) {
+			hintsMap.put(JAI.KEY_INTERPOLATION, BIC_INTERPOLATION);
+			hintsMap.put(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		}
+		// line look better with this hint, they are less blurred
+		hintsMap.put(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
 
         // make sure the hints are set before we start rendering the map
         graphic.setRenderingHints(hintsMap);
