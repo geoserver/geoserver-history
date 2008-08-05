54349a1d8759a06cda1d4a3a552ededf99564732
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-05 16:50:47 +0000

    Styled ajax paging navigator, tenzochris will use the classes to style appropriately the links
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9845 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8bf78c1df221b6f3fb74773ba6731ba8e46a87df [formerly 6bd3eac3edd6721992f215309c4d390a167f2dc4]
    Former-commit-id: a307d64a305ec441779b5bfaee803ea957b6435e

---
+++ geoserver/community/web2/core/src/main/java/org/geoserver/web/wicket/GeoServerPagingNavigator.java
@@ -0,0 +1,39 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web.wicket;
+
+import org.apache.wicket.ajax.markup.html.navigation.paging.AjaxPagingNavigator;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.navigation.paging.IPageable;
+
+/**
+ * A custom navigator that sets classes for the elements
+ * @author Andrea Aime - TOPP
+ *
+ */
+class GeoServerPagingNavigator extends AjaxPagingNavigator {
+
+    public GeoServerPagingNavigator(String id, IPageable pageable) {
+        super(id, pageable);
+    }
+    
+    @Override
+    protected Link newPagingNavigationLink(String id, IPageable pageable, int pageNumber) {
+        Link link = super.newPagingNavigationLink(id, pageable, pageNumber);
+        // we turn the id into the css class
+        link.add(new SimpleAttributeModifier("class", id));
+        return link;
+    }
+    
+    @Override
+    protected Link newPagingNavigationIncrementLink(String id, IPageable pageable, int increment) {
+        Link link = super.newPagingNavigationIncrementLink(id, pageable, increment);
+        // we turn the id into the css class
+        link.add(new SimpleAttributeModifier("class", id));
+        return link;
+    }
+    
+}
