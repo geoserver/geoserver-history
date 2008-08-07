3c062da5a62d139eb271a35e4517b400c5b469b6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-07 00:35:40 +0000

    added a resolving proxy
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9888 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0db56dd29eb3d3ade973caafb8fdb15b5fa3739f [formerly 10310df9ba8a5c5648a52ab02a2a127bf81b1dab]
    Former-commit-id: 1d8a1fd41396a610c6ac84d5875ea73507d12b2e

---
+++ geoserver/main/src/main/java/org/geoserver/catalog/impl/ResolvingProxy.java
@@ -0,0 +1,87 @@
+package org.geoserver.catalog.impl;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.NamespaceInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+
+
+/**
+ * A proxy which holds onto an identifier which will later be 
+ * resolved into a real object.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project
+ *
+ */
+public class ResolvingProxy extends ProxyBase {
+
+    /**
+     * Wraps an object in the proxy.
+     * 
+     * @throws RuntimeException If creating the proxy fails.
+     */
+    public static <T> T create( String ref, Class<T> clazz ) {
+        InvocationHandler h = new ResolvingProxy( ref );
+        
+        Class proxyClass = 
+            Proxy.getProxyClass( clazz.getClassLoader(), clazz );
+        
+        T proxy;
+        try {
+            proxy = (T) proxyClass.getConstructor(
+                new Class[] { InvocationHandler.class }).newInstance(new Object[] { h } );
+        }
+        catch( Exception e ) {
+            throw new RuntimeException( e );
+        }
+        
+        return proxy;
+    }
+    
+    public static <T> T resolve( Catalog catalog, T object ) {
+        if ( object instanceof Proxy ) {
+            InvocationHandler h = Proxy.getInvocationHandler( object );
+            if ( h instanceof ResolvingProxy ) {
+                String ref = ((ResolvingProxy)h).getRef();
+                if ( object instanceof WorkspaceInfo ) {
+                    return (T) catalog.getWorkspace( ref );
+                }
+                if ( object instanceof NamespaceInfo ) {
+                    return (T) catalog.getNamespace( ref );
+                }
+            }
+        }
+        
+        return object;
+    }
+    
+    /**
+     * the reference
+     */
+    String ref;
+    
+    public ResolvingProxy(String ref) {
+        this.ref = ref;
+    }
+    
+    public String getRef() {
+        return ref;
+    }
+    
+    @Override
+    protected Object handleGetUnSet(Object proxy, Method method, String property) throws Throwable {
+        if ( "id".equalsIgnoreCase( property ) ) {
+            return ref;
+        }
+            
+        return null;
+    }
+    
+    @Override
+    protected Object handleOther(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
