3d4531fdfbfaad53c7b7d60009500b62692115a2
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-08 13:35:28 +0000

    GEOS-2110, adding keyword based filtering to srs list page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@9934 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ba238800e24f544650572ec08c10561741bed77d [formerly 7c81ae5045e5a71f2044bf867e0cc12294201c29]
    Former-commit-id: bcad70871946a9da32121a1cc4d7f18ebf8a996c

--- geoserver/community/web2/demo/src/main/java/org/geoserver/web/demo/SRSListPage.java
+++ geoserver/community/web2/demo/src/main/java/org/geoserver/web/demo/SRSListPage.java
@@ -4,6 +4,7 @@
  */
 package org.geoserver.web.demo;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -14,12 +15,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.model.Model;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.wicket.GeoServerPagingNavigator;
 import org.geotools.factory.Hints;
@@ -33,12 +40,12 @@ import org.vfny.geoserver.crs.GeoserverCustomWKTFactory;
 
 /**
  * Lists all the SRS available in GeoServer
+ * 
  * @author Andrea Aime - OpenGeo
- *
+ * 
  */
 public class SRSListPage extends GeoServerBasePage {
 
-    
     private static final int MAX_ROWS = 25;
 
     private static final Logger LOGGER = Logging.getLogger("org.geoserver.web.demo");
@@ -48,9 +55,9 @@ public class SRSListPage extends GeoServerBasePage {
                     GeoserverCustomWKTFactory.class));
 
     public SRSListPage() {
-        List<String> codeList = new ArrayList<String>(buildCodeList());
-        Collections.sort(codeList, new CodeComparator());
-        PageableListView srsList = new PageableListView("srslist", codeList, MAX_ROWS) {
+        // setup pageable list
+        List<String> codeList = new ArrayList<String>(buildCodeList(null));
+        final PageableListView srsList = new PageableListView("srslist", codeList, MAX_ROWS) {
 
             @Override
             protected void populateItem(ListItem item) {
@@ -58,33 +65,66 @@ public class SRSListPage extends GeoServerBasePage {
                 item.add(new SimpleAttributeModifier("class", item.getIndex() % 2 == 0 ? "even"
                         : "odd"));
 
-                // grab the code 
+                // grab the code
                 String code = (String) item.getModelObject();
-                final BookmarkablePageLink link = new BookmarkablePageLink("codeLink", SRSDescriptionPage.class, new PageParameters("code=EPSG:" + code) );
+                final BookmarkablePageLink link = new BookmarkablePageLink("codeLink",
+                        SRSDescriptionPage.class, new PageParameters("code=EPSG:" + code));
                 link.add(new Label("code", code));
                 item.add(link);
-                
+
                 // grab a description
                 String description = "-";
                 try {
-                    description = CRS.getAuthorityFactory(true).getDescriptionText("EPSG:" + code).toString(getLocale());
-                } catch(Exception e) {
-                    LOGGER.log(Level.FINE, "Could not get the description for srs code EPSG:{0}", code);
+                    description = CRS.getAuthorityFactory(true).getDescriptionText("EPSG:" + code)
+                            .toString(getLocale());
+                } catch (Exception e) {
+                    LOGGER.log(Level.FINE, "Could not get the description for srs code EPSG:{0}",
+                            code);
                 }
                 item.add(new Label("description", description));
             }
         };
         srsList.setOutputMarkupId(true);
-        WebMarkupContainer container  = new WebMarkupContainer("listContainer");
-        container.setOutputMarkupId(true);
-        container.add(srsList);
-        add(container);
+        final WebMarkupContainer listContainer = new WebMarkupContainer("listContainer");
+        listContainer.setOutputMarkupId(true);
+        listContainer.add(srsList);
+        add(listContainer);
         add(new GeoServerPagingNavigator("topNav", srsList));
         add(new GeoServerPagingNavigator("bottomNav", srsList));
+
+        // setup filter
+        Form filterForm = new Form("filterForm");
+        final TextField filter = new TextField("filter", new Model());
+        filter.setOutputMarkupId(true);
+        filterForm.add(filter);
+        AjaxButton filterSubmit = new AjaxButton("applyFilter") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                String[] filters = null;
+                String unparsed = filter.getValue();
+                if(unparsed != null)
+                    filters = unparsed.split("\\s+");
+                srsList.setModel(new Model((Serializable) buildCodeList(filters)));
+                target.addComponent(srsList.getParent());
+            }
+        };
+        filterForm.add(filterSubmit);
+        AjaxButton filterReset = new AjaxButton("resetFilter") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                filter.setModel(new Model());
+                srsList.setModel(new Model((Serializable) buildCodeList(null)));
+                target.addComponent(filter);
+                target.addComponent(srsList.getParent());
+            }
+        };
+        filterForm.add(filterReset);
+        add(filterForm);
         
+
     }
 
-    List<String> buildCodeList() {
+    List<String> buildCodeList(String[] filters) {
         Set<String> codes = CRS.getSupportedCodes("EPSG");
 
         try {
@@ -96,20 +136,42 @@ public class SRSListPage extends GeoServerBasePage {
         // make a set with each code
         Set<String> idSet = new HashSet<String>();
         for (String code : codes) {
-            String id = code.substring(code.indexOf(':') + 1); // just the non
-                                                               // prefix part
-            idSet.add(id);
+            // make sure we're using just the non prefix part
+            String id = code.substring(code.indexOf(':') + 1);
+
+            // if filtering is required, filter against the code and the description
+            if (filters != null) {
+                String description = null;
+                
+                try {
+                    description = CRS.getAuthorityFactory(true).getDescriptionText("EPSG:" + id)
+                            .toString(getLocale());
+                } catch (Exception e) {
+                }
+                for (String filter : filters) {
+                    if (id.contains(filter)
+                            || (description != null && description.contains(filter))) {
+                        idSet.add(id);
+                        break;
+                    }
+                }
+
+            } else {
+                idSet.add(id);
+            }
+
         }
         List<String> ids = new ArrayList<String>(idSet);
-        Collections.sort(ids); // sort to get them in order
+        Collections.sort(ids, new CodeComparator()); // sort to get them in order
         return ids;
     }
-    
+
     /**
-     * Compares the codes so that most of the codes ger compared as numbers,
-     * but unfortunately some non numeric ones can sneak in...
+     * Compares the codes so that most of the codes ger compared as numbers, but
+     * unfortunately some non numeric ones can sneak in...
+     * 
      * @author Andrea Aime - TOPP
-     *
+     * 
      */
     public class CodeComparator implements Comparator<String> {
 
@@ -117,21 +179,21 @@ public class SRSListPage extends GeoServerBasePage {
             Integer c1 = null, c2 = null;
             try {
                 c1 = Integer.parseInt(s1);
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 //
             }
             try {
                 c2 = Integer.parseInt(s2);
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 //
             }
-            if(c1 == null) {
-                if(c2 == null)
+            if (c1 == null) {
+                if (c2 == null)
                     return s1.compareTo(s2);
                 else
                     return -1;
             } else {
-                if(c2 == null)
+                if (c2 == null)
                     return 1;
                 else
                     return c1 - c2;
@@ -139,6 +201,5 @@ public class SRSListPage extends GeoServerBasePage {
         }
 
     }
-
 
 }
