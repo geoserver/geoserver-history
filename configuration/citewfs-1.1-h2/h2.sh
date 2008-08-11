71536da0f5ad47376d31e56b61d935960d525491
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-11 23:20:10 +0000

    moved xlink specific configuration to a different data directory
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@9954 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a29877224cced63dfff3d2d92d6b73816883d920 [formerly 6f315bd5a9ca685c558e323a9debaeef5e9ad2c4]
    Former-commit-id: 0d359cad2581b846da2e51abd095cc60d0f3bde1

--- configuration/citewfs-1.1-h2/h2.sh
+++ configuration/citewfs-1.1-h2/h2.sh
@@ -1,7 +1,7 @@
 H2="$HOME/.m2/repository/org/h2database/h2/1.0-SNAPSHOT/h2-1.0-SNAPSHOT.jar"
 M2_REPO="$HOME/.m2/repository"
 CP="$H2:$M2_REPO/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar:$M2_REPOy/org/geotools/gt2-main/2.4-SNAPSHOT/gt2-main-2.4-SNAPSHOT.jar:$M2_REPO/com/vividsolutions/jts/1.8/jts-1.8.jar"
-SF="2"
+SF="0"
 
 if [ "load" = "$1" ]; then
    java -cp $CP org.h2.tools.DeleteDbFiles -dir . -db cite
