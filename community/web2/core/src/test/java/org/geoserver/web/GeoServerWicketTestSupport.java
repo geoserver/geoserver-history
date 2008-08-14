ad04df9b2f9cb156c419b1ad6f37ded9fc3d01db
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-14 21:42:43 +0000

    Refactor and provide 'logout' and 'login' methods for use in tests.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10013 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 232ed30d5ed0309e3a699de5739c8f4b37dedead [formerly 3735298eb164c5d2efc80c9b051f5e76a4b96cba]
    Former-commit-id: 8794a37271e3da099aa031c112c3c0a1200e7979

--- community/web2/core/src/test/java/org/geoserver/web/GeoServerWicketTestSupport.java
+++ community/web2/core/src/test/java/org/geoserver/web/GeoServerWicketTestSupport.java
@@ -1,6 +1,12 @@
 package org.geoserver.web;
 
 import org.geoserver.test.GeoServerTestSupport;
+
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.context.SecurityContextImpl;
+import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.apache.wicket.util.tester.WicketTester;
 
 public abstract class GeoServerWicketTestSupport extends GeoServerTestSupport {
@@ -16,5 +22,31 @@ public abstract class GeoServerWicketTestSupport extends GeoServerTestSupport {
 
     public GeoServerApplication getGeoServerApplication(){
         return GeoServerApplication.get();
+    }
+
+    public void login(){
+        SecurityContextHolder.setContext(new SecurityContextImpl());
+        SecurityContextHolder.getContext().setAuthentication(
+            new UsernamePasswordAuthenticationToken(
+            "admin",
+            "geoserver",
+            new GrantedAuthority[]{
+                new GrantedAuthorityImpl("ROLE_ADMINISTRATOR")
+            }
+            )
+        );
+    }
+
+    public void logout(){
+        SecurityContextHolder.setContext(new SecurityContextImpl());
+        SecurityContextHolder.getContext().setAuthentication(
+            new UsernamePasswordAuthenticationToken(
+            "anonymousUser",
+            "",
+            new GrantedAuthority[]{
+                new GrantedAuthorityImpl("ROLE_ANONYMOUS")
+            }
+            )
+        );
     }
 }
