d27d3dcb610c8e04fcb7d5a84bfb46a135da296d
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-15 15:08:18 +0000

    Simple test for the Contact Info page.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10035 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 08195378ed9fd696987ac261fbddcb08ee0f17ce [formerly cba5b43d200c23a80b0c984a58d8cc86032d729b]
    Former-commit-id: acce8db25cad864233811db59b958639f1b9f803

--- community/web2/core/src/test/java/org/geoserver/web/GeoServerWicketTestSupport.java
+++ community/web2/core/src/test/java/org/geoserver/web/GeoServerWicketTestSupport.java
@@ -10,7 +10,7 @@ import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 import org.apache.wicket.util.tester.WicketTester;
 
 public abstract class GeoServerWicketTestSupport extends GeoServerTestSupport {
-    WicketTester tester;
+    public WicketTester tester;
 
     public void oneTimeSetUp() throws Exception {
         super.oneTimeSetUp();
