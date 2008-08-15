66d722ba135d580565830ceb9253bcbdb6b38196
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-15 14:24:16 +0000

    Removing more unused code.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10025 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 69f22f6095012a30259b118abad78b81716d6bfd [formerly f970e59398dfd3bb9c6253e1d7d5c9ce2c4a4bf6]
    Former-commit-id: 97238d4ac7750a694f828f2fe8ca24afe6fa282f

--- community/web2/core/src/main/java/org/geoserver/web/acegi/GeoServerSession.java
+++ community/web2/core/src/main/java/org/geoserver/web/acegi/GeoServerSession.java
@@ -25,37 +25,6 @@ public class GeoServerSession extends WebSession{
         return (GeoServerSession)Session.get();
     }
 
-    public boolean authenticate(String username, String password){
-        if (username == null) username = "";
-        if (password == null) password = "";
-
-        UsernamePasswordAuthenticationToken authToken =
-            new UsernamePasswordAuthenticationToken(username, password);
-
-        try{
-            Authentication authentication = 
-                findAuthenticationManager((GeoServerApplication)getApplication()).authenticate(authToken);
-            RememberMeServices rememberMeService = 
-                findRememberMeServices((GeoServerApplication)getApplication());
-            if (rememberMeService != null){
-                rememberMeService.loginSuccess(
-                        ((WebRequestCycle)WebRequestCycle.get()).getWebRequest().getHttpServletRequest(), 
-                        ((WebRequestCycle)WebRequestCycle.get()).getWebResponse().getHttpServletResponse(), 
-                        authentication
-                        );
-            }
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            return true;
-        } catch (BadCredentialsException e){
-            //TODO: Log
-        } catch (AuthenticationException e){
-            //TODO: Log
-        }
- 
-        return false;
-    }
-
     public Authentication getAuthentication(){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null &&
@@ -64,17 +33,5 @@ public class GeoServerSession extends WebSession{
            ) return null;
 
         return auth;
-    }
-
-    public void signout(){
-        SecurityContextHolder.getContext().setAuthentication(null);
-    }
-
-    private static RememberMeServices findRememberMeServices(GeoServerApplication application){
-        return (RememberMeServices)application.getApplicationContext().getBean("rememberMeServices");
-    }
-
-    private static AuthenticationManager findAuthenticationManager(GeoServerApplication application){
-        return (AuthenticationManager)application.getApplicationContext().getBean("authenticationManager");
     }
 }
