c687c734d36349c963361fc0adad94685aa37019
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-18 16:57:32 +0000

    GEOS-2129, implemented pending functionality for StatusPage
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10042 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 53241fc4d200b3b530148e48626ba0896c640567 [formerly bbe6e9c7771000be7efc66b5c40d2b68f56be3ab]
    Former-commit-id: d041cde8ab8bbdfe58763586419c2a1096bed5b0

--- community/web2/core/src/main/java/org/geoserver/web/admin/StatusPage.java
+++ community/web2/core/src/main/java/org/geoserver/web/admin/StatusPage.java
@@ -1,16 +1,20 @@
 package org.geoserver.web.admin;
 
 import org.geoserver.web.GeoServerBasePage;
+import org.geoserver.web.util.MapModel;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.jai.JAIInfo;
 import org.geotools.data.DataStore;
 import org.geotools.data.LockingManager;
+import org.vfny.geoserver.global.GeoServer;
 
 import javax.media.jai.JAI;
 
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
@@ -18,66 +22,107 @@ import org.apache.wicket.markup.html.link.Link;
 import com.sun.media.jai.util.SunTileCache;
 
 public class StatusPage extends ServerAdminPage {
-    public StatusPage(){
-        //TODO: if we just provide the values directly as the models they won't be refreshed on a page reload (ugh).
-        add(new Label("locks", Long.toString(getLockCount())));
-        add(new Label("connections", Long.toString(getConnectionCount())));
-        add(new Label("memory", formatUsedMemory()));
-        add(new Label("jvm.version", System.getProperty("java.vendor") + ": " + System.getProperty("java.version")));
-        add(new Label("jai.available", 
-                    Boolean.toString(ClassLoader.getSystemClassLoader().getResource("javax/media/jai/buildVersion") != null))
-           );
 
-        JAI jai = ((JAIInfo) 
-                getGeoServerApplication()
-                .getGeoServer()
-                .getGlobal()
-                .getMetadata()
-                .get(JAIInfo.KEY))
-            .getJAI();
+    /**
+     * The map used as the model source so the label contents are updated
+     */
+    private final Map<String, String> values;
 
-        SunTileCache jaiCache = ((JAIInfo) 
-                getGeoServerApplication()
-                .getGeoServer()
-                .getGlobal()
-                .getMetadata()
-                .get(JAIInfo.KEY))
-            .getTileCache();
+    private static final String KEY_LOCKS = "locks";
 
-        add(new Label("jai.memory.available",
-                    Long.toString(jaiCache.getMemoryCapacity()))
-           );
+    private static final String KEY_CONNECTIONS = "connections";
 
-        add(new Label("jai.memory.used", 
-                    Long.toString(jaiCache.getCacheMemoryUsed()))
-           );
-        add(new Label("jai.memory.threshold",
-                    Float.toString(100.0f * jaiCache.getMemoryThreshold()))
-           );
-        add(new Label("jai.tile.threads", 
-                    Integer.toString(jai.getTileScheduler().getParallelism()))
-           );
-        add(new Label("jai.tile.priority",
-                    Integer.toString(jai.getTileScheduler().getPriority()))
-           );
+    private static final String KEY_MEMORY = "memory";
 
-        add(new Link("free.locks"){
-                public void onClick(){
-                }
-            });
-        add(new Link("free.memory"){
-                public void onClick(){
-                    System.gc();
-                }
-            });
-        add(new Link("free.memory.jai"){
-                public void onClick(){
-                }
-            });
+    private static final String KEY_JVM_VERSION = "jvm_version";
+
+    private static final String KEY_JAI_AVAILABLE = "jai_available";
+
+    private static final String KEY_JAI_MAX_MEM = "jai_max_mem";
+
+    private static final String KEY_JAI_MEM_USAGE = "jai_mem_usage";
+
+    private static final String KEY_JAI_MEM_THRESHOLD = "jai_mem_threshold";
+
+    private static final String KEY_JAI_TILE_THREADS = "jai_tile_threads";
+
+    private static final String KEY_JAI_TILE_THREAD_PRIORITY = "jai_tile_thread_priority";
+
+    public StatusPage() {
+        values = new HashMap<String, String>();
+        updateModel();
+
+        // TODO: if we just provide the values directly as the models they won't
+        // be refreshed on a page reload (ugh).
+        add(new Label("locks", new MapModel(values, KEY_LOCKS)));
+        add(new Label("connections", new MapModel(values, KEY_CONNECTIONS)));
+        add(new Label("memory", new MapModel(values, KEY_MEMORY)));
+        add(new Label("jvm.version", new MapModel(values, KEY_JVM_VERSION)));
+        add(new Label("jai.available", new MapModel(values, KEY_JAI_AVAILABLE)));
+        add(new Label("jai.memory.available", new MapModel(values, KEY_JAI_MAX_MEM)));
+        add(new Label("jai.memory.used", new MapModel(values, KEY_JAI_MEM_USAGE)));
+        add(new Label("jai.memory.threshold", new MapModel(values, KEY_JAI_MEM_THRESHOLD)));
+        add(new Label("jai.tile.threads", new MapModel(values, KEY_JAI_TILE_THREADS)));
+        add(new Label("jai.tile.priority", new MapModel(values, KEY_JAI_TILE_THREAD_PRIORITY)));
+
+        add(new Link("free.locks") {
+            private static final long serialVersionUID = 1L;
+
+            public void onClick() {
+                // TODO: see GEOS-2130
+                updateModel();
+            }
+        });
+        add(new Link("free.memory") {
+            private static final long serialVersionUID = 1L;
+
+            public void onClick() {
+                System.gc();
+                System.runFinalization();
+                updateModel();
+            }
+        });
+        
+        add(new Link("free.memory.jai") {
+            private static final long serialVersionUID = 1L;
+
+            public void onClick() {
+                SunTileCache jaiCache = ((JAIInfo) getGeoServer().getGlobal().getMetadata().get(
+                        JAIInfo.KEY)).getTileCache();
+                final long capacityBefore = jaiCache.getMemoryCapacity();
+                jaiCache.flush();
+                jaiCache.setMemoryCapacity(0); // to be sure we realease all tiles
+                System.gc();
+                System.runFinalization();
+                jaiCache.setMemoryCapacity(capacityBefore);
+            }
+        });
 
         add(new Label("reload.date.geoserver", "Jul 14, 3:07 PM"));
         add(new Label("reload.date.configuration", "Jul 14, 3:07 PM"));
         add(new Label("reload.date.xml", "Mar 14, 2:15 PM"));
+    }
+
+    private void updateModel() {
+        values.put(KEY_LOCKS, Long.toString(getLockCount()));
+        values.put(KEY_CONNECTIONS, Long.toString(getConnectionCount()));
+        values.put(KEY_MEMORY, formatUsedMemory());
+        values.put(KEY_JVM_VERSION, System.getProperty("java.vendor") + ": "
+                + System.getProperty("java.version"));
+        values.put(KEY_JAI_AVAILABLE, Boolean.toString(ClassLoader.getSystemClassLoader()
+                .getResource("javax/media/jai/buildVersion") != null));
+
+        JAI jai = ((JAIInfo) getGeoServer().getGlobal().getMetadata().get(JAIInfo.KEY)).getJAI();
+
+        SunTileCache jaiCache = ((JAIInfo) getGeoServer().getGlobal().getMetadata()
+                .get(JAIInfo.KEY)).getTileCache();
+
+        values.put(KEY_JAI_MAX_MEM, Long.toString(jaiCache.getMemoryCapacity()));
+        values.put(KEY_JAI_MEM_USAGE, Long.toString(jaiCache.getCacheMemoryUsed()));
+        values.put(KEY_JAI_MEM_THRESHOLD, Float.toString(100.0f * jaiCache.getMemoryThreshold()));
+        values.put(KEY_JAI_TILE_THREADS, Integer.toString(jai.getTileScheduler().getParallelism()));
+        values.put(KEY_JAI_TILE_THREAD_PRIORITY, Integer.toString(jai.getTileScheduler()
+                .getPriority()));
     }
 
     /**
@@ -91,20 +136,20 @@ public class StatusPage extends ServerAdminPage {
         final long GB = KB * MB;
         final NumberFormat formatter = NumberFormat.getInstance();
         formatter.setMaximumFractionDigits(2);
-        
+
         String formattedUsedMemory;
-        if(usedBytes > GB){
+        if (usedBytes > GB) {
             formattedUsedMemory = formatter.format(usedBytes / GB) + " GB";
-        }else if(usedBytes > MB){
+        } else if (usedBytes > MB) {
             formattedUsedMemory = formatter.format(usedBytes / MB) + " MB";
-        }else{
+        } else {
             formattedUsedMemory = formatter.format(usedBytes / KB) + " KB";
         }
-        
+
         return formattedUsedMemory;
     }
 
-    private synchronized int getLockCount(){
+    private synchronized int getLockCount() {
         int count = 0;
 
         for (Iterator i = getDataStores().iterator(); i.hasNext();) {
@@ -118,12 +163,12 @@ public class StatusPage extends ServerAdminPage {
             try {
                 DataStore store = meta.getDataStore(null);
                 LockingManager lockingManager = store.getLockingManager();
-                if (lockingManager != null){
+                if (lockingManager != null) {
                     // we can't actually *count* locks right now?
                     // count += lockingManager.getLockSet().size();
                 }
             } catch (IllegalStateException notAvailable) {
-                continue; 
+                continue;
             } catch (Throwable huh) {
                 continue;
             }
@@ -140,14 +185,14 @@ public class StatusPage extends ServerAdminPage {
 
             if (!meta.isEnabled()) {
                 // Don't count connections from disabled datastores.
-                continue; 
+                continue;
             }
 
             try {
                 DataStore dataStore = meta.getDataStore(null);
             } catch (Throwable notAvailable) {
-                //TODO: Logging.
-                continue; 
+                // TODO: Logging.
+                continue;
             }
 
             count += 1;
@@ -156,8 +201,8 @@ public class StatusPage extends ServerAdminPage {
         return count;
     }
 
-    private List<DataStoreInfo> getDataStores(){
-        return getGeoServerApplication().getGeoServer().getCatalog().getDataStores();
+    private List<DataStoreInfo> getDataStores() {
+        return getGeoServer().getCatalog().getDataStores();
     }
 
 }
