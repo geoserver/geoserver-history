2edcf2cb1bc5a09adb303c8ba4fdb04bd27e92e1
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-19 20:06:08 +0000

    Add tests for WFS web module.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10052 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ee05bca7d5226a9d6e4f4d20ad60c76d442648e2 [formerly 7d925cf4999e17e5c12742e3ec7a3be0fd953eb1]
    Former-commit-id: 22fd8978010de43e879223a3297bb45e2349e134

---
+++ community/web2/wfs/src/test/java/org/geoserver/wfs/web/WFSAdminPageTest.java
@@ -0,0 +1,14 @@
+package org.geoserver.wfs.web;
+
+import org.geoserver.wfs.WFSInfo;
+import org.geoserver.web.GeoServerWicketTestSupport;
+
+public class WFSAdminPageTest extends GeoServerWicketTestSupport{
+    public void testValues() throws Exception {
+        WFSInfo wfs = getGeoServerApplication().getGeoServer().getService(WFSInfo.class);
+
+        login();
+        tester.startPage(WFSAdminPage.class);
+        tester.assertModelValue("form:maxFeatures", wfs.getMaxFeatures());
+    }
+}
