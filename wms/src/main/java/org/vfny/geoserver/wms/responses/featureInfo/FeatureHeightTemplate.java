8d867fdbe6e817c59bb713860355697d26363191
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-28 16:37:02 +0000

    Port KML Height features from 1.7.x
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10097 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7a99c54fe953c75f7b9cda3b4bd402bc1158d1cc [formerly 4590b634e2b9662e15a00d52129aef0d16244118]
    Former-commit-id: 83ad4781ff226be74e1573c34f2f434c3f89691e

---
+++ wms/src/main/java/org/vfny/geoserver/wms/responses/featureInfo/FeatureHeightTemplate.java
@@ -0,0 +1,42 @@
+package org.vfny.geoserver.wms.responses.featureInfo;
+
+import java.io.IOException;
+
+import org.opengis.feature.simple.SimpleFeature;
+
+/**
+ * Template supporting height values for features.
+ *
+ * @author David Winslow <dwinslow@opengeo.org>
+ */
+public class FeatureHeightTemplate {
+
+    FeatureTemplate delegate;
+    
+    public FeatureHeightTemplate() {
+        this(new FeatureTemplate());
+    }
+    
+    public FeatureHeightTemplate(FeatureTemplate delegate) {
+        this.delegate = delegate;
+    }
+    
+    /**
+     * Executes the template against the feature.
+     * <p>
+     * This method returns:
+     * <ul>
+     *  <li><code>{"01/01/07"}</code>: timestamp as 1 element array
+     *  <li><code>{"01/01/07","01/12/07"}</code>: timespan as 2 element array
+     *  <li><code>{null,"01/12/07"}</code>: open ended (start) timespan as 2 element array
+     *  <li><code>{"01/12/07",null}</code>: open ended (end) timespan as 2 element array 
+     *  <li><code>{}</code>: no timestamp information as empty array
+     * </ul>
+     * </p>
+     * @param feature The feature to execute against.
+     */
+    public double execute(SimpleFeature feature) throws IOException {
+        String output = delegate.template(feature, "height.ftl", getClass());
+        return Double.valueOf(output);
+    }
+}
