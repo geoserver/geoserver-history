5791568a2a75590d852c8271dce8d43b75db70a1
sbenthall <sbenthall@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-08-29 19:29:22 +0000

    CSV/Excel WFS output format
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10108 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4fdad54ca640ccee9df22b76058f6a2f6d68adef [formerly 93766f68776f6f439096857198df5b33ccd0b995]
    Former-commit-id: 2774ab4328ad61ff5e96cf84098107601fb32936

---
+++ geoserver/community/csvexcel/src/main/java/org/geoserver/CSVOutputFormat.java
@@ -0,0 +1,93 @@
+package org.geoserver;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import net.opengis.wfs.FeatureCollectionType;
+
+import org.geoserver.platform.Operation;
+import org.geoserver.platform.ServiceException;
+import org.geoserver.wfs.WFSGetFeatureOutputFormat;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+
+public class CSVOutputFormat extends WFSGetFeatureOutputFormat {
+
+    public CSVOutputFormat() {
+        //this is the name of your output format, it is the string
+        // that will be used when requesting the format in a 
+        // GEtFeature request: 
+        // ie ;.../geoserver/wfs?request=getfeature&outputFormat=myOutputFormat
+        super("csv");
+    }
+    
+    @Override
+    public String getMimeType(Object value, Operation operation)
+               throws ServiceException {
+         // return the mime type of the format here, the parent 
+         // class returns 'text/xml'
+         return "text/csv";
+    }
+    
+    @Override
+    protected boolean canHandleInternal(Operation operation) {
+        //any additional checks that need to be performed to 
+        // determine when the output format should be "engaged" 
+        // should go here
+        return super.canHandleInternal(operation);
+    }
+    
+    @Override
+    protected void write(FeatureCollectionType featureCollection,
+            OutputStream output, Operation getFeature) throws IOException,
+            ServiceException {
+    	   //write out content here
+        
+        //create a writer
+        BufferedWriter w = new BufferedWriter( new OutputStreamWriter( output ) );
+                   
+        //get the feature collection
+        FeatureCollection fc = (FeatureCollection) featureCollection.getFeature().get(0);
+           
+        //write out the header
+        SimpleFeatureType ft = (SimpleFeatureType) fc.getSchema();
+        for ( int i = 0; i < ft.getAttributeCount(); i++ ) {
+            AttributeDescriptor ad = ft.getDescriptor( i );
+            w.write( ad.getLocalName() );
+               
+            if ( i < ft.getAttributeCount()-1 ) {
+               w.write( "," );
+            }
+        }
+        w.write( "\n" );
+           
+        //write out the features
+        FeatureIterator i = fc.features();
+        try {
+            while( i.hasNext() ) {
+                SimpleFeature f = (SimpleFeature) i.next();
+                for ( int j = 0; j < f.getAttributeCount(); j++ ) {
+                    Object att = f.getAttribute( j );
+                    if ( att != null ) {
+                        w.write( att.toString() );
+                    }
+                    if ( j < f.getAttributeCount()-1 ) {
+                        w.write(",");    
+                    }
+                }    
+                w.write( "\n" );
+            }
+        }
+        finally {
+            fc.close( i );
+        }
+           
+        w.flush();
+    }
+
+}
