fa45aa50e221ef9bd742ae1c89ba0eeb9c30da70
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-09-05 21:53:12 +0000

    Fix tests since superoverlay structure changed.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10151 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5ef14174fdaa7af9944415976714d305fb93f15b [formerly 6ed8c20ab3196b2263c86443122ef2dd8427ce1e]
    Former-commit-id: e1d02ed582242c0f9c71efe06c97062111a9fb80

--- geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/kml/KMLTransformerTest.java
+++ geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/map/kml/KMLTransformerTest.java
@@ -313,8 +313,6 @@ public class KMLTransformerTest extends WMSTestSupport {
         assertEquals("kml", document.getDocumentElement().getNodeName());
         assertEquals( 3, document.getElementsByTagName("Region").getLength() );
         assertEquals( 2, document.getElementsByTagName("NetworkLink").getLength() );
-        assertEquals( 2, document.getElementsByTagName("GroundOverlay").getLength() );
-        
     }
 
     public void testTransformer() throws Exception {
