2c8669961cb6ab291cecb27d3ccdcdd74b4da75e
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-09-22 21:23:31 +0000

    added issued CoverageReadRequest structure tests
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x_WCS10EMF_ND@10307 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b7f06da7562cd2c131874bc2b2dbb19ece72d869

--- geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/GetMapResponseNDCoverageTest.java
+++ geoserver/wms/src/test/java/org/vfny/geoserver/wms/responses/GetMapResponseNDCoverageTest.java
@@ -1,16 +1,29 @@
 package org.vfny.geoserver.wms.responses;
 
-import static org.easymock.classextension.EasyMock.*;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.notNull;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
 
+import java.awt.Rectangle;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.EnumSet;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+
+import junit.framework.TestCase;
 
 import org.easymock.EasyMock;
-import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.impl.CatalogImpl;
 import org.geoserver.catalog.impl.CoverageInfoImpl;
@@ -21,8 +34,8 @@ import org.geoserver.catalog.impl.StyleInfoImpl;
 import org.geoserver.catalog.impl.WorkspaceInfoImpl;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.io.CoverageAccess;
+import org.geotools.coverage.io.CoverageCapabilities;
 import org.geotools.coverage.io.CoverageReadRequest;
-import org.geotools.coverage.io.CoverageRequest;
 import org.geotools.coverage.io.CoverageResponse;
 import org.geotools.coverage.io.CoverageSource;
 import org.geotools.coverage.io.CoverageAccess.AccessType;
@@ -31,6 +44,8 @@ import org.geotools.coverage.io.impl.range.DefaultFieldType;
 import org.geotools.coverage.io.impl.range.DefaultRangeType;
 import org.geotools.coverage.io.range.FieldType;
 import org.geotools.coverage.io.range.RangeType;
+import org.geotools.data.Parameter;
+import org.geotools.data.ResourceInfo;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.NameImpl;
 import org.geotools.geometry.GeneralEnvelope;
@@ -40,17 +55,20 @@ import org.geotools.styling.Style;
 import org.geotools.styling.StyleFactory;
 import org.opengis.coverage.Coverage;
 import org.opengis.coverage.DiscreteCoverage;
+import org.opengis.feature.type.Name;
+import org.opengis.geometry.BoundingBox;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform2D;
+import org.opengis.temporal.Instant;
+import org.opengis.temporal.TemporalGeometricPrimitive;
 import org.opengis.util.ProgressListener;
 import org.vfny.geoserver.global.CoverageInfo;
 import org.vfny.geoserver.global.MapLayerInfo;
 import org.vfny.geoserver.global.WMS;
 import org.vfny.geoserver.wms.GetMapProducer;
 import org.vfny.geoserver.wms.requests.GetMapRequest;
-import org.vfny.geoserver.wms.requests.WMSCapabilitiesRequest;
 
 import com.vividsolutions.jts.geom.Envelope;
-
-import junit.framework.TestCase;
 
 public class GetMapResponseNDCoverageTest extends TestCase {
 
@@ -309,9 +327,6 @@ public class GetMapResponseNDCoverageTest extends TestCase {
         RangeType fields = new DefaultRangeType("testRange", "range desc", fieldTypes);
         coverageInfo.setFields(fields);
 
-        CoverageAccess cvAccess = EasyMock.createMock(CoverageAccess.class);
-        CoverageSource coverageSource = EasyMock.createMock(CoverageSource.class);
-
         expect(
                 cvAccess.access(new NameImpl("testCoverageName"), null, AccessType.READ_ONLY, null,
                         null)).andReturn(coverageSource);
@@ -330,6 +345,110 @@ public class GetMapResponseNDCoverageTest extends TestCase {
         }
         verify(cvAccess);
         verify(coverageSource);
+    }
+
+    /**
+     * Check getCoverage builds up the correct {@link CoverageReadRequest} for
+     * {@link CoverageSource#read(CoverageReadRequest, ProgressListener)}
+     */
+    public void testGetCoverageIssuesTheCorrectRequest() throws IOException {
+        // now mock up the layer fieldId and coverage FieldTypes...
+        layer.setFieldId("testFieldName");
+
+        // set the request values for the temporal subset (note the dates are unsorted in the
+        // request)
+        Date time1 = new Date(1000L);
+        Date time2 = new Date(0L);
+        Date time3 = new Date(2000L);
+        request.setTime(Arrays.asList(new Date[] { time1, time2, time3 }));
+
+        Set<FieldType> fieldTypes = new HashSet<FieldType>();
+        // make sure there's a matching FieldType in the coverage for the layer field id
+        fieldTypes.add(new DefaultFieldType(new NameImpl("testFieldName"), null, null, null, null));
+        fieldTypes.add(new DefaultFieldType(new NameImpl("anotherFieldName"), null, null, null,
+                null));
+        coverageInfo.setFields(new DefaultRangeType("testRange", "range desc", fieldTypes));
+
+        //Set the request value for the elevation param
+        request.setElevation(new Integer(1500));
+        
+        MockCoverageSource source = new MockCoverageSource(coverageSource);
+
+        expect(
+                cvAccess.access(new NameImpl("testCoverageName"), null, AccessType.READ_ONLY, null,
+                        null)).andReturn(source);
+
+        CoverageResponse response = createMock(CoverageResponse.class);
+        expect(
+                coverageSource.read((CoverageReadRequest) anyObject(),
+                        (ProgressListener) anyObject())).andReturn(response);
+
+        // record the dispose() expectation, it should always be called
+        coverageSource.dispose();
+
+        // return status failure so it does not advances further than issuing the read request
+        expect(response.getStatus()).andReturn(Status.FAILURE);
+        expect(response.getExceptions()).andStubReturn(
+                Collections.singleton(new Exception("Fake exception")));
+        replay(response);
+        replay(cvAccess);
+        replay(coverageSource);
+
+        try {
+            GetMapResponse.getCoverage(request, layer, requestedEnvelope, cvAccess);
+            fail("Expected IOException if there are more than one range matching the layer field id");
+        } catch (IOException e) {
+            assertTrue(e.getMessage(), e.getMessage().contains("FAILURE"));
+        }
+        verify(cvAccess);
+        verify(coverageSource);
+
+        // no assert the correct request were sent
+        final CoverageReadRequest issuedRequest = source.issuedRequest;
+
+        // was the request envelope set?
+        BoundingBox geographicArea = issuedRequest.getGeographicArea();
+        assertNotNull(geographicArea);
+        assertSame(DefaultGeographicCRS.WGS84, geographicArea.getCoordinateReferenceSystem());
+        assertEquals(-180D, geographicArea.getMinX());
+        assertEquals(-90D, geographicArea.getMinY());
+        assertEquals(180D, geographicArea.getMaxX());
+        assertEquals(90D, geographicArea.getMaxY());
+
+        // was the range subset set based on the layer's field id?
+        RangeType rangeSubset = issuedRequest.getRangeSubset();
+        assertNotNull(rangeSubset);
+        assertEquals(1, rangeSubset.getFieldTypes().size());
+        assertNotNull(rangeSubset.getFieldType("testFieldName"));
+
+        // was the temporal subset set and in the correct order?
+        SortedSet<TemporalGeometricPrimitive> temporalSubset = issuedRequest.getTemporalSubset();
+        assertNotNull(temporalSubset);
+        assertEquals(3, temporalSubset.size());
+        Iterator<TemporalGeometricPrimitive> iterator = temporalSubset.iterator();
+        TemporalGeometricPrimitive t1 = iterator.next();
+        TemporalGeometricPrimitive t2 = iterator.next();
+        TemporalGeometricPrimitive t3 = iterator.next();
+        assertTrue(t1 instanceof Instant);
+        assertTrue(t2 instanceof Instant);
+        assertTrue(t3 instanceof Instant);
+
+        // TODO: this backward order is due to what I think is a bug in
+        // DefaultTemporalPrimitive.compareTo. If that's fixed, just change the expectation order
+        // bellow
+        assertEquals(2000L, ((Instant) t1).getPosition().getDate().getTime());
+        assertEquals(1000L, ((Instant) t2).getPosition().getDate().getTime());
+        assertEquals(0L, ((Instant) t3).getPosition().getDate().getTime());
+        
+        //assert the CoverageReadRequest was set with the elevation parameters
+        Set<org.opengis.geometry.Envelope> verticalSubset = issuedRequest.getVerticalSubset();
+        assertNotNull(verticalSubset);
+        assertEquals(1, verticalSubset.size());
+        org.opengis.geometry.Envelope verticalRange = verticalSubset.iterator().next();
+        assertTrue(verticalRange instanceof GeneralEnvelope);
+        assertEquals(1, ((GeneralEnvelope)verticalRange).getDimension());        
+        assertEquals(Double.valueOf(1500), ((GeneralEnvelope)verticalRange).getMinimum(0));
+        assertEquals(Double.valueOf(1500), ((GeneralEnvelope)verticalRange).getMaximum(0));
     }
 
     /**
@@ -385,5 +504,100 @@ public class GetMapResponseNDCoverageTest extends TestCase {
         catalog.add(coverageLayerInfo);
         catalog.add(coverageStoreInfo);
         catalog.add(coverageInfo);
+    }
+
+    /**
+     * An empty CoverageSource implementation for tests to use in asserting the operation arguments,
+     * etc
+     */
+    private static class MockCoverageSource implements CoverageSource {
+
+        private CoverageSource original;
+
+        boolean disposeCalled = false;
+
+        public CoverageReadRequest issuedRequest;
+
+        public MockCoverageSource() {
+            // will return null for all operations
+            original = EasyMock.createNiceMock(CoverageSource.class);
+        }
+
+        public MockCoverageSource(CoverageSource wrapped) {
+            this.original = wrapped;
+        }
+
+        public void dispose() {
+            original.dispose();
+            disposeCalled = true;
+        }
+
+        public EnumSet<CoverageCapabilities> getCapabilities() {
+            return original.getCapabilities();
+        }
+
+        public CoordinateReferenceSystem getCoordinateReferenceSystem(ProgressListener listener)
+                throws IOException {
+            return original.getCoordinateReferenceSystem(listener);
+        }
+
+        public MathTransform2D getGridToWorldTransform(boolean brief, ProgressListener listener)
+                throws IOException {
+            return original.getGridToWorldTransform(brief, listener);
+        }
+
+        public List<BoundingBox> getHorizontalDomain(boolean overall, ProgressListener listener)
+                throws IOException {
+            return original.getHorizontalDomain(overall, listener);
+        }
+
+        public ResourceInfo getInfo(ProgressListener listener) {
+            return original.getInfo(listener);
+        }
+
+        public String[] getMetadataNames(ProgressListener listener) throws IOException {
+            return original.getMetadataNames(listener);
+        }
+
+        public String getMetadataValue(String name, ProgressListener listener) throws IOException {
+            return original.getMetadataValue(name, listener);
+        }
+
+        public Name getName(ProgressListener listener) {
+            return original.getName(listener);
+        }
+
+        public int[] getOptimalDataBlockSizes() {
+            return original.getOptimalDataBlockSizes();
+        }
+
+        public RangeType getRangeType(ProgressListener listener) throws IOException {
+            return original.getRangeType(listener);
+        }
+
+        public List<Rectangle> getRasterDomain(boolean overall, ProgressListener listener)
+                throws IOException {
+            return original.getRasterDomain(overall, listener);
+        }
+
+        public Map<String, Parameter<?>> getReadParameterInfo() {
+            return original.getReadParameterInfo();
+        }
+
+        public Set<TemporalGeometricPrimitive> getTemporalDomain(ProgressListener listener)
+                throws IOException {
+            return original.getTemporalDomain(listener);
+        }
+
+        public Set<org.opengis.geometry.Envelope> getVerticalDomain(boolean overall,
+                ProgressListener listener) throws IOException {
+            return original.getVerticalDomain(overall, listener);
+        }
+
+        public CoverageResponse read(CoverageReadRequest request, ProgressListener listener)
+                throws IOException {
+            this.issuedRequest = request;
+            return original.read(request, listener);
+        }
     }
 }
