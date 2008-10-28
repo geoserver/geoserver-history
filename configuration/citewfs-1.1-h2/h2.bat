5c117ab55a48658f1e16fb28b25447db68cb85e1
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-10-28 18:48:47 +0000

    updated instructions to run cite tests with the embedded engine
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@10482 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 06a05c23523afc920dad454c8fa651114650c691 [formerly 5903b071a1f9f9ab923be81f12f757c1c32ad497]
    Former-commit-id: 0f58297b263844781aa7f520bde60b7b1a9359a7

--- configuration/citewfs-1.1-h2/h2.bat
+++ configuration/citewfs-1.1-h2/h2.bat
@@ -3,7 +3,8 @@
 set HOME=c:\Documents and Settings\YourUser
 set H2=%HOME%\.m2\repository\org\h2database\h2\1.0-SNAPSHOT\h2-1.0-SNAPSHOT.jar
 set M2_REPO=%HOME%\.m2\repository
-set CP="%H2%";"%M2_REPO%/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar";"%M2_REPO%/org/geotools/gt2-main/2.4-SNAPSHOT/gt2-main-2.4-SNAPSHOT.jar";"%M2_REPO%/com/vividsolutions/jts/1.8/jts-1.8.jar"
+set GT_VERSION=2.5-SNAPSHOT
+set CP="%H2%";"%M2_REPO%/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar";"%M2_REPO%/org/geotools/gt-main/%GT_VERSION%/gt-main-%GT_VERSION%.jar";"%M2_REPO%/com/vividsolutions/jts/1.8/jts-1.8.jar"
 echo Classpath: %CP%
 set SF="2"
 
