46f55e5116514b51ab6ebd27285442a3e7374c6c
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-04 18:45:22 +0000

    GEOS-2335 Per layer security subsystem initializes before the catalog, leading to spurious error messages
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10525 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 32858d053e4d68f464f394aeade2155ec2f4b77f [formerly 8dadfce26769c6e27b1e6ce3b38d65d98bf8c190]
    Former-commit-id: e0c7361c3f251b89cd0486dd4fb1255041f58ee3

---
+++ main/src/test/java/org/geoserver/security/SecureCatalogIntegrationTest.java
@@ -0,0 +1,59 @@
+package org.geoserver.security;
+
+import java.io.File;
+
+import org.acegisecurity.AcegiSecurityException;
+import org.geoserver.data.test.IOUtils;
+import org.geoserver.data.test.MockData;
+import org.geoserver.test.GeoServerTestSupport;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.FeatureStore;
+import org.geotools.feature.FeatureCollection;
+import org.opengis.filter.Filter;
+
+/**
+ * Tests that security rules are applied in a real simulation of a GeoServer
+ * startup (loading the Spring context, reading the catalog and whatnot)
+ * 
+ * @author Andrea Aime
+ * 
+ */
+public class SecureCatalogIntegrationTest extends GeoServerTestSupport {
+
+    @Override
+    protected void populateDataDirectory(MockData dataDirectory) throws Exception {
+        super.populateDataDirectory(dataDirectory);
+        File security = new File(dataDirectory.getDataDirectoryRoot(), "security");
+        security.mkdir();
+        File layers = new File(security, "layers.properties");
+        IOUtils.copy(SecureCatalogIntegrationTest.class
+                .getResourceAsStream("functional.properties"), layers);
+    }
+
+    public void testFullAccess() throws Exception {
+        FeatureSource source = getCatalog().getFeatureTypeInfo(MockData.LINES).getFeatureSource();
+        FeatureCollection fc = source.getFeatures();
+        FeatureStore store = (FeatureStore) source;
+        store.removeFeatures(Filter.INCLUDE);
+    }
+
+    public void testCannotRead() throws Exception {
+        try {
+            getCatalog().getFeatureTypeInfo(MockData.BUILDINGS).getFeatureSource();
+            fail("This should have failed with a security exception!");
+        } catch (AcegiSecurityException e) {
+            // fine, we should not be able to get to the feature source
+        }
+    }
+
+    public void testCannotWrite() throws Exception {
+        FeatureStore fs = (FeatureStore) getCatalog().getFeatureTypeInfo(MockData.DELETES)
+                .getFeatureSource();
+        try {
+            fs.removeFeatures(Filter.INCLUDE);
+            fail("This should have failed with a security exception!");
+        } catch (AcegiSecurityException e) {
+            // fine, we should not be able to get to the feature source
+        }
+    }
+}
