0a75826cbd4056cb3b7446ffdbe876ccd5c322d8
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-04 10:42:49 +0000

    Fixing the build for the case in which there is no online datastore to test with
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10513 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0fa00b19e07fbfdecc1a77fb4728f1d28786eef0 [formerly 785af73f7803cc116aef72bf0c3a5a80619ccdfc]
    Former-commit-id: d94f3935bedb4111efbf52bd5719cdaaed2c0d0c

--- wfsv/src/test/java/org/geoserver/wfsv/RollbackTest.java
+++ wfsv/src/test/java/org/geoserver/wfsv/RollbackTest.java
@@ -12,66 +12,68 @@ public class RollbackTest extends WFSVTestSupport {
     protected void setUpInternal() throws Exception {
         super.setUpInternal();
         
-        // perform a transaction
-        String transaction = //
-        "<wfs:Transaction service=\"WFSV\" version=\"1.1.0\"\r\n" + 
-        "  xmlns:topp=\"http://www.openplans.org/topp\"\r\n" + 
-        "  xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wfs=\"http://www.opengis.net/wfs\"\r\n" + 
-        "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n" + 
-        "  xmlns:gml=\"http://www.opengis.net/gml\"\r\n" + 
-        "  handle=\"Updating Signature rock label\">\r\n" + 
-        "  <wfs:Insert>\r\n" + 
-        "    <topp:archsites>\r\n" + 
-        "      <topp:cat>2</topp:cat>\r\n" + 
-        "      <topp:str1>Alien crash site</topp:str1>\r\n" + 
-        "      <topp:the_geom>\r\n" + 
-        "        <gml:Point srsName=\"http://www.opengis.net/gml/srs/epsg.xml#26713\">\r\n" + 
-        "          <gml:coordinates decimal=\".\" cs=\",\" ts=\" \">604000,4930000</gml:coordinates>\r\n" + 
-        "        </gml:Point>\r\n" + 
-        "      </topp:the_geom>\r\n" + 
-        "    </topp:archsites>\r\n" + 
-        "  </wfs:Insert>\r\n" + 
-        "  <wfs:Update typeName=\"topp:archsites\">\r\n" + 
-        "    <wfs:Property>\r\n" + 
-        "      <wfs:Name>str1</wfs:Name>\r\n" + 
-        "      <wfs:Value>Signature Rock, updated</wfs:Value>\r\n" + 
-        "    </wfs:Property>\r\n" + 
-        "    <ogc:Filter>\r\n" + 
-        "      <ogc:FeatureId fid=\"archsites.1\" />\r\n" + 
-        "    </ogc:Filter>\r\n" + 
-        "  </wfs:Update>\r\n" + 
-        "  <wfs:Delete typeName=\"topp:archsites\">\r\n" + 
-        "    <ogc:Filter>\r\n" + 
-        "      <ogc:FeatureId fid=\"archsites.2\" />\r\n" + 
-        "    </ogc:Filter>\r\n" + 
-        "  </wfs:Delete>\r\n" + 
-        "</wfs:Transaction>\r\n" + 
-        "";
-        Document doc = postAsDOM(root(), transaction);
-//        print(doc);
-
-        // let's just ensure the transaction was successful
-        assertXpathEvaluatesTo("1", "count(/wfs:TransactionResponse)", doc);
-        assertXpathEvaluatesTo("archsites.5",
-                "/wfs:TransactionResponse/wfs:InsertResults/wfs:Feature/ogc:FeatureId/@fid", doc);
-        assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalInserted", doc);
-        assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalUpdated", doc);
-        assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalDeleted", doc);
-        
-        // ask the current state, make sure the updates do show
-        String current = "<wfs:GetFeature service=\"WFSV\" version=\"1.1.0\"\r\n"
-                + "  outputFormat=\"GML2\"\r\n"
-                + "  xmlns:topp=\"http://www.openplans.org/topp\"\r\n"
-                + "  xmlns:wfs=\"http://www.opengis.net/wfs\"\r\n"
-                + "  xmlns:ogc=\"http://www.opengis.net/ogc\">\r\n"
-                + "  <wfs:Query typeName=\"topp:archsites\"/>\r\n" + "</wfs:GetFeature>\r\n";
-        
-        doc = postAsDOM(root(), current);
-        assertXpathEvaluatesTo("4", "count(/wfs:FeatureCollection/gml:featureMember)", doc);
-        assertXpathEvaluatesTo("Signature Rock, updated",
-                "//topp:archsites[@fid=\"archsites.1\"]/topp:str1", doc);
-        assertXpathEvaluatesTo("0", "count(//topp:archsites[@fid=\"archsites.2\"])", doc);
-        assertXpathEvaluatesTo("1", "count(//topp:archsites[@fid=\"archsites.5\"])", doc);
+        if (getTestData().isTestDataAvailable()) {
+            // perform a transaction
+            String transaction = //
+            "<wfs:Transaction service=\"WFSV\" version=\"1.1.0\"\r\n" + 
+            "  xmlns:topp=\"http://www.openplans.org/topp\"\r\n" + 
+            "  xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:wfs=\"http://www.opengis.net/wfs\"\r\n" + 
+            "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n" + 
+            "  xmlns:gml=\"http://www.opengis.net/gml\"\r\n" + 
+            "  handle=\"Updating Signature rock label\">\r\n" + 
+            "  <wfs:Insert>\r\n" + 
+            "    <topp:archsites>\r\n" + 
+            "      <topp:cat>2</topp:cat>\r\n" + 
+            "      <topp:str1>Alien crash site</topp:str1>\r\n" + 
+            "      <topp:the_geom>\r\n" + 
+            "        <gml:Point srsName=\"http://www.opengis.net/gml/srs/epsg.xml#26713\">\r\n" + 
+            "          <gml:coordinates decimal=\".\" cs=\",\" ts=\" \">604000,4930000</gml:coordinates>\r\n" + 
+            "        </gml:Point>\r\n" + 
+            "      </topp:the_geom>\r\n" + 
+            "    </topp:archsites>\r\n" + 
+            "  </wfs:Insert>\r\n" + 
+            "  <wfs:Update typeName=\"topp:archsites\">\r\n" + 
+            "    <wfs:Property>\r\n" + 
+            "      <wfs:Name>str1</wfs:Name>\r\n" + 
+            "      <wfs:Value>Signature Rock, updated</wfs:Value>\r\n" + 
+            "    </wfs:Property>\r\n" + 
+            "    <ogc:Filter>\r\n" + 
+            "      <ogc:FeatureId fid=\"archsites.1\" />\r\n" + 
+            "    </ogc:Filter>\r\n" + 
+            "  </wfs:Update>\r\n" + 
+            "  <wfs:Delete typeName=\"topp:archsites\">\r\n" + 
+            "    <ogc:Filter>\r\n" + 
+            "      <ogc:FeatureId fid=\"archsites.2\" />\r\n" + 
+            "    </ogc:Filter>\r\n" + 
+            "  </wfs:Delete>\r\n" + 
+            "</wfs:Transaction>\r\n" + 
+            "";
+            Document doc = postAsDOM(root(), transaction);
+    //        print(doc);
+    
+            // let's just ensure the transaction was successful
+            assertXpathEvaluatesTo("1", "count(/wfs:TransactionResponse)", doc);
+            assertXpathEvaluatesTo("archsites.5",
+                    "/wfs:TransactionResponse/wfs:InsertResults/wfs:Feature/ogc:FeatureId/@fid", doc);
+            assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalInserted", doc);
+            assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalUpdated", doc);
+            assertXpathEvaluatesTo("1", "/wfs:TransactionResponse/wfs:TransactionSummary/wfs:totalDeleted", doc);
+            
+            // ask the current state, make sure the updates do show
+            String current = "<wfs:GetFeature service=\"WFSV\" version=\"1.1.0\"\r\n"
+                    + "  outputFormat=\"GML2\"\r\n"
+                    + "  xmlns:topp=\"http://www.openplans.org/topp\"\r\n"
+                    + "  xmlns:wfs=\"http://www.opengis.net/wfs\"\r\n"
+                    + "  xmlns:ogc=\"http://www.opengis.net/ogc\">\r\n"
+                    + "  <wfs:Query typeName=\"topp:archsites\"/>\r\n" + "</wfs:GetFeature>\r\n";
+            
+            doc = postAsDOM(root(), current);
+            assertXpathEvaluatesTo("4", "count(/wfs:FeatureCollection/gml:featureMember)", doc);
+            assertXpathEvaluatesTo("Signature Rock, updated",
+                    "//topp:archsites[@fid=\"archsites.1\"]/topp:str1", doc);
+            assertXpathEvaluatesTo("0", "count(//topp:archsites[@fid=\"archsites.2\"])", doc);
+            assertXpathEvaluatesTo("1", "count(//topp:archsites[@fid=\"archsites.5\"])", doc);
+        }
     }
 
     public void testRollbackWfs11() throws Exception {
