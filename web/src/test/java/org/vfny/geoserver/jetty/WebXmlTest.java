293ff31d711b0f539209913885e15e3e26ee228a
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-06 18:29:40 +0000

    GEOS-2344 Fix web.xml duplication (and add web.xml validation)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10546 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 27d180ea4189349e4bcbd100837c0a60a385bdbd [formerly a0cade40539174fd2ac018fc5b0814034591958b]
    Former-commit-id: d33b8d29c6797989b213ab4280175ddb871d52c9

---
+++ web/src/test/java/org/vfny/geoserver/jetty/WebXmlTest.java
@@ -0,0 +1,19 @@
+package org.vfny.geoserver.jetty;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import junit.framework.TestCase;
+
+import org.custommonkey.xmlunit.Validator;
+import org.xml.sax.InputSource;
+
+public class WebXmlTest extends TestCase {
+
+    public void testWebXmlDTDCompliance() throws Exception {
+        // makes sure web.xml is DTD compliant (without requiring internet access in the process)
+        InputSource is = new InputSource(new FileInputStream("src/main/webapp/WEB-INF/web.xml"));
+        Validator v = new Validator(is, new File("src/test/java/org/vfny/geoserver/jetty/web-app_2_3.dtd").toURL().toString());
+        assertTrue(v.isValid());
+    }
+}
