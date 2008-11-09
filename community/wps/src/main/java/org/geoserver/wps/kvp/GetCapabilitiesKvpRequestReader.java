e2c4511f8ab16d7a6c5413e263e210b7f21a37fe
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-09 11:15:06 +0000

    Work around GEOS-1719
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10571 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: efa4c6fe86b6d0a289bdb8c4a3b83179f943c09a [formerly dafc7764d53c55ac46e54ee9c3c2ffa92e1782ea]
    Former-commit-id: e2b8e2de446862130da7d658a4efaacc4cc7d49c

--- community/wps/src/main/java/org/geoserver/wps/kvp/GetCapabilitiesKvpRequestReader.java
+++ community/wps/src/main/java/org/geoserver/wps/kvp/GetCapabilitiesKvpRequestReader.java
@@ -6,12 +6,17 @@
 package org.geoserver.wps.kvp;
 
 import java.util.Map;
+
+import net.opengis.ows11.AcceptVersionsType;
 import net.opengis.wps.GetCapabilitiesType;
+
+import org.geoserver.ows.kvp.OWS11AcceptVersionsKvpParser;
 
 /**
  * GetCapabilities KVP request reader
  *
  * @author Lucas Reed, Refractions Research Inc
+ * @author Andrea Aime, OpenGeo
  */
 public class GetCapabilitiesKvpRequestReader extends WPSKvpRequestReader {
     public GetCapabilitiesKvpRequestReader() {
@@ -21,9 +26,13 @@ public class GetCapabilitiesKvpRequestReader extends WPSKvpRequestReader {
     @Override
     @SuppressWarnings("unchecked")
     public Object read(Object request, Map kvp, Map rawKvp) throws Exception {
+        // make sure we get the right accepts versions param -> workaround for GEOS-1719
+        if(rawKvp.containsKey("acceptVersions")) {
+            OWS11AcceptVersionsKvpParser avp = new OWS11AcceptVersionsKvpParser();
+            AcceptVersionsType avt = (AcceptVersionsType) avp.parse((String) rawKvp.get("acceptVersions"));
+            kvp.put("acceptVersions", avt);
+        }
         request = super.read(request, kvp, rawKvp);
-
-        // Version arbitration could be done at this point
 
         return request;
     }
