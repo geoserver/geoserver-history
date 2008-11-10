61d22e472dd4abcf801348e6882617afe92c91aa
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-10 16:14:45 +0000

    GEOS-2367 A single error in catalog loading may prevent GeoServer from starting up
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10583 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a3d02c4503bada747f9ecdb44130ee3a00ee2943 [formerly d06da1706967a492666828d6f947bc322dd791b2]
    Former-commit-id: a0393d19458fcbcdd6facaebb722c514eb3c82fd

---
+++ geoserver/main/src/test/java/org/geoserver/vfny/global/TolerantStartupTest.java
@@ -0,0 +1,52 @@
+package org.geoserver.vfny.global;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.catalog.ProjectionPolicy;
+import org.geoserver.data.test.MockData;
+import org.geoserver.test.GeoServerTestSupport;
+import org.springframework.core.io.ResourceLoader;
+import org.vfny.geoserver.global.Config;
+import org.vfny.geoserver.global.Data;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+public class TolerantStartupTest extends GeoServerTestSupport {
+    
+    @Override
+    public MockData buildTestData() throws Exception {
+        MockData md = new MockData();
+        
+        QName name = MockData.BASIC_POLYGONS;
+        URL properties = MockData.class.getResource(name.getLocalPart() + ".properties");
+        String styleName = name.getLocalPart();
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put(MockData.KEY_STYLE, styleName);
+        props.put(MockData.KEY_SRS_HANDLINGS, ProjectionPolicy.REPROJECT_TO_DECLARED.getCode());
+        props.put(MockData.KEY_SRS_NUMBER, "900913");
+        Envelope env = new Envelope(-13000000, -7500000, 2800000, 6400000);
+        props.put(MockData.KEY_NATIVE_ENVELOPE, env);
+        md.addPropertiesType(name, properties, props);
+        
+        md.addWellKnownTypes(new QName[] {MockData.BUILDINGS});
+        
+        return md;
+    }
+    
+    @Override
+    protected String getLogConfiguration() {
+        return "/DEFAULT_LOGGING.properties";
+    }
+    
+    public void testContextStartup() {
+        Config config = (Config) applicationContext.getBean("config");
+        assertNotNull(config.getData().getFeaturesTypes().get(getLayerId(MockData.BUILDINGS)));        
+        assertNull(config.getData().getFeaturesTypes().get(getLayerId(MockData.BASIC_POLYGONS)));
+    }
+
+}
