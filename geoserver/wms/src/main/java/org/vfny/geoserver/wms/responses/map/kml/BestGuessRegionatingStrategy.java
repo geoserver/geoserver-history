6b18ebd35c57c512acd0e7df9b15e3c618840b8d
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-11 20:38:47 +0000

    Add a few files that I missed for the merge; oops.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10598 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 76fe500c284c73e7fc4b5e98a215987db3b1976a [formerly b6361bb5cbc135fbf0cb03a8cda543196fef8260]
    Former-commit-id: 176807166694422a92d798d7de06c00d9b2ffbec

---
+++ geoserver/wms/src/main/java/org/vfny/geoserver/wms/responses/map/kml/BestGuessRegionatingStrategy.java
@@ -0,0 +1,43 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.vfny.geoserver.wms.responses.map.kml;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.map.MapLayer;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.vfny.geoserver.wms.WMSMapContext;
+
+import com.vividsolutions.jts.geom.Point;
+
+/**
+ * Make a best guess as to the appropriate strategy to use for a featuretype
+ * and do it automatically.  The heuristic is pretty simple; it's based 
+ * entirely on the default geometry of the featuretype:
+ * <ol>
+ * <li> For polygons, use the area. </li>
+ * <li> For line data, use the length. </li>
+ * <li> For point data or mixed geometry types, don't sort at all. </li>
+ * </ol>
+ *
+ * This is applied ONLY when the regionating strategy is 'auto' and no strategy
+ * is set by the admin.
+ *
+ * @author David Winslow <dwinslow@opengeo.org>
+ */
+public class BestGuessRegionatingStrategy implements RegionatingStrategy {
+
+    public Filter getFilter(WMSMapContext context, MapLayer layer) {
+        SimpleFeatureType type = 
+            ((FeatureSource<SimpleFeatureType, SimpleFeature>)layer.getFeatureSource()).getSchema();
+        Class geomtype = type.getGeometryDescriptor().getType().getBinding();
+
+        if (Point.class.isAssignableFrom(geomtype))
+            return new RandomRegionatingStrategy().getFilter(context, layer);
+
+        return new GeometryRegionatingStrategy().getFilter(context, layer);
+    }
+}
