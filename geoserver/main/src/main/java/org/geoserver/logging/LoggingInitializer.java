4d51dd76df776225141fad42003eb060110da9cd
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-14 03:00:03 +0000

    GEOS-2284, saving non file and console appenders when reloading log configuration
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10614 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0ec894f85c559fd3288c43e80e4f91254718d715 [formerly 95b5e3ff70cc49d8b499c5e0fc2e683972900408]
    Former-commit-id: 48ce584316fe351e60c6274d6c992f8abbf37ea7

--- geoserver/main/src/main/java/org/geoserver/logging/LoggingInitializer.java
+++ geoserver/main/src/main/java/org/geoserver/logging/LoggingInitializer.java
@@ -9,12 +9,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
 import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.PropertyConfigurator;
 import org.geoserver.config.ConfigurationListener;
@@ -182,6 +185,20 @@ public class LoggingInitializer {
 
     public static void configureGeoServerLogging(InputStream loggingConfigStream, boolean suppressStdOutLogging, boolean suppressFileLogging, String logFileName) throws FileNotFoundException, IOException,
                         ConfigurationException {
+        //JD: before we wipe out the logging configuration, save any appenders that are not 
+        // console or file based. This allows for other types of appenders to remain in tact
+        // when geoserver is reloaded.
+        List<Appender> appenders = new ArrayList();
+        Enumeration a = LogManager.getRootLogger().getAllAppenders();
+        while( a.hasMoreElements() ) {
+            Appender appender = (Appender) a.nextElement();
+            if ( !( appender instanceof ConsoleAppender || appender instanceof FileAppender )  ){ 
+                //save it 
+                appenders.add( appender );
+            }
+        }
+        
+        
         Properties lprops = new Properties();
         lprops.load(loggingConfigStream);
         LogManager.resetConfiguration();
@@ -223,6 +240,12 @@ public class LoggingInitializer {
                 }
             }
         } 
+        
+        //add the appenders we saved above
+        for ( Appender appender : appenders ) {
+            LogManager.getRootLogger().addAppender( appender );
+        }
+
         LOGGER.fine("FINISHED CONFIGURING GEOSERVER LOGGING -------------------------");
     }
 }
