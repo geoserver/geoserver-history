6d2976572cabb470410169dc1feed9fc18dfd951
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-17 17:09:22 +0000

    some minor fixes for validation purposes, wps model name changes
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10632 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: eec837e2f9357c1fc660e06b0ba3ed6b4d2d827d [formerly 57437c2f07dbee431d43c6b66d7228af0adf09ba]
    Former-commit-id: a3e871d6fa8cb18ddb845899760036abf65efa76

--- community/wps/src/main/java/org/geoserver/wps/GetCapabilities.java
+++ community/wps/src/main/java/org/geoserver/wps/GetCapabilities.java
@@ -9,23 +9,25 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
+import net.opengis.ows11.KeywordsType;
 import net.opengis.ows11.OperationType;
 import net.opengis.ows11.OperationsMetadataType;
 import net.opengis.ows11.Ows11Factory;
 import net.opengis.ows11.ResponsiblePartySubsetType;
 import net.opengis.ows11.ServiceIdentificationType;
 import net.opengis.ows11.ServiceProviderType;
-import net.opengis.wps.DefaultType2;
-import net.opengis.wps.GetCapabilitiesType;
-import net.opengis.wps.LanguagesType;
-import net.opengis.wps.LanguagesType1;
-import net.opengis.wps.ProcessBriefType;
-import net.opengis.wps.ProcessOfferingsType;
-import net.opengis.wps.WPSCapabilitiesType;
-import net.opengis.wps.WpsFactory;
+import net.opengis.wps10.DefaultType2;
+import net.opengis.wps10.GetCapabilitiesType;
+import net.opengis.wps10.LanguagesType;
+import net.opengis.wps10.LanguagesType1;
+import net.opengis.wps10.ProcessBriefType;
+import net.opengis.wps10.ProcessOfferingsType;
+import net.opengis.wps10.WPSCapabilitiesType;
+import net.opengis.wps10.Wps10Factory;
 
 import org.geoserver.config.GeoServerInfo;
 import org.geoserver.ows.Ows11Util;
+import org.geoserver.ows.util.OwsUtils;
 import org.geoserver.ows.util.RequestUtils;
 import org.geotools.process.ProcessFactory;
 import org.geotools.process.Processors;
@@ -55,11 +57,14 @@ public class GetCapabilities {
         // TODO: add update sequence negotiation
         
         // encode the response
-        WpsFactory wpsf = WpsFactory.eINSTANCE;
+        Wps10Factory wpsf = Wps10Factory.eINSTANCE;
         Ows11Factory owsf = Ows11Factory.eINSTANCE;
         
         WPSCapabilitiesType caps = wpsf.createWPSCapabilitiesType();
         caps.setVersion("1.0.0");
+        
+        //TODO: make configurable
+        caps.setLang( "en" );
         
         //ServiceIdentification
         ServiceIdentificationType si = owsf.createServiceIdentificationType();
@@ -67,7 +72,12 @@ public class GetCapabilities {
         
         si.getTitle().add( Ows11Util.languageString( wps.getTitle() ) );
         si.getAbstract().add( Ows11Util.languageString( wps.getAbstract() ) );
-        si.getKeywords().add( Ows11Util.keywords( wps.getKeywords() ) );
+        
+        KeywordsType kw = Ows11Util.keywords( wps.getKeywords( ) ); ;
+        if ( kw != null ) {
+            si.getKeywords().add( kw );
+        }
+        
         si.setServiceType( Ows11Util.code( "WPS" ) );
         si.getServiceTypeVersion().add( "1.0.0" );
         si.setFees( wps.getFees() );
@@ -80,6 +90,15 @@ public class GetCapabilities {
         ServiceProviderType sp = owsf.createServiceProviderType();
         caps.setServiceProvider( sp );
         
+        //TODO: set provder name from context
+        if ( wps.getGeoServer().getContact().getContactOrganization() != null ) {
+            sp.setProviderName(  wps.getGeoServer().getContact().getContactOrganization()  );    
+        }
+        else {
+            sp.setProviderName( "GeoServer" );
+        }
+        
+        
         sp.setProviderSite(owsf.createOnlineResourceType());
         sp.getProviderSite().setHref( wps.getGeoServer().getOnlineResource() );
         sp.setServiceContact( responsibleParty( wps.getGeoServer(), owsf ) );
@@ -90,14 +109,17 @@ public class GetCapabilities {
         
         OperationType gco = owsf.createOperationType();
         gco.setName("GetCapabilities");
+        gco.getDCP().add( Ows11Util.dcp( "wps", gco.getName(), request ) );
         om.getOperation().add( gco );
         
         OperationType dpo = owsf.createOperationType();
         dpo.setName( "DescribeProcess");
+        dpo.getDCP().add( Ows11Util.dcp( "wps", dpo.getName(), request ) );
         om.getOperation().add( dpo );
         
         OperationType eo = owsf.createOperationType();
         eo.setName( "Execute" );
+        eo.getDCP().add( Ows11Util.dcp( "wps", eo.getName(), request ) );
         om.getOperation().add( eo );
         
         ProcessOfferingsType po = wpsf.createProcessOfferingsType();
@@ -113,17 +135,16 @@ public class GetCapabilities {
             p.setAbstract( Ows11Util.languageString( pf.getDescription().toString() ) );
         }
 
-        LanguagesType1 l = wpsf.createLanguagesType1();
-        final DefaultType2 defaultLang = wpsf.createDefaultType2();
-        defaultLang.setLanguage("en");
-        l.setDefault(defaultLang);
-        LanguagesType language = wpsf.createLanguagesType();
-        language.setLanguage("en");
-        l.setSupported(language);
-        caps.setLanguages( l );
+        LanguagesType1 languages = wpsf.createLanguagesType1();
+        caps.setLanguages( languages );
         
-        l.setDefault( defaultLang );
-        l.getDefault().setLanguage( Locale.getDefault().getLanguage() );
+        DefaultType2 defaultLanguage = wpsf.createDefaultType2();
+        languages.setDefault(defaultLanguage);
+        defaultLanguage.setLanguage("en-US");
+        
+        LanguagesType supportedLanguages = wpsf.createLanguagesType();
+        languages.setSupported( supportedLanguages );
+        supportedLanguages.getLanguage().add( "en-US");
         
         return caps;
         // Version detection and alternative invocation if being implemented.
