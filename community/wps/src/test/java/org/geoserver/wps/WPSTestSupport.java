0b3c3fce019b8e548f60d5d9276ac47d45f08835
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-17 17:09:11 +0000

    updated validation code
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10627 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1b75bb516c3fc5c873e8e49721b736420b66a903 [formerly a83f78d9790426bed84fe0d9e3c4c5a44091b9e8]
    Former-commit-id: 662e98a2eaa7cd52cb7553dbc5b9399f54a6875d

--- community/wps/src/test/java/org/geoserver/wps/WPSTestSupport.java
+++ community/wps/src/test/java/org/geoserver/wps/WPSTestSupport.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -17,6 +20,9 @@ import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.custommonkey.xmlunit.XpathEngine;
 import org.geoserver.test.GeoServerTestSupport;
+import org.geoserver.wps.xml.WPSConfiguration;
+import org.geotools.xml.DOMParser;
+import org.geotools.xml.Parser;
 import org.w3c.dom.Document;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
@@ -24,26 +30,27 @@ import org.xml.sax.SAXParseException;
 
 public class WPSTestSupport extends GeoServerTestSupport {
 
-    protected static final XpathEngine xpath;
-    protected static final Schema WPS_SCHEMA;
-    
+//    protected static final XpathEngine xpath;
+//    protected static final Schema WPS_SCHEMA;
+//    
     static {
-        try {
-            final SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            WPS_SCHEMA = factory.newSchema(new File("./schemas/wps/1.0.0/wpsAll.xsd"));
-        } catch(Exception e) {
-            throw new RuntimeException("Could not parse the WCS 1.1.1 schemas", e);
-        }
-
+//        try {
+//            final SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+//            WPS_SCHEMA = factory.newSchema(new File("./schemas/wps/1.0.0/wpsAll.xsd"));
+//        } catch(Exception e) {
+//            throw new RuntimeException("Could not parse the WCS 1.1.1 schemas", e);
+//        }
+//
         // init xmlunit
         Map<String, String> namespaces = new HashMap<String, String>();
         namespaces.put("wps", "http://www.opengis.net/wps/1.0.0");
         namespaces.put("ows", "http://www.opengis.net/ows/1.1");
         namespaces.put("gml", "http://www.opengis.net/gml");
+        namespaces.put("wfs", "http://www.opengis.net/wfs");
         namespaces.put("xlink", "http://www.w3.org/1999/xlink");
         
         XMLUnit.setXpathNamespaceContext(new SimpleNamespaceContext(namespaces));
-        xpath = XMLUnit.newXpathEngine();
+        //xpath = XMLUnit.newXpathEngine();
     }
     
     protected String root() {
@@ -52,42 +59,20 @@ public class WPSTestSupport extends GeoServerTestSupport {
     
     /**
      * Workaround for XSD bindings to encode the xml:lang attribute 
+     * @throws TransformerException 
+     * @throws ParserConfigurationException 
      */
-    protected void checkValidationErrors(Document dom, Schema schema, String... skips) throws SAXException, IOException {
-        final Validator validator = schema.newValidator();
-        final List<Exception> validationErrors = new ArrayList<Exception>();
-        validator.setErrorHandler(new ErrorHandler() {
-            
-            public void warning(SAXParseException exception) throws SAXException {
-                System.out.println(exception.getMessage());
+    protected void checkValidationErrors(Document dom) throws Exception {
+        
+        Parser p = new Parser( new WPSConfiguration() );
+        p.setValidating( true );
+        p.parse( new DOMSource( dom ) );
+    
+        if ( !p.getValidationErrors().isEmpty() ) {
+            for ( Iterator e = p.getValidationErrors().iterator(); e.hasNext(); ) {
+                System.out.println( e.next().toString() );
             }
-
-            public void fatalError(SAXParseException exception) throws SAXException {
-                validationErrors.add(exception);
-            }
-
-            public void error(SAXParseException exception) throws SAXException {
-                validationErrors.add(exception);
-            }
-
-          });
-        validator.validate(new DOMSource(dom));
-        int errorCount = 0;
-        StringBuilder sb = new StringBuilder();
-        if (validationErrors != null && validationErrors.size() > 0) {
-            for (Exception ve : validationErrors) {
-                boolean skipError = false;
-                for (String skip : skips) {
-                    if(ve.getMessage().contains(skip))
-                        skipError = true;
-                }
-                if(!skipError) {
-                    sb.append(ve.getMessage()).append("\n");
-                    errorCount++;
-                }
-            }
+            fail( "Document did not validate.");
         }
-        if(errorCount > 0)
-            fail(sb.toString());
     }
 }
