917c6e25ee9bf736f7f7f68efb2d02d568721fa7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-17 17:09:22 +0000

    some minor fixes for validation purposes, wps model name changes
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@10632 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: aad38efe5efe8d820a5705fadf824b5b9f3a0f87 [formerly 65d3c9709336e91d000b68c870e8f514539ce0b9]
    Former-commit-id: 0ed2ca7709e141c389609250e27355a1b5cfb358

--- geoserver/community/wps/src/main/java/org/geoserver/wps/DescribeProcess.java
+++ geoserver/community/wps/src/main/java/org/geoserver/wps/DescribeProcess.java
@@ -6,11 +6,27 @@
 package org.geoserver.wps;
 
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import net.opengis.ows11.AnyValueType;
+import net.opengis.ows11.CodeType;
+import net.opengis.ows11.Ows11Factory;
+import net.opengis.wps10.ComplexDataDescriptionType;
+import net.opengis.wps10.DataInputsType;
+import net.opengis.wps10.DescribeProcessType;
+import net.opengis.wps10.InputDescriptionType;
+import net.opengis.wps10.LiteralInputType;
+import net.opengis.wps10.LiteralOutputType;
+import net.opengis.wps10.OutputDescriptionType;
+import net.opengis.wps10.ProcessDescriptionType;
+import net.opengis.wps10.ProcessDescriptionsType;
+import net.opengis.wps10.ProcessOutputsType;
+import net.opengis.wps10.SupportedComplexDataInputType;
+import net.opengis.wps10.SupportedComplexDataType;
+import net.opengis.wps10.Wps10Factory;
 
 import org.geoserver.ows.Ows11Util;
 import org.geoserver.wfs.xml.XSProfile;
@@ -19,30 +35,8 @@ import org.geoserver.wps.ppio.LiteralPPIO;
 import org.geoserver.wps.ppio.ProcessParameterIO;
 import org.geotools.data.Parameter;
 import org.geotools.process.ProcessFactory;
-import org.geotools.process.Processors;
 import org.opengis.feature.type.Name;
 import org.springframework.context.ApplicationContext;
-
-import net.opengis.ows11.CodeType;
-import net.opengis.ows11.DomainMetadataType;
-import net.opengis.wps.ComplexDataCombinationType;
-import net.opengis.wps.ComplexDataCombinationsType;
-import net.opengis.wps.ComplexDataDescriptionType;
-import net.opengis.wps.DataInputsType;
-import net.opengis.wps.DataInputsType1;
-import net.opengis.wps.DescribeProcessType;
-import net.opengis.wps.InputDescriptionType;
-import net.opengis.wps.InputType;
-import net.opengis.wps.LiteralDataType;
-import net.opengis.wps.LiteralInputType;
-import net.opengis.wps.LiteralOutputType;
-import net.opengis.wps.OutputDescriptionType;
-import net.opengis.wps.ProcessDescriptionType;
-import net.opengis.wps.ProcessDescriptionsType;
-import net.opengis.wps.ProcessOutputsType;
-import net.opengis.wps.SupportedComplexDataInputType;
-import net.opengis.wps.SupportedComplexDataType;
-import net.opengis.wps.WpsFactory;
 
 /**
  * First-call DescribeProcess class
@@ -56,6 +50,9 @@ public class DescribeProcess {
     Locale locale;
     XSProfile xsp;
     
+    Wps10Factory wpsf = Wps10Factory.eINSTANCE;
+    Ows11Factory owsf = Ows11Factory.eINSTANCE; 
+    
     public DescribeProcess(WPSInfo wps, ApplicationContext context) {
         this.wps = wps;
         this.context = context;
@@ -68,44 +65,44 @@ public class DescribeProcess {
 
     public ProcessDescriptionsType run(DescribeProcessType request) {
         
-        WpsFactory f = WpsFactory.eINSTANCE;
-        ProcessDescriptionsType pds = f.createProcessDescriptionsType();
+        ProcessDescriptionsType pds = wpsf.createProcessDescriptionsType();
         
         for ( Iterator i = request.getIdentifier().iterator(); i.hasNext(); ) {
             CodeType id = (CodeType) i.next();
-            processDescription( id, pds, f );
+            processDescription( id, pds );
         }
         
         return pds;
     }
     
-    void processDescription( CodeType id, ProcessDescriptionsType pds, WpsFactory f ) {
+    void processDescription( CodeType id, ProcessDescriptionsType pds ) {
         ProcessFactory pf = WPSUtils.findProcessFactory( id );
         if ( pf == null ) {
             throw new WPSException( "No such process: " + id.getValue() );
         }
         
-        ProcessDescriptionType pd = f.createProcessDescriptionType();
+        ProcessDescriptionType pd = wpsf.createProcessDescriptionType();
         pds.getProcessDescription().add( pd );
         
+        pd.setProcessVersion( "1.0.0" );
         pd.setIdentifier( Ows11Util.code( id.getValue() ) );
         pd.setTitle( Ows11Util.languageString(pf.getTitle()) );
         pd.setAbstract( Ows11Util.languageString(pf.getDescription()) );
         
         //data inputs
-        DataInputsType inputs = f.createDataInputsType();
+        DataInputsType inputs = wpsf.createDataInputsType();
         pd.setDataInputs(inputs);
-        dataInputs( inputs, pf, f );
+        dataInputs( inputs, pf );
         
         //process outputs
-        ProcessOutputsType outputs = f.createProcessOutputsType();
+        ProcessOutputsType outputs = wpsf.createProcessOutputsType();
         pd.setProcessOutputs( outputs );
-        processOutputs( outputs, pf, f );
+        processOutputs( outputs, pf );
      }
     
-    void dataInputs( DataInputsType inputs, ProcessFactory pf, WpsFactory f ) {
+    void dataInputs( DataInputsType inputs, ProcessFactory pf ) {
         for(Parameter<?> p : pf.getParameterInfo().values()) {
-            InputDescriptionType input = f.createInputDescriptionType();
+            InputDescriptionType input = wpsf.createInputDescriptionType();
             inputs.getInput().add( input );
             
             input.setIdentifier( Ows11Util.code( p.key ) );
@@ -127,7 +124,7 @@ public class DescribeProcess {
             if ( ppios.size() == 1 && ppios.get( 0 ) instanceof LiteralPPIO ) {
                 LiteralPPIO lppio = (LiteralPPIO) ppios.get( 0 );
                 
-                LiteralInputType literal = f.createLiteralInputType();
+                LiteralInputType literal = wpsf.createLiteralInputType();
                 input.setLiteralData( literal );
                 
                 //map the java class to an xml type name
@@ -137,19 +134,20 @@ public class DescribeProcess {
                         literal.setDataType( Ows11Util.type( typeName.getLocalPart() ) );        
                     }    
                 }
+                literal.setAnyValue( owsf.createAnyValueType() );
 
                 //TODO: output the default value
             }
             else {
                 //handle the complex data case
-                SupportedComplexDataInputType complex = f.createSupportedComplexDataInputType();
+                SupportedComplexDataInputType complex = wpsf.createSupportedComplexDataInputType();
                 input.setComplexData( complex );
                 
-                complex.setSupported( f.createComplexDataCombinationsType() );
+                complex.setSupported( wpsf.createComplexDataCombinationsType() );
                 for ( ProcessParameterIO ppio : ppios ) {
                     ComplexPPIO cppio = (ComplexPPIO) ppio;
 
-                    ComplexDataDescriptionType format = f.createComplexDataDescriptionType();
+                    ComplexDataDescriptionType format = wpsf.createComplexDataDescriptionType();
                     format.setMimeType( cppio.getMimeType() );
                     
                     //add to supported
@@ -157,10 +155,10 @@ public class DescribeProcess {
                     
                     //handle the default    
                     if ( complex.getDefault() == null ) {
-                        ComplexDataDescriptionType def = f.createComplexDataDescriptionType();
+                        ComplexDataDescriptionType def = wpsf.createComplexDataDescriptionType();
                         def.setMimeType( format.getMimeType() );
                         
-                        complex.setDefault( f.createComplexDataCombinationType() );
+                        complex.setDefault( wpsf.createComplexDataCombinationType() );
                         complex.getDefault().setFormat( def );
                     }
                 }
@@ -168,10 +166,10 @@ public class DescribeProcess {
         }
     }
     
-    void processOutputs( ProcessOutputsType outputs, ProcessFactory pf, WpsFactory f ) {
+    void processOutputs( ProcessOutputsType outputs, ProcessFactory pf ) {
         Map<String,Parameter<?>> outs = pf.getResultInfo(null);
         for ( Parameter p : outs.values() ) {
-            OutputDescriptionType output = f.createOutputDescriptionType();
+            OutputDescriptionType output = wpsf.createOutputDescriptionType();
             outputs.getOutput().add( output );
             
             output.setIdentifier( Ows11Util.code( p.key ) );
@@ -186,7 +184,7 @@ public class DescribeProcess {
             if ( ppios.size() == 1 && ppios.get( 0 ) instanceof LiteralPPIO ) {
                 LiteralPPIO lppio = (LiteralPPIO) ppios.get( 0 );
                 
-                LiteralOutputType literal = f.createLiteralOutputType();
+                LiteralOutputType literal = wpsf.createLiteralOutputType();
                 output.setLiteralOutput(literal);
                 
                 //map the java class to an xml type name
@@ -199,14 +197,14 @@ public class DescribeProcess {
             }
             else {
                 //handle the complex data case
-                SupportedComplexDataType complex = f.createSupportedComplexDataType();
+                SupportedComplexDataType complex = wpsf.createSupportedComplexDataType();
                 output.setComplexOutput( complex );
                 
-                complex.setSupported( f.createComplexDataCombinationsType() );
+                complex.setSupported( wpsf.createComplexDataCombinationsType() );
                 for ( ProcessParameterIO ppio : ppios ) {
                     ComplexPPIO cppio = (ComplexPPIO) ppio;
 
-                    ComplexDataDescriptionType format = f.createComplexDataDescriptionType();
+                    ComplexDataDescriptionType format = wpsf.createComplexDataDescriptionType();
                     format.setMimeType( cppio.getMimeType() );
                     
                     //add to supported
@@ -214,10 +212,10 @@ public class DescribeProcess {
                     
                     //handle the default    
                     if ( complex.getDefault() == null ) {
-                        ComplexDataDescriptionType def = f.createComplexDataDescriptionType();
+                        ComplexDataDescriptionType def = wpsf.createComplexDataDescriptionType();
                         def.setMimeType( format.getMimeType() );
                         
-                        complex.setDefault( f.createComplexDataCombinationType() );
+                        complex.setDefault( wpsf.createComplexDataCombinationType() );
                         complex.getDefault().setFormat( def );
                     }
                 }
