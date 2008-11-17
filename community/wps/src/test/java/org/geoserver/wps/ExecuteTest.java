b76d2dbb0cb97289d62a4745dab3c0dbab96612d
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-17 17:09:10 +0000

    added test for inline feature collection
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10626 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1611cd2421832d6ed1567116559a2463396080bd [formerly d33320f37610533ada7459b6523dbc45a28f0afd]
    Former-commit-id: 6f4148ab8bc99b15467b837b461cc977b5c1bf17

--- community/wps/src/test/java/org/geoserver/wps/ExecuteTest.java
+++ community/wps/src/test/java/org/geoserver/wps/ExecuteTest.java
@@ -1,5 +1,10 @@
 package org.geoserver.wps;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.w3c.dom.Document;
 
 import static org.custommonkey.xmlunit.XMLAssert.*;
@@ -17,11 +22,11 @@ public class ExecuteTest extends WPSTestSupport {
                     "<wps:Data>" +
                       "<wps:ComplexData>" + 
                         "<gml:Polygon xmlns:gml='http://www.opengis.net/gml'>" +
-                          "<gml:outerBoundaryIs>" + 
+                          "<gml:exterior>" + 
                             "<gml:LinearRing>" + 
-                              "<gml:coordinates>1,1 2,1 2,2 1,2 1,1</gml:coordinates>" + 
+                              "<gml:coordinates>1 1 2 1 2 2 1 2 1 1</gml:coordinates>" + 
                             "</gml:LinearRing>" + 
-                          "</gml:outerBoundaryIs>" + 
+                          "</gml:exterior>" + 
                         "</gml:Polygon>" +
                       "</wps:ComplexData>" + 
                     "</wps:Data>" +     
@@ -43,10 +48,9 @@ public class ExecuteTest extends WPSTestSupport {
          "</wps:Execute>";
         
         Document d = postAsDOM( "wps", xml );
-        print(d);
         
         // remove these hacks once the XSD encoder issues have been fixed
-        checkValidationErrors(d, WPS_SCHEMA, "'lang'", "'creationTime'");
+        checkValidationErrors(d);
         
         assertEquals( "wps:ExecuteResponse", d.getDocumentElement().getNodeName() );
         
@@ -55,5 +59,56 @@ public class ExecuteTest extends WPSTestSupport {
             "/wps:ExecuteResponse/wps:ProcessOutputs/wps:Output/wps:Data/wps:ComplexData/gml:Polygon", d);
     }
 
-
+    public void testFeatureCollectionInline() throws Exception {
+        String xml = "<wps:Execute service='WPS' version='1.0.0' xmlns:wps='http://www.opengis.net/wps/1.0.0' " + 
+        "xmlns:ows='http://www.opengis.net/ows/1.1'>" + 
+      "<ows:Identifier>BufferFeatureCollection</ows:Identifier>" + 
+       "<wps:DataInputs>" + 
+          "<wps:Input>" + 
+              "<ows:Identifier>features</ows:Identifier>" + 
+              "<wps:Data>" +
+                "<wps:ComplexData>" + 
+                     readFileIntoString("states-FeatureCollection.xml") + 
+                "</wps:ComplexData>" + 
+              "</wps:Data>" +     
+          "</wps:Input>" + 
+          "<wps:Input>" + 
+             "<ows:Identifier>buffer</ows:Identifier>" + 
+             "<wps:Data>" + 
+               "<wps:LiteralData>10</wps:LiteralData>" + 
+             "</wps:Data>" + 
+          "</wps:Input>" + 
+     "</wps:DataInputs>" +
+     "<wps:ResponseForm>" +  
+       "<wps:ResponseDocument storeExecuteResponse='false'>" + 
+         "<wps:Output>" +
+           "<ows:Identifier>geom-buffered</ows:Identifier>" +
+         "</wps:Output>" + 
+       "</wps:ResponseDocument>" +
+     "</wps:ResponseForm>" + 
+   "</wps:Execute>";
+        
+        Document d = postAsDOM( "wps", xml );
+        print( d );
+        // remove these hacks once the XSD encoder issues have been fixed
+        checkValidationErrors(d);
+        
+        assertEquals( "wps:ExecuteResponse", d.getDocumentElement().getNodeName() );
+        
+        assertXpathExists( "/wps:ExecuteResponse/wps:Status/wps:ProcessSucceeded", d);
+        assertXpathExists( 
+            "/wps:ExecuteResponse/wps:ProcessOutputs/wps:Output/wps:Data/wps:ComplexData/wfs:FeatureCollection", d);
+    }
+    
+    String readFileIntoString( String filename ) throws IOException {
+        BufferedReader in = 
+            new BufferedReader( new InputStreamReader(getClass().getResourceAsStream( filename ) ) );
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        while( (line = in.readLine() ) != null ) {
+            sb.append( line );
+        }
+        in.close();
+        return sb.toString();
+    }
 }
