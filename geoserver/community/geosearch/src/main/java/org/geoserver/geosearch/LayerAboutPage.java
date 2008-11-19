a757500c3c875543014ded154835d2d9c6a4b6df
sbenthall <sbenthall@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-19 23:52:25 +0000

    more layer about page improvements
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10663 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e7f3d4e3f5a05bd4ac19c5afd4db26484b55eca4 [formerly 31e5d1de42e4eb44433a68533489c93860943128]
    Former-commit-id: fa64027499ffb498430eeb5325f03a55b4a07bad

--- geoserver/community/geosearch/src/main/java/org/geoserver/geosearch/LayerAboutPage.java
+++ geoserver/community/geosearch/src/main/java/org/geoserver/geosearch/LayerAboutPage.java
@@ -1,5 +1,8 @@
 package org.geoserver.geosearch;
 
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.restlet.Restlet;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
@@ -15,11 +18,16 @@ import org.geoserver.geosearch.GeoServerProxyAwareRestlet;
 import org.geoserver.ows.util.RequestUtils;
 import org.geoserver.rest.DataFormat;
 import org.geoserver.rest.FreemarkerFormat;
+import org.geoserver.rest.RESTUtils;
 import org.geoserver.rest.RestletException;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
 
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.Namespace;
+
+import com.vividsolutions.jts.geom.Envelope;
 
 import java.io.IOException;
 import java.util.Map;
@@ -27,10 +35,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.logging.Logger;
 
 import freemarker.template.SimpleHash;
 
 public class LayerAboutPage extends GeoServerProxyAwareRestlet {
+	private static final Logger LOGGER = org.geotools.util.logging.Logging.getLogger("TODO-- get this right");	
+	
     private final DataFormat format =
         new FreemarkerFormat("layerpage.ftl", getClass(), MediaType.TEXT_HTML);
 
@@ -53,27 +64,53 @@ public class LayerAboutPage extends GeoServerProxyAwareRestlet {
         String namespace = (String)request.getAttributes().get("namespace");
         String layer = (String)request.getAttributes().get("layer");
 
-        response.setEntity(format.makeRepresentation(getContext(namespace, layer)));
+        response.setEntity(format.makeRepresentation(getContext(namespace, layer, request)));
     }
     
-    private SimpleHash getContext(String namespace, String layer){
+    private SimpleHash getContext(String namespace, String layer, Request request){
     	FeatureTypeInfo info = lookupType(namespace, layer);
     	
     	SimpleHash map = new SimpleHash();
     	
-    	map.put("name", info.getName());
-    	map.put("srs", info.getSRS());
+    	//basic
+    	map.put("title", info.getTitle());
+    	map.put("abstract", info.getAbstract());
+    	
+    	//metadata
+    	map.put("keywords", info.getKeywords());
 		map.put("declaredCRS", info.getDeclaredCRS());	    	
-    	try{
-        	map.put("nativeCRS", info.getNativeCRS());
+		try{
+			map.put("nativeCRS", info.getNativeCRS());
+		}catch(Exception e){
+			//need to find a logger
+		}
+    	
+		//general parameters for data requests
+		map.put("base", RESTUtils.getBaseURL(request));
+    	map.put("name", info.getName());
+    	map.put("srs", "EPSG:" + info.getSRS());
+    	
+    	ReferencedEnvelope bbox = getBBOX(info);
+    	
+    	String bboxString = bbox.getMinX() + "," + bbox.getMinY() + "," + bbox.getMaxX() + ","
+        + bbox.getMaxY();    	
+    	map.put("bbox", bboxString);
+    	
+    	map.put("tilesOrigin", bbox.getMinX()+","+bbox.getMinY());    	
+    	
+    	int[] imageBox = getMapWidthHeight(bbox);
+        map.put("width", String.valueOf(imageBox[0]));
+        map.put("height", String.valueOf(imageBox[1]));
+    	
+        map.put("maxResolution", getMaxResolution(bbox));
+        
+    	try{        	
         	map.put("boundingBox", info.getBoundingBox());
         	map.put("lonLatBoundingBox", info.getLatLongBoundingBox());
     	}catch(IOException e){
     		//well shucks.
     	}
-    	map.put("keywords", info.getKeywords());
-    	map.put("title", info.getTitle());
-    	map.put("abstract", info.getAbstract());
+    	
     	
     	return map;
     }
@@ -98,4 +135,106 @@ public class LayerAboutPage extends GeoServerProxyAwareRestlet {
         }
         return featureType;
     }
+    
+    
+    private ReferencedEnvelope getBBOX(FeatureTypeInfo layer){
+    	
+        String bboxList;
+    	
+        // We need to create a 4326 CRS for comparison to layer's crs
+        CoordinateReferenceSystem latLonCrs = null;
+
+        try { // get the CRS object for lat/lon 4326
+            latLonCrs = CRS.decode("EPSG:" + 4326);
+        } catch (NoSuchAuthorityCodeException e) {
+            String msg = "Error looking up SRS for EPSG: " + 4326 + ":" + e.getLocalizedMessage();
+            //currently does nothing with this string
+        } catch (FactoryException e) {
+            String msg = "Error looking up SRS for EPSG: " + 4326 + ":" + e.getLocalizedMessage();
+            //currently does nothing with this string                        
+        }
+    	
+    	//yoinked from MapPreviewAction
+        try {
+
+            CoordinateReferenceSystem layerCrs = layer.getDeclaredCRS();
+
+            /* A quick and efficient way to grab the bounding box is to get it
+             * from the featuretype info where the lat/lon bbox is loaded
+             * from the DTO. We do this with layer.getLatLongBoundingBox().
+             * We need to reproject the bounding box from lat/lon to the layer crs
+             * for display
+             */
+            Envelope orig_bbox = layer.getLatLongBoundingBox();
+
+            if ((orig_bbox.getWidth() == 0) || (orig_bbox.getHeight() == 0)) {
+                orig_bbox.expandBy(0.1);
+            }
+
+            ReferencedEnvelope bbox = new ReferencedEnvelope(orig_bbox, latLonCrs);
+
+            if (!CRS.equalsIgnoreMetadata(layerCrs, latLonCrs)) {
+                // first check if we have a native bbox
+                bbox = layer.getBoundingBox();
+            }
+
+            // we now have a bounding box in the same CRS as the layer
+            if ((bbox.getWidth() == 0) || (bbox.getHeight() == 0)) {
+                bbox.expandBy(0.1);
+            }
+            
+            if (layer.isEnabled()) {
+                // expand bbox by 5% to allow large symbolizers to fit the map
+                bbox.expandBy(bbox.getWidth() / 20, bbox.getHeight() / 20);
+                return bbox;
+            }
+        } catch(Exception e) {
+
+        }
+        
+        return null;
+    }
+    
+    //yoinked from MapPreviewAction
+    private int[] getMapWidthHeight(Envelope bbox) {
+        int width;
+        int height;
+        double ratio = bbox.getHeight() / bbox.getWidth();
+
+        if (ratio < 1) {
+            width = 750;
+            height = (int) Math.round(750 * ratio);
+        } else {
+            width = (int) Math.round(550 / ratio);
+            height = 550;
+        }
+
+        // make sure we reach some minimal dimensions (300 pixels is more or less 
+        // the height of the zoom bar)
+        if (width < 300) {
+            width = 300;
+        }
+
+        if (height < 300) {
+            height = 300;
+        }
+
+        // add 50 pixels horizontally to account for the zoom bar
+        return new int[] { width + 50, height };
+    }
+    
+
+	private double getMaxResolution(ReferencedEnvelope areaOfInterest) {
+		double w = areaOfInterest.getWidth();
+		double h = areaOfInterest.getHeight();
+
+		return ((w > h) ? w : h) / 256;
+	}
+	
+    /*
+    //returns true if this GeoServer instance uses GWC, false otherwise.
+    private boolean hasGWC()){
+    	String url = 
+    }
+    */
 }
--- geoserver/community/geosearch/src/main/java/org/geoserver/geosearch/layerpage.ftl
+++ geoserver/community/geosearch/src/main/java/org/geoserver/geosearch/layerpage.ftl
@@ -4,22 +4,103 @@
          layersParam = "&layers="+name
          nameParam = "&typeName="+name
          kmlUrl="../../../wms/kml?mode=flat&superoverlay=true"
-         shapefileUrl=wmsUrl+"/wfs?Format=SHAPE-ZIP&request=GetFeature&version=1.1.1"+nameParam
          gml2="GML2"
+         gml2gzip="GML2-GZIP"
          gml3="gml3"
          json="json"
          PDFParam="&Format=application/pdf"
-         coords = boundingBox?matches("-?\\d+(\\.\\d*)?")
-         bboxParam="&bbox="+coords[0]+","+coords[2]+","+coords[1]+","+coords[3]
+         bboxParam="&bbox="+bbox
+         dimParams="&width="+width+"&height="+height
+         srsParam="&srs="+srs
 >
   <head>
     <title>${title} - Powered by GeoServer </title>
+    <link rel="stylesheet" type="text/css" href="http://localhost:8080/geoserver/openlayers/theme/default/style.css"/>
+    <!-- Basic CSS definitions -->
+    <style type="text/css">
+        /* General settings */
+        body {
+            font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
+            font-size: small;
+        }
+        
+        /* The map and the location bar */
+        #map {
+            clear: both;
+            position: relative;
+            width: ${width}px;
+            height: ${height}px;
+            border: 1px solid black;
+        }            
+    </style>
+    <script src="../../../openlayers/OpenLayers.js" type="text/javascript">
+    </script>
+    <script defer="defer" type="text/javascript">
 
+    var format = 'image/png';
+    var untiled;
+    var map;
+
+    function init(){
+        var bounds = new OpenLayers.Bounds(${bbox});
+
+        var options = {
+            controls: [],
+            maxExtent: bounds,
+            maxResolution: ${maxResolution},
+            projection: '${srs}',
+            units: 'degrees'
+        };
+
+        map = new OpenLayers.Map('map', options);
+        
+        // setup tiled layer
+        tiled = new OpenLayers.Layer.WMS(
+            "${name} - Tiled", "../../../wms",
+            {
+                height: '${height}',
+                width: '${width}',
+                layers: '${name}',
+                styles: '',
+                srs: '${srs}',
+                format: format,
+                tiled: 'true',
+                tilesOrigin : "${tilesOrigin}"
+            },
+            {buffer: 0} 
+        );
+
+        // setup single tiled layer
+        untiled = new OpenLayers.Layer.WMS(
+            "${name} - Untiled", "../../../wms",
+            {
+                height: '${height}',
+                width: '${width}',
+                layers: '${name}',
+                styles: '',
+                srs: '${srs}',
+                format: format
+            },
+            {singleTile: true, ratio: 1} 
+        );
+
+        map.addLayers([untiled, tiled]);
+
+        // build up all controls            
+        map.addControl(new OpenLayers.Control.PanZoom());
+        map.addControl(new OpenLayers.Control.Navigation());
+        map.addControl(new OpenLayers.Control.Scale($('scale')));
+        map.addControl(new OpenLayers.Control.MousePosition({element: $('location')}));
+        map.zoomToExtent(bounds);
+
+    }
+    </script>
   </head>
-  <body>
+  <body onload="init()">
     <h1>${title}</h1>
     <p>${abstract}</p>
-    <p></p>
+    
+    <div id="map"></div>
 
     <h2>Metadata</h2>
     <table border="1">
@@ -31,12 +112,26 @@
       <tr><td>Declared CRS</td><td>${declaredCRS}</td><td>
     </table>
 
+    <h2>View Data</h2>
+    View the data as
+    <ul>
+      <li>
+        <a href="${wmsUrl + PDFParam + layersParam + bboxParam + '&styles=' + srsParam + dimParams }">PDF</a>
+      </li>
+    </ul>
+
     <h2>Download Data</h2>
     Download the data as:
     <ul>
       <li><a href="${kmlUrl + layersParam}">KML</a></li>
       <li><a href="${wfsUrl + nameParam + '&outputFormat=' + 'SHAPE-ZIP'}">Shapefile</a></li>   
-      <li><a href="${wfsUrl + nameParam + '&outputFormat=' + json}">JSON</a></li> 
+      <li><a href="${wfsUrl + nameParam + '&outputFormat=' + json}">JSON</a></li>
+      <li><a href="${wfsUrl + nameParam + '&outputFormat=' + gml2}">GML2</a></li>
+      <li><a href="${wfsUrl + nameParam + '&outputFormat=' + gml3}">GML3</a></li>
+      <!-- 
+         Problem: browser tries to up zip file as XML.
+         <li><a href="${wfsUrl + nameParam + '&outputFormat=' + gml2gzip}">GML2</a> gZipped</li>
+      -->
     </ul>
   </body>
 </html>
