588aecac05d567a0a7068e7229d5b7d193348cc8
mbarto <mbarto@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-20 10:02:43 +0000

    imagemap community module: added special handling for multi<geo> with 1 geometry to single<geo> and related testcases
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10668 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c12c912a20f98d3804931ead55203b22341d1b6a [formerly b65e9bfc5a9d0d2a00c3090ee0696b855788d69e]
    Former-commit-id: 804de4af7eaa623d69cb836e5fcaba8c557e597c

--- geoserver/community/imagemap/src/main/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/utils/GeometryTypeConverterFactory.java
+++ geoserver/community/imagemap/src/main/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/utils/GeometryTypeConverterFactory.java
@@ -74,67 +74,81 @@ public class GeometryTypeConverterFactory implements ConverterFactory {
 					// hierarchy compatible geometries -> nothing to do
 					if(target.isAssignableFrom(source.getClass()))
 						return source;
-					
-					// multi<geometry> types: for each one we
-					// try the followings:
-					//  - if source is <geometry> we create a multi<geometry> with just 1 element
-					//  - if source is a GeometryCollection, we try to convert each element to <geometry>
-					//  - else we firtsly convert the geometry to a <geometry> and then we create a multi<geometry> with the obtained element
-					if(MultiPoint.class.isAssignableFrom(target)) {
-						Point[] points=new Point[] {};
-						if(source instanceof Point)
-							points=new Point[] {(Point)source};
-						else if(source instanceof GeometryCollection)
-							points=this.convertAll((GeometryCollection)source,Point.class).toArray(points);												
-						else
-							points=new Point[] {(Point)this.convert(source,Point.class)};
-						return gFac.createMultiPoint(points);
-					}
-					if(MultiLineString.class.isAssignableFrom(target)) {
-						LineString[] lineStrings=new LineString[] {};
-						if(source instanceof LineString)
-							lineStrings=new LineString[] {(LineString)source};
-						else if(source instanceof GeometryCollection)
-							lineStrings=this.convertAll((GeometryCollection)source,LineString.class).toArray(lineStrings);							
-						else
-							lineStrings=new LineString[] {(LineString)this.convert(source,LineString.class)};
-						return gFac.createMultiLineString(lineStrings);
-					}
-					if(MultiPolygon.class.isAssignableFrom(target)) {
-						Polygon[] polygons=new Polygon[] {};
-						if(source instanceof Polygon)
-							polygons=new Polygon[] {(Polygon)source};
-						else if(source instanceof GeometryCollection)
-							polygons=this.convertAll((GeometryCollection)source,Polygon.class).toArray(polygons);
-						else
-							polygons=new Polygon[] {(Polygon)this.convert(source,Polygon.class)};
-						return gFac.createMultiPolygon(polygons);
-					}
-					
-					// target is a geometrycollection: we add the source to
-					// a new geometrycollection
-					if(GeometryCollection.class.isAssignableFrom(target))
-						return gFac.createGeometryCollection(new Geometry[] {(Geometry)source});						
-					
-					// target is a point: we return the centroid of any complex geometry
-					if(Point.class.isAssignableFrom(target)) {
-						if(LOGGER.isLoggable(Level.FINE))
-							LOGGER.fine("Converting Geometry "+source.toString()+" to Point. This could be unsafe");
-						return ((Geometry)source).getCentroid();
-					}
-					
-					// target is a linestring: we return the linestring connecting all the geometry coordinates
-					if(LineString.class.isAssignableFrom(target)) {
-						if(LOGGER.isLoggable(Level.FINE))
-							LOGGER.fine("Converting Geometry "+source.toString()+" to LineString. This could be unsafe");
-						return gFac.createLineString(getLineStringCoordinates(((Geometry)source).getCoordinates()));
-					}
-					// target is a polygon: we return a polygon connecting all the coordinates of the given geometry
-					if(Polygon.class.isAssignableFrom(target)) {
-						if(LOGGER.isLoggable(Level.FINE))
-							LOGGER.fine("Converting Geometry "+source.toString()+" to Polygon. This could be unsafe");
-						Coordinate[] coords=getPolygonCoordinates(((Geometry)source).getCoordinates());
-						return gFac.createPolygon(gFac.createLinearRing(coords), new LinearRing[] {});
+					if(source instanceof Geometry) { 
+						Geometry sourceGeometry=(Geometry)source;
+						// multi<geometry> types: for each one we
+						// try the followings:
+						//  - if source is <geometry> we create a multi<geometry> with just 1 element
+						//  - if source is a GeometryCollection, we try to convert each element to <geometry>
+						//  - else we firtsly convert the geometry to a <geometry> and then we create a multi<geometry> with the obtained element
+						if(MultiPoint.class.isAssignableFrom(target)) {
+							Point[] points=new Point[] {};
+							if(source instanceof Point)
+								points=new Point[] {(Point)source};
+							else if(source instanceof GeometryCollection)
+								points=this.convertAll((GeometryCollection)source,Point.class).toArray(points);												
+							else
+								points=new Point[] {(Point)this.convert(source,Point.class)};
+							return gFac.createMultiPoint(points);
+						}
+						if(MultiLineString.class.isAssignableFrom(target)) {
+							LineString[] lineStrings=new LineString[] {};
+							if(source instanceof LineString)
+								lineStrings=new LineString[] {(LineString)source};
+							else if(source instanceof GeometryCollection)
+								lineStrings=this.convertAll((GeometryCollection)source,LineString.class).toArray(lineStrings);							
+							else
+								lineStrings=new LineString[] {(LineString)this.convert(source,LineString.class)};
+							return gFac.createMultiLineString(lineStrings);
+						}
+						if(MultiPolygon.class.isAssignableFrom(target)) {
+							Polygon[] polygons=new Polygon[] {};
+							if(source instanceof Polygon)
+								polygons=new Polygon[] {(Polygon)source};
+							else if(source instanceof GeometryCollection)
+								polygons=this.convertAll((GeometryCollection)source,Polygon.class).toArray(polygons);
+							else
+								polygons=new Polygon[] {(Polygon)this.convert(source,Polygon.class)};
+							return gFac.createMultiPolygon(polygons);
+						}
+						
+						// target is a geometrycollection: we add the source to
+						// a new geometrycollection
+						if(GeometryCollection.class.isAssignableFrom(target))
+							return gFac.createGeometryCollection(new Geometry[] {(Geometry)source});						
+						
+						// target is a point: we return the centroid of any complex geometry
+						if(Point.class.isAssignableFrom(target)) {
+							if(source instanceof MultiPoint && sourceGeometry.getNumGeometries()==1)
+								return ((MultiPoint)source).getGeometryN(0);
+							else {
+								if(LOGGER.isLoggable(Level.FINE))
+									LOGGER.fine("Converting Geometry "+source.toString()+" to Point. This could be unsafe");
+								return ((Geometry)source).getCentroid();
+							}
+						}
+						
+						// target is a linestring: we return the linestring connecting all the geometry coordinates
+						if(LineString.class.isAssignableFrom(target)) {
+							if(source instanceof MultiLineString && sourceGeometry.getNumGeometries()==1)
+								return ((MultiLineString)source).getGeometryN(0);
+							else {
+								if(LOGGER.isLoggable(Level.FINE))
+									LOGGER.fine("Converting Geometry "+source.toString()+" to LineString. This could be unsafe");
+								return gFac.createLineString(getLineStringCoordinates(((Geometry)source).getCoordinates()));
+							}
+						}
+						// target is a polygon: we return a polygon connecting all the coordinates of the given geometry
+						if(Polygon.class.isAssignableFrom(target)) {
+							if(source instanceof MultiPolygon && sourceGeometry.getNumGeometries()==1)
+								return ((MultiPolygon)source).getGeometryN(0);
+							else {
+								if(LOGGER.isLoggable(Level.FINE))
+									LOGGER.fine("Converting Geometry "+source.toString()+" to Polygon. This could be unsafe");
+								Coordinate[] coords=getPolygonCoordinates(((Geometry)source).getCoordinates());
+								return gFac.createPolygon(gFac.createLinearRing(coords), new LinearRing[] {});
+							}
+						}
 					}
 						
 					return null;
