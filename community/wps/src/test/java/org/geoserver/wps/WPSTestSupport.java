1a0662bf5466500f002b78dfc5f8bf0587eb18fe
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-21 11:01:16 +0000

    Remove commented code
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/geoserver@10685 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 96ea6376a32702d076b9051aae5eac78eeeeba82 [formerly e019b4765df839fdbbf7826b83a8f543f8e0e232]
    Former-commit-id: 945688588fff7e877c89441b0f7a0d8b56e06a31

--- community/wps/src/test/java/org/geoserver/wps/WPSTestSupport.java
+++ community/wps/src/test/java/org/geoserver/wps/WPSTestSupport.java
@@ -1,46 +1,23 @@
 package org.geoserver.wps;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
 
 import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLUnit;
-import org.custommonkey.xmlunit.XpathEngine;
 import org.geoserver.test.GeoServerTestSupport;
 import org.geoserver.wps.xml.WPSConfiguration;
-import org.geotools.xml.DOMParser;
 import org.geotools.xml.Parser;
 import org.w3c.dom.Document;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 public class WPSTestSupport extends GeoServerTestSupport {
 
-//    protected static final XpathEngine xpath;
-//    protected static final Schema WPS_SCHEMA;
-//    
     static {
-//        try {
-//            final SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-//            WPS_SCHEMA = factory.newSchema(new File("./schemas/wps/1.0.0/wpsAll.xsd"));
-//        } catch(Exception e) {
-//            throw new RuntimeException("Could not parse the WCS 1.1.1 schemas", e);
-//        }
-//
         // init xmlunit
         Map<String, String> namespaces = new HashMap<String, String>();
         namespaces.put("wps", "http://www.opengis.net/wps/1.0.0");
@@ -50,7 +27,6 @@ public class WPSTestSupport extends GeoServerTestSupport {
         namespaces.put("xlink", "http://www.w3.org/1999/xlink");
         
         XMLUnit.setXpathNamespaceContext(new SimpleNamespaceContext(namespaces));
-        //xpath = XMLUnit.newXpathEngine();
     }
     
     protected String root() {
