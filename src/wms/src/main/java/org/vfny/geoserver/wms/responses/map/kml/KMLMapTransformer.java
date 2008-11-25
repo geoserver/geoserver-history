5d367a0d52031bffaf285ace4cd43043471b0e3d
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-25 20:23:46 +0000

    Fix for GEOS-2439; problems encoding dynamic styles in KML output.
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10761 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 881035ac211ccd183a09096c219b9913b30ac315 [formerly 226c6862bae9495e1a8ac92bc2ca938c8dd7ed55]
    Former-commit-id: b25f59b93ddc50a778a9d34256b872d71762b59f

--- src/wms/src/main/java/org/vfny/geoserver/wms/responses/map/kml/KMLMapTransformer.java
+++ src/wms/src/main/java/org/vfny/geoserver/wms/responses/map/kml/KMLMapTransformer.java
@@ -534,7 +534,10 @@ public abstract class KMLMapTransformer extends KMLTransformerBase {
                     opacity = 1.0;
                 }
 
-                encodeColor((Color)symbolizer.getFill().getColor().evaluate(feature, Color.class), opacity);
+                encodeColor(
+                        (Color)symbolizer.getFill().getColor().evaluate(feature, Color.class),
+                        opacity
+                        );
             } else {
                 // make it transparent
                 encodeColor("00aaaaaa");
@@ -709,7 +712,7 @@ public abstract class KMLMapTransformer extends KMLTransformerBase {
                     opacity = 1.0;
                 }
 
-                encodeColor((Color)symbolizer.getFill().getColor().evaluate(feature), opacity);
+                encodeColor((Color)symbolizer.getFill().getColor().evaluate(feature, Color.class), opacity);
             } else {
                 // default
                 encodeColor("ffffffff");
