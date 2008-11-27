6b20063f097487e9202db30037b0a2f1bc602468
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-27 18:44:44 +0000

    Some javadocs
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10803 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b08dda8923155126d5b999bf19045a21173e0cf0 [formerly ec4ac305a43aee866c089edbdb5153414b3a74fd]
    Former-commit-id: 13c072232599ff159486f32e75778bdedd5551e5

--- src/wms/src/main/java/org/vfny/geoserver/wms/responses/GSLabelCacheItem.java
+++ src/wms/src/main/java/org/vfny/geoserver/wms/responses/GSLabelCacheItem.java
@@ -6,19 +6,34 @@ import org.geotools.renderer.style.TextStyle2D;
 
 public class GSLabelCacheItem extends LabelCacheItem {
     int maxDisplacement = 0;
+
     int minGroupDistance = 0;
+
     int repeat = 0;
+
     boolean labelAllGroup = false;
+
     boolean removeGroupOverlaps = false;
+
     boolean allowOverruns = true;
+
     boolean followLineEnabled = false;
+
     double maxAngleDelta;
+
     int autoWrap = 100;
 
     public GSLabelCacheItem(String layerId, TextStyle2D textStyle, LiteShape2 shape, String label) {
         super(layerId, textStyle, shape, label);
     }
 
+    /**
+     * Max amount of pixels the label will be moved around trying to find a non
+     * conflicting location (how and if the moving will be done is geometry type
+     * dependent)
+     * 
+     * @return
+     */
     public int getMaxDisplacement() {
         return maxDisplacement;
     }
@@ -27,6 +42,12 @@ public class GSLabelCacheItem extends LabelCacheItem {
         this.maxDisplacement = maxDisplacement;
     }
 
+    /**
+     * When enabled, repeats labels every "repeat" pixels (works on lines only
+     * atm)
+     * 
+     * @return
+     */
     public int getRepeat() {
         return repeat;
     }
@@ -35,6 +56,12 @@ public class GSLabelCacheItem extends LabelCacheItem {
         this.repeat = repeat;
     }
 
+    /**
+     * When grouping, wheter we should label only the biggest geometry, or the
+     * others as well
+     * 
+     * @return
+     */
     public boolean labelAllGroup() {
         return labelAllGroup;
     }
@@ -50,7 +77,12 @@ public class GSLabelCacheItem extends LabelCacheItem {
     public void setRemoveGroupOverlaps(boolean removeGroupOverlaps) {
         this.removeGroupOverlaps = removeGroupOverlaps;
     }
-    
+
+    /**
+     * Wheter labels are allowed to go past the start/end of the line
+     * 
+     * @return
+     */
     public boolean allowOverruns() {
         return allowOverruns;
     }
@@ -63,10 +95,21 @@ public class GSLabelCacheItem extends LabelCacheItem {
         return minGroupDistance;
     }
 
+    /**
+     * Minimum cartesian distance between two labels in the same group, in
+     * pixels
+     * 
+     * @param minGroupDistance
+     */
     public void setMinGroupDistance(int minGroupDistance) {
         this.minGroupDistance = minGroupDistance;
     }
 
+    /**
+     * Enables curved labels on linear features
+     * 
+     * @return
+     */
     public boolean isFollowLineEnabled() {
         return followLineEnabled;
     }
@@ -75,6 +118,13 @@ public class GSLabelCacheItem extends LabelCacheItem {
         this.followLineEnabled = followLineEnabled;
     }
 
+    /**
+     * Max angle between two subsequence characters in a curved label, in
+     * degrees. Good visual results are obtained with an angle of less than 25
+     * degrees.
+     * 
+     * @return
+     */
     public double getMaxAngleDelta() {
         return maxAngleDelta;
     }
@@ -83,6 +133,12 @@ public class GSLabelCacheItem extends LabelCacheItem {
         this.maxAngleDelta = maxAngleDelta;
     }
 
+    /**
+     * Automatically wraps long labels when the label width, in pixels, exceeds
+     * the autowrap length
+     * 
+     * @return
+     */
     public int getAutoWrap() {
         return autoWrap;
     }
