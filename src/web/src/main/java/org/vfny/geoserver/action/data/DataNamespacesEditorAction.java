a528eb1736b638f858fe9ba8e02d50ed9dac95b8
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-27 13:52:04 +0000

    GEOS-2451 UI should prevent creating multiple namespaces with same URI
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@10795 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 84c01f31f396d1b4e439b782e956949a13845a5f [formerly 93333498ac6a260f7d0ad3167f7a9a7c1ed4e45d]
    Former-commit-id: cfac89f288e0de7dfb6f0c9efabfed89caf2ee43

--- src/web/src/main/java/org/vfny/geoserver/action/data/DataNamespacesEditorAction.java
+++ src/web/src/main/java/org/vfny/geoserver/action/data/DataNamespacesEditorAction.java
@@ -4,6 +4,8 @@
  */
 package org.vfny.geoserver.action.data;
 
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -13,6 +15,9 @@ import org.vfny.geoserver.config.NameSpaceConfig;
 import org.vfny.geoserver.form.data.DataNamespacesEditorForm;
 import org.vfny.geoserver.global.UserContainer;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,11 +38,27 @@ public class DataNamespacesEditorAction extends ConfigAction {
         String URI = namespacesForm.getURI();
         String prefix = namespacesForm.getPrefix();
 
+        // let's check name and URI are unique
         DataConfig dataConfig = (DataConfig) getDataConfig();
-        NameSpaceConfig config = null;
+        NameSpaceConfig config = getUserContainer(request).getNamespaceConfig();
+        ActionErrors errors = new ActionErrors();
+        for (Iterator it = dataConfig.getNameSpaces().values().iterator(); it.hasNext();) {
+            NameSpaceConfig other = (NameSpaceConfig) it.next();
+            if(other == config)
+                continue;
+            if(other.getPrefix().equals(prefix))
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                        new ActionError("error.prefix.duplicate", new Object[] {other.getPrefix(), other.getUri()}));
+            if(other.getUri().equals(URI))
+                errors.add(ActionErrors.GLOBAL_ERROR,
+                        new ActionError("error.uri.duplicate", other.getPrefix()));
+        }
+        if(errors.size() > 0) {
+            saveErrors(request, errors);
+            return mapping.findForward("config.data.namespace.editor");
+        }
 
-        config = getUserContainer(request).getNamespaceConfig();
-
+        
         config.setPrefix(prefix);
         config.setUri(URI);
 
