31113b868b67c5c6039731b8c08f2a7a1f3bba4f
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2008-11-28 16:05:38 +0000

    GEOS-2455 Support response form in WPS execute
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@10815 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 15243fcfc760d85d6ad408ba43185902fc1fbb49 [formerly acdc3588f142db9351e89fead3e0dc8ba37810c8]
    Former-commit-id: c22bb1060c946b47d0a5a98eb53c6696c72913ef

--- src/community/wps/src/test/java/org/geoserver/wps/ExecuteTest.java
+++ src/community/wps/src/test/java/org/geoserver/wps/ExecuteTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.geotools.gml3.GMLConfiguration;
 import org.w3c.dom.Document;
 
 import static org.custommonkey.xmlunit.XMLAssert.*;
@@ -48,8 +49,6 @@ public class ExecuteTest extends WPSTestSupport {
          "</wps:Execute>";
         
         Document d = postAsDOM( "wps", xml );
-        
-        // remove these hacks once the XSD encoder issues have been fixed
         checkValidationErrors(d);
         
         assertEquals( "wps:ExecuteResponse", d.getDocumentElement().getNodeName() );
@@ -57,6 +56,46 @@ public class ExecuteTest extends WPSTestSupport {
         assertXpathExists( "/wps:ExecuteResponse/wps:Status/wps:ProcessSucceeded", d);
         assertXpathExists( 
             "/wps:ExecuteResponse/wps:ProcessOutputs/wps:Output/wps:Data/wps:ComplexData/gml:Polygon", d);
+    }
+    
+    public void testDataInlineRawOutput() throws Exception {
+        String xml =  
+          "<wps:Execute service='WPS' version='1.0.0' xmlns:wps='http://www.opengis.net/wps/1.0.0' " + 
+              "xmlns:ows='http://www.opengis.net/ows/1.1'>" + 
+            "<ows:Identifier>buffer</ows:Identifier>" + 
+             "<wps:DataInputs>" + 
+                "<wps:Input>" + 
+                    "<ows:Identifier>geom1</ows:Identifier>" + 
+                    "<wps:Data>" +
+                      "<wps:ComplexData>" + 
+                        "<gml:Polygon xmlns:gml='http://www.opengis.net/gml'>" +
+                          "<gml:exterior>" + 
+                            "<gml:LinearRing>" + 
+                              "<gml:coordinates>1 1 2 1 2 2 1 2 1 1</gml:coordinates>" + 
+                            "</gml:LinearRing>" + 
+                          "</gml:exterior>" + 
+                        "</gml:Polygon>" +
+                      "</wps:ComplexData>" + 
+                    "</wps:Data>" +     
+                "</wps:Input>" + 
+                "<wps:Input>" + 
+                   "<ows:Identifier>buffer</ows:Identifier>" + 
+                   "<wps:Data>" + 
+                     "<wps:LiteralData>1</wps:LiteralData>" + 
+                   "</wps:Data>" + 
+                "</wps:Input>" + 
+           "</wps:DataInputs>" +
+           "<wps:ResponseForm>" + 
+           "    <wps:RawDataOutput>" + 
+           "        <ows:Identifier>geom-buffered</ows:Identifier>" + 
+           "    </wps:RawDataOutput>" + 
+           "  </wps:ResponseForm>" +
+           "</wps:Execute>";
+        
+        Document d = postAsDOM( "wps", xml );
+        checkValidationErrors(d, new GMLConfiguration());
+        
+        assertEquals( "gml:Polygon", d.getDocumentElement().getNodeName() );
     }
 
     public void testFeatureCollectionInline() throws Exception {
@@ -89,8 +128,7 @@ public class ExecuteTest extends WPSTestSupport {
                "</wps:Execute>";
         
         Document d = postAsDOM( "wps", xml );
-        print( d );
-        // remove these hacks once the XSD encoder issues have been fixed
+        // print( d );
         checkValidationErrors(d);
         
         assertEquals( "wps:ExecuteResponse", d.getDocumentElement().getNodeName() );
