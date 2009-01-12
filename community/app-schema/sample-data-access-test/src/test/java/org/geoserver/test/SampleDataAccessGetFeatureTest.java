054ed375c6ce20c7f361c7b2b9820653cd8772a5
bencaradocdavies <bencaradocdavies@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-12 06:10:16 +0000

    Fixed properties and formatting.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@10996 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: dab9dddecdad8fd8d650c37c9385c105d6f0cd63 [formerly 06c9c99e0e7f9683150088ce01981a8fae502f46]
    Former-commit-id: a0b618103a6a598e85984e5c7d05681efbb18214

--- community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGetFeatureTest.java
+++ community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGetFeatureTest.java
@@ -3,21 +3,19 @@ package org.geoserver.test;
 import org.geotools.data.SampleDataAccess;
 
 /**
- * WFS GetFeature to test integration of {@link SampleDataAccess} with
- * GeoServer.
+ * WFS GetFeature to test integration of {@link SampleDataAccess} with GeoServer.
  * 
  * @author Ben Caradoc-Davies, CSIRO Exploration and Mining
  */
-public class SampleDataAccessGetFeatureTest extends
-		SampleDataAccessGeoServerTestSupport {
+public class SampleDataAccessGetFeatureTest extends SampleDataAccessGeoServerTestSupport {
 
-	/**
-	 * Test if GetFeature returns parseable XML.
-	 * 
-	 * @throws Exception
-	 */
-	public void testGetFeature() throws Exception {
-		getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
-	}
+    /**
+     * Test if GetFeature returns parsable XML.
+     * 
+     * @throws Exception
+     */
+    public void testGetFeature() throws Exception {
+        getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
+    }
 
 }
