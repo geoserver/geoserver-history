203dbd003b1ef86c88ad3d046137f0b74d5807b0
bencaradocdavies <bencaradocdavies@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-12 06:10:16 +0000

    Fixed properties and formatting.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@10996 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 47184345cc544796586ebc12f8c94801d6a4d66b [formerly d9d77a335cc14fd096efddf655b0cf48487dc308]
    Former-commit-id: 25b5846c55d06cbd8ffa97b0cb0f36569b6b14b5

--- src/community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGeoServerTestSupport.java
+++ src/community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGeoServerTestSupport.java
@@ -7,20 +7,18 @@ import org.geotools.data.SampleDataAccess;
 import org.geotools.util.logging.Logging;
 
 /**
- * Abstract base class for test cases that test integration of
- * {@link SampleDataAccess} with GeoServer.
+ * Abstract base class for test cases that test integration of {@link SampleDataAccess} with
+ * GeoServer.
  * 
  * @author Ben Caradoc-Davies, CSIRO Exploration and Mining
  */
-public abstract class SampleDataAccessGeoServerTestSupport extends
-		GeoServerAbstractTestSupport {
+public abstract class SampleDataAccessGeoServerTestSupport extends GeoServerAbstractTestSupport {
 
-	Logger LOGGER = Logging
-			.getLogger(SampleDataAccessGeoServerTestSupport.class);
+    Logger LOGGER = Logging.getLogger(SampleDataAccessGeoServerTestSupport.class);
 
-	@Override
-	protected TestData buildTestData() throws Exception {
-		return new SampleDataAccessMockData();
-	}
+    @Override
+    protected TestData buildTestData() throws Exception {
+        return new SampleDataAccessMockData();
+    }
 
 }
