a0f580080e4c53b00c34312f1190aeb06eb8d765
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-14 08:54:22 +0000

    Copyright headers
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11031 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4d20c2e38b01e37f594f34ef02cbf8be73dd8522 [formerly e87b071bf8b9c3f3325ec6a9ab04c7e65dd34c2a]
    Former-commit-id: 15f15396e177a35a95f5190d5c3363ad30b3fd79

--- src/community/ogrout/src/main/java/org/geoserver/wfs/response/Ogr2OgrConfigurator.java
+++ src/community/ogrout/src/main/java/org/geoserver/wfs/response/Ogr2OgrConfigurator.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.wfs.response;
 
 public class Ogr2OgrConfigurator {
@@ -7,6 +11,7 @@ public class Ogr2OgrConfigurator {
     public Ogr2OgrConfigurator(Ogr2OgrOutputFormat format) {
         this.format = format;
     }
+
     
     
 }
