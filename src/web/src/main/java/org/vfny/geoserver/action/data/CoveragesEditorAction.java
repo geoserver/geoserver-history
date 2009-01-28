08cfc8a8157de289a0598a16f1a17424dbf73d40
afabiani <afabiani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-28 18:05:29 +0000

    - GEOS-2593
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11181 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 66292355f9596b4bef4d9966791091b4eb7fddb7 [formerly 127f8ecfff702474522f80c005c9ee2e16871f1c]
    Former-commit-id: 5594f4baf7ebdcbcf5dd9164c6a0191c87ad9971

--- src/web/src/main/java/org/vfny/geoserver/action/data/CoveragesEditorAction.java
+++ src/web/src/main/java/org/vfny/geoserver/action/data/CoveragesEditorAction.java
@@ -4,6 +4,19 @@
  */
 package org.vfny.geoserver.action.data;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Level;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -13,8 +26,6 @@ import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.factory.FactoryRegistryException;
 import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.referencing.CRS;
-import org.geotools.referencing.NamedIdentifier;
-import org.geotools.resources.CRSUtilities;
 import org.opengis.coverage.grid.Format;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.FactoryException;
@@ -34,19 +45,6 @@ import org.vfny.geoserver.global.Data;
 import org.vfny.geoserver.global.GeoserverDataDirectory;
 import org.vfny.geoserver.global.MetaDataLink;
 import org.vfny.geoserver.global.UserContainer;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -164,8 +162,7 @@ public final class CoveragesEditorAction extends ConfigAction {
         CoverageStoreInfo cvStoreInfo = catalog.getFormatInfo(formatID);
 
         if (cvStoreInfo == null) {
-            org.geoserver.catalog.CoverageStoreInfo cvStore = 
-                getCatalog().getFactory().createCoverageStore();
+            org.geoserver.catalog.CoverageStoreInfo cvStore = getCatalog().getFactory().createCoverageStore();
             cvStoreInfo = new CoverageStoreInfo( cvStore, getCatalog() );
             cvStoreInfo.load( getDataConfig().getDataFormat(formatID).toDTO() );
             //cvStoreInfo = new CoverageStoreInfo(getDataConfig().getDataFormat(formatID).toDTO(),
@@ -176,8 +173,7 @@ public final class CoveragesEditorAction extends ConfigAction {
         AbstractGridCoverage2DReader reader = (AbstractGridCoverage2DReader) cvStoreInfo.getReader();
 
         if (reader == null) {
-            reader = (AbstractGridCoverage2DReader) ((AbstractGridFormat) format).getReader(GeoserverDataDirectory
-                    .findDataFile(cvStoreInfo.getUrl()));
+            reader = (AbstractGridCoverage2DReader) ((AbstractGridFormat) format).getReader(GeoserverDataDirectory.findDataFile(cvStoreInfo.getUrl()));
         }
 
         try {
@@ -190,6 +186,9 @@ public final class CoveragesEditorAction extends ConfigAction {
                 String userCRS = coverageForm.getUserDefinedCrsIdentifier();
 
                 if (userCRS != null) {
+                    coverageForm.setRequestCRSs((!userCRS.toUpperCase().startsWith("EPSG:") ? "EPSG:" : "") + userCRS);
+                    coverageForm.setResponseCRSs((!userCRS.toUpperCase().startsWith("EPSG:") ? "EPSG:" : "") + userCRS);
+
                     MathTransform transform;
 
                     if (!userCRS.toUpperCase().startsWith("EPSG:")) {
@@ -325,11 +324,12 @@ public final class CoveragesEditorAction extends ConfigAction {
      * @param request
      *
      * @return
+     * @throws ServletException 
      * @throws FactoryException
      */
     private ActionForward executeSubmit(ActionMapping mapping, CoveragesEditorForm form,
         UserContainer user, HttpServletRequest request)
-        throws IOException {
+        throws IOException, ServletException {
         final CoverageConfig config = user.getCoverageConfig();
 
         try {
@@ -338,6 +338,14 @@ public final class CoveragesEditorAction extends ConfigAction {
             final IOException ex = new IOException(e.getLocalizedMessage());
             ex.initCause(e);
             throw ex;
+        }
+
+        if(config.getRequestCRSs() == null || config.getRequestCRSs().isEmpty()) {
+            throw new ServletException("Coverage Request and Respose CRS could not be null or empty");            
+        }
+        
+        if(config.getResponseCRSs() == null || config.getResponseCRSs().isEmpty()) {
+            throw new ServletException("Coverage Request and Respose CRS could not be null or empty");            
         }
 
         final DataConfig dataConfig = (DataConfig) getDataConfig();
@@ -450,8 +458,12 @@ public final class CoveragesEditorAction extends ConfigAction {
                 coverageForm.setUserDefinedCrsIdentifier("UNKNOWN");
             } else if (s.toUpperCase().startsWith("EPSG:")) {
                 coverageForm.setUserDefinedCrsIdentifier(s);
+                coverageForm.setRequestCRSs(s);
+                coverageForm.setResponseCRSs(s);
             } else {
                 coverageForm.setUserDefinedCrsIdentifier("EPSG:" + s);
+                coverageForm.setRequestCRSs("EPSG:" + s);
+                coverageForm.setResponseCRSs("EPSG:" + s);
             }
         } catch (Exception e) {
             coverageForm.setUserDefinedCrsIdentifier("UNKNOWN");
@@ -515,7 +527,8 @@ public final class CoveragesEditorAction extends ConfigAction {
         final int length = array.length;
 
         for (int i = 0; i < length; i++) {
-            requestCRSs.add(array[i]);
+            if(array[i] != null && array[i].trim().length() > 0)
+                requestCRSs.add(array[i]);
         }
 
         return requestCRSs;
@@ -528,7 +541,8 @@ public final class CoveragesEditorAction extends ConfigAction {
         final int length = array.length;
 
         for (int i = 0; i < length; i++) {
-            responseCRSs.add(array[i]);
+            if(array[i] != null && array[i].trim().length() > 0)
+                responseCRSs.add(array[i]);
         }
 
         return responseCRSs;
