33334e47af047fa8d166de0fa4b0b302e003dce2
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-28 14:51:47 +0000

    GEOS-2471 Support namespace filter WMS getCapabilities
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11176 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f8e981b3813102d2025115b59ccb0ec03d90d389 [formerly 36f3929edb128243ee7e2306d2e438425a05ed9a]
    Former-commit-id: df144da2d7db8f20627893f80d7892505f0fc585

--- src/wms/src/test/java/org/geoserver/wms/CapabilitiesTest.java
+++ src/wms/src/test/java/org/geoserver/wms/CapabilitiesTest.java
@@ -1,5 +1,7 @@
 package org.geoserver.wms;
 
+import org.custommonkey.xmlunit.XMLUnit;
+import org.custommonkey.xmlunit.XpathEngine;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -16,5 +18,13 @@ public class CapabilitiesTest extends WMSTestSupport {
         assertEquals("WMT_MS_Capabilities", e.getLocalName());
     }
     
+    public void testFilteredCapabilitiesCite() throws Exception {
+        Document dom = dom(get("wms?request=getCapabilities&namespace=cite"), true);
+        Element e = dom.getDocumentElement();
+        assertEquals("WMT_MS_Capabilities", e.getLocalName());
+        XpathEngine xpath =  XMLUnit.newXpathEngine();
+        assertTrue(xpath.getMatchingNodes("//Layer/Name[starts-with(., cite)]", dom).getLength() > 0);
+        assertEquals(0, xpath.getMatchingNodes("//Layer/Name[not(starts-with(., cite))]", dom).getLength());
+    }
     
 }
