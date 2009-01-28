c369041e5f0f7052c8ce002a4396ff100a993cd0
bencaradocdavies <bencaradocdavies@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-01-28 07:45:41 +0000

    Add info.xml and wfs dependency. DataAccess can be loaded by post-GSIP31 GeoServer. Encoding still fails, but core infrastructure is good.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11171 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e718548bc85e0a25b2a9414d498f89ad5b41d38c [formerly 391327f24b8515587fb2b0afd5756aa11514738f]
    Former-commit-id: fe7b8200ca560e64ea447e0395f4f32db170711a

--- src/community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGetFeatureTest.java
+++ src/community/app-schema/sample-data-access-test/src/test/java/org/geoserver/test/SampleDataAccessGetFeatureTest.java
@@ -6,7 +6,14 @@
 
 package org.geoserver.test;
 
+import java.io.OutputStream;
+
+import junit.framework.Test;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
 import org.geotools.data.SampleDataAccess;
+import org.w3c.dom.Document;
 
 /**
  * WFS GetFeature to test integration of {@link SampleDataAccess} with GeoServer.
@@ -16,12 +23,33 @@ import org.geotools.data.SampleDataAccess;
 public class SampleDataAccessGetFeatureTest extends SampleDataAccessGeoServerTestSupport {
 
     /**
+     * Read-only test so can use one-time setup.
+     * 
+     * @return
+     */
+    public static Test suite() {
+        return new OneTimeTestSetup(new SampleDataAccessGetFeatureTest());
+    }
+
+    /**
      * Test if GetFeature returns parsable XML.
      * 
      * @throws Exception
      */
     public void testGetFeature() throws Exception {
-        getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
+        Document doc = getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature" /* "&version=1.1.0&service=wfs" */ );
+        prettyPrint(doc, System.out);
+        assertEquals("wfs:FeatureCollection", doc.getDocumentElement()
+                .getNodeName());
     }
-
+    
+    public void prettyPrint(Document doc, OutputStream out) throws Exception {
+        OutputFormat format = new OutputFormat(doc);
+        format.setLineWidth(80);
+        format.setIndenting(true);
+        format.setIndent(4);
+        XMLSerializer serializer = new XMLSerializer(out, format);
+        serializer.serialize(doc);
+    }   
+    
 }
