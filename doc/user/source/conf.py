be7b49994f56136452cb59ca9f14aa2b77eb024a
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-02 18:23:03 +0000

    turned off module and index
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11196 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e4794eea0dc103bdf3cdacc7280083a29db19087 [formerly ac7bf0c8a92a701a2eda9b6a440ebae3bab19c93]
    Former-commit-id: f8db2717b84befa6be6a519c3ad08149459963b5

--- doc/user/source/conf.py
+++ doc/user/source/conf.py
@@ -122,10 +122,10 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_use_modindex = True
+html_use_modindex = False
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
