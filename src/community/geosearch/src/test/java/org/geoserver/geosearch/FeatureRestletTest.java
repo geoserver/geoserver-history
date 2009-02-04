0dab352f84710df020e667826165fe6228e5cff2
dwinslow <dwinslow@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-04 21:28:37 +0000

    Fix broken test exposed (not caused :P) by rest module update
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11256 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 328f11aef65fb36130a9801b25208c5defacd1a2 [formerly efb65c59914d19ce1eace516f5fd960401d49815]
    Former-commit-id: 279b5013ceb4600fe2f07276fd159a14e0981567

--- src/community/geosearch/src/test/java/org/geoserver/geosearch/FeatureRestletTest.java
+++ src/community/geosearch/src/test/java/org/geoserver/geosearch/FeatureRestletTest.java
@@ -18,7 +18,13 @@ public class FeatureRestletTest extends GeoServerTestSupport {
     public void testSingleFeature() throws Exception{
         QName typename = MockData.BASIC_POLYGONS;
         final String path = 
-            "/rest/" + typename.getPrefix() + "/" + typename.getLocalPart() + "/1107531493630.kml";
+            "/rest/layers/" 
+            + typename.getPrefix() 
+            + ":" 
+            + typename.getLocalPart() 
+            + "/"
+            + typename.getLocalPart()
+            + ".1107531493630.kml";
 
         getFeatureTypeInfo(typename).setIndexingEnabled(true);
 
