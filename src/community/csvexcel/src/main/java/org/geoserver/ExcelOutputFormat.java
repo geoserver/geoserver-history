5e8edcfbea2ab6dd6badecd888465e9e3981cc3d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-10 13:38:55 +0000

    License header, type safety, set the cell contents using the proper type so that the spreadsheet recognizes numbers, dates, and booleans as such
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11289 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5be9b854081a88bcb38db0ef3b5b45c192d4a62d [formerly a6f591cece50735f86e0c06b0f35a4d5ecc331e7]
    Former-commit-id: 97f9aeb6b29a991b83aeec7ca4e3f16b861500d3

--- src/community/csvexcel/src/main/java/org/geoserver/ExcelOutputFormat.java
+++ src/community/csvexcel/src/main/java/org/geoserver/ExcelOutputFormat.java
@@ -1,14 +1,19 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
 package org.geoserver;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 
 import net.opengis.wfs.FeatureCollectionType;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -43,17 +48,7 @@ public class ExcelOutputFormat extends WFSGetFeatureOutputFormat {
     @Override
     public String getMimeType(Object value, Operation operation)
                throws ServiceException {
-         // return the mime type of the format here, the parent 
-         // class returns 'text/xml'
          return "application/msexcel";
-    }
-    
-    @Override
-    protected boolean canHandleInternal(Operation operation) {
-        //any additional checks that need to be performed to 
-        // determine when the output format should be "engaged" 
-        // should go here
-        return super.canHandleInternal(operation);
     }
     
     /**
@@ -63,50 +58,59 @@ public class ExcelOutputFormat extends WFSGetFeatureOutputFormat {
     protected void write(FeatureCollectionType featureCollection,
             OutputStream output, Operation getFeature) throws IOException,
             ServiceException {
-    	   //write out content here
-    	
-        //get the feature collection
-        FeatureCollection fc = (FeatureCollection) featureCollection.getFeature().get(0);
-    	
-        //Create the workbook and sheet
+        //Create the workbook 
+        HSSFWorkbook wb = new HSSFWorkbook();
         
-    	HSSFWorkbook wb = new HSSFWorkbook();    	
-    	HSSFSheet sheet = wb.createSheet("sheet");
+        for (Iterator it = featureCollection.getFeature().iterator(); it.hasNext();) {
+            FeatureCollection<SimpleFeatureType, SimpleFeature> fc = (FeatureCollection<SimpleFeatureType, SimpleFeature>) it.next();
     	
-  
-        //write out the header
-    	HSSFRow header = sheet.createRow((short) 0);
-    	
-        SimpleFeatureType ft = (SimpleFeatureType) fc.getSchema();
-        HSSFCell cell;
-        
-        for ( int i = 0; i < ft.getAttributeCount(); i++ ) {
-            AttributeDescriptor ad = ft.getDescriptor( i );
+            // create the sheet for this feature collection    	
+        	HSSFSheet sheet = wb.createSheet(fc.getSchema().getTypeName());
+      
+            // write out the header
+        	HSSFRow header = sheet.createRow((short) 0);
+        	
+            SimpleFeatureType ft = (SimpleFeatureType) fc.getSchema();
+            HSSFCell cell;
             
-            cell = header.createCell((short) i);
-            cell.setCellValue(ad.getLocalName());
-        }
-        
-        //write out the features
-        FeatureIterator i = fc.features();
-        int r = 0; // row index
-        try {
-        	HSSFRow row;
-            while( i.hasNext() ) {
-            	r++; //start at 1, since header is at 0
-                SimpleFeature f = (SimpleFeature) i.next();
-                row = sheet.createRow((short) r);
-                for ( int j = 0; j < f.getAttributeCount(); j++ ) {
-                    Object att = f.getAttribute( j );
-                    if ( att != null ) {
-                    	cell = row.createCell((short) j);
-                    	cell.setCellValue( att.toString() );
-                    }
-                }    
+            for ( int i = 0; i < ft.getAttributeCount(); i++ ) {
+                AttributeDescriptor ad = ft.getDescriptor( i );
+                
+                cell = header.createCell(i);
+                cell.setCellValue(new HSSFRichTextString(ad.getLocalName()));
             }
-        }
-        finally {
-            fc.close( i );
+            
+            // write out the features
+            FeatureIterator<SimpleFeature> i = fc.features();
+            int r = 0; // row index
+            try {
+            	HSSFRow row;
+                while( i.hasNext() ) {
+                	r++; //start at 1, since header is at 0
+                    SimpleFeature f = i.next();
+                    row = sheet.createRow((short) r);
+                    for ( int j = 0; j < f.getAttributeCount(); j++ ) {
+                        Object att = f.getAttribute( j );
+                        if ( att != null ) {
+                        	cell = row.createCell(j);
+                        	if(att instanceof Number) {
+                      	        cell.setCellValue(((Number) att).doubleValue());
+                        	} else if(att instanceof Date) {
+                        	    cell.setCellValue((Date) att);
+                        	} else if(att instanceof Calendar) {
+                        	    cell.setCellValue((Calendar) att);
+                        	} else if(att instanceof Boolean) {
+                        	    cell.setCellValue((Boolean) att);
+                        	} else {
+                        	    // ok, it seems we have no better way than dump it as a string
+                        	    cell.setCellValue(new HSSFRichTextString(att.toString()));
+                        	}
+                        }
+                    }    
+                }
+            } finally {
+                fc.close( i );
+            }
         }
         
         //write to output
