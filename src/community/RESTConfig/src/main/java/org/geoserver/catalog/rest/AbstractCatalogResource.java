63d4537c74d9687a1cfef4d738db7a3800187221
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-11 19:10:39 +0000

    factored out some methods and properties into base class, added configurePersister() template method
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11318 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 95376b53c43db5a485ab2f858f15b6b4d4541cda [formerly f4edee1a269a2e4f8327177f712849056be531da]
    Former-commit-id: 5a06505fc707fd399f0576ae8423a8771f94c6fe

--- src/community/RESTConfig/src/main/java/org/geoserver/catalog/rest/AbstractCatalogResource.java
+++ src/community/RESTConfig/src/main/java/org/geoserver/catalog/rest/AbstractCatalogResource.java
@@ -3,55 +3,26 @@ package org.geoserver.catalog.rest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.logging.Logger;
 
 import org.geoserver.catalog.Catalog;
-import org.geoserver.config.GeoServer;
 import org.geoserver.config.util.XStreamPersister;
-import org.geoserver.platform.GeoServerExtensions;
-import org.geoserver.rest.ReflectiveResource;
 import org.geoserver.rest.format.DataFormat;
 import org.geoserver.rest.format.ReflectiveJSONFormat;
 import org.geoserver.rest.format.ReflectiveXMLFormat;
-import org.geotools.util.logging.Logging;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
-import org.vfny.geoserver.global.Data;
-import org.vfny.geoserver.global.Service;
-import org.vfny.geoserver.global.dto.WCSDTO;
-import org.vfny.geoserver.global.dto.WFSDTO;
-import org.vfny.geoserver.global.dto.WMSDTO;
-import org.vfny.geoserver.global.xml.XMLConfigWriter;
 
-public abstract class AbstractCatalogResource extends ReflectiveResource {
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-    /**
-     * logger
-     */
-    static Logger LOGGER = Logging.getLogger( "org.geoserver.catalog.rest");
-    /**
-     * the catalog
-     */
-    protected Catalog catalog;
-    /**
-     * the class of the resource
-     */
-    protected Class clazz;
-    
+public abstract class AbstractCatalogResource extends CatalogResourceBase {
+
     public AbstractCatalogResource(Context context,Request request, Response response, Class clazz,
         Catalog catalog) {
         
-        super(context, request, response);
-        this.catalog = catalog;
-        this.clazz = clazz;
+        super(context, request, response, clazz, catalog);
     }
-
-    @Override
-    protected DataFormat createHTMLFormat(Request request,Response response) {
-        return new CatalogFreemarkerHTMLFormat( clazz, request, response, this );
-    }
-       
+   
     @Override
     protected ReflectiveXMLFormat createXMLFormat(Request request,Response response) {
         return new ReflectiveXMLFormat() {
@@ -59,6 +30,8 @@ public abstract class AbstractCatalogResource extends ReflectiveResource {
             protected void write(Object data, OutputStream output) throws IOException  {
                 XStreamPersister p = new XStreamPersister.XML();
                 p.setCatalog( catalog );
+                
+                configurePersister(p);
                 p.save( data, output );
             }
             
@@ -67,6 +40,8 @@ public abstract class AbstractCatalogResource extends ReflectiveResource {
                     throws IOException {
                 XStreamPersister p = new XStreamPersister.XML();
                 p.setCatalog( catalog );
+                
+                configurePersister(p);
                 return p.load( in, clazz );
             }
         };
@@ -80,6 +55,8 @@ public abstract class AbstractCatalogResource extends ReflectiveResource {
                     throws IOException {
                 XStreamPersister p = new XStreamPersister.JSON();
                 p.setCatalog(catalog);
+                
+                configurePersister(p);
                 p.save( data, output );
             }
             
@@ -88,39 +65,17 @@ public abstract class AbstractCatalogResource extends ReflectiveResource {
                     throws IOException {
                 XStreamPersister p = new XStreamPersister.JSON();
                 p.setCatalog(catalog);
+                
+                configurePersister(p);
                 return p.load( input, clazz );
             }
         };
     }
     
     /**
-     * Method that persists the catalog after a change has been made.
+     * Method for subclasses to perform additional configuration of the 
+     * xstream instance used for serializing/de-serializing objects.
      */
-    protected void saveCatalog() throws Exception {
-        saveCatalog( catalog );
-    }
-    
-    /**
-     * Static method to persist the catalog to be called by non subclasses. 
-     */
-    protected static void saveCatalog( Catalog catalog ) throws Exception {
-        Data data = (Data) GeoServerExtensions.bean( "data" );
-        synchronized( GeoServer.CONFIGURATION_LOCK ) {
-            XMLConfigWriter.store( data.toDTO() , catalog.getResourceLoader().getBaseDirectory() );
-        }
-    }
-    
-    protected void saveConfiguration() throws Exception {
-        org.vfny.geoserver.global.GeoServer global = 
-            (org.vfny.geoserver.global.GeoServer) GeoServerExtensions.bean( "geoServer");
-        Service wfs = (Service) GeoServerExtensions.bean( "wfs" );
-        Service wms = (Service) GeoServerExtensions.bean( "wms" );
-        Service wcs = (Service) GeoServerExtensions.bean( "wcs" );
-        
-        synchronized (GeoServer.CONFIGURATION_LOCK) {
-            XMLConfigWriter.store((WCSDTO)wcs.toDTO(),(WMSDTO)wms.toDTO(),(WFSDTO)wfs.toDTO(),
-                global.toDTO(),catalog.getResourceLoader().getBaseDirectory());    
-        }
-        
+    protected void configurePersister(XStreamPersister persister) {
     }
 }
