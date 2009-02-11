b5b5022589383a02b20379ef44ceb066069a810d
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-11 04:32:35 +0000

    changed mock input stream to hold byte array, changed return type of getBinaryInputStream()
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11306 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f60cea962c8f605a04ac30c4f181c231a7c2016a [formerly a650ce44f5dada94a36daba5590c4452757dbab0]
    Former-commit-id: 61b66dd60fb0809da10c311f54e56e29a1fc1680

--- main/src/test/java/org/geoserver/test/GeoServerAbstractTestSupport.java
+++ main/src/test/java/org/geoserver/test/GeoServerAbstractTestSupport.java
@@ -591,7 +591,7 @@ public abstract class GeoServerAbstractTestSupport extends OneTimeSetupTest {
      * @param response
      * @return
      */
-    protected InputStream getBinaryInputStream(MockHttpServletResponse response) {
+    protected ByteArrayInputStream getBinaryInputStream(MockHttpServletResponse response) {
         try {
             MockServletOutputStream os = (MockServletOutputStream) response.getOutputStream();
             final Field field = os.getClass().getDeclaredField("buffer");
@@ -1104,13 +1104,18 @@ public abstract class GeoServerAbstractTestSupport extends OneTimeSetupTest {
     }
 
     public static class GeoServerMockHttpServletRequest extends MockHttpServletRequest {
-        private String myBody;
-
-        public void setBodyContent(String body){
-            super.setBodyContent(body);
+        private byte[] myBody;
+        
+        @Override
+        public void setBodyContent(byte[] body) {
             myBody = body;
         }
-
+        
+        @Override
+        public void setBodyContent(String body) {
+            myBody = body.getBytes();
+        }
+        
         public ServletInputStream getInputStream(){
             return new GeoServerMockServletInputStream(myBody);
         }
@@ -1121,10 +1126,10 @@ public abstract class GeoServerAbstractTestSupport extends OneTimeSetupTest {
         private int myOffset = 0;
         private int myMark = -1;
 
-        public GeoServerMockServletInputStream(String body){
-            myBody = body.getBytes();
+        public GeoServerMockServletInputStream(byte[] body){
+            myBody = body;
         }
-
+        
         public int available() {
             return myBody.length - myOffset;
         }
