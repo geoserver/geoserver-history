7652e4ddd587cec8e054e2b446cbd299479b04b4
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-02-18 18:52:42 +0000

    Make the test compile again, use new utility methods in MockData to setup coverages
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11399 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 961f4c5e672d2cbfd7acfbf96cdc39e168a3b74f [formerly ca6bea5408ac9124261ecff282d2956ab7b6fb25]
    Former-commit-id: d654d62901e072785daff7fd23d1c96ab1857780

--- src/community/web2/wcs/src/test/java/org/geoserver/wcs/web/GeoServerWicketCoverageTestSupport.java
+++ src/community/web2/wcs/src/test/java/org/geoserver/wcs/web/GeoServerWicketCoverageTestSupport.java
@@ -9,25 +9,6 @@ import javax.xml.namespace.QName;
 public abstract class GeoServerWicketCoverageTestSupport extends GeoServerWicketTestSupport {
     @Override
     protected void populateDataDirectory(MockData dataDirectory) throws Exception {
-        dataDirectory.addCoverage(
-                CoverageTestSupport.TASMANIA_DEM, 
-                CoverageTestSupport.class.getResource("tazdem.tiff"),
-                CoverageTestSupport.TIFF,
-                null);
-        dataDirectory.addCoverage(
-                CoverageTestSupport.TASMANIA_BM, 
-                CoverageTestSupport.class.getResource("tazbm.tiff"),
-                CoverageTestSupport.TIFF,
-                null);
-        dataDirectory.addCoverage(
-                CoverageTestSupport.ROTATED_CAD,
-                CoverageTestSupport.class.getResource("rotated.tiff"),
-                CoverageTestSupport.TIFF,
-                null);
-        dataDirectory.addCoverage(
-                CoverageTestSupport.WORLD,
-                CoverageTestSupport.class.getResource("world.tiff"),
-                CoverageTestSupport.TIFF,
-                null);
+        dataDirectory.addWellKnownCoverageTypes();
     }
 }
