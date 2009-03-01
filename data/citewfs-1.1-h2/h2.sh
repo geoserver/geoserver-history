47167c506536632ba155016ff771582f60564081
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-01 17:42:58 +0000

    updated h2 db loading script
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11524 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 215b2a649ae9b2d2efac89d1deaff29dc2f160bd [formerly 99a7138b9502a83c1e477fd0363b3d22b7e1d9a3]
    Former-commit-id: 5e3725efe3d49c734a191a52cbfa707a21ff1745

--- data/citewfs-1.1-h2/h2.sh
+++ data/citewfs-1.1-h2/h2.sh
@@ -1,7 +1,7 @@
-H2="$HOME/.m2/repository/org/h2database/h2/1.0-SNAPSHOT/h2-1.0-SNAPSHOT.jar"
+H2="$HOME/.m2/repository/org/h2database/h2/1.1.104/h2-1.1.104.jar"
 M2_REPO="$HOME/.m2/repository"
 GT_VERSION=2.5-SNAPSHOT
-CP="$H2:$M2_REPO/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar:$M2_REPOy/org/geotools/gt-main/$GT_VERSION/gt-main-$GT_VERSION.jar:$M2_REPO/com/vividsolutions/jts/1.8/jts-1.8.jar"
+CP="$H2:$M2_REPO/org/geotools/gt-main/$GT_VERSION/gt-main-$GT_VERSION.jar:$M2_REPO/org/geotools/gt-jdbc-h2/$GT_VERSION/gt-jdbc-h2-$GT_VERSION.jar:$M2_REPO/com/vividsolutions/jts/1.9/jts-1.9.jar"
 SF="0"
 
 if [ "load" = "$1" ]; then
