e8190fac746f971f6df64aeb692ca8808bba42e9
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-05 17:27:13 +0000

    General improvements to the stores page code
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11611 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 38cf52c157a70b9a2a48399d6b4abd7a035afef3 [formerly 118d11271d02cf477cb59903c7fd4977afcd6e74]
    Former-commit-id: d179b28415b2b310d71a30b0cbf60af4e22b7000

--- src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
@@ -1,6 +1,8 @@
 package org.geoserver.web.data.table;
 
-import static org.geoserver.web.data.table.StoreProvider.*;
+import static org.geoserver.web.data.table.StoreProvider.ENABLED_PROPERTY;
+import static org.geoserver.web.data.table.StoreProvider.NAME_PROPERTY;
+import static org.geoserver.web.data.table.StoreProvider.WORKSPACE_PROPERTY;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -28,12 +30,10 @@ import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.data.NamespaceEditPage;
-import org.geoserver.web.data.ResourceConfigurationPage;
 import org.geoserver.web.data.datastore.DataStoreConfiguration;
 import org.geoserver.web.wicket.GeoServerPagingNavigator;
 
@@ -50,102 +50,41 @@ public class StorePage extends GeoServerBasePage {
 
     DataView dataView;
 
+    private ModalWindow popupWindow;
+
+    private WebMarkupContainer tableContainer;
+
+    private AjaxLink removeLink;
+
     public StorePage() {
-        // setup the dialog
-        final ModalWindow popupWindow = new ModalWindow("popupWindow");
+        // the popup window used for messages
+        popupWindow = new ModalWindow("popupWindow");
         add(popupWindow);
 
-        // the layer container
-        final WebMarkupContainer tableContainer = new WebMarkupContainer("listContainer");
+        // the container for the table (for ajax updates)
+        tableContainer = new WebMarkupContainer("listContainer");
 
         // build the filter form
         Form form = new Form("filterForm");
         add(form);
         form.add(filter = new TextField("filter", new Model()));
         filter.setOutputMarkupId(true);
-        AjaxButton filterSubmit = new AjaxButton("applyFilter") {
-
-            @Override
-            protected void onSubmit(AjaxRequestTarget target, Form form) {
-                String[] keywords = filter.getModelObjectAsString().split("\\s+");
-                stores.setKeywords(keywords);
-                dataView.setCurrentPage(0);
-                target.addComponent(tableContainer);
-                target.addComponent(navigator);
-            }
-
-        };
-        form.add(filterSubmit);
-        AjaxButton filterReset = new AjaxButton("resetFilter") {
-
-            @Override
-            protected void onSubmit(AjaxRequestTarget target, Form form) {
-                stores.setKeywords(null);
-                filter.setModelObject("");
-                dataView.setCurrentPage(0);
-                target.addComponent(tableContainer);
-                target.addComponent(navigator);
-                target.addComponent(filter);
-            }
-
-        };
-        form.add(filterReset);
+        form.add(filterSubmitButton());
+        form.add(filterResetButton());
 
         // add the filter match label
         add(matched = new Label("filterMatch"));
         matched.setVisible(false);
 
-        // add the remove link
-        final AjaxLink removeLink = new AjaxLink("remove") {
-
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                String content = popupWindow.getContentId();
-                if (selection.isEmpty()) {
-                    popupWindow.setContent(new Label(content, "Selection is empty, dude!"));
-                    popupWindow.show(target);
-                } else {
-                    String msg = "Ok, so you wanted to remove " + selection
-                            + " uh? Well, you have some code to implement before that!!";
-                    popupWindow.setContent(new Label(content, msg));
-                    popupWindow.show(target);
-                }
-            }
-
-        };
-        add(removeLink);
+        // add the link for removing stores
+        removeLink = removeLink();
         removeLink.setEnabled(false);
+        add(removeLink);
 
         // the workspaces drop down
-        final DropDownChoice workspaces = new DropDownChoice("wsDropDown", new Model(),
-                new LoadableDetachableModel() {
-
-                    @Override
-                    protected Object load() {
-                        List<WorkspaceInfo> workspaces = getCatalog().getWorkspaces();
-                        List<String> wsNames = new ArrayList<String>();
-                        for (WorkspaceInfo store : workspaces) {
-                            wsNames.add(store.getName());
-                        }
-                        return wsNames;
-                    }
-                });
-        workspaces.add(new AjaxFormComponentUpdatingBehavior("onchange") {
-
-            @Override
-            protected void onUpdate(AjaxRequestTarget target) {
-                popupWindow
-                        .setContent(new Label(
-                                popupWindow.getContentId(),
-                                "Ah, so you wanted to add a store into "
-                                        + workspaces.getModelObjectAsString()
-                                        + " huh? Well, now go into the code and actually implement the 'add layer' workflow then!"));
-                popupWindow.show(target);
-            }
-        });
-
+        final DropDownChoice workspaces = workspacesDropDown();
         add(workspaces);
-        
+
         // setup the table
         tableContainer.setOutputMarkupId(true);
         add(tableContainer);
@@ -161,74 +100,25 @@ public class StorePage extends GeoServerBasePage {
 
                 // build an indirection so that we don't store the actual layer
                 // but just the model the item is using, which is detachable
-                final AjaxCheckBox selected = new AjaxCheckBox("selected", new Model() {
-                    @Override
-                    public Object getObject() {
-                        StoreInfo si = (StoreInfo) model.getObject();
-                        return selection.contains(si.getName());
-                    }
-                }) {
-                    @Override
-                    protected void onUpdate(AjaxRequestTarget target) {
-                        StoreInfo si = (StoreInfo) model.getObject();
-                        if (selection.contains(si.getName()))
-                            selection.remove(si.getName());
-                        else
-                            selection.add(si.getName());
-                        target.addComponent(tableContainer);
-
-                        // update the remove link, enabled, only if there
-                        // is some selection
-                        removeLink.setEnabled(selection.size() > 0);
-                        target.addComponent(removeLink);
-                    }
-                };
+                final AjaxCheckBox selected = selectionCheckBox(model);
                 item.add(selected);
 
-                // build an indirection so that we don't store the actual layer
-                // but just the model the item is using, which is detachable
-                Label type = new Label("type", new Model() {
-                    @Override
-                    public Object getObject() {
-                        StoreInfo li = (StoreInfo) model.getObject();
-                        if (li instanceof DataStoreInfo)
-                            return "vector";
-                        else
-                            return "raster";
-                    }
-                });
+                // the store icon (vector/raster)
+                Label type = new Label("type", new StoreIconModel(model));
                 item.add(type);
 
+                // the workspace link
                 PropertyModel wsProperty = new PropertyModel(model, WORKSPACE_PROPERTY);
-                Link wsLink = new Link("wsLink", wsProperty) {
-                    public void onClick() {
-                        WorkspaceInfo info = getCatalog().getWorkspaceByName(
-                                getModelObjectAsString());
-                        if (info != null)
-                            setResponsePage(new NamespaceEditPage(info.getId()));
-                    }
-                };
+                Link wsLink = workspaceLink(wsProperty);
                 item.add(wsLink);
                 wsLink.add(new Label("ws", wsProperty));
 
-                AjaxLink nameLink = new AjaxLink("nameLink",
-                        new PropertyModel(model, NAME_PROPERTY)) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        String storeName = getModelObjectAsString();
-                        DataStoreInfo store = getCatalog().getDataStoreByName(storeName);
-                        if (store != null)
-                            setResponsePage(new DataStoreConfiguration(store.getId()));
-                        else {
-                            popupWindow.setContent(new Label(popupWindow.getContentId(),
-                                    "Sorry bud, no coverage store editor so far"));
-                            popupWindow.show(target);
-                        }
-                    }
-                };
+                // the store name link
+                AjaxLink nameLink = storeNameLink(model);
                 item.add(nameLink);
                 nameLink.add(new Label("name", new PropertyModel(model, NAME_PROPERTY)));
 
+                // is the store enabled?
                 item.add(new Label("enabled", new PropertyModel(model, ENABLED_PROPERTY)));
             }
 
@@ -246,5 +136,156 @@ public class StorePage extends GeoServerBasePage {
         navigator = new GeoServerPagingNavigator("navigator", dataView);
         navigator.setOutputMarkupId(true);
         add(navigator);
+    }
+
+    private AjaxLink removeLink() {
+        return new AjaxLink("remove") {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                String content = popupWindow.getContentId();
+                if (selection.isEmpty()) {
+                    popupWindow.setContent(new Label(content, "Selection is empty, dude!"));
+                    popupWindow.show(target);
+                } else {
+                    String msg = "Ok, so you wanted to remove " + selection
+                            + " uh? Well, you have some code to implement before that!!";
+                    popupWindow.setContent(new Label(content, msg));
+                    popupWindow.show(target);
+                }
+            }
+
+        };
+    }
+
+    private DropDownChoice workspacesDropDown() {
+        final DropDownChoice workspaces;
+        workspaces = new DropDownChoice("wsDropDown", new Model(), new WorkspacesModel());
+        workspaces.add(new AjaxFormComponentUpdatingBehavior("onchange") {
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                popupWindow
+                        .setContent(new Label(
+                                popupWindow.getContentId(),
+                                "Ah, so you wanted to add a store into "
+                                        + workspaces.getModelObjectAsString()
+                                        + " huh? Well, now go into the code and actually implement the 'add layer' workflow then!"));
+                popupWindow.show(target);
+            }
+        });
+        return workspaces;
+    }
+
+    private AjaxButton filterResetButton() {
+        return new AjaxButton("resetFilter") {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                stores.setKeywords(null);
+                filter.setModelObject("");
+                dataView.setCurrentPage(0);
+                target.addComponent(tableContainer);
+                target.addComponent(navigator);
+                target.addComponent(filter);
+            }
+
+        };
+    }
+
+    private AjaxButton filterSubmitButton() {
+        return new AjaxButton("applyFilter") {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                String[] keywords = filter.getModelObjectAsString().split("\\s+");
+                stores.setKeywords(keywords);
+                dataView.setCurrentPage(0);
+                target.addComponent(tableContainer);
+                target.addComponent(navigator);
+            }
+
+        };
+    }
+
+    private final class StoreIconModel extends Model {
+        private final IModel model;
+
+        private StoreIconModel(IModel model) {
+            this.model = model;
+        }
+
+        @Override
+        public Object getObject() {
+            StoreInfo li = (StoreInfo) model.getObject();
+            if (li instanceof DataStoreInfo)
+                return "vector";
+            else
+                return "raster";
+        }
+    }
+
+    private AjaxLink storeNameLink(final IModel model) {
+        return new AjaxLink("nameLink", new PropertyModel(model, NAME_PROPERTY)) {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                String storeName = getModelObjectAsString();
+                DataStoreInfo store = getCatalog().getDataStoreByName(storeName);
+                if (store != null)
+                    setResponsePage(new DataStoreConfiguration(store.getId()));
+                else {
+                    popupWindow.setContent(new Label(popupWindow.getContentId(),
+                            "Sorry bud, no coverage store editor so far"));
+                    popupWindow.show(target);
+                }
+            }
+        };
+    }
+
+    private Link workspaceLink(PropertyModel wsProperty) {
+        return new Link("wsLink", wsProperty) {
+            public void onClick() {
+                WorkspaceInfo info = getCatalog().getWorkspaceByName(getModelObjectAsString());
+                if (info != null)
+                    setResponsePage(new NamespaceEditPage(info.getId()));
+            }
+        };
+    }
+
+    private AjaxCheckBox selectionCheckBox(final IModel model) {
+        return new AjaxCheckBox("selected", new Model() {
+            @Override
+            public Object getObject() {
+                StoreInfo si = (StoreInfo) model.getObject();
+                return selection.contains(si.getName());
+            }
+        }) {
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                StoreInfo si = (StoreInfo) model.getObject();
+                if (selection.contains(si.getName()))
+                    selection.remove(si.getName());
+                else
+                    selection.add(si.getName());
+                target.addComponent(tableContainer);
+
+                // update the remove link, enabled, only if there
+                // is some selection
+                removeLink.setEnabled(selection.size() > 0);
+                target.addComponent(removeLink);
+            }
+        };
+    }
+
+    protected class WorkspacesModel extends LoadableDetachableModel {
+        @Override
+        protected Object load() {
+            List<WorkspaceInfo> workspaces = getCatalog().getWorkspaces();
+            List<String> wsNames = new ArrayList<String>();
+            for (WorkspaceInfo store : workspaces) {
+                wsNames.add(store.getName());
+            }
+            return wsNames;
+        }
     }
 }
