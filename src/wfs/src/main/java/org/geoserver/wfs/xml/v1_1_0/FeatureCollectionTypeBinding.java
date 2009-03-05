78420b38b1200d1d0657759452e770b9e46ef4cb
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-05 19:06:21 +0000

    GEOS-2583, Generate feature collection bounds in wfs 1.1 output when feature bounding is enabled
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@11614 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: dad47ec60779641fc21f2a6956d78840ff62e1f3 [formerly 6ca18237a92c7b00bb2a527254df41e7e93e1307]
    Former-commit-id: b510aaaea8f2be45c766618635b990eb491c5b1d

--- src/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/FeatureCollectionTypeBinding.java
+++ src/wfs/src/main/java/org/geoserver/wfs/xml/v1_1_0/FeatureCollectionTypeBinding.java
@@ -4,15 +4,22 @@
  */
 package org.geoserver.wfs.xml.v1_1_0;
 
+import java.util.Iterator;
+
+import javax.xml.namespace.QName;
+
 import net.opengis.wfs.FeatureCollectionType;
 import net.opengis.wfs.WfsFactory;
+
 import org.geoserver.feature.CompositeFeatureCollection;
 import org.geotools.feature.FeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.gml3.GML;
+import org.geotools.gml3.GMLConfiguration;
 import org.geotools.xml.AbstractComplexEMFBinding;
+import org.geotools.xml.Configuration;
 import org.geotools.xml.ElementInstance;
 import org.geotools.xml.Node;
-import javax.xml.namespace.QName;
 
 
 /**
@@ -76,9 +83,11 @@ import javax.xml.namespace.QName;
  */
 public class FeatureCollectionTypeBinding extends AbstractComplexEMFBinding {
     WfsFactory wfsfactory;
+    boolean generateBounds;
 
-    public FeatureCollectionTypeBinding(WfsFactory wfsfactory) {
+    public FeatureCollectionTypeBinding(WfsFactory wfsfactory, Configuration configuration) {
         this.wfsfactory = wfsfactory;
+        this.generateBounds = !configuration.getProperties().contains(GMLConfiguration.NO_FEATURE_BOUNDS);
     }
 
     public int getExecutionMode() {
@@ -128,6 +137,19 @@ public class FeatureCollectionTypeBinding extends AbstractComplexEMFBinding {
                 //just return the single
                 return featureCollection.getFeature().iterator().next();
             }
+        } else if(GML.boundedBy.equals(name) && generateBounds) {
+        	FeatureCollectionType featureCollection = (FeatureCollectionType) object;
+
+        	ReferencedEnvelope env = null;
+        	for (Iterator it = featureCollection.getFeature().iterator(); it.hasNext();) {
+				FeatureCollection fc = (FeatureCollection) it.next();
+				if(env == null)
+					env = fc.getBounds();
+				else
+					env.expandToInclude(fc.getBounds());
+				
+			}
+        	return env;
         }
 
         //delegate to parent lookup
