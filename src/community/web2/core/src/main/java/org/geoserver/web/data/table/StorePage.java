a8c3bb199cd7ae0aba00dce669a16bcfc5ccb398
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-11 16:13:51 +0000

    Switched store page to use the new sortable/pageable/filterable table facilities
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11664 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7e06596465152af34f8573449b33eaee312fa877 [formerly b50bf7235d6977f93d81e2bbc0e65146e60b5fee]
    Former-commit-id: 93a49d6e3556b79ceb1cf500c3eff74381a33815

--- src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
@@ -4,163 +4,68 @@
  */
 package org.geoserver.web.data.table;
 
-import static org.geoserver.web.data.table.StoreProvider.ENABLED_PROPERTY;
-import static org.geoserver.web.data.table.StoreProvider.NAME_PROPERTY;
-import static org.geoserver.web.data.table.StoreProvider.WORKSPACE_PROPERTY;
+import static org.geoserver.web.data.table.StoreProvider.ENABLED;
+import static org.geoserver.web.data.table.StoreProvider.NAME;
+import static org.geoserver.web.data.table.StoreProvider.TYPE;
+import static org.geoserver.web.data.table.StoreProvider.WORKSPACE;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
-import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
-import org.apache.wicket.extensions.markup.html.repeater.data.sort.OrderByBorder;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
-import org.geoserver.web.GeoServerBasePage;
+import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.data.NamespaceEditPage;
 import org.geoserver.web.data.NewDataPage;
 import org.geoserver.web.data.datastore.DataStoreConfiguration;
-import org.geoserver.web.wicket.GeoServerPagingNavigator;
+import org.geoserver.web.data.table.GSDataProvider.Property;
 
-public class StorePage extends GeoServerBasePage {
-    TextField filter;
-
-    Label matched;
-
-    StoreProvider stores = new StoreProvider();
-
-    Set<String> selection = new HashSet<String>();
-
-    GeoServerPagingNavigator navigator;
-
-    DataView dataView;
-
-    private ModalWindow popupWindow;
-
-    private WebMarkupContainer tableContainer;
-
-    private AjaxLink removeLink;
+/**
+ * Page listing all the available stores. Follows the usual filter/sort/page approach,
+ * provides ways to bulk delete stores and to add new ones
+ * @author Andrea Aime - OpenGeo
+ */
+public class StorePage extends GeoServerSecuredPage {
+    StoreProvider provider = new StoreProvider();
+    ModalWindow popupWindow;
+    
 
     public StorePage() {
-        // the popup window used for messages
+        // the popup window for messages
         popupWindow = new ModalWindow("popupWindow");
         add(popupWindow);
+        
+        add(new GSTablePanel<StoreInfo>("table", provider) {
 
-        // the container for the table (for ajax updates)
-        tableContainer = new WebMarkupContainer("listContainer");
-
-        // build the filter form
-        Form form = new Form("filterForm");
-        add(form);
-        form.add(filter = new TextField("filter", new Model()));
-        filter.setOutputMarkupId(true);
-        form.add(filterSubmitButton());
-        form.add(filterResetButton());
-
-        // add the filter match label
-        add(matched = new Label("filterMatch"));
-        matched.setVisible(false);
-
-        // add the link for removing stores
-        removeLink = removeLink();
-        removeLink.setEnabled(false);
-        add(removeLink);
-
+            @Override
+            protected Component getComponentForProperty(String id, IModel itemModel,
+                    Property<StoreInfo> property) {
+                if(property == TYPE) {
+                    return new Label(id, TYPE.getModel(itemModel));
+                } else if(property == WORKSPACE) {
+                    return workspaceLink(id, itemModel);
+                } else if(property == NAME) {
+                    return storeNameLink(id, itemModel);
+                } else if(property == ENABLED) {
+                    return new Label(id, ENABLED.getModel(itemModel));
+                } 
+                throw new IllegalArgumentException("Don't know a property named " + property.getName());
+            }
+            
+        });
+        
         // the workspaces drop down
-        final DropDownChoice workspaces = workspacesDropDown();
-        add(workspaces);
-
-        // setup the table
-        tableContainer.setOutputMarkupId(true);
-        add(tableContainer);
-        dataView = new DataView("storesList", stores) {
-
-            @Override
-            protected void populateItem(Item item) {
-                final IModel model = item.getModel();
-
-                // odd/even style
-                item.add(new SimpleAttributeModifier("class", item.getIndex() % 2 == 0 ? "even"
-                        : "odd"));
-
-                // build an indirection so that we don't store the actual layer
-                // but just the model the item is using, which is detachable
-                final AjaxCheckBox selected = selectionCheckBox(model);
-                item.add(selected);
-
-                // the store icon (vector/raster)
-                Label type = new Label("type", new StoreIconModel(model));
-                item.add(type);
-
-                // the workspace link
-                PropertyModel wsProperty = new PropertyModel(model, WORKSPACE_PROPERTY);
-                Link wsLink = workspaceLink(wsProperty);
-                item.add(wsLink);
-                wsLink.add(new Label("ws", wsProperty));
-
-                // the store name link
-                AjaxLink nameLink = storeNameLink(model);
-                item.add(nameLink);
-                nameLink.add(new Label("name", new PropertyModel(model, NAME_PROPERTY)));
-
-                // is the store enabled?
-                item.add(new Label("enabled", new PropertyModel(model, ENABLED_PROPERTY)));
-            }
-
-        };
-        tableContainer.add(dataView);
-
-        // add the sorting links
-        add(new OrderByBorder("orderType", "type", stores));
-        add(new OrderByBorder("orderWs", "workspace", stores));
-        add(new OrderByBorder("orderName", "name", stores));
-        add(new OrderByBorder("orderEnabled", "enabled", stores));
-
-        // add the paging navigator
-        dataView.setItemsPerPage(10);
-        navigator = new GeoServerPagingNavigator("navigator", dataView);
-        navigator.setOutputMarkupId(true);
-        add(navigator);
-    }
-
-    private AjaxLink removeLink() {
-        return new AjaxLink("remove") {
-
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                String content = popupWindow.getContentId();
-                if (selection.isEmpty()) {
-                    popupWindow.setContent(new Label(content, "Selection is empty, dude!"));
-                    popupWindow.show(target);
-                } else {
-                    String msg = "Ok, so you wanted to remove " + selection
-                            + " uh? Well, you have some code to implement before that!!";
-                    popupWindow.setContent(new Label(content, msg));
-                    popupWindow.show(target);
-                }
-            }
-
-        };
+        add(workspacesDropDown());
     }
 
     private DropDownChoice workspacesDropDown() {
@@ -177,56 +82,9 @@ public class StorePage extends GeoServerBasePage {
         return workspaces;
     }
 
-    private AjaxButton filterResetButton() {
-        return new AjaxButton("resetFilter") {
 
-            @Override
-            protected void onSubmit(AjaxRequestTarget target, Form form) {
-                stores.setKeywords(null);
-                filter.setModelObject("");
-                dataView.setCurrentPage(0);
-                target.addComponent(tableContainer);
-                target.addComponent(navigator);
-                target.addComponent(filter);
-            }
-
-        };
-    }
-
-    private AjaxButton filterSubmitButton() {
-        return new AjaxButton("applyFilter") {
-
-            @Override
-            protected void onSubmit(AjaxRequestTarget target, Form form) {
-                String[] keywords = filter.getModelObjectAsString().split("\\s+");
-                stores.setKeywords(keywords);
-                dataView.setCurrentPage(0);
-                target.addComponent(tableContainer);
-                target.addComponent(navigator);
-            }
-
-        };
-    }
-
-    private final class StoreIconModel extends Model {
-        private final IModel model;
-
-        private StoreIconModel(IModel model) {
-            this.model = model;
-        }
-
-        @Override
-        public Object getObject() {
-            StoreInfo li = (StoreInfo) model.getObject();
-            if (li instanceof DataStoreInfo)
-                return "vector";
-            else
-                return "raster";
-        }
-    }
-
-    private AjaxLink storeNameLink(final IModel model) {
-        return new AjaxLink("nameLink", new PropertyModel(model, NAME_PROPERTY)) {
+    private Component storeNameLink(String id, final IModel itemModel) {
+        return new SimpleAjaxLink(id, NAME.getModel(itemModel)) {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 String storeName = getModelObjectAsString();
@@ -242,37 +100,12 @@ public class StorePage extends GeoServerBasePage {
         };
     }
 
-    private Link workspaceLink(PropertyModel wsProperty) {
-        return new Link("wsLink", wsProperty) {
-            public void onClick() {
+    private Component workspaceLink(String id, IModel itemModel) {
+        return new SimpleAjaxLink(id,WORKSPACE.getModel(itemModel)) {
+            public void onClick(AjaxRequestTarget target) {
                 WorkspaceInfo info = getCatalog().getWorkspaceByName(getModelObjectAsString());
                 if (info != null)
                     setResponsePage(new NamespaceEditPage(info.getId()));
-            }
-        };
-    }
-
-    private AjaxCheckBox selectionCheckBox(final IModel model) {
-        return new AjaxCheckBox("selected", new Model() {
-            @Override
-            public Object getObject() {
-                StoreInfo si = (StoreInfo) model.getObject();
-                return selection.contains(si.getName());
-            }
-        }) {
-            @Override
-            protected void onUpdate(AjaxRequestTarget target) {
-                StoreInfo si = (StoreInfo) model.getObject();
-                if (selection.contains(si.getName()))
-                    selection.remove(si.getName());
-                else
-                    selection.add(si.getName());
-                target.addComponent(tableContainer);
-
-                // update the remove link, enabled, only if there
-                // is some selection
-                removeLink.setEnabled(selection.size() > 0);
-                target.addComponent(removeLink);
             }
         };
     }
