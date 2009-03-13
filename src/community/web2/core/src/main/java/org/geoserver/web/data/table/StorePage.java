729366472e4749458f29b822138ce261862e31cf
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-13 19:02:03 +0000

    Propert formatting, linking to coverage store editor
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11686 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 52b928252f7d9bd27aee0352f6e5ea9fd7ec4798 [formerly 850b9b977f8e4deb96b97abba06967230fca4046]
    Former-commit-id: f631132c5d668c8a031b9146520d034668efdb40

--- src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
@@ -6,9 +6,6 @@ package org.geoserver.web.data.table;
 
 import static org.geoserver.web.data.table.StoreProvider.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
@@ -16,127 +13,133 @@ import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
 import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.data.NamespaceEditPage;
 import org.geoserver.web.data.NewDataPage;
+import org.geoserver.web.data.coverage.CoverageStoreConfiguration;
 import org.geoserver.web.data.datastore.DataStoreConfiguration;
 import org.geoserver.web.wicket.ConfirmationAjaxLink;
 import org.geoserver.web.wicket.GSTablePanel;
 import org.geoserver.web.wicket.SimpleAjaxLink;
+import org.geoserver.web.wicket.WorkspaceChoice;
 import org.geoserver.web.wicket.GSDataProvider.Property;
 
 /**
- * Page listing all the available stores. Follows the usual filter/sort/page approach,
- * provides ways to bulk delete stores and to add new ones
+ * Page listing all the available stores. Follows the usual filter/sort/page
+ * approach, provides ways to bulk delete stores and to add new ones
+ * 
  * @author Andrea Aime - OpenGeo
  */
 public class StorePage extends GeoServerSecuredPage {
     StoreProvider provider = new StoreProvider();
+
     ModalWindow popupWindow;
+
     GSTablePanel<StoreInfo> table;
 
     public StorePage() {
         // the popup window for messages
         popupWindow = new ModalWindow("popupWindow");
         add(popupWindow);
-        
+
         table = new GSTablePanel<StoreInfo>("table", provider) {
 
             @Override
-            protected Component getComponentForProperty(String id, IModel itemModel,
-                    Property<StoreInfo> property) {
-                if(property == TYPE) {
+            protected Component getComponentForProperty(String id,
+                    IModel itemModel, Property<StoreInfo> property) {
+                if (property == TYPE) {
                     return new Label(id, TYPE.getModel(itemModel));
-                } else if(property == WORKSPACE) {
+                } else if (property == WORKSPACE) {
                     return workspaceLink(id, itemModel);
-                } else if(property == NAME) {
+                } else if (property == NAME) {
                     return storeNameLink(id, itemModel);
-                } else if(property == ENABLED) {
+                } else if (property == ENABLED) {
                     return new Label(id, ENABLED.getModel(itemModel));
-                } else if(property == REMOVE) {
+                } else if (property == REMOVE) {
                     return removeLink(id, itemModel);
                 }
-                throw new IllegalArgumentException("Don't know a property named " + property.getName());
+                throw new IllegalArgumentException(
+                        "Don't know a property named " + property.getName());
             }
-            
+
         };
         table.setOutputMarkupId(true);
         add(table);
-        
+
         // the workspaces drop down
         add(workspacesDropDown());
     }
 
     private DropDownChoice workspacesDropDown() {
         final DropDownChoice workspaces;
-        workspaces = new DropDownChoice("wsDropDown", new Model(), new WorkspacesModel());
+        workspaces = new WorkspaceChoice("wsDropDown", new Model(null));
         workspaces.add(new AjaxFormComponentUpdatingBehavior("onchange") {
 
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
-                String wsName = workspaces.getModelObjectAsString();
-                setResponsePage(new NewDataPage(wsName));
+                WorkspaceInfo ws = (WorkspaceInfo) workspaces.getModelObject();
+                setResponsePage(new NewDataPage(ws.getName()));
             }
         });
         return workspaces;
     }
-
 
     private Component storeNameLink(String id, final IModel itemModel) {
         return new SimpleAjaxLink(id, NAME.getModel(itemModel)) {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 String storeName = getLink().getModelObjectAsString();
-                DataStoreInfo store = getCatalog().getDataStoreByName(storeName);
-                if (store != null)
-                    setResponsePage(new DataStoreConfiguration(store.getId()));
-                else {
-                    popupWindow.setContent(new Label(popupWindow.getContentId(),
-                            "Sorry bud, no coverage store editor so far"));
+                StoreInfo store = getCatalog().getStoreByName(storeName,
+                        StoreInfo.class);
+                if (store == null) {
+                    popupWindow.setContent(new Label(
+                            popupWindow.getContentId(),
+                            "Cannot find the store " + store
+                                    + " anymore, I guess it has been removed. "
+                                    + "Will refresh the store list."));
                     popupWindow.show(target);
+                    target.addComponent(table);
+                } else if (store instanceof DataStoreInfo) {
+                    setResponsePage(new DataStoreConfiguration(store.getId()));
+                } else {
+                    setResponsePage(new CoverageStoreConfiguration(store
+                            .getId()));
                 }
             }
         };
     }
 
     private Component workspaceLink(String id, IModel itemModel) {
-        return new SimpleAjaxLink(id,WORKSPACE.getModel(itemModel)) {
+        return new SimpleAjaxLink(id, WORKSPACE.getModel(itemModel)) {
             public void onClick(AjaxRequestTarget target) {
-                WorkspaceInfo info = getCatalog().getWorkspaceByName(getModelObjectAsString());
+                WorkspaceInfo info = getCatalog().getWorkspaceByName(
+                        getModelObjectAsString());
                 if (info != null)
                     setResponsePage(new NamespaceEditPage(info.getId()));
             }
         };
     }
-    
+
     protected Component removeLink(String id, final IModel itemModel) {
         StoreInfo info = (StoreInfo) itemModel.getObject();
         // TODO: i18n this!
-        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(id, null, new Model("remove"),
-                new Model("About to remove \"" + info.getName() + "\". Are you sure? All contained layers will be removed as well")) {
+        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(
+                id,
+                null,
+                new Model("remove"),
+                new Model(
+                        "About to remove \""
+                                + info.getName()
+                                + "\". Are you sure? All contained layers will be removed as well")) {
             public void onClick(AjaxRequestTarget target) {
                 getCatalog().remove((StoreInfo) itemModel.getObject());
                 target.addComponent(table);
             }
         };
         return linkPanel;
-    }
-
-    protected class WorkspacesModel extends LoadableDetachableModel {
-        @Override
-        protected Object load() {
-            List<WorkspaceInfo> workspaces = getCatalog().getWorkspaces();
-            List<String> wsNames = new ArrayList<String>();
-            for (WorkspaceInfo store : workspaces) {
-                wsNames.add(store.getName());
-            }
-            return wsNames;
-        }
     }
 }
