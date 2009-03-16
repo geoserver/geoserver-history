aff74bf75ab4ec2ba652f34a01fbb4c964d20e37
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-16 12:08:17 +0000

    Fixed imports
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11695 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0d108de7d28c2b46905f45314db38eb9aaf27c58 [formerly 4afe9eeed56ab46d56b7c377f2e515fd1303dd90]
    Former-commit-id: c83f05f6bdb1533c817a338b1df26ea35a290066

--- src/community/web2/core/src/main/java/org/geoserver/web/wicket/GSDataProvider.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/wicket/GSDataProvider.java
@@ -17,7 +17,6 @@ import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.web.GeoServerApplication;
@@ -159,7 +158,7 @@ public abstract class GSDataProvider<T> extends SortableDataProvider {
      * @return
      */
     protected abstract List<T> getItems();
-
+    
     /**
      * Returns a comparator given the sort property.
      * 
