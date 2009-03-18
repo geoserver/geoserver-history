cc591e742997b5d048f85ee2ccd2120ca119f611
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-18 17:25:58 +0000

    GEOS-2154, pages for namespace/workspace browsing and editing
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11731 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e946acf9ee160d8346f3c13822c801299d2b049e [formerly 8015cd5d0c5dec6f65984189acc2ba9cbe854232]
    Former-commit-id: eae1cebea931fffef3f179c73c4b7d5d93185bbf

---
+++ community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspaceEditPage.java
@@ -0,0 +1,56 @@
+package org.geoserver.web.data.workspace;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.NamespaceInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.web.GeoServerSecuredPage;
+import org.geoserver.web.data.table.StorePanel;
+import org.geoserver.web.data.table.StoreProvider;
+
+public class WorkspaceEditPage extends GeoServerSecuredPage {
+
+    IModel wsModel;
+    IModel nsModel;
+    
+    public WorkspaceEditPage( WorkspaceInfo ws ) {
+        
+        wsModel = new WorkspaceDetachableModel( ws );
+        add( new Label("name", new PropertyModel( wsModel , "name" ) ) );
+
+        NamespaceInfo ns = getCatalog().getNamespaceByPrefix( ws.getName() );
+        nsModel = new NamespaceDetachableModel(ns);
+        
+        Form form = new Form( "form", new CompoundPropertyModel( nsModel ) ) {
+            protected void onSubmit() {
+                Catalog catalog = getCatalog();
+                catalog.save( (NamespaceInfo) nsModel.getObject() );
+                catalog.save( (WorkspaceInfo) wsModel.getObject() );
+            }
+        };
+        add(form);
+        form.add(new TextField("uRI"));
+        
+        //stores
+        StorePanel storePanel = new StorePanel( "storeTable", new StoreProvider( ws ) );
+        form.add(storePanel);
+        
+        form.add(new SubmitLink("save"){
+            @Override
+            public void onSubmit() {
+                super.onSubmit();
+            }
+        });
+        form.add(new BookmarkablePageLink("cancel", WorkspacePage.class));
+        
+     
+    }
+    
+}
---
+++ community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspaceProvider.java
@@ -0,0 +1,34 @@
+package org.geoserver.web.data.workspace;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.wicket.model.IModel;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.web.wicket.GeoServerDataProvider;
+
+public class WorkspaceProvider extends GeoServerDataProvider<WorkspaceInfo> {
+
+    public static Property<WorkspaceInfo> NAME = 
+        new BeanProperty<WorkspaceInfo>( "name", "name" );
+
+    public static Property<WorkspaceInfo> REMOVE = 
+        new PropertyPlaceholder<WorkspaceInfo>( "remove" );
+
+    static List PROPERTIES = Arrays.asList( NAME, REMOVE );
+   
+    @Override
+    protected List<WorkspaceInfo> getItems() {
+        return getCatalog().getWorkspaces();
+    }
+
+    @Override
+    protected List<Property<WorkspaceInfo>> getProperties() {
+        return PROPERTIES;
+    }
+
+    public IModel model(Object obj) {
+        return new WorkspaceDetachableModel( (WorkspaceInfo) obj );
+    }
+
+}
