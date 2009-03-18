dd308fab5698ba5966ac6b0415431f6a9c501b3e
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-18 17:12:35 +0000

    GEOS-2765, factored out store page into a re-usable panel
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11729 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: df92487254708196c317f8a2b09ae98264c6422f [formerly edf1fe8b44b0ce8ad2aabd68736a5e34c3c95fc8]
    Former-commit-id: 81c2f8fe6737ef6ea3c9e63d5b42a63f5b3835d2

--- src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
@@ -37,36 +37,10 @@ import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 public class StorePage extends GeoServerSecuredPage {
     StoreProvider provider = new StoreProvider();
 
-    ModalWindow popupWindow;
-
-    GeoServerTablePanel<StoreInfo> table;
+    StorePanel table;
 
     public StorePage() {
-        // the popup window for messages
-        popupWindow = new ModalWindow("popupWindow");
-        add(popupWindow);
-
-        table = new GeoServerTablePanel<StoreInfo>("table", provider) {
-
-            @Override
-            protected Component getComponentForProperty(String id,
-                    IModel itemModel, Property<StoreInfo> property) {
-                if (property == TYPE) {
-                    return new Label(id, TYPE.getModel(itemModel));
-                } else if (property == WORKSPACE) {
-                    return workspaceLink(id, itemModel);
-                } else if (property == NAME) {
-                    return storeNameLink(id, itemModel);
-                } else if (property == ENABLED) {
-                    return new Label(id, ENABLED.getModel(itemModel));
-                } else if (property == REMOVE) {
-                    return removeLink(id, itemModel);
-                }
-                throw new IllegalArgumentException(
-                        "Don't know a property named " + property.getName());
-            }
-
-        };
+        table = new StorePanel( "table", provider );
         table.setOutputMarkupId(true);
         add(table);
 
@@ -88,60 +62,5 @@ public class StorePage extends GeoServerSecuredPage {
             }
         });
         return workspaces;
-    }
-
-    private Component storeNameLink(String id, final IModel itemModel) {
-        return new SimpleAjaxLink(id, NAME.getModel(itemModel)) {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                String storeName = getLink().getModelObjectAsString();
-                StoreInfo store = getCatalog().getStoreByName(storeName,
-                        StoreInfo.class);
-                if (store == null) {
-                    popupWindow.setContent(new Label(
-                            popupWindow.getContentId(),
-                            "Cannot find the store " + store
-                                    + " anymore, I guess it has been removed. "
-                                    + "Will refresh the store list."));
-                    popupWindow.show(target);
-                    target.addComponent(table);
-                } else if (store instanceof DataStoreInfo) {
-                    setResponsePage(new DataStoreConfiguration(store.getId()));
-                } else {
-                    setResponsePage(new CoverageStoreConfiguration(store
-                            .getId()));
-                }
-            }
-        };
-    }
-
-    private Component workspaceLink(String id, IModel itemModel) {
-        return new SimpleAjaxLink(id, WORKSPACE.getModel(itemModel)) {
-            public void onClick(AjaxRequestTarget target) {
-                WorkspaceInfo info = getCatalog().getWorkspaceByName(
-                        getModelObjectAsString());
-                if (info != null)
-                    setResponsePage(new NamespaceEditPage(info.getId()));
-            }
-        };
-    }
-
-    protected Component removeLink(String id, final IModel itemModel) {
-        StoreInfo info = (StoreInfo) itemModel.getObject();
-        // TODO: i18n this!
-        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(
-                id,
-                null,
-                new Model("remove"),
-                new Model(
-                        "About to remove \""
-                                + info.getName()
-                                + "\". Are you sure? All contained layers will be removed as well")) {
-            public void onClick(AjaxRequestTarget target) {
-                getCatalog().remove((StoreInfo) itemModel.getObject());
-                target.addComponent(table);
-            }
-        };
-        return linkPanel;
     }
 }
---
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/table/StorePanel.java
@@ -0,0 +1,120 @@
+package org.geoserver.web.data.table;
+
+import static org.geoserver.web.data.table.StoreProvider.ENABLED;
+import static org.geoserver.web.data.table.StoreProvider.NAME;
+import static org.geoserver.web.data.table.StoreProvider.REMOVE;
+import static org.geoserver.web.data.table.StoreProvider.TYPE;
+import static org.geoserver.web.data.table.StoreProvider.WORKSPACE;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.catalog.StoreInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.web.GeoServerApplication;
+import org.geoserver.web.data.NamespaceEditPage;
+import org.geoserver.web.data.coverage.CoverageStoreConfiguration;
+import org.geoserver.web.data.datastore.DataStoreConfiguration;
+import org.geoserver.web.wicket.ConfirmationAjaxLink;
+import org.geoserver.web.wicket.GeoServerTablePanel;
+import org.geoserver.web.wicket.SimpleAjaxLink;
+import org.geoserver.web.wicket.GeoServerDataProvider.Property;
+
+public class StorePanel extends GeoServerTablePanel<StoreInfo> {
+    
+    ModalWindow popupWindow;
+
+    public StorePanel( String id ) {
+        this( id, new StoreProvider() );
+    }
+    
+    public StorePanel( String id, StoreProvider provider ) {
+        super(id, provider);
+        
+        // the popup window for messages
+        popupWindow = new ModalWindow("popupWindow");
+        add(popupWindow);
+    }
+    
+    private Catalog getCatalog() {
+        return GeoServerApplication.get().getCatalog();
+    }
+    
+    @Override
+    protected Component getComponentForProperty(String id,
+            IModel itemModel, Property<StoreInfo> property) {
+        if (property == TYPE) {
+            return new Label(id, TYPE.getModel(itemModel));
+        } else if (property == WORKSPACE) {
+            return workspaceLink(id, itemModel);
+        } else if (property == NAME) {
+            return storeNameLink(id, itemModel);
+        } else if (property == ENABLED) {
+            return new Label(id, ENABLED.getModel(itemModel));
+        } else if (property == REMOVE) {
+            return removeLink(id, itemModel);
+        }
+        throw new IllegalArgumentException(
+                "Don't know a property named " + property.getName());
+    }
+
+    private Component storeNameLink(String id, final IModel itemModel) {
+        return new SimpleAjaxLink(id, NAME.getModel(itemModel)) {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                String storeName = getLink().getModelObjectAsString();
+                StoreInfo store = getCatalog().getStoreByName(storeName,
+                        StoreInfo.class);
+                if (store == null) {
+                    popupWindow.setContent(new Label(
+                            popupWindow.getContentId(),
+                            "Cannot find the store " + store
+                                    + " anymore, I guess it has been removed. "
+                                    + "Will refresh the store list."));
+                    popupWindow.show(target);
+                    target.addComponent(StorePanel.this);
+                } else if (store instanceof DataStoreInfo) {
+                    setResponsePage(new DataStoreConfiguration(store.getId()));
+                } else {
+                    setResponsePage(new CoverageStoreConfiguration(store
+                            .getId()));
+                }
+            }
+        };
+    }
+
+    private Component workspaceLink(String id, IModel itemModel) {
+        return new SimpleAjaxLink(id, WORKSPACE.getModel(itemModel)) {
+            public void onClick(AjaxRequestTarget target) {
+                WorkspaceInfo info = getCatalog().getWorkspaceByName(
+                        getModelObjectAsString());
+                if (info != null)
+                    setResponsePage(new NamespaceEditPage(info.getId()));
+            }
+        };
+    }
+    
+    protected Component removeLink(String id, final IModel itemModel) {
+        StoreInfo info = (StoreInfo) itemModel.getObject();
+        // TODO: i18n this!
+        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(
+                id,
+                null,
+                new Model("remove"),
+                new Model(
+                        "About to remove \""
+                                + info.getName()
+                                + "\". Are you sure? All contained layers will be removed as well")) {
+            public void onClick(AjaxRequestTarget target) {
+                getCatalog().remove((StoreInfo) itemModel.getObject());
+                target.addComponent(StorePanel.this);
+            }
+        };
+        return linkPanel;
+    }
+}
