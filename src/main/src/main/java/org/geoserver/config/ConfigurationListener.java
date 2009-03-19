3f365726d0f6ba47f78ceabb268e5c8c238ab254
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-19 02:57:30 +0000

    added logging config events, and a post service change event
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11740 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a9d6153ceb0d2538ce642fb56eaf4f713fde0b3f [formerly 68e77e024bb181cb18f6533a81f719996c954c25]
    Former-commit-id: 2b3f4d325b6f0ef361d3d84a9e5dd1407731bdd7

--- src/main/src/main/java/org/geoserver/config/ConfigurationListener.java
+++ src/main/src/main/java/org/geoserver/config/ConfigurationListener.java
@@ -23,6 +23,28 @@ public interface ConfigurationListener {
     void handleGlobalChange( GeoServerInfo global, List<String> propertyNames, List<Object> oldValues, List<Object> newValues );
     
     /**
+     * Handles the event fired post change to global configuration. 
+     * 
+     * @param global The global config object.
+     */
+    void handlePostGlobalChange( GeoServerInfo global );
+    
+    /**
+     * Handles a change to the logging configuration.
+     * 
+     * @param logging The logging config object.
+     * @param propertyNames The names of the properties that were changed.
+     * @param oldValue The old values for the properties that were changed.
+     * @param newValue The new values for the properties that were changed.
+     */
+    void handleLoggingChange( LoggingInfo logging, List<String> propertyNames, List<Object> oldValues, List<Object> newValues );
+
+    /**
+     * Handles the event fired post change to logging configuration. 
+     */
+    void handlePostLoggingChange( LoggingInfo logging );
+    
+    /**
      * Handles a change to a service configuration.
      * 
      * @param service The service config object.
@@ -31,6 +53,13 @@ public interface ConfigurationListener {
      * @param newValue The new values for the properties that were changed.
      */
     void handleServiceChange( ServiceInfo service, List<String> propertyNames, List<Object> oldValues, List<Object> newValues );
+    
+    /**
+     * Handles the event fired post change to service configuration.
+     * 
+     * @param service The service config object.
+     */
+    void handlePostServiceChange( ServiceInfo service );
     
     /**
      * A callback notifying when GeoServer configuration has been reloaded.
