b9a86d291a13bff65f4b3299133941c1eaa0602f
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-19 14:16:54 +0000

    Improve model usage
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11768 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1918830a3f49530383c9421e835546630ce4f9f1 [formerly 8efd326c2c23a7f604e4c5aba9a9fa274bbb0512]
    Former-commit-id: 687fa29006965cdd0097d9586d86b37b099db219

--- community/web2/core/src/main/java/org/geoserver/web/data/FeatureResourceConfigurationPanel.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/FeatureResourceConfigurationPanel.java
@@ -8,18 +8,18 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.AttributeTypeInfo;
 import org.geoserver.catalog.FeatureTypeInfo;
 
 @SuppressWarnings("serial")
 public class FeatureResourceConfigurationPanel extends ResourceConfigurationPanel {
 
-    public FeatureResourceConfigurationPanel(String id, IModel model) {
+    public FeatureResourceConfigurationPanel(String id, final IModel model) {
         super(id, model);
         
         // just use the direct attributes, this is not editable atm
-        final FeatureTypeInfo typeInfo = (FeatureTypeInfo) model.getObject();
-        ListView attributes = new ListView("attributes", typeInfo.getAttributes()) {
+        ListView attributes = new ListView("attributes", new PropertyModel(model, "attributes")) {
 
             @Override
             protected void populateItem(ListItem item) {
@@ -33,6 +33,8 @@ public class FeatureResourceConfigurationPanel extends ResourceConfigurationPane
                 item.add(new Label("name", attribute.getName()));
                 item.add(new Label("minmax", attribute.getMinOccurs() + "/" + attribute.getMaxOccurs()));
                 try {
+                    // working around a serialization issue
+                    FeatureTypeInfo typeInfo = (FeatureTypeInfo) model.getObject();
                     org.opengis.feature.type.PropertyDescriptor pd = typeInfo.getFeatureType().getDescriptor(attribute.getName());
                     String typeName = pd.getType().getBinding().getSimpleName();
 //                    String typeName = attribute.getAttribute().getType().getBinding().getSimpleName();
