a9ef1d569c06e42c6d1a1afa5150269e86b5bc28
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-19 11:39:57 +0000

    Use drop downs for regionation attribute and method, add validation to the number of features per tile
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11765 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ee9cb35c2d26efde3e9f5f5788b00c240172c0e4 [formerly afe18926fc294881b662483a093a9ad42469096b]
    Former-commit-id: 720f94e5aef0c463473b9778e6c3ebeb0b5fbdac

--- community/web2/wms/src/main/java/org/geoserver/wms/web/publish/KMLLayerConfigPanel.java
+++ community/web2/wms/src/main/java/org/geoserver/wms/web/publish/KMLLayerConfigPanel.java
@@ -4,25 +4,72 @@
  */
 package org.geoserver.wms.web.publish;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converters.IntegerConverter;
+import org.apache.wicket.validation.validator.NumberValidator;
+import org.geoserver.catalog.FeatureTypeInfo;
 import org.geoserver.web.publish.LayerConfigurationPanel;
 import org.geoserver.web.util.MapModel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.markup.html.form.TextField;
+import org.opengis.feature.type.PropertyDescriptor;
+
+import com.vividsolutions.jts.algorithm.MinimumDiameter;
 
 @SuppressWarnings("serial")
 public class KMLLayerConfigPanel extends LayerConfigurationPanel {
+    
+    /**
+     * TODO: replace this with a list coming from the KML regionation classes 
+     */
+    static final List<String> KML_STRATEGIES = Arrays.asList("external-sorting", "geometry", "native-sorting", "random");
+    
     public KMLLayerConfigPanel(String id, IModel model){
         super(id, model);
 
-        add(new TextField("kml.regionateAttribute", 
-                    new MapModel(new PropertyModel(model, "resource.metadata"), "kml.regionateAttribute"))
+        PropertyModel metadata = new PropertyModel(model, "resource.metadata");
+        add(new DropDownChoice("kml.regionateAttribute", 
+                new MapModel(metadata, "kml.regionateAttribute"), 
+                new AttributeNamesModel(new PropertyModel(model, "resource"))));
+        add(new DropDownChoice("kml.regionateStrategy", 
+                    new MapModel(metadata, "kml.regionateStrategy"), KML_STRATEGIES)
            );
-        add(new TextField("kml.regionateStrategy", 
-                    new MapModel(new PropertyModel(model, "resource.metadata"), "kml.regionateStrategy"))
-           );
-        add(new TextField("kml.regionateFeatureLimit",
-                    new MapModel(new PropertyModel(model, "resource.metadata"), "kml.regionateFeatureLimit"))
-           );
+        TextField maxFeatures = new TextField("kml.regionateFeatureLimit",
+                    new MapModel(metadata, "kml.regionateFeatureLimit"), Integer.class);
+        maxFeatures.add(NumberValidator.minimum(1));
+        add(maxFeatures);
+    }
+    
+    private static class AttributeNamesModel extends LoadableDetachableModel {
+        IModel featureTypeInfo;
+        
+        public AttributeNamesModel(IModel featureTypeInfo) {
+            this.featureTypeInfo = featureTypeInfo;
+        }
+
+        @Override
+        protected Object load() {
+            try {
+                FeatureTypeInfo fti = (FeatureTypeInfo) featureTypeInfo.getObject();
+                List<String> result = new ArrayList<String>();
+                for (PropertyDescriptor property :  fti.getFeatureType().getDescriptors()) {
+                    result.add(property.getName().getLocalPart());
+                }
+                Collections.sort(result);
+                return result;
+            } catch(IOException e) {
+                throw new RuntimeException("Could not load feature type attribute list", e);
+            }
+        }
+        
     }
 }
