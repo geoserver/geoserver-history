263321ec6ab7643cf94e72189b13a2ad13724d4e
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-19 17:54:13 +0000

    removed NamespaceEditPage, replaced with WorkspaceEditPage
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11775 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3bef49680197824172ee113f994558ab6111d954 [formerly 9c4ce8b02138cab5ca7d78e8e33ebf955dea4386]
    Former-commit-id: 5ba0b3441ae0d82050929e2c12b788a4879c4d9b

--- community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/table/StorePage.java
@@ -4,29 +4,14 @@
  */
 package org.geoserver.web.data.table;
 
-import static org.geoserver.web.data.table.StoreProvider.*;
-
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
-import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerSecuredPage;
-import org.geoserver.web.data.NamespaceEditPage;
 import org.geoserver.web.data.NewDataPage;
-import org.geoserver.web.data.coverage.CoverageStoreConfiguration;
-import org.geoserver.web.data.datastore.DataStoreConfiguration;
-import org.geoserver.web.wicket.ConfirmationAjaxLink;
-import org.geoserver.web.wicket.GeoServerTablePanel;
-import org.geoserver.web.wicket.SimpleAjaxLink;
 import org.geoserver.web.wicket.WorkspaceChoice;
-import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 
 /**
  * Page listing all the available stores. Follows the usual filter/sort/page
