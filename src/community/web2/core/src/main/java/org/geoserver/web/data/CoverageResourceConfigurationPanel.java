e396f8681c79cd46cd4a3147365ca3bcae2620c0
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-20 15:02:24 +0000

    Coverage specific param editor
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11786 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b6156c17885f56db9f092fdeb85690233bf2021a [formerly 0736ed8b8964d3153948e5daf7c8a77767e72d3e]
    Former-commit-id: 68a8758002f88e762bf39337c183a5fb73c134ab

---
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/CoverageResourceConfigurationPanel.java
@@ -0,0 +1,57 @@
+package org.geoserver.web.data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.geoserver.catalog.CoverageInfo;
+import org.geoserver.web.data.datastore.panel.ColorPickerPanel;
+import org.geoserver.web.data.datastore.panel.TextParamPanel;
+import org.geoserver.web.publish.LayerConfigurationPanel;
+import org.geoserver.web.util.MapModel;
+
+/**
+ * A configuration panel for CoverageInfo properties that related to WCS publication
+ * @author Andrea Aime - OpenGeo
+ *
+ */
+@SuppressWarnings("serial")
+public class CoverageResourceConfigurationPanel extends ResourceConfigurationPanel {
+
+    public CoverageResourceConfigurationPanel(String id, IModel model){
+        super(id, model);
+
+        final CoverageInfo coverage = (CoverageInfo) getResourceInfo();
+        final IModel paramsModel = new PropertyModel(getModel(), "parameters");
+        
+        List<String> keys = new ArrayList<String>(coverage.getParameters().keySet());
+        Collections.sort(keys);
+        ListView paramsList = new ListView("parameters", keys) {
+            
+            @Override
+            protected void populateItem(ListItem item) {
+                Component inputComponent = getInputComponent("parameterPanel", paramsModel, item.getModelObjectAsString());
+                item.add(inputComponent);
+            }
+        };
+        // needed for form components not to loose state
+        paramsList.setReuseItems(true);
+        add(paramsList);
+        
+        if(keys.size() == 0)
+            setVisible(false);
+   }
+    
+    private Component getInputComponent(String id,
+            IModel paramsModel, String keyName) {
+        if(keyName.contains("Color"))
+            return new ColorPickerPanel(id, new MapModel(paramsModel, keyName), keyName, false);
+        else
+            return new TextParamPanel(id, new MapModel(paramsModel, keyName), keyName, false);
+    }
+}
