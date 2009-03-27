3a76d4b6c45a44b882afa821fb59b834b8a73168
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-27 12:35:34 +0000

    Adding a test that we should run, but commented out because I cannot make it work (the same check done in the browser agains a live app works thought)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11882 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f459cc091868c1c4a233cb34e3b34789769521b9 [formerly ee12389842e526d44343635577e1eaab56852445]
    Former-commit-id: f1fb0e26208afc174e523ad0aa04d73d1885d13e

--- community/web2/core/src/test/java/org/geoserver/web/data/ResourceConfigurationPageTest.java
+++ community/web2/core/src/test/java/org/geoserver/web/data/ResourceConfigurationPageTest.java
@@ -1,17 +1,52 @@
 package org.geoserver.web.data;
 
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.tester.FormTester;
 import org.geoserver.web.GeoServerWicketTestSupport;
+import org.geoserver.web.publish.BasicLayerConfig;
 import org.geoserver.catalog.ResourceInfo;
 
 public class ResourceConfigurationPageTest extends GeoServerWicketTestSupport {
-    public void testValues() {
+    
+    public void testBasic() {
         ResourceInfo info = getGeoServerApplication()
             .getCatalog()
             .getResources(ResourceInfo.class).get(0);
 
         login();
         tester.startPage(new ResourceConfigurationPage(info, false));
-        tester.assertLabel("resourcename", info.getName());
+        tester.assertLabel("resourcename", info.getPrefixedName());
         tester.assertComponent("resource:tabs:panel:theList:0:content", BasicResourceConfig.class);
     }
+    
+    // I can't make the last assertion work, my wicket-fu is not good enough or else the
+    // 
+//    public void testTabSwitch() {
+//        ResourceInfo info = getGeoServerApplication()
+//            .getCatalog()
+//            .getResources(ResourceInfo.class).get(0);
+//
+//        login();
+//        tester.startPage(new ResourceConfigurationPage(info, false));
+//        FormTester ft = tester.newFormTester("resource");
+//        ft.setValue("tabs:panel:theList:0:content:title", "Some other title");
+//        ft.submit();
+//        assertEquals("Some other title", ft.getTextComponentValue("tabs:panel:theList:0:content:title"));
+//        tester.assertModelValue("resource:tabs:panel:theList:0:content:title", "Some other title");
+//        
+//        // switch to the other page
+//        tester.clickLink("resource:tabs:tabs-container:tabs:1:link");
+//        tester.assertRenderedPage(ResourceConfigurationPage.class);
+//        tester.assertComponent("resource:tabs:panel:theList:0:content", BasicLayerConfig.class);
+//        
+//        // switch back
+//        tester.clickLink("resource:tabs:tabs-container:tabs:0:link");
+//        tester.assertRenderedPage(ResourceConfigurationPage.class);
+//        tester.assertComponent("resource:tabs:panel:theList:0:content", BasicResourceConfig.class);
+//        
+//        // check the title is still what we did set (switching tabs did not make us loose the value)
+//        tester.assertComponent("resource:tabs:panel:theList:0:content:title", TextField.class);
+//        ft = tester.newFormTester("resource");
+//        tester.assertModelValue("resource:tabs:panel:theList:0:content:title", "Some other title");
+//    }
 }
