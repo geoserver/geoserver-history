617499e54b424e7baca7116e5139c88e1d61590a
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-27 13:59:26 +0000

    License, javadoc, warning about where it's not safe to use the model
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11885 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ca6ebbd13df3f81d5b81430f43fe85d23526d757 [formerly 21bafe09b94c37bdaab2706f122a69c460b9e07a]
    Former-commit-id: d727f73e7a72e1cf235fd1f5c4b78dd0068b0d18

--- community/web2/core/src/main/java/org/geoserver/web/data/table/LayerDetachableModel.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/table/LayerDetachableModel.java
@@ -1,9 +1,19 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web.data.table;
 
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.web.GeoServerApplication;
 
+/**
+ * A loadable model for layers. Warning, don't use it in a tabbed form
+ * or in any other places where you might need to keep the modifications
+ * in a resource stable across page loads.
+ */
+@SuppressWarnings("serial")
 public class LayerDetachableModel extends LoadableDetachableModel {
     String name;
 
