1917453ee7fe24cfe2d878265531512df60d0aa5
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-30 09:30:02 +0000

    Copyright headers, suppress serial warnings, add basic class javadocs
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11899 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4025e5c01efb67f7072f58323b7a5e8127d2b673 [formerly beab193c9f6acc80e80da0e7d5b12196cec9d9fd]
    Former-commit-id: 977a7cbb318dfb1b6ede89480e18d04ff283b93d

--- src/community/web2/wms/src/main/java/org/geoserver/wms/web/data/StyleProvider.java
+++ src/community/web2/wms/src/main/java/org/geoserver/wms/web/data/StyleProvider.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.wms.web.data;
 
 import java.util.Arrays;
@@ -8,6 +12,10 @@ import org.geoserver.catalog.StyleInfo;
 import org.geoserver.web.data.style.StyleDetachableModel;
 import org.geoserver.web.wicket.GeoServerDataProvider;
 
+/**
+ * A {@link GeoServerDataProvider} provider for styles
+ */
+@SuppressWarnings("serial")
 public class StyleProvider extends GeoServerDataProvider<StyleInfo> {
 
     public static Property<StyleInfo> NAME = 
