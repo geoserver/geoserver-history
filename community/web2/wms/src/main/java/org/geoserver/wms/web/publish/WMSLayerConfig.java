cd663268551b7c14773700d68ec18bcdad824d62
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-30 15:42:27 +0000

    Make the default style/extra style panels actually work and use efficient models, add a test form them
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11903 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3985aaa4260b840494318617748f149e12fa9df2 [formerly 2eacf0b0d09de94e70bee4f53157c6fb7be9c88c]
    Former-commit-id: 2bfb91adb03028a88a3ab80780bfae1a234a6ba4

--- community/web2/wms/src/main/java/org/geoserver/wms/web/publish/WMSLayerConfig.java
+++ community/web2/wms/src/main/java/org/geoserver/wms/web/publish/WMSLayerConfig.java
@@ -5,14 +5,15 @@
 package org.geoserver.wms.web.publish;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.wicket.extensions.markup.html.form.palette.Palette;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StyleInfo;
@@ -20,49 +21,58 @@ import org.geoserver.web.publish.LayerConfigurationPanel;
 
 /**
  * Configures {@link LayerInfo} WMS specific attributes 
- * @author Andrea Aime - OpenGeo
- *
  */
 @SuppressWarnings("serial")
 public class WMSLayerConfig extends LayerConfigurationPanel {
 
-    private List<String> extraStyles;
-    private List<String> availableStyles;
 
-    public WMSLayerConfig(String id, IModel model){
-        super(id, model);
-        extraStyles     = new ArrayList<String>();
-        for (StyleInfo info : getLayerInfo().getStyles()) extraStyles.add(info.getId());
-        availableStyles = listStyles();
-        availableStyles.removeAll(extraStyles);
-        availableStyles.remove(getLayerInfo().getDefaultStyle());
+    public WMSLayerConfig(String id, IModel layerModel){
+        super(id, layerModel);
 
-        StyleInfo defaultStyle = getLayerInfo().getDefaultStyle();
+        // default style chooser. A default style is required
+        StylesModel styles = new StylesModel();
+        DropDownChoice defaultStyle = new DropDownChoice("defaultStyle", 
+                    new PropertyModel(layerModel, "defaultStyle"), styles);
+        defaultStyle.setRequired(true);
+        add(defaultStyle);
 
-        add(new DropDownChoice("defaultStyle", 
-                    new Model(defaultStyle == null ? "No style selected" : defaultStyle.getId()),
-                    new PropertyModel(this, "availableStyles"))
-           );
-
+        // build a palette with no reordering allowed, since order doesn't affect anything
         add(new Palette("extraStyles",
-                    new PropertyModel(this, "extraStyles"),
-                    new PropertyModel(this, "availableStyles"),
-                    new ChoiceRenderer(),
-                    10,
-                    false // no reordering allowed since order doesn't affect anything
-                    )
+                new LayerStylesModel(layerModel), styles,
+                    new StyleNameRenderer(), 10, false)
            );
 
-        add(new TextField("wmsPath", new PropertyModel(model, "path")));
+        add(new TextField("wmsPath", new PropertyModel(layerModel, "path")));
     }
 
-    private List<String> listStyles(){
-        List<String> styles = new ArrayList<String>();
-
-        for (StyleInfo info : getLayerInfo().getResource().getCatalog().getStyles()){
-            styles.add(info.getName());
+    /**
+     * Wicket cannot deal with collection property that does not have a setter, so we
+     * do the magic inside there (and sort the style names in the process)
+     * @author Andrea Aime - OpenGeo
+     *
+     */
+    class LayerStylesModel implements IModel {
+        IModel layerModel;
+        
+        public LayerStylesModel(IModel layerModel) {
+            this.layerModel = layerModel;
         }
 
-        return styles;
+        public Object getObject() {
+            List<StyleInfo> styles = new ArrayList(((LayerInfo) layerModel.getObject()).getStyles());
+            Collections.sort(styles, new StyleNameComparator());
+            return styles;
+        }
+
+        public void setObject(Object object) {
+            Set<StyleInfo> styles = ((LayerInfo) layerModel.getObject()).getStyles();
+            styles.clear();
+            styles.addAll((Collection<StyleInfo>) object);
+        }
+
+        public void detach() {
+            layerModel.detach();
+        }
+        
     }
 }
