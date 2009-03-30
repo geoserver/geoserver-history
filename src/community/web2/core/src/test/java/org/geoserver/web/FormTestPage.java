092784f632341f22344a24fa4486da077981327a
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-03-30 15:48:32 +0000

    Better javadocs, renamed component ids to make them more obvious
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11906 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 84f59c66b6d4413c91804e093e4d02296dc85786 [formerly 18e5e2ff3f9cbbce2b73d79fb31ed42114c208e9]
    Former-commit-id: e3c57a7c8df967871c9941c3d40a437ba9be6e5d

--- src/community/web2/core/src/test/java/org/geoserver/web/FormTestPage.java
+++ src/community/web2/core/src/test/java/org/geoserver/web/FormTestPage.java
@@ -8,13 +8,19 @@ import org.apache.wicket.markup.html.form.Form;
 
 /**
  * Helper class to test components that need a form around them to be tested
- * (typically custom panels with form components inside)
+ * (typically custom panels with form components inside).
+ * <p>
+ * The panel will be placed into a form named "form", the panel itself will be
+ * named "content"
  */
 public class FormTestPage extends WebPage {
 
+    private static final String PANEL = "panel";
+    private static final String FORM = "form";
+
     public FormTestPage(ComponentBuilder builder) {
-        Form form = new Form("form");
-        form.add(builder.buildComponent("content"));
+        Form form = new Form(FORM);
+        form.add(builder.buildComponent(PANEL));
         add(form);
     }
 
