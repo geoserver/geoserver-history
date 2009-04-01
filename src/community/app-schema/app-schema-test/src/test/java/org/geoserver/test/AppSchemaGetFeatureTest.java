3e34c466ea13b094b22d9ae1a97b8ba809a2f125
bencaradocdavies <bencaradocdavies@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-01 07:25:12 +0000

    Added GetFeature test for app-schema. The test is disabled, waiting for GEOS-2753 patches, and does not yet test anything (it just logs the WFS response).
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@11932 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e48d69a10d391c8e8e9bab62d7cfffba78ae90ce [formerly b206dd0ca66a8e89820e12eda79e3ac78615a070]
    Former-commit-id: c9bd1b6b3841cbded26fa0fb177b0e277d4312b7

---
+++ src/community/app-schema/app-schema-test/src/test/java/org/geoserver/test/AppSchemaGetFeatureTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2001 - 2008 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import junit.framework.Test;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+
+/**
+ * WFS GetFeature to test integration of {@link AppSchemaDataAccess} with GeoServer.
+ * 
+ * @author Ben Caradoc-Davies, CSIRO Exploration and Mining
+ */
+public class AppSchemaGetFeatureTest extends AppSchemaGeoServerTestSupport {
+
+    /**
+     * Read-only test so can use one-time setup.
+     * 
+     * @return
+     */
+    public static Test suite() {
+        return new OneTimeTestSetup(new AppSchemaGetFeatureTest());
+    }
+
+    /**
+     * Test if GetFeature returns parsable XML.
+     * 
+     * @throws Exception
+     */
+    public void testGetFeature() throws Exception {
+        Document doc = getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
+        LOGGER.info("WFS response:\n" + prettyString(doc));
+        assertEquals("wfs:FeatureCollection", doc.getDocumentElement().getNodeName());
+    }
+
+    /**
+     * Return {@link Document} as a pretty-printed string.
+     * 
+     * @param doc
+     * @return
+     * @throws Exception
+     */
+    public String prettyString(Document doc) throws Exception {
+        OutputStream out = new ByteArrayOutputStream();
+        prettyPrint(doc, out);
+        return out.toString();
+    }
+
+    /**
+     * Pretty-print a {@link Document} to an {@link OutputStream}.
+     * 
+     * @param doc
+     * @param out
+     * @throws Exception
+     */
+    public void prettyPrint(Document doc, OutputStream out) throws Exception {
+        OutputFormat format = new OutputFormat(doc);
+        format.setLineWidth(80);
+        format.setIndenting(true);
+        format.setIndent(4);
+        XMLSerializer serializer = new XMLSerializer(out, format);
+        serializer.serialize(doc);
+    }
+
+}
