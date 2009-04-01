fe2848c6614109e2efdbc30396b50280de87c3a8
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-01 15:22:41 +0000

    Adding copyright headers
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@11939 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 68a09a120d779c52a9c07a8585933f6fe679143e [formerly af1beefdf8461b3d2b3135ea477e14e40441f0cb]
    Former-commit-id: 0f214b04e8055bd8927b0548e15e2115bbeb0966

--- community/web2/core/src/main/java/org/geoserver/web/data/workspace/NamespaceDetachableModel.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/workspace/NamespaceDetachableModel.java
@@ -1,9 +1,14 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web.data.workspace;
 
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.web.GeoServerApplication;
 
+@SuppressWarnings("serial")
 public class NamespaceDetachableModel extends LoadableDetachableModel {
 
     String id;
--- community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspacePage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspacePage.java
@@ -1,19 +1,16 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web.data.workspace;
 
-import static org.geoserver.web.data.workspace.WorkspaceProvider.NAME;
-import static org.geoserver.web.data.workspace.WorkspaceProvider.REMOVE;
-
-import java.util.List;
+import static org.geoserver.web.data.workspace.WorkspaceProvider.*;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
-import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.CatalogBuilder;
-import org.geoserver.catalog.LayerInfo;
-import org.geoserver.catalog.ResourceInfo;
-import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.wicket.ConfirmationAjaxLink;
