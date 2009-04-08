195d537f3192d9efaeb53fe85feb9ac88c456206
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-08 19:04:56 +0000

    updated batch script
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@12016 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4f20bea8d59e8b4f92949e6150d60dca4fe4619d [formerly c67799c0139c7db3b0c5085753c4376f78ca231a]
    Former-commit-id: 6065b3bee9c24224c1f6a052c9d17c9abdb28021

--- data/citewfs-1.1-h2/h2.bat
+++ data/citewfs-1.1-h2/h2.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set HOME=c:\Documents and Settings\YourUser
-set H2=%HOME%\.m2\repository\org\h2database\h2\1.0-SNAPSHOT\h2-1.0-SNAPSHOT.jar
 set M2_REPO=%HOME%\.m2\repository
+set H2=%M2_REPO%\org\h2database\h2\1.1.104\h2-1.1.104.jar
 set GT_VERSION=2.5-SNAPSHOT
-set CP="%H2%";"%M2_REPO%/org/openplans/spatialdbbox/1.0-SNAPSHOT/spatialdbbox-1.0-SNAPSHOT.jar";"%M2_REPO%/org/geotools/gt-main/%GT_VERSION%/gt-main-%GT_VERSION%.jar";"%M2_REPO%/com/vividsolutions/jts/1.8/jts-1.8.jar"
+set CP="%H2%";"%M2_REPO%/org/geotools/gt-main/%GT_VERSION%/gt-main-%GT_VERSION%.jar";"%M2_REPO%/org/geotools/gt-jdbc-h2/%GT_VERSION%/gt-jdbc-h2-%GT_VERSION%.jar";"%M2_REPO%/com/vividsolutions/jts/1.9/jts-1.9.jar"
 echo Classpath: %CP%
 set SF="2"
 
