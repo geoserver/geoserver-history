fe6799353d829741fd08a34a9dbb208c2bb8e604
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-08 10:38:56 +0000

    Fixing test (and hopefully the build along with it)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12004 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7ea123c97f9c6afecfdc799dd2e8145a02afc4cd [formerly e042b522a40eef967f475ad4743fc24f55c337e4]
    Former-commit-id: 1f0df248df9740bd9d731bbde38202bcb88cab47

--- community/web2/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
+++ community/web2/demo/src/test/java/org/geoserver/web/demo/MapPreviewPageTest.java
@@ -2,11 +2,9 @@ package org.geoserver.web.demo;
 
 import org.geoserver.web.GeoServerWicketTestSupport;
 
-import org.apache.wicket.markup.html.link.ExternalLink;
-
 public class MapPreviewPageTest extends GeoServerWicketTestSupport {
     public void testValues() throws Exception {
         tester.startPage(MapPreviewPage.class);
-        tester.assertComponent("layerContainer:layer:0:layerLink", ExternalLink.class);
+        tester.assertRenderedPage(MapPreviewPage.class);
     }
 }
