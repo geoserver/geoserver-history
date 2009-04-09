e509ccaad3816d38c492cb0bb875dd184fcc9eb8
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-09 08:55:38 +0000

    Splitting up the layers and stores pages in packages following the example or styles, layer groups
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12038 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ee9e207a8fdbbef8dc380df476b20be9b2707369 [formerly e8818a193c727453678e221301af8c0a3e6cc0cd]
    Former-commit-id: 024f373fff146093241a5b3a8ed2861b07f20cc5

--- community/web2/core/src/main/java/org/geoserver/web/data/layer/NewLayerPage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/layer/NewLayerPage.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.web.data.table;
+package org.geoserver.web.data.layer;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
--- community/web2/core/src/main/java/org/geoserver/web/data/store/StorePage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/store/StorePage.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.web.data.table;
+package org.geoserver.web.data.store;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.DropDownChoice;
