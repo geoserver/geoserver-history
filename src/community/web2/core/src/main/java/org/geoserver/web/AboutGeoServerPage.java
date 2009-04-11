4b3e6532b937110eb60ce28fb75549ef07e7565a
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-11 00:24:26 +0000

    GEOS-2161, added an about geoserver page for additoinal information
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12070 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b8f81a707bd73b72ed2152cc34f8700c637feeea [formerly bc994a456a66edebcbb38418d1072a41bba98bde]
    Former-commit-id: 4a485f4ea7af929a9362e26972d8ce2baa38852d

---
+++ src/community/web2/core/src/main/java/org/geoserver/web/AboutGeoServerPage.java
@@ -0,0 +1,11 @@
+package org.geoserver.web;
+
+/**
+ * An about GeoServer page providing various bits of information.
+ * 
+ * @author Justin Deoliveira, OpenGeo
+ *
+ */
+public class AboutGeoServerPage extends GeoServerBasePage {
+
+}
