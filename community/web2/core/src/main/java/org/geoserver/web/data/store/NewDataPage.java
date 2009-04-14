e358ec9c608f0331268170c968a1cfe10742fd9c
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-14 15:47:45 +0000

    GEOS-2862, move NewDataPage from data to data.store package
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12099 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fddf87d96fd5e308ea74b17a8c38f263f8975b57 [formerly db701e70f9249f399a2b6d1544e99dc8bbbd6e41]
    Former-commit-id: 841b367b96c664d223c74546faf8130201b690c7

--- community/web2/core/src/main/java/org/geoserver/web/data/store/NewDataPage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/store/NewDataPage.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package org.geoserver.web.data;
+package org.geoserver.web.data.store;
 
 import java.util.ArrayList;
 import java.util.Collections;
