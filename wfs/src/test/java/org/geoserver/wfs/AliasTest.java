5d5670037454a83ebb339599b2aa5ab802dbb6ae
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-15 01:09:54 +0000

    updated dft test cases based on schema encoding changes
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12130 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 135acf464634cc5e3b0de23442acbd357ebb1666 [formerly ea587c06668488a80628be433a6328386d9a1a6f]
    Former-commit-id: f5a4a0427b6ba648072f4bf685affbfa3a0847d4

--- wfs/src/test/java/org/geoserver/wfs/AliasTest.java
+++ wfs/src/test/java/org/geoserver/wfs/AliasTest.java
@@ -76,7 +76,7 @@ public class AliasTest extends WFSTestSupport {
     public void testDescribeFeatureType() throws Exception {
         Document doc = getAsDOM("wfs?request=DescribeFeatureType&typename=cdf:ft15&version=1.0.0");
         print(doc);
-        assertEquals("xs:schema", doc.getDocumentElement().getNodeName());
+        assertEquals("xsd:schema", doc.getDocumentElement().getNodeName());
 
         XMLAssert.assertXpathEvaluatesTo("ft15", "/xs:schema/xs:element/@name", doc);
     }
