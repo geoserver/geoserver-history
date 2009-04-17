5d1be876d479be3f34508bebb87aa8673290caec
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-17 10:10:49 +0000

    Some more validation and testing for the http layer config (leftover of a work I already started, but also sample on how to test the panels making up the layer info configuration
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12171 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 9af5a205d5443deaeb352c285f99bfb0563fedfc [formerly b5fcb2ae378cb8d16c539098ba627a1c52d4eb5b]
    Former-commit-id: 0e6a890a57092e1bbe05a8376ba1ee13c9f1d98b

--- community/web2/core/src/main/java/org/geoserver/web/publish/HTTPLayerConfig.java
+++ community/web2/core/src/main/java/org/geoserver/web/publish/HTTPLayerConfig.java
@@ -4,18 +4,24 @@
  */
 package org.geoserver.web.publish;
 
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.validator.NumberValidator;
 import org.geoserver.web.util.MapModel;
 
+/**
+ * Configures the HTTP caching parameters 
+ */
+@SuppressWarnings("serial")
 public class HTTPLayerConfig extends LayerConfigurationPanel {
     public HTTPLayerConfig(String id, IModel model){
         super(id, model);
         add(new CheckBox("cachingEnabled", new MapModel(new PropertyModel(model, "resource.metadata"), "cachingEnabled")));
-        add(new TextField("cacheAgeMax", new MapModel(new PropertyModel(model, "resource.metadata"), "cacheAgeMax")));
+        TextField maxAge = new TextField("cacheAgeMax", new MapModel(new PropertyModel(model, "resource.metadata"), "cacheAgeMax"), Long.class);
+        maxAge.add(NumberValidator.range(0, Long.MAX_VALUE));
+        add(maxAge);
     }
 }
 
