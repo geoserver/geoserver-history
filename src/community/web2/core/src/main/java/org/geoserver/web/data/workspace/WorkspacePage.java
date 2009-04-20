50d76630fe0b4fd904be7c852a098188624d2f11
arneke <arneke@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-20 16:41:00 +0000

    Big i18n commit so that there is a backup and to avoid conflicts. Not final.
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12189 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8bcc5bf3f9bb51793d7086a6badefc10f1b06859 [formerly 0239f1c4b2e9b4ab07ab82dca65fcad0c9fe9d1f]
    Former-commit-id: 07da6eb91e647d191219358da66e1811e202aa22

--- src/community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspacePage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/workspace/WorkspacePage.java
@@ -10,6 +10,9 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.geoserver.catalog.CatalogBuilder;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerSecuredPage;
@@ -58,11 +61,19 @@ public class WorkspacePage extends GeoServerSecuredPage {
     
     Component removeWorkspaceLink(String id, final IModel itemModel ) {
         final WorkspaceInfo workspace = (WorkspaceInfo) itemModel.getObject();
-        StringBuilder sb = new StringBuilder();
-        sb.append( "Are sure you sure want to remove workspace " ).append( workspace.getName() );
-        sb.append( ". Doing so will remove every store that is part of the workspace." );
         
-        return new ConfirmationAjaxLink( id, null, "remove", sb.toString() ) {
+        ResourceModel resRemove = new ResourceModel("removeWorkspace","Remove");
+        
+        StringResourceModel strResConfRemove = 
+        	new StringResourceModel(
+        		"confirmRemoveWorkspaceX", 
+        		this,
+        		new Model(""),
+        		new Object[] {
+        			workspace.getName()
+        		} );
+        
+        return new ConfirmationAjaxLink( id, null, resRemove, strResConfRemove) {
             @Override
             protected void onClick(AjaxRequestTarget target) {
                 CatalogBuilder cb = new CatalogBuilder( getCatalog() );
