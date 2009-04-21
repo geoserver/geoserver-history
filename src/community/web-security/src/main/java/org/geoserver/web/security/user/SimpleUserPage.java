0be538c5710afc5079aec0bd4bff2aae7c768d0d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-21 11:08:25 +0000

    New UI to add/edit/remove users
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12195 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8befbc43ecdd877ed6ad97fd978892598673d7d4 [formerly 681ba2f7b05a95e594ec7115109c20c039d1ac34]
    Former-commit-id: 660b4176b0ed65ba81513dfa1cb1317781309184

---
+++ src/community/web-security/src/main/java/org/geoserver/web/security/user/SimpleUserPage.java
@@ -0,0 +1,113 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web.security.user;
+
+import java.util.logging.Level;
+
+import org.acegisecurity.userdetails.User;
+import org.acegisecurity.userdetails.UserDetails;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.geoserver.security.GeoserverUserDao;
+import org.geoserver.web.GeoServerApplication;
+import org.geoserver.web.GeoServerSecuredPage;
+import org.geoserver.web.wicket.ConfirmationAjaxLink;
+import org.geoserver.web.wicket.GeoServerTablePanel;
+import org.geoserver.web.wicket.ParamResourceModel;
+import org.geoserver.web.wicket.SimpleAjaxLink;
+import org.geoserver.web.wicket.GeoServerDataProvider.Property;
+
+/**
+ * A page listing users, allowing for removal, addition and linking to an edit page
+ */
+@SuppressWarnings("serial")
+public class SimpleUserPage extends GeoServerSecuredPage {
+
+    private GeoServerTablePanel<User> users;
+
+    public SimpleUserPage() {
+        SimpleUserProvider provider = new SimpleUserProvider();
+        users = new GeoServerTablePanel<User>("table", provider) {
+
+            @Override
+            protected Component getComponentForProperty(String id, IModel itemModel,
+                    Property<User> property) {
+                if (property == SimpleUserProvider.USERNAME) {
+                    return editUserLink(id, itemModel, property);
+                } else if (property == SimpleUserProvider.ROLES) {
+                    return new Label(id, property.getModel(itemModel));
+                } else if (property == SimpleUserProvider.ADMIN) {
+                    return new Label(id, property.getModel(itemModel));
+                } else if (property == SimpleUserProvider.REMOVE) {
+                    // don't allow one to remove the admin
+                    if("admin".equals(((User) itemModel.getObject()).getUsername()))
+                        return new Label(id, new Model("-"));
+                    else
+                        return removeUserLink(id, itemModel);
+                } else {
+                    throw new RuntimeException("Uknown property " + property);
+                }
+
+            }
+        };
+        users.setOutputMarkupId(true);
+        add(users);
+        add(addUserLink());
+    }
+    
+    GeoserverUserDao getUserDao() {
+        return ((GeoserverUserDao) GeoServerApplication.get().getBean("userDetailsService"));
+    }
+
+    ConfirmationAjaxLink removeUserLink(String id, IModel itemModel) {
+        String username = ((User) itemModel.getObject()).getUsername();
+        IModel confirmRemoveModel = new ParamResourceModel("confirmRemoveUser", this, username);
+        return new ConfirmationAjaxLink(id, itemModel, new ParamResourceModel("removeUser", this, username), confirmRemoveModel) {
+
+            @Override
+            protected void onClick(AjaxRequestTarget target) {
+                try {
+                    String username = ((User) getModelObject()).getUsername();
+                    if(username.equals("admin"));
+                    getUserDao().removeUser(username);
+                    getUserDao().storeUsers();
+                } catch(Exception e) {
+                    LOGGER.log(Level.SEVERE, "Error occurred while removing the user and saving out the result", e);
+                    error(new ParamResourceModel("saveError", this, e.getMessage()));
+                    target.addComponent(feedbackPanel);
+                }
+                target.addComponent(users);
+            }
+            
+        };
+    }
+
+    AjaxLink addUserLink() {
+        return new AjaxLink("addUser", new Model()) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                setResponsePage(new NewUserPage());
+            }
+
+        };
+    }
+
+    Component editUserLink(String id, IModel itemModel, Property<User> property) {
+        return new SimpleAjaxLink(id, itemModel, property.getModel(itemModel)) {
+
+            @Override
+            protected void onClick(AjaxRequestTarget target) {
+                setResponsePage(new EditUserPage((UserDetails) getModelObject()));
+            }
+
+        };
+    }
+
+}
