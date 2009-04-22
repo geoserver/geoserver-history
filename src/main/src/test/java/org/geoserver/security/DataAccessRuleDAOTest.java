902be516239ef8fd1b71e7f111f1930204c12ed6
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-22 13:15:40 +0000

    Added add/set and remove methods
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12223 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0cbd790fc5acea99c76268fc52a3bbe19966edc7 [formerly 53c1df29a5e1f94ae42f7aa2d37aa83d0b6f10b6]
    Former-commit-id: 362934bad571d4c24772fa3151dea7b784da7c1c

---
+++ src/main/src/test/java/org/geoserver/security/DataAccessRuleDAOTest.java
@@ -0,0 +1,65 @@
+package org.geoserver.security;
+
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.impl.LayerInfoImpl;
+import org.geoserver.catalog.impl.WorkspaceInfoImpl;
+
+public class DataAccessRuleDAOTest extends TestCase {
+
+    DataAccessRuleDAO dao;
+    
+    @Override
+    protected void setUp() throws Exception {
+        // make a nice little catalog that does always tell us stuff is there 
+        Catalog catalog = createNiceMock(Catalog.class);
+        expect(catalog.getWorkspaceByName((String) anyObject())).andReturn(new WorkspaceInfoImpl()).anyTimes();
+        expect(catalog.getLayerByName((String) anyObject())).andReturn(new LayerInfoImpl()).anyTimes();
+        replay(catalog);
+        
+        // prepare some base rules
+        Properties props = new Properties();
+        props.put("topp.states.w", "ROLE_TSW");
+        props.put("topp.*.w", "ROLE_TW");
+        props.put("*.*.r", "*");
+        
+        dao = new MemoryDataAccessRuleDAO(catalog, props);
+    }
+    
+    public void testParse() {
+        assertEquals(3, dao.getRules().size());
+        
+        // check the first rule
+        DataAccessRule rule = dao.getRules().get(0);
+        assertEquals("*.*.r", rule.getKey());
+        assertEquals(1, rule.getRoles().size());
+        assertEquals("*", rule.getRoles().iterator().next());
+    }
+    
+    public void testAdd() {
+        assertEquals(3, dao.getRules().size());
+        DataAccessRule newRule = dao.parseDataAccessRule("*.*.w", "ROLE_GENERIC_W");
+        assertTrue(dao.addRule(newRule));
+        assertEquals(4, dao.getRules().size());
+        assertEquals(newRule, dao.getRules().get(1));
+        assertFalse(dao.addRule(newRule));
+    }
+    
+    public void testRemove() {
+        DataAccessRule newRule = dao.parseDataAccessRule("*.*.w", "ROLE_GENERIC_W");
+        assertFalse(dao.removeRule(newRule));
+        DataAccessRule first = dao.getRules().get(0);
+        assertTrue(dao.removeRule(first));
+        assertFalse(dao.removeRule(first));
+        assertEquals(2, dao.getRules().size());
+    }
+    
+}
