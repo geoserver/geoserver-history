8ec3c4ea486602638cd4180e20bc591a90b7adb1
simboss <simboss@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-22 23:25:30 +0000

    generics clean up
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x_WCS10EMF_ND@12232 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8e917efddfcf7a821989a96d2905aa4769aeec28 [formerly 55edfe9bed30a891098d17f3c10f2145ccb56f49]
    Former-commit-id: e16321b0d2ab0ee334cb2c1283c4d82869117dae

--- geoserver/community/hibernate/src/main/java/org/geoserver/catalog/hibernate/HbWorkspaceInfo.java
+++ geoserver/community/hibernate/src/main/java/org/geoserver/catalog/hibernate/HbWorkspaceInfo.java
@@ -4,7 +4,11 @@ import org.geoserver.catalog.impl.WorkspaceInfoImpl;
 
 public class HbWorkspaceInfo extends WorkspaceInfoImpl {
 
-    private boolean isDefault;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6850342434147452503L;
+	private boolean isDefault;
 
     public boolean isDefault() {
         return isDefault;
