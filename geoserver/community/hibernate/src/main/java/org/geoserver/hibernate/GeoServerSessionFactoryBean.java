f23cacb7fbe8cf134350106a8ddf7f5e0a460dc6
simboss <simboss@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-22 23:33:42 +0000

    -removed hibernate initializer since it was useless -merged methods here
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x_WCS10EMF_ND@12237 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 27ef5dabcb37c5413855fcafacdb14fe3ca8007b [formerly 1e71fb5bf9c53801d543a6b9bbb35a934228de8d]
    Former-commit-id: 98125c95e60b79d9d510ddfc61295dc6919f88c9

--- geoserver/community/hibernate/src/main/java/org/geoserver/hibernate/GeoServerSessionFactoryBean.java
+++ geoserver/community/hibernate/src/main/java/org/geoserver/hibernate/GeoServerSessionFactoryBean.java
@@ -1,10 +1,13 @@
 package org.geoserver.hibernate;
 
+import java.io.IOException;
+
 import org.hibernate.HibernateException;
 import org.hibernate.cfg.Configuration;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 
 /**
@@ -24,9 +27,16 @@ public class GeoServerSessionFactoryBean extends LocalSessionFactoryBean impleme
 
     public void postProcessConfiguration(Configuration config) throws HibernateException {
         super.postProcessConfiguration(config);
-
-        ConfigurationInitializer.postProcessConfiguration(config, applicationContext);
-        config.configure();
+     // gather up other mappings on the classpath
+	    try {
+	        Resource[] resources = applicationContext.getResources("classpath*:mappings.hbm.xml");
+	        for (int i = 0; i < resources.length; i++) {
+	        	config.addInputStream(resources[i].getInputStream());
+	        }
+	    } catch (IOException e) {
+	        // TODO: log this
+	    }
     }
+
 
 }
