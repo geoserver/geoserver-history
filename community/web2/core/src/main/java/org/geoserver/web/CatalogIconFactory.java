254b478073bd829cc45fb9293e4742299142fed9
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-29 22:19:30 +0000

    GEOS-2901, use store icons for both vector and raster stores
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12297 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a34d95e9c39269f2a95ed24265bdd3bfe2383792 [formerly 9c9e92f91133dcd5b3e9f0d45735736a65783315]
    Former-commit-id: c4d0f83ef41ec7effefe1e84b616a5933a8ffb49

--- community/web2/core/src/main/java/org/geoserver/web/CatalogIconFactory.java
+++ community/web2/core/src/main/java/org/geoserver/web/CatalogIconFactory.java
@@ -4,14 +4,23 @@
  */
 package org.geoserver.web;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 import org.apache.wicket.ResourceReference;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.CoverageStoreInfo;
+import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.LayerInfo;
+import org.geoserver.catalog.ResourcePool;
+import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.LayerInfo.Type;
 import org.geoserver.web.data.resource.DataStorePanelInfo;
+import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.data.DataAccessFactory;
+import org.geotools.xml.xsi.XSISimpleTypes.ID;
+import org.opengis.coverage.grid.Format;
 
 /**
  * Utility class used to lookup icons for various catalog objects
@@ -27,57 +36,115 @@ public class CatalogIconFactory implements Serializable {
 
     public static final ResourceReference UNKNOWN_ICON = new ResourceReference(
             GeoServerBasePage.class, "img/icons/silk/error.png");
-    
+
     public static final ResourceReference GROUP_ICON = new ResourceReference(
             GeoServerBasePage.class, "img/icons/silk/layers.png");
-    
+
     static final CatalogIconFactory INSTANCE = new CatalogIconFactory();
-    
+
     public static final CatalogIconFactory get() {
         return INSTANCE;
     }
-    
+
     private CatalogIconFactory() {
         // private constructor, this is a singleton
     }
 
     /**
      * Returns the appropriate icon for the specified layer
+     * 
      * @param info
      * @return
      */
     public ResourceReference getLayerIcon(LayerInfo info) {
         ResourceReference icon = UNKNOWN_ICON;
-        if(info.getType() == Type.VECTOR)
+        if (info.getType() == Type.VECTOR)
             icon = VECTOR_ICON;
-        else if(info.getType() == Type.RASTER)
+        else if (info.getType() == Type.RASTER)
             icon = RASTER_ICON;
         return icon;
     }
-    
+
     /**
-     * Returns the appropriate icon given the 
+     * Returns the appropriate icon for the specified store.
+     * 
+     * @param storeInfo
+     * @return
+     * @see #getStoreIcon(Class)
+     */
+    public ResourceReference getStoreIcon(final StoreInfo storeInfo) {
+        Class<?> factoryClass;
+
+        Catalog catalog = storeInfo.getCatalog();
+        final ResourcePool resourcePool = catalog.getResourcePool();
+
+        if (storeInfo instanceof DataStoreInfo) {
+            DataAccessFactory dataStoreFactory;
+            try {
+                dataStoreFactory = resourcePool.getDataStoreFactory((DataStoreInfo) storeInfo);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't aquire the datastore factory for store "
+                        + storeInfo.getName(), e);
+            }
+            factoryClass = dataStoreFactory.getClass();
+        } else if (storeInfo instanceof CoverageStoreInfo) {
+            AbstractGridFormat format = resourcePool
+                    .getGridCoverageFormat((CoverageStoreInfo) storeInfo);
+            factoryClass = format.getClass();
+        } else {
+            throw new IllegalStateException(storeInfo.getClass().getName());
+        }
+        return getStoreIcon(factoryClass);
+    }
+
+    /**
+     * Returns the appropriate icon given a data access or coverage factory class.
+     * <p>
+     * The lookup is performed by first searching for a registered {@link DataStorePanelInfo} for
+     * the given store factory class, if not found, the icon for the {@link DataStorePanelInfo}
+     * registered with the id {@code defaultVector} or {@code defaultRaster}, as appropriate will be
+     * used.
+     * </p>
+     * 
      * @param factoryClass
+     *            either a {@link DataAccessFactory} or a {@link Format} class
      * @return
      */
     public ResourceReference getStoreIcon(Class<?> factoryClass) {
         // look for the associated panel info if there is one
-        List<DataStorePanelInfo> infos = GeoServerApplication.get()
-                .getBeansOfType(DataStorePanelInfo.class);
+        final List<DataStorePanelInfo> infos;
+        infos = GeoServerApplication.get().getBeansOfType(DataStorePanelInfo.class);
+
         for (DataStorePanelInfo panelInfo : infos) {
-            if (panelInfo.getFactoryClass().equals(factoryClass))
-                return new ResourceReference(panelInfo.getIconBase(), panelInfo
-                        .getIcon());
+            if (factoryClass.equals(panelInfo.getFactoryClass())) {
+                return new ResourceReference(panelInfo.getIconBase(), panelInfo.getIcon());
+            }
         }
 
-        if (DataAccessFactory.class.isAssignableFrom(factoryClass))
+        if (DataAccessFactory.class.isAssignableFrom(factoryClass)) {
+            // search for the declared default vector store icon
+            for (DataStorePanelInfo panelInfo : infos) {
+                if ("defaultVector".equals(panelInfo.getId())) {
+                    return new ResourceReference(panelInfo.getIconBase(), panelInfo.getIcon());
+                }
+            }
+
             // fall back on generic vector icon otherwise
             return new ResourceReference(GeoServerApplication.class,
-                    "img/icons/geosilk/vector.png");
-        else
-            return new ResourceReference(GeoServerApplication.class,
-                    "img/icons/geosilk/raster.png");
+                    "img/icons/geosilk/database_vector.png");
 
+        } else if (Format.class.isAssignableFrom(factoryClass)) {
+            // search for the declared default coverage store icon
+            for (DataStorePanelInfo panelInfo : infos) {
+                if ("defaultRaster".equals(panelInfo.getId())) {
+                    return new ResourceReference(panelInfo.getIconBase(), panelInfo.getIcon());
+                }
+            }
+
+            // fall back on generic raster icon otherwise
+            return new ResourceReference(GeoServerApplication.class,
+                    "img/icons/geosilk/page_white_raster.png");
+        }
+        throw new IllegalArgumentException("Unrecognized store format class: " + factoryClass);
     }
-    
 }
