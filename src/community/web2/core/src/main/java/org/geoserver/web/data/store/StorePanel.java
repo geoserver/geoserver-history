78db3e5ea48acfbed3f76053207dd1f8512993f7
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-29 22:19:30 +0000

    GEOS-2901, use store icons for both vector and raster stores
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12297 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d73f9185a382bcadcf1325cbcddcb632c34b82e7 [formerly 70772c7472a287a6a45f839450378a56dc9899a8]
    Former-commit-id: 99705ff808240c6a00e5766d6464dc84089ce1d6

--- src/community/web2/core/src/main/java/org/geoserver/web/data/store/StorePanel.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/store/StorePanel.java
@@ -7,16 +7,19 @@ import static org.geoserver.web.data.store.StoreProvider.TYPE;
 import static org.geoserver.web.data.store.StoreProvider.WORKSPACE;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.web.CatalogIconFactory;
 import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.data.workspace.WorkspaceEditPage;
 import org.geoserver.web.wicket.ConfirmationAjaxLink;
@@ -26,30 +29,42 @@ import org.geoserver.web.wicket.SimpleAjaxLink;
 import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 
 public class StorePanel extends GeoServerTablePanel<StoreInfo> {
-    
-    ModalWindow popupWindow;
 
-    public StorePanel( String id ) {
-        this( id, new StoreProvider() );
+    private static final long serialVersionUID = 5957961031378924960L;
+
+    private ModalWindow popupWindow;
+
+    public StorePanel(String id) {
+        this(id, new StoreProvider());
     }
-    
-    public StorePanel( String id, StoreProvider provider ) {
+
+    public StorePanel(String id, StoreProvider provider) {
         super(id, provider);
-        
+
         // the popup window for messages
         popupWindow = new ModalWindow("popupWindow");
         add(popupWindow);
     }
-    
+
     private Catalog getCatalog() {
         return GeoServerApplication.get().getCatalog();
     }
-    
+
     @Override
-    protected Component getComponentForProperty(String id,
-            IModel itemModel, Property<StoreInfo> property) {
+    protected Component getComponentForProperty(String id, IModel itemModel,
+            Property<StoreInfo> property) {
+
+        final CatalogIconFactory icons = CatalogIconFactory.get();
+
         if (property == TYPE) {
-            return new Label(id, TYPE.getModel(itemModel));
+            final StoreInfo storeInfo = (StoreInfo) itemModel.getObject();
+
+            ResourceReference storeIcon = icons.getStoreIcon(storeInfo);
+
+            Fragment f = new Fragment(id, "iconFragment", StorePanel.this);
+            f.add(new Image("storeIcon", storeIcon));
+
+            return f;
         } else if (property == WORKSPACE) {
             return workspaceLink(id, itemModel);
         } else if (property == NAME) {
@@ -59,8 +74,7 @@ public class StorePanel extends GeoServerTablePanel<StoreInfo> {
         } else if (property == REMOVE) {
             return removeLink(id, itemModel);
         }
-        throw new IllegalArgumentException(
-                "Don't know a property named " + property.getName());
+        throw new IllegalArgumentException("Don't know a property named " + property.getName());
     }
 
     private Component storeNameLink(String id, final IModel itemModel) {
@@ -68,11 +82,9 @@ public class StorePanel extends GeoServerTablePanel<StoreInfo> {
             @Override
             public void onClick(AjaxRequestTarget target) {
                 String storeName = getLink().getModelObjectAsString();
-                StoreInfo store = getCatalog().getStoreByName(storeName,
-                        StoreInfo.class);
+                StoreInfo store = getCatalog().getStoreByName(storeName, StoreInfo.class);
                 if (store == null) {
-                    popupWindow.setContent(new Label(
-                            popupWindow.getContentId(),
+                    popupWindow.setContent(new Label(popupWindow.getContentId(),
                             "Cannot find the store " + store
                                     + " anymore, I guess it has been removed. "
                                     + "Will refresh the store list."));
@@ -90,24 +102,22 @@ public class StorePanel extends GeoServerTablePanel<StoreInfo> {
     private Component workspaceLink(String id, IModel itemModel) {
         return new SimpleAjaxLink(id, WORKSPACE.getModel(itemModel)) {
             public void onClick(AjaxRequestTarget target) {
-                WorkspaceInfo info = getCatalog().getWorkspaceByName(
-                        getModelObjectAsString());
+                WorkspaceInfo info = getCatalog().getWorkspaceByName(getModelObjectAsString());
                 if (info != null)
                     setResponsePage(new WorkspaceEditPage(info));
             }
         };
     }
-    
+
     protected Component removeLink(String id, final IModel itemModel) {
         StoreInfo info = (StoreInfo) itemModel.getObject();
 
         ResourceModel resRemove = new ResourceModel("removeStore", "Remove");
 
-        ParamResourceModel confirmRemove = new ParamResourceModel(
-                "confirmRemoveStoreX", this, info.getName());
+        ParamResourceModel confirmRemove = new ParamResourceModel("confirmRemoveStoreX", this, info
+                .getName());
 
-        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(id, null,
-                resRemove, confirmRemove) {
+        SimpleAjaxLink linkPanel = new ConfirmationAjaxLink(id, null, resRemove, confirmRemove) {
             public void onClick(AjaxRequestTarget target) {
                 getCatalog().remove((StoreInfo) itemModel.getObject());
                 target.addComponent(StorePanel.this);
