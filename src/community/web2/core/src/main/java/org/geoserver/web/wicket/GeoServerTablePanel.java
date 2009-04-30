f5d6f563c07bfb1092a7ac69cab544b4ceb3e950
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-04-30 14:35:55 +0000

    Better javadoc, minor refactorings
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12301 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 07807ae01e1b3ebdaf8dbaaa668ffcd2d7276dc7 [formerly c229aff1f57e80ddaee5bb7ecf029296ed1d7fe5]
    Former-commit-id: 6ef520ef5ed0e5589c789f22a6d4c685c37ff844

--- src/community/web2/core/src/main/java/org/geoserver/web/wicket/GeoServerTablePanel.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/wicket/GeoServerTablePanel.java
@@ -7,7 +7,7 @@ package org.geoserver.web.wicket;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.behavior.SimpleAttributeModifier;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -26,18 +26,18 @@ import org.apache.wicket.model.ResourceModel;
 import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 
 /**
- * An abstract filterable, sortable, pageable table with associated
- * filtering form and paging navigator.<p>
- * The construction of the page is driven by the properties returned
- * by a {@link GeoServerDataProvider}, subclasses only need to build a component
- * for each property by implementing the {@link #getComponentForProperty(String, IModel, Property)}
- * method
+ * An abstract filterable, sortable, pageable table with associated filtering form and paging
+ * navigator.
+ * <p>
+ * The construction of the page is driven by the properties returned by a
+ * {@link GeoServerDataProvider}, subclasses only need to build a component for each property by
+ * implementing the {@link #getComponentForProperty(String, IModel, Property)} method
+ * 
  * @param <T>
  */
 @SuppressWarnings("serial")
 public abstract class GeoServerTablePanel<T> extends Panel {
-    
-    
+
     private static final int DEFAULT_ITEMS_PER_PAGE = 25;
 
     // filter form components
@@ -47,12 +47,13 @@ public abstract class GeoServerTablePanel<T> extends Panel {
     DataView dataView;
 
     WebMarkupContainer listContainer;
-    
+
     Pager navigatorTop;
+
     Pager navigatorBottom;
 
     GeoServerDataProvider<T> dataProvider;
-    
+
     Form filterForm;
 
     public GeoServerTablePanel(String id, final GeoServerDataProvider<T> dataProvider) {
@@ -66,13 +67,13 @@ public abstract class GeoServerTablePanel<T> extends Panel {
         filterForm = new Form("filterForm");
         add(filterForm);
         filterForm.add(filter = new TextField("filter", new Model()));
-         filterForm.add(new AjaxSubmitButton("submit") {
+        filterForm.add(new AjaxButton("submit") {
 
             @Override
             protected void onSubmit(AjaxRequestTarget target, Form form) {
                 updateFilter(target, filter.getModelObjectAsString());
             }
-            
+
         });
 
         // setup the table
@@ -83,58 +84,50 @@ public abstract class GeoServerTablePanel<T> extends Panel {
             @Override
             protected void populateItem(Item item) {
                 final IModel itemModel = item.getModel();
-                
+
                 // odd/even style
-                item.add(new SimpleAttributeModifier("class",
-                        item.getIndex() % 2 == 0 ? "even" : "odd"));
+                item.add(new SimpleAttributeModifier("class", item.getIndex() % 2 == 0 ? "even"
+                        : "odd"));
 
                 // create one component per viewable property
-                item.add(new ListView("itemProperties", dataProvider
-                        .getVisibleProperties()) {
+                item.add(new ListView("itemProperties", dataProvider.getVisibleProperties()) {
 
                     @Override
                     protected void populateItem(ListItem item) {
-                        Property<T> property = (Property<T>) item
-                                .getModelObject();
-                       
-                        Component component = getComponentForProperty(
-                                "component", itemModel, property);
-                        
+                        Property<T> property = (Property<T>) item.getModelObject();
+
+                        Component component = getComponentForProperty("component", itemModel,
+                                property);
+
                         // add some checks for the id, the error message
                         // that wicket returns in case of mismatch is not
                         // that helpful
                         if (!"component".equals(component.getId()))
-                            throw new IllegalArgumentException(
-                                    "getComponentForProperty asked "
-                                            + "to build a component "
-                                            + "with id = 'component' "
-                                            + "for property '"
-                                            + property.getName()
-                                            + "', but got '"
-                                            + component.getId() + "' instead");
+                            throw new IllegalArgumentException("getComponentForProperty asked "
+                                    + "to build a component " + "with id = 'component' "
+                                    + "for property '" + property.getName() + "', but got '"
+                                    + component.getId() + "' instead");
                         item.add(component);
                     }
 
                 });
             }
-            
-            
 
         };
         listContainer.add(dataView);
 
         // add the sorting links
-        listContainer.add(new ListView("sortableLinks", dataProvider
-                .getVisibleProperties()) {
+        listContainer.add(new ListView("sortableLinks", dataProvider.getVisibleProperties()) {
 
             @Override
             protected void populateItem(ListItem item) {
                 Property<T> property = (Property<T>) item.getModelObject();
-                
+
                 // Simple i18n
                 String pageName = this.getPage().getClass().getSimpleName();
-                ResourceModel resMod = new ResourceModel(pageName+".th."+property.getName(), property.getName());
-                
+                ResourceModel resMod = new ResourceModel(pageName + ".th." + property.getName(),
+                        property.getName());
+
                 if (property.getComparator() != null) {
                     Fragment f = new Fragment("header", "sortableHeader", item);
                     AjaxLink link = sortLink(dataProvider, item);
@@ -155,32 +148,42 @@ public abstract class GeoServerTablePanel<T> extends Panel {
         add(navigatorBottom = new Pager("navigatorBottom"));
         navigatorBottom.setOutputMarkupId(true);
     }
-    
+
+    /**
+     * Number of visible items per page, should the default {@link #DEFAULT_ITEMS_PER_PAGE} not
+     * satisfy the programmer needs
+     * 
+     * @param items
+     */
     public void setItemsPerPage(int items) {
         dataView.setItemsPerPage(items);
     }
-    
-    public GeoServerTablePanel<T> setFilterable( boolean filterable ) {
-        filterForm.setVisible( filterable );
+
+    /**
+     * Enables/disables filtering for this table. When no filtering is enabled, the top form with
+     * the top pager and the search box will disappear. Returns self for chaining.
+     */
+    public GeoServerTablePanel<T> setFilterable(boolean filterable) {
+        filterForm.setVisible(filterable);
         return this;
     }
-    
-    AjaxLink sortLink(final GeoServerDataProvider<T> dataProvider,
-            ListItem item) {
+
+    /**
+     * Builds a sort link that will force sorting on a certain column, and flip it to the other
+     * direction when clicked again
+     */
+    AjaxLink sortLink(final GeoServerDataProvider<T> dataProvider, ListItem item) {
         return new AjaxLink("link", item.getModel()) {
 
             @Override
             public void onClick(AjaxRequestTarget target) {
                 SortParam currSort = dataProvider.getSort();
                 Property<T> property = (Property<T>) getModelObject();
-                if (currSort == null
-                        || !property.getName().equals(
-                                currSort.getProperty())) {
-                    dataProvider.setSort(new SortParam(property
-                            .getName(), true));
+                if (currSort == null || !property.getName().equals(currSort.getProperty())) {
+                    dataProvider.setSort(new SortParam(property.getName(), true));
                 } else {
-                    dataProvider.setSort(new SortParam(property
-                            .getName(), !currSort.isAscending()));
+                    dataProvider
+                            .setSort(new SortParam(property.getName(), !currSort.isAscending()));
                 }
                 target.addComponent(listContainer);
             }
@@ -188,29 +191,10 @@ public abstract class GeoServerTablePanel<T> extends Panel {
         };
     }
 
-
-//    private AjaxButton filterResetButton() {
-//        return new AjaxButton("resetFilter") {
-//
-//            @Override
-//            protected void onSubmit(AjaxRequestTarget target, Form form) {
-//                updateFilter(target, "");
-//            }
-//
-//        };
-//    }
-//
-//    private AjaxButton filterSubmitButton() {
-//        return new AjaxButton("applyFilter") {
-//
-//            @Override
-//            protected void onSubmit(AjaxRequestTarget target, Form form) {
-//                updateFilter(target, filter.getModelObjectAsString().trim());
-//            }
-//
-//        };
-//    }
-    
+    /**
+     * Parses the keywords and sets them into the data provider, forces update of the components
+     * that need to as a result of the different filtering
+     */
     private void updateFilter(AjaxRequestTarget target, String flatKeywords) {
         if ("".equals(flatKeywords)) {
             dataProvider.setKeywords(null);
@@ -220,10 +204,8 @@ public abstract class GeoServerTablePanel<T> extends Panel {
             String[] keywords = flatKeywords.split("\\s+");
             dataProvider.setKeywords(keywords);
             dataView.setCurrentPage(0);
-            ParamResourceModel paramResMod = new ParamResourceModel(
-                    "matchedXOutOfY", this, 
-                    Integer.valueOf(dataProvider.size()), 
-                    Integer.valueOf(dataProvider.fullSize()));
+            ParamResourceModel paramResMod = new ParamResourceModel("matchedXOutOfY", this, Integer
+                    .valueOf(dataProvider.size()), Integer.valueOf(dataProvider.fullSize()));
         }
         navigatorTop.updateMatched();
         navigatorBottom.updateMatched();
@@ -232,14 +214,14 @@ public abstract class GeoServerTablePanel<T> extends Panel {
         target.addComponent(navigatorTop);
         target.addComponent(navigatorBottom);
     }
-    
+
     /**
      * Turns filtering abilities on/off.
      */
     public void setFilterVisible(boolean filterVisible) {
         filterForm.setVisible(filterVisible);
     }
-    
+
     /**
      * Returns the component that will represent a property of a table item. Usually it should be a
      * label, or a link, but you can return pretty much everything.
@@ -251,15 +233,29 @@ public abstract class GeoServerTablePanel<T> extends Panel {
     protected abstract Component getComponentForProperty(String id, IModel itemModel,
             Property<T> property);
 
+    /**
+     * The two pages in the table panel. Includes a paging navigator and a status label telling the
+     * user what she is seeing
+     */
     class Pager extends Panel {
 
         GeoServerPagingNavigator navigator;
+
         Label matched;
 
         Pager(String id) {
             super(id);
 
-            add(navigator = new GeoServerPagingNavigator("navigator", dataView) {
+            add(navigator = updatingPagingNavigator());
+            add(matched = new Label("filterMatch", new Model()));
+            updateMatched();
+        }
+
+        /**
+         * Builds a paging navigator that will update both of the labels when the page changes.
+         */
+        private GeoServerPagingNavigator updatingPagingNavigator() {
+            return new GeoServerPagingNavigator("navigator", dataView) {
                 @Override
                 protected void onAjaxEvent(AjaxRequestTarget target) {
                     super.onAjaxEvent(target);
@@ -268,39 +264,45 @@ public abstract class GeoServerTablePanel<T> extends Panel {
                     target.addComponent(navigatorTop);
                     target.addComponent(navigatorBottom);
                 }
-            });
-            ParamResourceModel paramResMod = new ParamResourceModel("showingAllRecords", this,
-                        start(), end(), dataProvider.fullSize());
-            add(matched = new Label("filterMatch", paramResMod));
+            };
         }
 
+        /**
+         * Updates the label given the current page and filtering status
+         */
         void updateMatched() {
             if (dataProvider.getKeywords() == null) {
                 ParamResourceModel paramResMod = new ParamResourceModel("showingAllRecords", this,
-                        start(), end(), dataProvider.fullSize());
+                        first(), last(), dataProvider.fullSize());
                 matched.setModel(paramResMod);
             } else {
                 ParamResourceModel paramResMod = new ParamResourceModel("matchedXOutOfY", this,
-                        start(), end(), dataProvider.size(), dataProvider.fullSize());
+                        first(), last(), dataProvider.size(), dataProvider.fullSize());
                 matched.setModel(paramResMod);
             }
         }
-        
-        int start() {
-            if(dataView.getDataProvider().size() > 0)
+
+        /**
+         * User oriented index of the first item in the current page
+         */
+        int first() {
+            if (dataView.getDataProvider().size() > 0)
                 return dataView.getItemsPerPage() * dataView.getCurrentPage() + 1;
             else
                 return 0;
         }
-        
-        int end() {
+
+        /**
+         * User oriented index of the last item in the current page
+         */
+        int last() {
             int count = dataView.getPageCount();
             int page = dataView.getCurrentPage();
-            if(page < (count - 1))
+            if (page < (count - 1))
                 return dataView.getItemsPerPage() * (page + 1);
             else
                 return dataView.getDataProvider().size();
-                
+
         }
     }
 }
