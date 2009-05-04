39697e989cc948eac9868375ae33dfda355d5723
arneke <arneke@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-04 01:54:27 +0000

    Fixing tests. Could not really figure out how to check for SimpleAjaxLink, so used label and clicklink instead
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12313 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: f89006cd51ac99075c16cc3a09a2d6a0a0a18c94 [formerly 4b121ebd7c4bbcf3c70f845544c6c561a1f0f0ce]
    Former-commit-id: 44ca6603f01af312ebbe2799596b2be34fdeadf8

--- community/web2/demo/src/test/java/org/geoserver/web/demo/SRSListPageTest.java
+++ community/web2/demo/src/test/java/org/geoserver/web/demo/SRSListPageTest.java
@@ -1,13 +1,12 @@
 package org.geoserver.web.demo;
 
 import org.geoserver.web.GeoServerWicketTestSupport;
-import org.apache.wicket.markup.html.link.Link;
 
 public class SRSListPageTest extends GeoServerWicketTestSupport {
     public void testBasicPage() throws Exception {
         tester.startPage(SRSListPage.class);
-        tester.assertComponent("srsListPanel:listContainer:srslist:0:codeLink", Link.class);
-        tester.clickLink("srsListPanel:listContainer:srslist:0:codeLink");
+        tester.assertLabel("srsListPanel:table:listContainer:items:1:itemProperties:0:component:link:label","2000");
+        tester.clickLink("srsListPanel:table:listContainer:items:1:itemProperties:0:component:link");
         tester.assertRenderedPage(SRSDescriptionPage.class);
     }
 }
