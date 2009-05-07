61f3e1531a9c9556e047cecdb2251281d7cb90e0
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-07 08:26:30 +0000

    Adding copyright headers
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12348 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1d8f80796fdab5d1770f0b6d0bda84fe276a31e8 [formerly ce593875e0d849800d6cef4eb3e192719974dcc7]
    Former-commit-id: be5c747dfda2831eb8627554017b60d73f4d752b

--- src/community/web2/core/src/main/java/org/geoserver/web/UnauthorizedPage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/UnauthorizedPage.java
@@ -1,3 +1,7 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web;
 
 import org.apache.wicket.markup.html.basic.Label;
@@ -6,8 +10,6 @@ import org.apache.wicket.model.ResourceModel;
 
 /**
  * Displays a message suggesting the user to login or to elevate his privileges
- * @author Andrea Aime - TOPP
- *
  */
 public class UnauthorizedPage extends GeoServerBasePage {
 
