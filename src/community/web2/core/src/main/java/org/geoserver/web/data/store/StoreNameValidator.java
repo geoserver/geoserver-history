90bd8de8a15488f7720212323ba8efea35b7a1ac
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-07 20:24:09 +0000

    GEOS-2995, made StoreNameValidator an IFormValidator to perform the validation taking into acount the selected workspace
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12368 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b5cd1ab50a1a10fb8c9e44c486a0e6bd06bd9e70 [formerly 702036d8ee7ad55f4cf3a74eb6cff463e18376a2]
    Former-commit-id: 630f0f1b1fc5ccb98cf4295bf1341a553180d68e

--- src/community/web2/core/src/main/java/org/geoserver/web/data/store/StoreNameValidator.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/data/store/StoreNameValidator.java
@@ -1,36 +1,88 @@
+/* Copyright (c) 2001 - 2009 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
 package org.geoserver.web.data.store;
 
-import org.apache.wicket.validation.IValidatable;
-import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.validation.IFormValidator;
+import org.apache.wicket.validation.IValidationError;
 import org.apache.wicket.validation.ValidationError;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.StoreInfo;
+import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.web.GeoServerApplication;
 
 /**
- * Check it's not already configured (we assume another validator already dealt
- * with the value non being null or empty, so here we just have to check it's
- * not in the catalog
+ * A Form validator that takes the workspace and store name form components and validates there's no
+ * an existing {@link StoreInfo} in the selected workspace with the same name than the one assigned
+ * through the store name form component.
  * 
  * @author Andrea Aime - OpenGeo
+ * @author Gabriel Roldan - OpenGeo
  */
 @SuppressWarnings("serial")
-class StoreNameValidator implements IValidator {
-    Class<? extends StoreInfo> storeType;
+class StoreNameValidator implements IFormValidator {
 
-    public StoreNameValidator(Class<? extends StoreInfo> storeType) {
-        this.storeType = storeType;
+    FormComponent workspaceComponent;
+
+    FormComponent storeNameComponent;
+
+    private String edittingStoreId;
+
+    /**
+     * 
+     * @param workspaceFormComponent
+     *            the form component for the {@link WorkspaceInfo} assigned to the {@link StoreInfo}
+     *            being edited
+     * @param storeNameFormComponent
+     *            the form component for the name assigned to the {@link StoreInfo}
+     * @param edittingStoreId
+     *            the id for the store being edited. May be {@code null} if we're talking of a new
+     *            Store
+     */
+    public StoreNameValidator(final FormComponent workspaceFormComponent,
+            final FormComponent storeNameFormComponent, final String edittingStoreId) {
+        this.workspaceComponent = workspaceFormComponent;
+        this.storeNameComponent = storeNameFormComponent;
+        this.edittingStoreId = edittingStoreId;
     }
 
-    public void validate(IValidatable validatable) {
-        String value = (String) validatable.getValue();
+    public FormComponent[] getDependentFormComponents() {
+        return new FormComponent[] { workspaceComponent, storeNameComponent };
+    }
+
+    /**
+     * Performs the cross validation between the selected workspace and the assigned store name
+     * <p>
+     * If there's already a {@link StoreInfo} in the selected workspace with the same name than the
+     * choosed one the store name form component is set with a proper {@link IValidationError error
+     * message}
+     * </p>
+     * 
+     * @see IFormValidator#validate(Form)
+     */
+    public void validate(final Form form) {
+        final FormComponent[] components = getDependentFormComponents();
+        final FormComponent wsComponent = components[0];
+        final FormComponent nameComponent = components[1];
+
+        WorkspaceInfo workspace = (WorkspaceInfo) wsComponent.getConvertedInput();
+        String name = (String) nameComponent.getConvertedInput();
 
         Catalog catalog = GeoServerApplication.get().getCatalog();
-        if (catalog.getStoreByName(value, storeType) != null) {
-            ValidationError error = new ValidationError();
-            error.setMessage("The Data Source name '" + value
-                    + "' is already used");
-            validatable.error(error);
+
+        final StoreInfo existing = catalog.getStoreByName(workspace, name, StoreInfo.class);
+        if (existing != null) {
+            final String existingId = existing.getId();
+            if (!existingId.equals(edittingStoreId)) {
+                ValidationError error = new ValidationError();
+                error.addMessageKey("StoreNameValidator.storeExistsInWorkspace");
+                error.setVariable("workspace", workspace.getName());
+                error.setVariable("storeName", name);
+                nameComponent.error((IValidationError) error);
+            }
         }
     }
 
