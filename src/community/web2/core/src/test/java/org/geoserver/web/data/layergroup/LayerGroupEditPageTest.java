920cc793196d110d9c14f999031ff07ac0295ded
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-07 20:14:49 +0000

    GEOS-3005) New layer group will  report an IllegalArgumentException if the layer group name already exists
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12366 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 72a0de5a326c1e7cefef0e26b2d64e8f4fa7dc6d [formerly 85f6d93db7df4f35fcb58b447d169c5844b87622]
    Former-commit-id: 0ac192002545492ba78db919f363be4589ee16d8

--- src/community/web2/core/src/test/java/org/geoserver/web/data/layergroup/LayerGroupEditPageTest.java
+++ src/community/web2/core/src/test/java/org/geoserver/web/data/layergroup/LayerGroupEditPageTest.java
@@ -1,47 +1,8 @@
 package org.geoserver.web.data.layergroup;
 
-import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.CatalogBuilder;
-import org.geoserver.catalog.FeatureTypeInfo;
-import org.geoserver.catalog.LayerGroupInfo;
-import org.geoserver.data.test.MockData;
-import org.geoserver.web.GeoServerWicketTestSupport;
-import org.geotools.geometry.jts.ReferencedEnvelope;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
 
-public class LayerGroupEditPageTest extends GeoServerWicketTestSupport {
+public class LayerGroupEditPageTest extends LayerGroupBaseTest {
     
-    @Override
-    protected void setUpInternal() throws Exception {
-        Catalog catalog = getCatalog();
-        String lakes = MockData.LAKES.getLocalPart();
-        String forests = MockData.FORESTS.getLocalPart();
-        String bridges = MockData.BRIDGES.getLocalPart();
-        
-        setNativeBox(catalog, lakes);
-        setNativeBox(catalog, forests);
-        setNativeBox(catalog, bridges);
-        
-        LayerGroupInfo lg = catalog.getFactory().createLayerGroup();
-        lg.setName("lakes");
-        lg.getLayers().add(catalog.getLayerByName(lakes));
-        lg.getStyles().add(catalog.getStyleByName(lakes));
-        lg.getLayers().add(catalog.getLayerByName(forests));
-        lg.getStyles().add(catalog.getStyleByName(forests));
-        lg.getLayers().add(catalog.getLayerByName(bridges));
-        lg.getStyles().add(catalog.getStyleByName(bridges));
-        CatalogBuilder builder = new CatalogBuilder(catalog);
-        builder.calculateLayerGroupBounds(lg);
-        catalog.add(lg);
-    }
-    
-    public void setNativeBox(Catalog catalog, String name) throws Exception {
-        FeatureTypeInfo fti = catalog.getFeatureTypeByName(name);
-        fti.setNativeBoundingBox(fti.getFeatureSource(null, null).getBounds());
-        fti.setLatLonBoundingBox(new ReferencedEnvelope(fti.getNativeBoundingBox(), DefaultGeographicCRS.WGS84));
-        catalog.save(fti);
-    }
-
     public void testComputeBounds() {
         LayerGroupEditPage page = new LayerGroupEditPage(getCatalog().getLayerGroupByName("lakes"));
         tester.startPage(page);
