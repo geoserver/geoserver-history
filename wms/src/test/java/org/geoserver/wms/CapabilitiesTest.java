2d1f1ecc79e522e84c2c3db51e22bb4974d487bb
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-19 04:34:56 +0000

    fix for GEOS-3053 and a minimal test to ensure layers are added to the capabilities document
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12432 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4f644f2cee24ed4ce36a969c1995f4c8cb8d87bc [formerly 0fc5c6a6a5592cbdabde349519789e8c94536c39]
    Former-commit-id: 58bfa0020f6d858d511fdfd40b8238620177afcd

--- wms/src/test/java/org/geoserver/wms/CapabilitiesTest.java
+++ wms/src/test/java/org/geoserver/wms/CapabilitiesTest.java
@@ -1,15 +1,39 @@
 package org.geoserver.wms;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import junit.framework.Test;
+
 import org.custommonkey.xmlunit.XMLUnit;
 import org.custommonkey.xmlunit.XpathEngine;
+import org.geoserver.catalog.LayerGroupInfo;
+import org.geoserver.catalog.LayerInfo;
+import org.geoserver.data.test.MockData;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 
 public class CapabilitiesTest extends WMSTestSupport {
     
     public CapabilitiesTest() {
         super();
+    }
+
+    /**
+     * This is a READ ONLY TEST so we can use one time setup
+     */
+    public static Test suite() {
+        return new OneTimeTestSetup(new CapabilitiesTest());
+    }
+
+    @Override
+    protected void populateDataDirectory(MockData dataDirectory)
+            throws Exception {
+        super.populateDataDirectory(dataDirectory);
+        dataDirectory.addWellKnownCoverageTypes();
     }
 
     public void testCapabilities() throws Exception {
@@ -27,4 +51,21 @@ public class CapabilitiesTest extends WMSTestSupport {
         assertEquals(0, xpath.getMatchingNodes("//Layer/Name[not(starts-with(., cite))]", dom).getLength());
     }
     
+    public void testLayerCount() throws Exception {
+        List<LayerInfo> layers = new ArrayList<LayerInfo>(getCatalog().getLayers());
+        for (ListIterator<LayerInfo> it = layers.listIterator(); it.hasNext();) {
+            LayerInfo next = it.next();
+            if (next.getName().equals(MockData.GEOMETRYLESS.getLocalPart())) {
+                it.remove();
+            }
+        }
+
+        Document dom = dom(get("wms?request=getCapabilities"), true);
+
+        XpathEngine xpath = XMLUnit.newXpathEngine();
+        NodeList nodeLayers = xpath.getMatchingNodes("/WMT_MS_Capabilities/Capability/Layer/Layer",
+                dom);
+
+        assertEquals(layers.size(), nodeLayers.getLength());
+    }
 }
