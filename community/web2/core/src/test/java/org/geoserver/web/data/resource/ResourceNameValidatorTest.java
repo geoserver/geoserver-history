852c67b0061c82c6dc9a5f511610e6037295232a
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-22 11:23:35 +0000

    GEOS-3065, Can't create file based CoverageStore with relative URL, FileExistsValidator does not find my URL's
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12458 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 71c641a9daac505a4c5d366be0ceccccaca93168 [formerly 9fab47a3d228a1928dcca0b9bd7a092f2b1f5f9f]
    Former-commit-id: 3e752e1c7dc7fd1c20c5b0badfb7be9b32d4c87f

--- community/web2/core/src/test/java/org/geoserver/web/data/resource/ResourceNameValidatorTest.java
+++ community/web2/core/src/test/java/org/geoserver/web/data/resource/ResourceNameValidatorTest.java
@@ -1,38 +1,13 @@
 package org.geoserver.web.data.resource;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.wicket.validation.IValidatable;
-import org.apache.wicket.validation.IValidationError;
+import org.geoserver.web.StringValidatable;
 import org.geoserver.web.data.resource.BasicResourceConfig.ResourceNameValidator;
 
 import junit.framework.TestCase;
 
 public class ResourceNameValidatorTest extends TestCase {
 
-    static class StringValidatable implements IValidatable {
-        List<IValidationError> errors = new ArrayList<IValidationError>();
-        String value;
-        
-        public StringValidatable(String value) {
-            this.value = value;
-        }
-
-        public void error(IValidationError error) {
-            errors.add(error);            
-        }
-
-        public Object getValue() {
-            return value;
-        }
-
-        public boolean isValid() {
-            return errors.size() == 0;
-        }
-        
-    }
-    
     public void testValidUnderscoreMiddle() {
         StringValidatable validatable = new StringValidatable("abc_def");
         new ResourceNameValidator().validate(validatable);
