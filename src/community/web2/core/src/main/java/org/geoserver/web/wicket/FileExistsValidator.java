30354c9e62db953368487574d3d833e643ce3e9b
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-25 19:50:35 +0000

    GEOS-3072, web2 FileExistsValidatorTest.testAbsoluteRaw() fails on Windows, breaking the build
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12487 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 77aa427b933107b5a97341e7825011955fac61fa [formerly cb1984e9f28987555124bd747a7ed3effee0f16b]
    Former-commit-id: 0f96672d641bf8b3505d98aec354f5f3ef3b0a31

--- src/community/web2/core/src/main/java/org/geoserver/web/wicket/FileExistsValidator.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/wicket/FileExistsValidator.java
@@ -6,8 +6,10 @@ package org.geoserver.web.wicket;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 
+import org.apache.commons.httpclient.URIException;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.validator.AbstractValidator;
 import org.geotools.util.Converters;
@@ -23,29 +25,26 @@ public class FileExistsValidator extends AbstractValidator {
     protected void onValidate(IValidatable validatable) {
         String uriSpec = Converters.convert(validatable.getValue(), String.class);
         
+        // Make sure we are dealing with a local path
         try {
-            // Make sure we are dealing with a local path
             URI uri = new URI(uriSpec);
             if(uri.getScheme() != null && !"file".equals(uri.getScheme()))
                 return;
             
             // ok, strip away the scheme and just get to the path
             String path = uri.getPath();
-            
-            // absolute path?
-            if(path == null || !new File(path).exists()) {
-                // local to data dir?
-                File relFile = GeoserverDataDirectory.findDataFile(uriSpec);
-                if(relFile == null || !relFile.exists()) {
-                    error(validatable, "FileExistsValidator.fileNotFoundError", 
-                            Collections.singletonMap("file", path));
-                }
-            }
-        } catch(Exception e) {
-            error(validatable, "FileExistsValidator.invalidPathError", 
-                    Collections.singletonMap("path", uriSpec));
+            if(path != null && new File(path).exists())
+                return;
+        } catch(URISyntaxException e) {
+            // may be a windows path, move on               
         }
         
+        // local to data dir?
+        File relFile = GeoserverDataDirectory.findDataFile(uriSpec);
+        if(relFile == null || !relFile.exists()) {
+            error(validatable, "FileExistsValidator.fileNotFoundError", 
+                    Collections.singletonMap("file", uriSpec));
+        }
     }
 
 }
