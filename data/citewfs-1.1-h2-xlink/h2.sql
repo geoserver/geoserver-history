af68fba0557df305daf26662697fbc3992bf584c
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-26 23:23:18 +0000

    GEOS-3071, updating h2 setup
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12496 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: d85e47795f5e884dd5862d8ec13811c038edf6ad [formerly 4bea24e8c6cf5aad454f5414030e25f6d2795771]
    Former-commit-id: d4227a805886114e278babfdaabd3a89f9ebd8f7

--- data/citewfs-1.1-h2-xlink/h2.sql
+++ data/citewfs-1.1-h2-xlink/h2.sql
@@ -1,9 +1,8 @@
--- H2 spatial functions
-
-CREATE ALIAS AsWKT for "org.openplans.spatialdbbox.JTS.AsWKT";
-CREATE ALIAS AsText for "org.openplans.spatialdbbox.JTS.AsWKT";
-CREATE ALIAS EnvelopeAsText for "org.openplans.spatialdbbox.JTS.EnvelopeAsText";
-CREATE ALIAS GeomFromText FOR "org.openplans.spatialdbbox.JTS.GeomFromText";
-CREATE ALIAS Envelope FOR "org.openplans.spatialdbbox.JTS.Envelope";
-CREATE ALIAS GetSRID FOR "org.openplans.spatialdbbox.JTS.GetSRID";
-CREATE ALIAS GeometryType FOR "org.openplans.spatialdbbox.JTS.GeometryType";
+CREATE ALIAS GeoToolsVersion for "org.geotools.data.h2.JTS.GeoToolsVersion";
+CREATE ALIAS AsWKT for "org.geotools.data.h2.JTS.AsWKT";
+CREATE ALIAS AsText for "org.geotools.data.h2.JTS.AsWKT";
+CREATE ALIAS EnvelopeAsText for "org.geotools.data.h2.JTS.EnvelopeAsText";
+CREATE ALIAS GeomFromText for "org.geotools.data.h2.JTS.GeomFromText";
+CREATE ALIAS Envelope for "org.geotools.data.h2.JTS.Envelope";
+CREATE ALIAS GetSRID FOR "org.geotools.data.h2.JTS.GetSRID";
+CREATE ALIAS GeometryType for "org.geotools.data.h2.JTS.GeometryType";
