4f8c9b085cad4944a097260e795437e2f9453a64
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-05-29 19:37:08 +0000

    GEOS-3105, use ResourcePool to get the coverage so it resolves relative URL's for us
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12517 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ed7fae2908a71b30aaf4556270ad8b235ae6d8b6 [formerly c1dbf8bfbe5defce2d0e6ffa3d8570715c353e25]
    Former-commit-id: 9791d8edd7a06d050ec51f977408cffdf2b3065c

--- community/web2/core/src/main/java/org/geoserver/web/data/store/CoverageStoreEditPage.java
+++ community/web2/core/src/main/java/org/geoserver/web/data/store/CoverageStoreEditPage.java
@@ -13,9 +13,6 @@ import org.apache.wicket.markup.html.form.Form;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.CoverageStoreInfo;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
-import org.geotools.coverage.grid.io.GridFormatFinder;
-import org.geotools.coverage.grid.io.UnknownFormat;
-import org.opengis.coverage.grid.Format;
 import org.opengis.coverage.grid.GridCoverageReader;
 
 /**
@@ -46,32 +43,30 @@ public class CoverageStoreEditPage extends AbstractCoverageStorePage {
         final Catalog catalog = getCatalog();
 
         final String url = info.getURL();
+        if (null == info.getType()) {
+            throw new IllegalArgumentException("Coverage type has not been set");
+        }
+
         Map<String, Serializable> connectionParameters = info.getConnectionParameters();
 
-        Format format = GridFormatFinder.findFormat(url);
-        if (format instanceof UnknownFormat) {
+        AbstractGridFormat gridFormat = catalog.getResourcePool().getGridCoverageFormat(info);
+        if (gridFormat == null) {
             throw new IllegalArgumentException(
                     "No grid format found capable of connection to the provided URL");
         }
-        if (!(format instanceof AbstractGridFormat)) {
-            throw new IllegalArgumentException("Unknown format type: "
-                    + format.getClass().getName());
-        }
-        AbstractGridFormat gridFormat = (AbstractGridFormat) format;
+
         GridCoverageReader reader;
         try {
-            reader = gridFormat.getReader(url);
-        } catch (RuntimeException e) {
+            // get the reader through ResourcePool so it resolves relative URL's for us
+            reader = catalog.getResourcePool().getGridCoverageReader(info, null);
+        } catch (IOException e) {
             LOGGER
                     .log(Level.INFO, "Editing CoverageStore " + info.getName() + ": <" + url + ">",
                             e);
             throw new IllegalArgumentException("Unable to access the coverage. Original error: "
                     + e.getMessage());
         }
-        if (reader == null) {
-            throw new IllegalArgumentException(
-                    "Can't obtain a coverage reader for the provided URL");
-        }
+
         try {
             reader.dispose();
         } catch (IOException e) {
