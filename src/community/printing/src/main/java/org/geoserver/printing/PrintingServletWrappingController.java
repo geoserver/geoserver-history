425f03cd1e82d50a820cb424df14f95fd500c710
agerber <agerber@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-06-16 19:18:15 +0000

    printing. now with more code conventions
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12640 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 09352d19cd276b29d2025a9a5fb6ae4f29f55000 [formerly 5f6704f68f5b78119b90accbe0e07469e879df45]
    Former-commit-id: fb23023fd812e4f3f318c693ac1ef83174c8bfde

--- src/community/printing/src/main/java/org/geoserver/printing/PrintingServletWrappingController.java
+++ src/community/printing/src/main/java/org/geoserver/printing/PrintingServletWrappingController.java
@@ -14,28 +14,28 @@ import org.vfny.geoserver.global.GeoserverDataDirectory;
  *
  */
 public class PrintingServletWrappingController extends
-		ServletWrappingController {
-	
+ServletWrappingController {
+
     private Logger LOG = org.geotools.util.logging.Logging.getLogger("org.geoserver.printing");
-	
-	public void setInitParameters(Properties initParameters)
-	{
-		/*find the config parameter and update it so it points to $GEOSERVER_DATA_DIR/printing/$CONFIG */
-		String configProp = initParameters.getProperty("config");		
-		try{
-			File dir = GeoserverDataDirectory.findCreateConfigDir("printing");
-			File qualifiedConfig = new File(dir, configProp);
-			initParameters.setProperty("config", qualifiedConfig.getCanonicalPath());			
-		}
-		catch(org.vfny.geoserver.global.ConfigurationException e){
-			LOG.warning("Explosion while attempting to access/create config directory for MapFish " +
-					"printing module.  Module will fail when run. Config exception is: " + e);
-		}
-		catch(java.io.IOException e){
-			LOG.warning("Explosion while calculating canonical path for MapFish printing servlet. " +
-					"Module will fail when run.  IO Exception is: " + e);
-		}
-		super.setInitParameters(initParameters);
-	}
+
+    public void setInitParameters(Properties initParameters)
+    {
+        /*find the config parameter and update it so it points to $GEOSERVER_DATA_DIR/printing/$CONFIG */
+        String configProp = initParameters.getProperty("config");		
+        try{
+            File dir = GeoserverDataDirectory.findCreateConfigDir("printing");
+            File qualifiedConfig = new File(dir, configProp);
+            initParameters.setProperty("config", qualifiedConfig.getCanonicalPath());			
+        }
+        catch(org.vfny.geoserver.global.ConfigurationException e){
+            LOG.warning("Explosion while attempting to access/create config directory for MapFish " +
+                    "printing module.  Module will fail when run. Config exception is: " + e);
+        }
+        catch(java.io.IOException e){
+            LOG.warning("Explosion while calculating canonical path for MapFish printing servlet. " +
+                    "Module will fail when run.  IO Exception is: " + e);
+        }
+        super.setInitParameters(initParameters);
+    }
 
 }
