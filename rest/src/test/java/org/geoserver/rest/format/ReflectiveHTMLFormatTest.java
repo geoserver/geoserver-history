bcee91ece77deb22f09829f6f89684057be92be6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-06-17 17:27:34 +0000

    ported rest module from 1.7.x
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12647 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3386492217a5a4c4f94ae2f544c40f98e9767d43 [formerly 7541986b5f0a75dce6186ae94a98c2497ba1ae60]
    Former-commit-id: 09b0a3370f346421f12b4d283d598e26b7eb7793

---
+++ rest/src/test/java/org/geoserver/rest/format/ReflectiveHTMLFormatTest.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.rest.format;
+
+import org.geoserver.rest.Foo;
+import org.geoserver.rest.format.ReflectiveHTMLFormat;
+import org.restlet.data.Request;
+import org.restlet.resource.Representation;
+
+import junit.framework.TestCase;
+
+public class ReflectiveHTMLFormatTest extends TestCase {
+
+    public void test() throws Exception {
+        Request request = new Request();
+        request.setResourceRef( "http://localhost/rest/foo.html");
+        request.getResourceRef().setBaseRef( "http://localhost/rest");
+        
+        ReflectiveHTMLFormat fmt = new ReflectiveHTMLFormat(Foo.class,request,null,null);
+        Representation rep = fmt.toRepresentation(new Foo("one",2,3.0));
+        rep.write(System.out);
+    }
+    
+}
