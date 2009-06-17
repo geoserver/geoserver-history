9e60c037ed751d34a49d3f57791822d00656f8eb
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-06-17 17:27:34 +0000

    ported rest module from 1.7.x
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12647 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 503abcef71c1f54333328dc9383ac40f93b4ae9b [formerly 56e462c9bb4b23316742c19bd57381afca179570]
    Former-commit-id: 3e43d16d09c21e20c9b43d72338e0409d14d1c98

---
+++ src/rest/src/main/java/org/geoserver/rest/BeanDelegatingRestlet.java
@@ -0,0 +1,40 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org.  All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.rest;
+
+import org.restlet.Restlet;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * A place holder restlet which delegates to another restlet which is 
+ * defined in a spring context.
+ * 
+ * @author David Winslow, OpenGEO
+ *
+ */
+public class BeanDelegatingRestlet extends Restlet{
+    /**
+     * name of the bean to lookup.
+     */
+    String beanName;
+    /**
+     * the application context.
+     */
+    ApplicationContext context;
+
+
+    public BeanDelegatingRestlet(ApplicationContext context, String beanName){
+       this.context = context;
+       this.beanName = beanName;
+    }
+
+    public void handle(Request req, Response res){
+        Object bean = context.getBean(beanName);
+        Restlet restlet = (Restlet)bean;
+        restlet.handle(req, res);
+    }
+}
