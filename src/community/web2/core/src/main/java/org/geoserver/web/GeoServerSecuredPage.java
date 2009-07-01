a8e093714a1c5d565c5e680f76c3cc83f4d37e5e
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-01 16:11:25 +0000

    Make sure the standalong login page redirects back to the page that the user was supposed to reach on successful login
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12726 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 69721a6aa22e1987ea96e0d33353e4b5e072b00e [formerly 9119ddb92927e3b0fb6d0852ad3c9268cc2993ec]
    Former-commit-id: e5c4d290c3670c3cd76c81611c339cf43fa1094f

--- src/community/web2/core/src/main/java/org/geoserver/web/GeoServerSecuredPage.java
+++ src/community/web2/core/src/main/java/org/geoserver/web/GeoServerSecuredPage.java
@@ -4,7 +4,15 @@
  */
 package org.geoserver.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.acegisecurity.Authentication;
+import org.acegisecurity.ui.AbstractProcessingFilter;
+import org.acegisecurity.ui.ExceptionTranslationFilter;
+import org.acegisecurity.ui.savedrequest.SavedRequest;
+import org.acegisecurity.ui.webapp.AuthenticationProcessingFilter;
+import org.apache.wicket.protocol.http.WebRequest;
 
 
 /**
@@ -20,8 +28,18 @@ public class GeoServerSecuredPage extends GeoServerBasePage {
     public GeoServerSecuredPage() {
         super();
         Authentication auth = getSession().getAuthentication();
-        if(auth == null || !auth.isAuthenticated())
+        if(auth == null || !auth.isAuthenticated()) {
+            // emulate what acegi url control would do so that we get a proper redirect after login
+            HttpServletRequest httpRequest = ((WebRequest) getRequest()).getHttpServletRequest();
+            ExceptionTranslationFilter translator = (ExceptionTranslationFilter) getGeoServerApplication().getBean("consoleExceptionTranslationFilter");
+            SavedRequest savedRequest = new SavedRequest(httpRequest, translator.getPortResolver());
+            
+            HttpSession session = httpRequest.getSession();
+            session.setAttribute(AbstractProcessingFilter.ACEGI_SAVED_REQUEST_KEY, savedRequest);
+            
+            // then redirect to the login page
             setResponsePage(GeoServerLoginPage.class);
+        }
         else if (!getPageAuthorizer().isAccessAllowed(this.getClass(), auth))
             setResponsePage(UnauthorizedPage.class);
     }
