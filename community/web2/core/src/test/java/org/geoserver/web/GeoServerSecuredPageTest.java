86d2e370ad87cff9ea7f0b2aeea4fcd1c3437075
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-01 16:11:25 +0000

    Make sure the standalong login page redirects back to the page that the user was supposed to reach on successful login
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12726 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 282b0c5fc5788cc33ce3ae77b541a35f636b8933 [formerly 64e3fa9743bf96853e48363981ca44d9618b2671]
    Former-commit-id: b64fec94c681c89551bfe5c0c0f52b0d820ec29b

--- community/web2/core/src/test/java/org/geoserver/web/GeoServerSecuredPageTest.java
+++ community/web2/core/src/test/java/org/geoserver/web/GeoServerSecuredPageTest.java
@@ -1,5 +1,7 @@
 package org.geoserver.web;
 
+import org.acegisecurity.ui.AbstractProcessingFilter;
+import org.acegisecurity.ui.savedrequest.SavedRequest;
 import org.geoserver.web.data.layer.LayerPage;
 
 public class GeoServerSecuredPageTest extends GeoServerWicketTestSupport {
@@ -7,6 +9,10 @@ public class GeoServerSecuredPageTest extends GeoServerWicketTestSupport {
         logout();
         tester.startPage(LayerPage.class);
         tester.assertRenderedPage(GeoServerLoginPage.class);
+        // make sure the acegi emulation is properly setup
+        SavedRequest sr = (SavedRequest) tester.getServletSession().getAttribute(AbstractProcessingFilter.ACEGI_SAVED_REQUEST_KEY);
+        assertNotNull(sr);
+        assertTrue(sr.getFullRequestUrl().endsWith("?wicket:bookmarkablePage=:org.geoserver.web.data.layer.LayerPage"));
     }
 
     public void testSecuredPageAllowsAccessWhenLoggedIn() {
