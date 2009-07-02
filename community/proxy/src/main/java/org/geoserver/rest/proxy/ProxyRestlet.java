13470d92749f83c374fe02f586c3c3106cf51fa9
agerber <agerber@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-02 22:40:53 +0000

    Getting toward code complete (need to finish restlet)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12736 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1970783fe3cbd564ec32de4b569d71f1af0384e7 [formerly 44c627905792f35d1919bf0f9bd04d259e1711b8]
    Former-commit-id: 7482ca49c1ad48b9efef091b6f8f77a2a8fd2fe7

--- community/proxy/src/main/java/org/geoserver/rest/proxy/ProxyRestlet.java
+++ community/proxy/src/main/java/org/geoserver/rest/proxy/ProxyRestlet.java
@@ -8,8 +8,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.geoserver.proxy.ProxyConfig;
+import org.geoserver.proxy.ProxyConfig.Mode;
 import org.geoserver.rest.RestletException;
+import org.geoserver.security.PropertyFileWatcher;
 import org.restlet.Restlet;
 import org.restlet.data.Form;
 import org.restlet.data.MediaType;
@@ -24,17 +29,28 @@ import org.restlet.resource.StreamRepresentation;
  * webserver.
  * 
  * @author David Winslow <cdwinslow@opengeo.org>
+ * @author Alan Gerber <agerber@openplans.org>
  */
 public class ProxyRestlet extends Restlet {
     private static Logger LOGGER = org.geotools.util.logging.Logging.getLogger(ProxyRestlet.class);
-
+    private ProxyConfig config;
+    private PropertyFileWatcher configWatcher = new PropertyFileWatcher(ProxyConfig.proxyConfFile);
+    
     @Override
     public void handle(Request request, Response response) {
+    	/*Check the proxy's config has been modified*/
+    	if (configWatcher.isStale())
+    	{
+    		config = ProxyConfig.loadConfFromDisk();
+    	}
+    	/*Grab the argument*/
         Form f = request.getResourceRef().getQueryAsForm();
+        /*The first argument should be the request for a URL to grab by proxy*/
         String url = f.getFirstValue("url");
         try {
             URL resolved = new URL(url);
-            if (resolved.getProtocol().equals("http")) {
+            /*Allow the request to go through if it's in the list of allowed URLs*/
+            if (this.checkURLPermission(resolved) == true) {
                 final HttpURLConnection connection = (HttpURLConnection) resolved.openConnection();
                 connection.setRequestMethod(request.getMethod().toString());
                 
@@ -55,7 +71,9 @@ public class ProxyRestlet extends Restlet {
                         throw new UnsupportedOperationException();
                     }
                 });
-            } else {
+            }
+            /*Otherwise tell the client it can't do what it wanted to*/
+            else {
                 throw new RestletException("Proxy only accepts HTTP requests",
                         Status.CLIENT_ERROR_BAD_REQUEST);
             }
@@ -67,6 +85,58 @@ public class ProxyRestlet extends Restlet {
             throw new RestletException("Couldn't connect to proxied service",
                     Status.SERVER_ERROR_BAD_GATEWAY);
         }
+    }
+    
+/*    public void init(List<String> permissionRegexes){
+    	//for 
+    }*/
+    
+    /*Checks a URL against all regular expressions for permitted locations to connect*/
+    private boolean checkURLPermission(URL locator) {    	
+    	/*Check that the correct protocol is being used*/
+        if (locator.getProtocol().equals("http")) {
+        	boolean hostnameOk = false, mimetypeOk = false;
+        	/*Check if the hostname matches a whitelist if configured to do so*/
+        	if (config.mode == Mode.HOSTNAME ||
+        		config.mode == Mode.HOSTNAMEANDMIMETYPE ||
+        		config.mode == Mode.HOSTNAMEORMIMETYPE) {
+        		/*Iterate through the whitelist of hosts*/
+        		for (Pattern pattern : config.hostnameWhitelist) {
+        			/*Check if the regex matches the URL.*/
+        			Matcher matcher = pattern.matcher(locator.toString());
+        			if (matcher.find()){
+        				/*Then the URL is allowed.*/
+        				hostnameOk = true;
+        			}
+        		}
+        	}
+    		/*Check if the MIMEType matches a whitelist if configured to do so*/
+    		if (config.mode == Mode.MIMETYPE ||
+    			config.mode == Mode.HOSTNAMEANDMIMETYPE ||
+    			config.mode == Mode.HOSTNAMEORMIMETYPE) {
+    			//TODO: Actually check the MIMEType
+    			if (true) {
+    				mimetypeOk = true;
+    			}
+    			else {
+    				mimetypeOk = false;
+    			}
+        	}
+    		
+    		/*Return whether an action is permitted based on how proxy is configured*/
+    		switch(config.mode){
+    			case MIMETYPE:
+    				return mimetypeOk;
+    			case HOSTNAME:
+    				return hostnameOk;
+    			case HOSTNAMEANDMIMETYPE:
+    				return hostnameOk && mimetypeOk;
+    			case HOSTNAMEORMIMETYPE:
+    				return hostnameOk || mimetypeOk;
+    		}
+        }
+        /*The request is not permitted to go through.*/
+    	return false;
     }
 
     private void copyStream(InputStream in, OutputStream out) throws IOException {
