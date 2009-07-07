a2084ad3a28ec9c4eabc677a82a93905360de0aa
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-07 01:17:58 +0000

    GEOS-3211, ported CatalogRepository to main
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@12749 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 38682268a02f1b0886772c5eb7c8aff8c63b61a5 [formerly 376e8323c31c79c76a87e1c0b201617f1d3d5fc7]
    Former-commit-id: 2173371a06f9bbcd4160be176b2712d876cb9781

--- src/extension/feature-pregeneralized/src/main/java/org/geoserver/data/gen/CatalogRepository.java
+++ src/extension/feature-pregeneralized/src/main/java/org/geoserver/data/gen/CatalogRepository.java
@@ -2,93 +2,19 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-
-
 package org.geoserver.data.gen;
 
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Logger;
-
-import org.geoserver.catalog.Catalog;
-import org.geoserver.catalog.DataStoreInfo;
-import org.geoserver.platform.GeoServerExtensions;
-import org.geotools.data.DataAccess;
-import org.geotools.data.DataStore;
 import org.geotools.data.Repository;
-import org.opengis.feature.type.Name;
 
 /**
  * @author Christian Mueller
  * 
  * Repository implementation using the geoserver catalog
- *
+ * 
+ * @deprecated use {@link org.geoserver.catalog.CatalogRepository}
  */
-public class CatalogRepository implements Repository {
-		private Catalog catalogObject;
-		private Logger log; 
-		
-	public CatalogRepository() {
-		super();		
-		log= Logger.getLogger(this.getClass().getName());
-	}
-			
-	
-	
+public class CatalogRepository extends org.geoserver.catalog.CatalogRepository implements Repository {
 
-	public DataStore dataStore(Name name) {
-                
-                String workspace = name .getNamespaceURI();
-                String localName=name.getLocalPart();
-		
-		DataStoreInfo info = getCatalog().getDataStoreByName(workspace,localName);
-		if (info==null) {
-			throw new RuntimeException("Cannot find datastore "+ localName + "in workspace "+ workspace);
-		}
-		try {
-			return info.getDataStore(null);
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
-		}
-	}
-
-
-	public Catalog getCatalog() {
-            
-                if (catalogObject!=null) return catalogObject;
-            
-		String beanName="catalog2";
-		catalogObject = (Catalog) GeoServerExtensions.bean(beanName);
-		if (catalogObject==null) {
-			log.warning("Cannot find bean "+beanName);
-			catalogObject=(Catalog) GeoServerExtensions.bean(Catalog.class);			
-		}	
-		if (catalogObject==null) {
-			log.severe("Cannot find bean implementing "+Catalog.class.getName());
-			throw new RuntimeException("Cannot find geoserver catalog");			
-		}
-                return catalogObject;
-	}
-
-
-
-
-    public DataAccess<?, ?> access(Name name) {
-        return dataStore(name);
+    public CatalogRepository() {
     }
-
-
-
-
-    /* (non-Javadoc)
-     * @see org.geotools.data.Repository#getDataStores()
-     * 
-     * return empty list, is not the job of this repository
-     */
-    public List<DataStore> getDataStores() {
-        return Collections.emptyList();
-    }
-
 }
