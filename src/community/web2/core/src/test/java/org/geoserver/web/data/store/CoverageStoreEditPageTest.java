dac769dbcb263c5dbb9e41a239e67005bacde2bd
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-09 12:59:58 +0000

    Tests and fixes for the coverage store editor page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@12793 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2b4b6ce3fdd66316fb47d718020262bd5a129a57 [formerly d269ff7a57158dd3c0736d5bfb1e90711de30c95]
    Former-commit-id: c9f0f32a1bf5112b0f910bbc7b493e5a6b3a8d52

---
+++ src/community/web2/core/src/test/java/org/geoserver/web/data/store/CoverageStoreEditPageTest.java
@@ -0,0 +1,54 @@
+package org.geoserver.web.data.store;
+
+import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.catalog.CoverageStoreInfo;
+import org.geoserver.data.test.MockData;
+import org.geoserver.web.GeoServerWicketTestSupport;
+
+public class CoverageStoreEditPageTest extends GeoServerWicketTestSupport {
+
+    @Override
+    protected void populateDataDirectory(MockData dataDirectory) throws Exception {
+        super.populateDataDirectory(dataDirectory);
+        dataDirectory.addWellKnownCoverageTypes();
+    }
+    
+    @Override
+    protected void setUpInternal() throws Exception {
+        CoverageStoreInfo coverageStore = getCatalog().getStoreByName(MockData.TASMANIA_BM.getLocalPart(), CoverageStoreInfo.class);
+        tester.startPage(new CoverageStoreEditPage(coverageStore.getId()));
+        
+        // print(tester.getLastRenderedPage(), true, true);
+    }
+    
+    public void testLoad() {
+        tester.assertRenderedPage(CoverageStoreEditPage.class);
+        tester.assertNoErrorMessage();
+        
+        tester.assertLabel("rasterStoreForm:storeType", "GeoTIFF");
+        tester.assertModelValue("rasterStoreForm:namePanel:border:paramValue", "BlueMarble");
+    }
+    
+    public void testChangeName() {
+        FormTester form = tester.newFormTester("rasterStoreForm");
+        form.setValue("namePanel:border:paramValue", "BlueMarbleModified");
+        form.submit();
+        tester.clickLink("rasterStoreForm:save");
+        
+        tester.assertNoErrorMessage();
+        tester.assertRenderedPage(StorePage.class);
+        assertNotNull(getCatalog().getStoreByName("BlueMarbleModified", CoverageStoreInfo.class));
+    }
+    
+    public void testNameRequired() {
+        FormTester form = tester.newFormTester("rasterStoreForm");
+        form.setValue("namePanel:border:paramValue", null);
+        form.submit();
+        tester.clickLink("rasterStoreForm:save");
+        
+        tester.assertRenderedPage(CoverageStoreEditPage.class);
+        tester.assertErrorMessages(new String[] {"Store name is required"});
+    }
+    
+    
+}
