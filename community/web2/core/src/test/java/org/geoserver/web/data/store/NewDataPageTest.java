0d516ba201fe617d80f7bd5a40f92fe282561692
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-09 14:07:28 +0000

    More tests for the store list page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12795 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: eae0a01158b7fe725369c279abce437f65776d04 [formerly afe4bd716c3b9ffa868bb6db00b2ec83627e475c]
    Former-commit-id: 45584bad06d36b0a85dc1cfd3e49028442a469be

---
+++ community/web2/core/src/test/java/org/geoserver/web/data/store/NewDataPageTest.java
@@ -0,0 +1,42 @@
+/* Copyright (c) 2001 - 2009 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.web.data.store;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListView;
+import org.geoserver.web.GeoServerWicketTestSupport;
+
+public class NewDataPageTest extends GeoServerWicketTestSupport {
+
+    @Override
+    protected void setUpInternal() throws Exception {
+        tester.startPage(new NewDataPage());
+        
+        //print(tester.getLastRenderedPage(), true, true);
+    }
+    
+    public void testLoad() {
+        tester.assertRenderedPage(NewDataPage.class);
+        tester.assertNoErrorMessage();
+        
+        tester.assertComponent("storeForm:vectorResources", ListView.class);
+        tester.assertComponent("storeForm:rasterResources", ListView.class);
+    }
+    
+    public void testClickLink() {
+        Label label = (Label) findComponentByContent(tester.getLastRenderedPage(), "Properties", Label.class);
+        // getPath() will start with 0: which indicates the page
+        tester.clickLink(label.getParent().getPath().substring(2));
+        
+        tester.assertNoErrorMessage();
+        tester.assertRenderedPage(DataAccessNewPage.class);
+        print(tester.getLastRenderedPage(), true, true);
+        tester.assertModelValue("dataStoreForm:storeType", "Properties");
+        
+    }
+
+}
