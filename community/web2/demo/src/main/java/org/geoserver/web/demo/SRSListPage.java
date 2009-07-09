df9a4f85c68c3f301681086454621732ca663982
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-09 15:41:27 +0000

    Adapt demo page to changes in srslistpanel
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12802 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 145175a4be6f3352b7d4b73ae26e2bd9d794b8d9 [formerly b34c1ceae3e991dd207429933b02df14981c93eb]
    Former-commit-id: 3bcd07c2fc3c51ad962e366d0c54cf99525c844d

--- community/web2/demo/src/main/java/org/geoserver/web/demo/SRSListPage.java
+++ community/web2/demo/src/main/java/org/geoserver/web/demo/SRSListPage.java
@@ -4,14 +4,10 @@
  */
 package org.geoserver.web.demo;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.model.IModel;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.wicket.SRSListPanel;
-import org.geoserver.web.wicket.SRSProvider;
-import org.geoserver.web.wicket.SimpleAjaxLink;
 
 /**
  * Lists all the SRS available in GeoServer
@@ -25,16 +21,11 @@ public class SRSListPage extends GeoServerBasePage {
 
     SRSListPanel srsListPanel() {
         return new SRSListPanel("srsListPanel") {
+
             @Override
-            protected Component createLinkForCode(String linkId, IModel itemModel) {
-                return new SimpleAjaxLink(linkId, SRSProvider.CODE.getModel(itemModel)) {
-                    @Override
-                    public void onClick(AjaxRequestTarget target) {
-                        String epsgCode = getModelObjectAsString();
-                        setResponsePage(SRSDescriptionPage.class, new PageParameters("code=EPSG:"
-                                + epsgCode));
-                    }
-                };
+            protected void onCodeClicked(AjaxRequestTarget target, String epsgCode) {
+                setResponsePage(SRSDescriptionPage.class, new PageParameters("code=EPSG:"
+                        + epsgCode));
             }
         };
     }
