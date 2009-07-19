8d366681ee636e366d1d7b1eb754e176bba46d6d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-19 16:44:40 +0000

    Ported over a forgotten test
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@12878 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4259cbe3d90f028535b99db4d3ae0e53733a77c8 [formerly ae33bc4825c5da04d0a77fab376da776df6aa204]
    Former-commit-id: d227ebfd317ba5b9ea415157e842fa6d28b0a3bd

--- wfs/src/test/java/org/geoserver/wfs/response/ShapeZipTest.java
+++ wfs/src/test/java/org/geoserver/wfs/response/ShapeZipTest.java
@@ -34,6 +34,8 @@ public class ShapeZipTest extends WFSTestSupport {
     private static final QName ALL_TYPES = new QName(MockData.CITE_URI, "AllTypes", MockData.CITE_PREFIX);
     private static final QName GEOMMID = new QName(MockData.CITE_URI, "geommid", MockData.CITE_PREFIX);
     private static final QName NULLGEOM = new QName(MockData.CITE_URI, "nullgeom", MockData.CITE_PREFIX);
+    private static final QName DOTS = new QName(MockData.CITE_URI, "dots.in.name", MockData.CITE_PREFIX);
+
     private Operation op;
     private GetFeatureType gft;
     
@@ -45,6 +47,7 @@ public class ShapeZipTest extends WFSTestSupport {
         dataDirectory.addPropertiesType(ALL_TYPES, ShapeZipTest.class.getResource("AllTypes.properties"), params);
         dataDirectory.addPropertiesType(GEOMMID, ShapeZipTest.class.getResource("geommid.properties"), params);
         dataDirectory.addPropertiesType(NULLGEOM, ShapeZipTest.class.getResource("nullgeom.properties"), params);
+        dataDirectory.addPropertiesType(DOTS, ShapeZipTest.class.getResource("dots.in.name.properties"), params);
     }   
 
     @Override
@@ -118,6 +121,19 @@ public class ShapeZipTest extends WFSTestSupport {
         zip.write(fct, bos, op);
         
         final String[] expectedTypes = new String[] {"nullgeom"};
+        checkShapefileIntegrity(expectedTypes, new ByteArrayInputStream(bos.toByteArray()));
+    }
+    
+    public void testDots() throws Exception {
+        FeatureSource<SimpleFeatureType, SimpleFeature> fs;
+        fs =  getFeatureSource(DOTS);
+        ShapeZipOutputFormat zip = new ShapeZipOutputFormat();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        FeatureCollectionType fct = WfsFactory.eINSTANCE.createFeatureCollectionType();
+        fct.getFeature().add(fs.getFeatures());
+        zip.write(fct, bos, op);
+        
+        final String[] expectedTypes = new String[] {"dots.in.name"};
         checkShapefileIntegrity(expectedTypes, new ByteArrayInputStream(bos.toByteArray()));
     }
 
