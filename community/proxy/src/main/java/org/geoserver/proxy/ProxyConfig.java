e981380ff3f6cd8deb6e79234b0c2907194a7c2d
agerber <agerber@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-28 22:44:55 +0000

    proxy lookin purdy
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@13001 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6580794e326f9f858155034bb4ba6d0d7e418c7f [formerly 524d0df1c7174d696eca36ff31991e26091d1e57]
    Former-commit-id: 46b742def3b264cf78bc13f4327e7158f9b134bf

--- community/proxy/src/main/java/org/geoserver/proxy/ProxyConfig.java
+++ community/proxy/src/main/java/org/geoserver/proxy/ProxyConfig.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 import org.vfny.geoserver.global.ConfigurationException;
 import org.vfny.geoserver.global.GeoserverDataDirectory;
@@ -20,12 +23,14 @@ import com.thoughtworks.xstream.XStream;
  * 
  * @author Alan Gerber <agerber@openplans.org>
  */
+//vvv everybody else is doing this
+@SuppressWarnings("serial")
 public class ProxyConfig implements java.io.Serializable{
-    /**
-     * Eclipse wants this here.  I reckon I should listen to it.
-     */
-    private static final long serialVersionUID = 1;
-
+    //This rwlock controls access to the file in which proxy configuration is stored.
+    private static ReadWriteLock configFileLock = new ReentrantReadWriteLock();
+    private static Lock configReadLock = configFileLock.readLock();
+    private static Lock configWriteLock = configFileLock.writeLock();    
+    
     /*
      * Sets the mode of the proxy server: -HOSTNAMEORMIMETYPE means a request must match have
      * matches on the hostname AND MIMEType whitelists -HOSTNAMEANDMIMETYPE means a request must
@@ -57,10 +62,10 @@ public class ProxyConfig implements java.io.Serializable{
     public Mode mode;
 
     /* A list of regular expressions describing hostnames the proxy is permitted to forward to */
-    public List<Pattern> hostnameWhitelist;
+    public List<String> hostnameWhitelist;
 
     /* A list of regular expressions describing MIMETypes the proxy is permitted to forward */
-    public List<Pattern> mimetypeWhitelist;
+    public List<String> mimetypeWhitelist;
 
     /*The name of the directory where configuration is stored*/
     private static final String confDirName = "proxy";
@@ -73,18 +78,29 @@ public class ProxyConfig implements java.io.Serializable{
      */
     public static File getConfigFile() throws ConfigurationException
     {
-        File dir = GeoserverDataDirectory.findCreateConfigDir(ProxyConfig.confDirName);
-        File proxyConfFile = new File(dir, ProxyConfig.confFileName);
+        File dir = GeoserverDataDirectory.findCreateConfigDir(confDirName);
+        File proxyConfFile = new File(dir, confFileName);
         return proxyConfFile;
     }
     
-
-    public static final ProxyConfig DEFAULT;
+    /*The default proxy configuration allows all requests to localhost and all OGC mimetypes*/
+    private static final ProxyConfig DEFAULT;
     static {
         DEFAULT = new ProxyConfig();
-        DEFAULT.mode = Mode.HOSTNAME;
-        DEFAULT.hostnameWhitelist = new ArrayList<Pattern>();
-        DEFAULT.mimetypeWhitelist = new ArrayList<Pattern>();
+        DEFAULT.mode = Mode.HOSTNAMEORMIMETYPE;
+        DEFAULT.hostnameWhitelist = Arrays.asList(
+                "localhost");
+        DEFAULT.mimetypeWhitelist = Arrays.asList(
+                "application/xml", "text/xml",
+                "application/vnd.ogc.se_xml",           // OGC Service Exception 
+                "application/vnd.ogc.se+xml",           // OGC Service Exception
+                "application/vnd.ogc.success+xml",      // OGC Success (SLD Put)
+                "application/vnd.ogc.wms_xml",          // WMS Capabilities
+                "application/vnd.ogc.context+xml",      // WMC
+                "application/vnd.ogc.gml",              // GML
+                "application/vnd.ogc.sld+xml",          // SLD
+                "application/vnd.google-earth.kml+xml"  // KML;
+        );
     }
 
     private static final Logger LOG = org.geotools.util.logging.Logging
@@ -95,19 +111,17 @@ public class ProxyConfig implements java.io.Serializable{
         ProxyConfig retval;
 
         try {
-            /*File put together here because it's unknown whether GSDD class is init'd in a 
-             * static context*/
-            File dir = GeoserverDataDirectory.findCreateConfigDir(confDirName);
-            File proxyConfFile = new File(dir, confFileName);
-            // TODO: threadsafe?
+            File proxyConfFile = getConfigFile();
             InputStream proxyConfStream = new FileInputStream(proxyConfFile);
-            // TODO: do i need a new one of these?
             XStream xs = new XStream();
+            //Take the read lock, then read the file
+            configReadLock.lock();
             retval = (ProxyConfig) (xs.fromXML(proxyConfStream));
+            configReadLock.unlock();
         } catch (Exception e) {
-            /* TODO: make this reasonable */
             LOG.warning("Failed to open configuration for Proxy module. Using default. Exception:"
                     + e.toString());
+            //writeConfigToDisk(DEFAULT);
             retval = DEFAULT;
         }
         return retval;
@@ -115,16 +129,15 @@ public class ProxyConfig implements java.io.Serializable{
 
     public static boolean writeConfigToDisk(ProxyConfig pc) {
         try {
-            /*File put together here because it's unknown whether GSDD class is init'd in a 
-             * static context*/
-            File dir = GeoserverDataDirectory.findCreateConfigDir(confDirName);
-            File proxyConfFile = new File(dir, confFileName);
-            // TODO: do i need a new one of these? how expensive is building this vs. saving it?
+            File proxyConfFile = getConfigFile();
             XStream xs = new XStream();
             String xml = xs.toXML(pc);
             FileWriter fw = new FileWriter(proxyConfFile, false); // false means overwrite old file
+            //Take the write lock on the file & lock it
+            configWriteLock.lock();
             fw.write(xml);
             fw.close();
+            configWriteLock.unlock();
             return true;
         } catch (Exception e) {
             LOG.warning("Failed to save configuration for Proxy module. Exception:"
@@ -133,28 +146,20 @@ public class ProxyConfig implements java.io.Serializable{
         }
     }
     
-    /*Output a textual representation of the list*/
+    /*Output a textual representation of the config
+     *@return a String representation of the config
+     */
     @Override
     public String toString(){
         StringBuilder stringForm = new StringBuilder(256);
         stringForm.append("Mode: " + this.mode.modeName + "\n");
         stringForm.append("Hostname regex whitelist: \n");
-        for (Pattern pattern : this.hostnameWhitelist)
-            stringForm.append(pattern + "\n");
+        for (String hostname : this.hostnameWhitelist)
+            stringForm.append(hostname + "\n");
         stringForm.append("MIMEType regex whitelist: \n");
-        for (Pattern pattern : this.mimetypeWhitelist)
-            stringForm.append(pattern + "\n");
+        for (String mimetype : this.mimetypeWhitelist)
+            stringForm.append(mimetype + "\n");
         stringForm.append(this.mode.modeName + "\n");        
         return stringForm.toString();
-    }
-    
-    /*Tester main method*/
-    public static void main(String[] args)
-    {
-        System.out.println(DEFAULT);
-        //lol don't do this the data directory ain't init'd
-        //ProxyConfig testConf = loadConfFromDisk();
-        //System.out.println(testConf);
-        //writeConfigToDisk(testConf);
     }
 }
