16e6851e1fc5d4cfc1eb3e282d3b123770f6adf5
agerber <agerber@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-29 17:45:11 +0000

    made hostname list refresh
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13009 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: edf73b87ed6f125d7e9392b9a372917cc04e127b [formerly 810479b2164837cc43679a7b9cc0170f3eb5e696]
    Former-commit-id: 81044b2eac7403f1c6a2a25fe06071d586b45dce

--- src/community/proxy/src/main/java/org/geoserver/web/proxy/HostRemovalLink.java
+++ src/community/proxy/src/main/java/org/geoserver/web/proxy/HostRemovalLink.java
@@ -45,5 +45,6 @@ public class HostRemovalLink extends AjaxLink {
         //disable the removal link, since nothing is selected any more
         setEnabled(false);
         target.addComponent(HostRemovalLink.this);
+        target.addComponent(tableObjects);
     }
 }   
