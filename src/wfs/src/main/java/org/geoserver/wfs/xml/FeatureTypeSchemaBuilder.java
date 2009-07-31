c19ff371baeb2fa6c53e639acf2576b0b62f20d7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-07-31 22:13:11 +0000

    GEOS-3311, fix for schema.xml not specifying gml namespace mapping, also did a check that the feature element is included in teh generated schema for DFT
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13038 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: cb12f17e24844914faf686dac61b30bc1ed7db48 [formerly ac07f624219fd5b44578130c189a63614ec98e9d]
    Former-commit-id: 1f65f0a5660f30ca034890250b90d519c0c90ab3

--- src/wfs/src/main/java/org/geoserver/wfs/xml/FeatureTypeSchemaBuilder.java
+++ src/wfs/src/main/java/org/geoserver/wfs/xml/FeatureTypeSchemaBuilder.java
@@ -306,6 +306,9 @@ public abstract class FeatureTypeSchemaBuilder {
                 //look up the complex type
                 List contents = ftSchema.getContents();
 
+                //ensure that an element for the feature is present
+                boolean hasElement = false;
+                
                 for (Iterator i = contents.iterator(); i.hasNext();) {
                     XSDSchemaContent content = (XSDSchemaContent) i.next();
                     content.setElement(null);
@@ -330,6 +333,53 @@ public abstract class FeatureTypeSchemaBuilder {
                             i.remove();
                         }
                     }
+                    
+                    // check for element
+                    if ( !hasElement && content instanceof XSDElementDeclaration ) {
+                        XSDElementDeclaration element = (XSDElementDeclaration) content;
+                        if ( name.equals( element.getName() ) && 
+                            featureTypeMeta.getNamespace().getURI().equals( element.getTargetNamespace() ) ) {
+                            hasElement = true;
+                        }
+                    }
+                }
+                
+                if ( !hasElement ) {
+                    //need to create an element declaration in the schema
+                    XSDElementDeclaration element = factory.createXSDElementDeclaration();
+                    element.setName( featureTypeMeta.getName() );
+                    element.setTargetNamespace( featureTypeMeta.getNamespace().getURI() );
+                    element.setSubstitutionGroupAffiliation(
+                        schema.resolveElementDeclaration(gmlNamespace, substitutionGroup));
+                    
+                    //find the type of the element
+                    List<XSDComplexTypeDefinition> candidates = new ArrayList<XSDComplexTypeDefinition>();
+                    for ( Iterator t = ftSchema.getTypeDefinitions().iterator(); t.hasNext(); ) {
+                        XSDTypeDefinition type = (XSDTypeDefinition) t.next();
+                        if ( type instanceof XSDComplexTypeDefinition ) {
+                            XSDTypeDefinition base = type.getBaseType();
+                            while(base != null ) {
+                                if ( baseType.equals(base.getName())
+                                    && gmlNamespace.equals( base.getTargetNamespace() ) ) {
+                                    
+                                    candidates.add( (XSDComplexTypeDefinition) type );
+                                    break;
+                                }   
+                                if ( base.equals( base.getBaseType() ) ) {
+                                    break;
+                                }
+                                base = base.getBaseType();
+                            }
+                        }
+                    }
+                    
+                    if ( candidates.size() != 1 ) {
+                        throw new IllegalStateException("Could not determine feature type for " +
+                        "generated element. Must specify explicitly in schema.xsd.");
+                    }
+                    
+                    element.setTypeDefinition( candidates.get(0));
+                    schema.getContents().add(element);
                 }
 
                 schema.getContents().addAll(contents);
