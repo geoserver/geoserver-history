b594208c5d469645717aea678c08d38ba7d58ff2
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-08-11 21:37:27 +0000

    GEOS-3110, first cut of welcome page
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13114 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 57cdd87f98e29bc6423299c090a7157b324b5ea1 [formerly 731c1b0b0fd7ffec485db3538756cc51651b8be1]
    Former-commit-id: e7154fb65ef54bbeaa5c44cc36e2d319b41d52b4

--- src/web/core/src/main/java/org/geoserver/web/GeoServerHomePage.java
+++ src/web/core/src/main/java/org/geoserver/web/GeoServerHomePage.java
@@ -5,12 +5,23 @@
 package org.geoserver.web;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.StoreInfo;
+import org.geoserver.config.ContactInfo;
+import org.geoserver.config.GeoServer;
 import org.geoserver.config.ServiceInfo;
+import org.geoserver.web.data.layer.LayerPage;
+import org.geoserver.web.data.layer.NewLayerPage;
+import org.geoserver.web.data.store.NewDataPage;
+import org.geoserver.web.data.store.StorePage;
+import org.geoserver.web.data.workspace.WorkspaceNewPage;
+import org.geoserver.web.data.workspace.WorkspacePage;
 import org.geotools.util.Version;
 
 /**
@@ -22,7 +33,27 @@ import org.geotools.util.Version;
 public class GeoServerHomePage extends GeoServerBasePage {
 
     public GeoServerHomePage() {
+        GeoServer gs = getGeoServer();
+        ContactInfo contact = gs.getGlobal().getContact();
 
+        //add some contact info
+        add(new ExternalLink("contactURL", contact.getOnlineResource())
+            .add( new Label("contactName", contact.getContactOrganization())));
+        add(new ExternalLink("contactEmail", "mailto:"+contact.getContactEmail()));
+        
+        Catalog catalog = getCatalog();
+        add(new BookmarkablePageLink("layersLink", LayerPage.class)
+            .add(new Label( "nlayers", ""+catalog.getLayers().size())));
+        add(new BookmarkablePageLink("addLayerLink", NewLayerPage.class));
+        
+        add(new BookmarkablePageLink("storesLink",StorePage.class)
+            .add(new Label( "nstores", ""+catalog.getStores(StoreInfo.class).size())));
+        add(new BookmarkablePageLink("addStoreLink", NewDataPage.class));
+        
+        add(new BookmarkablePageLink("workspacesLink",WorkspacePage.class)
+            .add(new Label( "nworkspaces", ""+catalog.getWorkspaces().size())));
+        add(new BookmarkablePageLink("addWorkspaceLink", WorkspaceNewPage.class));
+        
         // when hacking this service listing code please refer to 
         // http://jira.codehaus.org/browse/GEOS-2114
         ListView view = new ListView("services", getServices()) {
