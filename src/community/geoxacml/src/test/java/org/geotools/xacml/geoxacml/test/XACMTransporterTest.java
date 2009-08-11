599e1d3bf4b1cd8110d531826c05a59c4ce82735
mcr <mcr@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-08-11 07:00:28 +0000

    Adding support for local,remote and multithreaded XACML requests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13100 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e81a2a76f6f7c41697f268e5f23508ce14489467 [formerly d395d6b742b008d138e9b1267623a7fbb0667ea3]
    Former-commit-id: 53d3fffefedcdd9237e3a1b66d2ee56a18a45fc7

---
+++ src/community/geoxacml/src/test/java/org/geotools/xacml/geoxacml/test/XACMTransporterTest.java
@@ -0,0 +1,158 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.xacml.geoxacml.test;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.geotools.xacml.geoxacml.config.GeoXACML;
+import org.geotools.xacml.test.TestSupport;
+import org.geotools.xacml.transport.XACMLLocalTransport;
+
+import com.sun.xacml.PDP;
+import com.sun.xacml.ctx.RequestCtx;
+import com.sun.xacml.ctx.ResponseCtx;
+import com.sun.xacml.ctx.Result;
+import com.sun.xacml.ctx.Status;
+
+/**
+ * @author Christian Mueller
+ * 
+ *         Tests for bag functions
+ * 
+ */
+public class XACMTransporterTest extends TestCase {
+
+    public XACMTransporterTest() {
+        super();
+
+    }
+
+    public XACMTransporterTest(String arg0) {
+        super(arg0);
+
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        GeoXACML.initialize();
+        TestSupport.initOutputDir();
+    }
+
+    public void testXACMTransportSingleRequest() {
+
+        PDP pdp = TestSupport
+                .getPDP(TestSupport.getGeoXACMLFNFor("wildcard", "WildCardPolicy.xml"));
+
+        RequestCtx request = null;
+        try {
+            request = RequestCtx.getInstance(new FileInputStream(TestSupport.getGeoXACMLFNFor(
+                    "wildcard", "WildCardRequest.xml")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+
+        // serial
+        XACMLLocalTransport transport = new XACMLLocalTransport(pdp, false);
+        ResponseCtx response = transport.evaluateRequestCtx(request);
+
+        Result result = (Result) response.getResults().iterator().next();
+        assertTrue(result.getDecision() == Result.DECISION_PERMIT);
+        assertTrue(result.getStatus().getCode().iterator().next().equals(Status.STATUS_OK));
+
+        // multithreaded transporter, one request
+
+        transport = new XACMLLocalTransport(pdp, true);
+        response = transport.evaluateRequestCtx(request);
+
+        result = (Result) response.getResults().iterator().next();
+        assertTrue(result.getDecision() == Result.DECISION_PERMIT);
+        assertTrue(result.getStatus().getCode().iterator().next().equals(Status.STATUS_OK));
+
+    }
+
+    public void testXACMLTransportMultipleRequestsSerial() {
+
+        PDP pdp = TestSupport
+                .getPDP(TestSupport.getGeoXACMLFNFor("wildcard", "WildCardPolicy.xml"));
+        XACMLLocalTransport transport = new XACMLLocalTransport(pdp, false);
+        List<RequestCtx> requestList = createRequestList();
+        List<ResponseCtx> responseList = transport.evaluateRequestCtxList(requestList);
+        checkResponseList(responseList);
+
+    }
+
+    public void testXACMLTransportMultipleRequestsMultiThreaded() {
+
+        PDP pdp = TestSupport
+                .getPDP(TestSupport.getGeoXACMLFNFor("wildcard", "WildCardPolicy.xml"));
+        XACMLLocalTransport transport = new XACMLLocalTransport(pdp, true);
+        List<RequestCtx> requestList = createRequestList();
+        List<ResponseCtx> responseList = transport.evaluateRequestCtxList(requestList);
+        checkResponseList(responseList);
+
+    }
+
+    private void checkResponseList(List<ResponseCtx> responseList) {
+        for (int i = 0; i < responseList.size(); i++) {
+            ResponseCtx response = responseList.get(i);
+            Result result = (Result) response.getResults().iterator().next();
+            if (i % 2 == 0) {
+                assertTrue(result.getDecision() == Result.DECISION_PERMIT);
+                assertTrue(result.getStatus().getCode().iterator().next().equals(Status.STATUS_OK));
+            } else {
+                assertTrue(result.getDecision() == Result.DECISION_NOT_APPLICABLE);
+                assertTrue(result.getStatus().getCode().iterator().next().equals(Status.STATUS_OK));
+            }
+
+        }
+    }
+
+    private List<RequestCtx> createRequestList() {
+
+        RequestCtx request = null;
+        try {
+            request = RequestCtx.getInstance(new FileInputStream(TestSupport.getGeoXACMLFNFor(
+                    "wildcard", "WildCardRequest.xml")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+
+        RequestCtx request1 = null;
+        try {
+            request1 = RequestCtx.getInstance(new FileInputStream(TestSupport.getGeoXACMLFNFor(
+                    "wildcard", "WildCardRequest1.xml")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+
+        List<RequestCtx> resultList = new ArrayList<RequestCtx>();
+        for (int i = 0; i < 10; i++) {
+            resultList.add(request);
+            resultList.add(request1);
+        }
+        return resultList;
+    }
+}
