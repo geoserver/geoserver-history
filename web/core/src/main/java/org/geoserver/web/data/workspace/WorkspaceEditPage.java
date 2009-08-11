00a7370bf55b31b63f34a27c1a495307a4272c80
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-08-11 20:03:25 +0000

    GEOS-3322, update the datastoreInfo's namespace parameter upon namespace changes
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@13111 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 856f7d828829ab6279a61e0483671e907f2ca89d [formerly 810996a0eaf90b548dc1664eb4eed7f7f1642d4c]
    Former-commit-id: 797ceba079681dbecde2611eb1c0a3a8351d46ea

--- web/core/src/main/java/org/geoserver/web/data/workspace/WorkspaceEditPage.java
+++ web/core/src/main/java/org/geoserver/web/data/workspace/WorkspaceEditPage.java
@@ -4,7 +4,10 @@
  */
 package org.geoserver.web.data.workspace;
 
-import org.apache.wicket.markup.html.basic.Label;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.markup.html.form.TextField;
@@ -14,10 +17,17 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.validation.validator.UrlValidator;
 import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.catalog.event.CatalogAddEvent;
+import org.geoserver.catalog.event.CatalogListener;
+import org.geoserver.catalog.event.CatalogModifyEvent;
+import org.geoserver.catalog.event.CatalogPostModifyEvent;
+import org.geoserver.catalog.event.CatalogRemoveEvent;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.data.namespace.NamespaceDetachableModel;
+import org.geotools.util.logging.Logging;
 
 /**
  * Allows editing a specific workspace
@@ -25,6 +35,8 @@ import org.geoserver.web.data.namespace.NamespaceDetachableModel;
 @SuppressWarnings("serial")
 public class WorkspaceEditPage extends GeoServerSecuredPage {
 
+    private static final Logger LOGGER = Logging.getLogger("org.geoserver.web.data.workspace");
+    
     IModel wsModel;
     IModel nsModel;
     
@@ -37,10 +49,11 @@ public class WorkspaceEditPage extends GeoServerSecuredPage {
         
         Form form = new Form( "form", new CompoundPropertyModel( nsModel ) ) {
             protected void onSubmit() {
-                Catalog catalog = getCatalog();
-                catalog.save( (NamespaceInfo) nsModel.getObject() );
-                catalog.save( (WorkspaceInfo) wsModel.getObject() );
-                setResponsePage(WorkspacePage.class);
+                try {
+                    saveWorkspace();
+                } catch (RuntimeException e) {
+                    error(e.getMessage());
+                }
             }
         };
         add(form);
@@ -61,4 +74,107 @@ public class WorkspaceEditPage extends GeoServerSecuredPage {
      
     }
     
+    private void saveWorkspace() {
+        final Catalog catalog = getCatalog();
+
+        NamespaceInfo namespaceInfo = (NamespaceInfo) nsModel.getObject();
+        WorkspaceInfo workspaceInfo = (WorkspaceInfo) wsModel.getObject();
+
+        DataStoreNamespaceUpdatingListener listener = new DataStoreNamespaceUpdatingListener(
+                workspaceInfo, catalog);
+        catalog.addListener(listener);
+
+        try {
+            catalog.save(namespaceInfo);
+            catalog.save(workspaceInfo);
+        } finally {
+            catalog.removeListener(listener);
+        }
+        setResponsePage(WorkspacePage.class);
+    }
+
+    /**
+     * Listens to Catalog's namespace URI modifications and updates the "namespace"
+     * {@link DataStoreInfo#getConnectionParameters() connection parameter} for all the datastores
+     * configured for the workspace
+     */
+    private static final class DataStoreNamespaceUpdatingListener implements CatalogListener {
+
+        private final WorkspaceInfo workspaceInfo;
+
+        private final Catalog catalog;
+
+        public DataStoreNamespaceUpdatingListener(final WorkspaceInfo workspaceInfo,
+                final Catalog catalog) {
+            this.workspaceInfo = workspaceInfo;
+            this.catalog = catalog;
+        }
+
+        /**
+         * @see CatalogListener#handlePostModifyEvent(CatalogPostModifyEvent)
+         */
+        public void handlePostModifyEvent(CatalogPostModifyEvent event) {
+            if (event.getSource() instanceof NamespaceInfo) {
+                LOGGER.info("Updating namespace parameter for all DataStoreInfo"
+                        + " objects in workspace " + workspaceInfo.getName());
+
+                final NamespaceInfo nsInfo = (NamespaceInfo) event.getSource();
+                String namespaceURI = nsInfo.getURI();
+
+                List<DataStoreInfo> stores = catalog.getDataStoresByWorkspace(workspaceInfo);
+                if (stores.size() > 0) {
+                    for (DataStoreInfo store : stores) {
+                        if (store.getConnectionParameters().containsKey("namespace")) {
+                            store.getConnectionParameters().put("namespace", namespaceURI);
+                            LOGGER.info("Setting namespace for store " + store.getName()
+                                    + " to be " + namespaceURI);
+                            catalog.save(store);
+                        }
+                    }
+                    LOGGER.info("namespace parameter for " + stores.size()
+                            + " stores in workspace " + workspaceInfo.getName()
+                            + " successfully updated");
+                } else {
+                    LOGGER.info("No stores in workspace " + workspaceInfo.getName());
+                }
+            }
+        }
+
+        /**
+         * Ignored event
+         * 
+         * @see CatalogListener#handleModifyEvent(CatalogModifyEvent)
+         */
+        public void handleModifyEvent(CatalogModifyEvent event) {
+            // ignore
+        }
+
+        /**
+         * Ignored event
+         * 
+         * @see CatalogListener#handleRemoveEvent(CatalogRemoveEvent)
+         */
+        public void reloaded() {
+            // ignore
+        }
+
+        /**
+         * Ignored event
+         * 
+         * @see CatalogListener#handleRemoveEvent(CatalogRemoveEvent)
+         */
+        public void handleRemoveEvent(CatalogRemoveEvent event) {
+            // ignore
+        }
+
+        /**
+         * Ignored event
+         * 
+         * @see CatalogListener#handleAddEvent(CatalogAddEvent)
+         */
+        public void handleAddEvent(CatalogAddEvent event) {
+            // ignore
+        }
+    }
+
 }
--- web/core/src/test/java/org/geoserver/web/data/workspace/WorkspaceEditPageTest.java
+++ web/core/src/test/java/org/geoserver/web/data/workspace/WorkspaceEditPageTest.java
@@ -1,43 +1,80 @@
 package org.geoserver.web.data.workspace;
 
+import java.util.List;
+
 import org.apache.wicket.util.tester.FormTester;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.catalog.NamespaceInfo;
+import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.data.test.MockData;
 import org.geoserver.web.GeoServerWicketTestSupport;
 
 public class WorkspaceEditPageTest extends GeoServerWicketTestSupport {
-    
+
+    private WorkspaceInfo citeWorkspace;
+
     @Override
     protected void setUpInternal() throws Exception {
         super.setUpInternal();
         login();
-        tester.startPage(new WorkspaceEditPage(getCatalog().getWorkspaceByName(MockData.CITE_PREFIX)));
-        
+        citeWorkspace = getCatalog().getWorkspaceByName(MockData.CITE_PREFIX);
+        tester.startPage(new WorkspaceEditPage(citeWorkspace));
+
         // print(tester.getLastRenderedPage(), true, true);
     }
-    
+
     public void testLoad() {
         tester.assertRenderedPage(WorkspaceEditPage.class);
         tester.assertNoErrorMessage();
-        
+
         tester.assertModelValue("form:name", MockData.CITE_PREFIX);
         tester.assertModelValue("form:uri", MockData.CITE_URI);
     }
-    
+
     public void testValidURI() {
         FormTester form = tester.newFormTester("form");
         form.setValue("uri", "http://www.geoserver.org");
         form.submit();
-        
+
         tester.assertRenderedPage(WorkspacePage.class);
         tester.assertNoErrorMessage();
     }
-    
-    public void testInvalidURI()  {
+
+    public void testInvalidURI() {
         FormTester form = tester.newFormTester("form");
         form.setValue("uri", "not a valid uri");
         form.submit();
-        
+
         tester.assertRenderedPage(WorkspaceEditPage.class);
-        tester.assertErrorMessages(new String[] {"'not a valid uri' is not a valid URL."});
+        tester.assertErrorMessages(new String[] { "'not a valid uri' is not a valid URL." });
+    }
+
+    /**
+     * See GEOS-3322, upon a namespace URI change the datastores connection parameter shall be
+     * changed accordingly
+     */
+    public void testUpdatesDataStoresNamespace() {
+        final Catalog catalog = getCatalog();
+        final List<DataStoreInfo> storesInitial = catalog.getStoresByWorkspace(citeWorkspace,
+                DataStoreInfo.class);
+
+        final NamespaceInfo citeNamespace = catalog.getNamespaceByPrefix(citeWorkspace.getName());
+
+        for (DataStoreInfo store : storesInitial) {
+            assertEquals(citeNamespace.getURI(), store.getConnectionParameters().get("namespace"));
+        }
+
+        FormTester form = tester.newFormTester("form");
+        final String newNsURI = "http://www.geoserver.org/changed";
+        form.setValue("uri", newNsURI);
+        form.submit();
+        tester.assertNoErrorMessage();
+
+        List<DataStoreInfo> storesChanged = catalog.getStoresByWorkspace(citeWorkspace,
+                DataStoreInfo.class);
+        for (DataStoreInfo store : storesChanged) {
+            assertEquals(newNsURI, store.getConnectionParameters().get("namespace"));
+        }
     }
 }
