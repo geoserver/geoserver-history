e02029fed849cba8a8ccf6745e30f78f0c8d2f97
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-08-17 17:08:01 +0000

    Import cleanup
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@13141 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 77affa1bef1dfff84766b2f839f2f79b8cb6bffd [formerly 881f475137daebf7eec50ae7da0fe237d5945e01]
    Former-commit-id: 3a9e949de810954710b520d93351d8aa2b6891cb

--- web/core/src/test/java/org/geoserver/web/wicket/CRSPanelTest.java
+++ web/core/src/test/java/org/geoserver/web/wicket/CRSPanelTest.java
@@ -3,7 +3,6 @@ package org.geoserver.web.wicket;
 import java.io.Serializable;
 
 import org.apache.wicket.Session;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
