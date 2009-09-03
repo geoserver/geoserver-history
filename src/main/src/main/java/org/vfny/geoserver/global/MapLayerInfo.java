be36db856d2c4bc2029043682c7bcf402bd0e578
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-09-03 10:11:15 +0000

    Add equals and hashcode based on name and type
    git-svn-id: http://svn.codehaus.org/geoserver/branches/1.7.x@13267 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fa33a302b9c9572f40e4d7996158a81bd65a1713 [formerly 2437f2bad2ccdaaa6649cd39fb01ec6d28d9ff0d]
    Former-commit-id: 262ae7ebc856e3e7ffdf59e5f62d97cf843def79

--- src/main/src/main/java/org/vfny/geoserver/global/MapLayerInfo.java
+++ src/main/src/main/java/org/vfny/geoserver/global/MapLayerInfo.java
@@ -395,4 +395,33 @@ public final class MapLayerInfo extends GlobalLayerSupertype {
 		return layerFeatureConstraints;
 	}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + type;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MapLayerInfo other = (MapLayerInfo) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
+    }
+
+	
 }
