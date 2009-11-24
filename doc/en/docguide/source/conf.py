12ace35bffa80469ec9daa0e496521fb9aebe96f
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-11-24 22:09:43 +0000

    GEOS-3670, refactoring geoserver sphinx theme
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13678 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: be12e0cfc76d289c19b7c8dcf13fcc429f4618ad [formerly e5bdd94b76d94bb9ce0354195e6abfbf7d08e027]
    Former-commit-id: 2584693fc866bb8ce01e2c3b6ff824536fdee9bc

--- doc/en/docguide/source/conf.py
+++ doc/en/docguide/source/conf.py
@@ -79,15 +79,17 @@ pygments_style = 'sphinx'
 
 # Options for HTML output
 # -----------------------
+html_theme = 'geoserver'
+html_theme_path = ['../../theme']
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+#html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = project
+#html_title = project
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -104,7 +106,7 @@ html_title = project
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../../theme/_static']
+#html_static_path = ['../../theme/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
--- doc/en/user/source/conf.py
+++ doc/en/user/source/conf.py
@@ -82,11 +82,13 @@ pygments_style = 'sphinx'
 
 # Options for HTML output
 # -----------------------
+html_theme = 'geoserver'
+html_theme_path = ['../../theme']
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+#html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -107,7 +109,7 @@ html_title = project + " " + release + " " + manual
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../../theme/_static']
+#html_static_path = ['../../theme/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
