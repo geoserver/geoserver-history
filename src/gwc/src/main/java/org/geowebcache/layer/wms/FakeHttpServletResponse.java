bda7afe72ffa256d600f6a9b6ee0788eead4aeb8
arneke <arneke@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-11-25 20:50:08 +0000

    Bumping GWC to 1.2.X and doing away with HTTP calls. This implementation creates fake ServletRequest and ServletResponse objects
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13690 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: fb36bf4648e0b1fd285c473b3e38ed957839dc74 [formerly 59f1c445fd82f92ceb9d3532dbc2a4d3850abca4]
    Former-commit-id: d75d7bdf275d1ea9cd73493f1c2669bb02161b13

---
+++ src/gwc/src/main/java/org/geowebcache/layer/wms/FakeHttpServletResponse.java
@@ -0,0 +1,172 @@
+package org.geowebcache.layer.wms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.geotools.util.logging.Logging;
+
+public class FakeHttpServletResponse implements HttpServletResponse {
+    
+    private static Logger log = Logging.getLogger(HttpServletResponse.class.toString());
+    
+    FakeServletOutputStream fos = new FakeServletOutputStream();
+    
+    String contentType;
+    
+    HashMap<String,String> headers = new HashMap<String,String>();
+    
+    public byte[] getBytes() {
+        return fos.getBytes();
+    }
+    
+    /**
+     * Standard interface
+     */
+    public void addCookie(Cookie arg0) {
+        throw new ServletDebugException();
+        
+    }
+
+    public void addDateHeader(String arg0, long arg1) {
+        log.finer("Added date header: "+arg0 + " : " + arg1);
+        headers.put(arg0, Long.toString(arg1));
+    }
+
+    public void addHeader(String arg0, String arg1) {
+        log.finer("Added string header: "+arg0 + " : " + arg1);
+        headers.put(arg0, arg1); 
+    }
+
+    public void addIntHeader(String arg0, int arg1) {
+        log.finer("Added integer header: "+arg0 + " : " + arg1);
+        headers.put(arg0, Integer.toString(arg1));
+    }
+
+    public boolean containsHeader(String arg0) {
+        throw new ServletDebugException();
+    }
+
+    public String encodeRedirectURL(String arg0) {
+        throw new ServletDebugException();
+    }
+
+    public String encodeRedirectUrl(String arg0) {
+        throw new ServletDebugException();
+    }
+
+    public String encodeURL(String arg0) {
+        throw new ServletDebugException();
+    }
+
+    public String encodeUrl(String arg0) {
+        throw new ServletDebugException();
+    }
+
+    public void sendError(int arg0) throws IOException {
+        throw new ServletDebugException();
+    }
+
+    public void sendError(int arg0, String arg1) throws IOException {
+        throw new ServletDebugException();
+    }
+
+    public void sendRedirect(String arg0) throws IOException {
+        throw new ServletDebugException();
+    }
+
+    public void setDateHeader(String arg0, long arg1) {
+        throw new ServletDebugException();
+    }
+
+    public void setHeader(String arg0, String arg1) {
+        throw new ServletDebugException();
+    }
+
+    public void setIntHeader(String arg0, int arg1) {
+        throw new ServletDebugException();
+    }
+
+    public void setStatus(int arg0) {
+        throw new ServletDebugException();
+    }
+
+    public void setStatus(int arg0, String arg1) {
+        throw new ServletDebugException();
+    }
+
+    public void flushBuffer() throws IOException {
+        throw new ServletDebugException();
+    }
+
+    public int getBufferSize() {
+        throw new ServletDebugException();
+    }
+
+    public String getCharacterEncoding() {
+        throw new ServletDebugException();
+    }
+
+    public String getContentType() {
+        return this.contentType;
+    }
+
+    public Locale getLocale() {
+        throw new ServletDebugException();
+    }
+
+    public ServletOutputStream getOutputStream() throws IOException {
+        log.finer("Returning output stream");
+        return this.fos;
+    }
+
+    public PrintWriter getWriter() throws IOException {
+        throw new ServletDebugException();
+    }
+
+    public boolean isCommitted() {
+        throw new ServletDebugException();
+    }
+
+    public void reset() {
+        throw new ServletDebugException();
+        
+    }
+
+    public void resetBuffer() {
+        throw new ServletDebugException();
+        
+    }
+
+    public void setBufferSize(int arg0) {
+        throw new ServletDebugException();
+        
+    }
+
+    public void setCharacterEncoding(String arg0) {
+        throw new ServletDebugException();
+        
+    }
+
+    public void setContentLength(int arg0) {
+        throw new ServletDebugException();
+        
+    }
+
+    public void setContentType(String arg0) {
+        log.finer("Content type set to " + arg0);
+        this.contentType = arg0;
+    }
+
+    public void setLocale(Locale arg0) {
+        throw new ServletDebugException();
+        
+    }
+
+}
