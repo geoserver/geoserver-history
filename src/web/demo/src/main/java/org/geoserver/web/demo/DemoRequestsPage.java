6ee66a3cd7117303dcee973170c488f26767b1f0
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-12-11 15:05:56 +0000

    backport fix for GEOS-3616
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.0.x@13789 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5c7983c8871be64be384a9d99d384f0203c0a421 [formerly ea480b02b3c1473887d1647a46d840fe2faaa514]
    Former-commit-id: acb405fe3f4cf73acd2bffb97fc5e4dedbfdaa2e

--- src/web/demo/src/main/java/org/geoserver/web/demo/DemoRequestsPage.java
+++ src/web/demo/src/main/java/org/geoserver/web/demo/DemoRequestsPage.java
@@ -18,20 +18,24 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.wicket.Page;
-import org.apache.wicket.Request;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.IAjaxCallDecorator;
+import org.apache.wicket.ajax.calldecorator.AjaxCallDecorator;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.protocol.http.WebRequest;
-import org.geoserver.ows.util.RequestUtils;
+import org.geoserver.ows.util.ResponseUtils;
 import org.geoserver.web.GeoServerBasePage;
 import org.geoserver.web.wicket.EditAreaBehavior;
 import org.geotools.util.logging.Logging;
@@ -56,7 +60,7 @@ public class DemoRequestsPage extends GeoServerBasePage {
      * @version $Id$
      * @since 2.0.x
      */
-    public static class DemoRequestsModel implements Serializable {
+    static class DemoRequest implements Serializable {
         /**
          * The directory containing the demo files
          */
@@ -72,7 +76,7 @@ public class DemoRequestsPage extends GeoServerBasePage {
 
         private String password;
 
-        DemoRequestsModel(final File demoDir) {
+        DemoRequest(final File demoDir) {
             this.demoDir = demoDir;
         }
 
@@ -119,71 +123,165 @@ public class DemoRequestsPage extends GeoServerBasePage {
         public File getDemoDir() {
             return demoDir;
         }
-
-        /**
-         * Loads the contents of the demo request file named {@code reqFileName}
-         * and located in the {@link #getDemoDir() demo directory}.
-         * 
-         * @param reqFileName
-         *            the file name to load the contents for
-         * @return the file contents
-         * @throws IOException
-         *             if an io exception occurs opening or loading the file
-         */
-        public String getFileContents(final String reqFileName) throws IOException {
-            final File file = new File(demoDir, reqFileName);
-            final StringBuilder sb = new StringBuilder();
-            final BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            try {
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line);
-                    sb.append("\n");
-                }
-            } finally {
-                reader.close();
-            }
-            return sb.toString();
-        }
     }
 
-    private final DemoRequestsModel model;
+    private final File demoDir;
+
+    private TextField urlTextField;
+
+    private TextArea body;
+
+    private TextField username;
+
+    private PasswordTextField password;
 
     public DemoRequestsPage() {
-        File demoDir;
         try {
             demoDir = GeoserverDataDirectory.findCreateConfigDir("demo/");
         } catch (ConfigurationException e) {
             throw new WicketRuntimeException("Can't access demo requests directory: "
                     + e.getMessage());
         }
-        model = new DemoRequestsModel(demoDir);
+        DemoRequest model = new DemoRequest(demoDir);
+        setModel(new Model(model));
 
-        setUpRequestSelectionForm();
-        setUpRequestSumbitionForm();
+        setUpDemoRequestsForm(demoDir);
     }
 
-    private void setUpRequestSumbitionForm() {
-        final Form testWfsPostForm = new Form("TestWfsPostForm");
-        add(testWfsPostForm);
+    /**
+     * Package visible constructor aimed to help in setting up unit tests for this class
+     * 
+     * @param demoDir
+     */
+    DemoRequestsPage(final File demoDir) {
+        this.demoDir = demoDir;
+        DemoRequest model = new DemoRequest(demoDir);
+        setModel(new Model(model));
+        setUpDemoRequestsForm(demoDir);
+    }
 
-        TextField urlTextField = new TextField("url", new PropertyModel(model, "requestUrl"));
-        testWfsPostForm.add(urlTextField);
+    /**
+     * Loads the contents of the demo request file named {@code reqFileName} and located in the
+     * {@link #getDemoDir() demo directory}.
+     * 
+     * @param reqFileName
+     *            the file name to load the contents for
+     * @return the file contents
+     * @throws IOException
+     *             if an io exception occurs opening or loading the file
+     */
+    private String getFileContents(final String reqFileName) throws IOException {
+        final File file = new File(demoDir, reqFileName);
+        final StringBuilder sb = new StringBuilder();
+        final BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } finally {
+            reader.close();
+        }
+        return sb.toString();
+    }
 
-        TextArea body = new TextArea("body", new PropertyModel(model, "requestBody"));
+    private void setUpDemoRequestsForm(final File demoDir) {
+        final IModel requestModel = getModel();
+
+        final Form demoRequestsForm;
+        demoRequestsForm = new Form("demoRequestsForm");
+        demoRequestsForm.setOutputMarkupId(true);
+        demoRequestsForm.setModel(requestModel);
+        add(demoRequestsForm);
+
+        final List<String> demoList = getDemoList(demoDir);
+        final DropDownChoice demoRequestsList;
+        final IModel reqFileNameModel = new PropertyModel(requestModel, "requestFileName");
+        demoRequestsList = new DropDownChoice("demoRequestsList", reqFileNameModel, demoList,
+                new IChoiceRenderer() {
+                    public String getIdValue(Object obj, int index) {
+                        return String.valueOf(obj);
+                    }
+
+                    public Object getDisplayValue(Object obj) {
+                        return obj;
+                    }
+                });
+        demoRequestsForm.add(demoRequestsList);
+
+        /*
+         * Wanted to use a simpler OnChangeAjaxBehavior but target.addComponent(body) does not make
+         * the EditAreaBehavior to update the body contents inside it, but instead puts the plain
+         * TextArea contents above the empty xml editor
+         */
+        demoRequestsList.add(new AjaxFormSubmitBehavior(demoRequestsForm, "onchange") {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target) {
+                final String reqFileName = demoRequestsList.getModelValue();
+                final String contents;
+
+                final String baseUrl;
+                {
+                    WebRequest request = (WebRequest) DemoRequestsPage.this.getRequest();
+                    HttpServletRequest httpServletRequest;
+                    httpServletRequest = ((WebRequest) request).getHttpServletRequest();
+                    baseUrl = ResponseUtils.baseURL(httpServletRequest);
+                }
+                try {
+                    contents = getFileContents(reqFileName);
+                } catch (IOException e) {
+                    LOGGER.log(Level.WARNING, "Can't load demo file " + reqFileName, e);
+                    throw new WicketRuntimeException("Can't load demo file " + reqFileName, e);
+                }
+
+                boolean demoRequestIsHttpGet = reqFileName.endsWith(".url");
+                final String service = reqFileName.substring(0, reqFileName.indexOf('_'))
+                        .toLowerCase();
+                final String serviceUrl = baseUrl + service;
+                if (demoRequestIsHttpGet) {
+                    String url = baseUrl + contents;
+                    urlTextField.setModelObject(url);
+                    body.setModelObject("");
+                } else {
+                    urlTextField.setModelObject(serviceUrl);
+                    body.setModelObject(contents);
+                }
+
+                // target.addComponent(urlTextField);
+                // target.addComponent(body);
+                /*
+                 * Need to setResponsePage, addComponent causes the EditAreaBehavior to sometimes
+                 * not updating properly
+                 */
+                setResponsePage(DemoRequestsPage.this);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target) {
+                // nothing to do
+            }
+        });
+
+        urlTextField = new TextField("url", new PropertyModel(requestModel, "requestUrl"));
+        urlTextField.setMarkupId("requestUrl");
+        urlTextField.setOutputMarkupId(true);
+        demoRequestsForm.add(urlTextField);
+
+        body = new TextArea("body", new PropertyModel(requestModel, "requestBody"));
         // force the id otherwise this blasted thing won't be usable from other forms
-        body.setMarkupId("body");
+        body.setMarkupId("requestBody");
         body.setOutputMarkupId(true);
         body.add(new EditAreaBehavior());
-        testWfsPostForm.add(body);
+        demoRequestsForm.add(body);
 
-        TextField username = new TextField("username", new PropertyModel(model, "userName"));
-        testWfsPostForm.add(username);
+        username = new TextField("username", new PropertyModel(requestModel, "userName"));
+        demoRequestsForm.add(username);
 
-        PasswordTextField password = new PasswordTextField("password", new PropertyModel(model,
-                "password"));
+        password = new PasswordTextField("password", new PropertyModel(requestModel, "password"));
         password.setRequired(false);
-        testWfsPostForm.add(password);
+        demoRequestsForm.add(password);
 
         final ModalWindow responseWindow;
 
@@ -195,40 +293,36 @@ public class DemoRequestsPage extends GeoServerBasePage {
         responseWindow.setPageCreator(new ModalWindow.PageCreator() {
 
             public Page createPage() {
-                return new DemoRequestResponse(model);
+                return new DemoRequestResponse(requestModel);
             }
         });
 
-        testWfsPostForm.add(new AjaxSubmitLink("submit", testWfsPostForm) {
-
+        demoRequestsForm.add(new AjaxSubmitLink("submit", demoRequestsForm) {
             @Override
             public void onSubmit(AjaxRequestTarget target, Form testWfsPostForm) {
                 responseWindow.show(target);
             }
 
-        });
+            @Override
+            protected IAjaxCallDecorator getAjaxCallDecorator() {
+                // we need to force EditArea to update the textarea contents (which it hides)
+                // before submitting the form, otherwise the contents won't be the ones the user
+                // edited
+                return new AjaxCallDecorator() {
+                    @Override
+                    public CharSequence decorateScript(CharSequence script) {
+                        return "document.getElementById('requestBody').value = editAreaLoader.getValue('requestBody');"
+                                + script;
+                    }
+                };
+            }
 
-        // Commented out since I don't know yet how to open it in a new browser
-        // window...
-        // testWfsPostForm.add(new AjaxLink("submitNew") {
-        // private static final long serialVersionUID = 1L;
-        //
-        // @Override
-        // public void onClick(AjaxRequestTarget target) {
-        // responseWindow.show(target);
-        // }
-        //
-        // });
+        });
     }
 
-    private void setUpRequestSelectionForm() {
-        final Form selectForm = new Form("selectForm");
-        final DropDownChoice demoRequestsList;
-
-        add(selectForm);
-
+    private List<String> getDemoList(final File demoDir) {
         final List<String> demoList = new ArrayList<String>();
-        for (File file : model.getDemoDir().listFiles()) {
+        for (File file : demoDir.listFiles()) {
             if (!file.isDirectory()) {
                 final String name = file.getName();
                 if (name.endsWith(".url") || name.endsWith(".xml")) {
@@ -240,66 +334,6 @@ public class DemoRequestsPage extends GeoServerBasePage {
             }
         }
         Collections.sort(demoList);
-        demoRequestsList = new DemoRequestsDropDown("demoRequestsList", model, demoList);
-        selectForm.add(demoRequestsList);
-    }
-
-    /**
-     * A drop down option component to select a demo request, updates the
-     * model's {@link DemoRequestsModel#getRequestUrl() requestUrl} and
-     * {@link DemoRequestsModel#getRequestBody() requestBody} properties so the
-     * appropriate form input fields update their values automatically.
-     * 
-     * @author Gabriel Roldan
-     */
-    private static class DemoRequestsDropDown extends DropDownChoice {
-        private DemoRequestsModel demosModel;
-
-        public DemoRequestsDropDown(final String id, DemoRequestsModel model, List<String> demoList) {
-            super(id, new Model(), demoList);
-            this.demosModel = model;
-        }
-
-        @Override
-        protected boolean wantOnSelectionChangedNotifications() {
-            return true;
-        }
-
-        /**
-         * Updates the {@link DemoRequestsModel} so the form inputs get updated
-         */
-        @Override
-        protected void onSelectionChanged(final Object newSelection) {
-            final String reqFileName = (String) newSelection;
-            final String contents;
-
-            final String baseUrl;
-            {
-                Request request = super.getWebPage().getRequest();
-                if (!(request instanceof WebRequest)) {
-                    throw new RuntimeException("request is not a WebRequest, shouldn't happen!");
-                }
-                HttpServletRequest httpServletRequest;
-                httpServletRequest = ((WebRequest) request).getHttpServletRequest();
-                baseUrl = RequestUtils.baseURL(httpServletRequest);
-            }
-            try {
-                contents = demosModel.getFileContents(reqFileName);
-            } catch (IOException e) {
-                LOGGER.log(Level.WARNING, "Can't load demo file " + reqFileName, e);
-                throw new WicketRuntimeException("Can't load demo file");
-            }
-
-            boolean demoRequestIsHttpGet = reqFileName.endsWith(".url");
-            final String service = reqFileName.substring(0, reqFileName.indexOf('_')).toLowerCase();
-            final String serviceUrl = baseUrl + service;
-            if (demoRequestIsHttpGet) {
-                demosModel.setRequestUrl(baseUrl + contents);
-                demosModel.setRequestBody(null);
-            } else {
-                demosModel.setRequestUrl(serviceUrl);
-                demosModel.setRequestBody(contents);
-            }
-        }
+        return demoList;
     }
 }
