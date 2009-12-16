4d9ea1609185f850536bd7364371cad0e7049291
RiniAngreani <RiniAngreani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2009-12-16 03:15:58 +0000

    change level of logging to finer
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.0.x@13814 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8cb132f112953359321ff4c6be104762bfac1a08 [formerly c3dfd84c6921fad3b3a507dc810750e0d3fb29d7]
    Former-commit-id: 7c96dcea8d3b674c61309eede7b753b3d3709e6f

--- src/wfs/src/main/java/org/geoserver/wfs/xml/GML3OutputFormat.java
+++ src/wfs/src/main/java/org/geoserver/wfs/xml/GML3OutputFormat.java
@@ -175,7 +175,7 @@ public class GML3OutputFormat extends WFSGetFeatureOutputFormat {
                 encoder.setSchemaLocation(namespaceURI, userSchemaLocation);
             } else {
                 String schemaLocation = buildURL(gft.getBaseUrl(), "wfs", params, URLType.SERVICE);
-                LOGGER.config("Unable to find user-defined schema location for: " + namespaceURI
+                LOGGER.finer("Unable to find user-defined schema location for: " + namespaceURI
                         + ". Using a built schema location by default: " + schemaLocation);
                 encoder.setSchemaLocation(namespaceURI, schemaLocation);
             }
