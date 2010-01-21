42973de1c75f1297e94db64c40c2b2bf602581ef
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-01-21 18:44:07 +0000

    GEOS-3012, don't run remote ows tests by default
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@13952 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 7536d75a55a8bc86fd22eddbc830401b988adbbc [formerly 4d7c2179c1241dbb7416609c07dcc343a4661cdc]
    Former-commit-id: 66af60231dad838a1c2670402138b96fb0eb5b8d

--- src/wms/src/test/java/org/geoserver/wms/RemoteOWSTestSupport.java
+++ src/wms/src/test/java/org/geoserver/wms/RemoteOWSTestSupport.java
@@ -36,81 +36,48 @@ public class RemoteOWSTestSupport {
     
     public static final String WFS_SERVER_URL = "http://demo.opengeo.org/geoserver/wfs?";
     
-    // URL for RemoteOWS specified in remoteOws.sld
-    public static final String REMOTE_OWS_USER_STYLE_URL = "http://sigma.openplans.org:8080/geoserver/wfs?";
-    
     static Boolean remoteStatesAvailable;
         
     public static boolean isRemoteStatesAvailable(Logger logger) {
         if(remoteStatesAvailable == null) {
-            // let's check if the remote WFS tests are runnable
-            try {
-                WFSDataStoreFactory factory = new WFSDataStoreFactory();
-                Map<String, Serializable> params = new HashMap(factory.getImplementationHints());
-                URL url = new URL(WFS_SERVER_URL + "service=WFS&request=GetCapabilities");
-                params.put(WFSDataStoreFactory.URL.key, url);
-                params.put(WFSDataStoreFactory.TRY_GZIP.key, Boolean.TRUE);
-                //give it five seconds to respond...
-                params.put(WFSDataStoreFactory.TIMEOUT.key, Integer.valueOf(5000));
-                DataStore remoteStore = factory.createDataStore(params);
-                FeatureSource fs = remoteStore.getFeatureSource(TOPP_STATES);
-                remoteStatesAvailable = Boolean.TRUE;
-                // check a basic response can be answered correctly
-                DefaultQuery dq = new DefaultQuery(TOPP_STATES);
-                FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
-                dq.setFilter(ff.greater(ff.property("PERSONS"), ff.literal(20000000)));
-                FeatureCollection fc = fs.getFeatures(dq);
-                if(fc.size() != 1) {
-                    logger.log(Level.WARNING, "Remote database status invalid, there should be one and only one " +
-                            "feature with more than 20M persons in topp:states");
-                    remoteStatesAvailable = Boolean.FALSE;
-                }
-            } catch(IOException e) {
-                logger.log(Level.WARNING, "Skipping remote OWS test, either demo  " +
-                        "is down or the topp:states layer is not there", e);
+            // let's see if the remote ows tests are enabled to start with
+            String value = System.getProperty("remoteOwsTests");
+            if(value == null || !"TRUE".equalsIgnoreCase(value)) {
+                logger.log(Level.WARNING, "Skipping remote OWS test because they were not enabled via -DremoteOwsTests=true");
                 remoteStatesAvailable = Boolean.FALSE;
-            }
-        } 
-        return remoteStatesAvailable.booleanValue();
-    }
-    
-    /**
-     * Test whether the RemoteOWS URL specified in remoteOws.sld is responsive.
-     * 
-     * @param logger the logger to which status will be reported
-     * @return true if the service appears to be available
-     */
-    public static boolean isRemoteOwsUserStyleAvailable(Logger logger) {
-        InputStream input = null;
-        try {
-            URL url = new URL(REMOTE_OWS_USER_STYLE_URL + "service=WFS&request=GetCapabilities");
-            URLConnection connection = url.openConnection();
-            // service must connect within five seconds
-            connection.setConnectTimeout(5000);
-            // service must respond to read within five seconds
-            connection.setReadTimeout(5000);
-            input = connection.getInputStream();
-            if (input.read() < 0) {
-                // end of the stream
-                throw new IOException("Unexpected end of input stream");
             } else {
-                // successfully read a byte
-                logger.info("RemoteOWS user style appears to be available");
-                return true;
-            }
-        } catch (IOException e) {
-            // this includes SocketTimeoutException
-            logger.log(Level.WARNING, "RemoteOWS user style not available", e);
-            return false;
-        } finally  {
-            if (input != null) {
+                // let's check if the remote WFS tests are runnable
                 try {
-                    input.close();
-                } catch (IOException e) {
-                    // ignore, we tried
+                    WFSDataStoreFactory factory = new WFSDataStoreFactory();
+                    Map<String, Serializable> params = new HashMap(factory.getImplementationHints());
+                    URL url = new URL(WFS_SERVER_URL + "service=WFS&request=GetCapabilities");
+                    params.put(WFSDataStoreFactory.URL.key, url);
+                    params.put(WFSDataStoreFactory.TRY_GZIP.key, Boolean.TRUE);
+                    //give it five seconds to respond...
+                    params.put(WFSDataStoreFactory.TIMEOUT.key, Integer.valueOf(5000));
+                    DataStore remoteStore = factory.createDataStore(params);
+                    FeatureSource fs = remoteStore.getFeatureSource(TOPP_STATES);
+                    remoteStatesAvailable = Boolean.TRUE;
+                    // check a basic response can be answered correctly
+                    DefaultQuery dq = new DefaultQuery(TOPP_STATES);
+                    FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+                    dq.setFilter(ff.greater(ff.property("PERSONS"), ff.literal(20000000)));
+                    FeatureCollection fc = fs.getFeatures(dq);
+                    if(fc.size() != 1) {
+                        logger.log(Level.WARNING, "Remote database status invalid, there should be one and only one " +
+                                "feature with more than 20M persons in topp:states");
+                        remoteStatesAvailable = Boolean.FALSE;
+                    }
+                    
+                    logger.log(Level.WARNING, "Remote OWS tests are enabled, remote server appears to be up");
+                } catch(IOException e) {
+                    logger.log(Level.WARNING, "Skipping remote OWS test, either demo  " +
+                            "is down or the topp:states layer is not there", e);
+                    remoteStatesAvailable = Boolean.FALSE;
                 }
             }
         }
+        return remoteStatesAvailable.booleanValue();
     }
     
 }
