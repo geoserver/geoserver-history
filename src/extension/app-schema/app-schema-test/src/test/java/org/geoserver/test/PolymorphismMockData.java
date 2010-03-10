c30bf3419ab9aa68f63c82d58181906dc2902125
RiniAngreani <RiniAngreani@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-03-10 06:46:58 +0000

    GEOT-2674: Support polymorphism in app-schema
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14083 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 69fd4e32d3cc8d6c47303edb565b1d899b965618 [formerly 0a4ee23915d8e2639bc4aedb3d581c5fcf70b886]
    Former-commit-id: 69e39b98ef2e994ae5cd1ef27e008ff912bb030d

---
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/PolymorphismMockData.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright (c) 2001 - 20089 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.test;
+
+import org.geoserver.data.test.MockData;
+
+/**
+ * Mock data for PolymorphismWfsTest.
+ * 
+ * Inspired by {@link MockData}.
+ * 
+ * @author Rini Angreani, CSIRO Earth Science and Resource Engineering
+ */
+public class PolymorphismMockData extends AbstractAppSchemaMockData {
+
+    /**
+     * Prefix for ex namespace.
+     */
+    protected static final String EX_PREFIX = "ex";
+
+    /**
+     * URI for ex namespace.
+     */
+    protected static final String EX_URI = "http://example.com";
+
+    /**
+     * @see org.geoserver.test.AbstractAppSchemaMockData#addContent()
+     */
+    @Override
+    public void addContent() {
+        putNamespace(EX_PREFIX, EX_URI);
+        putNamespace(GSML_PREFIX, GSML_URI);
+        addFeatureType(EX_PREFIX, "PolymorphicFeature", "polymorphism.xml",
+                "PolymorphicFeature.properties", "CGITermValue.xml", "CGITermValue.properties",
+                "polymorphism.xsd");
+        // TODO: this is a workaround for http://jira.codehaus.org/browse/GEOT-2932
+        // remove MappedFeature definition below when it's fixed, since we don't need it
+        addFeatureType(GSML_PREFIX, "MappedFeature", "MappedFeaturePropertyfile.xml",
+                "MappedFeaturePropertyfile.properties");
+    }
+
+}
