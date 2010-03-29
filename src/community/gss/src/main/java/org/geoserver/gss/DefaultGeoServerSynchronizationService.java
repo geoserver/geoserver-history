8c1602a9a5741b9cdb6428a130543b44a5a5377a
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-03-29 09:11:56 +0000

    Hooking up 'GetCentralRevision' with real data
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14109 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b6e1d1f2efeca7e289c48fdab17bd57685a0b2a6 [formerly e9f6b4bfa4f46d96e3a83649db2a6113d9b1f8f2]
    Former-commit-id: 98175201d7c4291082c95f2c6872c9e720551c00

--- src/community/gss/src/main/java/org/geoserver/gss/DefaultGeoServerSynchronizationService.java
+++ src/community/gss/src/main/java/org/geoserver/gss/DefaultGeoServerSynchronizationService.java
@@ -6,6 +6,7 @@ package org.geoserver.gss;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -20,26 +21,38 @@ import org.geoserver.config.GeoServer;
 import org.geoserver.gss.CentralRevisionsType.LayerRevision;
 import org.geoserver.gss.GSSInfo.GSSMode;
 import org.geotools.data.DataAccess;
+import org.geotools.data.DefaultQuery;
 import org.geotools.data.Transaction;
+import org.geotools.data.VersioningDataStore;
 import org.geotools.data.jdbc.JDBCUtils;
 import org.geotools.data.postgis.VersionedPostgisDataStore;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.FilterFactory;
+import org.opengis.filter.sort.SortBy;
+import org.opengis.filter.sort.SortOrder;
 
 public class DefaultGeoServerSynchronizationService implements GeoServerSynchronizationService {
 
-    static final String SYNC_TABLES = "synch_tables";
-    
-    static final String UNIT_SYNC_TABLES_CREATION = "CREATE TABLE synch_tables(\n"
-            + "table_id SERIAL PRIMARY KEY, \n" + "name VARCHAR(256) NOT NULL, \n"
-            + "type CHAR(1) CHECK (type in ('p', 'b', '2') NOT NULL)";
-    
-    static final String SYNC_HISTORY = "synch_history";
-    
-    static final String SYNCH_HISTORY_CREATION = "CREATE TABLE synch_history(\n"
-        + "table_name VARCHAR(256) not null,\n"
-        + "local_revision LONG not null,\n"
-        + "central_revision LONG,\n"
-        + "primary key(table_name, local_revision))";
+    static final String SYNCH_TABLES = "synch_tables";
 
+    static final String UNIT_SYNC_TABLES_CREATION = "CREATE TABLE synch_tables(\n"
+            + "table_id SERIAL PRIMARY KEY, \n" // 
+            + "table_name VARCHAR(256) NOT NULL, \n" //
+            + "type CHAR(1) NOT NULL CHECK (type in ('p', 'b', '2')))";
+
+    static final String SYNCH_HISTORY = "synch_history";
+
+    static final String SYNCH_HISTORY_CREATION = "CREATE TABLE synch_history(\n"
+            + "table_name VARCHAR(256) NOT NULL,\n" //
+            + "local_revision BIGINT NOT NULL,\n" //
+            + "central_revision BIGINT,\n" //
+            + "conflictIds TEXT,\n" //
+            + "primary key(table_name, local_revision))";
+
+    private FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+    
     private Catalog catalog;
 
     private GSSInfo info;
@@ -58,8 +71,8 @@ public class DefaultGeoServerSynchronizationService implements GeoServerSynchron
             throw new GSSServiceException(
                     "The service is not properly configured, gssInfo not found");
         }
-        
-        if(info.getMode() == null) {
+
+        if (info.getMode() == null) {
             throw new GSSServiceException("The gss mode has not been configured");
         }
 
@@ -67,43 +80,45 @@ public class DefaultGeoServerSynchronizationService implements GeoServerSynchron
             throw new GSSServiceException("The service is disabled as the "
                     + "versioning datastore is not available/disabled");
         }
-        
+
         VersionedPostgisDataStore dataStore;
         try {
             DataAccess ds = info.getVersioningDataStore().getDataStore(null);
-            if(!(ds instanceof VersionedPostgisDataStore)) {
-                throw new GSSServiceException("The store attached to the gss module is not a PostGIS versioning one");
+            if (!(ds instanceof VersionedPostgisDataStore)) {
+                throw new GSSServiceException(
+                        "The store attached to the gss module is not a PostGIS versioning one");
             }
             dataStore = (VersionedPostgisDataStore) ds;
-            
-            if(info.getMode() == GSSMode.Unit) {
+
+            if (info.getMode() == GSSMode.Unit) {
                 // check the required metadata tables are there, if not, create them
                 Set<String> typeNames = new HashSet<String>(Arrays.asList(dataStore.getTypeNames()));
-                if(!typeNames.contains(SYNC_TABLES)) {
+                if (!typeNames.contains(SYNCH_TABLES)) {
                     runStatement(dataStore, UNIT_SYNC_TABLES_CREATION);
                 }
-                
-                if(!typeNames.contains(SYNC_HISTORY)) {
+                dataStore.setVersioned(SYNCH_TABLES, false, null, null);
+
+                if (!typeNames.contains(SYNCH_HISTORY)) {
                     runStatement(dataStore, SYNCH_HISTORY_CREATION);
                 }
-                
-                
+                dataStore.setVersioned(SYNCH_HISTORY, false, null, null);
             }
-        } catch(IOException e) {
-            throw new GSSServiceException("A problem occurred while checking the versioning store", e);
+        } catch (Exception e) {
+            throw new GSSServiceException("A problem occurred while checking the versioning store",
+                    e);
         }
 
-        
     }
 
-    void runStatement(VersionedPostgisDataStore dataStore, String sqlStatement) {
+    void runStatement(VersionedPostgisDataStore dataStore, String sqlStatement) throws IOException,
+            SQLException {
         Connection conn = null;
         Statement st = null;
         try {
             conn = dataStore.getConnection(Transaction.AUTO_COMMIT);
             st = conn.createStatement();
             st.execute(sqlStatement);
-        } catch(Exception e) {
+        } finally {
             // hmm... we should really try to use createSchema() instead, but atm
             // we don't have the necessary control over it
             JDBCUtils.close(st);
@@ -137,9 +152,38 @@ public class DefaultGeoServerSynchronizationService implements GeoServerSynchron
             if (fti == null) {
                 throw new GSSServiceException("Could not locate typeName: " + typeName);
             }
-
-            // TODO: actually get the central revision number from the local table
-            cr.getLayerRevisions().add(new LayerRevision(typeName, 0));
+            
+            FeatureIterator<SimpleFeature> fi = null;
+            try {
+                // get the versioning data store
+                VersioningDataStore ds = (VersioningDataStore) info.getVersioningDataStore().getDataStore(null);
+                
+                // check the table is actually synch-ed
+                DefaultQuery q = new DefaultQuery();
+                q.setFilter(ff.equal(ff.property("table_name"), ff.literal(fti.getName()), true));
+                int count = ds.getFeatureSource(SYNCH_TABLES).getCount(q);
+                if(count == 0) {
+                    throw new GSSServiceException(fti.getName() + " is not a synchronised layer");
+                }
+                
+                // gather the record from the synch history table
+                q = new DefaultQuery();
+                q.setFilter(ff.equal(ff.property("table_name"), ff.literal(fti.getName()), true));
+                q.setSortBy(new SortBy[] {ff.sort("central_revision", SortOrder.DESCENDING)});
+                q.setMaxFeatures(1);
+                fi = ds.getFeatureSource(SYNCH_HISTORY).getFeatures(q).features();
+                long revision = -1;
+                if(fi.hasNext()) {
+                   revision = ((Number) fi.next().getAttribute("central_revision")).longValue(); 
+                }
+                cr.getLayerRevisions().add(new LayerRevision(typeName, revision));
+            } catch(IOException e) {
+                throw new GSSServiceException("Could not compute the response", e);
+            } finally {
+                if(fi != null) {
+                    fi.close();
+                }
+            }
         }
 
         return cr;
--- src/community/gss/src/test/resources/unit.sql
+++ src/community/gss/src/test/resources/unit.sql
@@ -6,7 +6,9 @@ DROP TABLE roads cascade;
 DROP TABLE archsites cascade;
 DROP TABLE restricted cascade;
 
-
+-- clean up GSS metadata tables
+DROP TABLE synch_tables cascade;
+DROP TABLE synch_history cascade;
 
 -- restricted areas
 CREATE TABLE restricted (gid serial NOT NULL, cat bigint, the_geom geometry, CONSTRAINT enforce_dims_the_geom CHECK ((ndims(the_geom) = 2)), CONSTRAINT enforce_geotype_the_geom CHECK (((geometrytype(the_geom) = 'MULTIPOLYGON'::text) OR (the_geom IS NULL))), CONSTRAINT enforce_srid_the_geom CHECK ((srid(the_geom) = 26713)));
