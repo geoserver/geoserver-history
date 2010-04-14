cf97fba292a861ad54b2ce2c4fcb013d382e4840
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-04-14 09:51:26 +0000

    Adding support for PostDiff in the HTTP client
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14225 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a9e2c1801a447a8f0f0bb05f84bbba161f7e2900 [formerly bb25e438b6d107085cb3edc6f812640f7f5680a0]
    Former-commit-id: e60517bebd1d6897dbc9564f59ae260f7db51cd7

--- community/gss/src/main/java/org/geoserver/gss/HTTPGSSClient.java
+++ community/gss/src/main/java/org/geoserver/gss/HTTPGSSClient.java
@@ -5,26 +5,41 @@
 package org.geoserver.gss;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.xml.namespace.QName;
 
 import net.opengis.ows10.ExceptionReportType;
 import net.opengis.ows10.ExceptionType;
+import net.opengis.wfs.DeleteElementType;
+import net.opengis.wfs.InsertElementType;
 import net.opengis.wfs.TransactionType;
+import net.opengis.wfs.UpdateElementType;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.NamespaceInfo;
 import org.geoserver.gss.CentralRevisionsType.LayerRevision;
+import org.geoserver.gss.xml.GSS;
 import org.geoserver.gss.xml.GSSConfiguration;
+import org.geotools.xml.Encoder;
 import org.geotools.xml.Parser;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.type.Name;
+import org.xml.sax.helpers.NamespaceSupport;
 
 /**
- * A client that connects to a single host. The client is not supposed to be thread safe, use
- * multiple instances if necessary
+ * A client that connects to a single Unit.
  * 
  * @author Andrea Aime - OpenGeo
  * 
@@ -41,8 +56,10 @@ public class HTTPGSSClient implements GSSClient {
 
     GSSConfiguration configuration;
 
-    public HTTPGSSClient(HttpClient client, GSSConfiguration configuration, URL gssServiceURL,
-            String username, String password) {
+    Catalog catalog;
+
+    public HTTPGSSClient(HttpClient client, GSSConfiguration configuration, Catalog catalog,
+            URL gssServiceURL, String username, String password) {
         if (gssServiceURL.getPath().endsWith("/")) {
             try {
                 String external = gssServiceURL.toExternalForm();
@@ -60,29 +77,13 @@ public class HTTPGSSClient implements GSSClient {
     }
 
     public long getCentralRevision(QName layerName) throws IOException {
-        // grab the response and parse it to an object representation
-        Object response;
+        // execute the GetCentralRevision call
         GetMethod method = new GetMethod(address
-                + "?service=GSS&version=1.0.0&request=GetCentralRevision&typeName=" + prefixedName(layerName));
-        try {
-            int statusCode = client.executeMethod(method);
+                + "?service=GSS&version=1.0.0&request=GetCentralRevision&typeName="
+                + prefixedName(layerName));
+        Object response = executeMethod(layerName, method);
 
-            if (statusCode != HttpStatus.SC_OK) {
-                throw new IOException("HTTP client returned with code " + statusCode);
-            }
-
-            Parser parser = new Parser(configuration);
-            parser.setStrict(true);
-            parser.setFailOnValidationError(true);
-            response = parser.parse(method.getResponseBodyAsStream());
-        } catch (Exception e) {
-            throw (IOException) new IOException("Error occurred while asking unit about "
-                    + "the last central revision for layer " + layerName).initCause(e);
-        } finally {
-            method.releaseConnection();
-        }
-
-        // interpret the parsed response, if any
+        // interpret the parsed response
         if (response instanceof CentralRevisionsType) {
             CentralRevisionsType cr = (CentralRevisionsType) response;
             for (LayerRevision lr : cr.getLayerRevisions()) {
@@ -92,9 +93,6 @@ public class HTTPGSSClient implements GSSClient {
             }
             throw new IOException("Response to GetCentralRevision received, but it "
                     + "did not contain a central revision for " + layerName);
-        } else if (response instanceof ExceptionReportType) {
-            throw convertServiceException("The Unit service reported a failure: ",
-                    (ExceptionReportType) response);
         } else {
             if (response == null) {
                 throw new IOException("The response was parsed to a null object");
@@ -108,29 +106,164 @@ public class HTTPGSSClient implements GSSClient {
         return layerName.getPrefix() + ":" + layerName.getLocalPart();
     }
 
-    IOException convertServiceException(String intro, ExceptionReportType response) {
-        StringBuilder sb = new StringBuilder(intro);
-        for (Iterator it = response.getException().iterator(); it.hasNext();) {
-            ExceptionType et = (ExceptionType) it.next();
-            for (Iterator eit = et.getExceptionText().iterator(); eit.hasNext();) {
-                String text = (String) eit.next();
-                sb.append(text);
-                if (eit.hasNext())
-                    sb.append(". ");
-            }
-
-        }
-
-        return new IOException(sb.toString());
-    }
-
     public TransactionType getDiff(QName layerName, long fromVersion) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     public void postDiff(QName layerName, long fromVersion, long toVersion, TransactionType changes)
             throws IOException {
-        throw new UnsupportedOperationException();
+        // build the post diff request
+        PostDiffType postDiff = new PostDiffType();
+        postDiff.setTypeName(layerName);
+        postDiff.setFromVersion(fromVersion);
+        postDiff.setToVersion(toVersion);
+        postDiff.setTransaction(changes);
+
+        // prepare the encoder
+        Encoder encoder = buildEncoderForTransaction(changes);
+
+        // prepare POST request
+        PostMethod method = new PostMethod(address.toExternalForm());
+        method.setContentChunked(true);
+        method.setRequestEntity(new XMLEntity(postDiff, GSS.PostDiff, encoder));
+
+        // execute the request
+        Object response = executeMethod(layerName, method);
+
+        // interpret the parsed response
+        if (response instanceof PostDiffResponseType) {
+            // that's fine, it's like a placeholder for now
+        } else {
+            if (response == null) {
+                throw new IOException("The response was parsed to a null object");
+            }
+            throw new IOException("The response was parsed to an unrecognized object type: "
+                    + response.getClass());
+        }
+    }
+
+    /**
+     * Executes the http method, checks the response status, parses the response and returns it.
+     * Will throw an exception in case of communication errors, error codes, or service exceptions
+     * 
+     * @throws IOException
+     */
+    Object executeMethod(QName layerName, HttpMethod method) throws IOException {
+        Object response;
+        try {
+            // plain execution
+            int statusCode = client.executeMethod(method);
+
+            // check the HTTP status
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new IOException("HTTP client returned with code " + statusCode);
+            }
+
+            // parse the response
+            Parser parser = new Parser(configuration);
+            parser.setStrict(true);
+            parser.setFailOnValidationError(true);
+            response = parser.parse(method.getResponseBodyAsStream());
+        } catch (Exception e) {
+            throw (IOException) new IOException("Error occurred while executing "
+                    + "a call to the Unit").initCause(e);
+        } finally {
+            method.releaseConnection();
+        }
+
+        // convert a service exception into an IOException if necessary
+        if (response instanceof ExceptionReportType) {
+            ExceptionReportType report = (ExceptionReportType) response;
+            StringBuilder sb = new StringBuilder("The Unit service reported a failure: ");
+            for (Iterator it = report.getException().iterator(); it.hasNext();) {
+                ExceptionType et = (ExceptionType) it.next();
+                for (Iterator eit = et.getExceptionText().iterator(); eit.hasNext();) {
+                    String text = (String) eit.next();
+                    sb.append(text);
+                    if (eit.hasNext())
+                        sb.append(". ");
+                }
+
+            }
+
+            throw new IOException(sb.toString());
+        }
+
+        return response;
+    }
+
+    /**
+     * Builds a XML encoder for the specified transaction. The code will declare all
+     * prefix/namespace URI associations necessary for the elements in the transaction
+     */
+    Encoder buildEncoderForTransaction(TransactionType changes) throws IOException {
+        Encoder encoder = new Encoder(configuration, configuration.getXSD().getSchema());
+
+        // try to declare all namespace prefixes properly
+        NamespaceSupport namespaces = encoder.getNamespaces();
+        List<DeleteElementType> deletes = changes.getDelete();
+        List<UpdateElementType> updates = changes.getUpdate();
+        List<InsertElementType> inserts = changes.getInsert();
+        for (DeleteElementType delete : deletes) {
+            QName typeName = delete.getTypeName();
+            namespaces.declarePrefix(typeName.getPrefix(), typeName.getNamespaceURI());
+        }
+        for (UpdateElementType update : updates) {
+            QName typeName = update.getTypeName();
+            namespaces.declarePrefix(typeName.getPrefix(), typeName.getNamespaceURI());
+        }
+        for (InsertElementType insert : inserts) {
+            List<SimpleFeature> features = insert.getFeature();
+            for (SimpleFeature feature : features) {
+                Name typeName = feature.getType().getName();
+                NamespaceInfo nsi = catalog.getNamespaceByURI(typeName.getNamespaceURI());
+                if (nsi != null) {
+                    namespaces.declarePrefix(nsi.getPrefix(), nsi.getURI());
+                }
+            }
+        }
+
+        encoder.setEncoding(Charset.forName("UTF-8"));
+        return encoder;
+    }
+
+    /**
+     * An XML entitity based
+     * 
+     * @author aaime
+     * 
+     */
+    static class XMLEntity implements RequestEntity {
+
+        Encoder encoder;
+
+        Object object;
+
+        QName element;
+
+        public XMLEntity(Object object, QName element, Encoder encoder) {
+            super();
+            this.encoder = encoder;
+            this.object = object;
+            this.element = element;
+        }
+
+        public long getContentLength() {
+            return -1;
+        }
+
+        public String getContentType() {
+            return "text/xml";
+        }
+
+        public boolean isRepeatable() {
+            return true;
+        }
+
+        public void writeRequest(OutputStream os) throws IOException {
+            encoder.encode(object, element, os);
+        }
+
     }
 
 }
