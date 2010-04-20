465b1313f2275a5ddb9986c0af53d990983b19b9
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-04-20 18:27:46 +0000

    [GEOS-3849] Too many open files left by wicket UI
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14272 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 0fac7713e7dfd91751110ecc1f6a6c266ddce3fd [formerly f264eb7b02018c3259dd7fa4f6efde40566df111]
    Former-commit-id: 49ca46edc44550fcc309627a76b659ac17f94bdd

--- web/core/src/main/java/org/geoserver/web/GeoServerApplication.java
+++ web/core/src/main/java/org/geoserver/web/GeoServerApplication.java
@@ -4,27 +4,18 @@
  */
 package org.geoserver.web;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
-import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 import org.apache.wicket.Application;
 import org.apache.wicket.IConverterLocator;
 import org.apache.wicket.IRequestTarget;
 import org.apache.wicket.Page;
 import org.apache.wicket.Request;
-import org.apache.wicket.RequestCycle;
 import org.apache.wicket.Response;
 import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.PageExpiredException;
@@ -39,10 +30,6 @@ import org.apache.wicket.resource.loader.ComponentStringResourceLoader;
 import org.apache.wicket.resource.loader.IStringResourceLoader;
 import org.apache.wicket.spring.SpringWebApplication;
 import org.apache.wicket.util.convert.ConverterLocator;
-import org.apache.wicket.util.resource.AbstractResourceStream;
-import org.apache.wicket.util.resource.IResourceStream;
-import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
-import org.apache.wicket.util.resource.locator.ResourceStreamLocator;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.config.GeoServer;
 import org.geoserver.platform.GeoServerExtensions;
@@ -221,65 +208,6 @@ public class GeoServerApplication extends SpringWebApplication {
         return s;
     }
     
-    /**
-     * A custom resource stream locator which supports loading i18n properties
-     * files on a single file per module basis.
-     */
-    public static class GeoServerResourceStreamLocator extends ResourceStreamLocator {
-        static Pattern GS_PROPERTIES = Pattern.compile("GeoServerApplication.*.properties");
-        static Pattern GS_LOCAL_I18N = Pattern.compile("org/geoserver/.*(\\.properties|\\.xml)]");
-        
-        @SuppressWarnings({ "unchecked", "serial" })
-        public IResourceStream locate(Class clazz, String path) {
-            int i = path.lastIndexOf("/");
-            if (i != -1) {
-                String p = path.substring(i + 1);
-                if (GS_PROPERTIES.matcher(p).matches()) {
-                    try {
-                        // process the classpath for property files
-                        Enumeration<URL> urls = getClass().getClassLoader()
-                                .getResources(p);
-
-                        // build up a single properties file
-                        Properties properties = new Properties();
-
-                        while (urls.hasMoreElements()) {
-                            URL url = urls.nextElement();
-
-                            InputStream in = url.openStream();
-                            properties.load(in);
-                            in.close();
-                        }
-
-                        // transform the properties to a stream
-                        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-                        properties.store(out, "");
-
-                        return new AbstractResourceStream() {
-                            public InputStream getInputStream()
-                                    throws ResourceStreamNotFoundException {
-                                return new ByteArrayInputStream(out
-                                        .toByteArray());
-                            }
-
-                            public void close() throws IOException {
-                                out.close();
-                            }
-                        };
-                    } catch (IOException e) {
-                        LOGGER.log(Level.WARNING, "", e);
-                    }
-                } else if(GS_LOCAL_I18N.matcher(path).matches()) {
-                    return null;
-                } else if(path.matches("org/geoserver/.*" + clazz.getName() + ".*_.*.html")) {
-                    return null;
-                }
-            }
-
-            return super.locate(clazz, path);
-        }
-    }
-
     /*
      * Overrides to return a custom request cycle processor. This is done in
      * order to support "dynamic dispatching" from web.xml.
