634e4bb426e0931526b0aa67a7cade6d0bc57cc7
mcr <mcr@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-05-04 16:11:09 +0000

    llow reuse of Datastores from the GeoServer Catalog on 2.0.x
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.0.x@14307 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c9480f0fe3338b7dc85dfdd52766c38055d030f9 [formerly 0a64d4fe3520c1967adada0578172040ba859e71]
    Former-commit-id: 15516bee9ca68c77b0d9676e4472773068b52b81

--- src/extension/feature-pregeneralized/src/main/java/org/geoserver/data/gen/DSFinderRepository.java
+++ src/extension/feature-pregeneralized/src/main/java/org/geoserver/data/gen/DSFinderRepository.java
@@ -10,39 +10,58 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLDecoder;
 
+import org.apache.log4j.Logger;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.platform.GeoServerExtensions;
+import org.geotools.data.DataStore;
 import org.geotools.data.Repository;
+import org.opengis.feature.type.Name;
 import org.vfny.geoserver.global.GeoserverDataDirectory;
 
-
 /**
- * Implementation of {@link Repository} 
- * This class interprets the data source name as a file name or an URL for
- * a property file containing the ds creation parameters
+ * Implementation of {@link Repository}
  * 
- * For shape files ending with .shp or SHP, the shape file could be passed as
- * name
+ * The class makes a lookup in the goeserver catalog.
+ * 
+ * If nothing is found, the class interprets the data source name as a file name or an URL for a
+ * property file containing the ds creation parameters
+ * 
+ * For shape files ending with .shp or SHP, the shape file could be passed as name
  * 
  * 
  * @author Christian Mueller
- *
+ * 
  */
 public class DSFinderRepository extends org.geotools.data.gen.DSFinderRepository {
-	
-			
-	
 
-	
-	protected URL getURLForLocation(String location) throws IOException{
+    protected URL getURLForLocation(String location) throws IOException {
 
-		File f = GeoserverDataDirectory.findDataFile(location);
-		URL url = null;		
-		if (f.exists()) {
-			url=f.toURI().toURL();
-		} else {
-			url = new URL(location);
-		}		
-		url = new URL(URLDecoder.decode(url.toExternalForm(),"UTF8"));			
-		return url;
-	}
-	
+        File f = GeoserverDataDirectory.findDataFile(location);
+        URL url = null;
+        if (f.exists()) {
+            url = f.toURI().toURL();
+        } else {
+            url = new URL(location);
+        }
+        url = new URL(URLDecoder.decode(url.toExternalForm(), "UTF8"));
+        return url;
+    }
+
+    @Override
+    public DataStore dataStore(Name name) {
+        Catalog catalog = (Catalog) GeoServerExtensions.bean("catalog");
+        DataStoreInfo info = catalog
+                .getDataStoreByName(name.getNamespaceURI(), name.getLocalPart());
+        if (info != null) {
+            try {
+                return (DataStore) info.getDataStore(null);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        Logger.getLogger(this.getClass().getName()).info("Not in Geoserver catalog: "+name.toString());
+        return super.dataStore(name);
+    }
+
 }
