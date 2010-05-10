30302289806118067b8233820e5d2acf7d495ffb
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-05-10 11:16:29 +0000

    Fixing test failing due to changes in the components paths
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14347 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 39d7c783368c441cb030f5b87ec92133ec050d75 [formerly bc0d448c457aadc2d3e8c912e864f1d2d82f00d3]
    Former-commit-id: 8d1adef826672541fc2349f20f2c4c8fa2d04c98

--- src/web/wms/src/test/java/org/geoserver/wms/web/data/publish/WMSLayerConfigTest.java
+++ src/web/wms/src/test/java/org/geoserver/wms/web/data/publish/WMSLayerConfigTest.java
@@ -31,18 +31,19 @@ public class WMSLayerConfigTest extends GeoServerWicketTestSupport {
         tester.startPage(page);
         tester.assertRenderedPage(FormTestPage.class);
         tester.assertComponent("form", Form.class);
-        tester.assertComponent("form:panel:defaultStyle", DropDownChoice.class);
+        tester.assertComponent("form:panel:styles:defaultStyle", DropDownChoice.class);
         
         // check selecting something else works
         StyleInfo target = ((List<StyleInfo>) new StylesModel().getObject()).get(0); 
         FormTester ft = tester.newFormTester("form");
-        ft.select("panel:defaultStyle", 0);
+        ft.select("panel:styles:defaultStyle", 0);
         ft.submit();
-        tester.assertModelValue("form:panel:defaultStyle", target);
+        tester.assertModelValue("form:panel:styles:defaultStyle", target);
     }
     
     public void testNew() {
         final LayerInfo layer = getCatalog().getFactory().createLayer();
+        layer.setResource(getCatalog().getFactory().createFeatureType());
         FormTestPage page = new FormTestPage(new ComponentBuilder() {
 
             public Component buildComponent(String id) {
@@ -50,12 +51,12 @@ public class WMSLayerConfigTest extends GeoServerWicketTestSupport {
             }
         }
         );
-        Component layerConfig = page.get("form:panel:defaultStyle");
+        Component layerConfig = page.get("form:panel:styles:defaultStyle");
         
         tester.startPage(page);
         tester.assertRenderedPage(FormTestPage.class);
         tester.assertComponent("form", Form.class);
-        tester.assertComponent("form:panel:defaultStyle", DropDownChoice.class);
+        tester.assertComponent("form:panel:styles:defaultStyle", DropDownChoice.class);
         
         // check submitting like this will create errors, there is no selection
         tester.submitForm("form");
@@ -65,7 +66,7 @@ public class WMSLayerConfigTest extends GeoServerWicketTestSupport {
         // now set something and check there are no messages this time
         page.getSession().getFeedbackMessages().clear();
         FormTester ft = tester.newFormTester("form");
-        ft.select("panel:defaultStyle", 0);
+        ft.select("panel:styles:defaultStyle", 0);
         ft.submit();
         assertFalse(page.getSession().getFeedbackMessages().hasErrorMessageFor(layerConfig));
     }
