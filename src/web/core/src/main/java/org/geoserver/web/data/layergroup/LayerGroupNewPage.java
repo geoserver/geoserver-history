305c2a3c03a16745410dd2b5549e8fb668565c49
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-05-11 23:11:37 +0000

    GEOS-3634, fixing empty layer group issues
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14360 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6f4c0345545d5695575615558be23dec1696fe34 [formerly de6c8b7594f761b4989d12755b5760e54d3904bc]
    Former-commit-id: b653185d49252d187e113f54349bd3bdb906bab0

--- src/web/core/src/main/java/org/geoserver/web/data/layergroup/LayerGroupNewPage.java
+++ src/web/core/src/main/java/org/geoserver/web/data/layergroup/LayerGroupNewPage.java
@@ -4,70 +4,35 @@
  */
 package org.geoserver.web.data.layergroup;
 
-import java.util.Collections;
 import java.util.logging.Level;
 
-import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.SubmitLink;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.validation.IValidatable;
-import org.apache.wicket.validation.validator.AbstractValidator;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.LayerGroupInfo;
-import org.geoserver.web.GeoServerSecuredPage;
 
 /**
  * Allows creation of a new layer group
  */
 @SuppressWarnings("serial")
-public class LayerGroupNewPage extends GeoServerSecuredPage {
-
-    public LayerGroupNewPage() {
-        LayerGroupInfo lg = getCatalog().getFactory().createLayerGroup();
-        Form form = new Form( "form", new CompoundPropertyModel( lg ) );
-        add(form);
-        
-        TextField name = new TextField( "name" );
-        name.setRequired(true);
-        name.add(new GroupNameValidator());
-        form.add(name);
-        form.add( new SubmitLink( "submit", form ) {
-            @Override
-            public void onSubmit() {
-                LayerGroupInfo lg = (LayerGroupInfo) getForm().getModelObject();
-                
-                Catalog catalog = getCatalog();
-                try {
-                    catalog.add( lg );
-                    
-                    lg = catalog.getLayerGroup( lg.getId() );
-                    setResponsePage(LayerGroupEditPage.class, 
-                            new PageParameters(LayerGroupEditPage.GROUP + "=" + lg.getName()));
-                } catch( Exception e ) {
-                    error( e );
-                    LOGGER.log( Level.WARNING, "Error creating new layer group.", e );
-                }
-            }
-            
-        });
-        form.add( new SubmitLink( "cancel", form ) {
-            @Override
-            public void onSubmit() {
-                setResponsePage(LayerGroupPage.class);
-            }
-        });
-    }
+public class LayerGroupNewPage extends AbstractLayerGroupPage {
     
-    class GroupNameValidator extends AbstractValidator {
-
-        @Override
-        protected void onValidate(IValidatable validatable) {
-            String name = (String) validatable.getValue();
-            if(getCatalog().getLayerGroupByName(name) != null)
-                error(validatable, "duplicateGroupNameError", Collections.singletonMap("name", name));
-        }
-        
+    public LayerGroupNewPage() {
+        initUI(getCatalog().getFactory().createLayerGroup());
     }
+
+    @Override
+    protected void onSubmit() {
+        LayerGroupInfo lg = (LayerGroupInfo) lgModel.getObject();
+
+        Catalog catalog = getCatalog();
+        try {
+            catalog.add(lg);
+
+            lg = catalog.getLayerGroup(lg.getId());
+            setResponsePage(LayerGroupPage.class);
+        } catch (Exception e) {
+            error(e);
+            LOGGER.log(Level.WARNING, "Error creating new layer group.", e);
+        }
+    }
+
 }
