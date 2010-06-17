294634655a5b7efc76c9b4421c9a0f05c8611104
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-06-17 17:24:51 +0000

    GEOS-3998, fixing GeoServerInfo client properties bug and adding test
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14501 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5797612a56cf2bb9c00af7f1b991ae66108f60e3 [formerly e69cc0f13f0e240e2c45cd21165877f48e95fc0f]
    Former-commit-id: 0962a0d354b0a766e7952ef84105ff33257724ee

--- src/main/src/test/java/org/geoserver/config/GeoServerImplTest.java
+++ src/main/src/test/java/org/geoserver/config/GeoServerImplTest.java
@@ -1,11 +1,14 @@
 package org.geoserver.config;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
 import org.geoserver.config.impl.GeoServerImpl;
+import org.geoserver.config.impl.GeoServerInfoImpl;
 import org.geoserver.config.impl.ServiceInfoImpl;
 
 public class GeoServerImplTest extends TestCase {
@@ -131,5 +134,17 @@ public class GeoServerImplTest extends TestCase {
             sOldValues.addAll( oldValues );
             sNewValues.addAll( newValues );
         }
+    }
+    
+
+    public void testSetClientPropsHasEffect() throws Exception {
+        GeoServerInfoImpl gsii = new GeoServerInfoImpl(geoServer);
+        Map<Object, Object> before = gsii.getClientProperties();
+        
+        Map<Object, Object> newProps = new HashMap<Object, Object>();
+        newProps.put("123", "456");
+		gsii.setClientProperties(newProps);
+		
+		assertFalse(before.equals(newProps));
     }
 }
