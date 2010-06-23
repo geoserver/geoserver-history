e4c3c81f39e2d2d5b7b06c145a06f064997fc2be
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-06-23 14:59:15 +0000

    Adding download link
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14519 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 948a9acb3728a729dbe180dbf07ceb3d02d9f624 [formerly 58ef8c78cebef8d2b925c3879f4f628a4bbed2b4]
    Former-commit-id: 57178c48c7a74e894688a3fe5b2d14f3bce37a6a

--- src/web/core/src/main/java/org/geoserver/web/admin/LogPage.java
+++ src/web/core/src/main/java/org/geoserver/web/admin/LogPage.java
@@ -6,103 +6,154 @@ package org.geoserver.web.admin;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.logging.Level;
 
+import org.apache.wicket.IRequestTarget;
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.RequestCycle;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.protocol.http.WebResponse;
+import org.apache.wicket.util.io.Streams;
 import org.apache.wicket.validation.validator.NumberValidator;
 import org.geoserver.config.GeoServerDataDirectory;
 import org.geoserver.web.GeoServerSecuredPage;
 
 /**
  * Shows the log file contents
+ * 
  * @author Andrea Aime - OpenGeo
  */
 public class LogPage extends GeoServerSecuredPage {
     static final String LINES = "lines";
+
     int lines = 1000;
+
+    File logFile;
 
     public LogPage(PageParameters params) {
         Form form = new Form("form");
         add(form);
-        
+
+        // locate the geoserver.log file
+        GeoServerDataDirectory dd = getGeoServerApplication().getBeanOfType(
+                GeoServerDataDirectory.class);
+        File logDirectory = new File(dd.root(), "logs");
+        if (!logDirectory.exists()) {
+            error("Could not find the logs directory inside the data dir: "
+                    + logDirectory.getAbsolutePath());
+        }
+        logFile = new File(logDirectory, "geoserver.log");
+        if (!logFile.exists()) {
+            error("Could not find the GeoServer log file: " + logFile.getAbsolutePath());
+        }
+
         try {
-            if(params.getKey(LINES) != null) {
-                if(params.getInt(LINES) > 0) {
-                   lines = params.getInt(LINES); 
+            if (params.getKey(LINES) != null) {
+                if (params.getInt(LINES) > 0) {
+                    lines = params.getInt(LINES);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Error parsing the lines parameter: ", params.getKey(LINES));
         }
-        
+
         form.add(new SubmitLink("refresh") {
             @Override
             public void onSubmit() {
-                setResponsePage(LogPage.class, new PageParameters(LINES + "=" + String.valueOf(lines)));
+                setResponsePage(LogPage.class, new PageParameters(LINES + "="
+                        + String.valueOf(lines)));
             }
         });
-        
+
         TextField lines = new TextField("lines", new PropertyModel(this, "lines"));
         lines.add(NumberValidator.POSITIVE);
         form.add(lines);
-        
+
         TextArea logs = new TextArea("logs", new GSLogsModel());
         logs.setOutputMarkupId(true);
         logs.setMarkupId("logs");
         add(logs);
+
+        add(new Link("download") {
+
+            @Override
+            public void onClick() {
+                RequestCycle.get().setRequestTarget(new IRequestTarget() {
+
+                    public void detach(RequestCycle requestCycle) {
+                    }
+
+                    public void respond(RequestCycle requestCycle) {
+
+                        InputStream is = null;
+                        try {
+                            is = new FileInputStream(logFile);
+
+                            WebResponse r = (WebResponse) requestCycle.getResponse();
+                            r.setAttachmentHeader("geoserver.log");
+                            r.setContentType("text/plain");
+                            Streams.copy(is, r.getOutputStream());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            if(is != null) {
+                                try {
+                                    is.close();
+                                } catch (IOException e) {
+                                }
+                            }
+                        }
+                    }
+
+                });
+
+            }
+        });
+
     }
-    
-    
+
     public class GSLogsModel extends LoadableDetachableModel {
 
         @Override
         protected Object load() {
             BufferedReader br = null;
             try {
-                // locate the geoserver.log file
-                GeoServerDataDirectory dd = getGeoServerApplication().getBeanOfType(GeoServerDataDirectory.class);
-                File logDirectory = new File(dd.root(), "logs");
-                if(!logDirectory.exists()) {
-                    return "Could not find the logs directory inside the data dir: " + logDirectory.getAbsolutePath();
-                }
-                File logFile = new File(logDirectory, "geoserver.log");
-                if(!logFile.exists()) {
-                    return "Could not find the GeoServer log file: " + logFile.getAbsolutePath();
-                }
-                
                 // load the logs line by line, keep only the last 1000 lines
                 LinkedList<String> lineList = new LinkedList<String>();
-                
+
                 br = new BufferedReader(new FileReader(logFile));
                 String line;
-                while((line = br.readLine()) != null) {
+                while ((line = br.readLine()) != null) {
                     lineList.addLast(line);
-                    if(lineList.size() > LogPage.this.lines) {
+                    if (lineList.size() > LogPage.this.lines) {
                         lineList.removeFirst();
                     }
                 }
-                
+
                 StringBuilder result = new StringBuilder();
                 for (String logLine : lineList) {
                     result.append(logLine).append("\n");
                 }
                 return result;
-            } catch(Exception e) {
+            } catch (Exception e) {
                 error(e);
                 return e.getMessage();
             } finally {
-                if(br != null) {
+                if (br != null) {
                     try {
                         br.close();
-                    } catch(Exception e) {
+                    } catch (Exception e) {
                         // ignore
                     }
                 }
