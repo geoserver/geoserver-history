137d32cf007fa23efd646ff28227fb6d3f96596d
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-07-02 09:01:28 +0000

    Compute and report the blacklist just once
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14540 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 86c4413072001afb0f55b2a1e1cfc929acaa8397 [formerly edfaedbe7b498f49ed6564ed3ce345e66e86e7cd]
    Former-commit-id: 3579b08369aa6005b79a0c5ff538c8561fb4a15a

--- src/community/wps/src/main/java/org/geoserver/wps/GetCapabilities.java
+++ src/community/wps/src/main/java/org/geoserver/wps/GetCapabilities.java
@@ -5,10 +5,10 @@
 
 package org.geoserver.wps;
 
-import java.math.BigInteger;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,25 +19,18 @@ import net.opengis.ows11.Ows11Factory;
 import net.opengis.ows11.ResponsiblePartySubsetType;
 import net.opengis.ows11.ServiceIdentificationType;
 import net.opengis.ows11.ServiceProviderType;
-import net.opengis.wps10.ComplexDataDescriptionType;
 import net.opengis.wps10.DefaultType2;
 import net.opengis.wps10.GetCapabilitiesType;
-import net.opengis.wps10.InputDescriptionType;
 import net.opengis.wps10.LanguagesType;
 import net.opengis.wps10.LanguagesType1;
-import net.opengis.wps10.LiteralInputType;
 import net.opengis.wps10.ProcessBriefType;
 import net.opengis.wps10.ProcessOfferingsType;
-import net.opengis.wps10.SupportedComplexDataInputType;
 import net.opengis.wps10.WPSCapabilitiesType;
 import net.opengis.wps10.Wps10Factory;
 
 import org.geoserver.config.GeoServerInfo;
 import org.geoserver.ows.Ows11Util;
 import org.geoserver.ows.util.RequestUtils;
-import org.geoserver.wps.ppio.BoundingBoxPPIO;
-import org.geoserver.wps.ppio.ComplexPPIO;
-import org.geoserver.wps.ppio.LiteralPPIO;
 import org.geoserver.wps.ppio.ProcessParameterIO;
 import org.geotools.data.Parameter;
 import org.geotools.process.ProcessFactory;
@@ -51,8 +44,12 @@ import org.springframework.context.ApplicationContext;
  */
 public class GetCapabilities {
     public WPSInfo wps;
+
     ApplicationContext context;
-    static final Logger LOGGER = Logging.getLogger(GetCapabilities.class); 
+
+    static final Logger LOGGER = Logging.getLogger(GetCapabilities.class);
+
+    private static Set<Name> PROCESS_BLACKLIST = Collections.EMPTY_SET;
 
     public GetCapabilities(WPSInfo wps, ApplicationContext context) {
         this.wps = wps;
@@ -72,139 +69,153 @@ public class GetCapabilities {
         }
 
         // TODO: add update sequence negotiation
-        
+
         // encode the response
         Wps10Factory wpsf = Wps10Factory.eINSTANCE;
         Ows11Factory owsf = Ows11Factory.eINSTANCE;
-        
+
         WPSCapabilitiesType caps = wpsf.createWPSCapabilitiesType();
         caps.setVersion("1.0.0");
-        
-        //TODO: make configurable
-        caps.setLang( "en" );
-        
-        //ServiceIdentification
+
+        // TODO: make configurable
+        caps.setLang("en");
+
+        // ServiceIdentification
         ServiceIdentificationType si = owsf.createServiceIdentificationType();
-        caps.setServiceIdentification( si );
-        
-        si.getTitle().add( Ows11Util.languageString( wps.getTitle() ) );
-        si.getAbstract().add( Ows11Util.languageString( wps.getAbstract() ) );
-        
-        KeywordsType kw = Ows11Util.keywords( wps.getKeywords( ) ); ;
-        if ( kw != null ) {
-            si.getKeywords().add( kw );
+        caps.setServiceIdentification(si);
+
+        si.getTitle().add(Ows11Util.languageString(wps.getTitle()));
+        si.getAbstract().add(Ows11Util.languageString(wps.getAbstract()));
+
+        KeywordsType kw = Ows11Util.keywords(wps.getKeywords());
+        ;
+        if (kw != null) {
+            si.getKeywords().add(kw);
         }
-        
-        si.setServiceType( Ows11Util.code( "WPS" ) );
-        si.getServiceTypeVersion().add( "1.0.0" );
-        si.setFees( wps.getFees() );
-        
-        if ( wps.getAccessConstraints() != null ) {
-            si.getAccessConstraints().add( wps.getAccessConstraints() );    
+
+        si.setServiceType(Ows11Util.code("WPS"));
+        si.getServiceTypeVersion().add("1.0.0");
+        si.setFees(wps.getFees());
+
+        if (wps.getAccessConstraints() != null) {
+            si.getAccessConstraints().add(wps.getAccessConstraints());
         }
-        
-        //ServiceProvider
+
+        // ServiceProvider
         ServiceProviderType sp = owsf.createServiceProviderType();
-        caps.setServiceProvider( sp );
-        
-        //TODO: set provder name from context
+        caps.setServiceProvider(sp);
+
+        // TODO: set provder name from context
         GeoServerInfo geoServer = wps.getGeoServer().getGlobal();
-        if ( geoServer.getContact().getContactOrganization() != null ) {
-            sp.setProviderName(  geoServer.getContact().getContactOrganization()  );    
+        if (geoServer.getContact().getContactOrganization() != null) {
+            sp.setProviderName(geoServer.getContact().getContactOrganization());
+        } else {
+            sp.setProviderName("GeoServer");
         }
-        else {
-            sp.setProviderName( "GeoServer" );
-        }
-        
-        
+
         sp.setProviderSite(owsf.createOnlineResourceType());
-        sp.getProviderSite().setHref( geoServer.getOnlineResource() );
-        sp.setServiceContact( responsibleParty( geoServer, owsf ) );
-        
-        //OperationsMetadata
+        sp.getProviderSite().setHref(geoServer.getOnlineResource());
+        sp.setServiceContact(responsibleParty(geoServer, owsf));
+
+        // OperationsMetadata
         OperationsMetadataType om = owsf.createOperationsMetadataType();
-        caps.setOperationsMetadata( om );
-        
+        caps.setOperationsMetadata(om);
+
         OperationType gco = owsf.createOperationType();
         gco.setName("GetCapabilities");
-        gco.getDCP().add( Ows11Util.dcp( "wps", request ) );
-        om.getOperation().add( gco );
-        
+        gco.getDCP().add(Ows11Util.dcp("wps", request));
+        om.getOperation().add(gco);
+
         OperationType dpo = owsf.createOperationType();
-        dpo.setName( "DescribeProcess");
-        dpo.getDCP().add( Ows11Util.dcp( "wps", request ) );
-        om.getOperation().add( dpo );
-        
+        dpo.setName("DescribeProcess");
+        dpo.getDCP().add(Ows11Util.dcp("wps", request));
+        om.getOperation().add(dpo);
+
         OperationType eo = owsf.createOperationType();
-        eo.setName( "Execute" );
-        eo.getDCP().add( Ows11Util.dcp( "wps", request ) );
-        om.getOperation().add( eo );
-        
+        eo.setName("Execute");
+        eo.getDCP().add(Ows11Util.dcp("wps", request));
+        om.getOperation().add(eo);
+
         ProcessOfferingsType po = wpsf.createProcessOfferingsType();
-        caps.setProcessOfferings( po );
-        
-        for(ProcessFactory pf : Processors.getProcessFactories()) {
+        caps.setProcessOfferings(po);
+
+        for (ProcessFactory pf : Processors.getProcessFactories()) {
             for (Name name : pf.getNames()) {
-                if(canBindProcess(pf, name)) {
+                if (!getProcessBlacklist().contains(name)) {
                     ProcessBriefType p = wpsf.createProcessBriefType();
                     p.setProcessVersion(pf.getVersion(name));
-                    po.getProcess().add( p );
-                    
+                    po.getProcess().add(p);
+
                     p.setIdentifier(Ows11Util.code(name));
-                    p.setTitle(Ows11Util.languageString( pf.getTitle(name).toString()));
-                    p.setAbstract(Ows11Util.languageString( pf.getDescription(name).toString()));
+                    p.setTitle(Ows11Util.languageString(pf.getTitle(name).toString()));
+                    p.setAbstract(Ows11Util.languageString(pf.getDescription(name).toString()));
                 }
             }
-            
+
         }
 
         LanguagesType1 languages = wpsf.createLanguagesType1();
-        caps.setLanguages( languages );
-        
+        caps.setLanguages(languages);
+
         DefaultType2 defaultLanguage = wpsf.createDefaultType2();
         languages.setDefault(defaultLanguage);
         defaultLanguage.setLanguage("en-US");
-        
+
         LanguagesType supportedLanguages = wpsf.createLanguagesType();
-        languages.setSupported( supportedLanguages );
-        supportedLanguages.getLanguage().add( "en-US");
-        
+        languages.setSupported(supportedLanguages);
+        supportedLanguages.getLanguage().add("en-US");
+
         return caps;
         // Version detection and alternative invocation if being implemented.
     }
-    
+
     /**
      * Checks if our WPS can really handle this process inputs and outputs
+     * 
      * @param pf
      * @param name
      * @return
      */
-    boolean canBindProcess(ProcessFactory pf, Name name) {
-        // check inputs
-        for(Parameter<?> p : pf.getParameterInfo(name).values()) {
-            List<ProcessParameterIO> ppios = ProcessParameterIO.findAll( p, context);
-            if ( ppios.isEmpty() ) {
-                LOGGER.log(Level.INFO, "Blacklisting process " + name.getURI() + " as the input " 
-                        + p.key + " of type " + p.type + " cannot be handled");
-                return false;
+    Set<Name> getProcessBlacklist() {
+        synchronized (PROCESS_BLACKLIST) {
+            if (PROCESS_BLACKLIST == Collections.EMPTY_SET) {
+
+                Set<Name> blacklist = new HashSet<Name>();
+
+                for (ProcessFactory pf : Processors.getProcessFactories()) {
+                    for (Name name : pf.getNames()) {
+                        // check inputs
+                        for (Parameter<?> p : pf.getParameterInfo(name).values()) {
+                            List<ProcessParameterIO> ppios = ProcessParameterIO.findAll(p, context);
+                            if (ppios.isEmpty()) {
+                                LOGGER.log(Level.INFO, "Blacklisting process " + name.getURI()
+                                        + " as the input " + p.key + " of type " + p.type
+                                        + " cannot be handled");
+                                blacklist.add(name);
+                            }
+                        }
+
+                        // check outputs
+                        for (Parameter<?> p : pf.getResultInfo(name, null).values()) {
+                            List<ProcessParameterIO> ppios = ProcessParameterIO.findAll(p, context);
+                            if (ppios.isEmpty()) {
+                                LOGGER.log(Level.INFO, "Blacklisting process " + name.getURI()
+                                        + " as the output " + p.key + " of type " + p.type
+                                        + " cannot be handled");
+                                blacklist.add(name);
+                            }
+                        }
+                    }
+                }
+
+                PROCESS_BLACKLIST = blacklist;
             }
         }
-        
-        // check outputs
-        for(Parameter<?> p : pf.getResultInfo(name, null).values()) {
-            List<ProcessParameterIO> ppios = ProcessParameterIO.findAll( p, context);
-            if ( ppios.isEmpty() ) {
-                LOGGER.log(Level.INFO, "Blacklisting process " + name.getURI() + " as the output " 
-                        + p.key + " of type " + p.type + " cannot be handled");
-                return false;
-            }
-        }
-        
-        
-        return true;
+
+        return PROCESS_BLACKLIST;
     }
 
-    ResponsiblePartySubsetType responsibleParty( GeoServerInfo global, Ows11Factory f ) {
+    ResponsiblePartySubsetType responsibleParty(GeoServerInfo global, Ows11Factory f) {
         ResponsiblePartySubsetType rp = f.createResponsiblePartySubsetType();
         return rp;
     }
