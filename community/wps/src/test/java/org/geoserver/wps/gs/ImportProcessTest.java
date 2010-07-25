ac94b000f3dc5e06471c5bec78846d27a77331cd
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-07-25 08:05:34 +0000

    More work on the srs handling during import, adding tests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14624 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 2ac2573e124e2a8907bf37047a11b6173edad92b [formerly 3524995f3a609578143d6a511d1ea44aab73e717]
    Former-commit-id: 6f49619e6911639585193872cbe004877693f398

---
+++ community/wps/src/test/java/org/geoserver/wps/gs/ImportProcessTest.java
@@ -0,0 +1,82 @@
+package org.geoserver.wps.gs;
+
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.catalog.LayerInfo;
+import org.geoserver.data.test.MockData;
+import org.geoserver.test.GeoServerTestSupport;
+import org.geotools.data.Query;
+import org.geotools.data.crs.ForceCoordinateSystemFeatureResults;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.store.ReprojectingFeatureCollection;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.referencing.CRS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.FilterFactory;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+public class ImportProcessTest extends GeoServerTestSupport {
+
+	/**
+	 * Try to reimport buildings as another layer (different name, different
+	 * projection)
+	 */
+	public void testImportBuildings() throws Exception {
+		FeatureTypeInfo ti = getCatalog().getFeatureTypeByName(
+				getLayerId(MockData.BUILDINGS));
+		SimpleFeatureCollection rawSource = (SimpleFeatureCollection) ti
+				.getFeatureSource(null, null).getFeatures();
+		ForceCoordinateSystemFeatureResults forced = new ForceCoordinateSystemFeatureResults(
+				rawSource, CRS.decode("EPSG:4326"));
+		ReprojectingFeatureCollection reprojected = new ReprojectingFeatureCollection(
+				forced, CRS.decode("EPSG:900913"));
+
+		ImportProcess importer = new ImportProcess(getCatalog());
+		String result = importer.execute(reprojected, MockData.CITE_PREFIX,
+				MockData.CITE_PREFIX, "Buildings2", null, null, null);
+
+		assertEquals(MockData.CITE_PREFIX + ":" + "Buildings2", result);
+
+		// check the layer
+		LayerInfo layer = getCatalog().getLayerByName(result);
+		assertNotNull(layer);
+		assertEquals("polygon", layer.getDefaultStyle().getName());
+
+		// check the feature type info
+		FeatureTypeInfo fti = (FeatureTypeInfo) layer.getResource();
+		assertEquals("EPSG:900913", fti.getSRS());
+		SimpleFeatureSource fs = (SimpleFeatureSource) fti.getFeatureSource(
+				null, null);
+		assertEquals(2, fs.getCount(Query.ALL));
+
+		// _=the_geom:MultiPolygon,FID:String,ADDRESS:String
+		// Buildings.1107531701010=MULTIPOLYGON (((0.0008 0.0005, 0.0008 0.0007,
+		// 0.0012 0.0007, 0.0012 0.0005, 0.0008 0.0005)))|113|123 Main Street
+		// Buildings.1107531701011=MULTIPOLYGON (((0.002 0.0008, 0.002 0.001,
+		// 0.0024 0.001, 0.0024 0.0008, 0.002 0.0008)))|114|215 Main Street
+
+		FilterFactory ff = CommonFactoryFinder.getFilterFactory(null);
+
+		SimpleFeatureIterator fi = fs.getFeatures(
+				ff.equals(ff.property("FID"), ff.literal("113"))).features();
+		SimpleFeature f = fi.next();
+		fi.close();
+		assertEquals("113", f.getAttribute("FID"));
+		assertEquals("123 Main Street", f.getAttribute("ADDRESS"));
+		// very rough check for reprojection, 4326 -> 900913 inflates the
+		// numbers by a few orders of magnitude
+		assertTrue(((Geometry) f.getDefaultGeometry()).getEnvelopeInternal()
+				.getWidth() > 1);
+
+		fi = fs.getFeatures(ff.equals(ff.property("FID"), ff.literal("114")))
+				.features();
+		f = fi.next();
+		fi.close();
+		assertEquals("114", f.getAttribute("FID"));
+		assertEquals("215 Main Street", f.getAttribute("ADDRESS"));
+		assertTrue(((Geometry) f.getDefaultGeometry()).getEnvelopeInternal()
+				.getWidth() > 1);
+	}
+}
