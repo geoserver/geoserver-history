cf7c62f86845a39f7108a11e1695d63c91309e68
victortey <victortey@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-07-28 03:40:47 +0000

    GEOT-2679: Improve app-schema handling of nillable properties: add test cases to handle simple content of type integer.  Indentation is fixed
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14637 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ce2d32f7bfb358110bb9aaf0ec1bf105b82c06cf [formerly 3e4f0ec99723591bf850a19c3211170caf6fd7aa]
    Former-commit-id: 1993557c8499b9b1741bac0e57349e2342985373

--- src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/ValidationTestMockData.java
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/ValidationTestMockData.java
@@ -14,16 +14,20 @@ package org.geoserver.test;
  * @author Victor Tey, CSIRO Exploration and Mining
  */
 public class ValidationTestMockData extends AbstractAppSchemaMockData {
+    protected static final String ER_PREFIX = "er";
 
-	/**
-	 * @see org.geoserver.test.AbstractAppSchemaMockData#addContent()
-	 */
-	@Override
-	public void addContent() {
-		addFeatureType(GSML_PREFIX, "GeologicUnit", "Nillable.xml",
-				"Nillable.properties");
-		addFeatureType(GSML_PREFIX, "MappedFeature", "NillableWithError.xml",
-				"Nillable.properties");
-	}
+    protected static final String ER_URI = "urn:cgi:xmlns:GGIC:EarthResource:1.1";
+
+    /**
+     * @see org.geoserver.test.AbstractAppSchemaMockData#addContent()
+     */
+    @Override
+    public void addContent() {
+        addFeatureType(GSML_PREFIX, "GeologicUnit", "Nillable.xml", "Nillable.properties");
+        addFeatureType(GSML_PREFIX, "MappedFeature", "NillableWithError.xml", "Nillable.properties");
+        putNamespace(ER_PREFIX, ER_URI);
+        addFeatureType(ER_PREFIX, "Commodity", "nillableSimpleContentInteger.xml",
+                "Nillable.properties");
+    }
 
 }
