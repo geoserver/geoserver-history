253c81171baa94e43a4c40c6dee546eda838a4d3
victortey <victortey@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-07-28 03:40:47 +0000

    GEOT-2679: Improve app-schema handling of nillable properties: add test cases to handle simple content of type integer.  Indentation is fixed
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14637 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b369906ff4ba30d133019d430b0076df06070b32 [formerly 69f97e203415012364a567061eebe645588016a1]
    Former-commit-id: f3444a09f91f16da5357f6ccc8ff1232c2e07ccc

--- extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/ValidationTest.java
+++ extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/ValidationTest.java
@@ -17,135 +17,134 @@ import org.w3c.dom.Document;
  * 
  */
 public class ValidationTest extends AbstractAppSchemaWfsTestSupport {
-	private XpathEngine xpathEngine;
+    private XpathEngine xpathEngine;
 
-	/**
-	 * Read-only test so can use one-time setup.
-	 * 
-	 * @return
-	 */
-	public static Test suite() {
-		return new OneTimeTestSetup(new ValidationTest());
-	}
+    /**
+     * Read-only test so can use one-time setup.
+     * 
+     * @return
+     */
+    public static Test suite() {
+        return new OneTimeTestSetup(new ValidationTest());
+    }
 
-	@Override
-	protected NamespaceTestData buildTestData() {
-		return new ValidationTestMockData();
-	}
+    @Override
+    protected NamespaceTestData buildTestData() {
+        return new ValidationTestMockData();
+    }
 
-	/**
-	 * Test that when minOccur=0 the validation should let it pass
-	 */
-	public void testAttributeMinOccur0() {
-		Document doc = null;
-		doc = getAsDOM("wfs?request=GetFeature&typename=gsml:GeologicUnit");
-		LOGGER.info("WFS GetFeature&typename=gsml:GeologicUnit response:\n"
-				+ prettyString(doc));
-		assertXpathCount(
-				1,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gml:name",
-				doc);
-		assertXpathCount(
-				1,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gml:name",
-				doc);
-		assertXpathCount(
-				1,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gml:name",
-				doc);
+    /**
+     * Test that when minOccur=0 the validation should let it pass
+     */
+    public void testAttributeMinOccur0() {
+        Document doc = null;
+        doc = getAsDOM("wfs?request=GetFeature&typename=gsml:GeologicUnit");
+        LOGGER.info("WFS GetFeature&typename=gsml:GeologicUnit response:\n" + prettyString(doc));
+        assertXpathCount(1, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gml:name", doc);
+        assertXpathCount(1, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gml:name", doc);
+        assertXpathCount(1, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gml:name", doc);
 
-		assertXpathCount(
-				1,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
-				doc);
-		assertXpathEvaluatesTo(
-				"myBody1",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value[@codeSpace='myBodyCodespace1']",
-				doc);
-		assertXpathEvaluatesTo(
-				"compositionName",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
-				doc);
-		assertXpathEvaluatesTo(
-				"myBody1",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
-				doc);
-		assertXpathEvaluatesTo(
-				"myBody1",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:rank[@codeSpace='myBodyCodespace1']",
-				doc);
-		
+        assertXpathCount(
+                1,
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
+                doc);
+        assertXpathEvaluatesTo(
+                "myBody1",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value[@codeSpace='myBodyCodespace1']",
+                doc);
+        assertXpathEvaluatesTo(
+                "compositionName",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
+                doc);
+        assertXpathEvaluatesTo(
+                "myBody1",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
+                doc);
+        assertXpathEvaluatesTo(
+                "myBody1",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gsml:rank[@codeSpace='myBodyCodespace1']",
+                doc);
 
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
-				doc);
-		assertXpathEvaluatesTo(
-				"compositionName",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
-				doc);
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
-				doc);
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:rank",
-				doc);
+        assertXpathCount(
+                0,
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
+                doc);
+        assertXpathEvaluatesTo(
+                "compositionName",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
+                doc);
+        assertXpathCount(
+                0,
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
+                doc);
+        assertXpathCount(0, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gsml:rank", doc);
 
-		assertXpathCount(
-				1,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
-				doc);
-		assertXpathEvaluatesTo(
-				"myBody3",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value[@codeSpace='myBodyCodespace3']",
-				doc);
-		assertXpathEvaluatesTo(
-				"compositionName",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
-				doc);
-		assertXpathEvaluatesTo(
-				"myBody3",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
-				doc);
-		
-		assertXpathEvaluatesTo(
-				"myBody3",
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:rank[@codeSpace='myBodyCodespace3']",
-				doc);
-	}
+        assertXpathCount(
+                1,
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value",
+                doc);
+        assertXpathEvaluatesTo(
+                "myBody3",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:bodyMorphology/gsml:CGI_TermValue/gsml:value[@codeSpace='myBodyCodespace3']",
+                doc);
+        assertXpathEvaluatesTo(
+                "compositionName",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:composition/gsml:CompositionPart/gsml:lithology[1]/gsml:ControlledConcept/gml:name",
+                doc);
+        assertXpathEvaluatesTo(
+                "myBody3",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:composition/gsml:CompositionPart/gsml:lithology[2]/gsml:ControlledConcept/gml:name",
+                doc);
 
-	public void testAttributeMinOccur1() {
-		Document doc = null;
-		try {
-			doc = getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
-			LOGGER
-					.info("WFS GetFeature&typename=gsml:gsml:MappedFeature response:\n"
-							+ prettyString(doc));
-		} catch (Exception e) {
-			LOGGER
-					.info("WFS GetFeature&typename=gsml:MappedFeature response, exception expected:\n"
-							+ prettyString(doc));
-			assertEquals("ows:ExceptionReport", doc.getDocumentElement()
-					.getNodeName());
-		}
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gml:name",
-				doc);
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gml:name",
-				doc);
-		assertXpathCount(
-				0,
-				"//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gml:name",
-				doc);
-		String exceptionRpt = evaluate("//ows:ExceptionText", doc);
-		assertTrue(exceptionRpt
-				.contains("observationMethod requires a non null value"));
+        assertXpathEvaluatesTo(
+                "myBody3",
+                "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gsml:rank[@codeSpace='myBodyCodespace3']",
+                doc);
+    }
 
-	}
+    public void testSimpleContentInteger() {
+        Document doc = null;
+        doc = getAsDOM("wfs?request=GetFeature&typename=er:Commodity");
+        LOGGER.info("WFS GetFeature&typename=er:Commodity response:\n" + prettyString(doc));
+        assertXpathCount(1, "//er:Commodity[@gml:id='er.commodity.gu.1']/gml:name", doc);
+        assertXpathCount(1, "//er:Commodity[@gml:id='er.commodity.gu.1']/er:commodityRank", doc);
+        assertXpathEvaluatesTo("myName1", "//er:Commodity[@gml:id='er.commodity.gu.1']/gml:name",
+                doc);
+        assertXpathEvaluatesTo("1", "//er:Commodity[@gml:id='er.commodity.gu.1']/er:commodityRank",
+                doc);
+
+        assertXpathCount(1, "//er:Commodity[@gml:id='er.commodity.gu.2']/gml:name", doc);
+        assertXpathCount(0, "//er:Commodity[@gml:id='er.commodity.gu.2']/er:commodityRank", doc);
+        assertXpathEvaluatesTo("myName2", "//er:Commodity[@gml:id='er.commodity.gu.2']/gml:name",
+                doc);
+
+        assertXpathCount(1, "//er:Commodity[@gml:id='er.commodity.gu.3']/gml:name", doc);
+        assertXpathCount(1, "//er:Commodity[@gml:id='er.commodity.gu.3']/er:commodityRank", doc);
+        assertXpathEvaluatesTo("myName3", "//er:Commodity[@gml:id='er.commodity.gu.3']/gml:name",
+                doc);
+        assertXpathEvaluatesTo("3", "//er:Commodity[@gml:id='er.commodity.gu.3']/er:commodityRank",
+                doc);
+
+    }
+
+    public void testAttributeMinOccur1() {
+        Document doc = null;
+        try {
+            doc = getAsDOM("wfs?request=GetFeature&typename=gsml:MappedFeature");
+            LOGGER.info("WFS GetFeature&typename=gsml:gsml:MappedFeature response:\n"
+                    + prettyString(doc));
+        } catch (Exception e) {
+            LOGGER
+                    .info("WFS GetFeature&typename=gsml:MappedFeature response, exception expected:\n"
+                            + prettyString(doc));
+            assertEquals("ows:ExceptionReport", doc.getDocumentElement().getNodeName());
+        }
+        assertXpathCount(0, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.1']/gml:name", doc);
+        assertXpathCount(0, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.2']/gml:name", doc);
+        assertXpathCount(0, "//gsml:GeologicUnit[@gml:id='gsml.geologicunit.gu.3']/gml:name", doc);
+        String exceptionRpt = evaluate("//ows:ExceptionText", doc);
+        assertTrue(exceptionRpt.contains("observationMethod requires a non null value"));
+
+    }
 
 }
