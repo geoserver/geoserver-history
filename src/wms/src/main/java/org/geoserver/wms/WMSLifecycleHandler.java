a6380543bcb732479527d8542fbdb6393df18afc
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-08-03 12:55:30 +0000

    [GEOS-3958] Automatically register all fonts that are dropped in the styles directory
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14643 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a6e253085da498fe8cf0ba09c8f3d70f7767edbd [formerly ec2d3adaff4e9d8f0242853f260d569ba07fe674]
    Former-commit-id: e357c04585abfff8054e741e42453e91ae4e9346

--- src/wms/src/main/java/org/geoserver/wms/WMSLifecycleHandler.java
+++ src/wms/src/main/java/org/geoserver/wms/WMSLifecycleHandler.java
@@ -4,16 +4,40 @@
  */
 package org.geoserver.wms;
 
+import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+import org.geoserver.config.GeoServerDataDirectory;
 import org.geoserver.config.impl.GeoServerLifecycleHandler;
 import org.geotools.renderer.style.FontCache;
 import org.geotools.renderer.style.ImageGraphicFactory;
 import org.geotools.renderer.style.SVGGraphicFactory;
+import org.geotools.util.logging.Logging;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * Drops imaging caches
- * @author aaime
+ * 
+ * @author Andrea Aime - OpenGeo
  */
-public class WMSLifecycleHandler implements GeoServerLifecycleHandler {
+public class WMSLifecycleHandler implements GeoServerLifecycleHandler, ApplicationListener {
+
+    static final Logger LOGGER = Logging.getLogger(WMSLifecycleHandler.class);
+
+    GeoServerDataDirectory data;
+
+    public WMSLifecycleHandler(GeoServerDataDirectory data) {
+        this.data = data;
+    }
 
     public void onDispose() {
         // nothing to do
@@ -25,10 +49,49 @@ public class WMSLifecycleHandler implements GeoServerLifecycleHandler {
     }
 
     public void onReset() {
-        // kill the image and font caches
+        // kill the image caches
         ImageGraphicFactory.resetCache();
         SVGGraphicFactory.resetCache();
-        FontCache.getDefaultInstance().resetCache();
+
+        // reloads the font cache
+        reloadFontCache();
+    }
+
+    void reloadFontCache() {
+        List<Font> fonts = loadFontsFromDataDirectory();
+        final FontCache cache = FontCache.getDefaultInstance();
+        cache.resetCache();
+        for (Font font : fonts) {
+            cache.registerFont(font);
+        }
+    }
+
+    List<Font> loadFontsFromDataDirectory() {
+        List<Font> result = new ArrayList<Font>();
+        try {
+            Collection<File> files = FileUtils.listFiles(data.findStyleDir(), new String[] { "ttf",
+                    "TTF" }, true);
+            for (File file : files) {
+                try {
+                    final Font font = Font.createFont(Font.TRUETYPE_FONT, file);
+                    result.add(font);
+                    LOGGER.log(Level.INFO, "Loaded font file " + file + ", loaded font '"
+                            + font.getName() + "' in family '" + font.getFamily() + "'");
+                } catch (Exception e) {
+                    LOGGER.log(Level.WARNING, "Failed to load font file " + file, e);
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Failed to scan style directory for fonts", e);
+        }
+
+        return result;
+    }
+
+    public void onApplicationEvent(ApplicationEvent event) {
+        if(event instanceof ContextRefreshedEvent) {
+            reloadFontCache();
+        }
     }
 
 }
