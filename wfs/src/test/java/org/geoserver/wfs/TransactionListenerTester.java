d42547cdd0d156181657acbab7473b2fd1c15a66
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-08-20 23:59:30 +0000

    Revert "update TransactionListenerTest to account for the new expected post-transaction events"
    This reverts commit 20bb4c859dfa42b7002d46b149e6a010d5b7024a.
    
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14709 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bbe388df4430fb3bcde925a34a67c1c17fd69263 [formerly 4b6756d0a46f516fae97802829207b2a84911693]
    Former-commit-id: 15e1804a5c2664c234a69cfad14df29ff328b40b

--- wfs/src/test/java/org/geoserver/wfs/TransactionListenerTester.java
+++ wfs/src/test/java/org/geoserver/wfs/TransactionListenerTester.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.geotools.data.DataUtilities;
-import org.opengis.feature.simple.SimpleFeature;
 
 public class TransactionListenerTester implements TransactionListener {
     List events = new ArrayList();
-    List<List<SimpleFeature>> features = new ArrayList();
+    List features = new ArrayList();
     
     public void clear() {
         events.clear();
@@ -16,7 +15,7 @@ public class TransactionListenerTester implements TransactionListener {
 
     public void dataStoreChange(TransactionEvent event) throws WFSException {
         events.add(event);
-        features.add(DataUtilities.list(event.getAffectedFeatures()));
+        features.addAll(DataUtilities.list(event.getAffectedFeatures()));
     }
     
     
