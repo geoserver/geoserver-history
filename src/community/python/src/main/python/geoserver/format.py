83982ab2c6fa34966097a3a5e69d5767b4b16911
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-01 21:41:15 +0000

    some tweaks to vector format api
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14775 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 48f80a0db219afb6ec4d78079bb30fa9cc6f82f8 [formerly d6b264ce70a2305f35cfcd2d8ffd361af9ab614e]
    Former-commit-id: 9978a6d73d0666a9a7bf3b7a7a2d27aa87a19c09

--- src/community/python/src/main/python/geoserver/format.py
+++ src/community/python/src/main/python/geoserver/format.py
@@ -6,7 +6,14 @@ def vector_format(name, mime):
   """
   def wrap(func):
      def wrapper(features, output):
-        return func([_FeatureCollection(f) for f in features], output)
+        def f(fc1, fc2):
+          fc2.next = fc1 
+          return fc2
+        
+        list = [_FeatureCollection(f) for f in features]
+        list.reverse()
+        data = reduce(f, list)
+        return func(data, output)
      
      wrapper.__vector_format__ = None
      wrapper.name = name
@@ -14,14 +21,29 @@ def vector_format(name, mime):
      return wrapper
      
   return wrap
-  
 
 class _FeatureCollection(object):
   
-  def __init__(self, features):
+  def __init__(self, features, next=None):
       self.schema = Schema(ft=features.getSchema())
       self.features = _FeatureIterator(features)
+      self.next = next
       
+  class Iterator(object):
+     def __init__(self, item): 
+       self.item = item
+
+     def next(self):
+       if not self.item: 
+          raise StopIteration
+
+       it = self.item
+       self.item = self.item.next
+       return it
+
+  def __iter__(self):
+     return _FeatureCollection.Iterator(self)
+
 class _FeatureIterator(object):
 
   def __init__(self, features):
