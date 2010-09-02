25944aeb7ff5771b2eb5845ba131d32ac19d22cd
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-02 22:44:20 +0000

    updated doc versions from 2.0 to 2.1
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14782 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: efd778ef6fdbe3dcb0f6094beb4271c415ff4571 [formerly 29cbc9ed4bf8ba5947683746a8fa615337618bad]
    Former-commit-id: fa8fbfb4c1e0f846d31654d9c27b6a67f0617c63

--- doc/en/user/source/conf.py
+++ doc/en/user/source/conf.py
@@ -45,9 +45,9 @@ copyright = u'2009 GeoServer'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.0-SNAPSHOT'
+release = '2.1-SNAPSHOT'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
