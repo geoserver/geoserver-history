899e474bba1e117effa0ff0a165a287deab904e2
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-22 02:18:32 +0000

    GEOS-2658: fix svg tests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14864 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e63266ed73a458efed16066d4ae33510984c4008 [formerly acb123246de9f754d4d9f2802fbeb8084565bfad]
    Former-commit-id: 3f1b74f012367822695f8f9876d82186b92742ef

--- wms/src/test/java/org/geoserver/wms/svg/SVGMapProducerTest.java
+++ wms/src/test/java/org/geoserver/wms/svg/SVGMapProducerTest.java
@@ -5,10 +5,18 @@
 package org.geoserver.wms.svg;
 
 import java.awt.Color;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.Test;
 
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.geoserver.wms.WMSMapContext;
 import org.geoserver.wms.WMSTestSupport;
 import org.geotools.data.FeatureSource;
@@ -19,6 +27,8 @@ import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.styling.Style;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
@@ -71,12 +81,38 @@ public class SVGMapProducerTest extends WMSTestSupport {
         Style basicStyle = getCatalog().getStyleByName("Default").getStyle();
         map.addLayer(fs, basicStyle);
 
-        SVGStreamingMapOutputFormat producer = new SVGStreamingMapOutputFormat(
-                SVGMapOutputFormatProxy.MIME_TYPE, SVGMapOutputFormatProxy.OUTPUT_FORMATS);
-        producer.setMapContext(map);
-        producer.produceMap();
-
+        SVGStreamingMapOutputFormat producer = new SVGStreamingMapOutputFormat(getWMS());
+        EncodeSVG encodeSVG = producer.produceMap(map);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        producer.writeTo(out);
+        encodeSVG.encode(out);
+        // System.out.println(out.toString());
+
+        String expectedDoc = "<?xml version=\"1.0\" standalone=\"no\"?>"
+                + "<svg xmlns=\"http://www.w3.org/2000/svg\" " //
+                + "    xmlns:xlink=\"http://www.w3.org/1999/xlink\" " //
+                + "    stroke=\"green\"  "//
+                + "    fill=\"none\"  " //
+                + "    stroke-width=\"0.1%\" " //
+                + "    stroke-linecap=\"round\" "//
+                + "    stroke-linejoin=\"round\" "//
+                + "    width=\"300\"  "//
+                + "    height=\"300\"  " //
+                + "    viewBox=\"-250.0 -250.0 500.0 500.0\"  "//
+                + "    preserveAspectRatio=\"xMidYMid meet\"> "//
+                + "  <g id=\"test\" class=\"Default\"> "//
+                + "    <use x=\"10\" y=\"-10\" xlink:href=\"#point\"/> "//
+                + "    <path d=\"M50 -50l50 -50 \"/> "//
+                + "    <path d=\"M0 0l0 -200 200 0 0 200 -200 0 Z\"/> "//
+                + "  </g> "//
+                + "</svg> ";
+
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document expected = builder.parse(new InputSource(new StringReader(expectedDoc)));
+        Document result = builder.parse(new ByteArrayInputStream(out.toByteArray()));
+
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
+        XMLUnit.setIgnoreComments(true);
+        XMLAssert.assertXMLEqual(expected, result);
     }
 }
