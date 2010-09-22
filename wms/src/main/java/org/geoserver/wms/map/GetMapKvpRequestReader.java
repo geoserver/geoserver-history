185cbac8aac4b4cfc9de4c5e933bc696b56b8ad6
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-22 02:29:33 +0000

    GEOS-2658: functionally group getmap related classes together
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14893 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: aae23e4887234439332c748a3eda224883336f5a [formerly f31e12659dca7911f35a824220bb75e35cf72376]
    Former-commit-id: a29b9f0e4e51c9df518814b768a415a90277c845

--- wms/src/main/java/org/geoserver/wms/map/GetMapKvpRequestReader.java
+++ wms/src/main/java/org/geoserver/wms/map/GetMapKvpRequestReader.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-package org.geoserver.wms.kvp;
+package org.geoserver.wms.map;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
