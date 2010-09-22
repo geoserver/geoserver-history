6c53447ce920c92c229e1bac2609e389bafbb9fd
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-22 02:30:47 +0000

    GEOS-2658: functionally gropu getlegendgraphic related classes together
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@14896 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: e5d24e04f69eabd0d3fa1514162515f055471cc8 [formerly 0ddee8e8b9b9a0563a1a4e2908431c4efa95535a]
    Former-commit-id: f6a2bbbcd92bc93ba1e1a7678b82215e18ff1be8

--- src/wms/src/test/java/org/geoserver/wms/legendgraphic/GetLegendGraphicKvpReaderTest.java
+++ src/wms/src/test/java/org/geoserver/wms/legendgraphic/GetLegendGraphicKvpReaderTest.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-package org.geoserver.wms.kvp;
+package org.geoserver.wms.legendgraphic;
 
 import java.net.URL;
 import java.util.HashMap;
