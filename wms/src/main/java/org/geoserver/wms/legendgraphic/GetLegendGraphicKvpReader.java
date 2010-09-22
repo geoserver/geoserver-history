b0698f8acce3019825570b61bbafb2af4b9e62f2
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-22 02:30:47 +0000

    GEOS-2658: functionally gropu getlegendgraphic related classes together
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14896 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 46b70b2f669fb747c19c8767faf91393b94cb7fd [formerly ce963236352335a647183c0399539db1c72f368e]
    Former-commit-id: 3fad16aa9f799bb4dfed50b329ef0453f60d2d4f

--- wms/src/main/java/org/geoserver/wms/legendgraphic/GetLegendGraphicKvpReader.java
+++ wms/src/main/java/org/geoserver/wms/legendgraphic/GetLegendGraphicKvpReader.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-package org.geoserver.wms.kvp;
+package org.geoserver.wms.legendgraphic;
 
 import java.io.IOException;
 import java.io.InputStream;
--- wms/src/main/java/org/geoserver/wms/legendgraphic/GetLegendGraphicResponse.java
+++ wms/src/main/java/org/geoserver/wms/legendgraphic/GetLegendGraphicResponse.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, availible at the root
  * application directory.
  */
-package org.geoserver.wms.response;
+package org.geoserver.wms.legendgraphic;
 
 import java.io.IOException;
 import java.io.OutputStream;
