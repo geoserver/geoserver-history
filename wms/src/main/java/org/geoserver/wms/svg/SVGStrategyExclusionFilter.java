91900c92a3f42143a2d87e76860c8da87169feea
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-09-22 02:39:02 +0000

    GEOS-2658, make the SVG exclusion filter lazily access the WMS facade bean to avoid unit tests to fail due to app context reinitialization
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@14919 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3d0397c5abeacc76cadd063c2472cde902de57c0 [formerly 377948b39d38425138bdbf8b945aa556ff9fa10f]
    Former-commit-id: 08805ca3ceea1cea4481379a6e35164e89d24923

--- wms/src/main/java/org/geoserver/wms/svg/SVGStrategyExclusionFilter.java
+++ wms/src/main/java/org/geoserver/wms/svg/SVGStrategyExclusionFilter.java
@@ -7,45 +7,76 @@ package org.geoserver.wms.svg;
 import org.geoserver.platform.ExtensionFilter;
 import org.geoserver.platform.GeoServerExtensions;
 import org.geoserver.wms.WMS;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * An extension filter that tells {@link GeoServerExtensions#extensions(Class)} (where
  * {@code Class == GetMapOutputFormat.class}) whether the {@link SVGStreamingMapOutputFormat} or the
  * {@link SVGBatikMapOutputFormat} is to be excluded based on the {@link WMS#getSvgRenderer()}
  * config option.
+ * <p>
+ * Implementation note: it would be better if this bean received a reference to the {@link WMS}
+ * facade instead of looking it up through {@link GeoServerExtensions} but if so, unit tests break
+ * while setting up the mock application context when {@code SecureCatalogImpl}'s constructor
+ * performs an extension lookup with the following error: <i> Error creating bean with name 'wms':
+ * Requested bean is currently in creation: Is there an unresolvable circular reference?</i>
+ * </p>
  * 
  * @author Gabriel Roldan
  * 
  */
-public class SVGStrategyExclusionFilter implements ExtensionFilter {
+public class SVGStrategyExclusionFilter implements ExtensionFilter, ApplicationContextAware {
+
+    private String wmsBeanName;
 
     private WMS wms;
 
-    public SVGStrategyExclusionFilter() {
-        //
-    }
+    private ApplicationContext ctx;
 
     /**
-     * Setter for the WMS facade, needs to be a property instead of a constructor argument,
-     * otherwise it's not yet resolved by spring by the time this ExtensionFilter is created.
-     * 
-     * @param wms
+     * @param wmsBeanName
+     *            name of the bean of type {@link WMS} to be lazily looked up in the application
+     *            context
      */
-    public void setWMS(WMS wms) {
-        this.wms = wms;
+    public SVGStrategyExclusionFilter(final String wmsBeanName) {
+        this.wmsBeanName = wmsBeanName;
     }
 
     /**
+     * @param ctx
+     *            context where to look for the {@link WMS} bean named as specified to this class'
+     *            constructor
+     * @see ApplicationContextAware#setApplicationContext(ApplicationContext)
+     */
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.ctx = ctx;
+    }
+
+    /**
+     * @return {@code true} if {@code bean} is one of the SVG rendering strategies but not the one
+     *         that shall be used as per {@link WMS#getSvgRenderer()}
      * @see ExtensionFilter#exclude(String, Object)
      */
     public boolean exclude(String beanId, Object bean) {
         boolean exclude = false;
+        /*
+         * Lazy lookup of the WMS bean is performed here because this method is often called while
+         * the application context is still being built
+         */
         if (bean instanceof SVGStreamingMapOutputFormat) {
-            exclude = !SVG.canHandle(wms, WMS.SVG_SIMPLE);
+            exclude = !SVG.canHandle(getWMS(), WMS.SVG_SIMPLE);
         } else if (bean instanceof SVGBatikMapOutputFormat) {
-            exclude = !SVG.canHandle(wms, WMS.SVG_BATIK);
+            exclude = !SVG.canHandle(getWMS(), WMS.SVG_BATIK);
         }
         return exclude;
     }
 
+    private WMS getWMS() {
+        if (wms == null) {
+            wms = (WMS) ctx.getBean(wmsBeanName);
+        }
+        return wms;
+    }
 }
