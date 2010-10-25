07fa698191a91d5f13acf128911925595246018e
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-10-25 10:36:44 +0000

    [GEOS-4195] ogr2ogr output format should always use the GML intermediate representation
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.0.x@15016 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: ac80cad401bdc0627e8bb344d26a2c8e0b87fc00 [formerly a237334c7636e301aa78ff42080b76df5e45b5bd]
    Former-commit-id: b0832a8a9d1cb7a1aef8ec2e3a5a7cd8a23a8b3b

--- src/extension/ogr/src/main/java/org/geoserver/wfs/response/Ogr2OgrOutputFormat.java
+++ src/extension/ogr/src/main/java/org/geoserver/wfs/response/Ogr2OgrOutputFormat.java
@@ -8,12 +8,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
 import java.util.zip.ZipOutputStream;
 
 import javax.xml.namespace.QName;
@@ -37,35 +34,9 @@ import org.geotools.gml2.bindings.GML2EncodingUtils;
 import org.opengis.feature.GeometryAttribute;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.feature.type.AttributeDescriptor;
-import org.opengis.feature.type.GeometryDescriptor;
-import org.opengis.feature.type.GeometryType;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.MultiLineString;
-import com.vividsolutions.jts.geom.MultiPoint;
-import com.vividsolutions.jts.geom.MultiPolygon;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
-
 public class Ogr2OgrOutputFormat extends WFSGetFeatureOutputFormat {
-    
-    /**
-     * The types of geometries a shapefile can handle
-     */
-    private static final Set<Class> SHAPEFILE_GEOM_TYPES = new HashSet<Class>() {
-        {
-            add(Point.class);
-            add(LineString.class);
-            add(LinearRing.class);
-            add(Polygon.class);
-            add(MultiPoint.class);
-            add(MultiLineString.class);
-            add(MultiPolygon.class);
-        }
-    };
     
     /**
      * The fs path to ogr2ogr. If null, we'll assume ogr2ogr is in the PATH and
@@ -218,6 +189,7 @@ public class Ogr2OgrOutputFormat extends WFSGetFeatureOutputFormat {
             // delete the input and output directories
             IOUtils.delete(tempGS);
             IOUtils.delete(tempOGR);
+            zipOut.flush();
         } catch (Exception e) {
             throw new ServiceException("Exception occurred during output generation", e);
         }
@@ -230,61 +202,6 @@ public class Ogr2OgrOutputFormat extends WFSGetFeatureOutputFormat {
      * @return
      */
     private File writeToDisk(File tempDir,
-            FeatureCollection<SimpleFeatureType, SimpleFeature> curCollection) throws Exception {
-        if(isShapefileCompatible(curCollection.getSchema())) 
-            return writeShapefile(tempDir, curCollection);
-        else
-            return writeGML(tempDir, curCollection);
-    }
-
-    private File writeShapefile(File tempDir,
-            FeatureCollection<SimpleFeatureType, SimpleFeature> collection) {
-        SimpleFeatureType schema = collection.getSchema();
-
-        FeatureStore<SimpleFeatureType, SimpleFeature> fstore = null;
-        DataStore dstore = null;
-        File file = null;
-        try {
-            file = new File(tempDir, schema.getTypeName() + ".shp");
-            dstore = new ShapefileDataStore(file.toURL());
-            dstore.createSchema(schema);
-            
-            fstore = (FeatureStore<SimpleFeatureType, SimpleFeature>) dstore.getFeatureSource(schema.getTypeName());
-            fstore.addFeatures(collection);
-        } catch (IOException ioe) {
-            LOGGER.log(Level.WARNING,
-                "Error while writing featuretype '" + schema.getTypeName() + "' to shapefile.", ioe);
-            throw new ServiceException(ioe);
-        } finally {
-            if(dstore != null) {
-                dstore.dispose();
-            }
-        }
-        
-        return file; 
-    }
-
-    /**
-     * Returns true if the schema has just one geometry and the geom type is known
-     * @param schema
-     * @return
-     */
-    private boolean isShapefileCompatible(SimpleFeatureType schema) {
-        GeometryType gt = null;
-        for (AttributeDescriptor at : schema.getAttributeDescriptors()) {
-            if(at instanceof GeometryDescriptor) {
-                if(gt == null)
-                    gt = ((GeometryDescriptor) at).getType();
-                else
-                    // more than one geometry 
-                    return false;
-            }
-        } 
-        
-        return gt != null && SHAPEFILE_GEOM_TYPES.contains(gt.getBinding()); 
-    }
-
-    private File writeGML(File tempDir,
             FeatureCollection<SimpleFeatureType, SimpleFeature> curCollection) throws Exception {
         // create the temp file for this output
         File outFile = new File(tempDir, curCollection.getSchema().getTypeName() + ".gml");
