8f762fac712a7f09ff03971ae866a428eb047738
mbarto <mbarto@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-11-08 16:38:24 +0000

    imagemap extension: fixed a problem with reading a PointSymbolizer Size
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15060 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: bb8c16ca881af700a0af57f2207f442bd1ef66b7 [formerly 3a2e76b5f80ceadc69f8d39801008378334b10ea]
    Former-commit-id: fddcb980aa996d24d6726e15eebe3627d13a6ec2

--- src/extension/imagemap/src/test/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/HTMLImageMapTest.java
+++ src/extension/imagemap/src/test/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/HTMLImageMapTest.java
@@ -345,6 +345,29 @@ public class HTMLImageMapTest extends TestCase {
         assertTestResult("BuildingCenters", result);
 
     }
+	
+	public void testMapProducePointsWithSize() throws Exception {
+
+        final FeatureSource<SimpleFeatureType, SimpleFeature> fs = testDS
+                .getFeatureSource("BuildingCenters");
+        final ReferencedEnvelope env = new ReferencedEnvelope(fs.getBounds(), WGS84);
+
+        LOGGER.info("about to create map ctx for BuildingCenters with bounds " + env);
+
+        final WMSMapContext map = new WMSMapContext();
+        map.setAreaOfInterest(env);
+        map.setMapWidth(mapWidth);
+        map.setMapHeight(mapHeight);
+
+        map.setTransparent(false);
+
+        Style basicStyle = getTestStyle("BuildingCenters2.sld");
+        map.addLayer(fs, basicStyle);
+
+        EncodeHTMLImageMap result = mapProducer.produceMap(map);
+        assertTestResult("BuildingCenters2", result);
+
+    }
 
     public void testMapProduceMultiPoints() throws Exception {
 
