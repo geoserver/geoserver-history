67bc46173007d0828899cfa8fda15a30ecb0e2bf
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-11-15 15:11:09 +0000

    Have the gui module follow the usual naming conventions (module and package wise)
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@15071 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 138459256da3a4bd327787dab5c1295cadd13531 [formerly 9c8ca8429279ea51bdaa3df12dfff8b6f83044dd]
    Former-commit-id: 85312d9eedbb4ec41a1f5159480b13406dbeddcc

---
+++ community/wps/web-wps/src/main/java/org/geoserver/wps/web/InputParameterValues.java
@@ -0,0 +1,138 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wps.web;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.geoserver.wps.ppio.BoundingBoxPPIO;
+import org.geoserver.wps.ppio.ComplexPPIO;
+import org.geoserver.wps.ppio.ProcessParameterIO;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.Parameter;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.Processors;
+import org.opengis.feature.type.Name;
+
+/**
+ * Contains the set of values for a single parameter. For most input parameters
+ * it will be just one value actually
+ * 
+ * @author Andrea Aime - OpenGeo
+ */
+@SuppressWarnings("serial")
+class InputParameterValues implements Serializable {
+	public enum ParameterType {
+		LITERAL, TEXT, VECTOR_LAYER, RASTER_LAYER, REFERENCE, SUBPROCESS;
+	};
+
+	Name processName;
+	String paramName;
+	List<ParameterValue> values = new ArrayList<ParameterValue>();
+
+	public InputParameterValues(Name processName, String paramName) {
+		this.processName = processName;
+		this.paramName = paramName;
+		Parameter<?> p = getParameter();
+		for (int i = 0; i < Math.max(1, p.minOccurs); i++) {
+			values.add(new ParameterValue(guessBestType(), getDefaultMime(),
+					null));
+		}
+	}
+
+	private ParameterType guessBestType() {
+		if (!isComplex())
+			return ParameterType.LITERAL;
+		if (FeatureCollection.class.isAssignableFrom(getParameter().type)) {
+			return ParameterType.VECTOR_LAYER;
+		} else if (GridCoverage2D.class.isAssignableFrom(getParameter().type)) {
+			return ParameterType.RASTER_LAYER;
+		} else {
+			return ParameterType.TEXT;
+		}
+	}
+
+	String getDefaultMime() {
+		if (!isComplex()) {
+			return null;
+		} else {
+			return ((ComplexPPIO) getProcessParameterIO().get(0)).getMimeType();
+		}
+	}
+
+	public List<String> getSupportedMime() {
+		List<String> results = new ArrayList<String>();
+		for (ProcessParameterIO ppio : getProcessParameterIO()) {
+			ComplexPPIO cp = (ComplexPPIO) ppio;
+			results.add(cp.getMimeType());
+		}
+		return results;
+	}
+
+	public boolean isComplex() {
+		List<ProcessParameterIO> ppios = getProcessParameterIO();
+		return ppios.size() > 0 && ppios.get(0) instanceof ComplexPPIO;
+	}
+	
+	public boolean isBoundingBox() {
+	    List<ProcessParameterIO> ppios = getProcessParameterIO();
+        return ppios.size() > 0 && ppios.get(0) instanceof BoundingBoxPPIO; 
+	}
+
+	List<ProcessParameterIO> getProcessParameterIO() {
+		return ProcessParameterIO.findAll(getParameter(), null);
+	}
+
+	ProcessFactory getProcessFactory() {
+		return Processors.createProcessFactory(processName);
+	}
+
+	Parameter<?> getParameter() {
+		return getProcessFactory().getParameterInfo(processName).get(paramName);
+	}
+
+	/**
+	 * A single value, along with the chosen editor and its mime type
+	 */
+	static class ParameterValue implements Serializable {
+		ParameterType type;
+		String mime;
+		Serializable value;
+
+		public ParameterValue(ParameterType type, String mime,
+				Serializable value) {
+			this.type = type;
+			this.mime = mime;
+			this.value = value;
+		}
+
+		public ParameterType getType() {
+			return type;
+		}
+
+		public void setType(ParameterType type) {
+			this.type = type;
+		}
+
+		public String getMime() {
+			return mime;
+		}
+
+		public void setMime(String mime) {
+			this.mime = mime;
+		}
+
+		public Serializable getValue() {
+			return value;
+		}
+
+		public void setValue(Serializable value) {
+			this.value = value;
+		}
+
+	}
+}
