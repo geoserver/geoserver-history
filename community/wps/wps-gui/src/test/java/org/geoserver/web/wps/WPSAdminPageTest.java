a5e525634b2ec086e8383c96f7b5a7c713ea6be9
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-11-15 15:09:45 +0000

    Adding admin page for WPS
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@15069 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 18ef08cd26ea9adda309c81c500c9df53c85b272 [formerly b01275a5b024f7ea8d970721b002d19702d1645a]
    Former-commit-id: eed56b1ba5ad5b1bc30277165248749550010e84

---
+++ community/wps/wps-gui/src/test/java/org/geoserver/web/wps/WPSAdminPageTest.java
@@ -0,0 +1,20 @@
+package org.geoserver.web.wps;
+
+import org.geoserver.web.GeoServerWicketTestSupport;
+import org.geoserver.web.wicket.KeywordsEditor;
+import org.geoserver.wps.WPSInfo;
+
+public class WPSAdminPageTest extends GeoServerWicketTestSupport {
+
+    public void test() throws Exception {
+        login();
+        WPSInfo wps = getGeoServerApplication().getGeoServer().getService(WPSInfo.class);
+        
+        // start the page
+        tester.startPage(new WPSAdminPage());
+        
+        // test that components have been filled as expected
+        tester.assertComponent("form:keywords", KeywordsEditor.class);
+        tester.assertModelValue("form:keywords", wps.getKeywords());
+    }
+}
