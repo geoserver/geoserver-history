0d9e377a74b4566425a4140dba3cde738a7a0cfd
aaime <aaime@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-12-07 10:14:00 +0000

    GeoTIFF PPIO improvements
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@15141 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 4c768cdc9aa1386d949d9cd2372f7ca4a738e8c6 [formerly b7616a55acbd6b2df6cac3c81f45a613d65f0e52]
    Former-commit-id: 64ac9a9059c7679ee6015d348c74c826075a9900

--- extension/wps/wps-core/src/main/java/org/geoserver/wps/ppio/GeoTiffPPIO.java
+++ extension/wps/wps-core/src/main/java/org/geoserver/wps/ppio/GeoTiffPPIO.java
@@ -4,16 +4,26 @@
  */
 package org.geoserver.wps.ppio;
 
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.media.jai.JAI;
+
 import org.apache.commons.io.IOUtils;
+import org.geoserver.wps.WPSException;
 import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.io.AbstractGridCoverageWriter;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridFormatFinder;
+import org.geotools.coverage.grid.io.UnknownFormat;
+import org.geotools.coverage.grid.io.imageio.GeoToolsWriteParams;
 import org.geotools.gce.geotiff.GeoTiffFormat;
+import org.geotools.gce.geotiff.GeoTiffWriteParams;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterValueGroup;
 
 /**
  * Decodes/encodes a GeoTIFF file
@@ -22,6 +32,16 @@ import org.geotools.gce.geotiff.GeoTiffFormat;
  * 
  */
 public class GeoTiffPPIO extends BinaryPPIO {
+
+    private final static GeoTiffWriteParams DEFAULT_WRITE_PARAMS;
+
+    static {
+        // setting the write parameters (write out using tiling)
+        DEFAULT_WRITE_PARAMS = new GeoTiffWriteParams();
+        DEFAULT_WRITE_PARAMS.setTilingMode(GeoToolsWriteParams.MODE_EXPLICIT);
+        final Dimension defaultTileSize = JAI.getDefaultTileSize();
+        DEFAULT_WRITE_PARAMS.setTiling(defaultTileSize.width, defaultTileSize.height);
+    }
 
     protected GeoTiffPPIO() {
         super(GridCoverage2D.class, GridCoverage2D.class, "image/tiff");
@@ -37,13 +57,15 @@ public class GeoTiffPPIO extends BinaryPPIO {
             os = new FileOutputStream(f);
             IOUtils.copy(input, os);
         } finally {
-            if (os != null) {
-                os.close();
-            }
+            IOUtils.closeQuietly(os);
         }
 
         // and then we try to read it as a geotiff
         AbstractGridFormat format = GridFormatFinder.findFormat(f);
+        if (format instanceof UnknownFormat) {
+            throw new WPSException(
+                    "Could not find the GeoTIFF GT2 format, please check it's in the classpath");
+        }
         return format.getReader(f).read(null);
     }
 
@@ -51,12 +73,26 @@ public class GeoTiffPPIO extends BinaryPPIO {
     public void encode(Object value, OutputStream os) throws Exception {
         GridCoverage2D coverage = (GridCoverage2D) value;
 
-        // write the coverage to a temp file
-        File root = new File(System.getProperty("java.io.tmpdir", "."));
-        File f = File.createTempFile("wps", "tiff", root);
-
         GeoTiffFormat format = new GeoTiffFormat();
-        format.getWriter(os).write(coverage, null);
+        final ParameterValueGroup params = new GeoTiffFormat().getWriteParameters();
+        params.parameter(AbstractGridFormat.GEOTOOLS_WRITE_PARAMS.getName().toString()).setValue(
+                DEFAULT_WRITE_PARAMS);
+        final GeneralParameterValue[] wps = (GeneralParameterValue[]) params.values().toArray(
+                new GeneralParameterValue[1]);
+        // write out the coverage
+        AbstractGridCoverageWriter writer = (AbstractGridCoverageWriter) format.getWriter(os);
+        if (writer == null)
+            throw new WPSException(
+                    "Could not find the GeoTIFF writer, please check it's in the classpath");
+        try {
+            writer.write(coverage, wps);
+        } finally {
+            try {
+                writer.dispose();
+            } catch (Exception e) {
+                // swallow
+            }
+        }
     }
 
 }
