5990eb3a5e2511cc5f57dff5d29c149508de16ed
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-12-09 18:10:28 +0000

    add ftp config options for idleTimeout, serverAddress, and passivePorts
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15150 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 6e5f4130ba0ae4893cf2d18228e0a3fc417e54cd [formerly eaf10a4ab813329d1fd407bedd48032e2ead3579]
    Former-commit-id: 36d8fbbd230ea01fb1d845e0253541ba7c1767f8

--- src/community/ftp/src/main/java/org/geoserver/ftp/FTPConfig.java
+++ src/community/ftp/src/main/java/org/geoserver/ftp/FTPConfig.java
@@ -4,6 +4,8 @@
  */
 package org.geoserver.ftp;
 
+import java.net.InetAddress;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
@@ -16,9 +18,21 @@ class FTPConfig {
 
     private static final Integer DEFAULT_FTP_PORT = 8021;
 
+    private static final Integer DEFAULT_IDLE_TIMEOUT_SECONDS = 10;
+
+    private static final String DEFAULT_PASSIVE_PORT_RANGE = "2300-";
+
+    public static final String ALL_SERVER_ADDRESSES_FLAG = "all";
+
     private boolean enabled;
 
     private int ftpPort;
+
+    private int idleTimeout;
+
+    private String serverAddress;
+
+    private String passivePorts;
 
     public FTPConfig() {
         setDefaults();
@@ -27,6 +41,9 @@ class FTPConfig {
     void setDefaults() {
         enabled = true;
         ftpPort = DEFAULT_FTP_PORT;
+        idleTimeout = DEFAULT_IDLE_TIMEOUT_SECONDS;
+        passivePorts = DEFAULT_PASSIVE_PORT_RANGE;
+        serverAddress = ALL_SERVER_ADDRESSES_FLAG;
     }
 
     public boolean isEnabled() {
@@ -48,5 +65,40 @@ class FTPConfig {
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
+    }
+
+    /**
+     * Returns the number of seconds during which no network activity is allowed before a session is
+     * closed due to inactivity, and defaults to 10 seconds.
+     */
+    public int getIdleTimeout() {
+        return idleTimeout;
+    }
+
+    /**
+     * Gets the {@link InetAddress} used for binding the local socket. Defaults to null, that is,
+     * the server binds to all available network interfaces
+     */
+    public String getServerAddress() {
+        return serverAddress;
+    }
+
+    /**
+     * Returns the passive ports to be used for data connections. Ports can be defined as single
+     * ports, closed or open ranges.
+     * <p>
+     * Multiple definitions can be separated by commas, for example:
+     * <ul>
+     * <li>2300 : only use port 2300 as the passive port</li>
+     * <li>2300-2399 : use all ports in the range</li>
+     * <li>2300- : use all ports larger than 2300</li>
+     * <li>2300, 2305, 2400- : use 2300 or 2305 or any port larger than 2400</li>
+     * </ul>
+     * 
+     * Defaults to using any available port > 1023
+     * </p>
+     */
+    public String getPassivePorts() {
+        return passivePorts;
     }
 }
