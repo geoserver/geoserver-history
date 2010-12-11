e7d638e0d9b6dffec360baddf1bd0418bb7f5c14
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-12-11 20:35:21 +0000

    write comments for the config file
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.0.x@15152 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 3b453829d12d7f8ebc4fd833a12469429f3d266a [formerly 0f19b65fb8b709a975bde1d5a50feca72cc1420e]
    Former-commit-id: dd20bd811ad8b9f189a3b035b5c03a25d14cc7d2

--- src/community/ftp/src/main/java/org/geoserver/ftp/FTPConfigLoader.java
+++ src/community/ftp/src/main/java/org/geoserver/ftp/FTPConfigLoader.java
@@ -10,10 +10,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.geoserver.config.util.XStreamPersister;
-import org.geoserver.config.util.XStreamPersisterFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 import org.geoserver.platform.GeoServerResourceLoader;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
+import com.thoughtworks.xstream.io.xml.QNameMap;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import com.thoughtworks.xstream.io.xml.StaxWriter;
 
 /**
  * Loads and saves the GeoSever FTP Service {@link FTPConfig configuration} from and to the
@@ -23,17 +33,12 @@ import org.geoserver.platform.GeoServerResourceLoader;
  * 
  */
 class FTPConfigLoader {
-
     private static final String CONFIG_FILE_NAME = "ftp.xml";
 
     private GeoServerResourceLoader resourceLoader;
 
-    private XStreamPersisterFactory persisterFactory;
-
-    public FTPConfigLoader(final GeoServerResourceLoader resourceLoader,
-            final XStreamPersisterFactory persisterFactory) {
+    public FTPConfigLoader(final GeoServerResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
-        this.persisterFactory = persisterFactory;
     }
 
     public FTPConfig load() {
@@ -46,8 +51,8 @@ class FTPConfigLoader {
                 return ftpConfig;
             }
             in = new FileInputStream(configFile);
-            XStreamPersister xmlPersister = getPersister();
-            FTPConfig config = xmlPersister.load(in, FTPConfig.class);
+            XStream xs = getPersister();
+            FTPConfig config = (FTPConfig) xs.fromXML(in);
             return config;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -62,10 +67,36 @@ class FTPConfigLoader {
         }
     }
 
-    private XStreamPersister getPersister() {
-        XStreamPersister xmlPersister = persisterFactory.createXMLPersister();
-        xmlPersister.getXStream().alias("ftp", FTPConfig.class);
-        return xmlPersister;
+    private XStream getPersister() {
+        Map<String, String> comments = new HashMap<String, String>();
+        comments.put("enabled", "true to enable the FTP service, false to disable it");
+        comments.put("ftpPort", "Port where the FTP Service listens for connections");
+        comments.put("idleTimeout", "number of seconds during which no network activity "
+                + "is allowed before a session is closed due to inactivity");
+        comments.put("serverAddress",
+                "IP Address used for binding the local socket. If unset, the server binds "
+                        + "to all available network interfaces");
+        comments.put("passivePorts", "Ports to be used for PASV data connections. Ports can "
+                + "be defined as single ports, closed or open ranges:\n"
+                + " Multiple definitions can be separated by commas, for example:\n"
+                + "  2300 : only use port 2300 as the passive port\n"
+                + "  2300-2399 : use all ports in the range\n"
+                + "  2300- : use all ports larger than 2300\n"
+                + "  2300, 2305, 2400- : use 2300 or 2305 or any port larger than 2400\n");
+
+        comments.put("passiveAddress",
+                "Address on which the server will listen to passive data connections, \n"
+                        + "  if not set defaults to the same address as the control "
+                        + "socket for the session.");
+        comments.put(
+                "passiveExternalAddress",
+                "the address the server will claim to be listening on in the PASV reply.\n"
+                        + " Useful when the server is behind a NAT firewall and the client sees a different address than the server is using.");
+
+        HierarchicalStreamDriver streamDriver = new CommentingStaxWriter(comments);
+        XStream xStream = new XStream(streamDriver);
+        xStream.alias("ftp", FTPConfig.class);
+        return xStream;
     }
 
     public void save(FTPConfig config) {
@@ -74,8 +105,8 @@ class FTPConfigLoader {
         try {
             configFile = findConfigFile();
             out = new FileOutputStream(configFile);
-            XStreamPersister xmlPersister = getPersister();
-            xmlPersister.save(config, out);
+            XStream xs = getPersister();
+            xs.toXML(config, new OutputStreamWriter(out, "UTF-8"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
@@ -96,4 +127,70 @@ class FTPConfigLoader {
         }
         return configFile;
     }
+
+    private static final class CommentingStaxWriter extends StaxDriver {
+        final QNameMap qnameMap = new QNameMap();
+
+        private Map<String, String> comments;
+
+        public CommentingStaxWriter(Map<String, String> elementComments) {
+            this.comments = elementComments;
+        }
+
+        @Override
+        public StaxWriter createStaxWriter(XMLStreamWriter out, boolean writeStartEndDocument)
+                throws XMLStreamException {
+
+            return new StaxWriter(qnameMap, out, writeStartEndDocument, isRepairingNamespace(),
+                    xmlFriendlyReplacer()) {
+
+                private int indentLevel = 0;
+
+                @Override
+                public void startNode(final String elementName) {
+                    XMLStreamWriter xmlStreamWriter = super.getXMLStreamWriter();
+                    try {
+                        xmlStreamWriter.writeCharacters("\n");
+                        String comment = comments.get(elementName);
+                        if (comments != null) {
+                            indent(xmlStreamWriter);
+                            xmlStreamWriter.writeComment(comment);
+                            xmlStreamWriter.writeCharacters("\n");
+                            indent(xmlStreamWriter);
+                        }
+                    } catch (XMLStreamException ignored) {
+                        // shouldn't happen
+                    }
+                    super.startNode(elementName);
+                    indentLevel++;
+                }
+
+                private void indent(XMLStreamWriter xmlStreamWriter) throws XMLStreamException {
+                    for (int i = 0; i < indentLevel; i++) {
+                        xmlStreamWriter.writeCharacters("  ");
+                    }
+                }
+
+                @Override
+                public void endNode() {
+                    indentLevel--;
+                    System.err.println("indentLevel: " + indentLevel);
+                    if (indentLevel == 0) {
+                        XMLStreamWriter xmlStreamWriter = super.getXMLStreamWriter();
+                        try {
+                            xmlStreamWriter.writeCharacters("\n");
+                            super.endNode();
+                            xmlStreamWriter.writeCharacters("\n");
+                        } catch (XMLStreamException ignored) {
+                            // shouldn't happen
+                        }
+                    } else {
+                        super.endNode();
+                    }
+                }
+            };
+
+        }
+    }
+
 }
