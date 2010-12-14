e664ce7e171ec0b675e5f83bd46ba5a0ec779724
mbarto <mbarto@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-12-14 08:15:59 +0000

    imagemap extension: fixed DPI used for scale calculation, used 90 as specified by OGC standards
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15166 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: b0e6e822e416fe801257c17b34ead5aa01bedfe4 [formerly 676f36e9bd4e244361195c8148c39210f91c487d]
    Former-commit-id: 4f197cba584269ed689588572a791818baced9c6

--- src/extension/imagemap/src/main/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/EncodeHTMLImageMap.java
+++ src/extension/imagemap/src/main/java/org/vfny/geoserver/wms/responses/map/htmlimagemap/EncodeHTMLImageMap.java
@@ -249,7 +249,7 @@ public class EncodeHTMLImageMap extends WebMap{
     		Rule rule=rules[count];
     		double scaleDenominator;
 			try {
-				scaleDenominator = RendererUtilities.calculateScale(mapContext.getAreaOfInterest(), mapContext.getMapWidth(), mapContext.getMapHeight(),100);
+				scaleDenominator = RendererUtilities.calculateScale(mapContext.getAreaOfInterest(), mapContext.getMapWidth(), mapContext.getMapHeight(),90);
 			
 	            //is this rule within scale?
 	            if (EncodeHTMLImageMap.isWithInScale(rule,scaleDenominator)) {
