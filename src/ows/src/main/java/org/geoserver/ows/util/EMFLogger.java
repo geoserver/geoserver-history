855e474b0674979c666de01221ca0963afebf616
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2010-12-16 19:27:07 +0000

    created a request logging interceptor for non emf based request objects
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15170 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: c86d6c318986d6d03108b54816d1c21b965a1f72 [formerly f69aec19fc211d21f3fb19d86772bcb7cdd13ad0]
    Former-commit-id: 5d465189cecff76f3755684302470bc00eb2e569

--- src/ows/src/main/java/org/geoserver/ows/util/EMFLogger.java
+++ src/ows/src/main/java/org/geoserver/ows/util/EMFLogger.java
@@ -6,10 +6,7 @@ package org.geoserver.ows.util;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Logger;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
@@ -17,45 +14,19 @@ import org.eclipse.emf.ecore.EStructuralFeature;
  * A generic service object invocation logger based on EMF reflection
  * @author Justin DeOliveira, TOPP
  */
-public class EMFLogger implements MethodInterceptor {
-    /**
-     * Logging instance
-     */
-    Logger logger;
+public class EMFLogger extends RequestObjectLogger {
     
     public EMFLogger(String logPackage) {
-        logger = org.geotools.util.logging.Logging.getLogger(logPackage);
+        super(logPackage);
     }
 
-    public Object invoke(MethodInvocation invocation) throws Throwable {
-        StringBuffer log = new StringBuffer();
-        log.append("\n" + "Request: " + invocation.getMethod().getName());
-
-        if (invocation.getArguments().length > 0) {
-            EObject requestBean = null;
-
-            for (int i = 0; i < invocation.getArguments().length; i++) {
-                Object argument = (Object) invocation.getArguments()[i];
-
-                if (argument instanceof EObject) {
-                    requestBean = (EObject) argument;
-
-                    break;
-                }
-            }
-
-            if (requestBean != null) {
-                log(requestBean, 1, log);
-            }
-        }
-
-        Object result = invocation.proceed();
-        logger.info(log.toString());
-
-        return result;
-    }
-
-    void log(EObject object, int level, StringBuffer log) {
+    @Override
+    protected boolean isRequestObject(Object obj) {
+        return obj instanceof EObject;
+    }    
+    
+    protected void log(Object obj, int level, StringBuffer log) {
+        EObject object = (EObject) obj;
         List properties = object.eClass().getEAllStructuralFeatures();
 
         for (Iterator p = properties.iterator(); p.hasNext();) {
