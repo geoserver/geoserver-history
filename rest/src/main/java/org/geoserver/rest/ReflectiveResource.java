151a7277f142b184d8ff3980a14595b6812d26d7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-01-11 18:07:40 +0000

    added convenience method to ReflectiveResource for looking up query string value
    git-svn-id: http://svn.codehaus.org/geoserver/trunk/src@15242 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 1e365b6a2de6d7eebeb09465da103ab304ea2033 [formerly 2b880a679dd1578a9856549f3b355ee3971dfcbf]
    Former-commit-id: 8c59deb7a9c067f1953cbb5198141661cc1ea735

--- rest/src/main/java/org/geoserver/rest/ReflectiveResource.java
+++ rest/src/main/java/org/geoserver/rest/ReflectiveResource.java
@@ -22,6 +22,7 @@ import org.restlet.data.Status;
 
 import com.thoughtworks.xstream.XStream;
 import org.geoserver.rest.util.RESTUtils;
+import org.geotools.util.Converters;
 
 /**
  * Base class for resources which work reflectively from underlying target objects.
@@ -325,6 +326,42 @@ public abstract class ReflectiveResource extends AbstractResource {
      */
     protected String getAttribute(String attribute) {
         return RESTUtils.getAttribute(getRequest(), attribute);
+    }
+    
+    /**
+     * Convenience method for subclasses to look up the (URL-decoded) value of a value specified
+     * in the request query string.
+     * 
+     * @param key The name of the value to lookup. 
+     * 
+     * @return The converted value, or <code>null</code> if the value was not specified.
+     */
+    protected String getQueryStringValue(String key) {
+        return RESTUtils.getQueryStringValue(getRequest(), key);
+    }
+    
+    /**
+     * Convenience method for subclasses to look up the (URL-decoded) value of a value specified
+     * in the request query string, converting to the specified type.
+     * 
+     * @param key The name of the value to lookup. 
+     * @param clazz The class to convert to.
+     * @param defalt The default value to return if the attribute does not exist or no 
+     *   conversion was possible. May be <code>null</code>
+     * 
+     * @return The converted value, or <code>null</code> if either (a) the value was not 
+     *   specified or (b) the value could not be converted to the specified type.
+     */
+    protected <T> T getQueryStringValue(String key, Class<T> clazz, T defalt) {
+        String value = getQueryStringValue(key);
+        if (value != null) {
+            T converted = Converters.convert(value, clazz);
+            if (converted != null) {
+                return converted;
+            }
+        }
+
+        return defalt;
     }
     
     /**
