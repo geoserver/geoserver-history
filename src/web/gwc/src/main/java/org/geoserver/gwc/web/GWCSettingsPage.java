15d7090608e7b9ed1cf9b37d0943ee050ba495d4
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-01-12 02:15:53 +0000

    added global quota enablement config
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15247 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 096762c384070512c9a424c49c0efaf8050bc6c2 [formerly 9ec9e470705a07e7ec50d558e4d516961a5cd4d5]
    Former-commit-id: dc44deb471498f882695f318b9a4020a8b26f6f0

--- src/web/gwc/src/main/java/org/geoserver/gwc/web/GWCSettingsPage.java
+++ src/web/gwc/src/main/java/org/geoserver/gwc/web/GWCSettingsPage.java
@@ -2,12 +2,17 @@ package org.geoserver.gwc.web;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
+import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.Radio;
+import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.geoserver.catalog.MetadataMap;
@@ -17,11 +22,19 @@ import org.geoserver.web.GeoServerHomePage;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.util.MapModel;
 import org.geoserver.wms.WMSInfo;
+import org.geowebcache.diskquota.DiskQuotaConfig;
+import org.geowebcache.diskquota.ExpirationPolicy;
+import org.geowebcache.diskquota.Quota;
+import org.geowebcache.diskquota.StorageUnit;
 
 public class GWCSettingsPage extends GeoServerSecuredPage {
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @SuppressWarnings({ "unchecked", "rawtypes", "deprecation" })
     public GWCSettingsPage() {
+
+        add(HeaderContributor.forCss(GWCSettingsPage.class, "statusbar.css"));
+        setHeaderPanel(headerPanel());
+
         Form form = new Form("form");
         add(form);
 
@@ -33,18 +46,26 @@ public class GWCSettingsPage extends GeoServerSecuredPage {
             }
         };
 
+        final IModel<DiskQuotaConfig> diskQuotaModel = new LoadableDetachableModel<DiskQuotaConfig>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected DiskQuotaConfig load() {
+                final GWC gwc = getGWC();
+                DiskQuotaConfig quotaConfig = gwc.getDisQuotaConfig();
+                return quotaConfig;
+            }
+        };
         PropertyModel<MetadataMap> metadataModel = new PropertyModel<MetadataMap>(wmsInfoModel,
                 "metadata");
-
         IModel<Boolean> wmsIntegrationEnabledModel = new MapModel(metadataModel,
                 GWC.WMS_INTEGRATION_ENABLED_KEY);
 
-        setHeaderPanel(headerPanel());
-
-        CheckBox wmsIntegration = new CheckBox("enableWMSIntegration", wmsIntegrationEnabledModel);
-        wmsIntegration.add(new AttributeModifier("title", true, new StringResourceModel(
-                "GWCSettingsPage.enableWMSIntegration.title", (Component) null, null)));
+        CheckBox wmsIntegration = checkbox("enableWMSIntegration", wmsIntegrationEnabledModel,
+                "GWCSettingsPage.enableWMSIntegration.title");
         form.add(wmsIntegration);
+
+        addDiskQuotaPanel(form, diskQuotaModel);
 
         form.add(new Button("submit") {
             private static final long serialVersionUID = 1L;
@@ -54,6 +75,11 @@ public class GWCSettingsPage extends GeoServerSecuredPage {
                 GeoServer gs = getGeoServer();
                 WMSInfo wmsInfo = wmsInfoModel.getObject();
                 gs.save(wmsInfo);
+
+                // DiskQuotaConfig diskQuotaConfig = diskQuotaModel.getObject();
+                GWC gwc = getGWC();
+                gwc.saveDiskQuotaConfig();
+
                 setResponsePage(GeoServerHomePage.class);
             }
         });
@@ -68,10 +94,112 @@ public class GWCSettingsPage extends GeoServerSecuredPage {
 
     }
 
+    private void addDiskQuotaPanel(final Form form, final IModel<DiskQuotaConfig> diskQuotaModel) {
+        // quotaConfig.getCacheCleanUpFrequency();
+        // quotaConfig.getCacheCleanUpUnits();
+        // quotaConfig.getDiskBlockSize();
+        // quotaConfig.getGlobalExpirationPolicy();
+        // quotaConfig.getGlobalExpirationPolicyName();
+        // quotaConfig.getGlobalQuota();
+        // quotaConfig.getGlobalUsedQuota();
+        // quotaConfig.getMaxConcurrentCleanUps();
+        // quotaConfig.getNumLayers();
+
+        final DiskQuotaConfig diskQuotaConfig = diskQuotaModel.getObject();
+
+        IModel<Boolean> quotaEnablementModel = new PropertyModel<Boolean>(diskQuotaModel, "enabled");
+        CheckBox diskQuotaIntegration = checkbox("enableDiskQuota", quotaEnablementModel,
+                "GWCSettingsPage.enableDiskQuota.title");
+        form.add(diskQuotaIntegration);
+
+        IModel<ExpirationPolicy> globalQuotaPolicyModel = new PropertyModel<ExpirationPolicy>(
+                diskQuotaModel, "globalExpirationPolicy");
+
+        RadioGroup<ExpirationPolicy> globalQuota = new RadioGroup<ExpirationPolicy>("globalQuota",
+                globalQuotaPolicyModel);
+        form.add(globalQuota);
+
+        IModel<ExpirationPolicy> lfuModel = new LoadableDetachableModel<ExpirationPolicy>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected ExpirationPolicy load() {
+                return getGWC().getExpirationPolicy("LFU");
+            }
+        };
+        IModel<ExpirationPolicy> lruModel = new LoadableDetachableModel<ExpirationPolicy>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected ExpirationPolicy load() {
+                return getGWC().getExpirationPolicy("LRU");
+            }
+        };
+
+        Radio<ExpirationPolicy> globalQuotaPolicyLFU;
+        Radio<ExpirationPolicy> globalQuotaPolicyLRU;
+        globalQuotaPolicyLFU = new Radio<ExpirationPolicy>("globalQuotaPolicyLFU", lfuModel);
+        globalQuotaPolicyLRU = new Radio<ExpirationPolicy>("globalQuotaPolicyLRU", lruModel);
+
+        globalQuota.add(globalQuotaPolicyLFU);
+        globalQuota.add(globalQuotaPolicyLRU);
+
+        WebMarkupContainer green = new WebMarkupContainer("statusBarGreen");
+        WebMarkupContainer red = new WebMarkupContainer("statusBarRed");
+
+        final Quota limit = diskQuotaConfig.getGlobalQuota() == null ? new Quota(1, StorageUnit.MiB)
+                : new Quota(diskQuotaConfig.getGlobalQuota());
+        final Quota used = new Quota(diskQuotaConfig.getGlobalUsedQuota());
+        final Quota excess = used.difference(limit);
+        int usedPercentage;
+        int excessPercentage;
+
+        final int progressWidth = 200;
+
+        StorageUnit unit;
+        if (excess.getValue().doubleValue() > 0) {
+            StorageUnit excessUnits = excess.getUnits();
+            StorageUnit usedUnits = used.getUnits();
+            double usedValue = used.getValue().doubleValue();
+            double excessValue = excessUnits.convertTo(excess.getValue(), usedUnits).doubleValue();
+            excessPercentage = (int) Math.round((excessValue * progressWidth) / usedValue);
+            usedPercentage = progressWidth - excessPercentage;
+        } else {
+            unit = limit.getUnits();
+            double usedValue = used.getUnits().convertTo(used.getValue(), unit).doubleValue();
+            double limitValue = limit.getValue().doubleValue();
+            usedPercentage = (int) Math.round(usedValue * progressWidth / limitValue);
+            excessPercentage = 0;
+        }
+
+        green.add(new AttributeModifier("style", true, new Model<String>("width: " + usedPercentage
+                + "px;")));
+
+        String redStyle = "width: " + excessPercentage + "px; left: "
+                + (1 + progressWidth - excessPercentage) + "px;";
+        red.add(new AttributeModifier("style", true, new Model<String>(redStyle)));
+
+        form.add(green);
+        form.add(red);
+    }
+
+    private GWC getGWC() {
+        final GWC gwc = (GWC) getGeoServerApplication().getBean("gwcFacade");
+        return gwc;
+    }
+
     protected Component headerPanel() {
         Fragment header = new Fragment(HEADER_PANEL, "header", this);
 
         return header;
     }
 
+    private CheckBox checkbox(String id, IModel<Boolean> model, String titleKey) {
+        CheckBox checkBox = new CheckBox(id, model);
+        if (null != titleKey) {
+            checkBox.add(new AttributeModifier("title", true, new StringResourceModel(titleKey,
+                    (Component) null, null)));
+        }
+        return checkBox;
+    }
 }
