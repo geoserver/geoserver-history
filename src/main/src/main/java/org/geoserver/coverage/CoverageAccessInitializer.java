7bc03b4fac21001027ac0479da7f5415453f4829
dany111 <dany111@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-02-11 13:19:50 +0000

    GEOS-4370
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.1.x@15423 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a084eee57eb119cadff4960233f1ff8ba0e05c15 [formerly ed5363d6fd151a7bc17b0f9caa516d4732cc4c40]
    Former-commit-id: 65f0c9cb68fd2a182861543cb549ce570ffc5782

--- src/main/src/main/java/org/geoserver/coverage/CoverageAccessInitializer.java
+++ src/main/src/main/java/org/geoserver/coverage/CoverageAccessInitializer.java
@@ -13,11 +13,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.geoserver.config.ConfigurationListenerAdapter;
 import org.geoserver.config.CoverageAccessInfo;
+import org.geoserver.config.CoverageAccessInfo.QueueType;
 import org.geoserver.config.GeoServer;
 import org.geoserver.config.GeoServerInfo;
 import org.geoserver.config.GeoServerInitializer;
-import org.geoserver.config.CoverageAccessInfo.QueueType;
 import org.geoserver.config.impl.CoverageAccessInfoImpl;
+import org.geotools.factory.GeoTools;
+import org.geotools.factory.Hints;
 
 /**
  * Initializes Coverage Access settings from configuration.
@@ -52,6 +54,15 @@ public class CoverageAccessInitializer implements GeoServerInitializer {
             
             //First initialization
             if (executor == null){
+            	final Hints defHints = GeoTools.getDefaultHints();
+            	
+            	//Looking for executor from default Hints
+            	if (defHints != null && defHints.containsKey(Hints.EXECUTOR_SERVICE)){
+            		executor = (ThreadPoolExecutor) defHints.get(Hints.EXECUTOR_SERVICE);
+            	} 
+            }
+            if (executor == null){
+                // No Executor found: Create a new one
                 executor = new ThreadPoolExecutor(coverageAccess.getCorePoolSize(), 
                         coverageAccess.getMaxPoolSize(), coverageAccess.getKeepAliveTime(), TimeUnit.MILLISECONDS, 
                         coverageAccess.getQueueType() == QueueType.UNBOUNDED ? new LinkedBlockingQueue<Runnable>() : new SynchronousQueue<Runnable>());
@@ -71,7 +82,8 @@ public class CoverageAccessInitializer implements GeoServerInitializer {
                    (queue instanceof SynchronousQueue && queueType == QueueType.DIRECT) ){
                     executor.setCorePoolSize(coverageAccess.getCorePoolSize());
                     executor.setMaximumPoolSize(coverageAccess.getMaxPoolSize());
-                    executor.setKeepAliveTime(coverageAccess.getKeepAliveTime(), TimeUnit.MILLISECONDS);    
+                    executor.setKeepAliveTime(coverageAccess.getKeepAliveTime(), TimeUnit.MILLISECONDS);
+                    coverageAccess.setThreadPoolExecutor(executor);
                 } else {
                     
                     // The queue type has been changed. Initializing a new ThreadPoolExecutor by
