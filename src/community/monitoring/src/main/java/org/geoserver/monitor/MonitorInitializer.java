ab042ff6d0de1c795ee1f5fe7fa4f2cc4d5d1fd6
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-02-16 16:31:27 +0000

    ensuring an active session when updating interupted requests
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15437 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: a1997a065e4cc7ca62acf6e228b749f1f23321f1 [formerly 55e2654063a77a2a954c73e96fb13437e36e3a20]
    Former-commit-id: 4ea880d68255cb7238829f11dc57035fcb2ab255

--- src/community/monitoring/src/main/java/org/geoserver/monitor/MonitorInitializer.java
+++ src/community/monitoring/src/main/java/org/geoserver/monitor/MonitorInitializer.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 import org.geoserver.config.GeoServer;
 import org.geoserver.config.GeoServerInitializer;
+import org.geoserver.hibernate.HibUtil;
 import org.geoserver.monitor.Query.Comparison;
 import org.geoserver.monitor.RequestData.Status;
+import org.geoserver.monitor.hib.HibernateMonitorDAO2;
+import org.hibernate.SessionFactory;
 
 public class MonitorInitializer implements GeoServerInitializer {
 
@@ -16,14 +19,28 @@ public class MonitorInitializer implements GeoServerInitializer {
     }
     
     public void initialize(GeoServer geoServer) throws Exception {
-        //clear out any requests that were left in an inconsistent state
-        Query query = new Query().filter("status",
-            Arrays.asList(Status.RUNNING, Status.WAITING, Status.CANCELLING), Comparison.IN);
-        for (RequestData data : monitor.getDAO().getRequests(query)) {
-            if (InternalHostname.get().equals(data.getInternalHost())) {
-                //mark start as INTERRUPTED
-                data.setStatus(Status.INTERRUPTED);
-                monitor.getDAO().save(data);
+        //special case for hibernate, we need to have a session in order to make this work
+        SessionFactory sessionFactory = null;
+        if (monitor.getDAO() instanceof HibernateMonitorDAO2) {
+            sessionFactory = ((HibernateMonitorDAO2)monitor.getDAO()).getSessionFactory();
+            HibUtil.setUpSession(sessionFactory);
+        }
+        
+        try {
+            //clear out any requests that were left in an inconsistent state
+            Query query = new Query().filter("status",
+                Arrays.asList(Status.RUNNING, Status.WAITING, Status.CANCELLING), Comparison.IN);
+            for (RequestData data : monitor.getDAO().getRequests(query)) {
+                if (InternalHostname.get().equals(data.getInternalHost())) {
+                    //mark start as INTERRUPTED
+                    data.setStatus(Status.INTERRUPTED);
+                    monitor.getDAO().save(data);
+                }
+            }
+        }
+        finally {
+            if (sessionFactory != null) {
+                HibUtil.tearDownSession(sessionFactory, null);
             }
         }
     }
