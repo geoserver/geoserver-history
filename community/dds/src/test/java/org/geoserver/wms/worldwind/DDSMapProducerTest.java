25d565e7bb28de9b3dc03b2ea215030c1b5ef762
whatnick <whatnick@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-03-01 10:30:37 +0000

    GEOS-3586: Fixed up test setup with test scopes in pom (DDS Test clones GIF Test)
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.1.x/src@15496 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 5935daf0d313e4f1ce127ed5c65ec1bd904b803e [formerly de22ce94543de0ae4ffa13a377fd279af6b9d973]
    Former-commit-id: 0165fc8d5909f0851ba3a5fbe4bb4c49400056de

--- community/dds/src/test/java/org/geoserver/wms/worldwind/DDSMapProducerTest.java
+++ community/dds/src/test/java/org/geoserver/wms/worldwind/DDSMapProducerTest.java
@@ -1,48 +1,20 @@
 package org.geoserver.wms.worldwind;
 
-import java.awt.Color;
-import java.util.logging.Logger;
-
 import junit.framework.Test;
 
-public class DDSMapProducerTest extends DefaultRasterMapProducerTest {
+import org.geoserver.wms.map.RenderedImageMapOutputFormat;
+import org.geoserver.wms.map.RenderedImageMapOutputFormatTest;
 
-	/** DOCUMENT ME! */
-	private static final Logger LOGGER = org.geotools.util.logging.Logging
-			.getLogger(DDSMapProducerTest.class.getPackage().getName());
-
-	private DefaultRasterMapProducer rasterMapProducer;
-	
-	/** DOCUMENT ME! */
-    private static final Color BG_COLOR = Color.white;
-    
-    /** DOCUMENT ME! */
-    private String mapFormat = "image/dds";
+public class DDSMapProducerTest extends RenderedImageMapOutputFormatTest {
 
 	/**
-	 * This is a READ ONLY TEST so we can use one time setup
-	 */
-	public static Test suite() {
-		return new OneTimeTestSetup(new DDSMapProducerTest());
-	}
+     * This is a READ ONLY TEST so we can use one time setup
+     */
+    public static Test suite() {
+        return new OneTimeTestSetup(new DDSMapProducerTest());
+    }
 
-	protected DefaultRasterMapProducer getProducerInstance() {
-		return new DDSMapProducer(getWMS());
-	}
-
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @throws Exception
-	 *             DOCUMENT ME!
-	 */
-	public void setUpInternal() throws Exception {
-	    super.setUpInternal();
-	    this.rasterMapProducer = getProducerInstance();
-	}
-	
-	public String getMapFormat()
-	{
-		return this.mapFormat;
-	}
+    protected RenderedImageMapOutputFormat getProducerInstance() {
+        return new RenderedImageMapOutputFormat("image/dds", getWMS());
+    }
 }
