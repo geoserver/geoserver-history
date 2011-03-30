9762880c918eafbd7b083aee4c72c81236a251c7
jdeolive <jdeolive@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-03-30 18:10:20 +0000

    allowing for multiple db configurations to fall back on
    git-svn-id: http://svn.codehaus.org/geoserver/trunk@15613 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 8be23b8711a62d8b6dc5877de89f3e93ce6a7ec4 [formerly 03f59d25315ebfcf01346d176f3c3d4d4fa37a20]
    Former-commit-id: 660cacf25137bf4bc77c18708823585c39060a92

--- src/community/monitoring/src/main/java/org/geoserver/monitor/hib/MonitoringDataSource.java
+++ src/community/monitoring/src/main/java/org/geoserver/monitor/hib/MonitoringDataSource.java
@@ -11,14 +11,11 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.io.IOUtils;
 import org.geoserver.config.GeoServerDataDirectory;
 import org.geoserver.monitor.MonitorConfig;
-import org.geotools.util.logging.Logging;
 
 public class MonitoringDataSource extends BasicDataSource {
 
@@ -45,53 +42,98 @@ public class MonitoringDataSource extends BasicDataSource {
             }
             return super.getConnection();
         }
-        catch(SQLException e) {
+        catch(Exception e) {
             //LOGGER.log(Level.WARNING, "Database connection error", e);
             config.setError(e);
             config.setEnabled(false);
-            throw e;
+            
+            if (e instanceof SQLException) {
+                throw (SQLException) e;
+            }
+            
+            throw new SQLException(e);
         }
     }
 
-    void initializeDataSource() {
-        try {
-            File monitoringDir = dataDirectory.findOrCreateDataDir("monitoring");
-            File dbprops = new File(monitoringDir, "db.properties");
-            Properties db = new Properties();
-        
-            if (!dbprops.exists()) {
-                //use a default, and copy the template over
-                InputStream in = getClass().getResourceAsStream("db.properties");
-                IOUtils.copy(in, new FileOutputStream(dbprops));
+    void initializeDataSource() throws Exception {
+        File monitoringDir = dataDirectory.findOrCreateDataDir("monitoring");
+        File dbprops = new File(monitoringDir, "db.properties");
+        if (dbprops.exists()) {
+            //attempt to configure
+            try {
+                configureDataSource(dbprops, monitoringDir);
+            }
+            catch(SQLException e) {
+                //configure failed, try db1.properties
+                dbprops = new File(monitoringDir, "db1.properties");
+                if (dbprops.exists()) {
+                    try {
+                        configureDataSource(dbprops, monitoringDir);
+                        
+                        //secondary file worked, return
+                        return;
+                    }
+                    catch(SQLException e1) {
+                        //secondary file failed as well, try for third
+                        dbprops = new File(monitoringDir, "db2.properties");
+                        if (dbprops.exists()) {
+                            try {
+                                configureDataSource(dbprops, monitoringDir);
+                                
+                                //third file worked, return
+                                return;
+                            }
+                            catch(SQLException e2) {}
+                        }
+                    }
+                }
                 
-                db.load(getClass().getResourceAsStream("db.properties"));
+                throw e;
             }
-            else {
-                FileInputStream in = new FileInputStream(dbprops);
-                db.load(in);
-                in.close();
-            }
-            
-            //TODO: check for nulls
-            setDriverClassName(db.getProperty("driver"));
-            setUrl(getURL(db));
-            
-            if (db.containsKey("username")) {
-                setUsername(db.getProperty("username"));
-            }
-            if (db.containsKey("password")) {
-                setPassword(db.getProperty("password"));
-            }
-            
-            setDefaultAutoCommit(Boolean.valueOf(db.getProperty("defaultAutoCommit", "true")));
-            
-            //TODO: make other parameters configurable
-            setMinIdle(1);
-            setMaxActive(4);
-        } 
-        catch (Exception e) {
-            throw new RuntimeException("Error occured configuring database");
         }
+        else {
+            //no db.properties file, use internal default
+            configureDataSource(null, monitoringDir);
+        }
+    }
+    
+    void configureDataSource(File dbprops, File monitoringDir) throws Exception {
+        Properties db = new Properties();
+
+        if (dbprops == null) {
+            dbprops = new File(monitoringDir, "db.properties");
+            
+            //use a default, and copy the template over
+            InputStream in = getClass().getResourceAsStream("db.properties");
+            IOUtils.copy(in, new FileOutputStream(dbprops));
+            
+            db.load(getClass().getResourceAsStream("db.properties"));
+        }
+        else {
+            FileInputStream in = new FileInputStream(dbprops);
+            db.load(in);
+            in.close();
+        }
+        
+        //TODO: check for nulls
+        setDriverClassName(db.getProperty("driver"));
+        setUrl(getURL(db));
+        
+        if (db.containsKey("username")) {
+            setUsername(db.getProperty("username"));
+        }
+        if (db.containsKey("password")) {
+            setPassword(db.getProperty("password"));
+        }
+        
+        setDefaultAutoCommit(Boolean.valueOf(db.getProperty("defaultAutoCommit", "true")));
+        
+        //TODO: make other parameters configurable
+        setMinIdle(1);
+        setMaxActive(4);
+        
+        //test the connection
+        super.getConnection();
     }
     
     String getURL(Properties db) {
