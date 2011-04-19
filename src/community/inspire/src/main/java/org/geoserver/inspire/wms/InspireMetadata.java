22b94063ecdcb891855722fc12868d739d193eaa
groldan <groldan@ef1d6e69-97e5-0310-af46-8a06194da32a>
2011-04-19 19:24:47 +0000

    adapt WMS INSPIRE extension to current http://inspire.ec.europa.eu/schemas/inspire_vs/1.0 namespace and schema
    git-svn-id: http://svn.codehaus.org/geoserver/branches/2.1.x@15712 ef1d6e69-97e5-0310-af46-8a06194da32a
    
    Former-commit-id: 09b6439c0a7fc742fabcdbc50db703e1bc246ca1 [formerly a8cac28ba9742a592c13c949276c0fa508a5069e]
    Former-commit-id: 4a23d275960e8a702f02cace617a78a3519eecbc

--- src/community/inspire/src/main/java/org/geoserver/inspire/wms/InspireMetadata.java
+++ src/community/inspire/src/main/java/org/geoserver/inspire/wms/InspireMetadata.java
@@ -1,10 +1,10 @@
 package org.geoserver.inspire.wms;
 
 public enum InspireMetadata {
-    LANGUAGE("inspire.language"),
-    METADATA_URL("inspire.metadataURL");
+    LANGUAGE("inspire.language"), METADATA_URL("inspire.metadataURL");
 
     public String key;
+
     private InspireMetadata(String key) {
         this.key = key;
     }
--- src/community/inspire/src/test/java/org/geoserver/inspire/wms/WMSExtendedCapabilitiesTest.java
+++ src/community/inspire/src/test/java/org/geoserver/inspire/wms/WMSExtendedCapabilitiesTest.java
@@ -1,12 +1,11 @@
 package org.geoserver.inspire.wms;
 
+import static org.geoserver.inspire.wms.WMSExtendedCapabilitiesProvider.NAMESPACE;
+
 import org.geoserver.test.GeoServerTestSupport;
 import org.geoserver.wms.WMSInfo;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import static org.geoserver.inspire.wms.WMSExtendedCapabilitiesProvider.NAMESPACE;
 
 public class WMSExtendedCapabilitiesTest extends GeoServerTestSupport {
 
@@ -16,22 +15,34 @@ public class WMSExtendedCapabilitiesTest extends GeoServerTestSupport {
         wms.getMetadata().put(InspireMetadata.LANGUAGE.key, "fre");
         wms.getMetadata().put(InspireMetadata.METADATA_URL.key, "http://foo.com?bar=baz");
         getGeoServer().save(wms);
-        
+
         Document dom = getAsDOM("wms?request=getcapabilities");
         assertEquals(NAMESPACE, dom.getDocumentElement().getAttribute("xmlns:inspire_vs"));
-        
-        Element e = getFirstElementByTagName(dom, "inspire_vs:ExtendedCapabilities");
-        assertNotNull(e);
-        
-        Element f = getFirstElementByTagName(e, "inspire_vs:MetadataUrl");
-        assertNotNull(f);
-        
-        f = getFirstElementByTagName(f, "gmd:URL");
-        assertNotNull(f);
-        assertEquals("http://foo.com?bar=baz", f.getFirstChild().getNodeValue());
-        
-        f = getFirstElementByTagName(e, "inspire_vs:Language");
-        assertNotNull(f);
-        assertEquals("fre", f.getFirstChild().getNodeValue());
+
+        final Element extendedCaps = getFirstElementByTagName(dom,
+                "inspire_vs:ExtendedCapabilities");
+        assertNotNull(extendedCaps);
+
+        final Element mdUrl = getFirstElementByTagName(extendedCaps, "inspire_common:MetadataUrl");
+        assertNotNull(mdUrl);
+
+        final Element url = getFirstElementByTagName(mdUrl, "inspire_common:URL");
+        assertNotNull(url);
+        assertEquals("http://foo.com?bar=baz", url.getFirstChild().getNodeValue());
+
+        final Element suppLangs = getFirstElementByTagName(extendedCaps,
+                "inspire_common:SupportedLanguages");
+        assertNotNull(suppLangs);
+        final Element defLang = getFirstElementByTagName(suppLangs,
+                "inspire_common:DefaultLanguage");
+        assertNotNull(defLang);
+        final Element defLangVal = getFirstElementByTagName(defLang, "inspire_common:Language");
+        assertEquals("fre", defLangVal.getFirstChild().getNodeValue());
+
+        final Element respLang = getFirstElementByTagName(extendedCaps,
+                "inspire_common:ResponseLanguage");
+        assertNotNull(respLang);
+        final Element respLangVal = getFirstElementByTagName(defLang, "inspire_common:Language");
+        assertEquals("fre", respLangVal.getFirstChild().getNodeValue());
     }
 }
